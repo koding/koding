50647bebc53cbca59df9bc11b448a91c68708959
Christopher Thorn <cthorn@web0.beta.system.aws.koding.com>
2012-06-26 17:48:40 -0700

    work

--- website/1.0/routes.php
+++ website/1.0/routes.php
@@ -124,6 +124,6 @@ $router->add_route('/channel/auth', function () {
   trace('-------', $input);
   $session = get_session();
   
-  trace('chris', $_GET, $channel_name, $session);
+  trace('chris', $_POST, $channel_name, $session);
   
 });
