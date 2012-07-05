3ce12c98e19ab756e3bbb7a70fe290db42c42872
aleksey@koding.com <aleksey@kodingen.com>
2012-07-05 20:39:55 +0300

    ruby19 and irb

--- builders/puppet/modules/hosting_packages/manifests/ruby.pp
+++ builders/puppet/modules/hosting_packages/manifests/ruby.pp
@@ -4,15 +4,15 @@
 class hosting_packages::ruby {
     
     # modules installed from RPM
-    $ruby_modules = ["ruby-mysql","ruby-sqlite3" ]
+    $ruby_modules = ["ruby-mysql","ruby-sqlite3","ruby-devel","ruby19-devel","ruby-irb","ruby19-irb" ]
     
-    package { "ruby":
+    package { ["ruby","ruby19"]:
         ensure => installed,
     }
     
     package { $ruby_modules:
         ensure => installed,
-        require => [ Class["yumrepos::epel"], Package["ruby"] ],
+        require => [ Class["yumrepos::epel"], Package["ruby"] , Class["yumrepos::koding"] ],
         notify => Class["cloudlinux::cagefs_update"]
     }
 }
