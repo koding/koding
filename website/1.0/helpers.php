0dc55668943629537c02c9e87a628c28650b56d2
Gökmen Göksel <gokmen@goksel.me>
2012-08-03 11:32:36 +0300

    Let dev-api.koding works

--- website/1.0/helpers.php
+++ website/1.0/helpers.php
@@ -121,9 +121,9 @@ function get_mongo_db_name () {
 }
 
 function get_mongo_db () {
+  global $connStr;
   $db = get_mongo_db_name();
-  $connection_string = get_mongo_host().'/'.$db;
-  @$mongo = new Mongo($connection_string, array('persist' => 'api'));
+  @$mongo = new Mongo($connStr, array('persist' => 'api'));
   if(!isset($mongo)) {
     access_denied(2);
   }
