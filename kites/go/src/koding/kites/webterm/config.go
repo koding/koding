36ef160ebedf0393c4b5dae89c80109c522a0da3
Richard Musiol <mail@richard-musiol.de>
2012-09-10 18:54:05 +0200

    Replaced lve_wrapper with lve_exec.

--- kites/go/src/koding/kites/webterm/config.go
+++ kites/go/src/koding/kites/webterm/config.go
@@ -15,7 +15,7 @@ var configs = map[string]Config{
 	"default": {
 		amqpUrl:      "amqp://guest:x1srTA7!%25Vb%7D$n%7CS@web0.beta.system.aws.koding.com",
 		homePrefix:   "/Users/",
-		shellCommand: []string{"/bin/lve_wrapper", "/bin/bash"},
+		shellCommand: []string{"/bin/lve_exec", "/bin/bash"},
 	},
 
 	"local": {
