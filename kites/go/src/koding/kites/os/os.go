59a84a4a22a38c62e09318b7c2f24f76bf9cf577
Richard Musiol <mail@richard-musiol.de>
2012-09-19 00:23:56 +0200

    os kite: Added lve_exec.

--- kites/go/src/koding/kites/os/os.go
+++ kites/go/src/koding/kites/os/os.go
@@ -22,9 +22,12 @@ func main() {
 	kite.Start("amqp://guest:x1srTA7!%25Vb%7D$n%7CS@web0.beta.system.aws.koding.com", "os", func(user, method string, args interface{}) interface{} {
 		switch method {
 		case "spawn":
+			command := make([]string, 1+len(args.([]string)))
+			command[0] = "/bin/lve_exec"
+			copy(command[1:], args.([]string))
 			return run(args.([]string), user)
 		case "exec":
-			return run([]string{"/bin/bash", "-c", args.(string)}, user)
+			return run([]string{"/bin/lve_exec", "/bin/bash", "-c", args.(string)}, user)
 		default:
 			panic(fmt.Sprintf("Unknown method: %v.", method))
 		}
