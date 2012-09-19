e6a9d7b607658e3bdbf7b13ff026eced0b4b4acd
aleksey@koding.com <aleksey@myinvisible.net>
2012-09-19 07:25:53 +0300

    new key for son

--- builders/puppet/manifests/sysadmins/son.pp
+++ builders/puppet/manifests/sysadmins/son.pp
@@ -2,6 +2,6 @@ users::useradd { "son":
      fullname => "Son",
      uid => "604",
      hash => '$6$5T8pLfrq$RjZ4ySOF904qfbkTBvpLv08rX9F1K/gQd3geP/QLvHe15HUVfvhquEna0Y/9.La7Dl8WZwA4X.ySqtvQqzD5W.',
-     key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAudYzH3PFTs3Ck/HKBIwpuAbTt3Uns3jhBV1+xgHAvWLiazX+03NpP/tP2vufEcUf9x0+WTWp8EfbYvnx2MZQHTSFa+m+gWdwCdZOj4RsKgAuuxXydStw5Cst6Dj0EcNyoPbXzoRJf0JoqyEnkQAIZLdE6pN/hamTTFnssxTBYf2ePETlesQvbSkZMQTu5vNqbJdfDbR+oRfBIlov0rzywVOtyBdxLXoqvm08Wae/A50urIUFCEcec9XLbG5a5p9YvVFsfPQLkr4wO4kFrLNm5keuF0Yz9vqGoz0+X8pTijaOj+uW6lgB68LrWRVp5LUg1HZYH7h6Q0HjpsOvBSNUgQ==',
+     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqIou0ra0MKyqsQc/wxY8V2/ANqEJ20R8Fm/b9WijNTsIyOGvR4yUsC9A6FJd8bYrI9flxh1au133iAeoo3P5yKJp3s3UnXPxKIihYVnORjxAQq0sM46Xgfp6loSuIS3NT5tveo2aiN8/WuetuIJ6pYv5YuVy+EQkgZQQtwfCelNEgC4X/k4ViQR1j0zSs9GMMqhclWJccE5P0XWegOv3Y8LIXglwm0PRRyPtOWc1CluLuvqYzT+YyfpEO9UtIUhX2IIQJU7v1ktQb/trmSr23I/6s0U40G/nK1gXopqkiVO7q+AuzotZ/TpQJEGwN3VH/0oFT24WiqBQoYn1BNXWd'
      key_type => 'ssh-rsa',
 }
