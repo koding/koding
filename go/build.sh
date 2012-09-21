848ed68ec2d2a2c9c785d0aa7e023dece91147f3
Richard Musiol <mail@richard-musiol.de>
2012-09-21 20:05:35 +0200

    Made path to go executable absolute in build.sh

--- go/build.sh
+++ go/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 export GOPATH=$(cd "$(dirname "$0")"; pwd)
-go get -v koding/kites/webterm koding/kites/os
+/usr/local/go/bin/go get -v koding/kites/webterm koding/kites/os
 cp $GOPATH/bin/* $GOPATH/../kites
