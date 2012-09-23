cd7d52fc7e3f740e5e22c733c7f96562cd7e89f3
root <root@mon.prod.system.aws.koding.com>
2012-09-23 00:01:44 +0000

    build.sh

--- broker/build.sh
+++ broker/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/rebar get-deps compile
+rm -rf $DIR/deps
+$DIR/rebar clean get-deps compile
 erl +A 128 -pa $DIR/apps/*/ebin -pa $DIR/deps/*/ebin -s broker_app \
   -eval "io:format(\"~n~nServer is running~n\")." 
