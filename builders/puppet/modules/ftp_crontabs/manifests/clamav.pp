bba6c0ab4bea4b4805bceeafe91c820be77ac544
aleksey@koding.com <aleksey@myinvisible.net>
2012-09-24 15:51:21 +0300

    ftp crontabs fix

--- builders/puppet/modules/ftp_crontabs/manifests/clamav.pp
+++ builders/puppet/modules/ftp_crontabs/manifests/clamav.pp
@@ -1,7 +1,7 @@
 class ftp_crontabs::clamav {
     cron { "/usr/bin/freshclam --quiet": 
         user    => root,
-        hour    => */8,
+        hour    => '*/8',
         minute  => 0,
         require => [Class["clamav::install"],Class["clamav::service"]]
     }
