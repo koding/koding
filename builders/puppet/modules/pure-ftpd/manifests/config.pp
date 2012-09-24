3ec26103a837fba2e5a073592b0b283f0c9b2f78
aleksey@koding.com <aleksey@myinvisible.net>
2012-09-24 16:53:18 +0300

    ftp config template

--- builders/puppet/modules/pure-ftpd/manifests/config.pp
+++ builders/puppet/modules/pure-ftpd/manifests/config.pp
@@ -5,7 +5,8 @@ class pure-ftpd::config {
     file { "main_conf":
         path    => '/etc/pure-ftpd/pure-ftpd.conf',
         ensure  => file,
-        source  => "puppet:///modules/pure-ftpd/etc/pure-ftpd/pure-ftpd.conf",
+        #source  => "puppet:///modules/pure-ftpd/etc/pure-ftpd/pure-ftpd.conf",
+        content => template("pure-ftpd/pure-ftpd.conf.erb"),
         owner   => 'root',
         group   => 'root',
         require => Class["pure-ftpd::install"],
