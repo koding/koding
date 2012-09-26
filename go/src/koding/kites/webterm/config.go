f25dbe18795a9039ab4b3d581e660ff09dbd8a29
Christopher Thorn <chris@koding.com>
2012-09-26 16:26:42 -0700

    let's get web term working.

--- go/src/koding/kites/webterm/config.go
+++ go/src/koding/kites/webterm/config.go
@@ -12,6 +12,12 @@ type Config struct {
 
 var configs = map[string]Config{
 
+	"stage": {
+		amqpUrl:      "amqp://stage:#[85_[*zh7%4;4l6T]F!@web0.beta.system.aws.koding.com",
+		homePrefix:   "/Users/",
+		shellCommand: []string{"/bin/lve_exec", "/bin/bash"},
+	},
+
 	"default": {
 		amqpUrl:      "amqp://guest:x1srTA7!%25Vb%7D$n%7CS@web0.beta.system.aws.koding.com",
 		homePrefix:   "/Users/",
