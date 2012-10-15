f89e74a0a446bddca06f5746f2136b87faf613ed
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-15 15:58:30 +0300

    aide crontab

---
+++ builders/puppet/modules/hosting_crontabs/manifests/aide.pp
@@ -0,0 +1,11 @@
+class hosting_crontabs::aide {
+
+    cron { aide_scan: 
+        command => '/usr/sbin/aide --check',
+        user    => root,
+        hour    => [*/4],
+        minute  => 30,
+        require => Class["aide::db_init"],
+    }
+
+}
