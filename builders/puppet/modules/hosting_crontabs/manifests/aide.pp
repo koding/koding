064ad292827175ff746a360ba702e760a1354b82
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-15 16:02:35 +0300

    fix aide crontab rule

--- builders/puppet/modules/hosting_crontabs/manifests/aide.pp
+++ builders/puppet/modules/hosting_crontabs/manifests/aide.pp
@@ -3,7 +3,7 @@ class hosting_crontabs::aide {
     cron { aide_scan: 
         command => '/usr/sbin/aide --check',
         user    => root,
-        hour    => [*/4],
+        hour    => ['*/4'],
         minute  => 30,
         require => Class["aide::db_init"],
     }
