2f7d3c7a5d0a7c913b3d6d1aa3561a72bd500be0
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-15 15:44:32 +0300

    initialize aide db only if config deployed

--- builders/puppet/modules/aide/manifests/db_init.pp
+++ builders/puppet/modules/aide/manifests/db_init.pp
@@ -5,7 +5,7 @@ class aide::db_init {
     exec { "db_init":
         command => "/usr/sbin/aide --init && mv /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz",
         onlyif => "/usr/bin/test ! -e /var/lib/aide/aide.db.gz",
-        require => Class["aide::install"],
+        require => Class["aide::config"],
     }
     
 }
