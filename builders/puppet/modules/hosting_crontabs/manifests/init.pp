817cc5a967d674c2cb6c867a3707ed74a03e6064
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-16 15:32:38 +0300

    mysql quota crontab

--- builders/puppet/modules/hosting_crontabs/manifests/init.pp
+++ builders/puppet/modules/hosting_crontabs/manifests/init.pp
@@ -1,8 +1,10 @@
 class hosting_crontabs {
 
     include hosting_crontabs::scripts_dir
+
     include hosting_crontabs::clamav    
     include hosting_crontabs::phishing
     include hosting_crontabs::aide
-
+    include hosting_crontabs::mysql_quota
+t
 }
---
+++ builders/puppet/modules/hosting_crontabs/manifests/mysql_quota.pp
@@ -0,0 +1,34 @@
+class hosting_crontabs::mysql_quota {
+
+    file { "/opt/cronscripts/db_conf.py":
+         ensure  => file,
+         source  => "puppet:///modules/hosting_crontabs/opt/cronscripts/db_conf.py",
+         mode    => 755,
+         require => Class["hosting_crontabs::scripts_dir"],
+    }
+
+
+    file { "/opt/cronscripts/db.py":
+         ensure  => file,
+         source  => "puppet:///modules/hosting_crontabs/opt/cronscripts/db.py",
+         mode    => 755,
+         require => [Class["hosting_crontabs::scripts_dir"],File["/opt/cronscripts/db_conf.py"]],
+    }
+
+    cron { db_grant: 
+        command => "/opt/cronscripts/db.py --grant"
+        user    => root,
+        minute  => "*/5",
+        require => File['/opt/cronscripts/db.py'],
+    }
+    cron { db_revoke: 
+        command => "/opt/cronscripts/db.py --revoke"
+        user    => root,
+        hour    => "*/1",
+        minute  => 45,
+        require => File['/opt/cronscripts/db.py'],
+    }
+
+
+
+}
