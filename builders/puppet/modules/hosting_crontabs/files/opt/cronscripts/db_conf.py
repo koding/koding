f6118bef99b46ae08aef9f7ebd8aa3d0e285be60
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-16 15:50:43 +0300

    whitelist system dbs

--- builders/puppet/modules/hosting_crontabs/files/opt/cronscripts/db_conf.py
+++ builders/puppet/modules/hosting_crontabs/files/opt/cronscripts/db_conf.py
@@ -6,5 +6,5 @@ db_user = "system"
 db_pass = "gTW9ts2A4PXyECd69MQNAKx8v988x27cxFAu73pv"
 db_size_limit = 16
 
-db_whitelist = ["kwiki_3a82a0c96d"]
+db_whitelist = ["kwiki_3a82a0c96d","mysql","information_schema","innodb","tmp"]
 
