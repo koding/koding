d2da45309797af4764f1c977be67baf8d47cd645
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-18 21:58:37 +0300

    disable execution timeout for aide db initialization

--- builders/puppet/environments/development/modules/aide/manifests/db_init.pp
+++ builders/puppet/environments/development/modules/aide/manifests/db_init.pp
@@ -4,6 +4,7 @@ class aide::db_init {
     
     exec { "db_init":
         command => "/usr/sbin/aide --init && mv /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz",
+        timeout => 0,
         onlyif => "/usr/bin/test ! -e /var/lib/aide/aide.db.gz",
         require => Class["aide::config"],
     }
