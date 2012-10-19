650e9eb3b5aea7a1ea410b0294eb237e490732f0
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-19 19:57:02 +0300

    add nginx to secure group

--- builders/puppet/environments/development/modules/hosting_configs/manifests/init.pp
+++ builders/puppet/environments/development/modules/hosting_configs/manifests/init.pp
@@ -49,8 +49,13 @@ class hosting_configs {
         source => "puppet:///modules/hosting_configs/etc/yum.conf",
     }
 
-
-
+   file { "/Users":
+        ensure => directory,
+        owner => "root",
+        group => "secure",
+        mode => 0710,
+        require => Group["secure"],
+  }
     
     
 }
--- builders/puppet/environments/development/modules/hosting_httpd/manifests/user.pp
+++ builders/puppet/environments/development/modules/hosting_httpd/manifests/user.pp
@@ -1,10 +1,4 @@
 class hosting_httpd::user {
-    group { "secure":
-      ensure => present,
-      gid => 66,
-      require => Class["hosting_httpd::install"],
-  }
-
     user { "apache":
         ensure => present,
         groups => "secure",
