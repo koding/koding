9bf2eb37ffa1a401e095e2119bbc83da77ba34f0
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-19 12:12:16 +0300

    disable clamav for dev hosting servers

--- builders/puppet/environments/development/modules/hosting_crontabs/manifests/clamav.pp
+++ builders/puppet/environments/development/modules/hosting_crontabs/manifests/clamav.pp
@@ -1,6 +1,7 @@
 class hosting_crontabs::clamav {
 
     cron { clamav_scan: 
+        ensure => absent,
         command => '/usr/bin/ionice -c 3 -p `cat /var/run/clamav/clamd.pid` ; /bin/echo -e "MULTISCAN /Users" | /usr/bin/nc localhost 3310',
         user    => root,
         hour    => 2,
