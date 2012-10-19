50d8da2f7d851587f540becbd1e525f9b5a5a42e
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-19 18:57:01 +0300

    disable aide for dev env shared hosting hosts

--- builders/puppet/environments/development/modules/hosting_crontabs/manifests/aide.pp
+++ builders/puppet/environments/development/modules/hosting_crontabs/manifests/aide.pp
@@ -1,6 +1,7 @@
 class hosting_crontabs::aide {
 
     cron { aide_scan: 
+        ensure => absent,
         command => '/usr/sbin/aide --check >> /var/log/aide.log ; echo  "UserParameter=aide.result,echo $?" > /etc/zabbix/zabbix_agentd.conf.d/aide_result.conf',
         user    => root,
         hour    => ['*/4'],
