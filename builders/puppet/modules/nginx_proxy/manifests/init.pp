dffdbac6a41617afd3e343b5b6e45a8915b62001
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-22 13:25:38 +0300

    add nginx to secure group

--- builders/puppet/modules/nginx_proxy/manifests/init.pp
+++ builders/puppet/modules/nginx_proxy/manifests/init.pp
@@ -9,5 +9,5 @@
 #
 #
 class nginx_proxy {
-    include nginx_proxy::install, nginx_proxy::config, nginx_proxy::service, nginx_proxy::repo
+    include nginx_proxy::install, nginx_proxy::config, nginx_proxy::service, nginx_proxy::repo, nginx_proxy::user
 }
