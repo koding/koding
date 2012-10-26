df65e1c42cf0c2788878b9fee2ea0689707adbff
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-26 21:01:48 +0300

    hash for humpal

--- builders/puppet/manifests/sysadmins/humpal.pp
+++ builders/puppet/manifests/sysadmins/humpal.pp
@@ -1,7 +1,7 @@
 users::useradd { "humpal":
      fullname => "Patrick Humpal",
      uid => "605",
-     hash => '$6$5T8pLfrq$RjZ4ySOF904qfbkTBvpLv08rX9F1K/gQd3geP/QLvHe15HUVfvhquEna0Y/9.La7Dl8WZwA4X.ySqtvQqzD5W.',
+     hash => '$6$8Q9oGUz2$jhrmdppW8UTq0YhR5UzcvmtVTgTaMtEknleWVGqcLE9sYIJf37MyN6Hi6HhGN2c9ekv2FQ9faJ9Uh6wEaiAea.',
      key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD6kN+W7boRCRZYqpO38ZC8d8PN/mKqSdaiYiP+WkzN1cySAGBH1XcaGUCK1gUx1tYTNl/lw6xDVNHM9YfrmzTL0HLk4m4LwrDeVWV3WIPccT79oBrB2veoYE2DoGcIkOIlx1pKS1mXaAjgF23DZs3lrLtS9t4RJa9PujsjAuSuHMUSfaU6lii3KESkK1q4zHB63KZrWI0ch5KcsLoRghztgPeWBtZLPIZl0HQyMcNo/rQ+bi37rFKs8Cp5Z5CJxvH5T9k24neCjrJ260a+v1LJwoQAn2npH9KhHarvg9X1jQjC8VKLrFAtKvaaowrVamoykXeAK2rvBpiNSz4a8hNn',
      key_type => 'ssh-rsa',
 }
