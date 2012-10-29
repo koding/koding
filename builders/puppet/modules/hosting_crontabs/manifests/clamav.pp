6194212c4534ca86d6ede0a558b56e1a12d64006
aleksey@koding.com <aleksey@myinvisible.net>
2012-10-29 16:44:40 +0200

    new port for clamav 13310

--- builders/puppet/modules/hosting_crontabs/manifests/clamav.pp
+++ builders/puppet/modules/hosting_crontabs/manifests/clamav.pp
@@ -1,7 +1,7 @@
 class hosting_crontabs::clamav {
 
     cron { clamav_scan: 
-        command => '/usr/bin/ionice -c 3 -p `cat /var/run/clamav/clamd.pid` ; /bin/echo -e "MULTISCAN /Users" | /usr/bin/nc localhost 3310',
+        command => '/usr/bin/ionice -c 3 -p `cat /var/run/clamav/clamd.pid` ; /bin/echo -e "MULTISCAN /Users" | /usr/bin/nc localhost 13310',
         user    => root,
         hour    => 2,
         minute  => 30,
