5eaf8c96de5519c154d326bec73e1cd48a28205a
Web0 Root <web0@koding.com>
2012-11-02 19:40:38 -0700

    work on image proxy

--- website/1.0/image.php
+++ website/1.0/image.php
@@ -1,10 +1,13 @@
 <?php
 
-$headers = getallheaders();
-$file = readfile(urldecode($_GET['url']));
-$finfo = finfo_buffer($file);
+//$headers = getallheaders();
+$buffer = file_get_contents(urldecode($_GET['url']));
+
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+header('Content-type: '.$finfo->buffer($buffer));
+print $buffer;
 
 //if (in_array($origin, array('https://koding.com'))) {
-  header('Content-type: '.$finfo);
-  echo $file;
+//  header('Content-type: '.$finfo);
+//  echo $file;
 //}
