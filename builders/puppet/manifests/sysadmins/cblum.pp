5894b0491daa194581d68f9a47ed7fecdf2e546a
aleksey@koding.com <aleksey@myinvisible.net>
2012-11-05 22:37:41 +0200

    user cblum

---
+++ builders/puppet/manifests/sysadmins/cblum.pp
@@ -0,0 +1,7 @@
+users::useradd { "cblum":
+     fullname => "Chris Blum",
+     uid => "700",
+     hash => '$6$PupmONJw$VAVy2Y1IrSNiHiqv8edBdXaULyIP9GoNezU8thTP21aIRFlZjdBq1I7kJD7ifOVVDEGL4MJNSQcHG2P3Alnxm1',
+     key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAuDn1H+7CVttGwAvN7LI3u2dIEiUsEQKP+uknkdw55mYz2tLwOxvkWy/NxMjYFdHThMhvlW3LroNX+oQP/DH7TZQNUrzQVUbCYwQl8V+uyrFo/+t+qRAATYkCvrZezdeK0LhECPq8wZwDqoPOG06k7220IxAgXTNdPCVbTwj408Bn4ar1CiMpsjYCneWu+0DZ7mb7xkrCcyGOC+UAthhr9E3mHAQU3tFfVX1ydzRHV2tpyIJXB6aA8gmseQXKsqVhvKZmUogqBza6JPrlV7vTARv4IEPOQQD+cMHXDCMmYmUY3PkNsGgk/xowSwGrrHOFJIE5/ximxRu2a3AcydcoJQ==',
+     key_type => 'ssh-rsa',
+}
