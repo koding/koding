5292095a6fb47366d554588d27ae5e8e75117ff7
aleksey@koding.com <aleksey@myinvisible.net>
2012-11-09 17:27:30 +0200

    switch aide to zabbix_sender

---
+++ builders/puppet/modules/hosting_crontabs/files/opt/cronscripts/aide.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ZABBIX_SENDER="/usr/bin/zabbix_sender"
+ZABBIX_HOST="mon.prod.system.aws.koding.com"
+ZABBIX_PORT="10051"
+ZABBIX_KEY="aide.result"
+
+/usr/sbin/aide --check >> /var/log/aide.log 
+
+${ZABBIX_SENDER} -z ${ZABBIX_HOST} -p ${ZABBIX_PORT} -s ${HOSTNAME} -k ${ZABBIX_KEY} -o $?
