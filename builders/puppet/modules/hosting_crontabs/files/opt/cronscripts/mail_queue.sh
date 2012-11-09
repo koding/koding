99d7c647a4742ac4549e88a7e9bc0b457fd094fa
aleksey@koding.com <aleksey@myinvisible.net>
2012-11-09 15:52:11 +0200

    mail queue monitoring update

--- builders/puppet/modules/hosting_crontabs/files/opt/cronscripts/mail_queue.sh
+++ builders/puppet/modules/hosting_crontabs/files/opt/cronscripts/mail_queue.sh
@@ -4,7 +4,7 @@ ZABBIX_CONF="/etc/zabbix/zabbix_agentd.conf.d/mailq.conf"
 
 queue=$(/usr/sbin/postqueue -p |/usr/bin/tail -n1|/bin/cut -d' ' -f5)
 if echo ${queue}|grep -q [0-9] >/dev/null; then
-    echo "UserParameter=mailq.size[*],echo ${queue}" > ${ZABBIX_CONF}
+    echo "UserParameter=mailq.size,echo ${queue}" > ${ZABBIX_CONF}
 else
-    echo "UserParameter=mailq.size[*],echo 0" > ${ZABBIX_CONF}
+    echo "UserParameter=mailq.size,echo 0" > ${ZABBIX_CONF}
 fi
