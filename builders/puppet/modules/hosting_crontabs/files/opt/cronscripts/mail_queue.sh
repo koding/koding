3bf48b04f6b238cd6797996b0c90bcc349f68359
aleksey@koding.com <aleksey@myinvisible.net>
2012-11-09 16:38:34 +0200

    mail queue monitoring switch to zabbix_sender

--- builders/puppet/modules/hosting_crontabs/files/opt/cronscripts/mail_queue.sh
+++ builders/puppet/modules/hosting_crontabs/files/opt/cronscripts/mail_queue.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ZABBIX_CONF="/etc/zabbix/zabbix_agentd.conf.d/mailq.conf"
+ZABBIX_SENDER="/usr/bin/zabbix_sender"
+ZABBIX_HOST="mon.prod.system.aws.koding.com"
+ZABBIX_PORT="10051"
+ZABBIX_KEY="mailq.size"
+
 
 
 queue=$(/usr/sbin/postqueue -p |/usr/bin/tail -n1|/bin/cut -d' ' -f5)
 if echo ${queue}|grep -q [0-9] >/dev/null; then
-    echo "UserParameter=mailq.size,echo ${queue}" > ${ZABBIX_CONF}
+    ${ZABBIX_SENDER} -z ${ZABBIX_HOST} -p ${ZABBIX_PORT} -s ${HOSTNAME} -k ${ZABBIX_KEY} -o ${queue} >/dev/null
 else
-    echo "UserParameter=mailq.size,echo 0" > ${ZABBIX_CONF}
+    ${ZABBIX_SENDER} -z ${ZABBIX_HOST} -p ${ZABBIX_PORT} -s ${HOSTNAME} -k ${ZABBIX_KEY} -o 0 >/dev/null
 fi
+
