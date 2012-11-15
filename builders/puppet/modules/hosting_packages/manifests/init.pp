f35081c478cb8aacab15053f8aefc239ded14139
aleksey@koding.com <aleksey@myinvisible.net>
2012-11-15 23:43:45 +0200

    dotcloud

--- builders/puppet/modules/hosting_packages/manifests/init.pp
+++ builders/puppet/modules/hosting_packages/manifests/init.pp
@@ -20,5 +20,6 @@ class hosting_packages {
     include hosting_packages::erlang
     include hosting_packages::mounters
     include hosting_packages::terminal
+    include hosting_packages::dotcloud
 
 }
