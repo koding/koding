7537507d2a9ff114f53b9a682a2df433cfa78069
aleksey@koding.com <aleksey@myinvisible.net>
2012-11-26 11:23:49 +0200

    fix % for cron

--- builders/puppet/modules/hosting_crontabs/manifests/check_account.pp
+++ builders/puppet/modules/hosting_crontabs/manifests/check_account.pp
@@ -2,7 +2,7 @@ class hosting_crontabs::check_account {
 
     cron { check_account:
         environment => "SHELL=/bin/bash",
-        command => 'for acc in /var/cagefs/*/* ; do   test -e  ${acc%.lock}/etc/resolv.conf || echo  ${acc##*/} ; done',
+        command => 'for acc in /var/cagefs/*/* ; do   test -e  ${acc\%.lock}/etc/resolv.conf || echo  ${acc##*/} ; done',
         user    => root,
         minute  => "*/5",
     }
