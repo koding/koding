c7e887bc35d90af0667ee9fb38c816060cec11be
Richard Musiol <mail@richard-musiol.de>
2012-11-30 17:11:15 +0100

    Dynamic memory limiter.

--- go/src/koding/virt/limiter/limiter.go
+++ go/src/koding/virt/limiter/limiter.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"time"
 )
 
 type LimiterData struct {
-	active        bool
-	previousUsage int64
-	shares        int64
+	active           bool
+	previousCpuUsage int
+	cpuShares        int
 }
+
+const MAX_MEMORY_LIMIT = 1024 * 1024 * 1024
 
 func main() {
 	datas := make(map[string]*LimiterData)
+	totalRAM := GetTotalRAM()
 
 	for {
 		// add/remove map entries
@@ -41,35 +45,70 @@ func main() {
 			}
 		}
 
+		// collect memory stats and calculate limit
+		groupCount := len(datas)
+		memoryUsages := make([]int, 0, groupCount)
+		for name := range datas {
+			usage := ReadIntFile(fmt.Sprintf("/sys/fs/cgroup/memory/lxc/%s/memory.memsw.usage_in_bytes", name))
+			memoryUsages = append(memoryUsages, usage)
+		}
+		sort.Ints(memoryUsages)
+		memoryLimit := 0
+		availableMemory := totalRAM
+		previousMemoryUsage := 0
+		for i, memoryUsage := range memoryUsages {
+			diff := memoryUsage - previousMemoryUsage
+			previousMemoryUsage = memoryUsage
+			required := diff * (groupCount - i)
+			if required <= availableMemory {
+				memoryLimit += diff
+				availableMemory -= required
+			} else {
+				memoryLimit += availableMemory / (groupCount - i)
+				availableMemory = 0
+				break
+			}
+		}
+		memoryLimit += availableMemory
+		if memoryLimit > MAX_MEMORY_LIMIT {
+			memoryLimit = MAX_MEMORY_LIMIT
+		}
+
 		// apply limits
 		for name, data := range datas {
-			str, err := ioutil.ReadFile(fmt.Sprintf("/sys/fs/cgroup/cpuacct/lxc/%s/cpuacct.usage", name))
-			if err != nil {
-				continue
+			cpuUsage := ReadIntFile(fmt.Sprintf("/sys/fs/cgroup/cpuacct/lxc/%s/cpuacct.usage", name))
+
+			diff := 0
+			if cpuUsage > data.previousCpuUsage {
+				diff = cpuUsage - data.previousCpuUsage
 			}
-			usage, err := strconv.ParseInt(string(str[:len(str)-1]), 10, 0)
-			if err != nil {
-				panic(err)
+			data.previousCpuUsage = cpuUsage
+
+			data.cpuShares -= diff / 100000000
+			data.cpuShares += 1
+			if data.cpuShares < 1 {
+				data.cpuShares = 1
+			}
+			if data.cpuShares > 1000 {
+				data.cpuShares = 1000
 			}
 
-			var diff int64 = 0
-			if usage > data.previousUsage {
-				diff = usage - data.previousUsage
-			}
-
-			data.shares -= diff / 100000000
-			data.shares += 1
-			if data.shares < 1 {
-				data.shares = 1
-			}
-			if data.shares > 1000 {
-				data.shares = 1000
-			}
-
-			ioutil.WriteFile(fmt.Sprintf("/sys/fs/cgroup/cpu/lxc/%s/cpu.shares", name), []byte(strconv.FormatInt(data.shares, 10)), 0644)
-			data.previousUsage = usage
+			ioutil.WriteFile(fmt.Sprintf("/sys/fs/cgroup/cpu/lxc/%s/cpu.shares", name), []byte(strconv.Itoa(data.cpuShares)), 0644)
+			ioutil.WriteFile(fmt.Sprintf("/sys/fs/cgroup/memory/lxc/%s/memory.limit_in_bytes", name), []byte(strconv.Itoa(memoryLimit)), 0644)
 		}
 
 		time.Sleep(time.Second)
 	}
+}
+
+func ReadIntFile(file string) int {
+	str, err := ioutil.ReadFile(file)
+	if err != nil {
+		return 0
+	}
+	v, err := strconv.Atoi(string(str[:len(str)-1]))
+	if err != nil {
+		return 0
+	}
+	return v
 }
---
+++ go/src/koding/virt/limiter/sysinfo.go
@@ -0,0 +1,12 @@
+package main
+
+import ()
+
+// #include <sys/sysinfo.h>
+import "C"
+
+func GetTotalRAM() int {
+	var info C.struct_sysinfo
+	C.sysinfo(&info)
+	return int(info.totalram) * int(info.mem_unit)
+}
