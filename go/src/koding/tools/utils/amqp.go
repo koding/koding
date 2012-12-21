313cef2422959ff4b8a76e0ede52e07de1b9ca8e
Christopher Thorn <chris@koding.com>
2012-12-20 19:59:58 -0800

    rebased my hack on the correct version of kite.go and amqp.go

--- go/src/koding/tools/utils/amqp.go
+++ go/src/koding/tools/utils/amqp.go
@@ -60,11 +60,25 @@ func CreateAmqpChannel(conn *amqp.Connection) *amqp.Channel {
 	return channel
 }
 
-func DeclareAmqpExchange(channel *amqp.Channel, exchange string) {
-	err := channel.ExchangeDeclare(exchange, "topic", true, true, false, false, nil)
+func DeclareBindConsumeAmqpQueue(channel *amqp.Channel, kind, exchange, key string) <-chan amqp.Delivery {
+	if err := channel.ExchangeDeclare(exchange, kind, false, true, false, false, nil); err != nil {
+		panic(err)
+	}
+
+	if _, err := channel.QueueDeclare("", false, true, false, false, nil); err != nil {
+		panic(err)
+	}
+
+	if err := channel.QueueBind("", key, exchange, false, nil); err != nil {
+		panic(err)
+	}
+
+	stream, err := channel.Consume("", "", true, false, false, false, nil)
 	if err != nil {
 		panic(err)
 	}
+
+	return stream
 }
 
 func XDeclareAmqpPresenceExchange(channel *amqp.Channel, exchange string, serviceType string, serviceGenericName string, serviceUniqueName string) {
@@ -84,25 +98,4 @@ func XDeclareAmqpPresenceExchange(channel *amqp.Channel, exchange string, servic
 	if err != nil {
 		panic(err)
 	}
-}
-
-func DeclareBindConsumeAmqpQueue(channel *amqp.Channel, queue, key, exchange string, autodelete bool) <-chan amqp.Delivery {
-	DeclareAmqpExchange(channel, exchange)
-
-	_, err := channel.QueueDeclare(queue, false, autodelete, false, false, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	err = channel.QueueBind(queue, key, exchange, false, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	stream, err := channel.Consume(queue, "", true, false, false, false, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	return stream
 }
