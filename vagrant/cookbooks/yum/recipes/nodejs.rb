4e468f1467247a9741712a5f3aabe2085759a747
aleksey@koding.com <aleksey@myinvisible.net>
2012-12-25 17:13:20 -0800

    repos

---
+++ vagrant/cookbooks/yum/recipes/nodejs.rb
@@ -0,0 +1,12 @@
+#
+# Author:: Aleksey Mykhailov <aleksey@koding.com>"
+# Cookbook Name:: yum
+# Recipe:: nodejs
+#
+
+
+yum_repository "NodeJS" do
+    description "NodeJS repo"
+    url "http://nodejs.myinvisible.net/yum"
+    action :add
+end
