b867b1fb91c060731e993483424e0ab81da27305
aleksey@koding.com <aleksey@myinvisible.net>
2012-12-27 12:35:18 -0800

    enable experemental rabbitmq plugin

--- vagrant/roles/rabbitmq_server.rb
+++ vagrant/roles/rabbitmq_server.rb
@@ -3,6 +3,7 @@ description "The role for RabbitMQ servers"
 run_list [
             "recipe[rabbitmq]",
             "recipe[rabbitmq::mgmt_console]",
+            "recipe[rabbitmq::third_party_plugins]",
             "recipe[rabbitmq::vhosts]",
             "recipe[rabbitmq::users]",
         ]
