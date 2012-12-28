f7be075d0766a90302028b5a914a363bdc6c873e
Richard Musiol <mail@richard-musiol.de>
2012-12-28 15:47:49 +0100

    Conflict merge was missing in previous commit.

--- go/build.sh
+++ go/build.sh
@@ -3,13 +3,7 @@
 export GOPATH=$(cd "$(dirname "$0")"; pwd)
 
 ldflags="-X koding/tools/utils.version $(git rev-parse HEAD)"
-<<<<<<< HEAD
-
-/usr/local/go/bin/go get -v -ldflags "$ldflags" koding/broker koding/kites/irc koding/alice
-
-=======
-services=(koding/broker koding/kites/webterm koding/kites/irc koding/alice)
->>>>>>> dev-new
+services=(koding/broker koding/kites/irc koding/alice)
 if [ $(uname) == "Linux" ]; then
   services+=(koding/kites/os)
 fi
