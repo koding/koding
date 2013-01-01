6a0462d819d8cf6762d06a847970e9cf63245111
aleksey@koding.com <aleksey@myinvisible.net>
2013-01-01 21:42:06 +0200

    bahadir's user

---
+++ builders/puppet/manifests/sysadmins/bahadir.pp
@@ -0,0 +1,7 @@
+users::useradd { "bahadir":
+     fullname => "Bahadır Kandemir",
+     uid => "607",
+     hash => '$6$zExJZZg8$vAhZTJVFuXiRuNDxgCtvAe8IIKNuPScYNHz4AMF/3kCphnm9Nk6HtRrHZI3rgZ87Syrc.8BSYI8d/UppAmTTg0',
+     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBcyLoUe/+ShHJA3hwsJAiOnWTl7ngWnLxNbu/5Bhym8b3VTT41UtV3YLAW/6VmjGiB+MTUPxn57pZgPYKTCnWw4NCbge+wUMWnydruwsblKQrV5y9wQoOmLuC/nGj5mdGyNBfyoWJZ68I5AMQOnZfYant/zIcrAbnO0H+Jn5KdGUC6awXadRQGjrFgsHUHZuHO3733v8uyeFRq3QJ0AzSSisOTPMUf3/LV7K/MboQxifrUHnmJUM60YH29EIeP2aYQOS4JWjV4E1FYwUOF7Lx+ehJMYg5RdRRX/eEjMqbQGuboocZx3KKnic7v9U7ykz+25uftmejTdrteEukHsoX',
+     key_type => 'ssh-rsa',
+}
