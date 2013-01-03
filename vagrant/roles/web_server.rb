246e49f99769e0aff09602e570026f1e662380c3
aleksey@koding.com <aleksey@myinvisible.net>
2013-01-03 22:55:38 +0200

    initial nginx conf for web

--- vagrant/roles/web_server.rb
+++ vagrant/roles/web_server.rb
@@ -1,3 +1,10 @@
 name "web_server"
 description "The  role for WEB servers"
-run_list ["recipe[nodejs]","recipe[golang]","recipe[git]"]
+run_list ["recipe[nginx]", "recipe[nodejs]","recipe[golang]","recipe[git]"]
+
+
+default_attributes({ "nginx" => {
+                                "worker_processes" => "2",
+                                }
+                   })
+
