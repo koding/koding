5b133c6b65d8e4ff72c9c34e6cf8be977a140bff
aleksey@koding.com <aleksey@myinvisible.net>
2013-01-07 11:13:42 +0200

    exclude RemoteDrives and .tmp from scan enable clamav cronjob

--- builders/puppet/modules/hosting_crontabs/manifests/clamav.pp
+++ builders/puppet/modules/hosting_crontabs/manifests/clamav.pp
@@ -1,7 +1,6 @@
 class hosting_crontabs::clamav {
 
     cron { clamav_scan: 
-        ensure  => absent,
         command => '/usr/bin/ionice -c 3 -p `cat /var/run/clamav/clamd.pid` ; /bin/echo -e "MULTISCAN /Users" | /usr/bin/nc localhost 13310',
         user    => root,
         hour    => 2,
