8c2aa0e462659011ae6e0d2cc8538756b4af3b3b
Chris Nelson Blum <nelson@koding.com>
2013-01-09 16:33:52 +0100

    go/build.sh is now compile-able from everywhere (not only from everywhere inside the koding repo) go sources are now compiled in the web box automatically

--- go/build.sh
+++ go/build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 export GOPATH=$(cd "$(dirname "$0")"; pwd)
+export GIT_DIR=$GOPATH/../.git
 
 ldflags="-X koding/tools/utils.version $(git rev-parse HEAD)"
 services=(koding/broker koding/kites/irc koding/virt/idshift koding/virt/ldapserver koding/virt/proxy koding/alice)
--- vagrant/roles/web_server.rb
+++ vagrant/roles/web_server.rb
@@ -1,3 +1,3 @@
 name "web_server"
 description "The  role for WEB servers"
-run_list ["recipe[nodejs]","recipe[golang]","recipe[git]"]
+run_list ["recipe[nodejs]","recipe[golang]","recipe[git]", "recipe[build-go]"]
