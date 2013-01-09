1fb0da28c096c491ff35e7d028b381a6cc18a2da
aleksey@koding.com <aleksey@myinvisible.net>
2013-01-09 12:54:36 +0200

    new pass for rmusiol

--- builders/puppet/manifests/sysadmins/rmusiol.pp
+++ builders/puppet/manifests/sysadmins/rmusiol.pp
@@ -1,7 +1,7 @@
 users::useradd { "rmusiol":
      fullname => "rmusiol",
      uid => "606",
-     hash => '$6$dsfggXhdu$xgggggg1itDmZzkzAFoNgjhmv/eat5qTnSPUosC4z1jvCIU/MU5NSau.KC/gs4vLCAbJ4bsKv/oc6//9FA',
+     hash => '$6$tdHssQyw$Inkk5nuZYIIu23vHFzTdsDFoX1GXY1uCbHXP51.GehMc26xUTsZNMjFXQDb4GtN04C51Rk3zBh9V1xMHBHJQp/',
      key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAoyveQIHQ1BDCG/3SdlwzeDetXEz+TeLzADI6Xj0RVFbGpcFFkaY40tTxTAZAGOOWSzcuv7d86GIVn4CZVqQjkzivfwzRjlLl9en9oxD7/lH0O0bTWk/qdjRKh2FdsdR7pK0vTkkdSSw8JOIR6g3yXdPj5Cpc80vpgTsFQrguwpC6/34DqMML5T5nlXLmVBndpm2QDYZ0n4RF6MotnCXZdG6POdZjm33ihqF5YM9HWQ1JuOXIFDD2spDInwzMbpZO/QSS10KDjWUHO2Stykb4cvmfqJUw7OrMnXswtvs2B9kdMXRklVGjMhu/i+7GAvBxBUr70e3ao2ONjx5ytgyQgQ==',
      key_type => 'ssh-rsa',
 }
