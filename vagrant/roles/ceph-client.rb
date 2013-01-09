3c0d1e5a4d4b58036eac547c752828d5f9ce12dc
Chris Nelson Blum <nelson@koding.com>
2013-01-09 16:32:19 +0100

    cephClient Box is now also in the hosts Recipe cephClient will now also get the vagrant specific hosts file ldap Server chooses his ip based on node['hostname'] instead of a fixed IP

--- vagrant/roles/ceph-client.rb
+++ vagrant/roles/ceph-client.rb
@@ -1,3 +1,3 @@
 name "ceph-client"
 description "The role for Ceph Clients (accessing RBD storages e.g. for VM users)"
-run_list ["recipe[base_packages::debian]", "recipe[build-essential]", "recipe[git]", "recipe[lxc]", "recipe[lxc::prepareVMRoot]", "recipe[ceph-base]", "recipe[ceph]"]
+run_list ["recipe[base_packages::debian]", "recipe[build-essential]", "recipe[git]", "recipe[lxc]", "recipe[lxc::prepareVMRoot]", "recipe[ceph-base]", "recipe[ceph]","recipe[hosts]"]
