4e3fd6b0b8f68ec4c576feeede39e25adf39b991
Chris Nelson Blum <nelson@koding.com>
2013-01-11 12:43:01 +0100

    Only downloading S3 lxc files if they aren't downloaded yet (else they were pulled on every machine start)

--- vagrant/cookbooks/lxc/recipes/default.rb
+++ vagrant/cookbooks/lxc/recipes/default.rb
@@ -23,61 +23,65 @@ package "libseccomp0" do
 	action :install
 end
 
-remote_file "/tmp/liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  mode 0644
-  checksum "83cc6f95e6e56d0a1c649983c68a2f89396348a45e3022def6865334c6b7eba0"
-end
+if (! ::File.exists?("/tmp/lxc"))
+then
+  execute "mkdir -p /tmp/lxc"
 
-remote_file "/tmp/lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  mode 0644
-  checksum "f55098dc1c35b726f30c83f4d9de004ff1edab23c2df48897b490f1fb361fc36"
-end
+  remote_file "/tmp/lxc/liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    mode 0644
+    checksum "83cc6f95e6e56d0a1c649983c68a2f89396348a45e3022def6865334c6b7eba0"
+  end
 
-remote_file "/tmp/lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  mode 0644
-  checksum "fe8c975ca2a9702921b7c9d3e41c7b45ab74795c65bc923de4096281eb2c4e84"
-end
+  remote_file "/tmp/lxc/lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    mode 0644
+    checksum "f55098dc1c35b726f30c83f4d9de004ff1edab23c2df48897b490f1fb361fc36"
+  end
 
-remote_file "/tmp/lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  mode 0644
-  checksum "82e7522fa8e088c62f090c6f3588a438abdd3b3470b9cce2e4bd3728b1013900"
-end
+  remote_file "/tmp/lxc/lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    mode 0644
+    checksum "fe8c975ca2a9702921b7c9d3e41c7b45ab74795c65bc923de4096281eb2c4e84"
+  end
 
-remote_file "/tmp/python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  mode 0644
-  checksum "4127355bcdcdcbde4b0b9608f707f8b8042f2b196a405fb579143fedeced5cf8"
-end
+  remote_file "/tmp/lxc/lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    mode 0644
+    checksum "82e7522fa8e088c62f090c6f3588a438abdd3b3470b9cce2e4bd3728b1013900"
+  end
 
-dpkg_package "liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "/tmp/liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  action :install
-end
+  remote_file "/tmp/lxc/python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "https://s3.amazonaws.com/koding-vagrant-Lti5bj61mVnfMkhX/debian-packets/python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    mode 0644
+    checksum "4127355bcdcdcbde4b0b9608f707f8b8042f2b196a405fb579143fedeced5cf8"
+  end
 
-dpkg_package "lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "/tmp/lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  action :install
-end
+  dpkg_package "liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "/tmp/lxc/liblxc0_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    action :install
+  end
 
-dpkg_package "lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "/tmp/lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  action :install
-end
+  dpkg_package "lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "/tmp/lxc/lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    action :install
+  end
 
-dpkg_package "lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "/tmp/lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  action :install
-end
+  dpkg_package "lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "/tmp/lxc/lxc-dev_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    action :install
+  end
 
-dpkg_package "python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
-  source "/tmp/python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
-  action :install
-end
+  dpkg_package "lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "/tmp/lxc/lxc-dbg_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    action :install
+  end
 
+  dpkg_package "python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb" do
+    source "/tmp/lxc/python3-lxc_0.8.0~rc1-4ubuntu38userns3_amd64.deb"
+    action :install
+  end
+end 
 execute "service lxc-net stop"
 
 cookbook_file "/etc/default/lxc" do
