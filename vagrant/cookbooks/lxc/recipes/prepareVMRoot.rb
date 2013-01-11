7d391ae6e26267f732fd29bf8d9e275035302410
Chris Nelson Blum <nelson@koding.com>
2013-01-11 13:16:01 +0100

    Converted buildVMRoot.sh into a Chef Script

--- vagrant/cookbooks/lxc/recipes/prepareVMRoot.rb
+++ vagrant/cookbooks/lxc/recipes/prepareVMRoot.rb
@@ -7,8 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "mkdir -p /cgroup"
-execute "mount none -t cgroup /cgroup"
+if (! ::File.exists?("/cgroup"))
+	then
+	execute "mkdir -p /cgroup"
+	execute "mount none -t cgroup /cgroup"
+end
 
 execute "mkdir -p /var/lib/lxc/vmroot"
 cookbook_file "/var/lib/lxc/vmroot/config" do
@@ -16,31 +19,83 @@ cookbook_file "/var/lib/lxc/vmroot/config" do
 	mode "0755"
 end
 
-execute "sudo bash /opt/koding/builders/buildVMRoot.sh" do
-	creates "/var/lib/lxc/vmroot/rootfs"
-end
+# execute "sudo bash /opt/koding/builders/buildVMRoot.sh" do
+# 	creates "/var/lib/lxc/vmroot/rootfs"
+# end
 
 ####
 # Build VM Root
 ####
+# if (! ::File.exists?("/var/lib/lxc/vmroot/rootfs"))
+# 	then
+	# packages are installed with debootstrap
+	# additional packages are installed later on with apt-get
+	packages="ssh,curl,iputils-ping,iputils-tracepath,telnet,vim,rsync"
+	additional_packages="lighttpd htop iotop iftop nodejs nodejs-legacy php5-cgi erlang ghc swi-prolog clisp ruby ruby-dev ri rake golang python mercurial git subversion cvs bzr fish sudo net-tools wget aptitude emacs ldap-auth-client nscd"
+	suite="quantal"
+	variant="buildd"
+	target="/var/lib/lxc/vmroot/rootfs"
+	VM_upstart="/etc/init"
 
-# packages are installed with debootstrap
-# additional packages are installed later on with apt-get
-# packages="ssh,curl,iputils-ping,iputils-tracepath,telnet,vim,rsync"
-# additional_packages="lighttpd htop iotop iftop nodejs nodejs-legacy php5-cgi \
-#                      erlang ghc swi-prolog clisp ruby ruby-dev ri rake golang python \
-#                      mercurial git subversion cvs bzr \
-#                      fish sudo net-tools wget aptitude emacs \
-#                      ldap-auth-client nscd"
-# suite="quantal"
-# variant="buildd"
-# target="/var/lib/lxc/vmroot/rootfs"
-# VM_upstart="/etc/init"
+	mirror="http://ftp.halifax.rwth-aachen.de/ubuntu/"
 
-# mirror="http://ftp.halifax.rwth-aachen.de/ubuntu/"
+	# Not REALLY necessary because we have our if clause, but nice for testing when if is commented
+	execute "lxc-stop -n vmroot"
+	execute "sleep 1"
+	execute "rm -rf #{target}"
 
-# execute "rm -rf #{target}"
+	execute "$(which debootstrap) --include #{packages} --variant=#{variant} #{suite} #{target} #{mirror}"
 
-# execute "$(which debootstrap) --include #{packages} --variant=#{variant} #{suite} #{target} #{mirror}"
+	file "#{target}/etc/apt/sources.list" do
+		mode "0644"
+		content <<-EOH
+deb #{mirror} #{suite} main restricted universe multiverse
+deb #{mirror} #{suite}-updates main restricted universe multiverse
+deb #{mirror} #{suite}-security main restricted universe multiverse
+EOH
+	end
 
-# execute "lxc-attach -n vmroot --"
+	execute "/opt/koding/go/bin/idshift #{target}"
+	execute "lxc-start -n vmroot -d"
+	# Wait until VM starts
+	execute "sleep 5"
+
+	# Fix locales
+	execute "lxc-attach -n vmroot -- /usr/sbin/locale-gen en_US.UTF-8"
+	execute "lxc-attach -n vmroot -- /usr/sbin/update-locale LANG=\"en_US.UTF-8\""
+
+	# Fix fstab inside vmroot
+	execute "/bin/sed -i 's!none            /sys/fs/fuse/connections!#none            /sys/fs/fuse/connections!' #{target}/lib/init/fstab"
+	execute "/bin/sed -i 's!none            /sys/kernel/!#none            /sys/kernel/!' #{target}/lib/init/fstab"
+
+	# Deactivate unneccesary upstart services
+	execute "lxc-attach -n vmroot -- /usr/bin/rename s/\.conf/\.conf\.disabled/ #{VM_upstart}/tty*"
+	execute "lxc-attach -n vmroot -- /usr/bin/rename s/\.conf/\.conf\.disabled/ #{VM_upstart}/udev*"
+	execute "lxc-attach -n vmroot -- /usr/bin/rename s/\.conf/\.conf\.disabled/ #{VM_upstart}/upstart-*"
+	execute "lxc-attach -n vmroot -- /bin/mv #{VM_upstart}/ssh.conf #{VM_upstart}/ssh.conf.disabled"
+
+	execute "lxc-stop -n vmroot"
+	execute "sleep 1"
+	execute "lxc-start -n vmroot -d"
+	execute "sleep 5"
+
+	execute "lxc-attach -n vmroot -- /bin/hostname vmroot"
+
+	# APT-GET stuff
+	execute "lxc-attach -n vmroot -- /usr/bin/apt-get update"
+	script "install additional packages non-interactively" do
+		interpreter "bash"
+		user "root"
+		cwd "/tmp"
+		code <<-EOH
+	export DEBIAN_FRONTEND=noninteractive
+	lxc-attach -n vmroot -- /usr/bin/apt-get install #{additional_packages} -y -qq
+		EOH
+	end
+	# Install Sun Java
+	execute "lxc-attach -n vmroot -- /bin/mkdir -p /usr/share/update-sun-jre"
+	execute "lxc-attach -n vmroot -- /usr/bin/wget http://www.duinsoft.nl/pkg/pool/all/update-sun-jre.bin -O /root/update-sun-jre.bin"
+	execute "lxc-attach -n vmroot -- /bin/sh /root/update-sun-jre.bin"
+	# Configure the VMs to use LDAP lookup for users
+	execute "lxc-attach -n vmroot -- /usr/sbin/auth-client-config -t nss -p lac_ldap"
+# end
