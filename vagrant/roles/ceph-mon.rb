539f1ff5a57ed00c179c4c262028c5d5981affc5
Chris Nelson Blum <nelson@koding.com>
2013-01-11 17:29:35 +0100

    Minor change: * Aleksey removed the git cookbook (to loose windows and osx cookbook dependencies) * Removed ceph_server role as this is implemented by ceph-osd and ceph-mon

--- vagrant/roles/ceph-mon.rb
+++ vagrant/roles/ceph-mon.rb
@@ -1,3 +1,3 @@
 name "ceph-mon"
 description "The role for Ceph Monitors"
-run_list ["recipe[base_packages]", "recipe[ceph-base]","recipe[ceph-mon]"]
+run_list ["recipe[base_packages::debian]", "recipe[ceph-base]","recipe[ceph-mon]"]
--- vagrant/roles/web_server.rb
+++ vagrant/roles/web_server.rb
@@ -2,7 +2,7 @@ name "web_server"
 description "The  role for WEB servers"
 
 
-run_list ["recipe[nginx]","recipe[nginx::koding_local]", "recipe[nodejs]","recipe[golang]","recipe[git]", "recipe[build-go]"]
+run_list ["recipe[nginx]","recipe[nginx::koding_local]", "recipe[nodejs]","recipe[golang]", "recipe[build-go]"]
 
 default_attributes({ "nginx" => {
                                 "worker_processes" => "1",
