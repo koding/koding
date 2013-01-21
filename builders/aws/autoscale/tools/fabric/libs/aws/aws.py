934c096715ddedf62147d41b0398b1b121224290
alekseymykhailov <aleksey@koding.com>
2013-01-21 15:11:55 +0200

    delete pyc files add pyc to the gitignore

--- builders/aws/autoscale/tools/fabric/libs/aws/aws.py
+++ builders/aws/autoscale/tools/fabric/libs/aws/aws.py
@@ -7,9 +7,6 @@ from boto.ec2.connection import EC2Connection
 aws_access_key_id = 'AKIAJO74E23N33AFRGAQ'
 aws_secret_access_key = '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'
 
-centos_id     = 'ami-2f219746' # koding ami (centos)
-cloudlinux_id = 'ami-f2bc389b' # CloudLinux
-key_name      = 'koding'
 placement     = 'us-east-1'
 filters = {
 		'socialworker': {'tag-value': 'as_socialworker_grp'},
