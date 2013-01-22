5fa11d671e66e73fe01e5558a9eaee5a4f51c641
alekseymykhailov <aleksey@koding.com>
2013-01-22 12:24:19 +0200

    ceph ohai plugin initial

--- vagrant/cookbooks/ohai/files/default/plugins/ceph.rb
+++ vagrant/cookbooks/ohai/files/default/plugins/ceph.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 require 'rubygems'
-require 'yaml'
 require 'aws-sdk'
+require 'mash'
 
 
 config = { :access_key_id => 'AKIAJO74E23N33AFRGAQ',
@@ -10,6 +10,16 @@ config = { :access_key_id => 'AKIAJO74E23N33AFRGAQ',
 
 AWS.config(config)
 ec2 = AWS::EC2.new(:ec2_endpoint => 'ec2.us-east-1.amazonaws.com')
-ec2.instances.filter('tag-key', 'CephType').filter('tag-value', 'mon').to_a.each do |instance|
-    puts instance.id
-end
+
+ceph = Mash.new
+#ec2.instances.filter('tag-key', 'CephType').filter('tag-value', 'mon').to_a.each do |instance|
+#    puts instance.id
+#end
+
+ceph[:mon_nodes] = ec2.instances.filter('tag-key', 'CephType').filter('tag-value', 'mon').to_a
+ceph[:osd_nodes] = ec2.instances.filter('tag-key', 'CephType').filter('tag-value', 'osd').to_a
+ceph[:client_nodes] = ec2.instances.filter('tag-key', 'CephType').filter('tag-value', 'client').to_a
+
+puts ceph
+
+
