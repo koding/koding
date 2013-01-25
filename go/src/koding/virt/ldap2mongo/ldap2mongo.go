b32cd61c69d4830f9b0305a2c3b48e60bdbec66c
Richard Musiol <mail@richard-musiol.de>
2013-01-25 03:53:24 +0100

    virt: Working on ldap2mongo.

--- go/src/koding/virt/ldap2mongo/ldap2mongo.go
+++ go/src/koding/virt/ldap2mongo/ldap2mongo.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"koding/tools/db"
+	"koding/tools/utils"
 	"os/user"
 )
 
 func main() {
+	utils.Startup("ldap2mongodb", false)
 
+	iter := db.Users.Find(nil).Iter()
+	var mongoUser db.User
+	for iter.Next(&mongoUser) {
+		_, err := user.Lookup(mongoUser.Name)
+		if err != nil {
+			fmt.Println(mongoUser.Name, err.Error())
+		}
+	}
+	if iter.Err() != nil {
+		panic(iter.Err())
+	}
 }
