7d4b9d502425c8bd09fb8c18d683d79d97368a80
alekseymykhailov <aleksey@koding.com>
2013-01-25 17:09:01 +0200

    set hostname

--- vagrant/cookbooks/ceph/recipes/hosts.rb
+++ vagrant/cookbooks/ceph/recipes/hosts.rb
@@ -2,6 +2,19 @@
 # temp solution
 
 
+
+execute "set_hostname" do
+    command "/sbin/sysctl -w kernel.hostname=#{node[:ceph][:server_id]}"
+    action :nothing
+end
+
+file "/etc/hostname" do
+    action :create
+    content node[:ceph][:server_id]
+    notifies :run, "execute[set_hostname]", :immediately
+end
+
+
 template "/etc/hosts" do
   source "hosts.erb"
   variables({
