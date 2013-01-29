082aba55db8c96eb952643ad25a28c42d4061d52
alekseymykhailov <aleksey@koding.com>
2013-01-29 10:20:40 +0200

    generated templates

--- builders/aws/cloud-formation/generateTemplate.rb
+++ builders/aws/cloud-formation/generateTemplate.rb
@@ -5,11 +5,18 @@ require 'rubygems'
 require 'erb'
 require 'json'
  
- 
-bootstrap_script = IO.read("./ceph_osd_userdata.txt")
-cf_template_erb = IO.read("./ceph_cloud_formation.tmpl.erb")
+
+type = ARGV[0]
+if type.nil?
+    STDERR.puts "Usage: #{$0} <mon|osd> "
+    exit
+end
+output = "./json/#{type}_cloud_formation.tmpl.json" 
+bootstrap_script = IO.read("./user-data/ceph_#{type}_userdata.txt")
+cf_template_erb = IO.read("./templates/ceph_cloud_formation.tmpl.erb")
 cf_template = ERB.new(cf_template_erb).result(binding)
 # 
-template_file = File.new("ceph_cloud_formation.tmpl.json",'w')
+template_file = File.new(output,'w')
 template_file.puts cf_template
 template_file.close
+puts output
