d58886a1e30d786d6bdf02367198c8df01d5167d
Chris Nelson Blum <nelson@koding.com>
2013-01-30 17:05:05 +0100

    * changes to ip structure to connect VMs to the rest of the world * exchanged lighttp with apache and added ubuntu meta packages for e.g. man-db

--- vagrant/cookbooks/lxc/attributes/default.rb
+++ vagrant/cookbooks/lxc/attributes/default.rb
@@ -1,4 +1,5 @@
 default[:lxc][:network][:netmask] = '255.255.248.0'
+default[:lxc][:network][:network] = '172.16.0.0/21'
 default[:lxc][:network][:dhcp_start] = '172.16.0.2'
 default[:lxc][:network][:dhcp_end] = '172.16.7.254'
 default[:lxc][:network][:bridge_ip] = '172.16.0.1'
