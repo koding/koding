5ebb401e37f0bbd414b9b0adea7b6c5068e910cb
alekseymykhailov <aleksey@koding.com>
2013-01-30 12:16:30 +0200

    web server role update

--- vagrant/roles/web_server.rb
+++ vagrant/roles/web_server.rb
@@ -1,7 +1,7 @@
 name "web_server"
 description "The  role for WEB servers"
 
-run_list ["recipe[nginx]","recipe[nginx::koding_local]", "recipe[nodejs]","recipe[golang]", "recipe[supervisord]"]
+run_list ["role[base_server]","recipe[nginx]","recipe[nginx::koding_local]", "recipe[nodejs]","recipe[golang]", "recipe[supervisord]","recipe[kd_deploy]"]
 
 default_attributes({ "nginx" => {
                                 "worker_processes" => "1",
