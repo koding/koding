749a6f49c568e4aeee513bff58ce86af112d296f
Richard Musiol <mail@richard-musiol.de>
2013-01-30 19:33:19 +0100

    virt: Set environment.

--- go/src/koding/virt/lxc.go
+++ go/src/koding/virt/lxc.go
@@ -28,7 +28,7 @@ func (vm *VM) AttachCommand(uid int, tty string, command ...string) *exec.Cmd {
 	args = append(args, "--", "/usr/bin/sudo", "-i", "-u", "#"+strconv.Itoa(uid))
 	args = append(args, command...)
 	cmd := exec.Command("/usr/bin/lxc-attach", args...)
-	//cmd.Env = []string{"TERM=xterm"}
+	cmd.Env = []string{"TERM=xterm"}
 	return cmd
 }
 
