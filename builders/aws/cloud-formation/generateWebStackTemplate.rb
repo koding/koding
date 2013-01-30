1524f7cad5713eaa299781e296b8223ae182776c
alekseymykhailov <aleksey@koding.com>
2013-01-30 13:23:37 +0200

    add guestclenup to the infrastructure config

--- builders/aws/cloud-formation/generateWebStackTemplate.rb
+++ builders/aws/cloud-formation/generateWebStackTemplate.rb
@@ -7,10 +7,12 @@ require 'json'
  
 web_server_userdata = "./user-data/web_server-userdata.txt"
 socialworker_userdata = "./user-data/socialworker-userdata.txt"
+guestcleanup_userdata = "./user-data/guestcleanup-userdata.txt"
 
 output = "./json/web_stack_autoscale.tmpl.json" 
 web_server_bootstrap_script = IO.read(web_server_userdata)
 socialworker_bootstrap_script = IO.read(socialworker_userdata)
+guestcleanup_bootstrap_script = IO.read(guestcleanup_userdata)
 
 cf_template_erb = IO.read("./templates/web_stack_autoscale.tmpl.erb")
 cf_template = ERB.new(cf_template_erb).result(binding)
