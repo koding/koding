e8ec9fdccf90b6a7b4bb21e37ff0993cb96a01a1
Chris Nelson Blum <nelson@koding.com>
2013-01-30 14:46:20 +0100

    deactivated golang cookbook since the repo is not working for debian currently

--- vagrant/roles/ceph-client.rb
+++ vagrant/roles/ceph-client.rb
@@ -1,5 +1,6 @@
 name "ceph-client"
 description "The role for Ceph Clients (accessing RBD storages e.g. for VM users)"
+# run_list ["recipe[base_packages::debian]", "recipe[build-essential]", "recipe[hosts]", "recipe[golang]", "recipe[build-go]", "recipe[lxc]", "recipe[lxc::prepareVMRoot]", "recipe[ceph-base]", "recipe[ceph]", "recipe[nodejs]"]
 run_list ["recipe[base_packages::debian]", "recipe[build-essential]", "recipe[hosts]", "recipe[lxc]", "recipe[lxc::prepareVMRoot]", "recipe[ceph-base]", "recipe[ceph]", "recipe[nodejs]"]
 
 default_attributes({ 
