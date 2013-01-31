4084bbc5d44003bc4db758a4494391039d3572a6
alekseymykhailov <aleksey@koding.com>
2013-01-31 17:46:59 +0200

    update broker role

--- vagrant/roles/broker.rb
+++ vagrant/roles/broker.rb
@@ -1,7 +1,7 @@
 name "broker"
 description "The  role for Broker servers"
 
-run_list ["recipe[nodejs]","recipe[golang]","recipe[supervisord]"]
+run_list ["role[base_server]","recipe[nodejs]","recipe[golang]", "recipe[supervisord]","recipe[kd_deploy]"]
 
 default_attributes({
                      "kd_deploy" => {
