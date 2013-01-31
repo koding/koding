2a5e1639f123e88b8d103666204d2313689e9b7a
alekseymykhailov <aleksey@koding.com>
2013-01-31 19:19:26 +0200

    separate file for each cloud-formation conf

--- builders/aws/cloud-formation/generateWebStackTemplate.rb
+++ builders/aws/cloud-formation/generateWebStackTemplate.rb
@@ -4,25 +4,17 @@
 require 'rubygems'
 require 'erb'
 require 'json'
- 
-web_server_userdata = "./user-data/webstack/web_server-userdata.txt"
-socialworker_userdata = "./user-data/webstack/socialworker-userdata.txt"
-guestcleanup_userdata = "./user-data/webstack/guestcleanup-userdata.txt"
-authworker_userdata = "./user-data/webstack/authworker-userdata.txt"
-broker_userdata = "./user-data/webstack/broker-userdata.txt"
-cacheworker_userdata = "./user-data/webstack/cacheworker-userdata.txt"
-emailworker_userdata = "./user-data/webstack/cacheworker-userdata.txt"
+type = ARGV[0]
+if type.nil?
+    STDERR.puts "Usage: #{$0} server_type "
+    exit
+end 
 
-output = "./json/web_stack_autoscale.tmpl.json" 
-web_server_bootstrap_script = IO.read(web_server_userdata)
-socialworker_bootstrap_script = IO.read(socialworker_userdata)
-guestcleanup_bootstrap_script = IO.read(guestcleanup_userdata)
-authworker_bootstrap_script = IO.read(authworker_userdata)
-broker_bootstrap_script = IO.read(broker_userdata)
-cacheworker_bootstrap_script = IO.read(cacheworker_userdata)
-emailworker_bootstrap_script = IO.read(cacheworker_userdata)
+userdata = "./user-data/webstack/#{type}-userdata.txt"
+output = "./json/webstack/#{type}.tmpl.json" 
+bootstrap_script = IO.read(userdata)
 
-cf_template_erb = IO.read("./templates/web_stack_autoscale.tmpl.erb")
+cf_template_erb = IO.read("./templates/webstack/#{type}.tmpl.erb")
 cf_template = ERB.new(cf_template_erb).result(binding)
 
 template_file = File.new(output,'w')
