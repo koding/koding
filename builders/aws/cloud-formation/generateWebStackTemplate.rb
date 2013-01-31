092a4469ee57e6825b76e7cd15bb86496495c7e4
alekseymykhailov <aleksey@koding.com>
2013-01-31 17:52:56 +0200

    add broker to the cloud-formation cfg

--- builders/aws/cloud-formation/generateWebStackTemplate.rb
+++ builders/aws/cloud-formation/generateWebStackTemplate.rb
@@ -9,12 +9,14 @@ web_server_userdata = "./user-data/web_server-userdata.txt"
 socialworker_userdata = "./user-data/socialworker-userdata.txt"
 guestcleanup_userdata = "./user-data/guestcleanup-userdata.txt"
 authworker_userdata = "./user-data/authworker-userdata.txt"
+broker_userdata = "./user-data/broker-userdata.txt"
 
 output = "./json/web_stack_autoscale.tmpl.json" 
 web_server_bootstrap_script = IO.read(web_server_userdata)
 socialworker_bootstrap_script = IO.read(socialworker_userdata)
 guestcleanup_bootstrap_script = IO.read(guestcleanup_userdata)
 authworker_bootstrap_script = IO.read(authworker_userdata)
+broker_bootstrap_script = IO.read(broker_userdata)
 
 cf_template_erb = IO.read("./templates/web_stack_autoscale.tmpl.erb")
 cf_template = ERB.new(cf_template_erb).result(binding)
