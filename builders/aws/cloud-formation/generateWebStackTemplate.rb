5d292efdd00e9c9bbe65b3cdb742c810e64a5b95
alekseymykhailov <aleksey@koding.com>
2013-01-31 18:42:45 +0200

    add email worker to the cloud-formation cfg

--- builders/aws/cloud-formation/generateWebStackTemplate.rb
+++ builders/aws/cloud-formation/generateWebStackTemplate.rb
@@ -5,12 +5,13 @@ require 'rubygems'
 require 'erb'
 require 'json'
  
-web_server_userdata = "./user-data/web_server-userdata.txt"
-socialworker_userdata = "./user-data/socialworker-userdata.txt"
-guestcleanup_userdata = "./user-data/guestcleanup-userdata.txt"
-authworker_userdata = "./user-data/authworker-userdata.txt"
-broker_userdata = "./user-data/broker-userdata.txt"
-cacheworker_userdata = "./user-data/cacheworker-userdata.txt"
+web_server_userdata = "./user-data/webstack/web_server-userdata.txt"
+socialworker_userdata = "./user-data/webstack/socialworker-userdata.txt"
+guestcleanup_userdata = "./user-data/webstack/guestcleanup-userdata.txt"
+authworker_userdata = "./user-data/webstack/authworker-userdata.txt"
+broker_userdata = "./user-data/webstack/broker-userdata.txt"
+cacheworker_userdata = "./user-data/webstack/cacheworker-userdata.txt"
+emailworker_userdata = "./user-data/webstack/cacheworker-userdata.txt"
 
 output = "./json/web_stack_autoscale.tmpl.json" 
 web_server_bootstrap_script = IO.read(web_server_userdata)
@@ -19,6 +20,7 @@ guestcleanup_bootstrap_script = IO.read(guestcleanup_userdata)
 authworker_bootstrap_script = IO.read(authworker_userdata)
 broker_bootstrap_script = IO.read(broker_userdata)
 cacheworker_bootstrap_script = IO.read(cacheworker_userdata)
+emailworker_bootstrap_script = IO.read(cacheworker_userdata)
 
 cf_template_erb = IO.read("./templates/web_stack_autoscale.tmpl.erb")
 cf_template = ERB.new(cf_template_erb).result(binding)
