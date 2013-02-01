147cc37be51a556d1e20ba9121fc015b4760ca78
alekseymykhailov <aleksey@koding.com>
2013-02-01 17:29:32 +0200

    update environments

--- vagrant/environments/prod-leg-a.rb
+++ vagrant/environments/prod-leg-a.rb
@@ -1,10 +1,10 @@
 name "prod-leg-a"
-description "The production  A environment"
+description "The staging environment"
 
 cookbook_versions({
     "local_users"   => "0.1.3",
     "apt"           => "1.7.0",
-    "base_packages" => "1.1.0",
+    "base_packages" => "0.1.0",
     "erlang"        => "1.1.2",
     "golang"        => "0.1.0",
     "golang"        => "0.1.0",
@@ -15,5 +15,31 @@ cookbook_versions({
     "rabbitmq"      => "1.7.0",
     "supervisord"   => "0.1.0",
     "users"         => "1.3.0",
-    "yum"           => "2.0.6"
+    "yum"           => "2.0.6",
+    "kd_deploy"     => "0.1.1",
+    "ntp"           => "1.3.2"
+})
+
+
+default_attributes({ 
+                     "launch" => {
+                                 "config" => "stage",
+                     },
+                     "kd_deploy" => {
+                                "git_branch" => "master_STAGE",
+                                "revision_tag" => "HEAD",
+                                "release_action" => :deploy,
+                                "deploy_dir" => '/opt/koding',
+                     },
+                    "nginx" => {
+                                "worker_processes" => "1",
+                                "backend_ports" => [3020],
+                                "server_name" => "koding.com",
+                                "maintenance_page" => "maintenance.html",
+                                "static_files" => "/opt/koding/current/client"
+                     },
+                     :rabbitmq => {
+                                :admin_password => "dslkdscmckfjf55",
+                                :user_password => "djfjfhgh4455__5"
+                     }
 })
