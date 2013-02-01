b37c17ca9304bf4d447fdb91495e9858fbe99c20
alekseymykhailov <aleksey@koding.com>
2013-02-01 17:28:16 +0200

    update templates

--- builders/aws/cloud-formation/generateSysTemplate_staging.rb
+++ builders/aws/cloud-formation/generateSysTemplate_staging.rb
@@ -4,20 +4,23 @@
 require 'rubygems'
 require 'erb'
 require 'json'
-type = ARGV[0]
-if type.nil?
-    STDERR.puts "Usage: #{$0} server_type "
-    exit
-end 
 
-userdata = "./user-data/sys_staging/#{type}-userdata.txt"
-output = "./json/sys_staging/#{type}.tmpl.json" 
-bootstrap_script = IO.read(userdata)
+env = "staging"
 
-cf_template_erb = IO.read("./templates/sys/#{type}.tmpl.erb")
-cf_template = ERB.new(cf_template_erb).result(binding)
 
-template_file = File.new(output,'w')
-template_file.puts cf_template
-template_file.close
-puts output
+roles = %w( rabbitmq_server )
+
+roles.each do |role|
+    userdata = "./user-data/userdata.txt.erb"
+    output = "./json/sys_#{env}/#{role}.tmpl.json" 
+    userdata = IO.read(userdata)
+    bootstrap_script = ERB.new(userdata).result(binding)
+
+    cf_template_erb = IO.read("./templates/sys_#{env}/#{role}.tmpl.erb")
+    cf_template = ERB.new(cf_template_erb).result(binding)
+
+    template_file = File.new(output,'w')
+    template_file.puts cf_template
+    template_file.close
+    puts output
+end
--- builders/aws/cloud-formation/generateWebStackTemplate_staging.rb
+++ builders/aws/cloud-formation/generateWebStackTemplate_staging.rb
@@ -4,20 +4,23 @@
 require 'rubygems'
 require 'erb'
 require 'json'
-type = ARGV[0]
-if type.nil?
-    STDERR.puts "Usage: #{$0} server_type "
-    exit
-end 
 
-userdata = "./user-data/webstack_staging/#{type}-userdata.txt"
-output = "./json/webstack_staging/#{type}.tmpl.json" 
-bootstrap_script = IO.read(userdata)
+env = "staging"
 
-cf_template_erb = IO.read("./templates/webstack/#{type}.tmpl.erb")
-cf_template = ERB.new(cf_template_erb).result(binding)
 
-template_file = File.new(output,'w')
-template_file.puts cf_template
-template_file.close
-puts output
+roles = %w( authworker  broker  cacheworker  emailworker  guestcleanup  socialworker  web_server )
+
+roles.each do |role|
+    userdata = "./user-data/userdata.txt.erb"
+    output = "./json/webstack_#{env}/#{role}.tmpl.json" 
+    userdata = IO.read(userdata)
+    bootstrap_script = ERB.new(userdata).result(binding)
+
+    cf_template_erb = IO.read("./templates/webstack_#{env}/#{role}.tmpl.erb")
+    cf_template = ERB.new(cf_template_erb).result(binding)
+
+    template_file = File.new(output,'w')
+    template_file.puts cf_template
+    template_file.close
+    puts output
+end
