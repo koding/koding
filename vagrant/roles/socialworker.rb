daae4c5088846c0e5d6b16b25dc73d411c8b4cc1
alekseymykhailov <aleksey@koding.com>
2013-02-01 10:45:58 +0200

    social workere role update

--- vagrant/roles/socialworker.rb
+++ vagrant/roles/socialworker.rb
@@ -4,14 +4,7 @@ description "The  role for socialWorker servers"
 run_list ["role[base_server]","recipe[nodejs]","recipe[golang]", "recipe[supervisord]","recipe[kd_deploy]"]
 
 default_attributes({ 
-                     "kd_deploy" => {
-                                "git_branch" => "master_autoscale",
-                                "revision_tag" => "HEAD",
-                                "release_action" => :deploy,
-                                "deploy_dir" => '/opt/koding',
-                     },
                      "launch" => {
-                                "config" => "autoscale",
                                 "programs" => ["socialWorker"]
                      }
 })
