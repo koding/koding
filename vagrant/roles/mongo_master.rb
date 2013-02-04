932c926031e12c96e7474bba0c1c7ff0185a705f
alekseymykhailov <aleksey@koding.com>
2013-02-04 15:58:17 -0800

    new mongodb version

--- vagrant/roles/mongo_master.rb
+++ vagrant/roles/mongo_master.rb
@@ -7,7 +7,7 @@ env_run_lists "prod" =>  ["role[base_server]", "recipe[mongodb]"],
 
 default_attributes({ "mongodb" => {
                                 "master" => true,
-                                "version" => "2.2.2",
+                                "version" => "2.2.3",
                                 "data_device" => "/dev/vg0/fs_mongo_data",
                                 "log_device"  => "/dev/vg1/fs_mongo_log",
                                 "rest" => true,
