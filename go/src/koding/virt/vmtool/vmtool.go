4a02f330f89302ab9be96ecea5b8525c09141b2f
Richard Musiol <mail@richard-musiol.de>
2013-02-06 19:11:19 +0100

    virt: Added vmtool.

---
+++ go/src/koding/virt/vmtool/vmtool.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"koding/tools/utils"
+	"koding/virt"
+	"labix.org/v2/mgo/bson"
+	"net"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+var actions = map[string]func(){
+	"shutdown": func() {
+		withAll("lxc-shutdown")
+	},
+	"stop": func() {
+		withAll("lxc-stop")
+	},
+	"unprepare": func() {
+		for _, vm := range selectVMs(os.Args[2]) {
+			err := vm.Unprepare()
+			fmt.Printf("%v: %v\n", vm, err)
+		}
+	},
+	"create-test-vms": func() {
+		ipPoolFetch, _ := utils.NewIntPool(utils.IPToInt(net.IPv4(172, 16, 0, 2)), nil)
+		count, _ := strconv.Atoi(os.Args[2])
+		for i := 0; i < count; i++ {
+			fmt.Println(i)
+			vm := virt.VM{
+				Id: bson.NewObjectId(),
+				IP: utils.IntToIP(<-ipPoolFetch),
+			}
+			vm.Prepare(nil)
+			vm.StartCommand().Run()
+		}
+	},
+}
+
+func main() {
+	virt.LoadTemplates("templates")
+	action := actions[os.Args[1]]
+	action()
+}
+
+func selectVMs(selector string) []*virt.VM {
+	if selector == "all" {
+		dirs, err := ioutil.ReadDir("/var/lib/lxc")
+		if err != nil {
+			panic(err)
+		}
+		vms := make([]*virt.VM, 0)
+		for _, dir := range dirs {
+			if strings.HasPrefix(dir.Name(), "vm-") {
+				vms = append(vms, &virt.VM{Id: bson.ObjectIdHex(dir.Name()[3:])})
+			}
+		}
+		return vms
+	}
+	fmt.Println("Invalid selector: " + selector)
+	os.Exit(1)
+	return nil
+}
+
+func withAll(action string) {
+	for _, vm := range selectVMs(os.Args[2]) {
+		cmd := exec.Command(action, "-n", vm.String())
+		out, err := cmd.CombinedOutput()
+		fmt.Println(strings.Join(cmd.Args, " ") + ":")
+		fmt.Println(err)
+		fmt.Println(string(out))
+	}
+}
