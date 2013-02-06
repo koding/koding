c64a2410aa6b1ecdee34caea5f53cd2d54709d10
Chris Nelson Blum <nelson@koding.com>
2013-02-06 19:12:35 +0100

    ceph stop doesn't work with alexsey's ceph cluster...

--- vagrant/cookbooks/ceph/recipes/client.rb
+++ vagrant/cookbooks/ceph/recipes/client.rb
@@ -7,6 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+service "ceph" do
+        action :stop
+        stop_command "/etc/init.d/ceph -a stop"
+end
+
 include_recipe "ceph::ssh_keys"
 
 include_recipe "ceph::server_cfg"
