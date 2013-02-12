64de3dfc230b8772c68f98f42b960ab99c10d79d
alekseymykhailov <aleksey@koding.com>
2013-02-12 08:49:31 -0800

    enable remote_syslog

--- vagrant/roles/authworker.rb
+++ vagrant/roles/authworker.rb
@@ -1,10 +1,14 @@
 name "authworker"
 description "The  role for authWorker servers"
 
-run_list ["role[base_server]","recipe[nodejs]","recipe[golang]", "recipe[supervisord]","recipe[kd_deploy]"]
+run_list ["role[base_server]","recipe[nodejs]","recipe[golang]", "recipe[supervisord]","recipe[papertrail]","recipe[kd_deploy]"]
 
 default_attributes({ 
                      "launch" => {
                                 "programs" => ["authWorker"]
+                     },
+                     "log" => {
+                                "files" => ["/var/log/authWorker.log"]       
                      }
+
 })
--- vagrant/roles/cacheworker.rb
+++ vagrant/roles/cacheworker.rb
@@ -1,10 +1,14 @@
 name "cacheworker"
 description "The  role for cacheworker servers"
 
-run_list ["role[base_server]","recipe[nodejs]","recipe[golang]", "recipe[supervisord]","recipe[kd_deploy]"]
+run_list ["role[base_server]","recipe[nodejs]","recipe[golang]", "recipe[supervisord]","recipe[papertrail]","recipe[kd_deploy]"]
 
 default_attributes({ 
                      "launch" => {
                                 "programs" => ["cacheWorker"]
+                     },
+                     "log" => {
+                                "files" => ["/var/log/cacheWorker.log"]       
                      }
+                     
 })
