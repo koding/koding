4cdc0ef1bf8a299b9907d5a688fb14139983164b
alekseymykhailov <aleksey@koding.com>
2013-02-13 11:30:45 -0800

    update revision

--- vagrant/environments/prod-webstack-b.rb
+++ vagrant/environments/prod-webstack-b.rb
@@ -25,7 +25,7 @@ default_attributes({
                      },
                      "kd_deploy" => {
                                 "git_branch" => "master_RC",
-                                "revision_tag" => "koding-rc-4",
+                                "revision_tag" => "koding-rc-5",
                                 "release_action" => :deploy,
                                 "deploy_dir" => '/opt/koding',
                                 "rabbit_host" => "rabbit-b.prod.aws.koding.com"
