883718a934878d63274426b4adcb95334bf4ca0d
Chris Nelson Blum <nelson@koding.com>
2013-03-12 12:34:52 -0700

    added new vpn cookbook for route generation (in the future) updated ceph-client role with new user cookbook updated locale_user cookbook with new user names

---
+++ vagrant/cookbooks/vpn_setup/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: vpn_setup
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
--- vagrant/roles/ceph-client.rb
+++ vagrant/roles/ceph-client.rb
@@ -1,7 +1,7 @@
 name "ceph-client"
 description "The role for Ceph Clients (accessing RBD storages e.g. for VM users)"
 # For AWS add kd_clone!
-run_list ["recipe[base_packages::debian]", "recipe[build-essential]", "recipe[hosts]", "recipe[build-go]" , "recipe[lxc]", "recipe[lxc::prepareVMRoot]", "recipe[ceph]", "recipe[nodejs]", "recipe[local_users]"]
+run_list ["recipe[base_packages::debian]", "recipe[build-essential]", "recipe[hosts]", "recipe[build-go]" , "recipe[lxc]", "recipe[lxc::prepareVMRoot]", "recipe[ceph]", "recipe[nodejs]", "recipe[users::sysadmins]", "recipe[users::developer]"]
 
 default_attributes({ 
                      "kd_clone" => {
