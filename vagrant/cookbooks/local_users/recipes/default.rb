8cff956ab43db43233aa2a9d1615b6c050159432
Chris Nelson Blum <nelson@koding.com>
2013-03-12 12:34:52 -0700

    added new vpn cookbook for route generation (in the future) updated ceph-client role with new user cookbook updated locale_user cookbook with new user names

--- vagrant/cookbooks/local_users/recipes/default.rb
+++ vagrant/cookbooks/local_users/recipes/default.rb
@@ -14,10 +14,12 @@ end
 # bahadir  cthorn  dyasar  ec2-user  ggoksel  ri
 local_users_prod = {
                    "bahadir" => { "id" => 607, "pass" => "$1$lkG1zSHa$DkkXZsCYYX0i8jv2NhkJl.", "sshkey" => true},
-                   "cthorn"  => { "id" => 602, "pass" => "$1$PSbDpLs/$Y1uyQci7JEkLvvoQ9dnMB1", "sshkey" => true},
+                   "chris"  => { "id" => 602, "pass" => "$1$PSbDpLs/$Y1uyQci7JEkLvvoQ9dnMB1", "sshkey" => true},
                    "dyasar"  => { "id" => 601, "pass" => "$1$B4tVUcrC$rUeL1FMDHKBZ41c4w1yj4.", "sshkey" => true},
-                   "ggoksel" => { "id" => 606, "pass" => "$1$8xu2mplx$S8QqBws9V7nctl4u1ouMc/", "sshkey" => true},
+                   "gokmen" => { "id" => 606, "pass" => "$1$8xu2mplx$S8QqBws9V7nctl4u1ouMc/", "sshkey" => true},
                    "richard" => { "id" => 605, "pass" => "$1$O.JwAzmg$0ONO0g4t8SE1gOipz0Woh.", "sshkey" => true},
+                   "senthil" => { "id" => 605, "pass" => "$1$O.JwAzmg$0ONO0g4t8SE1gOipz0Woh.", "sshkey" => true},
+                   "fatihacet" => { "id" => 605, "pass" => "$1$O.JwAzmg$0ONO0g4t8SE1gOipz0Woh.", "sshkey" => true},
                    "cblum"   => { "id" => 666, "pass" => "$6$A9k6e/lC$Y59fsZYIV8iMLPkG9l.SuC8qwBMpQUFq4lMfAjKPQDcJ2dPw8nwrW1IsSNeDNYNMB/zOXgpPZjeK6fgvMt/Bn0", "sshkey" => true},
                  }
                 
