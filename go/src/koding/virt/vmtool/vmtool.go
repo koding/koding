25173184b71703719341e2cc9fc8b1d0ab81c62c
Armagan Kimyonoglu <armagan@koding.com>
2013-03-20 19:49:13 -0700

    backup VMs (without s3)

--- go/src/koding/virt/vmtool/vmtool.go
+++ go/src/koding/virt/vmtool/vmtool.go
@@ -80,6 +80,12 @@ var actions = map[string]func(){
 			fmt.Println(<-done)
 		}
 	},
+	"backup": func() {
+		for _, vm := range selectVMs(os.Args[2]) {
+			err := vm.Backup()
+			fmt.Printf("%v: %v\n", vm, err)
+		}
+	},
 	"dpkg-statistics": func() {
 		entries, err := ioutil.ReadDir("/var/lib/lxc/dpkg-statuses")
 		if err != nil {
