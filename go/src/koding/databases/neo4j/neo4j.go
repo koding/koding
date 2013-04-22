427547c9ea1a05cdbf91e6d0f9b64ae47de995fd
Cihangir SAVAS <cihangir@koding.com>
2013-04-22 10:50:05 -0700

    [go][koding][database][neo4j] added node deletion support

--- go/src/koding/databases/neo4j/neo4j.go
+++ go/src/koding/databases/neo4j/neo4j.go
@@ -14,35 +14,41 @@ var (
 	// todo update this constants, here must be only config file related strings after config files updated  
 	// BASE_URL         = config.Current.Neo4j.Url + config.Current.Neo4j.Port  // "http://localhost:7474"
 	BASE_URL         = "http://localhost:7474"
+	NODE_PATH        = "/db/data/index/node/koding/"
 	UNIQUE_NODE_PATH = "/db/data/index/node/koding?unique"
 	INDEX_PATH       = "/db/data/index/node"
 )
 
 // Gets URL and string data to be sent and makes POST request
 // reads response body and returns as string
-func sendRequest(url string, data string) string {
+func sendRequest(requestType, url, data string) string {
+
 	//convert string into bytestream
 	dataByte := strings.NewReader(data)
+	req, err := http.NewRequest(requestType, url, dataByte)
 
-	//make post request
-	req, err := http.Post(url, "application/json", dataByte)
+	// read response body
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// read response body
-	body, _ := ioutil.ReadAll(req.Body)
+	body, _ := ioutil.ReadAll(res.Body)
 
-	defer req.Body.Close()
+	defer res.Body.Close()
 
 	return string(body)
+
 }
 
 // connect source and target with relation property
 func CreateRelationship(relation, source, target string) map[string]interface{} {
 
 	relationshipData := fmt.Sprintf(`{"to" : "%s", "type" : "%s" }`, target, relation)
-	relRes := sendRequest(fmt.Sprintf("%s", source), relationshipData)
+	relRes := sendRequest("POST", fmt.Sprintf("%s", source), relationshipData)
 
 	relNode, err := jsonDecode(relRes)
 	if err != nil {
@@ -59,7 +65,22 @@ func CreateUniqueNode(id string, name string) map[string]interface{} {
 
 	postData := generatePostJsonData(id, name)
 
-	response := sendRequest(url, postData)
+	response := sendRequest("POST", url, postData)
+
+	nodeData, err := jsonDecode(response)
+	if err != nil {
+		fmt.Println("Problem with response", response)
+	}
+
+	return nodeData
+}
+
+// creates a unique node with given id and node name
+func DeleteNode(id string) map[string]interface{} {
+
+	url := BASE_URL + NODE_PATH
+
+	response := sendRequest("DELETE", url, "")
 
 	nodeData, err := jsonDecode(response)
 	if err != nil {
@@ -75,7 +96,7 @@ func CreateUniqueIndex(name string) {
 	//create unique index
 	url := BASE_URL + INDEX_PATH
 
-	bd := sendRequest(url, `{"name":"`+name+`"}`)
+	bd := sendRequest("POST", url, `{"name":"`+name+`"}`)
 
 	fmt.Println("Created unique index for data", bd)
 }
