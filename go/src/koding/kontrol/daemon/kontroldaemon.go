64345fe3e31080112ed96f24af940579fb15bf6e
Fatih Arslan <fatih@koding.com>
2013-04-29 14:00:29 +0300

    Remove unused function

--- go/src/koding/kontrol/daemon/kontroldaemon.go
+++ go/src/koding/kontrol/daemon/kontroldaemon.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/streadway/amqp"
 	"koding/kontrol/daemon/handler"
 	"koding/kontrol/daemon/handler/proxy"
@@ -154,20 +153,4 @@ func startRouting() error {
 	}
 
 	return nil
-}
-
-func (c *Consumer) Shutdown() error {
-	// will close() the deliveries channel
-	if err := c.channel.Cancel(c.tag, true); err != nil {
-		return fmt.Errorf("Consumer cancel failed: %s", err)
-	}
-
-	if err := c.conn.Close(); err != nil {
-		return fmt.Errorf("AMQP connection close error: %s", err)
-	}
-
-	defer log.Printf("AMQP shutdown OK")
-
-	// wait for handle() to exit
-	return <-c.done
 }
