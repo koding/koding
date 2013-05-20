e95fe2b9c95a5ff4520f3160eaff628ad451cbe3
Geraint Jones <geraint@koding.com>
2013-05-21 11:27:21 +1200

    Change vhost definition, remove default vhost

--- go/src/koding/kites/os/oskite.go
+++ go/src/koding/kites/os/oskite.go
@@ -339,11 +339,13 @@ func registerVmMethod(k *kite.Kite, method string, concurrent bool, callback fun
 					if err != nil {
 						panic(err)
 					}
-					file, err := rootVos.Remove("/etc/apache2/sites-available/000-default")
+					defer file.Close()
+				    
+				    err := rootVos.Remove("/etc/apache2/sites-available/000-default")
 					if err != nil {
 						panic(err)
 					}
-					defer file.Close()
+					
 					if err := virt.Templates.ExecuteTemplate(file, "apache-site", vm); err != nil {
 						panic(err)
 					}
