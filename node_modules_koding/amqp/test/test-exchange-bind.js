112f0295ceede51a27e45e1dbaf7168fc8a18595
Christopher Thorn <chris@koding.com>
2013-05-22 11:38:37 -0700

    node-amqp: merge koding fork with latest and greatest

---
+++ node_modules_koding/amqp/test/test-exchange-bind.js
@@ -0,0 +1,37 @@
+require('./harness');
+var testName = __filename.replace(__dirname+'/','').replace('.js','');
+var msgsReceived = 0;
+
+connection.addListener('ready', function () {
+    puts("connected to " + connection.serverProperties.product);
+    var callbackCalled = false;
+    
+    connection.exchange('node.'+testName+'.dstExchange', {type: 'topic'}, function(dstExchange) {
+        connection.exchange('node.'+testName+'.srcExchange', {type: 'topic'}, function(srcExchange) {
+            dstExchange.bind(srcExchange, '#', function () {
+                connection.queue( 'node.'+testName+'.nestedExchangeQueue', { durable: false, autoDelete : true },  function (queue) {
+                    queue.bind(dstExchange, '#', function () {
+                        queue.subscribe(function ( msg ) {
+                            puts(msg.data.toString());
+                            msgsReceived++;
+                        });
+                        srcExchange.publish('node.'+testName+'.nestedExchangeTest', 
+                            'Queue received message from non-directly-bound exchange.');
+
+                        setTimeout(function () {
+                          // wait one second to receive the message, then quit
+                          queue.destroy();
+                          dstExchange.destroy();
+                          srcExchange.destroy();
+                          connection.end();
+                        }, 1000);
+                    });
+                });
+            });
+        });
+    });
+});
+
+process.addListener('exit', function() {
+  assert.equal(1, msgsReceived);
+});
