9cafdae12fcaf89126d0ff8b3c86f5edcb920aee
Christopher Thorn <chris@koding.com>
2013-05-30 16:00:45 -0700

    followfeed: stress test; use a goroutine

--- go/src/koding/followfeed/producer.go
+++ go/src/koding/followfeed/producer.go
@@ -21,25 +21,26 @@ func startPublishing() {
 	channel := amqputil.CreateChannel(conn)
 
 	for i := 0; i < scale; i++ {
+		go func() {
+			key := userNum(i)
 
-		key := userNum(i)
-
-		if err := channel.Publish(
-			key,   // exchange
-			key,   // routing key
-			false, // mandatory
-			false, // immediate
-			amqp.Publishing{
-				Headers:         amqp.Table{},
-				ContentType:     "text/plain",
-				ContentEncoding: "",
-				Body:            []byte("hello to " + key),
-				DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
-				Priority:        0,              // 0-9
-				// a bunch of application/implementation-specific fields
-			},
-		); err != nil {
-			panic(err)
-		}
+			if err := channel.Publish(
+				key,   // exchange
+				key,   // routing key
+				false, // mandatory
+				false, // immediate
+				amqp.Publishing{
+					Headers:         amqp.Table{},
+					ContentType:     "text/plain",
+					ContentEncoding: "",
+					Body:            []byte("hello to " + key),
+					DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
+					Priority:        0,              // 0-9
+					// a bunch of application/implementation-specific fields
+				},
+			); err != nil {
+				panic(err)
+			}
+		}()
 	}
 }
