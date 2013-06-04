29abd4fa4c2a144436fc098251ad1f31e344838b
Christopher Thorn <chris@koding.com>
2013-06-03 17:53:04 -0700

    user presence: working onlineStatus flags
    WIP for queue bindings needed for realtime updates for follow feed.

--- go/src/koding/userpresence/userpresence.go
+++ go/src/koding/userpresence/userpresence.go
@@ -4,18 +4,26 @@ import (
 	"github.com/streadway/amqp"
 	"koding/databases/mongo"
 	"koding/tools/amqputil"
+	"koding/tools/config"
 	"labix.org/v2/mgo/bson"
 	"log"
+	"strings"
 )
 
+var followFeedChannel *amqp.Channel
+
 func main() {
+	var err error
+	if followFeedChannel, err = createFollowFeedChannel("followfeed"); err != nil {
+		panic(err)
+	}
 	startMonitoring()
 }
 
 func startMonitoring() {
-	amqpConn := amqputil.CreateConnection("userpresence")
+	mainAmqpConn := amqputil.CreateConnection("userpresence")
 
-	channel, err := amqpConn.Channel()
+	channel, err := mainAmqpConn.Channel()
 	if err != nil {
 		panic(err)
 	}
@@ -31,6 +39,7 @@ func startMonitoring() {
 		false,        // no wait
 		nil,          // arguments
 	)
+
 	channel.QueueDeclare(
 		resourceName, // the queue name
 		false,        // durable
@@ -39,6 +48,7 @@ func startMonitoring() {
 		false,        // no wait
 		nil,          // arguments
 	)
+
 	channel.QueueBind(
 		resourceName, // queue name
 		"",           // binding key (use an empty key to monitor messages)
@@ -46,6 +56,7 @@ func startMonitoring() {
 		false,        // no wait
 		nil,          // arguments
 	)
+
 	deliveries, err := channel.Consume(
 		resourceName, // queue name
 		"",           // ctag
@@ -55,33 +66,135 @@ func startMonitoring() {
 		false,        // no wait
 		nil,          // arguments
 	)
+
 	go handle(deliveries, make(chan error))
 	select {}
 }
 
-func handleJoin(username string) error {
+func createFollowFeedChannel(component string) (*amqp.Channel, error) {
+	conn, err := amqp.Dial(amqp.URI{
+		Scheme:   "amqp",
+		Host:     config.Current.FollowFeed.Host,
+		Port:     config.Current.FollowFeed.Port,
+		Username: strings.Replace(config.Current.FollowFeed.ComponentUser, "<component>", component, 1),
+		Password: config.Current.FollowFeed.Password,
+		Vhost:    config.Current.FollowFeed.Vhost,
+	}.String())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return conn.Channel()
+}
+
+func updateOnlineStatus(username, status string) error {
 	userCollection := mongo.GetCollection("jUsers")
 	accountCollection := mongo.GetCollection("jAccounts")
+
 	user := make(map[string]interface{})
 	account := make(map[string]interface{})
+
 	if err := userCollection.Find(bson.M{"username": username}).One(&user); err != nil {
 		return err
 	}
 	if err := accountCollection.Find(bson.M{"profile.nickname": username}).One(&account); err != nil {
 		return err
 	}
-	log.Printf("%s: %v %v", user, account)
+	// update the user's actual status
+	userCollection.Update(
+		bson.M{"_id": user["_id"]},
+		bson.M{
+			"$set": bson.M{
+				"onlineStatus.actual": status,
+			},
+		},
+	)
+
+	onlineStatus := make(map[string]string)
+	if user["onlineStatus"] == nil {
+		onlineStatus = user["onlineStatus"].(map[string]string)
+	}
+
+	var publicStatus string
+
+	if onlineStatus["userPreference"] == "" {
+		publicStatus = status
+	} else {
+		publicStatus = onlineStatus["userPreference"]
+	}
+	// update the user's public status
+	accountCollection.Update(
+		bson.M{"_id": account["_id"]},
+		bson.M{
+			"$set": bson.M{
+				"onlineStatus": publicStatus,
+			},
+		},
+	)
+
 	return nil
 }
 
-func handleLeave(user string) error {
-	log.Println(user + " has left")
+func changeFollowFeedExchangeBindings(username, action string) error {
+	if err := followFeedChannel.ExchangeDeclare(
+		username, // exchange name
+		"topic",  // kind
+		false,    // durable
+		false,    // auto delete
+		false,    // internal
+		false,    // no wait
+		nil,      // arguments
+	); err != nil {
+		return err
+	}
+
+	if _, err := followFeedChannel.QueueDeclare(
+		username, // queue name
+		false,    // durable
+		true,     // auto delete
+		false,    // exclusive
+		false,    // no wait
+		nil,      // arguments
+	); err != nil {
+		return err
+	}
+
+	switch action {
+	case "bind":
+		if err := followFeedChannel.QueueBind(
+			username, // queue name
+			username, // key
+			username, // exchange name
+			false,    // no wait
+			nil,      // arguments
+		); err != nil {
+			return err
+		}
+	case "unbind":
+		if err := followFeedChannel.QueueUnbind(
+			username, // queue name
+			username, // key
+			username, // exchange name
+			nil,      // arguments
+		); err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func handleJoin(username string) error {
+	return updateOnlineStatus(username, "online")
+}
+
+func handleLeave(username string) error {
+	return updateOnlineStatus(username, "offline")
 }
 
 func handle(deliveries <-chan amqp.Delivery, done chan error) {
 	for d := range deliveries {
-		action, user := d.Headers["action"], d.Headers["key"].(string)
+		action, user := d.Headers["action"].(string), d.Headers["key"].(string)
 		log.Printf("%v %v", action, user)
 		var err error
 		switch action {
@@ -90,6 +203,11 @@ func handle(deliveries <-chan amqp.Delivery, done chan error) {
 		case "unbind":
 			err = handleLeave(user)
 		}
+		if err != nil {
+			done <- err
+			continue
+		}
+		err = changeFollowFeedExchangeBindings(user, action)
 		if err != nil {
 			done <- err
 		}
