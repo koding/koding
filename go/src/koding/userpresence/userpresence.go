1796e0fbe7c0cf439790146dac9435b83247b919
Christopher Thorn <chris@koding.com>
2013-06-04 15:08:04 -0700

    user presence: control channel; auth.join and auth.leave;
    auth.leave triggers after a cancelable 10s timer fires

--- go/src/koding/userpresence/userpresence.go
+++ go/src/koding/userpresence/userpresence.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/streadway/amqp"
 	"koding/databases/mongo"
 	"koding/tools/amqputil"
@@ -8,9 +9,17 @@ import (
 	"labix.org/v2/mgo/bson"
 	"log"
 	"strings"
+	"time"
 )
 
-var followFeedChannel *amqp.Channel
+type socketIds map[string]bool
+
+var (
+	resourceName      string
+	followFeedChannel *amqp.Channel
+	socketIdsByUser   map[string]socketIds
+	timersByUser      map[string]*time.Timer
+)
 
 func main() {
 
@@ -22,69 +31,12 @@ func main() {
 
 	mainAmqpConn := amqputil.CreateConnection("userpresence")
 
+	resourceName = "users-presence"
+
 	startMonitoring(mainAmqpConn)
 	startControlling(mainAmqpConn)
 
 	select {}
-}
-
-func startControlling(mainAmqpConn *amqp.Connection) {
-
-	channel, err := mainAmqpConn.Channel()
-	if err != nil {
-		panic(err)
-	}
-
-	exchangeName := "users-presence-control"
-
-	if err := channel.ExchangeDeclare(
-		exchangeName, // exchange name
-		"fanout",     // kind
-		false,        // durable
-		true,         // auto delete
-		false,        // internal
-		false,        // no wait
-		nil,          // arguments
-	); err != nil {
-		panic(err)
-	}
-
-	if _, err := channel.QueueDeclare(
-		"",    // queue name
-		false, // durable
-		true,  // auto delete
-		true,  // exclusive
-		false, // no wait
-		nil,   // arguments
-	); err != nil {
-		panic(err)
-	}
-
-	if err := channel.QueueBind(
-		"",           // queue name
-		"",           // key
-		exchangeName, // exchange name
-		false,        // no wait
-		nil,          // arguments
-	); err != nil {
-		panic(err)
-	}
-
-	deliveries, err := channel.Consume(
-		"",    // use the anonymous queue from above
-		"",    // ctag
-		false, // no-ack
-		false, // exclusive
-		false, // no local
-		false, // no wait
-		nil,   // arguments
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	go handleControl(deliveries, make(chan error))
-
 }
 
 func startMonitoring(mainAmqpConn *amqp.Connection) {
@@ -93,8 +45,6 @@ func startMonitoring(mainAmqpConn *amqp.Connection) {
 	if err != nil {
 		panic(err)
 	}
-
-	resourceName := "users-presence"
 
 	if err := channel.ExchangeDeclare(
 		resourceName, // the exchange name
@@ -146,7 +96,32 @@ func startMonitoring(mainAmqpConn *amqp.Connection) {
 
 }
 
+func handlePresence(deliveries <-chan amqp.Delivery, done chan error) {
+	for d := range deliveries {
+		action, username := d.Headers["action"].(string), d.Headers["key"].(string)
+		log.Printf("%v %v", action, username)
+		var err error
+		switch action {
+		case "bind":
+			err = updateOnlineStatus(username, "online")
+		case "unbind":
+			err = updateOnlineStatus(username, "offline")
+		}
+		if err != nil {
+			done <- err
+			continue
+		}
+		err = changeFollowFeedExchangeBindings(username, action)
+		if err != nil {
+			done <- err
+		}
+	}
+	log.Printf("handle: deliveries channel closed")
+	done <- nil
+}
+
 func createFollowFeedChannel(component string) (*amqp.Channel, error) {
+
 	conn, err := amqp.Dial(amqp.URI{
 		Scheme:   "amqp",
 		Host:     config.Current.FollowFeed.Host,
@@ -164,19 +139,20 @@ func createFollowFeedChannel(component string) (*amqp.Channel, error) {
 }
 
 func updateOnlineStatus(username, status string) error {
-	userCollection := mongo.GetCollection("jUsers")
-	accountCollection := mongo.GetCollection("jAccounts")
+
+	users := mongo.GetCollection("jUsers")
+	accounts := mongo.GetCollection("jAccounts")
 
 	user, account := make(map[string]interface{}), make(map[string]interface{})
 
-	if err := userCollection.Find(bson.M{"username": username}).One(&user); err != nil {
+	if err := users.Find(bson.M{"username": username}).One(&user); err != nil {
 		return err
 	}
-	if err := accountCollection.Find(bson.M{"profile.nickname": username}).One(&account); err != nil {
+	if err := accounts.Find(bson.M{"profile.nickname": username}).One(&account); err != nil {
 		return err
 	}
 	// update the user's actual status
-	userCollection.Update(
+	users.Update(
 		bson.M{"_id": user["_id"]},
 		bson.M{
 			"$set": bson.M{
@@ -185,20 +161,21 @@ func updateOnlineStatus(username, status string) error {
 		},
 	)
 
-	onlineStatus := make(map[string]string)
-	if user["onlineStatus"] == nil {
-		onlineStatus = user["onlineStatus"].(map[string]string)
+	onlineStatus := make(map[string]interface{})
+
+	if user["onlineStatus"] != nil {
+		onlineStatus = (user["onlineStatus"]).(map[string]interface{})
 	}
 
 	var publicStatus string
 
-	if onlineStatus["userPreference"] == "" {
+	if onlineStatus["userPreference"] == nil {
 		publicStatus = status
 	} else {
-		publicStatus = onlineStatus["userPreference"]
+		publicStatus = onlineStatus["userPreference"].(string)
 	}
 	// update the user's public status
-	accountCollection.Update(
+	accounts.Update(
 		bson.M{"_id": account["_id"]},
 		bson.M{
 			"$set": bson.M{
@@ -258,39 +235,179 @@ func changeFollowFeedExchangeBindings(username, action string) error {
 	return nil
 }
 
-func handlePresence(deliveries <-chan amqp.Delivery, done chan error) {
+func startControlling(mainAmqpConn *amqp.Connection) {
+
+	socketIdsByUser = make(map[string]socketIds)
+	timersByUser = make(map[string]*time.Timer)
+
+	controlChannel, err := mainAmqpConn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	bindingChannel, err := mainAmqpConn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	resourceName := "users-presence-control"
+
+	if err := controlChannel.ExchangeDeclare(
+		resourceName, // exchange name
+		"fanout",     // kind
+		false,        // durable
+		true,         // auto delete
+		false,        // internal
+		false,        // no wait
+		nil,          // arguments
+	); err != nil {
+		panic(err)
+	}
+
+	if _, err := controlChannel.QueueDeclare(
+		resourceName, // queue name
+		false,        // durable
+		true,         // auto delete
+		true,         // exclusive
+		false,        // no wait
+		nil,          // arguments
+	); err != nil {
+		panic(err)
+	}
+
+	if err := controlChannel.QueueBind(
+		resourceName, // queue name
+		"",           // key
+		resourceName, // exchange name
+		false,        // no wait
+		nil,          // arguments
+	); err != nil {
+		panic(err)
+	}
+
+	deliveries, err := controlChannel.Consume(
+		"",    // use the anonymous queue from above
+		"",    // ctag
+		true,  // no-ack
+		false, // exclusive
+		false, // no local
+		false, // no wait
+		nil,   // arguments
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	go handleControl(deliveries, bindingChannel, make(chan error))
+
+}
+
+func handleControl(
+	deliveries <-chan amqp.Delivery,
+	bindingChannel *amqp.Channel,
+	done chan error,
+) {
 	for d := range deliveries {
-		action, username := d.Headers["action"].(string), d.Headers["key"].(string)
-		log.Printf("%v %v", action, username)
-		var err error
-		switch action {
-		case "bind":
-			err = updateOnlineStatus(username, "online")
-		case "unbind":
-			err = updateOnlineStatus(username, "offline")
-		}
-		if err != nil {
-			done <- err
+
+		if d.RoutingKey != "auth.join" && d.RoutingKey != "auth.leave" {
+			log.Printf("Consumed an invalid routing key: %s", d.RoutingKey)
 			continue
 		}
-		err = changeFollowFeedExchangeBindings(username, action)
-		if err != nil {
-			done <- err
+
+		msg := new(presenceControlMessage)
+		json.Unmarshal(d.Body, &msg)
+
+		var callee func(string, string, *amqp.Channel) error
+		switch d.RoutingKey {
+		case "auth.join":
+			callee = handleAuthJoin
+		case "auth.leave":
+			callee = handleAuthLeave
 		}
+
+		if msg.Username == "" || msg.SocketId == "" {
+			log.Printf("Invalid message: username: %s socketId: %s", msg.Username, msg.SocketId)
+			continue
+		}
+
+		if err := callee(msg.Username, msg.SocketId, bindingChannel); err != nil {
+			log.Printf("An error occurred: %v", err)
+		}
+
 	}
 	log.Printf("handle: deliveries channel closed")
 	done <- nil
 }
 
-func handleControl(deliveries <-chan amqp.Delivery, done chan error) {
-	for d := range deliveries {
-		log.Printf(
-			"got %dB delivery: [%v] %q",
-			len(d.Body),
-			d.DeliveryTag,
-			d.Body,
+func handleAuthJoin(username, socketId string, bindingChannel *amqp.Channel) error {
+
+	if _, ok := socketIdsByUser[username]; !ok {
+		socketIdsByUser[username] = make(socketIds)
+	}
+
+	if socketIdsByUser[username][socketId] != true {
+		socketIdsByUser[username][socketId] = true
+		if len(socketIdsByUser[username]) == 1 {
+			if err := addPresenceBinding(username, bindingChannel); err != nil {
+				return err
+			}
+		}
+	}
+
+	log.Print(socketIdsByUser)
+	return nil
+}
+
+func handleAuthLeave(username, socketId string, bindingChannel *amqp.Channel) error {
+
+	if _, ok := socketIdsByUser[username]; !ok {
+		return nil
+	}
+
+	delete(socketIdsByUser[username], socketId)
+
+	if len(socketIdsByUser[username]) == 0 {
+		if err := removePresenceBinding(username, bindingChannel); err != nil {
+			return err
+		}
+		delete(socketIdsByUser, username)
+	}
+
+	log.Print(socketIdsByUser)
+	return nil
+}
+
+type presenceControlMessage struct {
+	Username string
+	SocketId string
+}
+
+func addPresenceBinding(username string, bindingChannel *amqp.Channel) error {
+	if _, ok := timersByUser[username]; ok {
+		timersByUser[username].Stop()
+		delete(timersByUser, username)
+	}
+	return bindingChannel.QueueBind(
+		username,     // queue name
+		username,     // key
+		resourceName, // exchange name
+		false,        // no wait
+		nil,          // arguments
+	)
+}
+
+func removePresenceBinding(username string, bindingChannel *amqp.Channel) error {
+	if _, ok := timersByUser[username]; !ok {
+		timersByUser[username] = time.NewTimer(time.Second * 10)
+	}
+	select {
+	case <-timersByUser[username].C:
+		return bindingChannel.QueueUnbind(
+			username,     // queue name
+			username,     // key
+			resourceName, // exchange name
+			nil,          // arguments
 		)
 	}
-	log.Printf("handle: deliveries channel closed")
-	done <- nil
+	return nil
 }
