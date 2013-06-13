e873d189dde4edab544995ba0d041c1eb99c49e9
Fatih Arslan <fatih@koding.com>
2013-06-13 11:42:45 +0300

    rules: change the way rules are applied and used

--- go/src/koding/kontrol/kontrolproxy/proxyconfig/rules.go
+++ go/src/koding/kontrol/kontrolproxy/proxyconfig/rules.go
@@ -9,28 +9,47 @@ import (
 )
 
 type Rule struct {
-	// To disable or enable current rule
-	Enabled bool `bson:"enabled", json:"enabled"`
+	// Type of rule, like 'ip' or 'country'
+	Type string
 
-	// Rule is either allowing matches or denying
-	Mode string `bson:"mode", json:"mode"`
+	// Name of rule, future usage
+	Name string
 
 	// Matching string, like 1.1.1.1 or Turkey
 	Match string `bson:"rule", json:"rule"`
 }
 
+type Behaviour struct {
+	// To disable or enable current rule
+	Enabled bool `bson:"enabled", json:"enabled"`
+
+	// Behaviour of the rule, deny,allow or securepage
+	Action string `bson:"mode", json:"mode"`
+
+	// Rule name the behaviour is applied
+	RuleName string `bson:"rulename", json:"rulename"`
+}
+
 type Restriction struct {
 	Id         bson.ObjectId   `bson:"_id" json:"-"`
 	DomainName string          `bson:"domainname" json:"domainname"`
-	IPs        map[string]Rule `bson:"ips", json:"ips"`
-	Countries  map[string]Rule `bson:"countries", json:"countries"`
+	Rules      map[string]Rule `bson:"rules" json:"rules"`
+	RuleList   []Behaviour     `bson:"rulelist", json:"rulelist"`
 }
 
-func NewRule(enabled bool, mode, match string) *Rule {
+func NewRule(ruletype, name, match string) *Rule {
 	return &Rule{
-		Enabled: enabled,
-		Mode:    mode,
-		Match:   match,
+		Type:  ruletype,
+		Name:  name,
+		Match: match,
+	}
+}
+
+func NewBehaviour(enabled bool, action, name string) *Behaviour {
+	return &Behaviour{
+		Enabled:  enabled,
+		Action:   action,
+		RuleName: name,
 	}
 }
 
@@ -38,13 +57,13 @@ func NewRestriction(domainname string) *Restriction {
 	return &Restriction{
 		Id:         bson.NewObjectId(),
 		DomainName: domainname,
-		IPs:        make(map[string]Rule),
-		Countries:  make(map[string]Rule),
+		Rules:      make(map[string]Rule),
+		RuleList:   make([]Behaviour, 0),
 	}
 }
 
-func (p *ProxyConfiguration) AddRule(domainname, ruletype, match, mode string, enabled bool) error {
-	restriction, err := p.GetRule(domainname)
+func (p *ProxyConfiguration) AddRule(domainname, ruletype, match string) error {
+	restriction, err := p.GetRestriction(domainname)
 	if err != nil {
 		if err != mgo.ErrNotFound {
 			return err
@@ -56,25 +75,16 @@ func (p *ProxyConfiguration) AddRule(domainname, ruletype, match, mode string, e
 		return errors.New("match can't be empty")
 	}
 
-	switch ruletype {
-	case "ip", "file":
-		m := strings.Replace(match, ".", "_", -1) // mongo doesn't love dots for sub fields
-		_, ok := restriction.IPs[m]
-		if ok {
-			return fmt.Errorf("ip rule for '%s' already exist", match)
-		}
+	m := strings.Replace(match, ".", "_", -1) // mongo doesn't love dots for sub fields
+	name := ruletype + m
 
-		rule := *NewRule(enabled, mode, match)
-		restriction.IPs[m] = rule
-	case "country":
-		_, ok := restriction.Countries[match]
-		if ok {
-			return fmt.Errorf("country rule for '%s' already exist", match)
-		}
-
-		rule := *NewRule(enabled, mode, match)
-		restriction.Countries[match] = rule
+	_, ok := restriction.Rules[name]
+	if !ok {
+		return fmt.Errorf("%s rule for '%s' already exist", ruletype, match)
 	}
+
+	rule := *NewRule(ruletype, name, match)
+	restriction.Rules[name] = rule
 
 	_, err = p.Collection["rules"].Upsert(bson.M{"domainname": domainname}, restriction)
 	if err != nil {
@@ -84,7 +94,38 @@ func (p *ProxyConfiguration) AddRule(domainname, ruletype, match, mode string, e
 	return nil
 }
 
-func (p *ProxyConfiguration) DeleteRule(domainname string) error {
+func (p *ProxyConfiguration) AddBehaviour(enabled bool, domainname, action, name string, index int) error {
+	restriction, err := p.GetRestriction(domainname)
+	if err != nil {
+		if err != mgo.ErrNotFound {
+			return err
+		}
+		restriction = *NewRestriction(domainname)
+	}
+
+	behaviour := *NewBehaviour(enabled, action, name)
+	ruleList := insertBehaviour(restriction.RuleList, behaviour, index)
+	restriction.RuleList = ruleList
+
+	_, err = p.Collection["rules"].Upsert(bson.M{"domainname": domainname}, restriction)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *ProxyConfiguration) DeleteBehaviour(domainname, rulename string) error {
+	err := p.Collection["rules"].Update(
+		bson.M{"domainname": domainname},
+		bson.M{"$pull": bson.M{"ruleslist.name": rulename}},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (p *ProxyConfiguration) DeleteRestriction(domainname string) error {
 	err := p.Collection["rules"].Remove(bson.M{"domainname": domainname})
 	if err != nil {
 		return err
@@ -92,10 +133,10 @@ func (p *ProxyConfiguration) DeleteRule(domainname string) error {
 	return nil
 }
 
-func (p *ProxyConfiguration) DeleteRuleCountry(domainname, country string) error {
+func (p *ProxyConfiguration) DeleteRule(domainname, rulename string) error {
 	err := p.Collection["rules"].Update(
 		bson.M{"domainname": domainname},
-		bson.M{"$unset": bson.M{"countries." + country: "1"}},
+		bson.M{"$unset": bson.M{"rules." + rulename: "1"}},
 	)
 	if err != nil {
 		return err
@@ -103,19 +144,7 @@ func (p *ProxyConfiguration) DeleteRuleCountry(domainname, country string) error
 	return nil
 }
 
-func (p *ProxyConfiguration) DeleteRuleIp(domainname, ip string) error {
-	m := strings.Replace(ip, ".", "_", -1) // mongo doesn't love dots for sub fields
-	err := p.Collection["rules"].Update(
-		bson.M{"domainname": domainname},
-		bson.M{"$unset": bson.M{"ips." + m: "1"}},
-	)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (p *ProxyConfiguration) GetRule(domainname string) (Restriction, error) {
+func (p *ProxyConfiguration) GetRestriction(domainname string) (Restriction, error) {
 	restriction := Restriction{}
 	err := p.Collection["rules"].Find(bson.M{"domainname": domainname}).One(&restriction)
 	if err != nil {
@@ -124,67 +153,19 @@ func (p *ProxyConfiguration) GetRule(domainname string) (Restriction, error) {
 	return restriction, nil
 }
 
-func (p *ProxyConfiguration) GetRuleCountry(domainname, country string) (Rule, error) {
+func (p *ProxyConfiguration) GetRule(domainname, rulename string) (Rule, error) {
 	restriction := Restriction{}
-	err := p.Collection["rules"].Find(bson.M{"domainname": domainname}).Select(bson.M{"countries." + country: 1}).
+	err := p.Collection["rules"].Find(bson.M{"domainname": domainname}).Select(bson.M{"rules." + rulename: 1}).
 		One(&restriction)
 	if err != nil {
 		return Rule{}, err
 	}
 
-	rule, ok := restriction.Countries[country]
+	rule, ok := restriction.Rules[rulename]
 	if !ok {
-		return Rule{}, fmt.Errorf("country '%s' does not exist.", country)
+		return Rule{}, fmt.Errorf("rule '%s' does not exist.", rulename)
 	}
 	return rule, nil
-}
-
-func (p *ProxyConfiguration) GetRuleIp(domainname, ip string) (Rule, error) {
-	restriction := Restriction{}
-	m := strings.Replace(ip, ".", "_", -1) // mongo doesn't love dots for sub fields
-	err := p.Collection["rules"].Find(bson.M{"domainname": domainname}).Select(bson.M{"ips." + m: 1}).
-		One(&restriction)
-	if err != nil {
-		return Rule{}, err
-	}
-
-	rule, ok := restriction.IPs[m]
-	if !ok {
-		return Rule{}, fmt.Errorf("ip '%s' does not exist.", ip)
-	}
-	return rule, nil
-}
-
-func (p *ProxyConfiguration) GetRuleIps(domainname string) ([]Rule, error) {
-	restriction := Restriction{}
-	err := p.Collection["rules"].Find(bson.M{"domainname": domainname}).Select(bson.M{"ips": 1}).
-		One(&restriction)
-	if err != nil {
-		return []Rule{}, err
-	}
-
-	rules := make([]Rule, 0)
-	for _, rule := range restriction.IPs {
-		rules = append(rules, rule)
-	}
-
-	return rules, nil
-}
-
-func (p *ProxyConfiguration) GetRuleCountries(domainname string) ([]Rule, error) {
-	restriction := Restriction{}
-	err := p.Collection["rules"].Find(bson.M{"domainname": domainname}).Select(bson.M{"countries": 1}).
-		One(&restriction)
-	if err != nil {
-		return []Rule{}, err
-	}
-
-	rules := make([]Rule, 0)
-	for _, rule := range restriction.Countries {
-		rules = append(rules, rule)
-	}
-
-	return rules, nil
 }
 
 func (p *ProxyConfiguration) GetRules() []Restriction {
@@ -204,4 +185,13 @@ func (p *ProxyConfiguration) GetRuleByID(id bson.ObjectId) (Restriction, error) 
 		return restriction, err
 	}
 	return restriction, nil
+}
+
+func insertBehaviour(list []Behaviour, b Behaviour, i int) []Behaviour {
+	if len(list) == 0 {
+		i = 0
+	}
+	d := append(list[:i], append([]Behaviour{b}, list[i:]...)...)
+	fmt.Println("insert", d)
+	return d
 }
