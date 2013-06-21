bc93d1d7eccb1550e785b8d4abef1510b837c6d9
Geraint Jones <geraint@koding.com>
2013-06-20 20:01:11 -0700

    Code clean up, make the default reply SERVFAIL and SERVFAIL on any errors

--- go/src/koding/dns/responder/responder.go
+++ go/src/koding/dns/responder/responder.go
@@ -9,10 +9,6 @@ import (
 	"fmt"
 )
 
-// Compile-time options
-const includesPort = false // If false, sends only the address for TXT queries.
-// If true, includes the UDP or TCP port.
-// TODO: allow to secify the Qname it must respond to
 
 func addressSection(qname string, client net.IP) (result types.RR) {
 	result.Name = qname
@@ -28,30 +24,30 @@ func Respond(query types.DNSquery, config map[string]interface{}) types.DNSrespo
 		result types.DNSresponse
 	)
 	result.Ansection = nil
-	tcpAddr, _ := net.ResolveTCPAddr("tcp", query.Client.String())
-	ipaddressV4 := tcpAddr.IP.To4()
-	fmt.Println(query.Qname)
 	switch {
 	case query.Qtype == types.A:
+		// Set the return to NOERROR initially
 		result.Responsecode = types.NOERROR
-		if ipaddressV4 != nil {
 			ancount := 1
 			result.Ansection = make([]types.RR, ancount)
 			var vm virt.VM
+			// Lookup the IP for the given Hostname
 			if err := db.VMs.Find(bson.M{"hostnameAlias": query.Qname}).One(&vm); err != nil {
-				result.Ansection[0] = addressSection(query.Qname, ipaddressV4)
+				// Lookup failed, return SERVFAIL
+				result.Responsecode = types.SERVFAIL
 				fmt.Println(err)
 			}
 			if vm.IP != nil {
 				result.Ansection[0] = addressSection(query.Qname, vm.IP.To4())
-				fmt.Println(vm.IP)
+			} else {
+				// VM Has no IP, return SERVFAIL
+				result.Responsecode = types.SERVFAIL
 			}
 		} else {
 			// ancount := 0
 		}
 	default:
-		result.Responsecode = types.NOERROR
-	}
+		result.Responsecode = types.SERVFAIL
 	return result
 }
 
