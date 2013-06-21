83c20e9adb8e3e33b9e3118d76b987e3f9d2ac77
Geraint Jones <geraint@koding.com>
2013-06-21 11:50:40 -0700

    Lookup hostname > vm relationship in jDomains

--- go/src/koding/dns/responder/responder.go
+++ go/src/koding/dns/responder/responder.go
@@ -2,6 +2,7 @@ package responder
 
 import (
 	"koding/dns/types"
+	"koding/kontrol/kontrolproxy/proxyconfig"
 	"koding/tools/db"
 	"koding/virt"
 	"labix.org/v2/mgo"
@@ -24,21 +25,19 @@ func Respond(query types.DNSquery, config map[string]interface{}) types.DNSrespo
 	)
 	result.Ansection = nil
 	switch {
-	//Check first if this domain is ours or not. - Hardcoded for now. Need to think about security here
-	case query.Qname[len(query.Qname)-5:len(query.Qname)] == "kd.io":
-		switch {
-		case query.Qtype == types.A:
-			ancount := 1
-			var vm virt.VM
+	case query.Qtype == types.A:
+		ancount := 1
+		var vm virt.VM
+		var domain proxyconfig.Domain
+		// Lookup the hostnameAlias for the given domain
+		if err := db.Domains.Find(bson.M{"domain": query.Qname}).One(&domain); err == nil {
 			// Lookup the IP for the given Hostname
-			if err := db.VMs.Find(bson.M{"hostnameAlias": query.Qname}).One(&vm); err != nil {
+			if err := db.VMs.Find(bson.M{"hostnameAlias": domain.HostnameAlias}).One(&vm); err != nil {
 				if err != mgo.ErrNotFound {
 					// Holy cow, its not a not found - we have a problem batman. Lookup failed, return SERVFAIL
 					result.Responsecode = types.SERVFAIL
 				}
-
-			}
-			if vm.IP != nil {
+			} else if vm.IP != nil {
 				// VM IP Jackpot.
 				result.Ansection = make([]types.RR, ancount)
 				result.Ansection[0] = addressSection(query.Qname, vm.IP.To4())
@@ -47,10 +46,14 @@ func Respond(query types.DNSquery, config map[string]interface{}) types.DNSrespo
 				// VM Has no IP, return SERVFAIL
 				result.Responsecode = types.SERVFAIL
 			}
-		default:
+		} else if err == mgo.ErrNotFound {
+			// Not found, so not our domain - add recusion here.
+			result.Responsecode = types.SERVFAIL
+		} else if err != mgo.ErrNotFound {
+			// We have some other error. Lets bail
 			result.Responsecode = types.SERVFAIL
 		}
-		//No not our domain, 
+
 	default:
 		result.Responsecode = types.SERVFAIL
 	}
