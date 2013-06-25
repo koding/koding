dea6aa55f9e8a87443d33a9d18bad787f0c88e83
Cihangir SAVAS <cihangir@koding.com>
2013-06-25 15:29:53 -0700

    [go][worker][neo4jfeeder] change neo4jfeeder as durable

--- go/src/koding/workers/neo4jfeeder/neo4jfeeder.go
+++ go/src/koding/workers/neo4jfeeder/neo4jfeeder.go
@@ -55,7 +55,7 @@ func startConsuming() {
 	c.conn = amqputil.CreateConnection("neo4jFeeding")
 	c.channel = amqputil.CreateChannel(c.conn)
 
-	err := c.channel.ExchangeDeclare(EXCHANGE_NAME, "fanout", false, false, false, false, nil)
+	err := c.channel.ExchangeDeclare(EXCHANGE_NAME, "fanout", true, false, false, false, nil)
 	if err != nil {
 		log.Fatal("exchange.declare: %s", err)
 	}
