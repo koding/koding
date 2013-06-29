58d1cc9941e1eed98eb73c51b266ffb28787ac29
Fatih Arslan <fatih@koding.com>
2013-06-28 17:20:22 -0700

    kontrolapi: add resolve to domains

--- go/src/koding/kontrol/kontrolapi/domains.go
+++ go/src/koding/kontrol/kontrolapi/domains.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"io/ioutil"
 	"koding/kontrol/kontrolproxy/proxyconfig"
+	"koding/kontrol/kontrolproxy/resolver"
 	"net/http"
 )
 
@@ -40,6 +41,26 @@ func GetDomain(writer http.ResponseWriter, req *http.Request) {
 	}
 
 	data, err := json.MarshalIndent(domain, "", "  ")
+	if err != nil {
+		io.WriteString(writer, fmt.Sprintf("{\"err\":\"%s\"}\n", err))
+		return
+	}
+
+	writer.Write([]byte(data))
+}
+
+func ResolveDomain(writer http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	domainname := vars["domain"]
+	fmt.Printf("GET\t/domains/%s/resolv\n", domainname)
+
+	target, err := resolver.GetTarget(domainname)
+	if err != nil {
+		http.Error(writer, fmt.Sprintf("{\"err\":\"%s\"}\n", err), http.StatusBadRequest)
+		return
+	}
+
+	data, err := json.MarshalIndent(target.Url.String(), "", "  ")
 	if err != nil {
 		io.WriteString(writer, fmt.Sprintf("{\"err\":\"%s\"}\n", err))
 		return
