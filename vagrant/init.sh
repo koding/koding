1c09304215cb80c4e44a7a88a009cc8add427d89
Christopher Thorn <chris@koding.com>
2013-07-03 12:53:43 -0700

    vagrant/init.sh: use a tmp file
    There were some compatibility problems with the way that we were doing it before

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -1,8 +1,12 @@
-CMD="mongo localhost/koding --quiet --eval=\"print(db.jGroups.count())\""
-
-COUNT=$($CMD)
+#!/bin/bash
 
 DIR=$(cd "$(dirname "$0")"; pwd)
+
+SCRIPT=/tmp/mongocmd.sh
+
+echo "mongo localhost/koding --quiet --eval='print(db.jGroups.count())'" > $SCRIPT
+
+COUNT=$(sh $SCRIPT)
 
 if [ $COUNT -lt 1 ]; then
   echo "Running the import script"
