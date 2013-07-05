koding/koding

commit 3921602b3280cf1e0af58842325976fd462883ff

New VirtualBox version and improved checks in Vagrantfile.

Vagrantfile
@@ -1,10 +1,10 @@
 version = `VBoxManage --version 2> /dev/null` rescue "0"
-if version < "4.2.12r84980"
-  print "VirtualBox not installed or outdated. "
+if version < "4.2.14r86644" and ARGV[0] != "halt"
+  print "\e[31mVirtualBox not installed or outdated. \e[30m"
 
   install = false
-  if `uname`.strip == "Darwin"
-    print "Download and install VirtualBox automatically? (yN) "
+  if `uname`.strip == "Darwin" and system "tty > /dev/null"
+    print "Download and install VirtualBox automatically? This will halt running Vagrant machines. (yN) "
     install = ($stdin.gets.strip == "y")
   end
 
@@ -14,7 +14,8 @@ if version < "4.2.12r84980"
     exit! 1
   end
 
-  system "wget -O /tmp/VirtualBox.dmg http://download.virtualbox.org/virtualbox/4.2.12/VirtualBox-4.2.12-84980-OSX.dmg" or exit! 1
+  system "vagrant halt" or exit! 1
+  system "wget -O /tmp/VirtualBox.dmg http://download.virtualbox.org/virtualbox/4.2.14/VirtualBox-4.2.14-86644-OSX.dmg" or exit! 1
   system "hdiutil attach /tmp/VirtualBox.dmg" or exit! 1
   system "sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg  -target /" or exit! 1
   sleep 1 # somehow the installer stays active for some time
@@ -26,7 +27,7 @@ if $0 == "Vagrantfile" || Vagrant::VERSION < "1.2.2"
   print "Vagrant not installed or outdated. " unless $0 == "Vagrantfile"
 
   install = false
-  if `uname`.strip == "Darwin"
+  if `uname`.strip == "Darwin" and system "tty > /dev/null"
     print "Download and install Vagrant automatically? (yN) "
     install = ($stdin.gets.strip == "y")
   end

commit b6d345dd984c83dd0c0197b8f233e9adf30671e3

Autofix for vagrant bug with current virtualbox version introduced by 3921602b3280cf1e0af58842325976fd462883ff

Vagrantfile
@@ -23,6 +23,15 @@ if version < "4.2.14r86644" and ARGV[0] != "halt"
   puts "", "VirtualBox successfully installed.", ""
 end
 
+# Since Richard introduced Virtualbox 4.2.14 vagrant doesn't work
+# https://github.com/mitchellh/vagrant/issues/1847
+# This is a fix for this until this is addressed in vagrant / VirtualBox
+version = `VBoxManage --version 2> /dev/null` rescue "0"
+if not File.exists? File.expand_path "~/.vagrant.d/boxes/koding-13/virtualbox/box.mf" and version == "4.2.14r86644\n"
+  puts "", "\e[0;31mSeems like you updated to a virtualbox version with a vagrant bug :'( autofixing now!\e[0;0m", ""
+  system "/usr/bin/openssl sha1 ~/.vagrant.d/boxes/koding-13/virtualbox/*.vmdk ~/.vagrant.d/boxes/koding-13/virtualbox/*.ovf > ~/.vagrant.d/boxes/koding-13/virtualbox/box.mf"
+end
+
 if $0 == "Vagrantfile" || Vagrant::VERSION < "1.2.2"
   print "Vagrant not installed or outdated. " unless $0 == "Vagrantfile"
 