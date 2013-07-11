9ccd286cf19aa3ae4691958da86f14b466a3ec71
Halil Koklu <h@koding.com>
2013-07-11 21:24:32 +0200

    removed trailing php tag, as space in end of file can have side effects

--- php/image.php
+++ php/image.php
@@ -1,23 +1,23 @@
 <?php
-    $url = $_GET['url'];
 
-    $ch = curl_init ($url);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 10);
+$url = $_GET['url'];
 
-    $output = curl_exec($ch);
-    curl_close ($ch);
+$ch = curl_init ($url);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 10);
 
-    $output = explode("\r\n\r\n", $output, 2);
-    $header = explode("\r\n", $output[0]);
+$output = curl_exec($ch);
+curl_close ($ch);
 
-    foreach ($header as $key => $val) {
-        if ($key > 0 && stripos($val, "Transfer-Encoding:") !== 0) {
-            header($val);
-        }
+$output = explode("\r\n\r\n", $output, 2);
+$header = explode("\r\n", $output[0]);
+
+foreach ($header as $key => $val) {
+    if ($key > 0 && stripos($val, "Transfer-Encoding:") !== 0) {
+        header($val);
     }
+}
 
-    echo $output[1];
-?>
+echo $output[1];
