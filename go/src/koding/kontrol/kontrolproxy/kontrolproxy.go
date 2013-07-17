3007ee82977039f7f4cc0cfc13d693a613fdede8
Fatih Arslan <fatih@koding.com>
2013-07-16 17:27:30 -0700

    kontrolproxy: show accurate numbers

--- go/src/koding/kontrol/kontrolproxy/kontrolproxy.go
+++ go/src/koding/kontrol/kontrolproxy/kontrolproxy.go
@@ -64,8 +64,8 @@ var logs *syslog.Writer
 
 func main() {
 	var err error
-	n := runtime.GOMAXPROCS(runtime.NumCPU())
-	log.Println("using %d cpus", n)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	log.Println("using %d cpus for goroutines", runtime.NumCPU())
 
 	logs, err = syslog.New(syslog.LOG_DEBUG|syslog.LOG_USER, "KONTROL_PROXY")
 	if err != nil {
