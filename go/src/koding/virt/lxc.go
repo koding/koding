ab10c4a25234634f9ecfa2a9b64dc875e453e7e3
Richard Musiol <mail@richard-musiol.de>
2013-08-19 13:11:59 +0200

    os kite: Set MTU of VMs to 1476.

--- go/src/koding/virt/lxc.go
+++ go/src/koding/virt/lxc.go
@@ -12,6 +12,7 @@ func (vm *VM) Start() error {
 	if out, err := exec.Command("/usr/bin/lxc-start", "--name", vm.String(), "--daemon").CombinedOutput(); err != nil {
 		return commandError("lxc-start failed.", err, out)
 	}
+	exec.Command("/sbin/ifconfig", vm.VEth(), "mtu", "1476").Start() // error ignored
 	return vm.WaitForState("RUNNING", time.Second)
 }
 
