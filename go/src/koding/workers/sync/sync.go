39795b9a0e3b6345da19eda2f0a1cb0af71d2462
sent-hil <me@sent-hil.com>
2013-08-19 12:25:27 -0700

    sync: better logging

--- go/src/koding/workers/sync/sync.go
+++ go/src/koding/workers/sync/sync.go
@@ -82,19 +82,19 @@ func createRelationship(rel oldNeo.Relationship, amqpChannel *amqp.Channel) {
 func relationshipNeedsToBeSynced(result oldNeo.Relationship) bool {
 	exists, sourceId := checkNodeExists(result.SourceId.Hex())
 	if exists != true {
-		log.Println("err: No SourceNode:", result.SourceName, result.As)
+		log.Println("err: Id '%v'. No SourceNode '%v' with Id: '%v' for relationship: '%v'", result.Id, result.SourceName, result.SourceId, result.As)
 		return true
 	}
 
 	exists, targetId := checkNodeExists(result.TargetId.Hex())
 	if exists != true {
-		log.Println("err: No TargetNode:", result.TargetName, result.Id, result.As)
+		log.Println("err: Id '%v'. No TargetNode '%v' with Id: '%v' for relationship: '%v'", result.Id, result.TargetName, result.TargetId, result.As)
 		return true
 	}
 
 	exists = checkRelationshipExists(sourceId, targetId, result.As)
 	if exists != true {
-		log.Printf("err: No '%v' relationship exists between %v and %v", result.SourceName, result.TargetName, result.As)
+		log.Printf("err: Id '%v'. No '%v' relationship exists between %v and %v", result.Id, result.As, result.SourceName, result.TargetName)
 		return true
 	}
 
