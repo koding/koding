4996704928bee2cd07746df9facd1fd4c2aa4cc7
Fatih Arslan <fatih@koding.com>
2013-08-30 12:22:06 +0300

    Commit kites.go that is needed for us

---
+++ go/src/koding/kontrol/kontrolproxy/proxyconfig/kites.go
@@ -0,0 +1,74 @@
+package proxyconfig
+
+import (
+	"kite/protocol"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
+
+func NewKite() *protocol.Kite {
+	return &protocol.Kite{}
+}
+
+func (p *ProxyConfiguration) UpsertKite(kite *protocol.Kite) error {
+	query := func(c *mgo.Collection) error {
+		_, err := c.Upsert(bson.M{"uuid": kite.Uuid}, kite)
+		return err
+	}
+
+	return p.RunCollection("jKites", query)
+}
+
+func (p *ProxyConfiguration) GetKite(uuid string) (*protocol.Kite, error) {
+	kite := protocol.Kite{}
+	query := func(c *mgo.Collection) error {
+		return c.Find(bson.M{"uuid": uuid}).One(&kite)
+	}
+
+	err := p.RunCollection("jKites", query)
+	if err != nil {
+		return nil, err
+	}
+	return &kite, nil
+}
+
+func (p *ProxyConfiguration) UpdateKite(kite *protocol.Kite) error {
+	query := func(c *mgo.Collection) error {
+		return c.Update(bson.M{"uuid": kite.Uuid}, kite)
+	}
+
+	return p.RunCollection("jKites", query)
+}
+
+func (p *ProxyConfiguration) DeleteKite(uuid string) error {
+	query := func(c *mgo.Collection) error {
+		return c.Remove(bson.M{"uuid": uuid})
+	}
+
+	return p.RunCollection("jKites", query)
+}
+
+func (p *ProxyConfiguration) SizeKites() (int, error) {
+	var count int
+	var err error
+	query := func(c *mgo.Collection) error {
+		count, err = c.Count()
+		return err
+	}
+
+	err = p.RunCollection("jKites", query)
+	return count, err
+}
+
+func (p *ProxyConfiguration) ListKites() []*protocol.Kite {
+	kites := make([]*protocol.Kite, 0)
+	query := func(c *mgo.Collection) error {
+		// todo use Limit() to decrease the memory overhead, future
+		// improvements...
+		iter := c.Find(nil).Iter()
+		return iter.All(&kites)
+	}
+
+	p.RunCollection("jKites", query)
+	return kites
+}
