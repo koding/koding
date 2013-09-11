a22b4fdef32b04be0660ff0bb101485952218516
Huseyin Oguz Albayrak <huseyinalb@gmail.com>
2013-09-11 16:35:03 +0300

    refactored CheckAndGetUser and added GetUser

--- go/src/koding/db/models/user.go
+++ go/src/koding/db/models/user.go
@@ -11,13 +11,14 @@ type User struct {
 	Email         string        `bson:"email" json:"email"`
 	LastLoginDate time.Time     `bson:"lastLoginDate" json:"lastLoginDate"`
 	RegisteredAt  time.Time     `bson:"registeredAt" json:"registeredAt"`
-	Username      string        `bson:"username" json:"username"`
-	OldName       string        `bson:"oldUsername" json:"oldUserName"`
-	Password      string        `bson:"password" json:"password"`
-	Status        string        `bson:"status" json:"status"`
-	Salt          string        `bson:"salt" json:"salt"`
-	Shell         string        `bson:"shell" json:"shell"`
-	SshKeys       []struct {
+	// TODO left like this for consistency, but should be converted into Username
+	Name     string `bson:"username" json:"username"`
+	OldName  string `bson:"oldUsername" json:"oldUserName"`
+	Password string `bson:"password" json:"password"`
+	Status   string `bson:"status" json:"status"`
+	Salt     string `bson:"salt" json:"salt"`
+	Shell    string `bson:"shell" json:"shell"`
+	SshKeys  []struct {
 		Title string `bson:"title"`
 		Key   string `bson:"key"`
 	} `bson:"sshKeys"`
--- go/src/koding/db/mongodb/modelhelper/user.go
+++ go/src/koding/db/mongodb/modelhelper/user.go
@@ -10,24 +10,33 @@ import (
 	"labix.org/v2/mgo/bson"
 )
 
-func CheckAndGetUser(username string, password string) (models.User, error) {
-	user := models.User{}
-	query := func(c *mgo.Collection) error {
-		return c.Find(bson.M{"username": username}).One(&user)
-	}
-	err := mongodb.Run("jUsers", query)
+func CheckAndGetUser(username string, password string) (*models.User, error) {
+
+	user, err := GetUser(username)
 	if err != nil {
-		return user, err
+		return nil, fmt.Errorf("Username does not match")
 	}
 
 	iostring := sha1.New()
 	io.WriteString(iostring, user.Salt)
 	io.WriteString(iostring, password)
 	sha1pass := fmt.Sprintf("%x", iostring.Sum(nil))
-	if user.Password == sha1pass {
-		return user, nil
-	} else {
-		return user, fmt.Errorf("Password does not match")
+	if user.Password != sha1pass {
+		return nil, fmt.Errorf("Password does not match")
 	}
 
+	return user, nil
+}
+
+func GetUser(username string) (*models.User, error) {
+	user := new(models.User)
+	query := func(c *mgo.Collection) error {
+		return c.Find(bson.M{"username": username}).One(&user)
+	}
+	err := mongodb.Run("jUsers", query)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
