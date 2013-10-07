08ce6efce9564cce833db62f454f0889a927b153
Fatih Arslan <fatih@koding.com>
2013-10-07 21:05:48 +0300

    kontrol: move out session getter to modelhelpers

---
+++ go/src/koding/db/models/session.go
@@ -0,0 +1,10 @@
+package models
+
+import "labix.org/v2/mgo/bson"
+
+type Session struct {
+	Id       bson.ObjectId `bson:"_id" json:"-"`
+	ClientId string        `bson:"clientId"`
+	Username string        `bson:"username"`
+	GuestId  int           `bson:"guestId"`
+}
---
+++ go/src/koding/db/mongodb/modelhelper/session.go
@@ -0,0 +1,24 @@
+package modelhelper
+
+import (
+	"fmt"
+	"koding/db/models"
+	"koding/db/mongodb"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
+
+func GetSession(token string) (*models.Session, error) {
+	session := new(models.Session)
+
+	query := func(c *mgo.Collection) error {
+		return c.Find(bson.M{"clientId": token}).One(&session)
+	}
+
+	err := mongodb.Run("jSessions", query)
+	if err != nil {
+		return nil, fmt.Errorf("sessionID '%s' is not validated", token)
+	}
+
+	return session, nil
+}
