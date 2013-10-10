3c98d0eeae2dd0bb819f49361f8858803f6c4ce6
Cenk Alti <cenkalti@gmail.com>
2013-10-10 13:33:12 +0300

    reconnect subscriber on disconnect

--- go/src/koding/messaging/moh/subscriber.go
+++ go/src/koding/messaging/moh/subscriber.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Subscriber struct {
+	addr    string
 	ws      *websocket.Conn
 	handler MessageHandler
 }
@@ -15,17 +16,15 @@ type Subscriber struct {
 // returns a pointer to newly created Subscriber.
 // After creating a Subscriber you should subscribe to messages with Subscribe function.
 func NewSubscriber(addr string, handler MessageHandler) (*Subscriber, error) {
-	url := "ws://" + addr + "/"
-	origin := "http://localhost/" // dont know if this is required
-	ws, err := websocket.Dial(url, "", origin)
+	sub := &Subscriber{
+		addr:    addr,
+		handler: handler,
+	}
+	err := sub.connect()
 	if err != nil {
 		return nil, err
 	}
 
-	sub := &Subscriber{
-		ws:      ws,
-		handler: handler,
-	}
 	go sub.consumer()
 	return sub, err
 }
@@ -35,6 +34,43 @@ func (s *Subscriber) Subscribe(key string) error {
 	return websocket.Message.Send(s.ws, key)
 }
 
+func (s *Subscriber) connect() error {
+	url := "ws://" + s.addr + "/"
+	origin := "http://localhost/" // dont know if this is required
+	log.Println("Connecting to url:", url)
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Println("Cannot connect")
+		return err
+	}
+	log.Println("Connection is successfull")
+	s.ws = ws
+	return nil
+}
+
+// Connected returns the status of the websocket connection.
+func (s *Subscriber) Connected() bool {
+	// We are checking the pointer here because
+	// it will be set to nil on disconnect by consumer().
+	return s.ws != nil
+}
+
+// connector tries to connect to the server forever.
+// When the connection is established it runs a consumer() goroutine and returns.
+func (s *Subscriber) connector() {
+	for {
+		err := s.connect()
+		if err != nil {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		go s.consumer()
+		return
+	}
+}
+
+// consumer reads the messages from websocket until the connection is dropped.
+// When the connection drops it runs a connector() goroutine and returns.
 func (s *Subscriber) consumer() {
 	for {
 		var message []byte
@@ -42,8 +78,12 @@ func (s *Subscriber) consumer() {
 		err := websocket.Message.Receive(s.ws, &message)
 		if err != nil {
 			log.Println("Cannot read message from websocket")
-			time.Sleep(100 * time.Millisecond)
-			continue
+			s.ws.Close()
+			// Connected() checks this pointer.
+			// Set it to nil to indicate that we are disconnected.
+			s.ws = nil
+			go s.connector()
+			return
 		}
 		log.Println("Received data:", message)
 		s.handler(message)
