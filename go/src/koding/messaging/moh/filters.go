5891794715c3307671d1b7972adb8cb8a67b06d8
Cenk Alti <cenkalti@gmail.com>
2013-10-16 22:20:25 +0300

    add lock to Filters

--- go/src/koding/messaging/moh/filters.go
+++ go/src/koding/messaging/moh/filters.go
@@ -1,28 +1,52 @@
 package moh
 
+import (
+	"sync"
+)
+
 // Filters is a type used for managing the message subscription keys of subscribers.
 // Methods also updates the keys field of the connections.
 // Keep in mind that it's methods are not thread-safe.
-type Filters map[string]map[*connection]bool
+type Filters struct {
+	m map[string]map[*connection]bool
+	sync.RWMutex
+}
+
+func NewFilters() *Filters {
+	return &Filters{m: make(map[string]map[*connection]bool)}
+}
+
+func (f Filters) Get(key string) map[*connection]bool {
+	f.RLock()
+	defer f.RUnlock()
+
+	return f.m[key]
+}
 
 // Add adds the connection to the list for key.
 // Also adds the key to the keys field of the conn.
 func (f Filters) Add(conn *connection, key string) {
-	connections := f[key]
+	f.Lock()
+	defer f.Unlock()
+
+	connections := f.m[key]
 	if connections == nil {
-		f[key] = make(map[*connection]bool)
+		f.m[key] = make(map[*connection]bool)
 	}
-	f[key][conn] = true
+	f.m[key][conn] = true
 	conn.keys[key] = true
 }
 
 // Remove removes the connection from a key in filters.
 func (f Filters) Remove(conn *connection, key string) {
-	connections := f[key]
+	f.Lock()
+	defer f.Unlock()
+
+	connections := f.m[key]
 	delete(conn.keys, key)
 	delete(connections, conn)
 	if len(connections) == 0 {
-		delete(f, key)
+		delete(f.m, key)
 	}
 }
 
--- go/src/koding/messaging/moh/publisher.go
+++ go/src/koding/messaging/moh/publisher.go
@@ -12,31 +12,14 @@ type Publisher struct {
 	// Registered filters, holds pointers to open connections.
 	// All clients are registered to the "all" key by default for allowing broadcasting.
 	// Modifier operations on this type is made by registrar() function.
-	filters Filters
-
-	// subscribe, disconnect events from connections
-	events chan publisherEvent
+	filters *Filters
 
 	*MessagingServer
-}
-
-// Subscription requests from connections to be sent to Publisher.subscribe channel
-type publisherEvent struct {
-	conn      *connection
-	eventType int // values are defined as constants on global scope
-	key       string
 }
 
 // This is the magic subscription key for broadcast events.
 // Hoping that it is unique enough to not collide with another key.
 const all = "4658f005d49885355f4e771ed9dace10cca9563e"
-
-// Values for publisherEvent.eventType filed
-const (
-	subscribe = iota
-	disconnect
-	unsubscribe
-)
 
 // NewPublisher creates a new Publisher and returns a pointer to it.  The
 // publisher will listen on addr and accept websocket connections from
@@ -44,19 +27,17 @@ const (
 func NewPublisher(addr string) (*Publisher, error) {
 	p := &Publisher{
 		MessagingServer: NewMessagingServer(addr),
-		filters:         make(Filters),
-		events:          make(chan publisherEvent),
+		filters:         NewFilters(),
 	}
 	p.Handle("/", p)
 	go p.Serve() // Starts HTTP server
-	go p.registrar()
 	return p, nil
 }
 
 // Publish sends a message to registered Subscribers with the key.
 func (p *Publisher) Publish(key string, message []byte) {
 	// log.Println("Sending message to send channel", string(message))
-	for c := range p.filters[key] {
+	for c := range p.filters.Get(key) {
 		select {
 		case c.send <- message:
 			// log.Println("Message sent to send channel")
@@ -85,27 +66,15 @@ func (p *Publisher) websocketHandler(ws *websocket.Conn) {
 		send: make(chan []byte, 256),
 		keys: make(map[string]bool),
 	}
-	p.events <- publisherEvent{conn: c, eventType: subscribe, key: all}
-	defer func() { p.events <- publisherEvent{conn: c, eventType: disconnect} }()
-	go c.writer()
-	c.reader(p.events)
-}
 
-// registrar receives publiserEvents from the channel and updates filters.
-// Adds or removes the connections from filters as if necessary.  Synchronizes
-// the modifier operations on Publisher.filters field.
-func (p *Publisher) registrar() {
-	for event := range p.events {
-		switch event.eventType {
-		case subscribe:
-			p.filters.Add(event.conn, event.key)
-		case unsubscribe:
-			p.filters.Remove(event.conn, event.key)
-		case disconnect:
-			close(event.conn.send)
-			p.filters.RemoveAll(event.conn)
-		}
-	}
+	p.filters.Add(c, all)
+	defer func() {
+		p.filters.RemoveAll(c)
+		close(c.send)
+	}()
+
+	go c.writer()
+	c.reader(p.filters)
 }
 
 // connection represents a connected Subscriber in Publisher.
@@ -121,7 +90,7 @@ type connection struct {
 
 // reader reads the subscription requests from websocket and saves it in a map
 // for accessing later.
-func (c *connection) reader(ch chan publisherEvent) {
+func (c *connection) reader(filters *Filters) {
 	for {
 		var cmd subscriberCommand
 		err := websocket.JSON.Receive(c.ws, &cmd)
@@ -133,10 +102,10 @@ func (c *connection) reader(ch chan publisherEvent) {
 		// log.Printf("reader: Received a command from websocket: %+v\n", cmd)
 		if cmd.Name == "subscribe" {
 			key := cmd.Args["key"].(string)
-			ch <- publisherEvent{conn: c, eventType: subscribe, key: key}
+			filters.Add(c, key)
 		} else if cmd.Name == "unsubscribe" {
 			key := cmd.Args["key"].(string)
-			ch <- publisherEvent{conn: c, eventType: unsubscribe, key: key}
+			filters.Remove(c, key)
 		} else {
 			log.Println("Unknown command, dropping client")
 			break
