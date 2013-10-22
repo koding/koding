24371eec689c8a2a90e0a909352a017e2c9b8921
Senthil <senthil@koding.com>
2013-10-21 20:19:12 -0700

    graphite: add feeder, some analytics

---
+++ go/src/koding/workers/graphite_feeder/graphite_feeder.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/peterbourgon/g2s"
+	"koding/tools/config"
+	"log"
+	"strconv"
+)
+
+var (
+	ip     = config.Current.Statsd.Ip
+	port   = config.Current.Statsd.Port
+	STATSD g2s.Statter
+)
+
+func init() {
+	var err error
+
+	STATSD, err = g2s.Dial("udp", fmt.Sprintf("%v:%v", ip, port))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	for _, fn := range listOfAnalytics {
+		name, count := fn()
+		log.Println(name, count)
+		STATSD.Gauge(1, name, strconv.Itoa(count))
+	}
+}
+
+var listOfAnalytics = make([]func() (string, int), 0)
+
+func registerAnalytic(fn func() (string, int)) {
+	listOfAnalytics = append(listOfAnalytics, fn)
+}
