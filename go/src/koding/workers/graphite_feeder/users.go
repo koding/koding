7f5e016a05496612920bfd76c95c978664c729bf
Senthil <senthil@koding.com>
2013-10-22 10:30:01 -0700

    graphite: more user analytics

--- go/src/koding/workers/graphite_feeder/users.go
+++ go/src/koding/workers/graphite_feeder/users.go
@@ -10,8 +10,10 @@ import (
 func init() {
 	registerAnalytic(numberOfAccounts)
 	registerAnalytic(numberOfUsersWhoLinkedOauth)
+	registerAnalytic(numberOfEmailsImportedFromGoogle)
 	registerAnalytic(numberOfInvitesSent)
 	registerAnalytic(numberOfUsersWhoJoinedToday)
+	registerAnalytic(numberOfUsersWhoDeletedTheirAccount)
 }
 
 func numberOfAccounts() (string, int) {
@@ -44,6 +46,21 @@ func numberOfUsersWhoLinkedOauth() (string, int) {
 	return identifier, count
 }
 
+func numberOfEmailsImportedFromGoogle() (string, int) {
+	var identifier string = "number_of_emails_imported_from_google"
+	var count int
+	var err error
+	var query = func(c *mgo.Collection) error {
+		count, err = c.Find(nil).Count()
+
+		return err
+	}
+
+	mongodb.Run("jReferrableEmails", query)
+
+	return identifier, count
+}
+
 func numberOfInvitesSent() (string, int) {
 	var identifier string = "number_of_invites_sent"
 	var count int
@@ -66,7 +83,7 @@ func numberOfUsersWhoJoinedToday() (string, int) {
 	var query = func(c *mgo.Collection) error {
 		year, month, day := time.Now().Date()
 		var today = time.Date(year, month, day, 0, 0, 0, 0, time.Local)
-		var filter = bson.M{"meta.createdAt": bson.M{"$gt": today}}
+		var filter = bson.M{"meta.createdAt": bson.M{"$gte": today}}
 
 		count, err = c.Find(filter).Count()
 
@@ -74,6 +91,21 @@ func numberOfUsersWhoJoinedToday() (string, int) {
 	}
 
 	mongodb.Run("jAccounts", query)
+
+	return identifier, count
+}
+
+func numberOfUsersWhoDeletedTheirAccount() (string, int) {
+	var identifier string = "number_of_users_who_deleted_their_account"
+	var count int
+	var err error
+	var query = func(c *mgo.Collection) error {
+		count, err = c.Find(bson.M{"status": "deleted"}).Count()
+
+		return err
+	}
+
+	mongodb.Run("jUsers", query)
 
 	return identifier, count
 }
