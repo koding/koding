d6a664260bcb999622a9540255b3f5cc906bfcf1
Fatih Arslan <fatih@koding.com>
2013-10-24 15:01:10 +0300

    kontrol: use mongodb based tokens and some refactoring around protocols/models

--- go/src/koding/db/models/kites.go
+++ go/src/koding/db/models/kites.go
@@ -1,13 +1,33 @@
 package models
 
 import (
-	"koding/newkite/protocol"
+	"labix.org/v2/mgo/bson"
 	"net/rpc"
 	"time"
 )
 
 type Kite struct {
-	protocol.Base `bson:",inline"`
-	UpdatedAt     time.Time   `bson:"updatedAt"`
-	Client        *rpc.Client `json:"-"`
+	KiteBase  `bson:",inline"`
+	UpdatedAt time.Time   `bson:"updatedAt"`
+	Client    *rpc.Client `bson:"-" json:"-"`
+}
+
+type KiteBase struct {
+	Id        bson.ObjectId `bson:"_id" json:"-"`
+	Username  string        `bson:"username" json:"username"`
+	Kitename  string        `bson:"kitename" json:"kitename"`
+	Version   string        `bson:"version" json:"version"`
+	PublicKey string        `bson:"publicKey" json:"publicKey"`
+	Token     string        `bson:"token" json:"token"`
+	Uuid      string        `bson:"uuid" json:"uuid"`
+	Hostname  string        `bson:"hostname" json:"hostname"`
+	Addr      string        `bson:"addr" json:"addr"`
+
+	// this is used temporary to distinguish kites that are used for Koding
+	// client-side. An example is to use it with value "vm"
+	Kind string `bson:"kind" json:"kind"`
+
+	LocalIP  string `bson:"localIP" json:"localIP"`
+	PublicIP string `bson:"publicIP" json:"publicIP"`
+	Port     string `bson:"port" json:"port"`
 }
