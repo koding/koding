92c16ca1412816449eb29ef67649e4e0c780e217
Cihangir SAVAS <cihangir@koding.com>
2013-11-11 10:10:17 -0800

    [Rabbitmq] Created consumer helper

---
+++ go/src/koding/messaging/rabbitmq/conusmer.go
@@ -0,0 +1,152 @@
+package rabbitmq
+
+import (
+	"errors"
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+type Consumer struct {
+	conn       *amqp.Connection
+	channel    *amqp.Channel
+	deliveries <-chan amqp.Delivery
+	tag        string
+	handler    func(deliveries <-chan amqp.Delivery)
+	done       chan error
+	session    Session
+}
+
+func (c *Consumer) Deliveries() <-chan amqp.Delivery {
+	return c.deliveries
+}
+
+func NewConsumer(e Exchange, q Queue, bo BindingOptions, co ConsumerOptions) (*Consumer, error) {
+
+	if co.Tag == "" {
+		return nil, errors.New("Tag is not defined in consumer options")
+	}
+
+	c := &Consumer{
+		conn:    nil,
+		channel: nil,
+		tag:     co.Tag,
+		done:    make(chan error),
+		session: Session{
+			Exchange:        e,
+			Queue:           q,
+			ConsumerOptions: co,
+			BindingOptions:  bo,
+		},
+	}
+	err := c.connect()
+	if err != nil {
+		return nil, err
+	}
+
+	// go func() {
+	//  fmt.Printf("closing: %s", <-c.conn.NotifyClose(make(chan *amqp.Error)))
+	// }()
+
+	return c, nil
+}
+
+func (c *Consumer) connect() error {
+
+	e := c.session.Exchange
+	q := c.session.Queue
+	bo := c.session.BindingOptions
+	co := c.session.ConsumerOptions
+
+	var err error
+
+	c.conn, err = amqp.Dial(getConnectionString())
+	if err != nil {
+		return err
+	}
+	handleErrors(c.conn)
+	// got Connection, getting Channel
+	c.channel, err = c.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	// got channel, declaring Exchange
+	if err = c.channel.ExchangeDeclare(
+		e.Name,       // name of the exchange
+		e.Type,       // type
+		e.Durable,    // durable
+		e.AutoDelete, // delete when complete
+		e.Internal,   // internal
+		e.NoWait,     // noWait
+		e.Args,       // arguments
+	); err != nil {
+		return err
+	}
+
+	// declaring Queue
+	queue, err := c.channel.QueueDeclare(
+		q.Name,       // name of the queue
+		q.Durable,    // durable
+		q.AutoDelete, // delete when usused
+		q.Exclusive,  // exclusive
+		q.NoWait,     // noWait
+		q.Args,       // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	// declared Queue, binding to Exchange
+	if err = c.channel.QueueBind(
+		// bind to real queue
+		queue.Name,    // name of the queue
+		bo.RoutingKey, // bindingKey
+		e.Name,        // sourceExchange
+		bo.NoWait,     // noWait
+		bo.Args,       // arguments
+	); err != nil {
+		return err
+	}
+
+	// Queue bound to Exchange, starting Consume
+	deliveries, err := c.channel.Consume(
+		// consume from real queue
+		queue.Name,   // name
+		co.Tag,       // consumerTag,
+		co.AutoAck,   // autoAck
+		co.Exclusive, // exclusive
+		co.NoLocal,   // noLocal
+		co.NoWait,    // noWait
+		co.Args,      // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	c.deliveries = deliveries
+
+	return nil
+}
+
+func (c *Consumer) Consume(handler func(deliveries <-chan amqp.Delivery)) {
+	c.handler = handler
+	// handle all consumer errors, if required re-connect
+
+	handler(c.deliveries)
+	fmt.Println("handle: deliveries channel closed")
+	c.done <- nil
+}
+
+func (c *Consumer) Shutdown() error {
+	err := shutdown(c.conn, c.channel, c.tag)
+	if err != nil {
+		return nil
+	}
+	defer fmt.Println("Consumer shutdown OK")
+	fmt.Println("Waiting for handler to exit")
+	return <-c.done
+}
+
+func (c *Consumer) RegisterSignalHandler() {
+	registerSignalHandler(c)
+}
