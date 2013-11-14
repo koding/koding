aa0206a1fad60b0f4faea019f3b24c912ea9f32c
Fatih Arslan <fatih@koding.com>
2013-11-14 16:38:27 +0200

    kontrolproxy/cache: split memoy interface implementation to their own file

--- go/src/koding/kontrol/kontrolproxy/cache/cache.go
+++ go/src/koding/kontrol/kontrolproxy/cache/cache.go
@@ -9,7 +9,6 @@ import (
 	"net/http/httputil"
 	"path/filepath"
 	"strings"
-	"sync"
 )
 
 // Cache interface is used by CacheTransport to store and retrieve the cache content.
@@ -113,37 +112,4 @@ func (c *CacheTransport) save(req *http.Request, resp *http.Response) error {
 
 	c.cache.Set(key, respBytes)
 	return nil
-}
-
-type MemoryCache struct {
-	sync.RWMutex
-	items map[string][]byte
-}
-
-func NewMemoryCache() *MemoryCache {
-	return &MemoryCache{
-		items: map[string][]byte{},
-	}
-}
-
-func (c *MemoryCache) Get(key string) ([]byte, bool) {
-	c.RLock()
-	defer c.RUnlock()
-
-	value, ok := c.items[key]
-	return value, ok
-}
-
-func (c *MemoryCache) Set(key string, resp []byte) {
-	c.Lock()
-	defer c.Unlock()
-
-	c.items[key] = resp
-}
-
-func (c *MemoryCache) Delete(key string) {
-	c.Lock()
-	defer c.Unlock()
-
-	delete(c.items, key)
 }
