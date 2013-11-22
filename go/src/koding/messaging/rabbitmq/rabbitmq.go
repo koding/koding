01c612c293217724baed855fb41760ab7093057f
Cihangir SAVAS <cihangir@koding.com>
2013-11-21 17:16:17 -0800

    [RabbitMq] apply better godoc

--- go/src/koding/messaging/rabbitmq/rabbitmq.go
+++ go/src/koding/messaging/rabbitmq/rabbitmq.go
@@ -15,74 +15,93 @@ import (
 
 var log = logging.MustGetLogger("RabbitMQ")
 
-// Durable    :
-// 		Durable exchanges will survive server restarts and
-// AutoDelete :
-// 		Will remain declared when there are no remaining bindings.
-// Internal   :
-// 		Exchanges declared as `internal` do not accept accept publishings.Internal
-// 		exchanges are useful for when you wish to implement inter-exchange topologies
-// 		that should not be exposed to users of the broker.
-// NoWait     :
-// 		When noWait is true, declare without waiting for a confirmation from the server.
-// Args :
-// 		amqp.Table of arguments that are specific to the server's implementation of
-// 		the exchange can be sent for exchange types that require extra parameters.
 type Exchange struct {
-	Name                                  string
-	Type                                  string
-	Durable, AutoDelete, Internal, NoWait bool
-	Args                                  amqp.Table
+	// Exchange name
+	Name string
+
+	// Exchange type
+	Type string
+
+	// Durable exchanges will survive server restarts
+	Durable string
+
+	// Will remain declared when there are no remaining bindings.
+	AutoDelete string
+
+	// Exchanges declared as `internal` do not accept accept publishings.Internal
+	// exchanges are useful for when you wish to implement inter-exchange topologies
+	// that should not be exposed to users of the broker.
+	Internal string
+
+	// When noWait is true, declare without waiting for a confirmation from the server.
+	NoWait bool
+
+	// amqp.Table of arguments that are specific to the server's implementation of
+	// the exchange can be sent for exchange types that require extra parameters.
+	Args amqp.Table
 }
 
-// Name :
-// 		The queue name may be empty, in which the server will generate a unique name
-// 		which will be returned in the Name field of Queue struct.
-// Durable-AutoDelete-Args:
-// 		Check Exchange comments for those 3 fields
-// Exclusive :
-// 		Exclusive queues are only accessible by the connection that declares them and
-// 		will be deleted when the connection closes.  Channels on other connections
-// 		will receive an error when attempting declare, bind, consume, purge or delete a
-// 		queue with the same name.
-// NoWait :
-// 		When noWait is true, the queue will assume to be declared on the server.  A
-// 		channel exception will arrive if the conditions are met for existing queues
-// 		or attempting to modify an existing queue from a different connection.
 type Queue struct {
-	Name                                   string
-	Durable, AutoDelete, Exclusive, NoWait bool
-	Args                                   amqp.Table
+	// The queue name may be empty, in which the server will generate a unique name
+	// which will be returned in the Name field of Queue struct.
+	Name string
+
+	// Check Exchange comments for durable
+	Durable bool
+
+	// Check Exchange comments for autodelete
+	AutoDelete bool
+
+	// Exclusive queues are only accessible by the connection that declares them and
+	// will be deleted when the connection closes.  Channels on other connections
+	// will receive an error when attempting declare, bind, consume, purge or delete a
+	// queue with the same name.
+	Exclusive bool
+
+	// When noWait is true, the queue will assume to be declared on the server.  A
+	// channel exception will arrive if the conditions are met for existing queues
+	// or attempting to modify an existing queue from a different connection.
+	NoWait bool
+
+	// Check Exchange comments for Args
+	Args amqp.Table
 }
 
-// Tag:
-// 		The consumer is identified by a string that is unique and scoped for all
-// 		consumers on this channel.
-// AutoAck :
-// 		When autoAck (also known as noAck) is true, the server will acknowledge
-// 		deliveries to this consumer prior to writing the delivery to the network.  When
-// 		autoAck is true, the consumer should not call Delivery.Ack
-// Exclusive-NoWait-Args:
-// 		Check Queue struct documentation
-// NoLocal :
-//		When noLocal is true, the server will not deliver publishing sent from the same
-//		connection to this consumer. (Do not use Publish and Consume from same channel)
 type ConsumerOptions struct {
-	Tag       string     // consumerTag,
-	AutoAck   bool       // autoAck
-	Exclusive bool       // exclusive
-	NoLocal   bool       // noLocal
-	NoWait    bool       // noWait
-	Args      amqp.Table // arguments
+	// The consumer is identified by a string that is unique and scoped for all
+	// consumers on this channel.
+	Tag string
+
+	// When autoAck (also known as noAck) is true, the server will acknowledge
+	// deliveries to this consumer prior to writing the delivery to the network.  When
+	// autoAck is true, the consumer should not call Delivery.Ack
+	AutoAck bool // autoAck
+
+	// Check Queue struct documentation
+	Exclusive bool // exclusive
+
+	// When noLocal is true, the server will not deliver publishing sent from the same
+	// connection to this consumer. (Do not use Publish and Consume from same channel)
+	NoLocal bool // noLocal
+
+	// Check Queue struct documentation
+	NoWait bool // noWait
+
+	// Check Exchange comments for Args
+	Args amqp.Table // arguments
 }
 
-// Publishings messages to given Queue with matching -RoutingKey-
-// Every Queue has a default binding to Default Exchange with their Qeueu name
-// So you can send messages to a queue over default exchange
 type BindingOptions struct {
+	// Publishings messages to given Queue with matching -RoutingKey-
+	// Every Queue has a default binding to Default Exchange with their Qeueu name
+	// So you can send messages to a queue over default exchange
 	RoutingKey string
-	NoWait     bool
-	Args       amqp.Table
+
+	// Do not wait for a consumer
+	NoWait bool
+
+	// App specific data
+	Args amqp.Table
 }
 
 // type Table amqp.Table
