e06043fa90edbae802f17fa5c640df142d2a9f70
Can Yucel <can@koding.com>
2013-12-18 21:01:23 +0200

    Moderation: StatusUpdate and Tag go models are updated

--- go/src/koding/db/models/statusupdate.go
+++ go/src/koding/db/models/statusupdate.go
@@ -3,9 +3,20 @@ package models
 import "labix.org/v2/mgo/bson"
 
 type StatusUpdate struct {
-  Id         bson.ObjectId `bson:"_id" json:"-"`
-  Slug       string        `bson:"slug"`
-  Body       string        `bson:"body"`
-  OriginId   bson.ObjectId `bson:"originId"`
-  OriginType string        `bson:"originType"`
+  Id           bson.ObjectId            `bson:"_id" json:"-"`
+  Slug         string                   `bson:"slug"`
+  Slug_        string                   `bson:"slug_"`
+  Body         string                   `bson:"body"`
+  OriginId     bson.ObjectId            `bson:"originId"`
+  OriginType   string                   `bson:"originType"`
+  Meta         Meta                     `bson:"meta"`
+  RepliesCount int                      `bson:"repliesCount"`
+  Group        string                   `bson:"group"`
+  Counts       Count                    `bson:"counts"`
+  Attachments  []map[string]interface{} `bson:"attachments"`
+}
+
+type Count struct {
+  Followers int `bson:"followers"`
+  Following int `bson:"following"`
 }
--- go/src/koding/db/models/tag.go
+++ go/src/koding/db/models/tag.go
@@ -3,7 +3,18 @@ package models
 import "labix.org/v2/mgo/bson"
 
 type Tag struct {
-  ObjectId bson.ObjectId `bson:"_id" json:"-"`
-  Title    string        `bson:"title"`
-  Slug     string        `bson:"slug"`
+  Id     bson.ObjectId `bson:"_id" json:"-"`
+  Title  string        `bson:"title"`
+  Slug   string        `bson:"slug"`
+  Group  string        `bson:"group"`
+  Type   string        `bson:"type,omitempty"`
+  Counts TagCount      `bson:"counts"`
+  Meta   Meta          `bson:"meta"`
+}
+
+type TagCount struct {
+  Followers int `bson:"followers"`
+  Following int `bson:"following"`
+  Post      int `bson:"post,omitempty"`
+  Tagged    int `bson:"tagged"`
 }
