0a971558750f1045cf5d85e6868f5486ff7ff50e
Can Yucel <can@koding.com>
2013-12-20 20:44:32 +0200

    Moderation: StatusUpdate model helper added

---
+++ go/src/koding/db/mongodb/modelhelper/statusupdate.go
@@ -0,0 +1,28 @@
+package modelhelper
+
+import (
+  "koding/db/models"
+  "koding/db/mongodb"
+  "labix.org/v2/mgo"
+)
+
+var (
+  POST_COLL = "jNewStatusUpdates"
+)
+
+func GetStatusUpdateById(id string) (*models.StatusUpdate, error) {
+  statusUpdate := new(models.StatusUpdate)
+  if err := mongodb.One(POST_COLL, id, statusUpdate); err != nil {
+    return nil, err
+  }
+
+  return statusUpdate, nil
+}
+
+func UpdateStatusUpdate(s *models.StatusUpdate) error {
+  query := func(c *mgo.Collection) error {
+    return c.UpdateId(s.Id, s)
+  }
+
+  return mongodb.Run(POST_COLL, query)
+}
