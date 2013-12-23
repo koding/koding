782916b1bfe2690ef48c8f35693cd2cc4f2391e9
Can Yucel <can@koding.com>
2013-12-23 20:37:20 +0200

    Moderation: Category is added to Tag go model

--- go/src/koding/db/models/tag.go
+++ go/src/koding/db/models/tag.go
@@ -3,13 +3,14 @@ package models
 import "labix.org/v2/mgo/bson"
 
 type Tag struct {
-  Id     bson.ObjectId `bson:"_id" json:"-"`
-  Title  string        `bson:"title"`
-  Slug   string        `bson:"slug"`
-  Group  string        `bson:"group"`
-  Status string        `bson:"status,omitempty"`
-  Counts TagCount      `bson:"counts"`
-  Meta   Meta          `bson:"meta"`
+  Id       bson.ObjectId `bson:"_id" json:"-"`
+  Title    string        `bson:"title"`
+  Slug     string        `bson:"slug"`
+  Group    string        `bson:"group"`
+  Status   string        `bson:"status,omitempty"`
+  Counts   TagCount      `bson:"counts"`
+  Category string        `bson:"category"`
+  Meta     Meta          `bson:"meta"`
 }
 
 type TagCount struct {
