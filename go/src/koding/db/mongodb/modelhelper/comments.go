98c5029d80b68c071aa426906e22d606b216fc4a
Can Yucel <can@koding.com>
2013-12-25 18:03:22 +0200

    Moderation: Comments helper is added

---
+++ go/src/koding/db/mongodb/modelhelper/comments.go
@@ -0,0 +1,14 @@
+package modelhelper
+
+import (
+  "koding/db/mongodb"
+  "labix.org/v2/mgo"
+)
+
+func DeleteComment(selector Selector) error {
+  query := func(c *mgo.Collection) error {
+    return c.Remove(selector)
+  }
+
+  return mongodb.Run("jComments", query)
+}
