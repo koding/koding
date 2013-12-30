2f5f3a045d228747a676d2319b4b9836e233e29d
Can Yucel <can@koding.com>
2013-12-30 18:11:36 +0200

    Moderation: commented lines are removed from UpdateTag function

--- go/src/koding/db/mongodb/modelhelper/tags.go
+++ go/src/koding/db/mongodb/modelhelper/tags.go
@@ -13,16 +13,6 @@ func GetTagById(id string) (*models.Tag, error) {
 }
 
 func UpdateTag(t *models.Tag) error {
-	// We dont need to check if tag is exists or not
-	// Update methdd returns err if not found
-	// _, err := GetTagById(t.Id.Hex())
-	// if err != nil {
-	// 	if err == mgo.ErrNotFound {
-	// 		return fmt.Errorf("tag %s does not exist", t.Title)
-	// 	}
-	// 	return err
-	// }
-
 	query := func(c *mgo.Collection) error {
 		return c.UpdateId(t.Id, t)
 	}
