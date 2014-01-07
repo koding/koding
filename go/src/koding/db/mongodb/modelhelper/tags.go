9549bf81b8562aebd266a3eeb4d2ed04e017597a
Can Yucel <can@koding.com>
2014-01-07 22:57:19 +0200

    Migration: GetSomeTags, CheckTagExistence functions are added to tag helper

--- go/src/koding/db/mongodb/modelhelper/tags.go
+++ go/src/koding/db/mongodb/modelhelper/tags.go
@@ -19,3 +19,19 @@ func UpdateTag(t *models.Tag) error {
 
 	return mongodb.Run("jTags", query)
 }
+
+func GetSomeTags(s Selector, o Options) ([]models.Tag, error) {
+	tags := make([]models.Tag, 0)
+	query := func(c *mgo.Collection) error {
+		q := c.Find(s)
+		decorateQuery(q, o)
+		return q.All(&tags)
+	}
+	return tags, mongodb.Run("jTags", query)
+}
+
+func CheckTagExistence(id string) (bool, error) {
+	var exists bool
+	query := checkExistence(id, &exists)
+	return exists, mongodb.Run("jTags", query)
+}
