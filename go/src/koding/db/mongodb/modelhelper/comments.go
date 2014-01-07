60c6990ddcee2d9a7d3ecce2d7f3e9fbc1946304
Can Yucel <can@koding.com>
2014-01-07 23:06:11 +0200

    Migration: CheckCommentExistence, GetCommentById and AddComment functions are added to helper

--- go/src/koding/db/mongodb/modelhelper/comments.go
+++ go/src/koding/db/mongodb/modelhelper/comments.go
@@ -1,6 +1,7 @@
 package modelhelper
 
 import (
+	"koding/db/models"
 	"koding/db/mongodb"
 	"labix.org/v2/mgo"
 )
@@ -10,5 +11,21 @@ func DeleteComment(selector Selector) error {
 		return c.Remove(selector)
 	}
 
+	return mongodb.Run("jComments", query)
+}
+
+func CheckCommentExistence(id string) (bool, error) {
+	var exists bool
+	query := checkExistence(id, &exists)
+	return exists, mongodb.Run("jComments", query)
+}
+
+func GetCommentById(id string) (*models.Comment, error) {
+	comment := new(models.Comment)
+	return comment, mongodb.One("jComments", id, comment)
+}
+
+func AddComment(c *models.Comment) error {
+	query := insertQuery(c)
 	return mongodb.Run("jComments", query)
 }
