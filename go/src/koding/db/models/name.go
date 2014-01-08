7263880936acf9a1e9bdf83ac26e8288f2b06604
Can Yucel <can@koding.com>
2014-01-08 23:23:48 +0200

    Migration: name model and helper are added

---
+++ go/src/koding/db/models/name.go
@@ -0,0 +1,17 @@
+package models
+
+// import "labix.org/v2/mgo/bson"
+
+type Name struct {
+	// Id    bson.ObjectId `bson:"_id"`
+	Name  string `bson:"name"`
+	Slugs []Slug `bson:"slug"`
+}
+
+type Slug struct {
+	ConstructorName string `bson:"constructorName"`
+	CollectionName  string `bson:"collectionName"`
+	UsedAsPath      string `bson:"usedAsPath"`
+	Slug            string `bson:"slug"`
+	Group           string `bson:"group,omitempty"`
+}
