96270f2cef39f5b6dd056d38a182753a33558569
Cihangir SAVAS <cihangir@koding.com>
2014-01-09 14:07:55 -0800

    Go Model: add update account function

--- go/src/koding/db/mongodb/modelhelper/accounts.go
+++ go/src/koding/db/mongodb/modelhelper/accounts.go
@@ -3,6 +3,8 @@ package modelhelper
 import (
 	"koding/db/models"
 	"koding/db/mongodb"
+
+	"labix.org/v2/mgo"
 )
 
 func GetAccountById(id string) (*models.Account, error) {
@@ -14,4 +16,11 @@ func CheckAccountExistence(id string) (bool, error) {
 	var exists bool
 	query := checkExistence(id, &exists)
 	return exists, mongodb.Run("jAccounts", query)
+}
+
+func UpdateAccount(selector, options Selector) error {
+	query := func(c *mgo.Collection) error {
+		return c.Update(selector, options)
+	}
+	return mongodb.Run("jAccounts", query)
 }
