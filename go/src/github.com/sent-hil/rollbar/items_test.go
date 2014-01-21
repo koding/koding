dc286eeb173da78859758d3bbefd7d262b92b63e
Senthil <senthil@koding.com>
2014-01-21 11:34:52 -0800

    rollbar: update library

--- go/src/github.com/sent-hil/rollbar/items_test.go
+++ go/src/github.com/sent-hil/rollbar/items_test.go
@@ -12,7 +12,7 @@ var (
 )
 
 func TestGettingItems(t *testing.T) {
-	mux.HandleFunc("/items", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/items/", func(w http.ResponseWriter, r *http.Request) {
 		if m := "GET"; m != r.Method {
 			t.Errorf("Request method = %v, want %v", r.Method, m)
 		}
@@ -34,5 +34,32 @@ func TestGettingItems(t *testing.T) {
 	items := itemsResp.Result.Items
 	if len(items) < 1 {
 		t.Errorf("Items length: %v, want 2", len(items))
+	}
+}
+
+func TestGettingSingleItem(t *testing.T) {
+	mux.HandleFunc("/items/271964933/", func(w http.ResponseWriter, r *http.Request) {
+		if m := "GET"; m != r.Method {
+			t.Errorf("Request method = %v, want %v", r.Method, m)
+		}
+
+		filename := "fixtures/items/271964933.json"
+		content, err := ioutil.ReadFile(filename)
+		if err != nil {
+			panic(fmt.Sprintf("Error trying to read fixture file: %v", filename))
+		}
+
+		fmt.Fprint(w, string(content))
+	})
+
+	itemsResp, err := itemsService.GetItem(271964933)
+	if err != nil {
+		t.Errorf("Expected empty error response, got: %v", err)
+	}
+
+	var item = itemsResp.Result
+
+	if item.Id != 271964933 {
+		t.Errorf("Expected id in item: %v to matched argument: %v", item.Id, 271964933)
 	}
 }
--- go/src/github.com/sent-hil/rollbar/rollbar.go
+++ go/src/github.com/sent-hil/rollbar/rollbar.go
@@ -33,6 +33,7 @@ func NewClient(token string) *Client {
 
 func (c *Client) Request(verb, url string) (io.ReadCloser, error) {
 	url = c.BuildUrl(url)
+
 	req, err := http.NewRequest(verb, url, nil)
 	if err != nil {
 		return nil, err
@@ -46,6 +47,12 @@ func (c *Client) Request(verb, url string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
+// Rollbar api has a bug where all urls needs to have an extra slash
+// before access token; for example this will return a result:
+//    api.rollbar.com/api/1/deploys/?access_token=...
+// however this will not:
+//    api.rollbar.com/api/1/deploys?access_token=...
+// I've emailed them about it on Jan 14, 2014.
 func (c *Client) BuildUrl(url string) string {
 	return fmt.Sprintf("%v/%v/?access_token=%v", c.Endpoint, url, c.Token)
 }
