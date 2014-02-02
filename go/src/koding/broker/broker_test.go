b8be87ab82b15ac7128e5291164672870e6c9ae8
Cenk Alti <cenkalti@gmail.com>
2014-02-02 11:46:25 -0800

    refactor test

--- go/src/koding/broker/broker_test.go
+++ go/src/koding/broker/broker_test.go
@@ -11,23 +11,25 @@ import (
 	"code.google.com/p/go.net/websocket"
 )
 
-func TestBroker(t *testing.T) {
-	// Start broker
+// sock.js protocol is described here:
+// http://sockjs.github.io/sockjs-protocol/sockjs-protocol-0.3.3.html#section-36
+const url = "ws://localhost:8008/subscribe/0/0/websocket"
+const origin = "http://localhost/"
+
+func init() {
+	// Start broker before tests
 	go main()
 	time.Sleep(1e9)
+}
 
-	// sock.js protocol is described here:
-	// http://sockjs.github.io/sockjs-protocol/sockjs-protocol-0.3.3.html#section-36
-	const url = "ws://localhost:8008/subscribe/0/0/websocket"
-	const origin = "http://localhost/"
-	ws, err := websocket.Dial(url, "", origin)
+func TestBroker(t *testing.T) {
+	client, err := dialSockJS(url, origin)
 	if err != nil {
 		t.Errorf(err.Error())
 		return
 	}
 
-	client := newSockJSClient(ws)
-	go client.run()
+	go client.Run()
 
 	type testCase struct{ send, expect string }
 	cases := []testCase{
@@ -35,20 +37,28 @@ func TestBroker(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		err = client.sendAndExpectString(tc.send, tc.expect)
+		err = client.SendAndExpectString(tc.send, tc.expect)
 		if err != nil {
 			t.Errorf(err.Error())
 			return
 		}
 	}
 
-	ws.Close()
+	client.Close()
 }
 
 // cheap imitation of sockjs-client js library
 type sockJSClient struct {
 	ws       *websocket.Conn
 	messages chan []byte
+}
+
+func dialSockJS(url, origin string) (*sockJSClient, error) {
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		return nil, err
+	}
+	return newSockJSClient(ws), nil
 }
 
 func newSockJSClient(ws *websocket.Conn) *sockJSClient {
@@ -59,7 +69,7 @@ func newSockJSClient(ws *websocket.Conn) *sockJSClient {
 }
 
 // read messages from websocket and put it to the channel
-func (c *sockJSClient) run() error {
+func (c *sockJSClient) Run() error {
 	defer close(c.messages)
 	for {
 		var data []byte
@@ -70,6 +80,10 @@ func (c *sockJSClient) run() error {
 		fmt.Printf("--- read data: %+v\n", string(data))
 		c.didMessage(data)
 	}
+}
+
+func (c *sockJSClient) Close() {
+	c.ws.Close()
 }
 
 // adapted from: https://github.com/sockjs/sockjs-client/blob/master/lib/sockjs.js#L146
@@ -106,12 +120,12 @@ func (c *sockJSClient) didMessage(data []byte) error {
 }
 
 // send a string and expect reply
-func (c *sockJSClient) sendAndExpectString(sent, expected string) error {
-	return c.sendAndExpect([]byte(sent), []byte(expected))
+func (c *sockJSClient) SendAndExpectString(sent, expected string) error {
+	return c.SendAndExpect([]byte(sent), []byte(expected))
 }
 
 // send a []byte and expect reply
-func (c *sockJSClient) sendAndExpect(sent []byte, expected []byte) error {
+func (c *sockJSClient) SendAndExpect(sent []byte, expected []byte) error {
 	err := websocket.Message.Send(c.ws, sent)
 	if err != nil {
 		return err
