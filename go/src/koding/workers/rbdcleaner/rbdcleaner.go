8953f428d0bc60e26bc4b97efecee24209b7dbc0
Fatih Arslan <fatih@koding.com>
2014-02-02 17:20:32 -0800

    rbd-cleaner: finished. It removes images that are in rbd pool but not in jVMs

--- go/src/koding/workers/rbdcleaner/rbdcleaner.go
+++ go/src/koding/workers/rbdcleaner/rbdcleaner.go
@@ -1,30 +1,98 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"koding/db/models"
 	"koding/db/mongodb"
+	"koding/tools/logger"
+	"os/exec"
+	"strings"
+	"time"
 
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
+	"github.com/fatih/set"
+	"github.com/op/go-logging"
+)
+
+var (
+	log      = logger.New("rbd-cleaner")
+	interval = time.Minute * 30
 )
 
 func main() {
+	logging.SetLevel(logging.INFO, "rbd-cleaner")
+	log.Info("starting rbd-cleaner")
+	log.Info("interval set to %s", interval)
 
-	vm := new(models.VM)
-	allVMs := func(c *mgo.Collection) error {
-		iter := c.Find(nil).Select(bson.M{"_id": 1}).Iter()
-		for iter.Next(&vm) {
-			fmt.Println("vm", vm.Id.String())
-		}
-
-		if err := iter.Close(); err != nil {
-			return err
-		}
-
-		return nil
+	// first start
+	err := rbdCleaner()
+	if err != nil {
+		log.Error(err.Error())
 	}
 
-	err := mongodb.Run("jVMs", allVMs)
-	fmt.Println(err)
+	// and then for every other intervals
+	for _ = range time.Tick(interval) {
+		err := rbdCleaner()
+		if err != nil {
+			log.Error(err.Error())
+		}
+	}
+}
+
+func rbdCleaner() error {
+	s, err := rbdList()
+	if err != nil {
+		return err
+	}
+
+	log.Info("cleaner started. total images: %d", s.Size())
+	cleanCount := 0
+	errCount := 0
+	s.Each(func(rbdName interface{}) bool {
+		vmName := rbdName.(string)
+		vmId := strings.TrimPrefix(vmName, "vm-")
+		vm := new(models.VM)
+
+		err := mongodb.One("jVMs", vmId, vm)
+		if err == nil {
+			return true //  rbd image does exists in jVMS, don't touch it and continue
+		}
+
+		log.Info("removing image '%s' from rbd", vmName)
+		res, err := removeRBDImage(vmName)
+		if err != nil {
+			errCount++
+			log.Error("err removing '%s'\n", err)
+			return true
+		}
+
+		cleanCount++
+		log.Info(res)
+		return true
+	})
+	log.Info("cleaner ended. images cleaned: %d images with err: %d", cleanCount, errCount)
+
+	return nil
+}
+
+func removeRBDImage(imageName string) (string, error) {
+	out, err := exec.Command("/usr/bin/rbd", "rm", "vms/"+imageName).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(out)), nil
+}
+
+func rbdList() (*set.Set, error) {
+	s := set.New()
+	out, err := exec.Command("/usr/bin/rbd", "ls", "vms").CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+
+	rbds := string(bytes.TrimSpace(out))
+	for _, r := range strings.Split(rbds, "\n") {
+		s.Add(r)
+	}
+	return s, nil
 }
