dbba2d7c17b09fe3fb662efaef1e650311e3e762
Cihangir SAVAS <cihangir@koding.com>
2014-02-03 18:37:52 -0800

    Broker: add documentation

--- go/src/koding/broker/cache/cache.go
+++ go/src/koding/broker/cache/cache.go
@@ -3,33 +3,41 @@ package cache
 import "github.com/fatih/set"
 
 type SubscriptionSet struct {
-	set *set.Set
+	set      *set.Set
+	socketID string
 }
 
-func NewSubscriptionSet() *SubscriptionSet {
+func NewSubscriptionSet(socketID string) (*SubscriptionSet, error) {
 	s := &SubscriptionSet{
-		set: set.New(),
+		set:      set.New(),
+		socketID: socketID,
 	}
-	return s
+	return s, nil
 }
 
-func (s *SubscriptionSet) Each(f func(item interface{}) bool) {
+func (s *SubscriptionSet) Each(f func(item interface{}) bool) error {
 	s.set.Each(f)
+	// each doesnt return anything
+	return nil
 }
 
 func (s *SubscriptionSet) Subscribe(routingKeyPrefix string) error {
 	s.set.Add(routingKeyPrefix)
+	// add doesnt return any error
 	return nil
 }
 func (s *SubscriptionSet) Unsubscribe(routingKeyPrefix string) error {
 	s.set.Remove(routingKeyPrefix)
+	// remove doesnt return any error
 	return nil
 }
 
-func (s *SubscriptionSet) Has(routingKeyPrefix string) bool {
-	return s.set.Has(routingKeyPrefix)
+func (s *SubscriptionSet) Has(routingKeyPrefix string) (bool, error) {
+	// has only returns bool
+	return s.set.Has(routingKeyPrefix), nil
 }
 
-func (s *SubscriptionSet) Len() int {
-	return s.set.Size()
+func (s *SubscriptionSet) Len() (int, error) {
+	// size only returns count
+	return s.set.Size(), nil
 }
