fbd3ea2413a7829748944da90f2065c27ba505b9
Cihangir SAVAS <cihangir@koding.com>
2014-02-03 18:42:51 -0800

    Broker: create an interface for storing client subscriptions

---
+++ go/src/koding/broker/cache/storage.go
@@ -0,0 +1,54 @@
+package cache
+
+type SubscriptionInterface interface {
+	Each(f func(item interface{}) bool) error
+	Subscribe(routingKeyPrefix string) error
+	Unsubscribe(routingKeyPrefix string) error
+	Has(routingKeyPrefix string) (bool, error)
+	Len() (int, error)
+}
+
+type SubscriptionStorage struct {
+	storage SubscriptionInterface
+}
+
+func NewStorage(typeOf, socketID string) (*SubscriptionStorage, error) {
+
+	var err error
+	var be SubscriptionInterface
+
+	switch typeOf {
+	case "redis":
+		be, err = NewRedis(socketID)
+	default:
+		be, err = NewSubscriptionSet(socketID)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &SubscriptionStorage{
+		storage: be,
+	}, nil
+
+}
+
+func (s *SubscriptionStorage) Each(f func(item interface{}) bool) error {
+	return s.storage.Each(f)
+}
+
+func (s *SubscriptionStorage) Subscribe(routingKeyPrefix string) error {
+	return s.storage.Subscribe(routingKeyPrefix)
+}
+func (s *SubscriptionStorage) Unsubscribe(routingKeyPrefix string) error {
+	return s.storage.Unsubscribe(routingKeyPrefix)
+}
+
+func (s *SubscriptionStorage) Has(routingKeyPrefix string) (bool, error) {
+	return s.storage.Has(routingKeyPrefix)
+}
+
+func (s *SubscriptionStorage) Len() (int, error) {
+	return s.storage.Len()
+}
