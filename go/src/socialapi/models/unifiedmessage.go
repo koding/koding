e73db38c60d293b6bdd7015b1abff4d3db93f6ec
Cihangir SAVAS <cihangir@koding.com>
2014-02-13 00:55:53 -0800

    Social Api: add group and origin data into unified message

--- go/src/socialapi/models/unifiedmessage.go
+++ go/src/socialapi/models/unifiedmessage.go
@@ -3,9 +3,16 @@ package models
 import "time"
 
 type UnifiedMessage struct {
-	Id           int64
-	MessageType  string
-	Body         string
+	Id int64
+
+	MessageType string
+	Body        string
+
+	OriginType string
+	OriginId   int64
+
+	Group string
+
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 	DeletedAt    time.Time
