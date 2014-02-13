1a191542b4848cbb9594a723d9f845d6e59bcb40
Cihangir SAVAS <cihangir@koding.com>
2014-02-13 00:27:02 -0800

    DB: create a Db package

---
+++ go/src/socialapi/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+// Gorm is goroutines friendly, so you can create a global variable
+// to keep the connection and use it everywhere in your project
+var DB gorm.DB
+
+func init() {
+	var err error
+	DB, err = gorm.Open("postgres", "user=postgres password=123123123 dbname=godenemeler sslmode=disable")
+	if err != nil {
+		panic(fmt.Sprintf("Got error when connect database, the error is '%v'", err))
+	}
+	// By default, table name is plural of struct type, you can use struct type as table name with:
+	DB.SingularTable(true)
+	DB.LogMode(true)
+
+}
