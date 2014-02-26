2d8b6db75c1c48f654b8ac8815aa9dc4e2cd2ede
Cihangir SAVAS <cihangir@koding.com>
2014-02-25 22:58:46 -0800

    Kite: add documentation

--- go/src/koding/tools/kite/kite.go
+++ go/src/koding/tools/kite/kite.go
@@ -355,6 +355,8 @@ func (k *Kite) startRouting(stream <-chan amqp.Delivery, publishChannel *amqp.Ch
 						log.Warning("Dropped client because of message buffer overflow.")
 					}
 				} else {
+					// if user's routing key is old or doesnt exists, return a
+					// command to user for cycling the channel
 					log.Debug("Unknown routing key, send cycle channel for : %v", message.RoutingKey)
 					msg := []byte("{\"method\":\"cycleChannel\"}")
 					if err := publishChannel.Publish(k.PublishExchange, message.RoutingKey, false, false, amqp.Publishing{Body: msg}); err != nil {
