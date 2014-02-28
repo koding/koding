c845a577263b9faec1f0a67781c5bb12a64059c9
Cihangir SAVAS <cihangir@koding.com>
2014-02-27 18:56:13 -0800

    Redis/Tool: add a script for expiring redis keys

---
+++ go/src/koding/tools/redisexpire/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"koding/databases/redis"
+	"koding/tools/config"
+	"koding/tools/logger"
+	"time"
+
+	redigo "github.com/garyburd/redigo/redis"
+)
+
+var (
+	conf        *config.Config
+	flagDebug   = flag.Bool("d", false, "Debug mode")
+	flagProfile = flag.String("c", "", "Configuration profile from file")
+)
+
+func main() {
+	flag.Parse()
+	log := logger.New("Redis Expire Worker")
+
+	if *flagProfile == "" {
+		log.Fatal("Please specify profile via -c. Aborting.")
+	}
+	conf = config.MustConfig(*flagProfile)
+	redisSess, err := redis.NewRedisSession(conf.Redis)
+	if err != nil {
+		panic(err)
+	}
+	log.Info("Expire script started with config: %v", *flagProfile)
+
+	initialKey := fmt.Sprintf("%s-broker-client-*", conf.Environment)
+	sesssionKeys, err := redigo.Strings(redisSess.Do("KEYS", initialKey))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, sesssionKey := range sesssionKeys {
+		log.Debug("sesssionKey %v", sesssionKey)
+		time.Sleep(time.Millisecond * 10)
+		err := redisSess.Expire(sesssionKey, time.Minute*59)
+		if err != nil {
+			log.Error("An error occured while sending expire req %v", err)
+		}
+	}
+	log.Info("Expire script finished with config: %v", *flagProfile)
+}
