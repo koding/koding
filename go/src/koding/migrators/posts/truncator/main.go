302c5af0b2d965f1a87f670b3bac035e25be45e9
Cihangir SAVAS <cihangir@koding.com>
2014-03-03 15:58:00 -0800

    Truncator: switch using Iter helper

--- go/src/koding/migrators/posts/truncator/main.go
+++ go/src/koding/migrators/posts/truncator/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"koding/db/models"
-	mm "koding/db/mongodb"
+	"koding/db/mongodb"
+
 	helper "koding/db/mongodb/modelhelper"
+	"koding/helpers"
 	"koding/tools/config"
 	"koding/tools/logger"
 	"time"
@@ -19,150 +20,97 @@ var log = logger.New("post truncator")
 type strToInf map[string]interface{}
 
 var (
-	MAX_ITERATION_COUNT = 50
-	conf                *config.Config
-	SLEEPING_TIME       = 10 * time.Millisecond
-	oneMonthAgo         = time.Now().Add(-time.Hour * 24 * 30).UTC()
-	flagProfile         = flag.String("c", "vagrant", "Configuration profile from file")
-	flagSkip            = flag.Int("s", 0, "Configuration profile from file")
-	flagLimit           = flag.Int("l", 1000, "Configuration profile from file")
-	mongodb             *mm.MongoDB
-	deletedItems        = 0
+	conf          *config.Config
+	flagProfile   = flag.String("c", "vagrant", "Configuration profile from file")
+	flagDirection = flag.String("direction", "targetName", "direction name ")
+	flagSkip      = flag.Int("s", 0, "Configuration profile from file")
+	flagLimit     = flag.Int("l", 1000, "Configuration profile from file")
+	mongo         *mongodb.MongoDB
+	deletedItems  = 0
+	oneMonthAgo   = time.Now().Add(-time.Minute * 60 * 24 * 30).UTC()
 )
 
-func main() {
-	log.SetLevel(logger.DEBUG)
+func initialize() {
 	flag.Parse()
+	log.SetLevel(logger.INFO)
 	if *flagProfile == "" {
 		log.Fatal("Please specify profile via -c. Aborting.")
 	}
 
 	conf = config.MustConfig(*flagProfile)
 	helper.Initialize(conf.Mongo)
-	log.Info("Sync worker started")
-	mongodb = helper.Mongo
+	mongo = helper.Mongo
+}
+
+func main() {
+
+	// init the package
+	initialize()
+	log.Info("Truncater worker started")
+
+	var resultDataType map[string]interface{}
+
+	iterOptions := helpers.NewIterOptions()
+	iterOptions.Filter = helper.Selector{}
+	iterOptions.DataType = &resultDataType
+	iterOptions.Limit = *flagLimit
+	iterOptions.Skip = *flagSkip
+	iterOptions.Log = log
+
+	log.SetLevel(logger.DEBUG)
+
 	for _, coll := range ToBeDTruncatedNames {
-		collName := coll
-		err := mongodb.Run(collName, createQuery(collName))
+		iterOptions.CollectionName = coll
+		iterOptions.F = truncateItems(coll)
+		err := helpers.Iter(mongo, iterOptions)
 		if err != nil {
-			log.Fatal("Connecting to Mongo: %v", err)
+			log.Fatal("Error while iter: %v", err)
 		}
 	}
+
+	log.Info("Truncator worker finished")
 
 }
 
-func createQuery(collectionName string) func(coll *mgo.Collection) error {
-	return func(coll *mgo.Collection) error {
+func truncateItems(collectionName string) func(doc interface{}) {
+	return func(doc interface{}) {
+		result := *(doc.(*map[string]interface{}))
 
-		query := coll.Find(helper.Selector{})
-		totalCount, err := query.Count()
-		if err != nil {
-			log.Error("While getting count, exiting: %v", err)
-			return err
+		id, ok := result["_id"]
+		if !ok {
+			log.Error("result doesnt have _id %v", result)
+			return
+		}
+		var collectionId bson.ObjectId
+
+		collectionId = (id.(bson.ObjectId))
+
+		if !collectionId.Valid() {
+			log.Info("result id is not valid %v", collectionId)
+			return
 		}
 
-		if totalCount == 0 {
-			fmt.Println("Collection is empty ", collectionName)
-			log.Info("deleted item count %v", deletedItems)
-			return nil
+		if collectionId.Time().UTC().UnixNano() > oneMonthAgo.UnixNano() {
+			log.Info("not deleting docuemnt %v", collectionId.Time())
+			return
 		}
 
-		skip := *flagSkip
-		// this is a starting point
-		index := skip
-		// this is the item count to be processed
-		limit := *flagLimit
-		// this will be the ending point
-		count := index + limit
+		deleteRel(collectionId)
 
-		var result map[string]interface{}
+		log.Info("removing collectionId: %v from collectionName: %v ", collectionId.Hex(), collectionName)
 
-		iteration := 0
-		for {
-			// if we reach to the end of the all collection, exit
-			if index >= totalCount {
-				log.Info("All items are processed, exiting")
-				break
-			}
-
-			// this is the max re-iterating count
-			if iteration == MAX_ITERATION_COUNT {
-				break
-			}
-
-			// if we processed all items then exit
-			if index == count {
-				break
-			}
-
-			iter := query.Skip(index).Limit(count - index).Iter()
-			for iter.Next(&result) {
-				time.Sleep(SLEEPING_TIME)
-
-				deleteDoc(result, collectionName)
-
-				index++
-				log.Info("Index: %v", index)
-			}
-
-			if err := iter.Close(); err != nil {
-				log.Error("Iteration failed: %v", err)
-			}
-
-			if iter.Timeout() {
-				continue
-			}
-
-			log.Info("iter existed, starting over from %v  -- %v  item(s) are processsed on this iter", index+1, index-skip)
-			iteration++
+		if err := mongo.Run(collectionName, func(coll *mgo.Collection) error {
+			return coll.RemoveId(collectionId)
+		}); err != nil {
+			log.Error("couldnt remove collectionId: %v from collectionName: %v  ", collectionId.Hex(), collectionName)
 		}
-
-		if iteration == MAX_ITERATION_COUNT {
-			log.Info("Max iteration count %v reached, exiting", iteration)
-		}
-		log.Info("%v entries on this process", index-skip)
-		log.Info("deleted item count %v", deletedItems)
-
-		return nil
+		deletedItems++
 	}
-}
-
-func deleteDoc(result map[string]interface{}, collectionName string) {
-
-	id, ok := result["_id"]
-	if !ok {
-		log.Error("result doesnt have _id %v", result)
-		return
-	}
-	var collectionId bson.ObjectId
-
-	collectionId = (id.(bson.ObjectId))
-
-	if !collectionId.Valid() {
-		log.Info("result id is not valid %v", collectionId)
-		return
-	}
-
-	if collectionId.Time().UTC().UnixNano() > oneMonthAgo.UnixNano() {
-		log.Info("not deleting docuemnt %v", collectionId.Time())
-		return
-	}
-
-	deleteRel(collectionId)
-
-	log.Info("removing collectionId: %v from collectionName: %v ", collectionId.Hex(), collectionName)
-
-	if err := mongodb.Run(collectionName, func(coll *mgo.Collection) error {
-		return coll.RemoveId(collectionId)
-	}); err != nil {
-		log.Error("couldnt remove collectionId: %v from collectionName: %v  ", collectionId.Hex(), collectionName)
-	}
-	deletedItems++
 }
 
 func deleteRel(id bson.ObjectId) {
 	var rels []models.Relationship
-	if err := mongodb.Run("relationships", func(coll *mgo.Collection) error {
+	if err := mongo.Run("relationships", func(coll *mgo.Collection) error {
 		selector := helper.Selector{"$or": []helper.Selector{
 			helper.Selector{"sourceId": id},
 			helper.Selector{"targetId": id},
@@ -183,7 +131,7 @@ func deleteDocumentsFromRelationships(rels []models.Relationship) {
 	}
 
 	for _, rel := range rels {
-		if err := mongodb.Run("relationships", func(coll *mgo.Collection) error {
+		if err := mongo.Run("relationships", func(coll *mgo.Collection) error {
 			return coll.RemoveId(rel.Id)
 		}); err != nil {
 			log.Error("couldnt remove collectionId: %v from relationships  ", rel.Id.Hex())
