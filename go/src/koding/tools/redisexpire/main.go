8ff6f9f375cd190fb6a0108a93c1a6ab6e9d112a
Cihangir SAVAS <cihangir@koding.com>
2014-03-04 11:25:19 -0800

    RedisExpire: use flagDuration instead of string parsing

--- go/src/koding/tools/redisexpire/main.go
+++ go/src/koding/tools/redisexpire/main.go
@@ -15,7 +15,7 @@ var (
 	conf         *config.Config
 	flagDebug    = flag.Bool("d", false, "Debug mode")
 	flagProfile  = flag.String("c", "vagrant", "Configuration profile from file")
-	flagDuration = flag.String("t", "3600", "Duration for expire in seconds")
+	flagDuration = flag.Duration("t", time.Hour*1, "Duration for expire in seconds - Duration flag accept any input valid for time.ParseDuration.")
 	flagKey      = flag.String("k", "-broker-client-*", "Key for KEYS command, environment will be prepended")
 )
 
@@ -29,11 +29,6 @@ func main() {
 	log.SetLevel(logger.INFO)
 	if *flagDebug {
 		log.SetLevel(logger.DEBUG)
-	}
-
-	duration, err := time.ParseDuration(fmt.Sprintf("%ss", *flagDuration))
-	if err != nil {
-		panic(err)
 	}
 
 	redisSess, err := redis.NewRedisSession(conf.Redis)
@@ -52,7 +47,7 @@ func main() {
 		log.Debug("sesssionKey %v", sesssionKey)
 		time.Sleep(time.Millisecond * 10)
 
-		err := redisSess.Expire(sesssionKey, duration)
+		err := redisSess.Expire(sesssionKey, *flagDuration)
 		if err != nil {
 			log.Error("An error occured while sending expire req %v", err)
 		}
