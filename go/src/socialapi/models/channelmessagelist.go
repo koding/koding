6fc6d0d7e27db56dcc177f4e63d471eebe6a1948
Cihangir SAVAS <cihangir@koding.com>
2014-03-11 19:33:48 -0700

    Social: add model functions for channel message list

--- go/src/socialapi/models/channelmessagelist.go
+++ go/src/socialapi/models/channelmessagelist.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"socialapi/db"
+	"time"
+)
 
 type ChannelMessageList struct {
 	// unique identifier of the channel message list
@@ -14,4 +18,41 @@ type ChannelMessageList struct {
 
 	// Addition date of the message to the channel
 	AddedAt time.Time
+}
+
+func NewChannelMessageList() *ChannelMessageList {
+	now := time.Now().UTC()
+	return &ChannelMessageList{
+		ChannelId: 1,
+		MessageId: 1,
+		AddedAt:   now,
+	}
+}
+
+func (c *ChannelMessageList) Fetch() error {
+	if err := db.DB.First(c, c.Id).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ChannelMessageList) Save() error {
+	c.AddedAt = time.Now().UTC()
+
+	if err := db.DB.Save(c).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ChannelMessageList) Delete() error {
+	if c.Id == 0 {
+		return errors.New("Channel id is not set")
+	}
+
+	if err := db.DB.Delete(c).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
