c723dbe8690fcef7f35e38b3be6c822283525421
Cihangir SAVAS <cihangir@koding.com>
2014-03-11 17:31:40 -0700

    Socila: implement handlers for channel mesage

---
+++ go/src/socialapi/workers/api/modules/message/controller.go
@@ -0,0 +1,69 @@
+package message
+
+import (
+	"net/http"
+	"net/url"
+	"socialapi/models"
+	"socialapi/workers/api/modules/helpers"
+	"strconv"
+
+	"github.com/jinzhu/gorm"
+)
+
+func Create(u *url.URL, h http.Header, req *models.ChannelMessage) (int, http.Header, interface{}, error) {
+	if err := req.Save(); err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+
+	return helpers.NewOKResponse(req)
+}
+
+func Delete(u *url.URL, h http.Header, req *models.ChannelMessage) (int, http.Header, interface{}, error) {
+	id, err := strconv.ParseInt(u.Query().Get("id"), 10, 64)
+	if err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+
+	req.Id = id
+
+	if err := req.Delete(); err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+	// yes it is deleted but not removed completely from our system
+	return helpers.NewDeletedResponse()
+}
+
+func Update(u *url.URL, h http.Header, req *models.ChannelMessage) (int, http.Header, interface{}, error) {
+	id, err := strconv.ParseInt(u.Query().Get("id"), 10, 64)
+	if err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+	req.Id = id
+
+	if req.Id == 0 {
+		return helpers.NewBadRequestResponse()
+	}
+
+	if err := req.Save(); err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+
+	return helpers.NewOKResponse(req)
+}
+
+func Get(u *url.URL, h http.Header, req *models.ChannelMessage) (int, http.Header, interface{}, error) {
+	id, err := strconv.ParseInt(u.Query().Get("id"), 10, 64)
+	if err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+
+	req.Id = id
+	if err := req.Fetch(); err != nil {
+		if err == gorm.RecordNotFound {
+			return helpers.NewNotFoundResponse()
+		}
+		return helpers.NewBadRequestResponse()
+	}
+
+	return helpers.NewOKResponse(req)
+}
