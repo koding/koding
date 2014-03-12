c00574c894c08af6cfb9019a6853eebada69f1d7
Cihangir SAVAS <cihangir@koding.com>
2014-03-11 17:30:23 -0700

    Social: create comment model

---
+++ go/src/socialapi/models/comment.go
@@ -0,0 +1,26 @@
+package models
+
+import "time"
+
+type Comment struct {
+	// unique identifier of the channel message
+	Id int64
+
+	// holds parent message id of this comment
+	MessageId int64
+
+	// Body of the mesage
+	Body string
+
+	// type of the message
+	Type int
+
+	// Creator of the channel message
+	CreatorId int64
+
+	// Creation date of the message
+	CreatedAt time.Time
+
+	// Modification date of the message
+	ModifiedAt time.Time
+}
