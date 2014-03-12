ec4af46cc49b810f29bce68eb403111281da39b8
Cihangir SAVAS <cihangir@koding.com>
2014-03-11 17:28:02 -0700

    Social: remove files

--- go/src/socialapi/handlers/request.go
+++
@@ -1,6 +0,0 @@
-package handlers
-
-type Request struct {
-	ID    string      `json:"id"`
-	Stuff interface{} `json:"stuff"`
-}
--- go/src/socialapi/handlers/response.go
+++
@@ -1,6 +0,0 @@
-package handlers
-
-type Response struct {
-	ID    string      `json:"id"`
-	Stuff interface{} `json:"stuff"`
-}
