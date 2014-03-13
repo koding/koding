7a53c68c62612f4e26601895348eff0446e38c5f
Cihangir SAVAS <cihangir@koding.com>
2014-03-13 11:17:26 -0700

    Social: change models to use model helper functions

--- go/src/socialapi/models/channelmessage.go
+++ go/src/socialapi/models/channelmessage.go
@@ -1,10 +1,6 @@
 package models
 
-import (
-	"errors"
-	"socialapi/db"
-	"time"
-)
+import "time"
 
 type ChannelMessage struct {
 	// unique identifier of the channel message
@@ -24,6 +20,19 @@ type ChannelMessage struct {
 
 	// Modification date of the message
 	UpdatedAt time.Time
+	m         Model
+}
+
+func (c *ChannelMessage) GetId() int64 {
+	return c.Id
+}
+
+func (c *ChannelMessage) TableName() string {
+	return "channel_message"
+}
+
+func (c *ChannelMessage) Self() Modellable {
+	return c
 }
 
 const (
@@ -34,65 +43,26 @@ const (
 )
 
 func NewChannelMessage() *ChannelMessage {
-	now := time.Now().UTC()
-	return &ChannelMessage{
-		Type:      POST,
-		CreatedAt: now,
-		UpdatedAt: now,
-	}
-}
-
-func (c ChannelMessage) TableName() string {
-	return "channel_message"
+	return &ChannelMessage{}
 }
 
 func (c *ChannelMessage) Fetch() error {
-	if c.Id == 0 {
-		return errors.New("Channel message id is not set")
-	}
-	if err := First(c, c.Id); err != nil {
-		return err
-	}
-	return nil
+	return c.m.Fetch(c)
 }
 
 func (c *ChannelMessage) Update() error {
-	if c.Id == 0 {
-		return errors.New("Channel message id is not set")
-	}
-
-	if err := db.DB.
-		Table(c.TableName()).
-		Where(c.Id).
-		Update("body", c.Body).
-		Error; err != nil {
-		return err
-	}
-
-	return nil
+	// only update body
+	return c.m.UpdatePartial(c,
+		map[string]interface{}{
+			"body": c.Body,
+		},
+	)
 }
 
-func (c *ChannelMessage) Save() error {
-
-	now := time.Now().UTC()
-	// created at shouldnt be updated
-	c.CreatedAt = now
-	c.UpdatedAt = now
-
-	if err := Save(c); err != nil {
-		return err
-	}
-	return nil
+func (c *ChannelMessage) Create() error {
+	return c.m.Create(c)
 }
 
 func (c *ChannelMessage) Delete() error {
-	if c.Id == 0 {
-		return errors.New("Channel message id is not set")
-	}
-
-	if err := Delete(c); err != nil {
-		return err
-	}
-
-	return nil
+	return c.m.Delete(c)
 }
