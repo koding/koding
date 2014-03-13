34a07458db81ac659ace76840f3c66307e597dbd
Cihangir SAVAS <cihangir@koding.com>
2014-03-12 19:13:06 -0700

    Social: add tests for message model

---
+++ go/src/socialapi/tests/message.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"socialapi/models"
+)
+
+func testMessageOperations() {
+	post, err := createPost()
+	if err != nil {
+		fmt.Println("error while creating post", err)
+		err = nil
+	}
+	_, err = updatePost(post)
+	if err != nil {
+		fmt.Println("error while creating post", err)
+		err = nil
+	}
+
+	post2, err := getPost(post.Id)
+	if err != nil {
+		fmt.Println("error while getting the post", err)
+		err = nil
+	}
+
+	if post2.CreatedAt.Second() != post.CreatedAt.Second() {
+		fmt.Println("post created ats are not same")
+	}
+
+	err = deletePost(post.Id)
+	if err != nil {
+		fmt.Println("error while deleting the post", err)
+		err = nil
+	}
+
+	_, err = getPost(post.Id)
+	if err == nil {
+		fmt.Println("there should be an error while getting the post")
+	}
+
+	for i := 0; i < 10; i++ {
+		_, err := createPost()
+		if err != nil {
+			fmt.Println("error while creating post", err)
+			err = nil
+		}
+	}
+}
+
+func createPost() (*models.ChannelMessage, error) {
+	cm := models.NewChannelMessage()
+	cm.Body = "create a message"
+	cm.AccountId = ACCOUNT_ID
+
+	url := fmt.Sprintf("/channel/%d/message", CHANNEL_ID)
+	cmI, err := sendModel("POST", url, cm)
+	if err != nil {
+		return nil, err
+	}
+	return cmI.(*models.ChannelMessage), nil
+}
+
+func updatePost(cm *models.ChannelMessage) (*models.ChannelMessage, error) {
+	cm.Body = "after update"
+
+	url := fmt.Sprintf("/message/%d", cm.Id)
+	cmI, err := sendModel("POST", url, cm)
+	if err != nil {
+		return nil, err
+	}
+
+	return cmI.(*models.ChannelMessage), nil
+}
+
+func getPost(id int64) (*models.ChannelMessage, error) {
+
+	url := fmt.Sprintf("/message/%d", id)
+	cm := models.NewChannelMessage()
+	cmI, err := sendModel("GET", url, cm)
+	if err != nil {
+		return nil, err
+	}
+	return cmI.(*models.ChannelMessage), nil
+}
+
+func deletePost(id int64) error {
+	url := fmt.Sprintf("/message/%d", id)
+	_, err := sendRequest("DELETE", url, nil)
+	return err
+}
