6837b809cf3123ba523c2d999cfdc9a7d723144f
Cihangir SAVAS <cihangir@koding.com>
2014-03-13 11:14:48 -0700

    Social: change db to hold pointer to DB

--- go/src/socialapi/db/db.go
+++ go/src/socialapi/db/db.go
@@ -9,16 +9,16 @@ import (
 
 // Gorm is goroutines friendly, so you can create a global variable
 // to keep the connection and use it everywhere in your project
-var DB gorm.DB
+var DB *gorm.DB
 
 func init() {
 	var err error
-	DB, err = gorm.Open("postgres", "user=postgres password=123123123 dbname=social sslmode=disable")
+	db, err := gorm.Open("postgres", "user=postgres password=123123123 dbname=social sslmode=disable")
 	if err != nil {
 		panic(fmt.Sprintf("Got error when connect database, the error is '%v'", err))
 	}
 	// By default, table name is plural of struct type, you can use struct type as table name with:
-	DB.SingularTable(true)
-	DB.LogMode(true)
-
+	db.SingularTable(true)
+	db.LogMode(true)
+	DB = &db
 }
