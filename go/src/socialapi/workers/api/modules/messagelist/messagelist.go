e8ce8e2e15c4c488317d4c6ab6f7b471b2afb34b
Cihangir SAVAS <cihangir@koding.com>
2014-03-12 19:12:20 -0700

    Social: created message list module

---
+++ go/src/socialapi/workers/api/modules/messagelist/messagelist.go
@@ -0,0 +1,27 @@
+package messagelist
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"socialapi/models"
+	"socialapi/workers/api/modules/helpers"
+)
+
+func List(u *url.URL, h http.Header, req *models.ChannelMessageList) (int, http.Header, interface{}, error) {
+	channelId, err := helpers.GetId(u)
+	if err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+
+	fmt.Println(channelId)
+	// req.Id = id
+	// if err := req.Fetch(); err != nil {
+	// 	if err == gorm.RecordNotFound {
+	// 		return helpers.NewNotFoundResponse()
+	// 	}
+	// 	return helpers.NewBadRequestResponse()
+	// }
+
+	return helpers.NewOKResponse(req)
+}
