9c6b3a04a7692baa197338813abc6bef7537eca5
Cihangir SAVAS <cihangir@koding.com>
2014-03-14 19:35:57 -0700

    Social: add tests for history listing

---
+++ go/src/socialapi/tests/history.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"socialapi/models"
+)
+
+func testHistoryOperations() {
+	channel, err := createChannel()
+	if err != nil {
+		fmt.Println("error while creating channel", err)
+		err = nil
+	}
+
+	CHANNEL_ID = channel.Id
+
+	for i := 0; i < 10; i++ {
+		channelParticipant, err := createChannelParticipant(channel.Id)
+		if err != nil {
+			fmt.Println("error while creating channelParticipant 1", err)
+			err = nil
+			return
+		}
+
+		ACCOUNT_ID = channelParticipant.AccountId
+
+		_, err = createPost()
+		if err != nil {
+			fmt.Println("error while creating post", err)
+			err = nil
+		}
+
+		history, err := getHistory(CHANNEL_ID, ACCOUNT_ID)
+		if err != nil {
+			fmt.Println("Error while getting the history", err)
+		}
+		if history == nil {
+			fmt.Print("history is nil")
+			return
+		}
+		if len(history.MessageList) != i+1 {
+			fmt.Println("history length is not OK!", len(history.MessageList), i+1)
+		}
+	}
+
+}
+
+func getHistory(channelId, accountId int64) (*models.HistoryResponse, error) {
+	url := fmt.Sprintf("/channel/%d/history?accountId=%d", channelId, accountId)
+	res, err := sendRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var history models.HistoryResponse
+	err = json.Unmarshal(res, &history)
+	if err != nil {
+		return nil, err
+	}
+
+	return &history, nil
+}
