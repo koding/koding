e337a44349ffe08c6fb94e356c0402cd40941d0b
Fatih Arslan <fatih@koding.com>
2014-03-21 16:35:14 +0200

    terminal: add main package, rename old package to not conflict, compiles WIP

---
+++ go/src/koding/kites/terminal/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"koding/terminal"
+	"koding/tools/config"
+	"koding/tools/logger"
+	"log"
+	"os"
+)
+
+var (
+	flagProfile = flag.String("c", "", "Configuration profile from file")
+	flagRegion  = flag.String("r", "", "Configuration region from file")
+	flagDebug   = flag.Bool("d", false, "Debug mode")
+	flagVersion = flag.Bool("version", false, "Show version and exit")
+)
+
+func main() {
+	if *flagVersion {
+		fmt.Println(terminal.TERMINAL_VERSION)
+		os.Exit(0)
+	}
+
+	flag.Parse()
+	if *flagProfile == "" || *flagRegion == "" {
+		log.Fatal("Please specify profile via -c and region via -r. Aborting.")
+	}
+
+	var logLevel logger.Level
+	if *flagDebug {
+		logLevel = logger.DEBUG
+	} else {
+		logLevel = logger.GetLoggingLevelFromConfig("oskite", *flagProfile)
+	}
+
+	term := terminal.New(config.MustConfig(*flagProfile))
+	term.LogLevel = logLevel
+
+	// go go!
+	term.Run()
+}
