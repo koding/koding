9b3d279f735f5d3183eb5dd03f482e20ed869cb8
Cihangir SAVAS <cihangir@koding.com>
2014-03-21 00:01:24 -0700

    Social: add follow api handler

--- go/src/socialapi/workers/api/modules/account/account.go
+++ go/src/socialapi/workers/api/modules/account/account.go
@@ -23,6 +23,21 @@ func ListChannels(u *url.URL, h http.Header, _ interface{}) (int, http.Header, i
 
 	return helpers.NewOKResponse(channels)
 }
+
+func Follow(u *url.URL, h http.Header, req *models.Account) (int, http.Header, interface{}, error) {
+	targetId, err := helpers.GetURIInt64(u, "id")
+	if err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+
+	cp, err := req.Follow(targetId)
+	if err != nil {
+		return helpers.NewBadRequestResponse()
+	}
+
+	return helpers.NewOKResponse(cp)
+}
+
 func Unfollow(u *url.URL, h http.Header, req *models.Account) (int, http.Header, interface{}, error) {
 	targetId, err := helpers.GetURIInt64(u, "id")
 	if err != nil {
