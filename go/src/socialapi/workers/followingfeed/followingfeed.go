a80ce20a60db8c97eb5c8185af6a99e99ec1e279
Cihangir SAVAS <cihangir@koding.com>
2014-03-28 22:11:55 -0700

    Social: remove global variables

--- go/src/socialapi/workers/followingfeed/followingfeed.go
+++ go/src/socialapi/workers/followingfeed/followingfeed.go
@@ -6,38 +6,32 @@ import (
 	followingfeed "socialapi/workers/followingfeed/lib"
 	"socialapi/workers/helper"
 
-	"github.com/koding/bongo"
-	"github.com/koding/logging"
 	"github.com/koding/worker"
 )
 
 var (
-	Bongo       *bongo.Bongo
-	log         logging.Logger
-	conf        *config.Config
 	flagProfile = flag.String("c", "", "Configuration profile from file")
 	flagDebug   = flag.Bool("d", false, "Debug mode")
-	handler     *followingfeed.FollowingFeedController
 )
 
 func main() {
 	flag.Parse()
 	if *flagProfile == "" {
-		log.Fatal("Please define config file with -c", "")
+		panic("Please define config file with -c")
 	}
 
-	conf = config.Read(*flagProfile)
+	conf := config.Read(*flagProfile)
 
 	// create logger for our package
-	log = helper.CreateLogger("TopicFeedWorker", *flagDebug)
+	log := helper.CreateLogger("TopicFeedWorker", *flagDebug)
 
 	// panics if not successful
-	Bongo = helper.MustInitBongo(conf, log)
+	bongo := helper.MustInitBongo(conf, log)
 	// do not forgot to close the bongo connection
-	defer Bongo.Close()
+	defer bongo.Close()
 
 	// create message handler
-	handler = followingfeed.NewFollowingFeedController(log)
+	handler := followingfeed.NewFollowingFeedController(log)
 
 	listener := worker.NewListener("FollowingFeed", conf.EventExchangeName, log)
 	// blocking
