c627ba460e505d05c8ed987dcd0bb84ce743e160
Cihangir SAVAS <cihangir@koding.com>
2014-03-28 17:09:57 -0700

    Social: create rabbitmq helper, it is a mapper and initializer

---
+++ go/src/socialapi/workers/helper/rabbitmqhelper.go
@@ -0,0 +1,22 @@
+package helper
+
+import (
+	"koding/tools/config"
+	"github.com/koding/logging"
+
+	"github.com/koding/rabbitmq"
+)
+
+func NewRabbitMQ(conf *config.Config, log logging.Logger) *rabbitmq.RabbitMQ {
+	return rabbitmq.New(createRabbitMqConf(conf), log)
+}
+
+func createRabbitMqConf(conf *config.Config) *rabbitmq.Config {
+	return &rabbitmq.Config{
+		Host:     conf.Mq.Host,
+		Port:     conf.Mq.Port,
+		Username: conf.Mq.ComponentUser,
+		Password: conf.Mq.Password,
+		Vhost:    conf.Mq.Vhost,
+	}
+}
