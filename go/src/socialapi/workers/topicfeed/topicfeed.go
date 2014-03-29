dd9f49cd40cf967150717211fcb98717d219fbed
Cihangir SAVAS <cihangir@koding.com>
2014-03-28 22:02:13 -0700

    Social: remove start handler from worker

--- go/src/socialapi/workers/topicfeed/topicfeed.go
+++ go/src/socialapi/workers/topicfeed/topicfeed.go
@@ -6,12 +6,9 @@ import (
 	"socialapi/config"
 	"socialapi/workers/helper"
 	topicfeed "socialapi/workers/topicfeed/lib"
-
-	"github.com/jinzhu/gorm"
 	"github.com/koding/bongo"
 	"github.com/koding/logging"
 	"github.com/koding/worker"
-	"github.com/streadway/amqp"
 )
 
 var (
@@ -42,35 +39,10 @@ func main() {
 	// create message handler
 	handler = topicfeed.NewTopicFeedController(log)
 
-	listener := worker.NewListener("TopicFeed", conf.EventExchangeName)
+	listener := worker.NewListener("TopicFeed", conf.EventExchangeName, log)
 	// blocking
 	// listen for events
-	listener.Listen(helper.NewRabbitMQ(conf, log), startHandler)
+	listener.Listen(helper.NewRabbitMQ(conf, log), handler)
 	// close consumer
 	defer listener.Close()
-}
-
-func startHandler() func(delivery amqp.Delivery) {
-	log.Info("Worker Started to Consume")
-	return func(delivery amqp.Delivery) {
-		err := handler.HandleEvent(delivery.Type, delivery.Body)
-		switch err {
-		case nil:
-			delivery.Ack(false)
-		case topicfeed.HandlerNotFoundErr:
-			log.Notice("unknown event type (%s) recieved, \n deleting message from RMQ", delivery.Type)
-			delivery.Ack(false)
-		case gorm.RecordNotFound:
-			log.Warning("Record not found in our db (%s) recieved, \n deleting message from RMQ", string(delivery.Body))
-			delivery.Ack(false)
-		default:
-			// add proper error handling
-			// instead of puttting message back to same queue, it is better
-			// to put it to another maintenance queue/exchange
-			log.Error("an error occured %s, \n putting message back to queue", err)
-			// multiple false
-			// reque true
-			delivery.Nack(false, true)
-		}
-	}
 }
