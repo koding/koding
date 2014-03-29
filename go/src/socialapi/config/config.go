42103516c0021d494e01a1033d8c6ee044eb5eb1
Cihangir SAVAS <cihangir@koding.com>
2014-03-28 21:10:23 -0700

    Social: create struct for config

--- go/src/socialapi/config/config.go
+++ go/src/socialapi/config/config.go
@@ -1,8 +1,27 @@
 package config
 
-type Config struct {
-	Foo string
-	Bar int
-}
+type (
+	Postgres struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+		DBName   string
+	}
+	Mq struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+		Vhost    string
+	}
+
+	Config struct {
+		Postgres          Postgres
+		Mq                Mq
+		EventExchangeName string
+		Mongo             string
+	}
+)
 
 var EventExchangeName = "BrokerMessageBus"
