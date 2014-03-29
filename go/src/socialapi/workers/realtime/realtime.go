9a48c8793ad242ba2598042825117292cc4c719c
Cihangir SAVAS <cihangir@koding.com>
2014-03-28 22:12:07 -0700

    Social: remove global variables

--- go/src/socialapi/workers/realtime/realtime.go
+++ go/src/socialapi/workers/realtime/realtime.go
@@ -7,35 +7,29 @@ import (
 	"socialapi/workers/helper"
 	realtime "socialapi/workers/realtime/lib"
 
-	"github.com/koding/bongo"
-	"github.com/koding/logging"
 	"github.com/koding/worker"
 )
 
 var (
-	Bongo       *bongo.Bongo
-	log         = logging.NewLogger("RealtimeWorker")
-	conf        *config.Config
 	flagProfile = flag.String("c", "", "Configuration profile from file")
 	flagDebug   = flag.Bool("d", false, "Debug mode")
-	handler     *realtime.RealtimeWorkerController
 )
 
 func main() {
 	flag.Parse()
 	if *flagProfile == "" {
-		log.Fatal("Please define config file with -c", "")
+		panic("Please define config file with -c")
 	}
 
-	conf = config.Read(*flagProfile)
+	conf := config.Read(*flagProfile)
 
 	// create logger for our package
-	log = helper.CreateLogger("TopicFeedWorker", *flagDebug)
+	log := helper.CreateLogger("RealtimeWorker", *flagDebug)
 
 	// panics if not successful
-	Bongo = helper.MustInitBongo(conf, log)
+	bongo := helper.MustInitBongo(conf, log)
 	// do not forgot to close the bongo connection
-	defer Bongo.Close()
+	defer bongo.Close()
 
 	// init mongo connection
 	modelhelper.Initialize(conf.Mongo)
@@ -43,12 +37,10 @@ func main() {
 	//create connection to RMQ for publishing realtime events
 	rmq := helper.NewRabbitMQ(conf, log)
 
-	h, err := realtime.NewRealtimeWorkerController(rmq, log)
+	handler, err := realtime.NewRealtimeWorkerController(rmq, log)
 	if err != nil {
 		panic(err)
 	}
-
-	handler = h
 
 	listener := worker.NewListener("RealtimeWorker", conf.EventExchangeName, log)
 	// blocking
