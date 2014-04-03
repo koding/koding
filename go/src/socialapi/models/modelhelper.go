0a2d4f28e81e72af4ec34d0241e976ad703e1803
Cihangir SAVAS <cihangir@koding.com>
2014-04-02 23:41:44 -0700

    Socil: fix slug generation

--- go/src/socialapi/models/modelhelper.go
+++ go/src/socialapi/models/modelhelper.go
@@ -4,19 +4,29 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	"github.com/koding/bongo"
 	"github.com/stvp/slug"
 )
 
-// todo do not create slugs for "Replies" and "Chat Messages"
+// todo fix slug generation with the slugifiable.coffee implementation
 func Slugify(message *ChannelMessage) (*ChannelMessage, error) {
+
+	if message.TypeConstant != ChannelMessage_TYPE_POST {
+		return message, nil
+	}
+
 	// we want hypen between our words
 	slug.Replacement = '-'
 	res := NewChannelMessage()
 
 	sugguestedSlug := slug.Clean(message.Body)
+	if len(sugguestedSlug) > 80 {
+		sugguestedSlug = sugguestedSlug[:79]
+	}
+
 	query := map[string]interface{}{
 		"slug": sugguestedSlug,
 	}
@@ -31,10 +41,15 @@ func Slugify(message *ChannelMessage) (*ChannelMessage, error) {
 				return nil, err
 			}
 			message.Slug = sugguestedSlug
+			// message.Slug = &sugguestedSlug
+			// message.Slug.String = sugguestedSlug
+			// message.Slug.Valid = true
 			return message, nil
 		}
 		// iterate with the new slug
-		sugguestedSlug = sugguestedSlug + "-" + strconv.Itoa(rand.Intn(1000))
+		// this is not the best strategy to generate slug
+		// but also not the worst
+		sugguestedSlug = sugguestedSlug + "-" + strconv.Itoa(rand.Intn(1000000000))
 		query["slug"] = sugguestedSlug
 	}
 
