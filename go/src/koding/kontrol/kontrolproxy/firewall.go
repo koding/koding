f249f9cda83527fcb20ac7fb50369a64ce22db88
Fatih Arslan <fatih@koding.com>
2014-04-04 20:10:24 +0300

    kdproxy: debug statemens, WIP

--- go/src/koding/kontrol/kontrolproxy/firewall.go
+++ go/src/koding/kontrol/kontrolproxy/firewall.go
@@ -2,12 +2,13 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 	"koding/db/models"
 	"koding/db/mongodb/modelhelper"
 	"net/http"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/juju/ratelimit"
 )
@@ -32,8 +33,9 @@ type CheckCountry struct {
 }
 
 type CheckRequest struct {
-	Host string
-	Rate int
+	Host     string
+	Rate     int
+	RateType string
 }
 
 func firewallHandler(h http.Handler) http.Handler {
@@ -41,11 +43,14 @@ func firewallHandler(h http.Handler) http.Handler {
 		rest, err := modelhelper.GetRestrictionByDomain(r.Host)
 		if err != nil {
 			// don't block if we don't get a rule (pre-caution))
+			fmt.Println("no restriction available")
 			h.ServeHTTP(w, r)
 			return
 		}
 
+		fmt.Printf("%d restrictions \n", len(rest.RuleList))
 		for _, rule := range rest.RuleList {
+			fmt.Printf("rule %+v\n", rule)
 			if a := ApplyRule(rule, r); a != nil {
 				a.ServeHTTP(w, r)
 				return
@@ -58,9 +63,9 @@ func firewallHandler(h http.Handler) http.Handler {
 
 // ApplyRule checks the rule and returns an http.Handler to be executed. A nil
 // handler means there is no http.Handler to be executed. For example if the
-// user is allowed to pass the rule a "nil" http.Handler is returned, however
-// if the user is denied a `quotaExceeded` template handler is returned that
-// neneeds to be exectued
+// user is allowed to pass,  a "nil" http.Handler is returned, however if the
+// user is denied a `quotaExceeded` template handler is returned that neneeds
+// to be exectued
 func ApplyRule(rule models.Rule, r *http.Request) http.Handler {
 	if !rule.Enabled {
 		return nil
@@ -71,9 +76,12 @@ func ApplyRule(rule models.Rule, r *http.Request) http.Handler {
 		return nil // if not found just continue with next rule
 	}
 
+	fmt.Printf("filter %+v\n", filter)
+
 	// country is empty for now
 	checker, err := GetChecker(filter, getIP(r.RemoteAddr), "", r.Host)
 	if err != nil {
+		fmt.Println("GetChecker", err)
 		return nil
 	}
 
@@ -104,33 +112,46 @@ func ApplyRule(rule models.Rule, r *http.Request) http.Handler {
 }
 
 func GetChecker(f models.Filter, ip, country, host string) (Checker, error) {
+	fmt.Printf("ip %+v\n", ip)
+
 	switch f.Type {
 	case "ip":
 		return &CheckIP{IP: ip, Pattern: f.Match}, nil
 	case "country":
 		return &CheckCountry{Country: country, Pattern: f.Match}, nil
-	case "request":
+	case "request.second", "request.minute", "request.hourequest.hour":
 		rate, err := strconv.Atoi(f.Match)
 		if err != nil {
 			return nil, err
 		}
 
-		return &CheckRequest{Host: host, Rate: rate}, nil
+		return &CheckRequest{Host: host, Rate: rate, RateType: strings.TrimPrefix(f.Type, "request.")}, nil
 	}
 
-	return nil, errors.New("no checker found")
+	return nil, fmt.Errorf("no checker found for %s", f.Type)
 }
 
 func (c *CheckRequest) Check() bool {
+	var dividor float64
+	switch c.RateType {
+	case "second":
+		dividor = 1
+	case "minute":
+		dividor = 60
+	case "hour":
+		dividor = 3600
+	}
+
 	var b *ratelimit.Bucket
 	b, ok := buckets[c.Host]
 	if !ok {
-		b = ratelimit.NewBucketWithRate(float64(c.Rate), int64(c.Rate))
+		b = ratelimit.NewBucketWithRate(float64(c.Rate)/dividor, int64(c.Rate))
 		buckets[c.Host] = b
 	}
 
-	// one request
-	available := b.TakeAvailable(1)
+	available := b.TakeAvailable(1) // one request
+
+	fmt.Printf("available %+v\n", available)
 	if available == 0 {
 		return false
 	}
