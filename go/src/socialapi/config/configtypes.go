bec5171592c73cd72eb6e21d7cfa1114fd36be8f
Cihangir SAVAS <cihangir@koding.com>
2014-04-04 19:01:12 -0700

    Social: create config types

---
+++ go/src/socialapi/config/configtypes.go
@@ -0,0 +1,49 @@
+package config
+
+type (
+	Postgres struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+		DBName   string
+	}
+	Mq struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+		Vhost    string
+	}
+
+	Config struct {
+		Postgres          Postgres
+		Mq                Mq
+		EventExchangeName string
+		Mongo             string
+		Limits            Limits
+	}
+	Limits struct {
+		MessageBodyMinLen int
+	}
+)
+
+// return &Config{
+// 	// set postgres connection config
+// 	Postgres: Postgres{
+// 		Host:     "localhost",
+// 		Port:     5432,
+// 		Username: "postgres",
+// 		Password: "123123123",
+// 		DBName:   "social",
+// 	},
+// 	EventExchangeName: "BrokerMessageBus",
+// 	Mongo:             "localhost:27017/koding",
+// 	Mq: Mq{
+// 		Host:     "localhost",
+// 		Port:     5672,
+// 		Username: "PROD-k5it50s4676pO9O",
+// 		Password: "djfjfhgh4455__5",
+// 		Vhost:    "/",
+// 	},
+// }
