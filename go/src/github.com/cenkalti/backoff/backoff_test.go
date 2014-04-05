60ca514b5b402de5f383f10e61f5daa42d08aa4d
Cenk Alti <cenkalti@gmail.com>
2014-04-04 17:24:26 -0700

    kite: add dependency: cenkalti/backoff

---
+++ go/src/github.com/cenkalti/backoff/backoff_test.go
@@ -0,0 +1,21 @@
+package backoff
+
+import (
+	"time"
+
+	"testing"
+)
+
+func TestNextBackOffMillis(t *testing.T) {
+	subtestNextBackOff(t, 0, new(ZeroBackOff))
+	subtestNextBackOff(t, Stop, new(StopBackOff))
+}
+
+func subtestNextBackOff(t *testing.T, expectedValue time.Duration, backOffPolicy BackOff) {
+	for i := 0; i < 10; i++ {
+		next := backOffPolicy.NextBackOff()
+		if next != expectedValue {
+			t.Errorf("got: %d expected: %d", next, expectedValue)
+		}
+	}
+}
---
+++ go/src/github.com/cenkalti/backoff/retry.go
@@ -0,0 +1,39 @@
+package backoff
+
+import "time"
+
+// Retry the function f until it does not return error or BackOff stops.
+//
+// Example:
+// 	operation := func() error {
+// 		// An operation that may fail
+// 	}
+//
+// 	err := backoff.Retry(operation, backoff.NewExponentialBackoff())
+// 	if err != nil {
+// 		// handle error
+// 	}
+//
+// 	// operation is successfull
+func Retry(f func() error, b BackOff) error {
+	err := f()
+	if err == nil {
+		return nil
+	}
+
+	b.Reset()
+	for {
+		next := b.NextBackOff()
+		if next == Stop {
+			return err
+		}
+
+		time.Sleep(time.Duration(next))
+		err = f()
+		if err != nil {
+			continue
+		}
+
+		return nil
+	}
+}
