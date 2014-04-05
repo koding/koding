5f9a3a4d1b781ed9dce4891a71e581f9f9dcc01c
Cihangir SAVAS <cihangir@koding.com>
2014-04-04 18:59:35 -0700

    Social: add one function

--- go/src/socialapi/models/interaction.go
+++ go/src/socialapi/models/interaction.go
@@ -48,6 +48,10 @@ func NewInteraction() *Interaction {
 	return &Interaction{}
 }
 
+func (i *Interaction) One(q *bongo.Query) error {
+	return bongo.B.One(i, i, q)
+}
+
 func (i *Interaction) Fetch() error {
 	return bongo.B.Fetch(i)
 }
