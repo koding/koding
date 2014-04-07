17decdd15e603eba81d42ba6293e1f665c4934cc
Cihangir SAVAS <cihangir@koding.com>
2014-04-07 01:48:24 -0700

    Socail: add deletion tests

--- go/src/socialapi/tests/group_channel_test.go
+++ go/src/socialapi/tests/group_channel_test.go
@@ -19,6 +19,36 @@ func TestGroupChannel(t *testing.T) {
 
 		Convey("normal user cant delete it", nil)
 
+		Convey("owner cant delete it", func() {
+			account := models.NewAccount()
+			account.OldId = AccountOldId.Hex()
+			account, err := createAccount(account)
+			So(err, ShouldBeNil)
+			So(account, ShouldNotBeNil)
+
+			channel1, err := createChannelByGroupNameAndType(account.Id, "testgroup", models.Channel_TYPE_GROUP)
+			So(err, ShouldBeNil)
+			So(channel1, ShouldNotBeNil)
+
+			err = deleteChannel(account.Id, channel1.Id)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("normal user cant delete it", func() {
+			account := models.NewAccount()
+			account.OldId = AccountOldId.Hex()
+			account, err := createAccount(account)
+			So(err, ShouldBeNil)
+			So(account, ShouldNotBeNil)
+
+			channel1, err := createChannelByGroupNameAndType(account.Id, "testgroup", models.Channel_TYPE_GROUP)
+			So(err, ShouldBeNil)
+			So(channel1, ShouldNotBeNil)
+
+			err = deleteChannel(rand.Int63(), channel1.Id)
+			So(err, ShouldNotBeNil)
+		})
+
 		Convey("member can post status update", nil)
 
 		Convey("non-member can not post status update", nil)
