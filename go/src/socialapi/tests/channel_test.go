a34ba500311adbde7ede56024235fb48806a9398
Cihangir SAVAS <cihangir@koding.com>
2014-04-07 12:20:23 -0700

    Social: move basic channel operations into channel_test file

--- go/src/socialapi/tests/channel_test.go
+++ go/src/socialapi/tests/channel_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"socialapi/models"
+	"strconv"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -168,4 +172,59 @@ func TestChannelCreation(t *testing.T) {
 			})
 		})
 	})
+}
+
+func createChannel() (*models.Channel, error) {
+	c := models.NewChannel()
+	rand.Seed(time.Now().UnixNano())
+	groupName := c.GroupName + strconv.Itoa(rand.Intn(100000000))
+
+	return createChannelByGroupNameAndType(rand.Int63(), groupName, c.TypeConstant)
+}
+
+func createChannelByGroupNameAndType(creatorId int64, groupName, typeConstant string) (*models.Channel, error) {
+	c := models.NewChannel()
+	c.GroupName = groupName
+	c.CreatorId = creatorId
+	c.TypeConstant = typeConstant
+	c.Name = c.Name + strconv.Itoa(rand.Intn(100000000))
+	cm, err := sendModel("POST", "/channel", c)
+	if err != nil {
+		return nil, err
+	}
+	return cm.(*models.Channel), nil
+}
+
+func updateChannel(cm *models.Channel) (*models.Channel, error) {
+	url := fmt.Sprintf("/channel/%d", cm.Id)
+	cmI, err := sendModel("POST", url, cm)
+	if err != nil {
+		return nil, err
+	}
+
+	return cmI.(*models.Channel), nil
+}
+
+func getChannel(id int64) (*models.Channel, error) {
+
+	url := fmt.Sprintf("/channel/%d", id)
+	cm := models.NewChannel()
+	cmI, err := sendModel("GET", url, cm)
+	if err != nil {
+		return nil, err
+	}
+	return cmI.(*models.Channel), nil
+}
+
+func deleteChannel(creatorId, channelId int64) error {
+	c := models.NewChannel()
+	c.CreatorId = creatorId
+	c.Id = channelId
+
+	url := fmt.Sprintf("/channel/%d/delete", channelId)
+	_, err := sendModel("POST", url, c)
+	if err != nil {
+		return err
+	}
+	return nil
 }
