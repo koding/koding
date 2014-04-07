77bff8375e8dc7ff34e816e39322e5ba7bdd5641
Cihangir SAVAS <cihangir@koding.com>
2014-04-07 00:44:34 -0700

    Social: create group channel test file

---
+++ go/src/socialapi/tests/group_channel_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGroupChannel(t *testing.T) {
+	Convey("while testing group channel", t, func() {
+
+		Convey("channel should be there", nil)
+
+		Convey("owner should be able to update it", nil)
+
+		Convey("normal user should not be able to update it", nil)
+
+		Convey("owner cant delete it", nil)
+
+		Convey("normal user cant delete it", nil)
+
+		Convey("member can post status update", nil)
+
+		Convey("non-member can not post status update", nil)
+
+		Convey("member can post status update", nil)
+
+		Convey("non-member can not post status update", nil)
+
+	})
+}
