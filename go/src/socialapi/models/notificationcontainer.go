c4796d2b8f1669576879f5408cf40eef24fa3a2f
Can Yucel <can@koding.com>
2014-04-07 20:58:23 -0700

    SocialApi: NotificationContainer is added for defining data structure in notification listing

---
+++ go/src/socialapi/models/notificationcontainer.go
@@ -0,0 +1,21 @@
+package models
+
+type NotificationContainer struct {
+	Type     string         `json:"type"`
+	TargetId int64          `json:"targetId"`
+	Content  string         `json:"content"`
+	Actors   ActorContainer `json:"actors"`
+}
+
+type ActorContainer struct {
+	LatestActors []int64 `json:"actors"`
+	Count        int     `json:"count"`
+}
+
+func NewNotificationContainer() *NotificationContainer {
+	return &NotificationContainer{}
+}
+
+func NewActorContainer() *ActorContainer {
+	return &ActorContainer{}
+}
