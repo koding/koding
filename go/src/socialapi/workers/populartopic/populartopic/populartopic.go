9d9fe07e0d2057c10331ba447abf99f592bbadd3
Cihangir SAVAS <cihangir@koding.com>
2014-04-07 18:08:59 -0700

    Social: implement popular topics algorithm

--- go/src/socialapi/workers/populartopic/populartopic/populartopic.go
+++ go/src/socialapi/workers/populartopic/populartopic/populartopic.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/koding/logging"
+	"github.com/koding/redis"
 	"github.com/koding/worker"
 	"github.com/streadway/amqp"
 )
@@ -21,6 +22,7 @@ type Action func(*PopularTopicsController, *models.ChannelMessageList) error
 type PopularTopicsController struct {
 	routes map[string]Action
 	log    logging.Logger
+	redis  *redis.RedisSession
 }
 
 func (t *PopularTopicsController) DefaultErrHandler(delivery amqp.Delivery, err error) {
@@ -30,9 +32,10 @@ func (t *PopularTopicsController) DefaultErrHandler(delivery amqp.Delivery, err 
 	delivery.Nack(false, true)
 }
 
-func NewPopularTopicsController(log logging.Logger) *PopularTopicsController {
+func NewPopularTopicsController(log logging.Logger, redis *redis.RedisSession) *PopularTopicsController {
 	ffc := &PopularTopicsController{
-		log: log,
+		log:   log,
+		redis: redis,
 	}
 
 	routes := map[string]Action{
@@ -41,7 +44,6 @@ func NewPopularTopicsController(log logging.Logger) *PopularTopicsController {
 	}
 
 	ffc.routes = routes
-
 	return ffc
 }
 
@@ -71,179 +73,83 @@ func (f *PopularTopicsController) HandleEvent(event string, data []byte) error {
 }
 
 func (f *PopularTopicsController) MessageSaved(data *models.ChannelMessageList) error {
+	incrementedVal, err := f.redis.SortedSetIncrBy(GetWeeklyKey(data), 1, data.MessageId)
+	if err != nil {
+		return err
+	}
+
+	incrementedVal, err = f.redis.SortedSetIncrBy(GetMonthlyKey(data), 1, data.MessageId)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (f *PopularTopicsController) MessageDeleted(data *models.ChannelMessageList) error {
+	incrementedVal, err := f.redis.SortedSetIncrBy(GetWeeklyKey(data), -1, data.MessageId)
+	if err != nil {
+		return err
+	}
+
+	incrementedVal, err = f.redis.SortedSetIncrBy(GetMonthlyKey(data), -1, data.MessageId)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func prepareKey(dateKey string) string {
+func GetRedisPrefix() string {
 	return fmt.Sprintf(
-		"%s-%s-%s",
+		"%s:%s",
 		config.Get().Environment,
 		PopularTopicKey,
-		dateKey,
 	)
 }
 
-func GetWeeklyKey(cml *ChannelMessageList) string {
+func GetWeeklyKey(cml *models.ChannelMessageList) string {
 	weekNumber := 0
 	year := 2014
 
-	if cml.AddedAt != nil {
+	if !cml.AddedAt.IsZero() {
 		_, weekNumber = cml.AddedAt.ISOWeek()
 		year, _, _ = cml.AddedAt.UTC().Date()
 	} else {
 		// no need to convert it to UTC
-		_, weekNumber = time.Now.ISOWeek()
+		_, weekNumber = time.Now().ISOWeek()
 		year, _, _ = time.Now().UTC().Date()
 
 	}
 
 	dateKey := fmt.Sprintf(
-		"%s-%d-%d",
+		"%s:%d:%d",
 		"weekly",
 		year,
 		weekNumber,
 	)
-	return prepareKey(dateKey)
+	return dateKey
 }
 
-func GetMonthlyKey(cml *ChannelMessageList) string {
-	month := 0
+func GetMonthlyKey(cml *models.ChannelMessageList) string {
+	var month time.Month
 	year := 2014
 
-	if cml.AddedAt != nil {
+	if !cml.AddedAt.IsZero() {
 		year, month, _ = cml.AddedAt.UTC().Date()
 	} else {
 		year, month, _ = time.Now().UTC().Date()
 	}
 
 	dateKey := fmt.Sprintf(
-		"%s-%d-%d",
+		"%s:%d:%d",
 		"monthly",
 		year,
-		month,
+		int(month),
 	)
-	return prepareKey(dateKey)
+
+	return dateKey
 }
-
-// func (f *PopularTopicsController) MessageUpdated(data *models.ChannelMessage) error {
-// 	f.log.Debug("udpate message %s", data.Id)
-// 	// fetch message's current topics from the db
-// 	channels, err := fetchMessageChannels(data.Id)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// get current topics from
-// 	topics := extractTopics(data.Body)
-// 	if topics == nil {
-// 		return nil
-// 	}
-
-// 	// if message and the topics dont have any item, we can safely return
-// 	if len(channels) == 0 && len(topics) == 0 {
-// 		return nil
-// 	}
-
-// 	res := getTopicDiff(channels, topics)
-
-// 	// add messages
-// 	if len(res["added"]) > 0 {
-// 		initialChannel, err := fetchChannel(data.InitialChannelId)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		if err := ensureChannelMessages(initialChannel, data, res["added"]); err != nil {
-// 			return err
-// 		}
-// 	}
-
-// 	// delete messages
-// 	if len(res["deleted"]) > 0 {
-// 		if err := deleteChannelMessages(channels, data, res["deleted"]); err != nil {
-// 			return err
-// 		}
-// 	}
-
-// 	return nil
-// }
-
-// func deleteChannelMessages(channels []models.Channel, data *models.ChannelMessage, toBeDeletedTopics []string) error {
-// 	for _, channel := range channels {
-// 		for _, topic := range toBeDeletedTopics {
-// 			if channel.Name != topic {
-// 				continue
-// 			}
-
-// 			cml := models.NewChannelMessageList()
-// 			selector := map[string]interface{}{
-// 				"message_id": data.Id,
-// 				"channel_id": channel.Id,
-// 			}
-
-// 			if err := cml.DeleteMessagesBySelector(selector); err != nil {
-// 				return err
-// 			}
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func fetchMessageChannels(messageId int64) ([]models.Channel, error) {
-// 	cml := models.NewChannelMessageList()
-// 	return cml.FetchMessageChannels(messageId)
-// }
-
-// func getTopicDiff(channels []models.Channel, topics []string) map[string][]string {
-// 	res := make(map[string][]string)
-
-// 	// aggregate all channel names into map
-// 	channelNames := map[string]struct{}{}
-// 	for _, channel := range channels {
-// 		channelNames[channel.Name] = struct{}{}
-// 	}
-
-// 	// range over new topics, bacause we are gonna remove
-// 	// unused channels
-// 	for _, topic := range topics {
-// 		found := false
-// 		for channelName := range channelNames {
-// 			if channelName == topic {
-// 				found = true
-// 			}
-// 		}
-// 		if !found {
-// 			res["added"] = append(res["added"], topic)
-// 		} else {
-// 			// if we have topic in channels
-// 			// do remove it because at the end we are gonna mark
-// 			// channels as deleted which are still in channelNames
-// 			delete(channelNames, topic)
-// 		}
-// 	}
-// 	// flatten the deleted channel names
-// 	for channelName := range channelNames {
-// 		res["deleted"] = append(res["deleted"], channelName)
-// 	}
-
-// 	return res
-// }
-
-// func (f *PopularTopicsController) MessageDeleted(data *models.ChannelMessage) error {
-// 	cml := models.NewChannelMessageList()
-// 	selector := map[string]interface{}{
-// 		"message_id": data.Id,
-// 	}
-
-// 	if err := cml.DeleteMessagesBySelector(selector); err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
 
 func mapMessage(data []byte) (*models.ChannelMessageList, error) {
 	cm := models.NewChannelMessageList()
@@ -255,6 +161,7 @@ func mapMessage(data []byte) (*models.ChannelMessageList, error) {
 }
 
 func (f *PopularTopicsController) isEligible(cml *models.ChannelMessageList) (bool, error) {
+	// return true, nil
 	if cml.ChannelId == 0 {
 		f.log.Notice("ChannelId is not set for Channel Message List id: %d", cml.Id)
 		return false, nil
@@ -282,36 +189,3 @@ func fetchChannel(channelId int64) (*models.Channel, error) {
 
 	return c, nil
 }
-
-// // todo add caching here
-// func fetchTopicChannel(groupName, channelName string) (*models.Channel, error) {
-// 	c := models.NewChannel()
-
-// 	selector := map[string]interface{}{
-// 		"group_name":    groupName,
-// 		"name":          channelName,
-// 		"type_constant": models.Channel_TYPE_TOPIC,
-// 	}
-
-// 	err := c.One(bongo.NewQS(selector))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return c, nil
-// }
-
-// func createTopicChannel(creatorId int64, groupName, channelName, privacy string) (*models.Channel, error) {
-// 	c := models.NewChannel()
-// 	c.Name = channelName
-// 	c.CreatorId = creatorId
-// 	c.GroupName = groupName
-// 	c.Purpose = fmt.Sprintf("Channel for %s topic", channelName)
-// 	c.TypeConstant = models.Channel_TYPE_TOPIC
-// 	c.PrivacyConstant = privacy
-// 	if err := c.Create(); err != nil {
-// 		return nil, err
-// 	}
-
-// 	return c, nil
-// }
