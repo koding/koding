b7be4241796113748041f43f48354307ed7cc2e1
Cihangir SAVAS <cihangir@koding.com>
2014-04-08 23:22:13 -0700

    Social: add schema to the tables

--- go/src/socialapi/scripts/createdb.go
+++ go/src/socialapi/scripts/createdb.go
@@ -27,13 +27,13 @@ func main() {
 
 	helper.MustInitBongo(conf, log)
 	db.DB.LogMode(true)
-	db.DB.Exec("drop table channel_message_list;")
-	db.DB.Exec("drop table channel_message;")
-	db.DB.Exec("drop table message_reply;")
-	db.DB.Exec("drop table channel_participant;")
-	db.DB.Exec("drop table channel;")
-	db.DB.Exec("drop table interaction;")
-	db.DB.Exec("drop table account;")
+	db.DB.Exec("drop table api.channel_message_list;")
+	db.DB.Exec("drop table api.channel_message;")
+	db.DB.Exec("drop table api.message_reply;")
+	db.DB.Exec("drop table api.channel_participant;")
+	db.DB.Exec("drop table api.channel;")
+	db.DB.Exec("drop table api.interaction;")
+	db.DB.Exec("drop table api.account;")
 
 	if err := db.DB.CreateTable(&models.ChannelMessage{}).Error; err != nil {
 		panic(fmt.Sprintf("No error should happen when create table, but got %+v", err))
