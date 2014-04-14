28dbaaf81fa95d2e99f8c61403ad87789adfc1ef
Fatih Arslan <fatih@koding.com>
2014-04-14 18:18:37 +0300

    oskite/create: add progresser for vm.create

--- go/src/koding/oskite/stack.go
+++ go/src/koding/oskite/stack.go
@@ -10,14 +10,41 @@ type Templater interface {
 	Create() error
 }
 
-func vmCreateOld(args *dnode.Partial, c *kite.Channel, vos *virt.VOS) (interface{}, error) {
-	return vmCreate(vos)
+type createParamsOld struct {
+	OnProgress dnode.Callback
 }
 
-func vmCreate(vos *virt.VOS) (interface{}, error) {
+func (c *createParamsOld) Enabled() bool      { return c.OnProgress != nil }
+func (c *createParamsOld) Call(v interface{}) { c.OnProgress(v) }
+
+func vmCreateOld(args *dnode.Partial, c *kite.Channel, vos *virt.VOS) (interface{}, error) {
+	params := new(createParamsOld)
+	if args != nil && args.Unmarshal(&params) != nil {
+		return nil, &kite.ArgumentError{Expected: "{OnProgress: [function]}"}
+	}
+
+	return vmCreate(params, vos)
+}
+
+func vmCreate(params progresser, vos *virt.VOS) (interface{}, error) {
 	if !vos.Permissions.Sudo {
 		return nil, &kite.PermissionError{}
 	}
 
-	return nil, nil
+	return progress(vos, "vm.create "+vos.VM.HostnameAlias, params, func() error {
+		results := make(chan *virt.Step)
+		go prepareProgress(results, vos)
+
+		for step := range results {
+			if params.Enabled() {
+				params.Call(step)
+			}
+
+			if step.Err != nil {
+				return step.Err
+			}
+		}
+
+		return nil
+	})
 }
