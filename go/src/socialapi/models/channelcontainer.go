cf831932e71a0773bd319ef3ae6ae1b5891ecf88
Cihangir SAVAS <cihangir@koding.com>
2014-04-15 15:46:25 +0300

    Social: separate for loop into another function

--- go/src/socialapi/models/channelcontainer.go
+++ go/src/socialapi/models/channelcontainer.go
@@ -12,31 +12,37 @@ func NewChannelContainer() *ChannelContainer {
 	return &ChannelContainer{}
 }
 
-func PopulateChannelContainer(channelList []Channel, accountId int64) []*ChannelContainer {
+func PopulateChannelContainers(channelList []Channel, accountId int64) []*ChannelContainer {
 	channelContainers := make([]*ChannelContainer, len(channelList))
 
 	for i, channel := range channelList {
-		cp := NewChannelParticipant()
-		cp.ChannelId = channel.Id
-
-		// add participantCount
-		participantCount, err := cp.FetchParticipantCount()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// add participation status
-		isParticipated, err := cp.IsParticipated(accountId)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		cc := NewChannelContainer()
-		cc.Channel = channel
-		cc.IsParticipated = isParticipated
-		cc.ParticipantCount = participantCount
-		channelContainers[i] = cc
+		channelContainers[i] = PopulateChannelContainer(channel, accountId)
 	}
 
 	return channelContainers
+}
+
+func PopulateChannelContainer(channel Channel, accountId int64) *ChannelContainer {
+
+	cp := NewChannelParticipant()
+	cp.ChannelId = channel.Id
+
+	// add participantCount
+	participantCount, err := cp.FetchParticipantCount()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// add participation status
+	isParticipated, err := cp.IsParticipated(accountId)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cc := NewChannelContainer()
+	cc.Channel = channel
+	cc.IsParticipated = isParticipated
+	cc.ParticipantCount = participantCount
+
+	return cc
 }
