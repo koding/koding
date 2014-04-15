640ccc3a3b3bc7cd4a9c4738927ded5f08df6dff
Cihangir SAVAS <cihangir@koding.com>
2014-04-16 02:41:21 +0300

    Social: create channel message tests file

---
+++ go/src/socialapi/tests/channel_message_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"math/rand"
+	"socialapi/models"
+	"strconv"
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestChannelMessage(t *testing.T) {
+	Convey("While testing channel messages given a channel", t, func() {
+		groupName := "testgroup" + strconv.FormatInt(rand.Int63(), 10)
+
+		Convey("first we should create the account", func() {
+			account := models.NewAccount()
+			account.OldId = AccountOldId.Hex()
+			account, err := createAccount(account)
+			So(err, ShouldBeNil)
+			So(account, ShouldNotBeNil)
+
+			nonOwnerAccount := models.NewAccount()
+			nonOwnerAccount.OldId = AccountOldId.Hex()
+			nonOwnerAccount, err = createAccount(nonOwnerAccount)
+			So(err, ShouldBeNil)
+			So(nonOwnerAccount, ShouldNotBeNil)
+
+			groupChannel, err := createChannelByGroupNameAndType(account.Id, groupName, models.Channel_TYPE_GROUP)
+			So(err, ShouldBeNil)
+			So(groupChannel, ShouldNotBeNil)
+
+			Convey("message should be able added to the group channel", func() {
+				post, err := createPost(groupChannel.Id, account.Id)
+				So(err, ShouldBeNil)
+				So(post, ShouldNotBeNil)
+				So(post.Id, ShouldNotEqual, 0)
+				So(post.Body, ShouldNotEqual, "")
+				Convey("message can be edited by owner", func() {
+
+					initialPostBody := post.Body
+					post.Body = "edited message"
+
+					editedPost, err := updatePost(post)
+					So(err, ShouldBeNil)
+					So(editedPost, ShouldNotBeNil)
+					// body should not be same
+					So(initialPostBody, ShouldNotEqual, editedPost.Body)
+				})
+			})
+
+			// for now social worker handles this issue
+			Convey("message can be edited by an admin", nil)
+
+			Convey("message can not be edited by non-owner", nil)
+
+			Convey("message  can be deleted by owner", nil)
+
+			Convey("message can be deleted by an admin", nil)
+
+			Convey("message can not be edited by non-owner", nil)
+
+			Convey("owner can like message", nil)
+
+			Convey("non-owner can like message", nil)
+
+			Convey("owner can post reply to message", nil)
+
+			Convey("reply can be liked by reply-owner", nil)
+
+			Convey("reply can be liked", nil)
+
+			Convey("non-owner can post reply to message", nil)
+
+		})
+
+	})
+}
