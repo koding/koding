afaee9897f96feee3bad79424105d3c99938a8a2
Can Yucel <can@koding.com>
2014-04-17 16:51:30 -0700

    Social: Glance method is added to handler

--- go/src/socialapi/workers/api/modules/notification/notification.go
+++ go/src/socialapi/workers/api/modules/notification/notification.go
@@ -10,9 +10,28 @@ import (
 func List(u *url.URL, h http.Header, _ interface{}) (int, http.Header, interface{}, error) {
 	n := models.NewNotification()
 	list, err := n.List(helpers.GetQuery(u))
-
 	if err != nil {
 		return helpers.NewBadRequestResponse(err)
 	}
+
 	return helpers.NewOKResponse(list)
+}
+
+func Glance(u *url.URL, h http.Header, _ interface{}) (int, http.Header, interface{}, error) {
+	n := models.NewNotification()
+	accountId, err := helpers.GetURIInt64(u, "accountId")
+	if err != nil {
+		return helpers.NewBadRequestResponse(err)
+	}
+
+	n.AccountId = accountId
+	if err := n.Glance(); err != nil {
+		return helpers.NewBadRequestResponse(err)
+	}
+
+	res := map[string]interface{}{
+		"Glanced": true,
+	}
+
+	return helpers.NewOKResponse(res)
 }
