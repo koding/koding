feba63349da2ed2962bc8c8bf9a91a5b27ad1a7f
Cihangir SAVAS <cihangir@koding.com>
2014-04-18 00:01:54 +0300

    Social: add tests for topic follow status which are send in popular topics payload

--- go/src/socialapi/tests/popular_topic_test.go
+++ go/src/socialapi/tests/popular_topic_test.go
@@ -14,16 +14,16 @@ import (
 
 func TestPopularTopic(t *testing.T) {
 
+	account := models.NewAccount()
+	account.OldId = AccountOldId.Hex()
+	account, err := createAccount(account)
+
+	rand.Seed(time.Now().UnixNano())
+	groupName := "testgroup" + strconv.FormatInt(rand.Int63(), 10)
+
 	Convey("order should be preserved", t, func() {
-		account := models.NewAccount()
-		account.OldId = AccountOldId.Hex()
-		account, err := createAccount(account)
 		So(err, ShouldBeNil)
 		So(account, ShouldNotBeNil)
-
-		rand.Seed(time.Now().UnixNano())
-
-		groupName := "testgroup" + strconv.FormatInt(rand.Int63(), 10)
 		channel1, err := createChannelByGroupNameAndType(account.Id, groupName, models.Channel_TYPE_GROUP)
 		So(err, ShouldBeNil)
 		So(channel1, ShouldNotBeNil)
@@ -68,7 +68,21 @@ func TestPopularTopic(t *testing.T) {
 		So(popularTopics[2].IsParticipant, ShouldEqual, false)
 		So(popularTopics[2].ParticipantCount, ShouldEqual, 0)
 
+		// check following status
+		So(popularTopics[0].IsParticipant, ShouldBeFalse)
+		So(popularTopics[1].IsParticipant, ShouldBeFalse)
+		So(popularTopics[2].IsParticipant, ShouldBeFalse)
+		// follow the first topic
+		channelParticipant, err := addChannelParticipant(popularTopics[0].Channel.Id, account.Id, account.Id)
+		So(err, ShouldBeNil)
+		So(channelParticipant, ShouldNotBeNil)
+
+		popularTopics, err = fetchPopularTopics(account.Id, groupName)
+		So(err, ShouldBeNil)
+		So(popularTopics, ShouldNotBeNil)
+		So(popularTopics[0].IsParticipant, ShouldBeTrue)
 	})
+
 }
 
 func fetchPopularTopics(accountId int64, groupName string) ([]*models.ChannelContainer, error) {
