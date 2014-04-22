32cfd2fa3fcc5e866819e4478919737c8fd92cd4
Can Yucel <can@koding.com>
2014-04-22 16:17:45 -0700

    Social: group notification handler added with InteractGroup handler

--- go/src/socialapi/workers/api/handlers/handlers.go
+++ go/src/socialapi/workers/api/handlers/handlers.go
@@ -143,6 +143,8 @@ func Inject(mux *tigertonic.TrieServeMux) *tigertonic.TrieServeMux {
 	mux.Handle("POST", "/notification/glance", handlerWrapper(notification.Glance, "notification-glance"))
 	// add account followed notification
 	mux.Handle("POST", "/notification/follow", handlerWrapper(notification.Follow, "notification-follow"))
+	// add group joined/left notification
+	mux.Handle("POST", "/notification/group", handlerWrapper(notification.InteractGroup, "notification-group"))
 
 	// mux.Handle("POST", "/follow/{id}", handlerWrapper(post, "follow-id"))
 	// mux.Handle("POST", "/unfollow/{id}", handlerWrapper(post, "follow-id"))
--- go/src/socialapi/workers/api/modules/notification/notification.go
+++ go/src/socialapi/workers/api/modules/notification/notification.go
@@ -1,7 +1,9 @@
 package notification
 
 import (
+	"errors"
 	"github.com/jinzhu/gorm"
+	"github.com/koding/bongo"
 	"net/http"
 	"net/url"
 	"socialapi/models"
@@ -45,10 +47,45 @@ func Follow(u *url.URL, h http.Header, req *models.Activity) (int, http.Header, 
 
 	return helpers.NewDefaultOKResponse()
 }
+
+type GroupRequest struct {
+	Name         string  `json:"name"`
+	TypeConstant string  `json:"typeConstant"`
+	ActorId      int64   `json:"actorId"`
+	Admins       []int64 `json:"admins"`
 }
 
+func InteractGroup(u *url.URL, h http.Header, req *GroupRequest) (int, http.Header, interface{}, error) {
 
+	// first fetch channel id as target id
+	c := models.NewChannel()
+	selector := map[string]interface{}{
+		"type_constant": models.Channel_TYPE_GROUP,
+		"group_name":    req.Name,
+		"name":          req.Name,
+	}
 
+	if err := c.One(bongo.NewQS(selector)); err != nil {
+		return helpers.NewBadRequestResponse(err)
+	}
+
+	a := models.NewActivity()
+	a.TargetId = c.Id
+	a.ActorId = req.ActorId
+	a.TypeConstant = req.TypeConstant
+
+	var err error
+
+	n := models.NewNotification()
+	switch req.TypeConstant {
+	case models.NotificationContent_TYPE_JOIN:
+		err = n.JoinGroup(a, req.Admins)
+	case models.NotificationContent_TYPE_LEAVE:
+		err = n.LeaveGroup(a, req.Admins)
+	default:
+		err = errors.New("group interaction type not found")
+	}
+	if err != nil {
 		return helpers.NewBadRequestResponse(err)
 	}
 
