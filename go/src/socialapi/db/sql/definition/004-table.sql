91cc086046eb28a424261d2bc54a48c3dcadcbf4
Cihangir SAVAS <cihangir@koding.com>
2014-04-22 14:04:31 +0300

    Social/DB: fix sequence names

--- go/src/socialapi/db/sql/definition/004-table.sql
+++ go/src/socialapi/db/sql/definition/004-table.sql
@@ -5,7 +5,7 @@ SET ROLE social;
 -- ----------------------------
 DROP TABLE IF EXISTS "api"."channel";
 CREATE TABLE "api"."channel" (
-    "id" bigint NOT NULL DEFAULT nextval('channel_id_seq'::regclass),
+    "id" bigint NOT NULL DEFAULT nextval('api.channel_id_seq'::regclass),
     "name" varchar(200) NOT NULL COLLATE "default",
     "creator_id" bigint NOT NULL,
     "group_name" varchar(200) NOT NULL COLLATE "default",
@@ -24,7 +24,7 @@ ALTER TABLE "api"."channel" OWNER TO "socialapplication";
 -- ----------------------------
 DROP TABLE IF EXISTS "api"."account";
 CREATE TABLE "api"."account" (
-    "id" bigint NOT NULL DEFAULT nextval('account_id_seq'::regclass),
+    "id" bigint NOT NULL DEFAULT nextval('api.account_id_seq'::regclass),
     "old_id" varchar(24) NOT NULL COLLATE "default"
 )
 WITH (OIDS=FALSE);
@@ -35,7 +35,7 @@ ALTER TABLE "api"."account" OWNER TO "socialapplication";
 -- ----------------------------
 DROP TABLE IF EXISTS "api"."channel_message";
 CREATE TABLE "api"."channel_message" (
-    "id" bigint NOT NULL DEFAULT nextval('channel_message_id_seq'::regclass),
+    "id" bigint NOT NULL DEFAULT nextval('api.channel_message_id_seq'::regclass),
     "body" text COLLATE "default",
     "slug" varchar(100) NOT NULL COLLATE "default",
     "type_constant" varchar(100) NOT NULL COLLATE "default",
@@ -52,7 +52,7 @@ ALTER TABLE "api"."channel_message" OWNER TO "socialapplication";
 -- ----------------------------
 DROP TABLE IF EXISTS "api"."channel_message_list";
 CREATE TABLE "api"."channel_message_list" (
-    "id" bigint NOT NULL DEFAULT nextval('channel_message_list_id_seq'::regclass),
+    "id" bigint NOT NULL DEFAULT nextval('api.channel_message_list_id_seq'::regclass),
     "channel_id" bigint NOT NULL DEFAULT 0,
     "message_id" bigint NOT NULL DEFAULT 0,
     "added_at" timestamp(6) WITH TIME ZONE NOT NULL DEFAULT now()
@@ -65,7 +65,7 @@ ALTER TABLE "api"."channel_message_list" OWNER TO "socialapplication";
 -- ----------------------------
 DROP TABLE IF EXISTS "api"."channel_participant";
 CREATE TABLE "api"."channel_participant" (
-    "id" bigint NOT NULL DEFAULT nextval('channel_participant_id_seq'::regclass),
+    "id" bigint NOT NULL DEFAULT nextval('api.channel_participant_id_seq'::regclass),
     "channel_id" bigint NOT NULL DEFAULT 0,
     "account_id" bigint NOT NULL DEFAULT 0,
     "status_constant" varchar(100) NOT NULL COLLATE "default",
@@ -81,7 +81,7 @@ ALTER TABLE "api"."channel_participant" OWNER TO "socialapplication";
 -- ----------------------------
 DROP TABLE IF EXISTS "api"."interaction";
 CREATE TABLE "api"."interaction" (
-    "id" bigint NOT NULL DEFAULT nextval('interaction_id_seq'::regclass),
+    "id" bigint NOT NULL DEFAULT nextval('api.interaction_id_seq'::regclass),
     "message_id" bigint NOT NULL DEFAULT 0,
     "account_id" bigint NOT NULL DEFAULT 0,
     "type_constant" varchar(100) NOT NULL COLLATE "default",
@@ -95,7 +95,7 @@ ALTER TABLE "api"."interaction" OWNER TO "socialapplication";
 -- ----------------------------
 DROP TABLE IF EXISTS "api"."message_reply";
 CREATE TABLE "api"."message_reply" (
-    "id" bigint NOT NULL DEFAULT nextval('message_reply_id_seq'::regclass),
+    "id" bigint NOT NULL DEFAULT nextval('api.message_reply_id_seq'::regclass),
     "message_id" bigint NOT NULL DEFAULT 0,
     "reply_id" bigint NOT NULL DEFAULT 0,
     "created_at" timestamp(6) WITH TIME ZONE NOT NULL DEFAULT now()
