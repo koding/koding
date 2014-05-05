3d80b5c790678cc7c1d8ff3d2735d85420707792
Can Yucel <can@koding.com>
2014-05-05 18:51:52 +0300

    Social: Logger is added to modelHelper for injecting worker loggers

--- go/src/socialapi/models/modelhelper.go
+++ go/src/socialapi/models/modelhelper.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"github.com/koding/logging"
 	"math/rand"
 	"strconv"
 	"time"
@@ -11,6 +12,8 @@ import (
 	"github.com/koding/bongo"
 	"github.com/stvp/slug"
 )
+
+var Log logging.Logger
 
 // todo fix slug generation with the slugifiable.coffee implementation
 func Slugify(message *ChannelMessage) (*ChannelMessage, error) {
