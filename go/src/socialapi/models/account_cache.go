2729e80301d1255c15673ae2ad2148ab65bf5071
Cihangir SAVAS <cihangir@koding.com>
2014-05-08 17:53:11 +0300

    Social: replace api ids with old account ids

--- go/src/socialapi/models/account_cache.go
+++ go/src/socialapi/models/account_cache.go
@@ -21,3 +21,20 @@ func AccountOldIdById(id int64) (string, error) {
 	accountCache[id] = oldId
 	return oldId, nil
 }
+
+func AccountOldsIdByIds(ids []int64) ([]string, error) {
+	oldIds := make([]string, len(ids))
+	if len(oldIds) == 0 {
+		return oldIds, nil
+	}
+
+	for i, id := range ids {
+		oldId, err := AccountOldIdById(id)
+		if err != nil {
+			return oldIds, err
+		}
+		oldIds[i] = oldId
+	}
+
+	return oldIds, nil
+}
