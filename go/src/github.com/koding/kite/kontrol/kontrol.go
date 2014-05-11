d5e0b4334e64d4e05fdb29af8ece55ba40052a38
Fatih Arslan <fatih@koding.com>
2014-05-11 09:16:19 +0300

    kites: regserv is merged into kontrol

--- go/src/github.com/koding/kite/kontrol/kontrol.go
+++ go/src/github.com/koding/kite/kontrol/kontrol.go
@@ -20,6 +20,7 @@ import (
 	"github.com/koding/kite"
 	"github.com/koding/kite/config"
 	"github.com/koding/kite/dnode"
+	"github.com/koding/kite/kitekey"
 	"github.com/koding/kite/protocol"
 	"github.com/nu7hatch/gouuid"
 )
@@ -52,6 +53,12 @@ type Kontrol struct {
 	PeerAddr     string   // The public host:port used for peer communication.
 	PeerBindAddr string   // The listening host:port used for peer communication.
 	Peers        []string // other peers in cluster (must be peer address of other instances)
+
+	// MachineAuthenticate is used to authenticate the request in the
+	// "handleMachine" method.  The reason for a separate auth function is, the
+	// request must not be authenticated because clients do not have a kite.key
+	// before they register to this machine.
+	MachineAuthenticate func(r *kite.Request) error
 
 	// RSA keys
 	publicKey  string // for validating tokens
@@ -108,6 +115,7 @@ func New(conf *config.Config, version, publicKey, privateKey string) *Kontrol {
 	log = k.Log
 
 	k.HandleFunc("register", kontrol.handleRegister)
+	k.HandleFunc("registerMachine", kontrol.handleMachine).DisableAuthentication()
 	k.HandleFunc("getKites", kontrol.handleGetKites)
 	k.HandleFunc("getToken", kontrol.handleGetToken)
 	k.HandleFunc("cancelWatcher", kontrol.handleCancelWatcher)
@@ -163,6 +171,49 @@ func (k *Kontrol) ClearKites() error {
 		return fmt.Errorf("Cannot clear etcd: %s", err)
 	}
 	return nil
+}
+
+// RegisterSelf registers this host and writes a key to ~/.kite/kite.key
+func (k *Kontrol) RegisterSelf() error {
+	key, err := k.registerUser(k.Kite.Config.Username)
+	if err != nil {
+		return err
+	}
+	return kitekey.Write(key)
+}
+
+func (k *Kontrol) handleMachine(r *kite.Request) (interface{}, error) {
+	if k.MachineAuthenticate != nil {
+		if err := k.MachineAuthenticate(r); err != nil {
+			return nil, errors.New("cannot authenticate user")
+		}
+	}
+
+	username := r.Args.One().MustString() // username should be send as an argument
+	return k.registerUser(username)
+}
+
+func (k *Kontrol) registerUser(username string) (kiteKey string, err error) {
+	// Only accept requests of type machine
+	tknID, err := uuid.NewV4()
+	if err != nil {
+		return "", errors.New("cannot generate a token")
+	}
+
+	token := jwt.New(jwt.GetSigningMethod("RS256"))
+
+	token.Claims = map[string]interface{}{
+		"iss":        k.Kite.Kite().Username,            // Issuer
+		"sub":        username,                          // Subject
+		"iat":        time.Now().UTC().Unix(),           // Issued At
+		"jti":        tknID.String(),                    // JWT ID
+		"kontrolURL": k.Kite.Config.KontrolURL.String(), // Kontrol URL
+		"kontrolKey": strings.TrimSpace(k.publicKey),    // Public key of kontrol
+	}
+
+	k.Kite.Log.Info("Registered machine on user: %s", username)
+
+	return token.SignedString([]byte(k.privateKey))
 }
 
 // registerValue is the type of the value that is saved to etcd.
