285dbf87ad28382b5510d37891dd7b0a07e2daf3
Can Yucel <can@koding.com>
2014-05-13 18:34:57 +0300

    Social: Notification related sql scripts are moved

--- go/src/socialapi/db/sql/definition/003-sequence.sql
+++ go/src/socialapi/db/sql/definition/003-sequence.sql
@@ -54,27 +54,3 @@ DROP SEQUENCE IF EXISTS "api"."message_reply_id_seq";
 CREATE SEQUENCE "api"."message_reply_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
 -- ALTER TABLE "api"."message_reply_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."message_reply_id_seq" TO "socialapplication";
-
--- ----------------------------
---  Sequence structure for notification_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "api"."notification_id_seq";
-CREATE SEQUENCE "api"."notification_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."notification_id_seq" OWNER TO "socialapplication";
-GRANT USAGE ON SEQUENCE "api"."notification_id_seq" TO "socialapplication";
-
--- ----------------------------
---  Sequence structure for notification_content_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "api"."notification_content_id_seq";
-CREATE SEQUENCE "api"."notification_content_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."notification_content_id_seq" OWNER TO "socialapplication";
-GRANT USAGE ON SEQUENCE "api"."notification_content_id_seq" TO "socialapplication";
-
--- ----------------------------
---  Sequence structure for activity_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "api"."notification_activity_id_seq";
-CREATE SEQUENCE "api"."notification_activity_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."notification_activity_id_seq" OWNER TO "socialapplication";
-GRANT USAGE ON SEQUENCE "api"."notification_activity_id_seq" TO "socialapplication";
