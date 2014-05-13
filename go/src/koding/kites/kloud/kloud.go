effc603730daa6b8f78ef62512a9bb339457420f
Fatih Arslan <fatih@koding.com>
2014-05-13 16:29:39 +0300

    kloud: first commit

---
+++ go/src/koding/kites/kloud/kloud.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"koding/kodingkite"
+	"koding/tools/config"
+	"log"
+	"os"
+)
+
+const (
+	VERSION = "0.0.1"
+	NAME    = "kloud"
+)
+
+var (
+	flagIP      = flag.String("ip", "", "Change public ip")
+	flagPort    = flag.Int("port", 3000, "Change running port")
+	flagVersion = flag.Bool("version", false, "Show version and exit")
+	flagRegion  = flag.String("r", "", "Change region")
+	flagProfile = flag.String("c", "", "Configuration profile from file")
+)
+
+func main() {
+	flag.Parse()
+	if *flagProfile == "" || *flagRegion == "" {
+		log.Fatal("Please specify profile via -c and region via -r. Aborting.")
+	}
+
+	if *flagVersion {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
+	k, err := kodingkite.New(config.MustConfig(*flagProfile), NAME, VERSION)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	k.Config.Region = *flagRegion
+	k.Config.Port = *flagPort
+
+	k.HandleFunc("build", nil)
+	k.HandleFunc("start", nil)
+	k.HandleFunc("stop", nil)
+	k.HandleFunc("destroy", nil)
+	k.HandleFunc("info", nil)
+
+	k.Run()
+}
