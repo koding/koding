119913dee9a914d0ed8b601b49b8204065f2f159
Can Yucel <can@koding.com>
2014-05-14 12:21:58 +0300

    Social: handlerwrapper code is taken into koding/api package

--- go/src/socialapi/workers/notification/api/handlers.go
+++ go/src/socialapi/workers/notification/api/handlers.go
@@ -1,36 +1,22 @@
 package api
 
 import (
+	"github.com/koding/api"
 	"github.com/rcrowley/go-tigertonic"
-	"net/http"
 )
 
-// todo think for a different method here
-// also there must be an independent handler initializer
-var (
-	cors = tigertonic.NewCORSBuilder().AddAllowedOrigins("*")
-)
-
-func handlerWrapper(handler interface{}, logName string) http.Handler {
-	return cors.Build(
-		tigertonic.Timed(
-			tigertonic.Marshaled(handler),
-			logName,
-			nil,
-		))
-}
-
+// TODO wrap this TrieServeMux
 func InitHandlers(mux *tigertonic.TrieServeMux) *tigertonic.TrieServeMux {
 	// list notifications
-	mux.Handle("GET", "/notification/{accountId}", handlerWrapper(List, "notification-list"))
+	mux.Handle("GET", "/notification/{accountId}", api.HandlerWrapper(List, "notification-list"))
 	// glance notifications
-	mux.Handle("POST", "/notification/glance", handlerWrapper(Glance, "notification-glance"))
+	mux.Handle("POST", "/notification/glance", api.HandlerWrapper(Glance, "notification-glance"))
 	// add account followed notification
-	mux.Handle("POST", "/notification/follow", handlerWrapper(Follow, "notification-follow"))
+	mux.Handle("POST", "/notification/follow", api.HandlerWrapper(Follow, "notification-follow"))
 	// subscribe to message notification
-	mux.Handle("POST", "/notification/subscribe", handlerWrapper(SubscribeMessage, "notification-subscribe"))
+	mux.Handle("POST", "/notification/subscribe", api.HandlerWrapper(SubscribeMessage, "notification-subscribe"))
 	// unsubscribe from message notification
-	mux.Handle("POST", "/notification/unsubscribe", handlerWrapper(UnsubscribeMessage, "notification-unsubscribe"))
+	mux.Handle("POST", "/notification/unsubscribe", api.HandlerWrapper(UnsubscribeMessage, "notification-unsubscribe"))
 
 	return mux
 }
