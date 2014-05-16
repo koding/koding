a8f6a02894438c2716a2cf8cd4c06ae2fe351c22
Cihangir SAVAS <cihangir@koding.com>
2014-05-16 16:36:43 -0700

    Social: Send channel message container instead of channel message

--- go/src/socialapi/workers/realtime/realtime/realtime.go
+++ go/src/socialapi/workers/realtime/realtime/realtime.go
@@ -293,7 +293,12 @@ func (f *RealtimeWorkerController) sendChannelEvent(cml *models.ChannelMessageLi
 		return err
 	}
 
-	byteMessage, err := json.Marshal(cm)
+	cmc, err := cm.BuildEmptyMessageContainer()
+	if err != nil {
+		return err
+	}
+
+	byteMessage, err := json.Marshal(cmc)
 	if err != nil {
 		return err
 	}
