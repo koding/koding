372695d5de69d2ca90ca234580667be93e68cde5
Cihangir SAVAS <cihangir@koding.com>
2014-05-18 19:53:47 -0700

    Social: remove commented out lines

--- go/src/socialapi/db/sql/definition/003-sequence.sql
+++ go/src/socialapi/db/sql/definition/003-sequence.sql
@@ -4,7 +4,6 @@ SET ROLE social;
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "api"."account_id_seq";
 CREATE SEQUENCE "api"."account_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."account_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."account_id_seq" TO "socialapplication";
 
 -- ----------------------------
@@ -12,7 +11,6 @@ GRANT USAGE ON SEQUENCE "api"."account_id_seq" TO "socialapplication";
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "api"."channel_id_seq";
 CREATE SEQUENCE "api"."channel_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."channel_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."channel_id_seq" TO "socialapplication";
 
 -- ----------------------------
@@ -20,7 +18,6 @@ GRANT USAGE ON SEQUENCE "api"."channel_id_seq" TO "socialapplication";
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "api"."channel_message_id_seq";
 CREATE SEQUENCE "api"."channel_message_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."channel_message_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."channel_message_id_seq" TO "socialapplication";
 
 -- ----------------------------
@@ -28,7 +25,6 @@ GRANT USAGE ON SEQUENCE "api"."channel_message_id_seq" TO "socialapplication";
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "api"."channel_message_list_id_seq";
 CREATE SEQUENCE "api"."channel_message_list_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."channel_message_list_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."channel_message_list_id_seq" TO "socialapplication";
 
 -- ----------------------------
@@ -36,7 +32,6 @@ GRANT USAGE ON SEQUENCE "api"."channel_message_list_id_seq" TO "socialapplicatio
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "api"."channel_participant_id_seq";
 CREATE SEQUENCE "api"."channel_participant_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."channel_participant_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."channel_participant_id_seq" TO "socialapplication";
 
 -- ----------------------------
@@ -44,7 +39,6 @@ GRANT USAGE ON SEQUENCE "api"."channel_participant_id_seq" TO "socialapplication
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "api"."interaction_id_seq";
 CREATE SEQUENCE "api"."interaction_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."interaction_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."interaction_id_seq" TO "socialapplication";
 
 -- ----------------------------
@@ -52,5 +46,4 @@ GRANT USAGE ON SEQUENCE "api"."interaction_id_seq" TO "socialapplication";
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "api"."message_reply_id_seq";
 CREATE SEQUENCE "api"."message_reply_id_seq" INCREMENT 1 START 1 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
--- ALTER TABLE "api"."message_reply_id_seq" OWNER TO "socialapplication";
 GRANT USAGE ON SEQUENCE "api"."message_reply_id_seq" TO "socialapplication";
