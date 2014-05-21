54f5fd8024f457be7189aedc9faa8db7ac24e01a
Can Yucel <can@koding.com>
2014-05-21 18:20:55 +0300

    Notification: unsubscription token generator is added

---
+++ go/src/socialapi/workers/emailnotifier/controller/tokengenerator.go
@@ -0,0 +1,33 @@
+package emailnotifier
+
+import (
+	"fmt"
+	mongomodels "koding/db/models"
+	"koding/db/mongodb/modelhelper"
+	"socialapi/workers/notification/models"
+
+	"github.com/nu7hatch/gouuid"
+)
+
+func createToken(uc *UserContact, nc *models.NotificationContent, token string) error {
+	mt := &mongomodels.NotificationMailToken{
+		Recipient:        uc.UserOldId,
+		NotificationType: emailConfig[nc.TypeConstant],
+		UnsubscribeId:    token,
+	}
+
+	return modelhelper.CreateMailToken(mt)
+}
+
+func generateToken() (string, error) {
+	uuid1, err := uuid.NewV4()
+	if err != nil {
+		return "", err
+	}
+	uuid2, err := uuid.NewV4()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s%s", uuid1, uuid2), nil
+}
