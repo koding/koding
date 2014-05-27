5b7cb7bf1629d0780c2672a8b435e98db8d0bf46
Cihangir SAVAS <cihangir@koding.com>
2014-05-26 18:12:07 -0700

    Social: Social: add private message rest package

---
+++ go/src/socialapi/rest/privatemessage.go
@@ -0,0 +1,45 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"socialapi/models"
+)
+
+func SendPrivateMessage(senderId int64, body string, groupName string) (*models.ChannelContainer, error) {
+
+	pmr := models.PrivateMessageRequest{}
+	pmr.AccountId = senderId
+	pmr.Body = body
+	pmr.GroupName = groupName
+
+	url := "/privatemessage/send"
+	res, err := marshallAndSendRequest("POST", url, pmr)
+	if err != nil {
+		return nil, err
+	}
+
+	model := models.NewChannelContainer()
+	err = json.Unmarshal(res, model)
+	if err != nil {
+		return nil, err
+	}
+
+	return model, nil
+}
+
+func GetPrivateMessages(accountId int64, groupName string) ([]models.ChannelContainer, error) {
+	url := fmt.Sprintf("/privatemessage/list?accountId=%d&groupName=%s", accountId, groupName)
+	res, err := sendRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var privateMessages []models.ChannelContainer
+	err = json.Unmarshal(res, &privateMessages)
+	if err != nil {
+		return nil, err
+	}
+
+	return privateMessages, nil
+}
