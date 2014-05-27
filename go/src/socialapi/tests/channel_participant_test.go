af2243339d4f4b423e31ab2e38e8759e3d5af67e
Cihangir SAVAS <cihangir@koding.com>
2014-05-26 18:12:51 -0700

    Social: refactor tests to use rest package

--- go/src/socialapi/tests/channel_participant_test.go
+++ go/src/socialapi/tests/channel_participant_test.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"socialapi/models"
 	"testing"
-	"labix.org/v2/mgo/bson"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -30,51 +26,4 @@ func TestChannelParticipantOperations(t *testing.T) {
 		})
 
 	})
-}
-
-func createChannelParticipant(channelId int64) (*models.ChannelParticipant, error) {
-	account := models.NewAccount()
-	account.OldId = bson.NewObjectId().Hex()
-	account, _ = createAccount(account)
-	return addChannelParticipant(channelId, account.Id, account.Id)
-}
-
-func addChannelParticipant(channelId, requesterId, accountId int64) (*models.ChannelParticipant, error) {
-	c := models.NewChannelParticipant()
-	c.AccountId = requesterId
-
-	url := fmt.Sprintf("/channel/%d/participant/%d/add", channelId, accountId)
-	cmI, err := sendModel("POST", url, c)
-	if err != nil {
-		return nil, err
-	}
-	return cmI.(*models.ChannelParticipant), nil
-}
-
-func listChannelParticipants(channelId, accountId int64) ([]*models.ChannelParticipant, error) {
-
-	url := fmt.Sprintf("/channel/%d/participant?accountId=%d", channelId, accountId)
-	res, err := sendRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	var participants []*models.ChannelParticipant
-	err = json.Unmarshal(res, &participants)
-	if err != nil {
-		return nil, err
-	}
-
-	return participants, nil
-}
-
-func deleteChannelParticipant(channelId int64, requesterId, accountId int64) (*models.ChannelParticipant, error) {
-	c := models.NewChannelParticipant()
-	c.AccountId = requesterId
-
-	url := fmt.Sprintf("/channel/%d/participant/%d/delete", channelId, accountId)
-	cmI, err := sendModel("POST", url, c)
-	if err != nil {
-		return nil, err
-	}
-	return cmI.(*models.ChannelParticipant), nil
 }
--- go/src/socialapi/tests/history_test.go
+++ go/src/socialapi/tests/history_test.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"socialapi/models"
+	"socialapi/rest"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -16,25 +15,25 @@ func TestChannelHistory(t *testing.T) {
 		Convey("We should be able to create it(channel) first", func() {
 			account := models.NewAccount()
 			account.OldId = AccountOldId.Hex()
-			account, err = createAccount(account)
+			account, err = rest.CreateAccount(account)
 			So(err, ShouldBeNil)
 			So(account, ShouldNotBeNil)
 
-			channel, err = createChannel(account.Id)
+			channel, err = rest.CreateChannel(account.Id)
 			So(err, ShouldBeNil)
 			So(channel, ShouldNotBeNil)
 			Convey("While posting a new message to it", func() {
 				var channelParticipant *models.ChannelParticipant
 				var err error
 				Convey("We should be able to create a participant first", func() {
-					channelParticipant, err = createChannelParticipant(channel.Id)
+					channelParticipant, err = rest.CreateChannelParticipant(channel.Id)
 					So(err, ShouldBeNil)
 					So(channelParticipant, ShouldNotBeNil)
 
 					Convey("Create posts with created participant", func() {
 						channel := channel
 						for i := 0; i < 10; i++ {
-							post, err := createPost(channel.Id, channelParticipant.AccountId)
+							post, err := rest.CreatePost(channel.Id, channelParticipant.AccountId)
 							So(err, ShouldBeNil)
 							So(post, ShouldNotBeNil)
 							So(post.Id, ShouldNotEqual, 0)
@@ -42,7 +41,7 @@ func TestChannelHistory(t *testing.T) {
 
 						}
 						Convey("We should be able to fetch the history", func() {
-							history, err := getHistory(channel.Id, channelParticipant.Id)
+							history, err := rest.GetHistory(channel.Id, channelParticipant.Id)
 							So(err, ShouldBeNil)
 							So(history, ShouldNotBeNil)
 							So(len(history.MessageList), ShouldEqual, 10)
@@ -52,20 +51,4 @@ func TestChannelHistory(t *testing.T) {
 			})
 		})
 	})
-}
-
-func getHistory(channelId, accountId int64) (*models.HistoryResponse, error) {
-	url := fmt.Sprintf("/channel/%d/history?accountId=%d", channelId, accountId)
-	res, err := sendRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	var history models.HistoryResponse
-	err = json.Unmarshal(res, &history)
-	if err != nil {
-		return nil, err
-	}
-
-	return &history, nil
 }
