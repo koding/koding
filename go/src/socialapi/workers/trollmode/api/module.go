a0b95ed1458bdc3f6578c7cbdf523ad75aa63994
Cihangir SAVAS <cihangir@koding.com>
2014-05-26 18:15:07 -0700

    Social: do not use empty interface for post request

--- go/src/socialapi/workers/trollmode/api/module.go
+++ go/src/socialapi/workers/trollmode/api/module.go
@@ -7,7 +7,7 @@ import (
 	"socialapi/workers/api/modules/helpers"
 )
 
-func Mark(u *url.URL, h http.Header, _ interface{}) (int, http.Header, interface{}, error) {
+func Mark(u *url.URL, h http.Header, req map[string]interface{}) (int, http.Header, interface{}, error) {
 	targetId, err := helpers.GetURIInt64(u, "accountId")
 	if err != nil {
 		return helpers.NewBadRequestResponse(err)
