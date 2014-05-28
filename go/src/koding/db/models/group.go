ab5dc63cb29e48e27315affcff6e20c03bb09712
Can Yucel <can@koding.com>
2014-05-28 19:09:06 +0300

    Social: Privacy field added to Group Mongo models

--- go/src/koding/db/models/group.go
+++ go/src/koding/db/models/group.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Group struct {
-	Id    bson.ObjectId `bson:"_id" json:"-"`
-	Body  string        `bson:"body" json:"body"`
-	Title string        `bson:"title" json:"title"`
-	Slug  string        `bson:"slug" json:"slug"`
+	Id      bson.ObjectId `bson:"_id" json:"-"`
+	Body    string        `bson:"body" json:"body"`
+	Title   string        `bson:"title" json:"title"`
+	Slug    string        `bson:"slug" json:"slug"`
+	Privacy string        `bson:"privacy" json:"privacy"`
 }
