3832d7aa92957103d8a0c213635c2768e13b7330
Can Yucel <can@koding.com>
2014-06-03 12:11:09 +0300

    Social: create hstore extension for api schema

--- go/src/socialapi/db/sql/definition/002-schema.sql
+++ go/src/socialapi/db/sql/definition/002-schema.sql
@@ -2,6 +2,10 @@ SET ROLE social;
 
 CREATE SCHEMA api;
 
+SET ROLE postgres;
+CREATE EXTENSION hstore SCHEMA api;
+SET ROLE social;
+
 GRANT usage ON SCHEMA api to socialapplication;
 
-ALTER DATABASE social set search_path="$user", public, api
+ALTER DATABASE social set search_path="$user", public, api;
