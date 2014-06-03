80f87b989be32b805fd0104097d772d73024314a
Fatih Arslan <fatih@koding.com>
2014-06-02 18:12:45 -0700

    klient: use a protocol package for kite properties

---
+++ protocol/protocol.go
@@ -0,0 +1,11 @@
+// Protocol is a package that klient and several other packages uses. It
+// contains the information which is passed to the kite library during booting.
+// They are a part of the KiteQuert.
+package protocol
+
+const (
+	Version     = "0.0.1"
+	Name        = "klient"
+	Environment = "public-host"
+	Region      = "public-region"
+)
