72e89a192edcd668e6b49c09597c543c9e87cb8a
Can Yucel <can@koding.com>
2014-06-09 12:31:06 -0700

    Social: add comment for hstore extension creation

--- go/src/socialapi/db/sql/definition/002-schema.sql
+++ go/src/socialapi/db/sql/definition/002-schema.sql
@@ -2,6 +2,8 @@ SET ROLE social;
 
 CREATE SCHEMA api;
 
+-- since extension creation is handled in current database,
+-- hstore is created here
 SET ROLE postgres;
 CREATE EXTENSION IF NOT EXISTS hstore;
 SET ROLE social;
