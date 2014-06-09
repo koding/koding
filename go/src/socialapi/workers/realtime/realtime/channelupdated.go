dfe5ef9efdcb10f2a24d602c09417c838be2d2ed
Cihangir SAVAS <cihangir@koding.com>
2014-06-09 14:36:37 -0700

    Social: add documentation

--- go/src/socialapi/workers/realtime/realtime/channelupdated.go
+++ go/src/socialapi/workers/realtime/realtime/channelupdated.go
@@ -18,33 +18,45 @@ var (
 )
 
 type channelUpdatedEvent struct {
-	Channel            *models.Channel            `json:"channel"`
-	ChannelMessage     *models.ChannelMessage     `json:"channelMessage"`
-	EventType          ChannelUpdatedEventType    `json:"eventType"`
-	ChannelParticipant *models.ChannelParticipant `json:"-"`
-	UnreadCount        int                        `json:"unreadCount"`
+	Channel              *models.Channel            `json:"channel"`
+	ParentChannelMessage *models.ChannelMessage     `json:"channelMessage"`
+	ReplyChannelMessage  *models.ChannelMessage     `json:"-"`
+	EventType            ChannelUpdatedEventType    `json:"eventType"`
+	ChannelParticipant   *models.ChannelParticipant `json:"-"`
+	UnreadCount          int                        `json:"unreadCount"`
 }
 
+// sendChannelUpdatedEvent sends channel updated events
 func (f *Controller) sendChannelUpdatedEvent(cue *channelUpdatedEvent) error {
 	f.log.Debug("sending channel update event %+v", cue)
 
-	if err := f.filterChannelUpdatedEvents(cue); err != nil {
+	if err := f.validateChannelUpdatedEvents(cue); err != nil {
 		f.log.Error(err.Error())
 		return err
 	}
 
+	// fetch all participants of related channel
+	// if you ask why we are not sending those messaages to the channel's channel
+	// instead of sending events as notifications?, because we are also sending
+	// unread counts of the related channel's messages by the notifiee
 	participants, err := cue.Channel.FetchParticipantIds()
 	if err != nil {
 		f.log.Error("Error occured while fetching participants %s", err.Error())
 		return err
 	}
 
+	// if
 	if len(participants) == 0 {
-		f.log.Info("Participant count is %d, skipping", len(participants))
+		f.log.Notice("This channel (%d) doesnt have any participant but we are trying to send an event to it, please investigate", cue.Channel.Id)
 		return nil
 	}
 
 	for _, accountId := range participants {
+		if !f.isEligibleForBroadcasting(cue, accountId) {
+			f.log.Debug("not sending event to the creator of this operation %s", cue.EventType)
+			continue
+		}
+
 		cp := models.NewChannelParticipant()
 		cp.ChannelId = cue.Channel.Id
 		cp.AccountId = accountId
@@ -60,32 +72,65 @@ func (f *Controller) sendChannelUpdatedEvent(cue *channelUpdatedEvent) error {
 	return nil
 }
 
-func (f *Controller) filterChannelUpdatedEvents(cue *channelUpdatedEvent) error {
+func (f *Controller) isEligibleForBroadcasting(cue *channelUpdatedEvent, accountId int64) bool {
+	// if parent message is empty do send
+	// realtime  updates to the client
+	if cue.ParentChannelMessage == nil {
+		return true
+	}
+
+	// if reply is not set do send this event
+	if cue.ReplyChannelMessage == nil {
+		return true
+	}
+
+	// if parent mesasge's crateor is account
+	// dont send it
+	if cue.ParentChannelMessage.AccountId == accountId {
+		return false
+	}
+
+	// if reply mesasge's crateor is account
+	// dont send it
+	if cue.ReplyChannelMessage.AccountId == accountId {
+		return false
+	}
+
+	return true
+}
+
+func (f *Controller) validateChannelUpdatedEvents(cue *channelUpdatedEvent) error {
+	// channel shouldnt be nil
 	if cue.Channel == nil {
 		return fmt.Errorf("Channel is nil")
 	}
 
+	// channel id should be set inorder to send event to the channel
 	if cue.Channel.Id == 0 {
 		return fmt.Errorf("Channel id is not set")
 	}
 
+	// filter group events
 	// do not send any -updated- event to group channels
 	if cue.Channel.TypeConstant == models.Channel_TYPE_GROUP {
 		return fmt.Errorf("Not sending group (%s) event", cue.Channel.GroupName)
 	}
 
 	// do not send comment events to topic channels
+	// other than topic channel, channels persist their messages as replies
 	if cue.Channel.TypeConstant != models.Channel_TYPE_TOPIC {
 		return nil
 	}
 
-	if cue.ChannelMessage == nil {
+	// if we dont have a parent message it means this is a post addition/creation
+	if cue.ParentChannelMessage == nil {
 		return nil
 	}
 
-	if cue.ChannelMessage.TypeConstant != models.ChannelMessage_TYPE_POST {
+	// send only post operations the the client
+	if cue.ParentChannelMessage.TypeConstant != models.ChannelMessage_TYPE_POST {
 		return fmt.Errorf("Not sending non-post (%s) event to topic channel",
-			cue.ChannelMessage.TypeConstant,
+			cue.ParentChannelMessage.TypeConstant,
 		)
 	}
 
@@ -110,19 +155,19 @@ func (f *Controller) sendChannelUpdatedEventToParticipant(cue *channelUpdatedEve
 }
 
 func (f *Controller) calculateUnreadItemCount(cue *channelUpdatedEvent) (int, error) {
-	if cue.ChannelMessage == nil {
+	if cue.ParentChannelMessage == nil {
 		return models.NewChannelMessageList().UnreadCount(cue.ChannelParticipant)
 	}
 
 	if cue.Channel.TypeConstant == models.Channel_TYPE_PRIVATE_MESSAGE {
-		return models.NewMessageReply().UnreadCount(cue.ChannelMessage.Id, cue.ChannelParticipant.LastSeenAt)
+		return models.NewMessageReply().UnreadCount(cue.ParentChannelMessage.Id, cue.ChannelParticipant.LastSeenAt)
 	}
 
 	if cue.Channel.TypeConstant == models.Channel_TYPE_TOPIC {
 		return models.NewChannelMessageList().UnreadCount(cue.ChannelParticipant)
 	}
 
-	cml, err := cue.Channel.FetchMessageList(cue.ChannelMessage.Id)
+	cml, err := cue.Channel.FetchMessageList(cue.ParentChannelMessage.Id)
 	if err == nil {
 		return models.NewMessageReply().UnreadCount(cml.MessageId, cml.AddedAt)
 	}
