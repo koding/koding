6c02944c56228ef8559fc1a6d2a91422f23a0ce3
Geraint Jones <geraint@koding.com>
2014-06-10 14:22:13 -0700

    Got my logic backwards

--- go/src/koding/oskite/vm.go
+++ go/src/koding/oskite/vm.go
@@ -461,7 +461,7 @@ func vmStopAndUnprepare(args *dnode.Partial, channel *kite.Channel, vos *virt.VO
 }
 
 func unprepareProgress(t tracer.Tracer, vm *virt.VM, destroy bool) error {
-	if err = vm.Unprepare(t, destroy); err != nil {
+	if err = vm.Unprepare(t, destroy); err == nil {
 
 		if err = mongodbConn.Run("jVMs", func(c *mgo.Collection) error {
 			return c.Update(bson.M{"_id": vm.Id}, bson.M{"$set": bson.M{"hostKite": nil}})
