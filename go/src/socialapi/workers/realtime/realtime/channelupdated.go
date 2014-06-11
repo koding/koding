bab96ef2cc5d2c6665f45957e678b935cd4fb9ec
Cihangir SAVAS <cihangir@koding.com>
2014-06-10 17:37:32 -0700

    Social: comment-out check statement

--- go/src/socialapi/workers/realtime/realtime/channelupdated.go
+++ go/src/socialapi/workers/realtime/realtime/channelupdated.go
@@ -87,9 +87,12 @@ func (f *Controller) isEligibleForBroadcasting(cue *channelUpdatedEvent, account
 
 	// if parent message's crateor is account
 	// dont send it
-	if cue.ParentChannelMessage.AccountId == accountId {
-		return false
-	}
+	// this has introduced some bugs to system, like if someone
+	// comments to my post(i also pinned it)
+	// i wasnt getting any notification
+	// if cue.ParentChannelMessage.AccountId == accountId {
+	// 	return false
+	// }
 
 	// if reply message's crateor is account
 	// dont send it
