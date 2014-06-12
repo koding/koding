c87daa29cd09dd2ca6d34089aa40df4e30acccf2
Can Yucel <can@koding.com>
2014-06-12 11:03:26 -0700

    Sitemap: Add main package for sitemapgenerator worker

---
+++ go/src/socialapi/workers/sitemap/sitemapgenerator/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"socialapi/workers/common/runner"
+	"socialapi/workers/helper"
+
+	"socialapi/workers/sitemap/sitemapgenerator/generator"
+)
+
+var (
+	Name = "SitemapGenerator"
+)
+
+func main() {
+	r := runner.New(Name)
+	if err := r.Init(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	redisConn := helper.MustInitRedisConn(r.Conf.Redis)
+	defer redisConn.Close()
+
+	handler, err := generator.New(r.Log)
+	if err != nil {
+		r.Log.Error("Could not create sitemap generator: %s", err)
+	}
+
+	r.ShutdownHandler = handler.Shutdown
+	r.Wait()
+}
