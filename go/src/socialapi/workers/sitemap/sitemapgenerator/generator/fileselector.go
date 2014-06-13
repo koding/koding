929b354b827649618bfe6d32b6bbb9dd785f7e1a
Can Yucel <can@koding.com>
2014-06-13 11:56:07 -0700

    Sitemap: Add CachedFileSelector for choosing sitemap file name

--- go/src/socialapi/workers/sitemap/sitemapgenerator/generator/fileselector.go
+++ go/src/socialapi/workers/sitemap/sitemapgenerator/generator/fileselector.go
@@ -1,11 +1,32 @@
 package generator
 
+import (
+	"socialapi/workers/helper"
+	"socialapi/workers/sitemap"
+
+	"github.com/garyburd/redigo/redis"
+)
+
 type FileSelector interface {
-	Select() string
+	Select() (string, error)
 }
 
 type SimpleFileSelector struct{}
 
-func (s SimpleFileSelector) Select() string {
-	return "firstfile.xml"
+func (s SimpleFileSelector) Select() (string, error) {
+	return "sitemap", nil
+}
+
+type CachedFileSelector struct{}
+
+func (s CachedFileSelector) Select() (string, error) {
+	redisConn := helper.MustGetRedisConn()
+
+	item, err := redisConn.PopSetMember(sitemap.PrepareFileNameCacheKey())
+
+	if err != nil && err != redis.ErrNil {
+		return "", err
+	}
+
+	return item, err
 }
