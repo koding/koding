e70504deeb0175607bd8f883f09e01a4fde7dadb
Fatih Arslan <fatih@koding.com>
2014-06-16 14:18:57 -0700

    kloud/build: fix saving droplet name

--- go/src/koding/kites/kloud/provider/digitalocean/client.go
+++ go/src/koding/kites/kloud/provider/digitalocean/client.go
@@ -131,7 +131,7 @@ func (c *Client) Build(snapshotName, dropletName, username string) (*protocol.Bu
 	return &protocol.BuildResponse{
 		QueryString:  klient.String(),
 		IpAddress:    droplet.IpAddress,
-		InstanceName: droplet.Name,
+		InstanceName: dropletName, // we don't use droplet.Name because it might have the cached name
 		InstanceId:   droplet.Id,
 	}, nil
 }
