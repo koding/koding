e55d06ad656ce21f02dcfd3d8247372cbf988932
Can Yucel <can@koding.com>
2014-06-16 14:21:40 -0700

    Migration: Update account document in account migration

--- go/src/socialapi/workers/migrator/controller/accountmigrator.go
+++ go/src/socialapi/workers/migrator/controller/accountmigrator.go
@@ -5,6 +5,7 @@ import (
 	mongomodels "koding/db/models"
 	"koding/db/mongodb/modelhelper"
 	"socialapi/models"
+	"strconv"
 )
 
 func (mwc *Controller) migrateAllAccounts() error {
@@ -30,6 +31,13 @@ func (mwc *Controller) migrateAllAccounts() error {
 		if err := a.FetchOrCreate(); err != nil {
 			errCount++
 			mwc.log.Error("Error occurred for account %s: %s", oldAccount.Id.Hex())
+			continue
+		}
+
+		s := modelhelper.Selector{"_id": oldAccount.Id}
+		o := modelhelper.Selector{"$set": modelhelper.Selector{"socialApiId": strconv.FormatInt(id, 10)}}
+		if err := modelhelper.UpdateAccount(s, o); err != nil {
+			mwc.log.Warning("Could not update account document: %s", err)
 			continue
 		}
 
