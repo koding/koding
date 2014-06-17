f65ef5d73ad7cb3df7497ac9a7fadd0b994cf414
Cihangir SAVAS <cihangir@koding.com>
2014-06-16 20:57:31 -0700

    social: set account nick in account creation method

--- go/src/socialapi/rest/account.go
+++ go/src/socialapi/rest/account.go
@@ -3,6 +3,7 @@ package rest
 import "socialapi/models"
 
 func CreateAccount(a *models.Account) (*models.Account, error) {
+	a.Nick = a.OldId
 	acc, err := sendModel("POST", "/account", a)
 	if err != nil {
 		return nil, err
