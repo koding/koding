343bb190b0612c0aab2a54c305f04c042843406a
Cihangir SAVAS <cihangir@koding.com>
2014-06-16 17:25:28 -0700

    Social: add a function for updating added_at `UpdateAddedAt`

--- go/src/socialapi/models/channelmessagelist.go
+++ go/src/socialapi/models/channelmessagelist.go
@@ -303,3 +303,24 @@ func (c *ChannelMessageList) DeleteMessagesBySelector(selector map[string]interf
 	}
 	return nil
 }
+
+func (c *ChannelMessageList) UpdateAddedAt(channelId, messageId int64) error {
+	if messageId == 0 || channelId == 0 {
+		return errors.New("channelId/messageId is not set")
+	}
+
+	query := &bongo.Query{
+		Selector: map[string]interface{}{
+			"channel_id": channelId,
+			"message_id": messageId,
+		},
+	}
+
+	err := c.One(query)
+	if err != nil {
+		return err
+	}
+
+	c.AddedAt = time.Now().UTC()
+	return c.Update()
+}
