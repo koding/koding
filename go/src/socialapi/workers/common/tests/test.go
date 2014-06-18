b33f8423232fed937e28c54744156073abcf8937
Cihangir SAVAS <cihangir@koding.com>
2014-06-18 12:40:58 -0700

    Social: check for err first

--- go/src/socialapi/workers/common/tests/test.go
+++ go/src/socialapi/workers/common/tests/test.go
@@ -3,6 +3,6 @@ package tests
 import . "github.com/smartystreets/goconvey/convey"
 
 func ResultedWithNoErrorCheck(result interface{}, err error) {
-	So(result, ShouldNotBeNil)
 	So(err, ShouldBeNil)
+	So(result, ShouldNotBeNil)
 }
