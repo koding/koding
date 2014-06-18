ed665ca854e8c8e572f53fb4665db390b3dc4cbf
Cihangir SAVAS <cihangir@koding.com>
2014-06-18 13:46:25 -0700

    social: add MarkTroll and Is Troll functions

--- go/src/socialapi/models/metabits.go
+++ go/src/socialapi/models/metabits.go
@@ -1,3 +1,13 @@
 package models
 
 type MetaBits int16
+
+func (m MetaBits) MarkTroll() {
+	// set first bit as 1
+	m |= 1
+}
+
+func (m MetaBits) IsTroll() bool {
+	// get first bit
+	return (m & 1) == 1
+}
