76b97e1325326f62ef49222a9006eba4e1b657f9
Cihangir SAVAS <cihangir@koding.com>
2014-06-18 14:08:34 -0700

    Social: add troll variable to Const

--- go/src/socialapi/models/metabits.go
+++ go/src/socialapi/models/metabits.go
@@ -1,13 +1,17 @@
 package models
 
+const (
+	Troll = 1 << iota
+)
+
 type MetaBits int16
 
 func (m MetaBits) MarkTroll() {
 	// set first bit as 1
-	m |= 1
+	m |= Troll
 }
 
 func (m MetaBits) IsTroll() bool {
 	// get first bit
-	return (m & 1) == 1
+	return (m & Troll) == 1
 }
