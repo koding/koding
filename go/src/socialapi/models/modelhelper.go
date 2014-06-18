b25df153f0bab027cdf38e87acdaca43bfe668d1
Cihangir SAVAS <cihangir@koding.com>
2014-06-18 12:55:42 -0700

    Social: open commented-out code block

--- go/src/socialapi/models/modelhelper.go
+++ go/src/socialapi/models/modelhelper.go
@@ -62,10 +62,10 @@ func ZeroDate() time.Time {
 	return time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC)
 }
 
-func updateTrollModeBit(metaBits int16) int16 {
-	// if metaBits != 0 {
-	// 	return metaBits
-	// }
+func updateTrollModeBit(metaBits MetaBits) MetaBits {
+	if metaBits != 0 {
+		return metaBits
+	}
 
 	return 1
 }
