554c8b37a17a8915c3a70cbddd27744427f72939
Cihangir SAVAS <cihangir@koding.com>
2014-06-27 00:19:00 -0700

    Social: add tests for future implementations

---
+++ go/src/socialapi/workers/populartopic/populartopic/populartopic_test.go
@@ -0,0 +1,12 @@
+package populartopic
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMarkedAsTroll(t *testing.T) {
+	Convey("given a controller", t, func() {
+		Convey("troll topics should not be in the populartopic stream", nil)
+	})
+}
