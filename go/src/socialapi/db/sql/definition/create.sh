dd9299bcc547137ddd6d8c332479083fc749fb13
Can Yucel <can@koding.com>
2014-06-30 16:28:00 -0700

    Sitemap: Remove sitemap/create.sh and move contents under main create.sh file

--- go/src/socialapi/db/sql/definition/create.sh
+++ go/src/socialapi/db/sql/definition/create.sh
@@ -43,4 +43,16 @@ sudo -u postgres psql social < $1/notification_definition/004-table.sql
 # create constraints
 sudo -u postgres psql social < $1/notification_definition/005-constraint.sql
 
-/opt/koding/go/src/socialapi/workers/sitemap/sql/create.sh
+# SITEMAP WORKER SQL IMPORTS
+
+# create sequences
+sudo -u postgres psql social < $1/sitemap_definition/002-schema.sql
+
+# create sequences
+sudo -u postgres psql social < $1/sitemap_definition/003-sequence.sql
+
+# create tables
+sudo -u postgres psql social < $1/sitemap_definition/004-table.sql
+
+# create constraints
+sudo -u postgres psql social < $1/sitemap_definition/005-constraint.sql
