2f7f27e579039ebf3b3917ee952cfeafbb506719
Can Yucel <can@koding.com>
2014-07-01 19:11:04 -0700

    StaticPage: Change verbose query copy method

--- go/src/socialapi/request/query.go
+++ go/src/socialapi/request/query.go
@@ -96,17 +96,8 @@ func (q *Query) MapURL(u *url.URL) *Query {
 
 func (q *Query) Clone() *Query {
 	cq := NewQuery()
-	cq.Skip = q.Skip
-	cq.Limit = q.Limit
-	cq.To = q.To
-	cq.From = q.From
-	cq.GroupName = q.GroupName
-	cq.Type = q.Type
-	cq.Privacy = q.Privacy
-	cq.AccountId = q.AccountId
-	cq.Name = q.Name
-	cq.Slug = q.Slug
-	cq.ShowExempt = q.ShowExempt
+	*cq = *q
+
 	return cq
 }
 
