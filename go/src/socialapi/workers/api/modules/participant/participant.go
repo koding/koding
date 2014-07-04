eb964ba46af23190e00e6c3c046352e13f85fd50
Cihangir SAVAS <cihangir@koding.com>
2014-07-04 12:50:15 -0700

    Social: support multiple participants in participant operations

--- go/src/socialapi/workers/api/modules/participant/participant.go
+++ go/src/socialapi/workers/api/modules/participant/participant.go
@@ -25,123 +25,108 @@ func List(u *url.URL, h http.Header, _ interface{}) (int, http.Header, interface
 	)
 }
 
-// todo fix duplicate code block with Delete handler
-func Add(u *url.URL, h http.Header, req *models.ChannelParticipant) (int, http.Header, interface{}, error) {
-	// we are getting requester from body for now, but it will be gotten
-	// from the token
-	requesterId := req.AccountId
-	if requesterId == 0 {
-		return response.NewBadRequest(errors.New("Requester AccountId is not set"))
-	}
+func Add(u *url.URL, h http.Header, participants []*models.ChannelParticipant) (int, http.Header, interface{}, error) {
+	query := request.GetQuery(u)
+	channelId := query.Id
 
-	channelId, err := request.GetURIInt64(u, "id")
-	if err != nil {
+	if err := checkChannelPrerequisites(channelId, query.AccountId, participants); err != nil {
 		return response.NewBadRequest(err)
 	}
 
-	accountId, err := request.GetURIInt64(u, "accountId")
-	if err != nil {
-		return response.NewBadRequest(err)
+	for i := range participants {
+		participant := models.NewChannelParticipant()
+		participant.AccountId = participants[i].AccountId
+		participant.ChannelId = channelId
+
+		if err := participant.Create(); err != nil {
+			return response.NewBadRequest(err)
+		}
+
+		participants[i] = participant
 	}
 
-	if err := checkChannelPrerequisites(channelId, requesterId, accountId); err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	// do not forget to override account id
-	req.AccountId = accountId
-	req.ChannelId = channelId
-	req.StatusConstant = models.ChannelParticipant_STATUS_ACTIVE
-
-	if err := req.Create(); err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	return response.NewOK(req)
+	return response.NewOK(participants)
 }
 
-func checkChannelPrerequisites(channelId, requesterId, accountId int64) error {
-	c := models.NewChannel()
-	if err := c.ById(channelId); err != nil {
+func Delete(u *url.URL, h http.Header, participants []*models.ChannelParticipant) (int, http.Header, interface{}, error) {
+	query := request.GetQuery(u)
+	channelId := query.Id
+
+	if err := checkChannelPrerequisites(channelId, query.AccountId, participants); err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	for i := range participants {
+		participants[i].ChannelId = channelId
+		if err := participants[i].Delete(); err != nil {
+			return response.NewBadRequest(err)
+		}
+	}
+
+	return response.NewOK(participants)
+}
+
+func Presence(u *url.URL, h http.Header, participants []*models.ChannelParticipant) (int, http.Header, interface{}, error) {
+	query := request.GetQuery(u)
+	channelId := query.Id
+
+	if err := checkChannelPrerequisites(channelId, query.AccountId, participants); err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	for i := range participants {
+
+		participants[i].ChannelId = channelId
+		if err := participants[i].FetchParticipant(); err != nil {
+			return response.NewBadRequest(err)
+		}
+
+		participants[i].LastSeenAt = time.Now().UTC()
+
+		if err := participants[i].Update(); err != nil {
+			return response.NewBadRequest(err)
+		}
+	}
+
+	return response.NewOK(participants)
+}
+
+func checkChannelPrerequisites(channelId, requesterId int64, participants []*models.ChannelParticipant) error {
+	if channelId == 0 || requesterId == 0 {
+		return errors.New("values are not set")
+	}
+
+	if len(participants) == 0 {
+		return errors.New("0 participant is given for participant operation")
+	}
+
+	c, err := models.ChannelById(channelId)
+	if err != nil {
 		return err
 	}
 
+	// if requester tries to add participant into pinned activity channel
+	// return error
 	if c.TypeConstant == models.Channel_TYPE_PINNED_ACTIVITY {
-		return errors.New("You can not add/remove a new participant for pinned activity channel")
+		return errors.New("can not add/remove participants for pinned activity channel")
 	}
 
-	if c.TypeConstant == models.Channel_TYPE_CHAT {
-		if requesterId != c.CreatorId {
-			return errors.New("Only owners can add/remove participants to chat channel")
-		}
+	// return early for non private message channels
+	if c.TypeConstant != models.Channel_TYPE_PRIVATE_MESSAGE {
+		return nil
 	}
+
+	// check if requester is a participant of the private message channel
+	cp := models.NewChannelParticipant()
+	cp.ChannelId = channelId
+	isParticipant, err := cp.IsParticipant(requesterId)
+	if err != nil {
+		return err
+	}
+
+	if !isParticipant {
+		return fmt.Errorf("%d is not a participant of the channel %d, only participants can add/remove", requesterId, channelId)
+	}
+
 	return nil
-}
-
-func Delete(u *url.URL, h http.Header, req *models.ChannelParticipant) (int, http.Header, interface{}, error) {
-	// we are getting requester from body for now, but it will be gotten
-	// from the token
-	requesterId := req.AccountId
-	if requesterId == 0 {
-		return response.NewBadRequest(errors.New("Requester AccountId is not set"))
-	}
-
-	channelId, err := request.GetURIInt64(u, "id")
-	if err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	accountId, err := request.GetURIInt64(u, "accountId")
-	if err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	if err := checkChannelPrerequisites(channelId, requesterId, accountId); err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	// do not forget to override account id
-	req.AccountId = accountId
-	req.ChannelId = channelId
-
-	if err := req.Delete(); err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	return response.NewOK(req)
-}
-
-func Presence(u *url.URL, h http.Header, req *models.ChannelParticipant) (int, http.Header, interface{}, error) {
-	// we are getting requester from body for now, but it will be gotten
-	// from the token
-	requesterId := req.AccountId
-	if requesterId == 0 {
-		return response.NewBadRequest(errors.New("Requester AccountId is not set"))
-	}
-
-	channelId, err := request.GetURIInt64(u, "id")
-	if err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	accountId, err := request.GetURIInt64(u, "accountId")
-	if err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	// do not forget to override account id
-	req.AccountId = accountId
-	req.ChannelId = channelId
-
-	if err := req.FetchParticipant(); err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	req.LastSeenAt = time.Now().UTC()
-
-	if err := req.Update(); err != nil {
-		return response.NewBadRequest(err)
-	}
-
-	return response.NewOK(req)
 }
