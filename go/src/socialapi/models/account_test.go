bd5a506947af8830dd35ab89b2ce327a88980bf7
mehmetalisavas <mehmetali.savas@yandex.com>
2014-07-08 00:47:24 +0300

    starting working on account tests

---
+++ go/src/socialapi/models/account_test.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAccountNewAccount(t *testing.T) {
+	Convey("while testing new account", t, func() {
+		Convey("Function call should return account", func() {
+			So(NewAccount(), ShouldNotBeNil)
+		})
+	})
+}
+
+func TestAccountGetId(t *testing.T) {
+	Convey("while testing get id", t, func() {
+		Convey("Initialized struct ", func() {
+			Convey("should return given id", func() {
+				a := Account{Id: 42}
+				So(a.GetId(), ShouldEqual, 42)
+			})
+			Convey("Uninitialized struct ", func() {
+				Convey("should return given id", func() {
+					So(NewAccount().GetId(), ShouldEqual, 0)
+				})
+				So(NewAccount(), ShouldNotBeNil)
+			})
+		})
+	})
+}
