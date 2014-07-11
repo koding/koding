630f87f4712c7db2cadc090ad156dab7aa5c2b53
mehmetalisavas <mehmetali.savas@yandex.com>
2014-07-11 15:31:48 +0300

    interaction: some functions removed from here & added to interaction_bongo

--- go/src/socialapi/models/interaction.go
+++ go/src/socialapi/models/interaction.go
@@ -41,54 +41,6 @@ const (
 	Interaction_TYPE_DONWVOTE = "downvote"
 )
 
-func (i *Interaction) BeforeCreate() error {
-	return i.MarkIfExempt()
-}
-
-func (i *Interaction) BeforeUpdate() error {
-	return i.MarkIfExempt()
-}
-
-func (i *Interaction) AfterCreate() {
-	bongo.B.AfterCreate(i)
-}
-
-func (i *Interaction) AfterUpdate() {
-	bongo.B.AfterUpdate(i)
-}
-
-func (i Interaction) AfterDelete() {
-	bongo.B.AfterDelete(i)
-}
-
-func (i Interaction) GetId() int64 {
-	return i.Id
-}
-
-func (i Interaction) TableName() string {
-	return "api.interaction"
-}
-
-func NewInteraction() *Interaction {
-	return &Interaction{}
-}
-
-func (i *Interaction) One(q *bongo.Query) error {
-	return bongo.B.One(i, i, q)
-}
-
-func (i *Interaction) ById(id int64) error {
-	return bongo.B.ById(i, id)
-}
-
-func (i *Interaction) Create() error {
-	return bongo.B.Create(i)
-}
-
-func (i *Interaction) Update() error {
-	return bongo.B.Update(i)
-}
-
 func (i *Interaction) CreateRaw() error {
 	insertSql := "INSERT INTO " +
 		i.TableName() +
@@ -154,10 +106,6 @@ func (i *Interaction) getAccountId() (int64, error) {
 	}
 
 	return ii.AccountId, nil
-}
-
-func (i *Interaction) Some(data interface{}, q *bongo.Query) error {
-	return bongo.B.Some(i, data, q)
 }
 
 func (i *Interaction) Delete() error {
