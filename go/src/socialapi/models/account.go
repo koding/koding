203a143b3ec7b3abd444286e3c57eb0a558020f1
mehmetalisavas <mehmetali.savas@yandex.com>
2014-07-11 15:04:18 +0300

    account: some functions removed from here & added to account_bongo

--- go/src/socialapi/models/account.go
+++ go/src/socialapi/models/account.go
@@ -24,38 +24,6 @@ type Account struct {
 	Nick string `json:"nick"        sql:"NOT NULL;UNIQUE;TYPE:VARCHAR(25);`
 }
 
-func NewAccount() *Account {
-	return &Account{}
-}
-
-func (a Account) GetId() int64 {
-	return a.Id
-}
-
-func (a Account) TableName() string {
-	return "api.account"
-}
-
-func (a *Account) AfterUpdate() {
-	SetAccountToCache(a)
-}
-
-func (a *Account) AfterCreate() {
-	SetAccountToCache(a)
-}
-
-func (a *Account) One(q *bongo.Query) error {
-	return bongo.B.One(a, a, q)
-}
-
-func (a *Account) ById(id int64) error {
-	return bongo.B.ById(a, id)
-}
-
-func (a *Account) Update() error {
-	return bongo.B.Update(a)
-}
-
 func (a *Account) FetchOrCreate() error {
 	if a.OldId == "" {
 		return errors.New("old id is not set")
@@ -85,18 +53,6 @@ func (a *Account) FetchOrCreate() error {
 	}
 
 	return err
-}
-
-func (a *Account) Create() error {
-	if a.OldId == "" {
-		return errors.New("old id is not set")
-	}
-
-	return bongo.B.Create(a)
-}
-
-func (a *Account) Some(data interface{}, q *bongo.Query) error {
-	return bongo.B.Some(a, data, q)
 }
 
 func (a *Account) FetchChannels(q *request.Query) ([]Channel, error) {
