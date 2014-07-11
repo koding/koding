7dc6688ba4f741c420b7455953a701247c3bb8f0
mehmetalisavas <mehmetali.savas@yandex.com>
2014-07-11 15:07:05 +0300

    config: commented to read

--- go/src/socialapi/config/config.go
+++ go/src/socialapi/config/config.go
@@ -17,17 +17,25 @@ func MustGet() *Config {
 	return conf
 }
 
+// MustRead takes a relative file path
+// and tries to open and read it into Config struct
+// If file is not there or file is not given, it panics
+// If the given file is not formatted well panics
 func MustRead(path string) *Config {
+	// get working directory for joining with given config path
 	pwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 
+//join working dir and config path
 	configPath := filepath.Join(pwd, path)
 	if _, err := toml.DecodeFile(configPath, &conf); err != nil {
 		panic(err)
 	}
 
+	// we can override Environment property of 
+	//the config from env variable
 	// set environment variable
 	env := os.Getenv("SOCIAL_API_ENV")
 	if env != "" {
