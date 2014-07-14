383c7442a6d372ec2a381d6d701e221b5bc77aef
Cihangir SAVAS <cihangir@koding.com>
2014-07-14 01:35:48 -0700

    BongoWrapper: add tests for bongo scheme

---
+++ go/src/koding/kites/bongoscheme/scheme_test.go
@@ -0,0 +1,63 @@
+package bongoscheme
+
+import "testing"
+
+func TestCallingStaticFunc(t *testing.T) {
+	bongo, err := New(&Config{Version: "0.0.1", ClientURL: "http://localhost:3636/kite"})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	res, err := bongo.Model("JAccount").Static().Func("one").Call()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if res.MustString() != `{"arguments":[],"constructorName":"JAccount","method":"one","type":"static"}` {
+		t.Fatal("response is not same with the req")
+	}
+}
+
+func TestCallingStaticFuncWithArgs(t *testing.T) {
+	bongo, err := New(&Config{Version: "0.0.1", ClientURL: "http://localhost:3636/kite"})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	res, err := bongo.Model("JAccount").Static().Func("one").CallWith("gel", "beri")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if res.MustString() != `{"arguments":["gel","beri"],"constructorName":"JAccount","method":"one","type":"static"}` {
+		t.Fatal("response is not same with the req")
+	}
+}
+
+func TestCallingInstanceFunc(t *testing.T) {
+	bongo, err := New(&Config{Version: "0.0.1", ClientURL: "http://localhost:3636/kite"})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	res, err := bongo.Model("JAccount").Instance("123").Func("one").Call()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if res.MustString() != `{"arguments":[],"constructorName":"JAccount","id":"123","method":"one","type":"instance"}` {
+		t.Fatal("response is not same with the req")
+	}
+}
+
+func TestCallingInstanceFuncWithArgs(t *testing.T) {
+	bongo, err := New(&Config{Version: "0.0.1", ClientURL: "http://localhost:3636/kite"})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	res, err := bongo.Model("JAccount").Instance("123").Func("one").CallWith("nil", "dil")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if res.MustString() != `{"arguments":["nil","dil"],"constructorName":"JAccount","id":"123","method":"one","type":"instance"}` {
+		t.Fatal("response is not same with the req")
+	}
+}
