333485fc5fc593d072a624cbe3cc68f8d8349375
Fatih Arslan <fatih@koding.com>
2014-07-16 13:23:39 +0300

    kloud/koding: further customize amazon building

--- go/src/koding/kites/kloud/koding/provider.go
+++ go/src/koding/kites/kloud/koding/provider.go
@@ -2,12 +2,15 @@ package koding
 
 import (
 	"errors"
+	"fmt"
+	"time"
 
 	aws "github.com/koding/kloud/api/amazon"
 	"github.com/koding/kloud/eventer"
 	"github.com/koding/kloud/machinestate"
 	"github.com/koding/kloud/protocol"
 	"github.com/koding/kloud/provider/amazon"
+	"github.com/mitchellh/goamz/ec2"
 
 	"github.com/koding/logging"
 )
@@ -73,9 +76,73 @@ func (p *Provider) Build(opts *protocol.MachineOptions) (*protocol.Artifact, err
 		return nil, errors.New("server name is empty")
 	}
 
+	groupName := "koding-kloud" // TODO: make it from the package level and remove it from here
+	a.Log.Info("Checking if security group '%s' exists", groupName)
+	group, err := a.SecurityGroup(groupName)
+	if err != nil {
+		a.Log.Info("No security group with name: '%s' exists. Creating a new one...", groupName)
+		vpcs, err := a.ListVPCs()
+		if err != nil {
+			return nil, err
+		}
+
+		group = ec2.SecurityGroup{
+			Name:        groupName,
+			Description: "Koding Kloud Security Group",
+			VpcId:       vpcs.VPCs[0].VpcId,
+		}
+
+		a.Log.Info("Creating security group for this instance...")
+		// TODO: remove it after we are done
+		groupResp, err := a.Client.CreateSecurityGroup(group)
+		if err != nil {
+			return nil, err
+		}
+		group = groupResp.SecurityGroup
+
+		// Authorize the SSH access
+		perms := []ec2.IPPerm{
+			ec2.IPPerm{
+				Protocol:  "tcp",
+				FromPort:  22,
+				ToPort:    22,
+				SourceIPs: []string{"0.0.0.0/0"},
+			},
+		}
+
+		// We loop and retry this a few times because sometimes the security
+		// group isn't available immediately because AWS resources are eventaully
+		// consistent.
+		a.Log.Info("Authorizing SSH access on the security group: '%s'", group.Id)
+		for i := 0; i < 5; i++ {
+			_, err = a.Client.AuthorizeSecurityGroup(group, perms)
+			if err == nil {
+				break
+			}
+
+			a.Log.Warning("Error authorizing. Will sleep and retry. %s", err)
+			time.Sleep((time.Duration(i) * time.Second) + 1)
+		}
+		if err != nil {
+			return nil, fmt.Errorf("Error creating temporary security group: %s", err)
+		}
+	}
+
+	// add now our security group
+	a.Builder.SecurityGroupId = group.Id
+
 	// Use koding plans instead of those later
 	a.Builder.SourceAmi = DefaultAMI
 	a.Builder.InstanceType = DefaultInstanceType
+
+	// needed for vpc instances, go and grap one from one of our Koding's own
+	// subnets
+	a.Log.Info("Searching for subnets")
+	subs, err := a.ListSubnets()
+	if err != nil {
+		return nil, err
+	}
+	a.Builder.SubnetId = subs.Subnets[0].SubnetId
 
 	return a.Build(opts.InstanceName)
 }
