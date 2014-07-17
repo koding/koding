2603e1dc77bfd5feacd97dfe75125e9d43387e48
Cihangir SAVAS <cihangir@koding.com>
2014-07-17 16:33:19 -0700

    Social: add safe initializer for redis connection

--- go/src/socialapi/workers/helper/redishelper.go
+++ go/src/socialapi/workers/helper/redishelper.go
@@ -9,17 +9,33 @@ import (
 var redisConn *redis.RedisSession
 
 func MustInitRedisConn(c *config.Config) *redis.RedisSession {
-	r, err := redis.NewRedisSession(&redis.RedisConf{Server: c.Redis.URL, DB: c.Redis.DB})
+	r, err := InitRedisConn(c)
 	if err != nil {
 		panic(err)
 	}
-	redisConn = r
+
 	return r
+}
+
+func InitRedisConn(c *config.Config) (*redis.RedisSession, error) {
+	// if redisconn is already created, return early, return ofthen
+	if redisConn != nil {
+		return redisConn, nil
+	}
+
+	r, err := redis.NewRedisSession(&redis.RedisConf{Server: c.Redis.URL, DB: c.Redis.DB})
+	if err != nil {
+		return nil, err
+	}
+
+	redisConn = r
+	return r, nil
 }
 
 func MustGetRedisConn() *redis.RedisSession {
 	if redisConn == nil {
 		panic("Redis connection is nil You should call \"MustInitRedisConn(server string)\" first")
 	}
+
 	return redisConn
 }
