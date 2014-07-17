810f18e69935bc1c06a2015735fb0133c5e85eae
Cihangir SAVAS <cihangir@koding.com>
2014-07-16 18:11:37 -0700

    Social: set some default errors for further processing

--- go/src/socialapi/models/errors.go
+++ go/src/socialapi/models/errors.go
@@ -3,5 +3,8 @@ package models
 import "errors"
 
 var (
-	IdNotSet = errors.New("Id is not set")
+	IdNotSet                    = errors.New("Id is not set")
+	ErrChannelIsNotSet          = errors.New("channel is not set")
+	ErrMessageIsNotSet          = errors.New("message is not set")
+	ErrChannelContainerIsNotSet = errors.New("channel container is not set")
 )
