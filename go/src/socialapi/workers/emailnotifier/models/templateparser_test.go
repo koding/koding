48b2b60608a101d56c6835dfa5aa1d08deda0910
Samy Pessé <samypesse@gmail.com>
2014-07-18 12:04:29 -0700

    Add unit test for daily email

--- go/src/socialapi/workers/emailnotifier/models/templateparser_test.go
+++ go/src/socialapi/workers/emailnotifier/models/templateparser_test.go
@@ -9,7 +9,6 @@ import (
 func TestTemplateRender(t *testing.T) {
     uc := UserContact{}
 
-
     Convey("Daily email", t, func() {
         Convey("Should load a correct config", func() {
             So(func() { config.MustRead("./test.toml") }, ShouldNotPanic)
@@ -23,12 +22,23 @@ func TestTemplateRender(t *testing.T) {
                 err := tp.validateTemplateParser()
                 So(err, ShouldBeNil)
 
+
                 Convey("Should be able to inline css from style.css", func() {
                     base := "<html><head></head><body><a href='test'>test</a></body></html>"
                     html := tp.inlineCss(base)
 
                     So(html, ShouldNotBeNil)
                     So(html, ShouldNotEqual, base)
+                    So(html, ShouldContainSubstring, "style=")
+                })
+
+
+                Convey("Should be able to inline css from style.css", func() {
+                    var containers []*MailerContainer
+                    html, err := tp.RenderDailyTemplate(containers)
+
+                    So(err, ShouldBeNil)
+                    So(html, ShouldNotBeNil)
                     So(html, ShouldContainSubstring, "style=")
                 })
             })
