042987ffdf6c86a3b3eb9c72b5d0587478ae0ba6
Senthil <senthil@koding.com>
2014-07-25 13:56:23 -0700

    Social: spell check

--- go/src/socialapi/workers/pinnedpost/pinnedpost/pinnedpost.go
+++ go/src/socialapi/workers/pinnedpost/pinnedpost/pinnedpost.go
@@ -69,7 +69,7 @@ func (c *Controller) MessageReplyCreated(messageReply *models.MessageReply) erro
 }
 
 func (c *Controller) addMessage(accountId, messageId, channelId int64) error {
-	// fetch the parent channel for gorup name
+	// fetch the parent channel for group name
 	// get it from cache
 	channel, err := models.ChannelById(channelId)
 	if err != nil {
@@ -84,6 +84,7 @@ func (c *Controller) addMessage(accountId, messageId, channelId int64) error {
 
 	// add parent message into pinning channel
 	_, err = pinningChannel.AddMessage(messageId)
+
 	// if message is already in the channel ignore the error, and mark process as successful
 	if err == models.ErrAlreadyInTheChannel {
 		return nil
