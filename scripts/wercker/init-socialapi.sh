2265137df7659f9615b6c6746ed02b8af2c57e34
Can Yucel <can@koding.com>
2014-08-08 18:49:41 +0300

    Wercker: Run api service in background

---
+++ scripts/wercker/init-socialapi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp scripts/wercker/social-api.conf /etc/init/social-api.conf
+touch /etc/init.d/social-api
+initctl emit social-api POSTGRES_HOST=$WERCKER_POSTGRESQL_HOST \
+                        POSTGRES_PORT=$WERCKER_POSTGRESQL_PORT \
+                        POSTGRES_USERNAME=$WERCKER_POSTGRESQL_USERNAME \
+                        POSTGRES_PASSWORD=$WERCKER_POSTGRESQL_PASSWORD \
+                        POSTGRES_DBNAME=$WERCKER_POSTGRESQL_DATABASE \
+                        RABBITMQ_HOST=$WERCKER_RABBITMQ_HOST \
+                        RABBITMQ_PORT=$WERCKER_RABBITMQ_PORT \
+                        RABBITMQ_USERNAME=$WERCKER_RABBITMQ_USERNAME \
+                        RABBITMQ_PASSWORD=$WERCKER_RABBITMQ_PASSWORD \
+                        REDIS_URL=$WERCKER_REDIS_HOST:$WERCKER_REDIS_PORT \
+                        MONGO_URL=$WERCKER_MONGODB_URL \
+                        WERCKER_SOURCE_DIR=$WERCKER_SOURCE_DIR
