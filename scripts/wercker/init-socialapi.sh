686127a51d5cb2285658809f4acd2ab6e4085c79
Can Yucel <can@koding.com>
2014-08-12 19:12:19 +0300

    Wercker: Add Pinnedpost service

--- scripts/wercker/init-socialapi.sh
+++ scripts/wercker/init-socialapi.sh
@@ -30,3 +30,18 @@ initctl emit social-populartopic POSTGRES_HOST=$WERCKER_POSTGRESQL_HOST \
                         REDIS_URL=$WERCKER_REDIS_HOST:$WERCKER_REDIS_PORT \
                         MONGO_URL=$WERCKER_MONGODB_URL \
                         WERCKER_SOURCE_DIR=$WERCKER_SOURCE_DIR
+
+cp scripts/wercker/social-pinnedpost.conf /etc/init/social-pinnedpost.conf
+touch /etc/init.d/social-pinnedpost
+initctl emit social-pinnedpost POSTGRES_HOST=$WERCKER_POSTGRESQL_HOST \
+                        POSTGRES_PORT=$WERCKER_POSTGRESQL_PORT \
+                        POSTGRES_USERNAME=$WERCKER_POSTGRESQL_USERNAME \
+                        POSTGRES_PASSWORD=$WERCKER_POSTGRESQL_PASSWORD \
+                        POSTGRES_DBNAME=$WERCKER_POSTGRESQL_DATABASE \
+                        RABBITMQ_HOST=$WERCKER_RABBITMQ_HOST \
+                        RABBITMQ_PORT=$WERCKER_RABBITMQ_PORT \
+                        RABBITMQ_USERNAME=$WERCKER_RABBITMQ_USERNAME \
+                        RABBITMQ_PASSWORD=$WERCKER_RABBITMQ_PASSWORD \
+                        REDIS_URL=$WERCKER_REDIS_HOST:$WERCKER_REDIS_PORT \
+                        MONGO_URL=$WERCKER_MONGODB_URL \
+                        WERCKER_SOURCE_DIR=$WERCKER_SOURCE_DIR
