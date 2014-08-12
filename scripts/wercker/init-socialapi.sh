7e062ce1c717cda6dcc6fc8327fa3936b89e1fbb
Can Yucel <can@koding.com>
2014-08-12 18:33:14 +0300

    Wercker: Add populartopic as service

--- scripts/wercker/init-socialapi.sh
+++ scripts/wercker/init-socialapi.sh
@@ -14,3 +14,19 @@ initctl emit social-api POSTGRES_HOST=$WERCKER_POSTGRESQL_HOST \
                         REDIS_URL=$WERCKER_REDIS_HOST:$WERCKER_REDIS_PORT \
                         MONGO_URL=$WERCKER_MONGODB_URL \
                         WERCKER_SOURCE_DIR=$WERCKER_SOURCE_DIR
+
+
+cp scripts/wercker/social-populartopic.conf /etc/init/social-populartopic.conf
+touch /etc/init.d/social-populartopic
+initctl emit social-populartopic POSTGRES_HOST=$WERCKER_POSTGRESQL_HOST \
+                        POSTGRES_PORT=$WERCKER_POSTGRESQL_PORT \
+                        POSTGRES_USERNAME=$WERCKER_POSTGRESQL_USERNAME \
+                        POSTGRES_PASSWORD=$WERCKER_POSTGRESQL_PASSWORD \
+                        POSTGRES_DBNAME=$WERCKER_POSTGRESQL_DATABASE \
+                        RABBITMQ_HOST=$WERCKER_RABBITMQ_HOST \
+                        RABBITMQ_PORT=$WERCKER_RABBITMQ_PORT \
+                        RABBITMQ_USERNAME=$WERCKER_RABBITMQ_USERNAME \
+                        RABBITMQ_PASSWORD=$WERCKER_RABBITMQ_PASSWORD \
+                        REDIS_URL=$WERCKER_REDIS_HOST:$WERCKER_REDIS_PORT \
+                        MONGO_URL=$WERCKER_MONGODB_URL \
+                        WERCKER_SOURCE_DIR=$WERCKER_SOURCE_DIR
