279cab32b8c44ab636450981a1b1d6a03d345322
Cihangir SAVAS <cihangir@koding.com>
2014-08-12 15:42:32 -0700

    Social: add tests for topic name extracting

--- go/src/socialapi/workers/topicfeed/topicfeed/topicfeed_test.go
+++ go/src/socialapi/workers/topicfeed/topicfeed/topicfeed_test.go
@@ -11,5 +11,16 @@ func TestMarkedAsTroll(t *testing.T) {
 		Convey("duplicates should be returned as unique", func() {
 			So(len(extractTopics("hi #topic #topic my topic")), ShouldEqual, 1)
 		})
+
+		Convey("public should be removed from topics list", func() {
+			topics := extractTopics("hi #topic #public my topic")
+			So(len(topics), ShouldEqual, 1)
+			So(topics[0], ShouldEqual, "topic")
+		})
+
+		Convey("public should be removed from topics list", func() {
+			topics := extractTopics("hi #public  #public  my topic")
+			So(len(topics), ShouldEqual, 0)
+		})
 	})
 }
