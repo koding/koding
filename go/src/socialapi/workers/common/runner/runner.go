3e4f58df41aa5b9cdb7b20a809482629b76b9036
Cihangir SAVAS <cihangir@koding.com>
2014-08-18 21:06:11 -0700

    Social: as a temp fix read flag values from std package

--- go/src/socialapi/workers/common/runner/runner.go
+++ go/src/socialapi/workers/common/runner/runner.go
@@ -23,18 +23,31 @@ import (
 )
 
 var (
-	flagConfFile       *string
-	flagRegion         *string
-	flagDebug          *bool
-	flagVersion        *int
-	flagOutputMetrics  *bool
-	flagKiteInit       *bool
-	flagKiteLocal      *bool
-	flagKiteProxy      *bool
-	flagKiteKontrolURL *string
+	// flagConfFile       *string
+	// flagRegion         *string
+	// flagDebug          *bool
+	// flagVersion        *int
+	// flagOutputMetrics  *bool
+	// flagKiteInit       *bool
+	// flagKiteLocal      *bool
+	// flagKiteProxy      *bool
+	// flagKiteKontrolURL *string
 
-	flagHost *string
-	flagPort *string
+	// flagHost *string
+	// flagPort *string
+	flagConfFile       = flag.String("c", "", "Configuration profile from file")
+	flagRegion         = flag.String("r", "", "Region name")
+	flagDebug          = flag.Bool("d", false, "Debug mode")
+	flagVersion        = flag.Int("v", 0, "Worker Version")
+	flagOutputMetrics  = flag.Bool("outputMetrics", false, "Output metrics")
+	flagKiteInit       = flag.Bool("kite-init", false, "Init kite system with the worker.")
+	flagKiteLocal      = flag.Bool("kite-local", false, "Start kite system in local mode.")
+	flagKiteProxy      = flag.Bool("kite-proxy", false, "Start kite system behind a proxy")
+	flagKiteKontrolURL = flag.String("kite-kontrol-url", "", "Change kite's register URL to kontrol")
+
+	// for socialAPI worker
+	flagHost = flag.String("host", "0.0.0.0", "listen address")
+	flagPort = flag.String("port", "7000", "listen port")
 )
 
 type Runner struct {
@@ -59,33 +72,38 @@ func WrapWithVersion(name string, version *int) string {
 
 func generateFlagSet() *flag.FlagSet {
 	flagSet := flag.NewFlagSet("Runner", flag.ExitOnError)
-	flagConfFile = flagSet.String("c", "", "Configuration profile from file")
-	flagRegion = flagSet.String("r", "", "Region name")
-	flagDebug = flagSet.Bool("d", false, "Debug mode")
-	flagVersion = flagSet.Int("v", 0, "Worker Version")
-	flagOutputMetrics = flagSet.Bool("outputMetrics", false, "Output metrics")
-	flagKiteInit = flagSet.Bool("kite-init", false, "Init kite system with the worker.")
-	flagKiteLocal = flagSet.Bool("kite-local", false, "Start kite system in local mode.")
-	flagKiteProxy = flagSet.Bool("kite-proxy", false, "Start kite system behind a proxy")
-	flagKiteKontrolURL = flagSet.String("kite-kontrol-url", "", "Change kite's register URL to kontrol")
+	// flagConfFile = flag.String("c", "", "Configuration profile from file")
+	// flagRegion = flag.String("r", "", "Region name")
+	// flagDebug = flag.Bool("d", false, "Debug mode")
+	// flagVersion = flag.Int("v", 0, "Worker Version")
+	// flagOutputMetrics = flag.Bool("outputMetrics", false, "Output metrics")
+	// flagKiteInit = flag.Bool("kite-init", false, "Init kite system with the worker.")
+	// flagKiteLocal = flag.Bool("kite-local", false, "Start kite system in local mode.")
+	// flagKiteProxy = flag.Bool("kite-proxy", false, "Start kite system behind a proxy")
+	// flagKiteKontrolURL = flag.String("kite-kontrol-url", "", "Change kite's register URL to kontrol")
 
-	// for socialAPI worker
-	flagHost = flagSet.String("host", "0.0.0.0", "listen address")
-	flagPort = flagSet.String("port", "7000", "listen port")
+	// // for socialAPI worker
+	// flagHost = flag.String("host", "0.0.0.0", "listen address")
+	// flagPort = flag.String("port", "7000", "listen port")
 
 	return flagSet
 }
 func (r *Runner) Init() error {
-	r.FlagSet = generateFlagSet()
-	r.FlagSet.Parse(os.Args[1:])
-
 	return r.InitWithConfigFile(*flagConfFile)
 }
 
 // InitWithConfigFile used for externally setting config file.
 // This is used for testing purposes, and usage of Init method is encouraged
-func (r *Runner) InitWithConfigFile(flagConfFile string) error {
-	r.Conf = config.MustRead(flagConfFile, r.FlagSet)
+func (r *Runner) InitWithConfigFile(configFile string) error {
+	// r.FlagSet = generateFlagSet()
+
+	flag.Parse()
+
+	// r.FlagSet.Parse(os.Args[1:])
+	configFile = *flagConfFile
+
+	r.Conf = config.MustRead(configFile, r.FlagSet)
+
 	r.Conf.Debug = *flagDebug
 
 	r.Conf.Host = *flagHost
