c3af827f03de9fe0b8ec19a53cb89e9ecfb1bfef
Fatih Arslan <fatih@koding.com>
2014-08-28 12:21:41 +0300

    kloud/klient: ping after the deployment to finish it

--- go/src/koding/kites/kloud/deploy.go
+++ go/src/koding/kites/kloud/deploy.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"koding/db/mongodb"
+	"koding/kites/kloud/klient"
 	"path/filepath"
 	"strings"
 	"time"
@@ -222,6 +223,18 @@ func (k *KodingDeploy) ServeKite(r *kite.Request) (interface{}, error) {
 	}
 
 	query := kiteprotocol.Kite{ID: tknID.String()}
+
+	k.Log.Info("Connecting to remote Klient instance")
+	klient, err := klient.NewKlient(k.Kite, query.String())
+	if err != nil {
+		k.Log.Warning("Connecting to remote Klient instance err: %s", err)
+	} else {
+		defer klient.Close()
+		k.Log.Info("Sending a ping message")
+		if err := klient.Ping(); err != nil {
+			k.Log.Warning("Sending a ping message err:", err)
+		}
+	}
 
 	artifact.KiteQuery = query.String()
 	return artifact, nil
