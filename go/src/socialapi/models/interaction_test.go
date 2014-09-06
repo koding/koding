85dbfef2b886e73405de21561bc0e96bad7f19c9
mehmetalisavas <mehmetali.savas@yandex.com>
2014-09-06 17:00:17 +0300

    social/tests: isExempt tests are added for interaction

--- go/src/socialapi/models/interaction_test.go
+++ go/src/socialapi/models/interaction_test.go
@@ -45,3 +45,33 @@ func TestInteractiongetAccountId(t *testing.T) {
 		})
 	})
 }
+
+func TestInteractionisExempt(t *testing.T) {
+	r := runner.New("test")
+	if err := r.Init(); err != nil {
+		t.Fatalf("couldnt start bongo %s", err.Error())
+	}
+	defer r.Close()
+
+	Convey("While testing interaction is exempt or not", t, func() {
+		Convey("it should have error while getting account id from db when channel id is not set", func() {
+			i := NewInteraction()
+
+			ie, err := i.isExempt()
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldContainSubstring, "couldnt find accountId from content")
+			So(ie, ShouldEqual, false)
+		})
+
+		Convey("it should have error if account id is not set", func() {
+			i := NewInteraction()
+			i.Id = 1098
+
+			ie, err := i.isExempt()
+			So(err, ShouldNotBeNil)
+			So(err, ShouldEqual, bongo.RecordNotFound)
+			So(ie, ShouldEqual, false)
+		})
+
+	})
+}
