d63b6358464f322577796aaeb9b213bf22ae6c10
Senthil <senthil@koding.com>
2014-09-08 01:07:35 -0700

    popularpost: use query.Skip helper instead of parsing it manually

--- go/src/socialapi/workers/api/modules/popular/popular.go
+++ go/src/socialapi/workers/api/modules/popular/popular.go
@@ -139,14 +139,6 @@ func ListPosts(u *url.URL, h http.Header, _ interface{}) (int, http.Header, inte
 	query.Type = models.ChannelMessage_TYPE_POST
 
 	channelName := u.Query().Get("channelName")
-	skip := u.Query().Get("skip")
-
-	i, err := strconv.Atoi(skip)
-	if err != nil {
-		i = 0
-	}
-
-	query.Skip = i
 
 	keyname := popularpost.KeyName{
 		GroupName: query.GroupName, ChannelName: channelName,
