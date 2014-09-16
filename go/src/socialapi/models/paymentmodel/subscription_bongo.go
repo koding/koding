881e888167b1588082109a74b3e87b3c2ac72b27
Senthil <senthil@koding.com>
2014-09-16 12:07:44 -0700

    payment: add subscription_bongo file

---
+++ go/src/socialapi/models/paymentmodel/subscription_bongo.go
@@ -0,0 +1,23 @@
+package paymentmodel
+
+import "github.com/koding/bongo"
+
+func (s Subscription) GetId() int64 {
+	return s.Id
+}
+
+func (Subscription) TableName() string {
+	return "api.payment_subscription"
+}
+
+//----------------------------------------------------------
+// Crud methods
+//----------------------------------------------------------
+
+func (s *Subscription) One(q *bongo.Query) error {
+	return bongo.B.One(s, s, q)
+}
+
+func (s *Subscription) Create() error {
+	return bongo.B.Create(s)
+}
