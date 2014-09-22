810b266c516f5c025be8550c05c41e822dd7130e
Fatih Arslan <fatih@koding.com>
2014-09-22 12:46:19 +0300

    kloudctl: add reinit command

---
+++ go/src/koding/kites/kloud/kloudctl/command/reinit.go
@@ -0,0 +1,45 @@
+package command
+
+import (
+	"fmt"
+
+	"koding/kites/kloud/kloud"
+
+	"github.com/koding/kite"
+	"github.com/mitchellh/cli"
+)
+
+type Reinit struct {
+	id *string
+}
+
+func NewReinit() cli.CommandFactory {
+	return func() (cli.Command, error) {
+		f := NewFlag("reinit", "Reinitialize a machine")
+		f.action = &Reinit{
+			id: f.String("id", "", "Machine id of to be reinitialized."),
+		}
+		return f, nil
+	}
+}
+
+func (r *Reinit) Action(args []string, k *kite.Client) error {
+	reinitArgs := &KloudArgs{
+		MachineId: *r.id,
+		Username:  flagUsername,
+	}
+
+	resp, err := k.Tell("reinitialize", reinitArgs)
+	if err != nil {
+		return err
+	}
+
+	var result kloud.ControlResult
+	err = resp.Unmarshal(&result)
+	if err != nil {
+		return err
+	}
+
+	DefaultUi.Info(fmt.Sprintf("%+v", result))
+	return nil
+}
