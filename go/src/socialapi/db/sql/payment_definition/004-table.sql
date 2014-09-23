3d94f44a1c0241459c6e327f59685f84dac42cb0
Senthil <senthil@koding.com>
2014-09-23 11:57:30 -0700

    payment: add enum suffix to enum identifiers

--- go/src/socialapi/db/sql/payment_definition/004-table.sql
+++ go/src/socialapi/db/sql/payment_definition/004-table.sql
@@ -1,11 +1,11 @@
 -- ----------------------------
 --  Table structure for customer
 -- ----------------------------
-CREATE TYPE "payment"."provider" AS ENUM (
+CREATE TYPE "payment"."provider_enum" AS ENUM (
     'stripe',
     'paypal'
 );
-ALTER TYPE "payment"."provider" OWNER TO "social";
+ALTER TYPE "payment"."provider_enum" OWNER TO "social";
 
 DROP TABLE IF EXISTS "payment"."customer";
 CREATE TABLE "payment"."customer" (
@@ -26,20 +26,20 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON "payment"."customer" TO "social";
 -- ----------------------------
 --  Table structure for plan
 -- ----------------------------
-CREATE TYPE "payment"."plan_interval" AS ENUM (
+CREATE TYPE "payment"."plan_interval_enum" AS ENUM (
     'month',
     'year'
 );
-ALTER TYPE "payment"."plan_interval" OWNER TO "social";
+ALTER TYPE "payment"."plan_interval_enum" OWNER TO "social";
 
-CREATE TYPE "payment"."plan_title" AS ENUM (
+CREATE TYPE "payment"."plan_title_enum" AS ENUM (
     'free',
     'hobbyist',
     'developer',
     'professional',
     'super'
 );
-ALTER TYPE "payment"."plan_interval" OWNER TO "social";
+ALTER TYPE "payment"."plan_title_enum" OWNER TO "social";
 
 DROP TABLE IF EXISTS "payment"."plan";
 CREATE TABLE "payment"."plan" (
@@ -62,12 +62,12 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON "payment"."plan" TO "social";
 -- ----------------------------
 --  Table structure for subscription
 -- ----------------------------
-CREATE TYPE "payment"."subscription_state" AS ENUM (
+CREATE TYPE "payment"."subscription_state_enum" AS ENUM (
     'active',
     'expired',
     'canceled'
 );
-ALTER TYPE "payment"."subscription_state" OWNER TO "social";
+ALTER TYPE "payment"."subscription_state_enum" OWNER TO "social";
 
 DROP TABLE IF EXISTS "payment"."subscription";
 CREATE TABLE "payment"."subscription" (
