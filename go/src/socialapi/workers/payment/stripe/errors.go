f313e152068025cdfad15c0c8aa4df54fd38421d
Senthil <senthil@koding.com>
2014-09-26 11:26:15 -0700

    payment: move `handlerStripeError` to errors.go

--- go/src/socialapi/workers/payment/stripe/errors.go
+++ go/src/socialapi/workers/payment/stripe/errors.go
@@ -1,7 +1,23 @@
 package stripe
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/stripe/stripe-go"
+)
 
 var (
 	ErrStripePlanAlreadyExists = errors.New(`{"type":"invalid_request_error","message":"Plan already exists."}`)
 )
+
+// handlerStripeError casts error into stripe.Error if possible
+// and returns builtin error with human readable string, if not
+// it returns the original error.
+func handleStripeError(err error) error {
+	stripeError, ok := err.(*stripe.Error)
+	if ok {
+		return errors.New(stripeError.Msg)
+	}
+
+	return err
+}
