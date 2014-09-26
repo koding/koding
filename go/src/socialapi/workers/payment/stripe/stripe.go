3b6b1d35e2a4503573416b152c6a0f855929d5cd
Senthil <senthil@koding.com>
2014-09-26 11:26:15 -0700

    payment: move `handlerStripeError` to errors.go

--- go/src/socialapi/workers/payment/stripe/stripe.go
+++ go/src/socialapi/workers/payment/stripe/stripe.go
@@ -1,10 +1,6 @@
 package stripe
 
-import (
-	"errors"
-
-	stripe "github.com/stripe/stripe-go"
-)
+import stripe "github.com/stripe/stripe-go"
 
 var (
 	ProviderName = "stripe"
@@ -12,16 +8,4 @@ var (
 
 func InitializeClientKey(key string) {
 	stripe.Key = key
-}
-
-// handlerStripeError casts error into stripe.Error if possible
-// and returns builtin error with human readable string, if not
-// it returns the original error.
-func handleStripeError(err error) error {
-	stripeError, ok := err.(*stripe.Error)
-	if ok {
-		return errors.New(stripeError.Msg)
-	}
-
-	return err
 }
