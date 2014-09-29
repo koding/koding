b1539e576e127c723923d6cd8c2fe2c057d99b8c
devrim yasar <devrim@koding.com>
2014-09-29 11:15:34 -0700

    new tests repo as a native folder.
    fuck submodules.

--- nw/node_modules/faker/examples/library_test.js
+++
@@ -1,9 +0,0 @@
-var util = require('util');
-
-var definitions = require('../lib/definitions');
-
-var faker = require('../index');
-
-var card = faker.Helpers.createCard();
-
-util.puts(JSON.stringify(card));
