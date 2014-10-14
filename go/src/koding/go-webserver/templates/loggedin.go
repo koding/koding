ac01d3cdc5c6749069636f30903a5dfb74fad280
Senthil <senthil@koding.com>
2014-10-14 15:01:23 -0700

    payment: fix formatting

--- go/src/koding/go-webserver/templates/loggedin.go
+++ go/src/koding/go-webserver/templates/loggedin.go
@@ -59,7 +59,7 @@ var LoggedInHome = `
 
   <script type="text/javascript">
     var _user_id = '%s'; var _session_id = '%s'; var _sift = _sift || []; _sift.push(['_setAccount', 'f270274999']); _sift.push(['_setUserId', _user_id]); _sift.push(['_setSessionId', _session_id]); _sift.push(['_trackPageview']); (function() { function ls() { var e = document.createElement('script'); e.type = 'text/javascript'; e.async = true; e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s); } if (window.attachEvent) { window.attachEvent('onload', ls); } else { window.addEventListener('load', ls, false); } })();
-	</script>
+  </script>
 </body>
 </html>
 `
