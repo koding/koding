8c155a4513bca77d6ae39b083df9a27835779276
Senthil <senthil@koding.com>
2014-10-14 12:03:06 -0700

    payment: fix script formatting

--- go/src/koding/go-webserver/templates/loggedin.go
+++ go/src/koding/go-webserver/templates/loggedin.go
@@ -57,30 +57,8 @@ var LoggedInHome = `
     ga('create', 'UA-6520910-8', 'auto');
   </script>
 
-	<script type="text/javascript">
-		var _user_id = '%s'; // IMPORTANT! Set to the user's ID, username, or email address, or '' if not yet known.
-		var _session_id = '%s'; // Set to a unique session ID for the visitor's current browsing session.
-
-		var _sift = _sift || [];
-		_sift.push(['_setAccount', 'f270274999']);
-		_sift.push(['_setUserId', _user_id]);
-		_sift.push(['_setSessionId', _session_id]);
-		_sift.push(['_trackPageview']);
-		(function() {
-			function ls() {
-				var e = document.createElement('script');
-				e.type = 'text/javascript';
-				e.async = true;
-				e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js';
-				var s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(e, s);
-			}
-			if (window.attachEvent) {
-				window.attachEvent('onload', ls);
-			} else {
-				window.addEventListener('load', ls, false);
-			}
-		})();
+  <script type="text/javascript">
+    var _user_id = '%s'; var _session_id = '%s'; var _sift = _sift || []; _sift.push(['_setAccount', 'f270274999']); _sift.push(['_setUserId', _user_id]); _sift.push(['_setSessionId', _session_id]); _sift.push(['_trackPageview']); (function() { function ls() { var e = document.createElement('script'); e.type = 'text/javascript'; e.async = true; e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s); } if (window.attachEvent) { window.attachEvent('onload', ls); } else { window.addEventListener('load', ls, false); } })();
 	</script>
 </body>
 </html>
