576dc4852d2f3037dc1e1c6c40969707c7353375
Senthil <senthil@koding.com>
2014-10-16 13:42:41 -0700

    payment: remove unnecessary test data

--- go/src/socialapi/workers/payment/stripe/webhook_test.go
+++ go/src/socialapi/workers/payment/stripe/webhook_test.go
@@ -11,40 +11,7 @@ import (
 )
 
 func rawSubscriptionDeletedData(id string) []byte {
-	raw := `{
-		"id": "%s",
-		"plan": {
-			"interval": "month",
-			"name": "Free plan",
-			"created": 1410394341,
-			"amount": 0,
-			"currency": "usd",
-			"id": "1_00000000000000",
-			"object": "plan",
-			"livemode": false,
-			"interval_count": 1,
-			"trial_period_days": null,
-			"metadata": {},
-			"statement_description": "0 plan"
-		},
-		"object": "subscription",
-		"start": 1411683076,
-		"status": "canceled",
-		"customer": "cus_00000000000000",
-		"cancel_at_period_end": false,
-		"current_period_start": 1411683076,
-		"current_period_end": 1414275076,
-		"ended_at": 1411681278,
-		"trial_start": null,
-		"trial_end": null,
-		"canceled_at": null,
-		"quantity": 1,
-		"application_fee_percent": null,
-		"discount": null,
-		"metadata": {}
-	}
-`
-
+	raw := `{ "id": "%s", }`
 	data := fmt.Sprintf(raw, id)
 
 	return []byte(data)
