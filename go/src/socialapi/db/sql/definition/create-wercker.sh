774d6ffc33f88caa0f864803531ffb88fd63a67e
Senthil <senthil@koding.com>
2014-10-21 17:00:11 -0700

    ...

--- go/src/socialapi/db/sql/definition/create-wercker.sh
+++ go/src/socialapi/db/sql/definition/create-wercker.sh
@@ -60,3 +60,6 @@ psql $WERCKER_POSTGRESQL_URL < $1/payment_definition/004-table.sql
 
 # create constraints
 psql $WERCKER_POSTGRESQL_URL < $1/payment_definition/005-constraint.sql
+
+# modifications
+psql $WERCKER_POSTGRESQL_URL < $1/payment_definition/modifications/001-add-koding-to-enum.sql
