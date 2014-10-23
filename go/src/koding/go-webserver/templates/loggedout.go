e48ea5862625ebb426d86b0e34d932e0a7371314
Senthil <senthil@koding.com>
2014-10-23 14:53:46 -0700

    go-webserver: fix formatting

--- go/src/koding/go-webserver/templates/loggedout.go
+++ go/src/koding/go-webserver/templates/loggedout.go
@@ -2,33 +2,32 @@ package templates
 
 var LoggedOutHome = `
 <!doctype html>
-<html lang="en">
-<head>
-  {{template "header" . }}
+  <html lang="en">
+  <head>
+    {{template "header" . }}
 
-  <link rel="stylesheet" href="/a/site.landing/css/kd.css?{{.Version}}" />
-  <link rel="stylesheet" href="/a/site.landing/css/main.css?{{.Version}}" />
-</head>
-<body class='home'>
+    <link rel="stylesheet" href="/a/site.landing/css/kd.css?{{.Version}}" />
+    <link rel="stylesheet" href="/a/site.landing/css/main.css?{{.Version}}" />
+  </head>
 
-  <!--[if IE]><script>(function(){window.location.href='/unsupported.html'})();</script><![endif]-->
+  <body class='home'>
+    <!--[if IE]><script>(function(){window.location.href='/unsupported.html'})();</script><![endif]-->
+    <script src="/a/site.landing/js/libs.js?{{.Version}}"></script>
+    <script src="/a/site.landing/js/kd.libs.js?{{.Version}}"></script>
+    <script src="/a/site.landing/js/kd.js?{{.Version}}"></script>
+    <script src="/a/site.landing/js/main.js?{{.Version}}"></script>
 
-  <script src="/a/site.landing/js/libs.js?{{.Version}}"></script>
-  <script src="/a/site.landing/js/kd.libs.js?{{.Version}}"></script>
-  <script src="/a/site.landing/js/kd.js?{{.Version}}"></script>
-  <script src="/a/site.landing/js/main.js?{{.Version}}"></script>
+    <script>
+      (function(d) {
+        var config = {
+          kitId: 'rbd0tum',
+          scriptTimeout: 3000
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    </script>
 
-  <script>
-    (function(d) {
-      var config = {
-        kitId: 'rbd0tum',
-        scriptTimeout: 3000
-      },
-      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-    })(document);
-  </script>
-
-  {{template "analytics"}}
-</body>
+    {{template "analytics"}}
+  </body>
 </html>
 `
