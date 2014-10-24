86513cb1457fc4ed73f16c4ae0022d166f69d060
Senthil <senthil@koding.com>
2014-10-23 23:56:38 -0700

    go-webserver: add comments

--- go/src/koding/go-webserver/home.go
+++ go/src/koding/go-webserver/home.go
@@ -24,6 +24,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	user.Set("SessionId", userInfo.ClientId)
 	user.Set("Impersonating", userInfo.Impersonating)
 
+	// 4 goroutines below will work in parallel & send results
 	go collectItems(user, onItem, onDone, 4)
 
 	go sendAccount(userInfo.Account, outputter)
@@ -31,12 +32,13 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	go fetchWorkspaces(userInfo.AccountId, outputter)
 	go fetchSocial(userInfo.SocialApiId, outputter)
 
-	timer := time.NewTimer(time.Second * 2)
+	// return in 500ms regardless and let client get what it wants
+	timeout := time.NewTimer(time.Millisecond * 500)
 
 	select {
 	case <-onError:
 		writeLoggedOutHomeToResp(w)
-	case <-timer.C:
+	case <-timeout.C:
 		writeLoggedInHomeToResp(w, user)
 	case <-onDone:
 		writeLoggedInHomeToResp(w, user)
