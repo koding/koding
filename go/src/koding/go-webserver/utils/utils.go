1d2262adb4c8b6dbe9b0624ec19ca77ab013c578
Gokmen Goksel <gokmen@goksel.me>
2014-10-27 23:45:01 +0200

    Update utils.go with gofmt

--- go/src/koding/go-webserver/utils/utils.go
+++ go/src/koding/go-webserver/utils/utils.go
@@ -1,8 +1,8 @@
 package utils
 
 import (
-  "strings"
-  "net/http"
+	"net/http"
+	"strings"
 )
 
 type ipAddress string
@@ -13,28 +13,28 @@ type ipAddress string
 // Request.RemoteAddress contains port, which we want to remove i.e.:
 // "[::1]:58292" => "[::1]"
 func ipAddrFromRemoteAddr(s string) string {
-  idx := strings.LastIndex(s, ":")
-  if idx == -1 {
-    return s
-  }
-  return s[:idx]
+	idx := strings.LastIndex(s, ":")
+	if idx == -1 {
+		return s
+	}
+	return s[:idx]
 }
 
 func GetIpAddress(r *http.Request) string {
-  hdr := r.Header
-  hdrRealIp := hdr.Get("X-Real-Ip")
-  hdrForwardedFor := hdr.Get("X-Forwarded-For")
-  if hdrRealIp == "" && hdrForwardedFor == "" {
-    return ipAddrFromRemoteAddr(r.RemoteAddr)
-  }
-  if hdrForwardedFor != "" {
-    // X-Forwarded-For is potentially a list of addresses separated with ","
-    parts := strings.Split(hdrForwardedFor, ",")
-    for i, p := range parts {
-      parts[i] = strings.TrimSpace(p)
-    }
-    // TODO: should return first non-local address
-    return parts[0]
-  }
-  return hdrRealIp
+	hdr := r.Header
+	hdrRealIp := hdr.Get("X-Real-Ip")
+	hdrForwardedFor := hdr.Get("X-Forwarded-For")
+	if hdrRealIp == "" && hdrForwardedFor == "" {
+		return ipAddrFromRemoteAddr(r.RemoteAddr)
+	}
+	if hdrForwardedFor != "" {
+		// X-Forwarded-For is potentially a list of addresses separated with ","
+		parts := strings.Split(hdrForwardedFor, ",")
+		for i, p := range parts {
+			parts[i] = strings.TrimSpace(p)
+		}
+		// TODO: should return first non-local address
+		return parts[0]
+	}
+	return hdrRealIp
 }
