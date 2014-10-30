337e495fdaee21a19f29a9a628f3e678357119a0
Senthil <senthil@koding.com>
2014-10-30 16:26:26 -0700

    payment: add forked paypal library

---
+++ go/src/github.com/koding/paypal/paypal.go
@@ -0,0 +1,253 @@
+package paypal
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	NVP_SANDBOX_URL         = "https://api-3t.sandbox.paypal.com/nvp"
+	NVP_PRODUCTION_URL      = "https://api-3t.paypal.com/nvp"
+	CHECKOUT_SANDBOX_URL    = "https://www.sandbox.paypal.com/cgi-bin/webscr"
+	CHECKOUT_PRODUCTION_URL = "https://www.paypal.com/cgi-bin/webscr"
+	NVP_VERSION             = "84"
+)
+
+type PayPalClient struct {
+	username    string
+	password    string
+	signature   string
+	usesSandbox bool
+	client      *http.Client
+}
+
+type PayPalDigitalGood struct {
+	Name     string
+	Amount   float64
+	Quantity int16
+}
+
+type PayPalResponse struct {
+	Ack           string
+	CorrelationId string
+	Timestamp     string
+	Version       string
+	Build         string
+	Values        url.Values
+	usedSandbox   bool
+}
+
+type PayPalError struct {
+	Ack          string
+	ErrorCode    string
+	ShortMessage string
+	LongMessage  string
+	SeverityCode string
+}
+
+func (e *PayPalError) Error() string {
+	var message string
+	if len(e.ErrorCode) != 0 && len(e.ShortMessage) != 0 {
+		message = "PayPal Error " + e.ErrorCode + ": " + e.ShortMessage
+	} else if len(e.Ack) != 0 {
+		message = e.Ack
+	} else {
+		message = "PayPal is undergoing maintenance.\nPlease try again later."
+	}
+
+	return message
+}
+
+func (r *PayPalResponse) CheckoutUrl() string {
+	query := url.Values{}
+	query.Set("cmd", "_express-checkout")
+	query.Add("token", r.Values["TOKEN"][0])
+	checkoutUrl := CHECKOUT_PRODUCTION_URL
+	if r.usedSandbox {
+		checkoutUrl = CHECKOUT_SANDBOX_URL
+	}
+	return fmt.Sprintf("%s?%s", checkoutUrl, query.Encode())
+}
+
+func SumPayPalDigitalGoodAmounts(goods *[]PayPalDigitalGood) (sum float64) {
+	for _, dg := range *goods {
+		sum += dg.Amount * float64(dg.Quantity)
+	}
+	return
+}
+
+func NewDefaultClient(username, password, signature string, usesSandbox bool) *PayPalClient {
+	return &PayPalClient{username, password, signature, usesSandbox, new(http.Client)}
+}
+
+func NewClient(username, password, signature string, usesSandbox bool, client *http.Client) *PayPalClient {
+	return &PayPalClient{username, password, signature, usesSandbox, client}
+}
+
+func (pClient *PayPalClient) PerformRequest(values url.Values) (*PayPalResponse, error) {
+	values.Add("USER", pClient.username)
+	values.Add("PWD", pClient.password)
+	values.Add("SIGNATURE", pClient.signature)
+	values.Add("VERSION", NVP_VERSION)
+
+	endpoint := NVP_PRODUCTION_URL
+	if pClient.usesSandbox {
+		endpoint = NVP_SANDBOX_URL
+	}
+
+	formResponse, err := pClient.client.PostForm(endpoint, values)
+	if err != nil {
+		return nil, err
+	}
+	defer formResponse.Body.Close()
+
+	body, err := ioutil.ReadAll(formResponse.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	responseValues, err := url.ParseQuery(string(body))
+	response := &PayPalResponse{usedSandbox: pClient.usesSandbox}
+
+	if err == nil {
+		response.Ack = responseValues.Get("ACK")
+		response.CorrelationId = responseValues.Get("CORRELATIONID")
+		response.Timestamp = responseValues.Get("TIMESTAMP")
+		response.Version = responseValues.Get("VERSION")
+		response.Build = responseValues.Get("2975009")
+		response.Values = responseValues
+
+		errorCode := responseValues.Get("L_ERRORCODE0")
+		if len(errorCode) != 0 || strings.ToLower(response.Ack) == "failure" || strings.ToLower(response.Ack) == "failurewithwarning" {
+			pError := new(PayPalError)
+			pError.Ack = response.Ack
+			pError.ErrorCode = errorCode
+			pError.ShortMessage = responseValues.Get("L_SHORTMESSAGE0")
+			pError.LongMessage = responseValues.Get("L_LONGMESSAGE0")
+			pError.SeverityCode = responseValues.Get("L_SEVERITYCODE0")
+
+			err = pError
+		}
+	}
+
+	return response, err
+}
+
+type ExpressCheckoutSingleArgs struct {
+	Amount                             float64
+	CurrencyCode, ReturnURL, CancelURL string
+	Recurring                          bool
+	Item                               *PayPalDigitalGood
+}
+
+func (pClient *PayPalClient) SetExpressCheckoutDigitalGoods(paymentAmount float64, currencyCode string, returnURL, cancelURL string, goods []PayPalDigitalGood) (*PayPalResponse, error) {
+	values := url.Values{}
+	values.Set("METHOD", "SetExpressCheckout")
+	values.Add("PAYMENTREQUEST_0_AMT", "0")
+	values.Add("NOSHIPPING", "1")
+
+	values.Add("L_BILLINGTYPE0", "RecurringPayments")
+
+	values.Add("RETURNURL", returnURL)
+	values.Add("CANCELURL", cancelURL)
+
+	for i := 0; i < len(goods); i++ {
+		good := goods[i]
+
+		values.Add(fmt.Sprintf("%s%d", "L_PAYMENTREQUEST_0_NAME", i), good.Name)
+		values.Add(fmt.Sprintf("%s%d", "L_PAYMENTREQUEST_0_AMT", i), "0")
+		values.Add(fmt.Sprintf("%s%d", "L_PAYMENTREQUEST_0_QTY", i), fmt.Sprintf("%d", good.Quantity))
+		values.Add(fmt.Sprintf("%s%d", "L_PAYMENTREQUEST_0_ITEMCATEGORY", i), "Digital")
+	}
+
+	return pClient.PerformRequest(values)
+}
+
+// Convenience function for Sale (Charge)
+func (pClient *PayPalClient) DoExpressCheckoutSale(token, payerId, currencyCode string, finalPaymentAmount float64) (*PayPalResponse, error) {
+	return pClient.DoExpressCheckoutPayment(token, payerId, "Sale", currencyCode, finalPaymentAmount)
+}
+
+// paymentType can be "Sale" or "Authorization" or "Order" (ship later)
+func (pClient *PayPalClient) DoExpressCheckoutPayment(token, payerId, paymentType, currencyCode string, finalPaymentAmount float64) (*PayPalResponse, error) {
+	values := url.Values{}
+	values.Set("METHOD", "DoExpressCheckoutPayment")
+	values.Add("TOKEN", token)
+	values.Add("PAYERID", payerId)
+	values.Add("PAYMENTREQUEST_0_PAYMENTACTION", paymentType)
+	values.Add("PAYMENTREQUEST_0_CURRENCYCODE", currencyCode)
+	values.Add("PAYMENTREQUEST_0_AMT", fmt.Sprintf("%.2f", finalPaymentAmount))
+
+	return pClient.PerformRequest(values)
+}
+
+func (pClient *PayPalClient) GetExpressCheckoutDetails(token string) (*PayPalResponse, error) {
+	values := url.Values{}
+	values.Add("TOKEN", token)
+	values.Set("METHOD", "GetExpressCheckoutDetails")
+
+	return pClient.PerformRequest(values)
+}
+
+func (pClient *PayPalClient) CreateRecurringPaymentsProfile(token string, params map[string]string) (*PayPalResponse, error) {
+	values := url.Values{}
+	values.Add("TOKEN", token)
+	values.Set("METHOD", "CreateRecurringPaymentsProfile")
+
+	for key, value := range params {
+		values.Add(key, value)
+	}
+
+	return pClient.PerformRequest(values)
+}
+
+func (pClient *PayPalClient) BillOutstandingAmount(profileId string) (*PayPalResponse, error) {
+	values := url.Values{}
+	values.Set("METHOD", "BillOutstandingAmount")
+	values.Set("PROFILEID", profileId)
+
+	return pClient.PerformRequest(values)
+}
+
+//----------------------------------------------------------
+// Forked
+//----------------------------------------------------------
+
+func NewDigitalGood(name string, amount float64) *PayPalDigitalGood {
+	return &PayPalDigitalGood{
+		Name:     name,
+		Amount:   amount,
+		Quantity: 1,
+	}
+}
+
+func NewExpressCheckoutSingleArgs() *ExpressCheckoutSingleArgs {
+	return &ExpressCheckoutSingleArgs{
+		Amount:       0,
+		CurrencyCode: "USD",
+		Recurring:    true,
+	}
+}
+
+func (pClient *PayPalClient) SetExpressCheckoutSingle(args *ExpressCheckoutSingleArgs) (*PayPalResponse, error) {
+	values := url.Values{}
+	values.Set("METHOD", "SetExpressCheckout")
+	values.Add("PAYMENTREQUEST_0_AMT", fmt.Sprintf("%.2f", args.Amount))
+	values.Add("NOSHIPPING", "1")
+
+	values.Add("L_BILLINGTYPE0", "RecurringPayments")
+	values.Add("L_BILLINGAGREEMENTDESCRIPTION0", args.Item.Name)
+
+	values.Add("RETURNURL", args.ReturnURL)
+	values.Add("CANCELURL", args.CancelURL)
+
+	values.Add("L_PAYMENTREQUEST_0_NAME0", args.Item.Name)
+	values.Add("L_PAYMENTREQUEST_0_AMT0", fmt.Sprintf("%.2f", args.Amount))
+	values.Add("L_PAYMENTREQUEST_0_QTY0", "1")
+	values.Add("L_PAYMENTREQUEST_0_ITEMCATEGORY0", "Digital")
+
+	return pClient.PerformRequest(values)
+}
