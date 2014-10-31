c743f9a4168351572e89085478da75626410205e
Cihangir SAVAS <cihangir@koding.com>
2014-10-31 18:46:58 +0200

    Scripts: update curl

--- scripts/notify-cebeci.sh
+++ scripts/notify-cebeci.sh
@@ -6,4 +6,4 @@ MESSAGE=$3
 STATUS=$4
 PERCENTAGE=$5
 
-curl -X POST -d "name=$NAME&sha=$SHA&message=$MESSAGE&status=$STATUS&percentage=$PERCENTAGE" http://cihangir.ngrok.com/hook/wercker/incoming
+curl -s --connect-timeout 2 -X POST -d "name=$NAME&sha=$SHA&message=$MESSAGE&status=$STATUS&percentage=$PERCENTAGE" http://cihangir.ngrok.com/hook/wercker/incoming >> /dev/null
