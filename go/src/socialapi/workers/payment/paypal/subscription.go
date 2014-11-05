1251bba86b59c22b84afe04d3357589451cb11af
Senthil <senthil@koding.com>
2014-11-04 18:26:35 -0800

    payment: build item name with plan title and interval

--- go/src/socialapi/workers/payment/paypal/subscription.go
+++ go/src/socialapi/workers/payment/paypal/subscription.go
@@ -1,24 +1,13 @@
 package paypal
 
 import (
+	"fmt"
 	"socialapi/workers/payment/paymentmodels"
 	"time"
-
-	"github.com/koding/paypal"
 )
 
 func CreateSubscription(token string, plan *paymentmodels.Plan, customer *paymentmodels.Customer) error {
-	args := paypal.NewExpressCheckoutSingleArgs()
-	args.ReturnURL = returnURL
-	args.CancelURL = cancelURL
-	args.Item = paypal.NewDigitalGood(plan.Title, 0)
-
-	response, err := client.SetExpressCheckoutSingle(args)
-	err = handlePaypalErr(response, err)
-	if err != nil {
-		return err
-	}
-
+	digitalGoodName := fmt.Sprintf("%s-%s", plan.Title, plan.Interval)
 	params := map[string]string{
 		"PROFILESTARTDATE": time.Now().String(),
 		"SUBSCRIBERNAME":   customer.OldId,
@@ -26,11 +15,11 @@ func CreateSubscription(token string, plan *paymentmodels.Plan, customer *paymen
 		"AMT":              normalizeAmount(plan.AmountInCents),
 		"BILLINGFREQUENCY": "1",
 		"CURRENCYCODE":     CurrencyCode,
-		"DESC":             plan.Title,
+		"DESC":             digitalGoodName,
 		"AUTOBILLOUTAMT":   "AddToNextBilling",
 	}
 
-	response, err = client.CreateRecurringPaymentsProfile(token, params)
+	response, err := client.CreateRecurringPaymentsProfile(token, params)
 	err = handlePaypalErr(response, err)
 	if err != nil {
 		return err
