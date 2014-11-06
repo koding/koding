25b0e1c30b9de4e6ac6a61937e4a80fb4af52cf1
Cihangir SAVAS <cihangir@koding.com>
2014-11-06 12:44:32 +0200

    Wercker: update notify script

--- scripts/notify-cebeci.sh
+++ scripts/notify-cebeci.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+version=$(cat /var/app/current/VERSION || echo $WERCKER_GIT_COMMIT || echo "0")
 
+SHA=${version:0:8}
+BRANCH=$WERCKER_GIT_BRANCH
 NAME=$1
-SHA=$2
-MESSAGE=$3
-STATUS=$4
-PERCENTAGE=$5
+MESSAGE=$2
+STATUS=$3
+PERCENTAGE=$4
 
-curl -s --connect-timeout 2 -X POST -d "name=$NAME&sha=$SHA&message=$MESSAGE&status=$STATUS&percentage=$PERCENTAGE" http://cihangir.ngrok.com/hook/wercker/incoming >> /dev/null
+ENV_NAME="wercker"
+
+if [ ! -n "$EB_ENV_NAME" ]; then
+  ENV_NAME=$EB_ENV_NAME
+fi
+
+curl -s --connect-timeout 2 -X POST -d "name=$NAME&sha=$SHA&message=$MESSAGE&status=$STATUS&percentage=$PERCENTAGE&branch=$BRANCH&env=$ENV_NAME" http://cihangir.ngrok.com/hook/wercker/incoming >> /dev/null
