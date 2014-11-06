6215329ab66a5b154a519fb66cc633eae853b216
Senthil <senthil@koding.com>
2014-11-06 11:29:38 -0800

    payment: minor refactoring

--- go/src/socialapi/workers/payment/paypal/plan.go
+++ go/src/socialapi/workers/payment/paypal/plan.go
@@ -2,7 +2,6 @@ package paypal
 
 import (
 	"errors"
-	"socialapi/workers/payment/paymenterrors"
 	"socialapi/workers/payment/paymentmodels"
 )
 
@@ -13,20 +12,16 @@ func FindPlanFromToken(token string) (*paymentmodels.Plan, error) {
 		return nil, err
 	}
 
-	planTitleAndInterval := resp.Values.Get("L_PAYMENTREQUEST_0_NAME0")
-	if planTitleAndInterval == "" {
+	planInfo := resp.Values.Get("L_PAYMENTREQUEST_0_NAME0")
+	if planInfo == "" {
 		return nil, errors.New("no plan title or interval in paypal token")
 	}
 
-	planTitle, planInterval := parsePlanInfo(planTitleAndInterval)
-	plan := paymentmodels.NewPlan()
+	planTitle, planInterval := parsePlanInfo(planInfo)
 
+	plan := paymentmodels.NewPlan()
 	err = plan.ByTitleAndInterval(planTitle, planInterval)
 	if err != nil {
-		if paymenterrors.IsPlanNotFoundErr(err) {
-			return nil, paymenterrors.ErrPlanNotFound
-		}
-
 		return nil, err
 	}
 
