d65faabff658d2c32f173f9914a515ca0a41bd25
Senthil <senthil@koding.com>
2014-11-11 12:56:05 -0800

    payments: don't log user not found for getting cc, invoices

--- go/src/socialapi/workers/payment/payment.go
+++ go/src/socialapi/workers/payment/payment.go
@@ -128,7 +128,7 @@ func (a *AccountRequest) Subscriptions() (*SubscriptionsResponse, error) {
 
 func (a *AccountRequest) Invoices() ([]*stripe.StripeInvoiceResponse, error) {
 	invoices, err := stripe.FindInvoicesForCustomer(a.AccountId)
-	if err != nil {
+	if err != nil && err != paymenterrors.ErrCustomerNotFound {
 		Log.Error("Fetching invoices for account: %s failed. %s", a.AccountId, err)
 	}
 
@@ -137,7 +137,7 @@ func (a *AccountRequest) Invoices() ([]*stripe.StripeInvoiceResponse, error) {
 
 func (a *AccountRequest) CreditCard() (*stripe.CreditCardResponse, error) {
 	resp, err := stripe.GetCreditCard(a.AccountId)
-	if err != nil {
+	if err != nil && err != paymenterrors.ErrCustomerNotFound {
 		Log.Error("Fetching cc for account: %s failed. %s", a.AccountId, err)
 	}
 
