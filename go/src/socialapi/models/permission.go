63c0d21897b14f20621e86dc47954a34408438cf
Cihangir SAVAS <cihangir@koding.com>
2014-11-13 17:02:02 +0200

    Social: added new constants for permission table
    Signed-off-by: Cihangir SAVAS <cihangir@koding.com>

--- go/src/socialapi/models/permission.go
+++ go/src/socialapi/models/permission.go
@@ -1,6 +1,18 @@
 package models
 
 import "time"
+var (
+	Permission_ROLE_SUPERADMIN = "superadmin"
+	Permission_ROLE_ADMIN      = "admin"
+	Permission_ROLE_MODERATOR  = "moderator"
+	Permission_ROLE_MEMBER     = "member"
+	Permission_ROLE_GUEST      = "guest"
+)
+
+var (
+	Permission_STATUS_ALLOWED    = "allowed"
+	Permission_STATUS_DISALLOWED = "disallowed"
+)
 
 type Permission struct {
 	// unique identifier of the channel
