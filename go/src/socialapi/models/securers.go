430cc4ba6059271e110c5c5ea5414a49fe6ab273
Cihangir SAVAS <cihangir@koding.com>
2014-11-13 17:05:50 +0200

    Social: implemented channel securer function
    Signed-off-by: Cihangir SAVAS <cihangir@koding.com>

--- go/src/socialapi/models/securers.go
+++ go/src/socialapi/models/securers.go
@@ -1,6 +1,35 @@
 package models
 
-func ChannelSecurer(permissionName string, request *Channel, context *Context) error {
+import "errors"
+
+func ChannelSecurer(permissionName string, c *Channel, context *Context) error {
+	if c.Id == 0 {
+		return errors.New("request is not valid")
+	}
+
+	var accountId int64
+	// if user is logged in, get their account id
+	if context.IsLoggedIn() {
+		accountId = context.Client.Account.Id
+	}
+
+	// to do add caching
+	cp := NewChannelParticipant()
+	cp.ChannelId = c.Id
+	cp.AccountId = accountId
+	role, err := cp.FetchRole()
+	if err != nil {
+		return err
+	}
+
+	p := NewPermission()
+	p.ChannelId = c.Id
+	p.RoleConstant = role
+
+	if err := p.EnsureAllowance(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
