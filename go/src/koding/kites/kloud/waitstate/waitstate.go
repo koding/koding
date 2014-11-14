6fcf9e52ba27f4ca01d77edf04d7730fa1fb4166
H. İbrahim Güngör <igungor@gmail.com>
2014-11-14 12:23:00 +0200

    kloud: Decrease AWS polling for state changes.
    Eventer asks for the machine state for every 3 seconds. Send fake events
    more and poll AWS state less.

--- go/src/koding/kites/kloud/waitstate/waitstate.go
+++ go/src/koding/kites/kloud/waitstate/waitstate.go
@@ -12,11 +12,12 @@ var ErrWaitTimeout = errors.New("timeout while waiting for state")
 
 // WaitState is used to track the state of a given process.
 type WaitState struct {
-	StateFunc     func(int) (machinestate.State, error)
-	DesiredState  machinestate.State
-	Timeout       time.Duration
-	Interval      time.Duration
-	Start, Finish int
+	StateFunc       func(int) (machinestate.State, error)
+	DesiredState    machinestate.State
+	Timeout         time.Duration
+	EventerInterval time.Duration
+	PollerInterval  time.Duration
+	Start, Finish   int
 }
 
 // Wait calls the StateFunc with the specified interval and waits until it
@@ -27,8 +28,12 @@ func (w *WaitState) Wait() error {
 		w.Finish = 100
 	}
 
-	if w.Interval == 0 {
-		w.Interval = 3 * time.Second
+	if w.EventerInterval == 0 {
+		w.EventerInterval = 3 * time.Second
+	}
+
+	if w.PollerInterval == 0 {
+		w.PollerInterval = 30 * time.Second
 	}
 
 	if w.Timeout == 0 {
@@ -37,25 +42,30 @@ func (w *WaitState) Wait() error {
 
 	timeout := time.After(w.Timeout)
 
-	ticker := time.NewTicker(w.Interval)
-	defer ticker.Stop()
+	eventTicker := time.NewTicker(w.EventerInterval)
+	pollTicker := time.NewTicker(w.PollerInterval)
+	defer eventTicker.Stop()
+	defer pollTicker.Stop()
+
+	var err error
+	var state machinestate.State
 
 	for {
 		select {
-		case <-ticker.C:
+		case <-eventTicker.C:
 			if w.Start < w.Finish {
 				w.Start += 2
 			}
-
-			state, err := w.StateFunc(w.Start)
-			if err != nil {
-				return err
-			}
-
 			if state == w.DesiredState {
 				return nil
 			}
-		case <-time.After(time.Second * 5):
+		// Poll less, send events more.
+		case <-pollTicker.C:
+			state, err = w.StateFunc(w.Start)
+			if err != nil {
+				return err
+			}
+		case <-time.After(time.Second * 40):
 			// cancel the current ongoing process if it takes to long
 			fmt.Println("Canceling current event asking")
 			continue
