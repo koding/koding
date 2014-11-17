6a350b80006e408b2fd215e10e8b99c194353ccf
Can Yucel <can@koding.com>
2014-11-17 01:50:18 -0800

    email: add new templates

---
+++ go/src/socialapi/workers/email/templates/content.go
@@ -0,0 +1,37 @@
+package templates
+
+const MessageGroup = `
+<tr>
+  <td width="24px" valign="top">
+    {{template "gravatar" .}}
+  </td>
+  <td style="padding: 24px 0 0 12px; line-height: 18px;">
+    <b><a href="{{.Hostname}}/{{.Nickname}}" style="text-decoration: none; color:#222">{{.Nickname}}</a></b>{{.Title}}
+    <div style="border-top: 1px solid #eaeaea; margin: 6px 0 0;"></iv>
+    {{.Summary}}
+  </td>
+</tr>
+`
+
+const Message = `
+{{define "message" }}
+<div style="margin:6px 0 0;">
+  <span style="color: #a5a5a5; font-weight: 200; margin: 0 6px 0 0;">{{.Time}}</span>
+  {{.Body}}
+</div>
+{{end}}
+`
+
+const Gravatar = `
+{{define "gravatar"}}
+<img width="24px" height="24px" style="border-radius: 3px; padding-top: 24px;"
+    src="https://gravatar.com/avatar/{{.Hash}}?size=24&d=https%3A%2F%2Fkoding-cdn.s3.amazonaws.com%2Fimages%2Fdefault.avatar.24.png" />
+{{end}}
+`
+
+const Channel = `
+{{.Title}}
+<table width="100%" style="margin: 12px 0; font-size: 14px; color: #353535;" cellpadding="0" cellspacing="0">
+  {{.Summary}}
+</table>
+`
---
+++ go/src/socialapi/workers/email/templates/layout.go
@@ -0,0 +1,37 @@
+package templates
+
+const Layout = `
+<html>
+	<head>
+	  <meta charset="UTF-8">
+	  <title>[Koding] {{.Title}}</title>
+	</head>
+	<body style="background: #eeeeee; margin: 0; padding: 45px; font-family: 'Helvetica Neue', Helvetica, sans-serif;">
+		<style>
+	    .m-footer a {
+	      color: #bababa;
+	    }
+  	</style>
+  	<div style="background: #fff; max-width: 600px; margin: 0 auto; border: 1px solid #dcdcdc">
+	    <div style="padding: 8% 20px 6%; background: #fafafa; text-align:center;">
+	      <img src="https://koding.s3.amazonaws.com/images/email-logo.png" alt="Koding" width="142px" />
+	    </div>
+
+      <div style="border-top: 1px solid #dcdcdc; padding: 22px; font-size: 14px; color: #353535; line-height: 18px;">
+        <h4 style="color: #18ad5c; font-size: 18px; font-weight: 600; display: block; margin: 10px 0;">Hi {{.FirstName}},</h4>
+  	    {{.Body}}
+      </div>
+
+      <!-- FOOTER -->
+      <div class="m-footer" style="background: #fafafa; border-top: 1px solid #dcdcdc; padding: 22px; font-size: 12px; color: #a4a4a4; line-height: 18px;">
+      {{if .ShowLink }}
+        Unsubscribe from <a href="{{.Hostname}}/Unsubscribe/{{.Token}}/{{.RecipientEmail}}">{{.ContentType}}</a> notifications /
+      {{end}}
+        Unsubscribe from <a href="{{.Hostname}}/Unsubscribe/{{.Token}}/{{.RecipientEmail}}/all">all</a> emails from Koding.
+        <br/>
+        <a href="{{.Hostname}}">Koding</a>, Inc. 358 Brannan, San Francisco, CA 94107
+      </div>
+    </div>
+	</body>
+</html>
+`
