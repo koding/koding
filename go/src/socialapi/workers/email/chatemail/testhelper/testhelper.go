cd7d2a3fc99ae34d8c99b331b01d17acd848b5ac
Can Yucel <can@koding.com>
2014-11-17 01:55:28 -0800

    Email: Add testhelper for further usage

---
+++ go/src/socialapi/workers/email/chatemail/testhelper/testhelper.go
@@ -0,0 +1,41 @@
+package testhelper
+
+import (
+	"fmt"
+	"socialapi/config"
+	"socialapi/workers/email/chatemail/common"
+
+	"github.com/koding/redis"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func deleteKeys(redisConn *redis.RedisSession, pattern string) {
+	keys, _ := redisConn.Keys(pattern)
+
+	for _, key := range keys {
+		val, _ := redisConn.String(key)
+		redisConn.Del(val)
+	}
+}
+
+func ResetCache(redisConn *redis.RedisSession) {
+	redisConn.Del(common.AccountNextPeriodHashSetKey())
+	deleteKeys(redisConn, allPeriodAccountSetKey())
+	deleteKeys(redisConn, AllAccountChannelHashSetKey())
+}
+
+func AllAccountChannelHashSetKey() string {
+	return fmt.Sprintf("%s:%s:%s:*",
+		config.MustGet().Environment,
+		common.CachePrefix,
+		"account-channelhashset",
+	)
+}
+
+func allPeriodAccountSetKey() string {
+	return fmt.Sprintf("%s:%s:%s:*",
+		config.MustGet().Environment,
+		common.CachePrefix,
+		"periodaccountset",
+	)
+}
