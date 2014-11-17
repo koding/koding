5d3a98a7574d20b25cbb2d391f1cb3976cbfb6b8
Cihangir SAVAS <cihangir@koding.com>
2014-11-17 17:47:22 +0200

    Go: update go-tigertonic

--- go/src/github.com/rcrowley/go-tigertonic/server_test.go
+++ go/src/github.com/rcrowley/go-tigertonic/server_test.go
@@ -1,6 +1,8 @@
 package tigertonic
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 	"testing"
 	"time"
@@ -23,17 +25,21 @@ func TestServerCATLS(t *testing.T) {
 func TestServerGracefulStop(t *testing.T) {
 	chT := make(chan time.Time, 1)
 	s := NewServer(
-		"127.0.0.1:8888",
+		"127.0.0.1:0",
 		http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 			time.Sleep(2 * time.Millisecond)
 			w.WriteHeader(http.StatusNoContent)
 			chT <- time.Now()
 		}),
 	)
-	go s.ListenAndServe()
+	l, err := net.Listen("tcp", s.Addr)
+	if nil != err {
+		t.Fatal(err)
+	}
+	go s.Serve(l)
 	chR := make(chan *http.Response, 1)
 	go func() {
-		rs, err := http.Get("http://127.0.0.1:8888/")
+		rs, err := http.Get(fmt.Sprintf("http://%s", l.Addr()))
 		if nil != err {
 			t.Fatal(err)
 		}
@@ -52,5 +58,71 @@ func TestServerGracefulStop(t *testing.T) {
 	rs := <-chR
 	if http.StatusNoContent != rs.StatusCode {
 		t.Fatal(rs)
+	}
+	if _, err := http.Get(fmt.Sprintf("http://%s", l.Addr())); nil == err {
+		t.Fatal("GET / should have failed after server stopped")
+	}
+}
+
+func TestServerGracefulStopKeepAlive(t *testing.T) {
+	s := NewServer(
+		"127.0.0.1:0",
+		http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusNoContent)
+		}),
+	)
+	l, err := net.Listen("tcp", s.Addr)
+	if nil != err {
+		t.Fatal(err)
+	}
+	go s.Serve(l)
+	if _, err := http.Get(fmt.Sprintf("http://%s", l.Addr())); nil != err {
+		t.Fatal(err)
+	}
+	then := time.Now()
+	s.Close()
+	now := time.Now()
+	if then.Add(510 * time.Millisecond).Before(now) {
+		t.Fatal("connection not closed quickly")
+	}
+	if _, err := http.Get(fmt.Sprintf("http://%s", l.Addr())); nil == err {
+		t.Fatal("GET / should have failed after server stopped")
+	}
+}
+
+func TestServerGracefulStopMulti(t *testing.T) {
+	s := NewServer(
+		"127.0.0.1:0",
+		http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusNoContent)
+		}),
+	)
+	l1, err := net.Listen("tcp", s.Addr)
+	if nil != err {
+		t.Fatal(err)
+	}
+	l2, err := net.Listen("tcp", s.Addr)
+	if nil != err {
+		t.Fatal(err)
+	}
+	go s.Serve(l1)
+	go s.Serve(l2)
+	if _, err := http.Get(fmt.Sprintf("http://%s", l1.Addr())); nil != err {
+		t.Fatal(err)
+	}
+	if _, err := http.Get(fmt.Sprintf("http://%s", l2.Addr())); nil != err {
+		t.Fatal(err)
+	}
+	then := time.Now()
+	s.Close()
+	now := time.Now()
+	if then.Add(510 * time.Millisecond).Before(now) {
+		t.Fatal("connection not closed quickly")
+	}
+	if _, err := http.Get(fmt.Sprintf("http://%s", l1.Addr())); nil == err {
+		t.Fatal("GET / should have failed after server stopped")
+	}
+	if _, err := http.Get(fmt.Sprintf("http://%s", l2.Addr())); nil == err {
+		t.Fatal("GET / should have failed after server stopped")
 	}
 }
