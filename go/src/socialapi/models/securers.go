fa0a3942da43b15632544bfaad61d6f57764efdb
Cihangir SAVAS <cihangir@koding.com>
2014-11-17 17:44:17 +0200

    Social: stub all kind of securers

--- go/src/socialapi/models/securers.go
+++ go/src/socialapi/models/securers.go
@@ -1,11 +1,9 @@
 package models
 
-import "errors"
-
 func ChannelSecurer(permissionName string, c *Channel, context *Context) error {
-	if c.Id == 0 {
-		return errors.New("request is not valid")
-	}
+
+	// TODO run this function
+	return nil
 
 	var accountId int64
 	// if user is logged in, get their account id
@@ -26,6 +24,7 @@ func ChannelSecurer(permissionName string, c *Channel, context *Context) error {
 	p.ChannelId = c.Id
 	p.RoleConstant = role
 
+	// check if the user is allowed to take action
 	if err := p.EnsureAllowance(); err != nil {
 		return err
 	}
@@ -33,10 +32,66 @@ func ChannelSecurer(permissionName string, c *Channel, context *Context) error {
 	return nil
 }
 
+func ChannelReadSecurer(permissionName string, context *Context) error {
+	return nil
+}
+
 func MessageSecurer(permissionName string, request *ChannelMessage, context *Context) error {
 	return nil
 }
 
+func MessageReadSecurer(permissionName string, context *Context) error {
+	return nil
+}
+
 func AccountSecurer(permissionName string, request *Account, context *Context) error {
+	return nil
+}
+
+func AccountReadSecurer(permissionName string, context *Context) error {
+	return nil
+}
+
+func InteractionSecurer(permissionName string, request *Interaction, context *Context) error {
+	return nil
+}
+
+func InteractionReadSecurer(permissionName string, context *Context) error {
+	return nil
+}
+
+func ParticipantMultiSecurer(permissionName string, request []*ChannelParticipant, context *Context) error {
+	return nil
+}
+
+func ParticipantSecurer(permissionName string, request *ChannelParticipant, context *Context) error {
+	return nil
+}
+
+func ParticipantReadSecurer(permissionName string, context *Context) error {
+	return nil
+}
+
+func MessageListSecurer(permissionName string, request *ChannelMessageList, context *Context) error {
+	return nil
+}
+
+func MessageListReadSecurer(permissionName string, context *Context) error {
+	return nil
+}
+
+func PinnedActivitySecurer(permissionName string, request *PinRequest, context *Context) error {
+	return nil
+}
+
+func PinnedActivityReadSecurer(permissionName string, context *Context) error {
+	return nil
+}
+
+func PrivateMessageSecurer(permissionName string, request *PrivateMessageRequest, context *Context) error {
+	return nil
+}
+
+func PrivateMessageReadSecurer(permissionName string, context *Context) error {
 	return nil
 }
