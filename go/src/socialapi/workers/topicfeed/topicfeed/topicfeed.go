2dc2081595ca9f68f997e4444a9de72b1557cb53
Can Yucel <can@koding.com>
2014-11-17 18:02:46 -0800

    topicfeed: fix error message format

--- go/src/socialapi/workers/topicfeed/topicfeed/topicfeed.go
+++ go/src/socialapi/workers/topicfeed/topicfeed/topicfeed.go
@@ -39,12 +39,12 @@ func New(log logging.Logger) *Controller {
 
 func (t *Controller) DefaultErrHandler(delivery amqp.Delivery, err error) bool {
 	if delivery.Redelivered {
-		t.log.Error("Redelivered message gave error again, putting to maintenance queue", err)
+		t.log.Error("Redelivered message gave error again, putting to maintenance queue: %s", err)
 		delivery.Ack(false)
 		return true
 	}
 
-	t.log.Error("an error occurred putting message back to queue", err)
+	t.log.Error("an error occurred putting message back to queue: %s", err)
 	delivery.Nack(false, true)
 	return false
 }
