a3faa08fe4464780b9f7ab8b7eee202e8578818a
Fatih Acet <fatih@fatihacet.com>
2014-11-22 03:03:08 +0200

    Workspace: Added MachineLabel IsDefault fields into workspace.go.

--- go/src/koding/db/models/workspace.go
+++ go/src/koding/db/models/workspace.go
@@ -3,12 +3,14 @@ package models
 import "labix.org/v2/mgo/bson"
 
 type Workspace struct {
-	ObjectId   bson.ObjectId `bson:"_id" json:"_id"`
-	Name       string        `bson:"name" json:"name"`
-	Slug       string        `bson:"slug" json:"slug"`
-	ChannelId  string        `bson:"channelId" json:"channelId"`
-	MachineUID string        `bson:"machineUId" json:"machineUId"`
-	Owner      string        `bson:"owner" json:"owner"`
-	RootPath   string        `bson:"rootPath" json:"rootPath"`
-	Layout     interface{}   `bson:"layout" json:"layout"`
+	ObjectId     bson.ObjectId `bson:"_id" json:"_id"`
+	Name         string        `bson:"name" json:"name"`
+	Slug         string        `bson:"slug" json:"slug"`
+	ChannelId    string        `bson:"channelId" json:"channelId"`
+	MachineUID   string        `bson:"machineUId" json:"machineUId"`
+	MachineLabel string        `bson:"machineLabel" json:"machineLabel"`
+	Owner        string        `bson:"owner" json:"owner"`
+	RootPath     string        `bson:"rootPath" json:"rootPath"`
+	IsDefault    bool          `bson:"isDefault" json:"isDefault"`
+	Layout       interface{}   `bson:"layout" json:"layout"`
 }
