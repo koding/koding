a3586ee62af783b3c612f7a7e99152d2356a5962
Senthil <senthil@koding.com>
2014-11-26 12:27:05 -0800

    go-webserver: update workspace schema

--- go/src/koding/db/models/workspace.go
+++ go/src/koding/db/models/workspace.go
@@ -9,8 +9,8 @@ type Workspace struct {
 	ChannelId    string        `bson:"channelId" json:"channelId"`
 	MachineUID   string        `bson:"machineUId" json:"machineUId"`
 	MachineLabel string        `bson:"machineLabel" json:"machineLabel"`
-	Owner        string        `bson:"owner" json:"owner"`
 	RootPath     string        `bson:"rootPath" json:"rootPath"`
 	IsDefault    bool          `bson:"isDefault" json:"isDefault"`
 	Layout       interface{}   `bson:"layout" json:"layout"`
+	OriginId     bson.ObjectId `bson:"originId" json:"originId"`
 }
