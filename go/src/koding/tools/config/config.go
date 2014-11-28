d89fb9e7d81ceaaca5337514bfb6707a28733ef7
Can Yucel <can@koding.com>
2014-11-28 08:39:04 -0800

    pubnub: add config for client

--- go/src/koding/tools/config/config.go
+++ go/src/koding/tools/config/config.go
@@ -267,6 +267,9 @@ type RuntimeOptions struct {
 	Paypal      struct {
 		FormUrl string `json:"formUrl"`
 	} `json:"paypal"`
+	Pubnub struct {
+		SubscribeKey string
+	} `json:"pubnub"`
 }
 
 // TODO: THIS IS ADDED SO ALL GO PACKAGES CLEANLY EXIT EVEN WHEN
