7a7e67d5614a9c778529d9114fb85fb06d08b5dc
Can Yucel <can@koding.com>
2014-12-04 20:34:59 -0800

    gatekeeper: add broker adapter

---
+++ go/src/socialapi/workers/gatekeeper/models/broker.go
@@ -0,0 +1,99 @@
+package models
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"socialapi/config"
+	"socialapi/workers/common/handler"
+
+	"github.com/koding/rabbitmq"
+	"github.com/streadway/amqp"
+)
+
+type Broker struct {
+	rmqConn *amqp.Connection
+}
+
+func NewBroker(rmq *rabbitmq.RabbitMQ) (*Broker, error) {
+	rmqConn, err := rmq.Connect("NewGatekeeperBroker")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Broker{
+		rmqConn: rmqConn.Conn(),
+	}, nil
+}
+
+func (b *Broker) Authenticate(req *ChannelRequest) error {
+	return nil
+}
+
+func (b *Broker) Push(pm *PushMessage) error {
+	// fetch these secret names from socialapi
+	resp, err := fetchSecretNamesById(pm.ChannelId)
+	if err != nil {
+		return err
+	}
+
+	//convert data into json message
+	byteMessage, err := json.Marshal(pm.Body)
+	if err != nil {
+		return err
+	}
+
+	// get a new channel for publishing a message
+
+	channel, err := b.rmqConn.Channel()
+	if err != nil {
+		return err
+	}
+	// do not forget to close the channel
+	defer channel.Close()
+
+	for _, secretName := range resp.SecretNames {
+		routingKey := "socialapi.channelsecret." + secretName + "." + pm.EventName
+		if err := channel.Publish(
+			"broker",   // exchange name
+			routingKey, // routing key
+			false,      // mandatory
+			false,      // immediate
+			amqp.Publishing{Body: byteMessage}, // message
+		); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func fetchSecretNamesById(channelId int64) (*ChannelResponse, error) {
+	request := &handler.Request{
+		Type:     handler.GetRequest,
+		Endpoint: fmt.Sprintf("%s/channel/%d/secretnames", config.MustGet().ProxyURL, channelId),
+	}
+
+	resp, err := handler.MakeRequest(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf(resp.Status)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	channelResponse := new(ChannelResponse)
+	err = json.Unmarshal(body, channelResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return channelResponse, nil
+}
