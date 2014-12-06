2a03bc95f378c02575e127368a75d940738b6c2f
Fatih Arslan <fatih@koding.com>
2014-12-06 22:20:54 +0200

    kloudctl/stop: initial batch support for stop

--- go/src/koding/kites/kloud/kloudctl/command/stop.go
+++ go/src/koding/kites/kloud/kloudctl/command/stop.go
@@ -2,6 +2,8 @@ package command
 
 import (
 	"fmt"
+	"strings"
+	"sync"
 
 	"koding/kites/kloud/kloud"
 
@@ -10,40 +12,65 @@ import (
 )
 
 type Stop struct {
-	id *string
+	ids *string
 }
 
 func NewStop() cli.CommandFactory {
 	return func() (cli.Command, error) {
 		f := NewFlag("stop", "Stop a machine")
 		f.action = &Stop{
-			id: f.String("id", "", "Machine id of to be stopped."),
+			ids: f.String("ids", "", "Machine id of information being showed."),
 		}
 		return f, nil
 	}
 }
 
-func (s *Stop) Action(args []string, k *kite.Client) error {
-	stopArgs := &KloudArgs{
-		MachineId: *s.id,
-		Username:  flagUsername,
+func (s *Stop) SingleMachine(id string, k *kite.Client) (string, error) {
+	stop := &KloudArgs{
+		MachineId: id,
 	}
 
-	resp, err := k.Tell("stop", stopArgs)
+	resp, err := k.Tell("stop", stop)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	var result kloud.ControlResult
 	err = resp.Unmarshal(&result)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	DefaultUi.Info(fmt.Sprintf("%+v", result))
+	return result.EventId, nil
+}
 
-	if flagWatchEvents {
-		return watch(k, "stop", *s.id, defaultPollInterval)
+func (s *Stop) Action(args []string, k *kite.Client) error {
+	machines := strings.Split(*s.ids, ",")
+
+	var wg sync.WaitGroup
+	for _, id := range machines {
+		wg.Add(1)
+
+		go func(id string) {
+			defer wg.Done()
+			result, err := s.SingleMachine(id, k)
+			if err != nil {
+				DefaultUi.Error(err.Error())
+			} else {
+				DefaultUi.Info(fmt.Sprintf("%+v", result))
+			}
+		}(id)
 	}
+
+	DefaultUi.Info(fmt.Sprintf("stop called for '%d' machines:\n", len(machines)))
+
+	wg.Wait()
+
+	if len(machines) == 1 {
+		if flagWatchEvents {
+			return watch(k, "stop", machines[0], defaultPollInterval)
+		}
+	}
+
 	return nil
 }
