0f315bf2e59e8d3b59bcfe246064709ab64b8586
Fatih Arslan <fatih@koding.com>
2014-12-11 13:15:48 +0200

    kloud/main.go: proxy is not used, remove it

--- go/src/koding/kites/kloud/main.go
+++ go/src/koding/kites/kloud/main.go
@@ -27,7 +27,6 @@ import (
 
 	"github.com/koding/kite"
 	kiteconfig "github.com/koding/kite/config"
-	"github.com/koding/kite/protocol"
 	"github.com/koding/logging"
 	"github.com/koding/multiconfig"
 	"github.com/mitchellh/goamz/aws"
@@ -80,7 +79,6 @@ type Config struct {
 
 	// --- KONTROL CONFIGURATION ---
 	Public      bool   // Try to register with a public ip
-	Proxy       bool   // Try to register behind a koding proxy
 	RegisterURL string // Explicitly register with this given url
 }
 
@@ -115,25 +113,12 @@ func main() {
 		registerURL = u
 	}
 
-	if conf.Proxy {
-		k.Log.Info("Proxy mode is enabled")
-		// Koding proxies in production only
-		proxyQuery := &protocol.KontrolQuery{
-			Username:    "koding",
-			Environment: "production",
-			Name:        "proxy",
-		}
-
-		k.Log.Info("Seaching proxy: %#v", proxyQuery)
-		go k.RegisterToProxy(registerURL, proxyQuery)
-	} else {
-		if err := k.RegisterForever(registerURL); err != nil {
-			k.Log.Fatal(err.Error())
-		}
+	if err := k.RegisterForever(registerURL); err != nil {
+		k.Log.Fatal(err.Error())
 	}
 
+	// DataDog listens to it
 	go func() {
-		// TODO ~ parameterize this
 		err := http.ListenAndServe("0.0.0.0:6060", nil)
 		k.Log.Error(err.Error())
 	}()
