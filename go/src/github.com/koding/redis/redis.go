674f25f62bc430209588081ed8f502e15985e8c4
Senthil <senthil@koding.com>
2014-12-24 14:44:36 -0800

    go: update redis

--- go/src/github.com/koding/redis/redis.go
+++ go/src/github.com/koding/redis/redis.go
@@ -506,3 +506,15 @@ func (r *RedisSession) SortedSetAddSingle(key, member string, score interface{})
 	_, err := r.Do("ZADD", r.AddPrefix(key), score, member)
 	return err
 }
+
+// SortedSetRangebyScore key min max
+// returns all the elements in the sorted set at key with a score
+// between min and max.
+//
+// See: http://redis.io/commands/zrangebyscore
+func (r *RedisSession) SortedSetRangebyScore(key string, rest ...interface{}) ([]interface{}, error) {
+	prefixed := []interface{}{r.AddPrefix(key)}
+	prefixed = append(prefixed, rest...)
+
+	return redis.Values(r.Do("ZRANGEBYSCORE", prefixed...))
+}
