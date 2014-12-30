0a1501e6e46b00fc7bdb3e02c5f754e8502d7fce
Senthil <senthil@koding.com>
2014-12-29 16:32:30 -0800

    vmwatcher: implement fetching plan from endpoint

--- go/src/koding/vmwatch/exempt.go
+++ go/src/koding/vmwatch/exempt.go
@@ -1,13 +1,26 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"koding/db/mongodb/modelhelper"
+	"net/http"
+)
+
+var (
+	PlanUrl = "http://localhost:7000/payments/subscriptions"
+)
+
 var ExemptUsers = []interface{}{
 	"sent-hil",
 }
 
+// checks if user is exempt from metric checkers, if something goes
+// wrong while checking, we return true as a precaution
 func exemptFromStopping(metricName, username string) (bool, error) {
 	plan, err := getPlanForUser(username)
 	if err != nil {
-		return false, err
+		return true, err
 	}
 
 	if plan != "free" {
@@ -16,7 +29,7 @@ func exemptFromStopping(metricName, username string) (bool, error) {
 
 	isExempt, err := storage.ExemptGet(metricName, username)
 	if err != nil {
-		return false, err
+		return true, err
 	}
 
 	if isExempt {
@@ -27,5 +40,30 @@ func exemptFromStopping(metricName, username string) (bool, error) {
 }
 
 func getPlanForUser(username string) (string, error) {
-	return "", nil
+	account, err := modelhelper.GetAccount(username)
+	if err != nil {
+		return "", err
+	}
+
+	url := fmt.Sprintf("%s?account_id=%s", PlanUrl, account.Id)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", nil
+	}
+
+	var subscription *SubscriptionResponse
+	e := json.NewDecoder(resp.Body)
+	if err := e.Decode(&subscription); err != nil {
+		return "", nil
+	}
+
+	if resp.StatusCode != 200 {
+		return "", nil
+	}
+
+	return subscription.PlanTitle, nil
+}
+
+type SubscriptionResponse struct {
+	PlanTitle string `json:"planTitle"`
 }
