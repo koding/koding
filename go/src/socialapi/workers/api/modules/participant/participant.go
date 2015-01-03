f00e47517a37296212896c9efc2c31920c0cd278
Cihangir SAVAS <cihangir@koding.com>
2015-01-02 18:41:20 -0800

    Socialapi: add join/leave events for collaboration channel

--- go/src/socialapi/workers/api/modules/participant/participant.go
+++ go/src/socialapi/workers/api/modules/participant/participant.go
@@ -153,7 +153,8 @@ func DeleteDesertedChannelMessages(channelId int64) error {
 		return err
 	}
 
-	if c.TypeConstant != models.Channel_TYPE_PRIVATE_MESSAGE {
+	if c.TypeConstant != models.Channel_TYPE_PRIVATE_MESSAGE ||
+		c.TypeConstant != models.Channel_TYPE_COLLABORATION {
 		return nil
 	}
 
@@ -244,7 +245,7 @@ func checkChannelPrerequisites(channelId, requesterId int64, participants []*mod
 
 	// return early for non private message channels
 	// no need to continue from here for other channels
-	if c.TypeConstant != models.Channel_TYPE_PRIVATE_MESSAGE {
+	if c.TypeConstant != models.Channel_TYPE_PRIVATE_MESSAGE || c.TypeConstant != models.Channel_TYPE_COLLABORATION {
 		return nil
 	}
 
@@ -274,7 +275,7 @@ func addJoinActivity(channelId, participantId, addedBy int64) error {
 		return err
 	}
 
-	pmr := &models.PrivateMessageRequest{AccountId: participantId}
+	pmr := &models.PrivateChannelRequest{AccountId: participantId}
 
 	return pmr.AddJoinActivity(c, addedBy)
 }
@@ -289,11 +290,12 @@ func addLeaveActivity(channelId, participantId int64) error {
 		return err
 	}
 
-	pmr := &models.PrivateMessageRequest{AccountId: participantId}
+	pmr := &models.PrivateChannelRequest{AccountId: participantId}
 
 	return pmr.AddLeaveActivity(c)
 }
 
+// this function is tested via integration tests
 func fetchChannelWithValidation(channelId int64) (*models.Channel, error) {
 	c := models.NewChannel()
 	if err := c.ById(channelId); err != nil {
@@ -301,7 +303,9 @@ func fetchChannelWithValidation(channelId int64) (*models.Channel, error) {
 	}
 
 	// add activity information for private message channel
-	if c.TypeConstant != models.Channel_TYPE_PRIVATE_MESSAGE {
+	if c.TypeConstant != models.Channel_TYPE_PRIVATE_MESSAGE &&
+
+		c.TypeConstant != models.Channel_TYPE_COLLABORATION {
 		return nil, ErrSkipActivity
 	}
 
