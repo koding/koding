f67fb806552c569ec450f245536ee008725dde24
Can Yucel <can@koding.com>
2015-01-06 08:56:59 -0800

    pubnub: grant public access for message channels

--- go/src/socialapi/workers/migrator/controller/pubnubmigrator.go
+++ go/src/socialapi/workers/migrator/controller/pubnubmigrator.go
@@ -9,7 +9,12 @@ import (
 
 func (mwc *Controller) GrantPublicAccess() {
 	mwc.log.Notice("Channel Public Access grant operation started")
+	mwc.GrantChannelAccess()
+	mwc.GrantMessageAccess()
+}
 
+func (mwc *Controller) GrantChannelAccess() {
+	mwc.log.Notice("Granting public access for channels")
 	c := models.NewChannel()
 	query := bongo.B.DB.
 		Model(c).
@@ -42,6 +47,39 @@ func (mwc *Controller) GrantPublicAccess() {
 		}
 
 		err := mwc.pubnub.GrantPublicAccess(realtimemodels.NewPrivateMessageChannel(channel))
+		if err != nil {
+			mwc.log.Error("Could not grant public access to token %s: %s", token, err)
+		}
+	}
+}
+
+func (mwc *Controller) GrantMessageAccess() {
+	mwc.log.Notice("Granting message access for channels")
+
+	c := models.NewChannelMessage()
+	query := bongo.B.DB.
+		Model(c).
+		Table(c.BongoName()).
+		Select("api.channel_message.token")
+
+	rows, err := query.Rows()
+	defer rows.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	if rows == nil {
+		return
+	}
+
+	var token string
+	for rows.Next() {
+		rows.Scan(&token)
+		channel := realtimemodels.UpdateInstanceMessage{
+			Token: token,
+		}
+
+		err := mwc.pubnub.GrantPublicAccess(realtimemodels.NewMessageUpdateChannel(channel))
 		if err != nil {
 			mwc.log.Error("Could not grant public access to token %s: %s", token, err)
 		}
