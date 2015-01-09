ba02a1dc550d92c4772642585fafb3cbc15a0867
Can Yucel <can@koding.com>
2015-01-08 17:28:37 -0800

    migrate: add existential checks for account scripts

--- go/src/socialapi/db/sql/migrations/0002_add-token-to-account.up.sql
+++ go/src/socialapi/db/sql/migrations/0002_add-token-to-account.up.sql
@@ -1,3 +1,19 @@
-ALTER TABLE api.account ADD COLUMN token UUID NOT NULL DEFAULT uuid_generate_v4();
+DO $$
+  BEGIN
+    BEGIN
+      ALTER TABLE api.account ADD COLUMN token UUID NOT NULL DEFAULT uuid_generate_v4();
+    EXCEPTION
+      WHEN duplicate_column THEN RAISE NOTICE 'column already exists';
+    END;
+  END;
+$$;
 
-ALTER TABLE api.account ADD CONSTRAINT "account_token_key" UNIQUE ("token") NOT DEFERRABLE INITIALLY IMMEDIATE;
+DO $$
+  BEGIN
+    BEGIN
+      ALTER TABLE api.account ADD CONSTRAINT "account_token_key" UNIQUE ("token") NOT DEFERRABLE INITIALLY IMMEDIATE;
+    EXCEPTION
+      WHEN duplicate_table THEN RAISE NOTICE 'constraint already exists';
+    END;
+  END;
+$$;
