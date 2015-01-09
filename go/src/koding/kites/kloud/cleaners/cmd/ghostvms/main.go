c1bbe2a72382dceba936f84e1030e3f40174050f
Fatih Arslan <fatih@koding.com>
2015-01-09 17:11:43 +0200

    cost: first cmd to search for instances without any MongodDB document

---
+++ go/src/koding/kites/kloud/cleaners/cmd/ghostvms/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"koding/kites/kloud/cleaners/lookup"
+	"os"
+	"text/tabwriter"
+
+	"github.com/koding/multiconfig"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/ec2"
+)
+
+type Config struct {
+	// AWS Access and Secret Key
+	AccessKey string `required:"true"`
+	SecretKey string `required:"true"`
+}
+
+func main() {
+	conf := new(Config)
+
+	// Load the config, it's reads environment variables or from flags
+	multiconfig.New().MustLoad(conf)
+
+	auth := aws.Auth{
+		AccessKey: conf.AccessKey,
+		SecretKey: conf.SecretKey,
+	}
+
+	t := lookup.New(auth)
+
+	fmt.Printf("Searching for production instances ...\n")
+
+	filter := ec2.NewFilter()
+	filter.Add("tag-value", "production")
+	// Anything except "terminated" and "shutting-down"
+	filter.Add("instance-state-name", "pending", "running", "stopping", "stopped")
+
+	t.Filter = filter
+	t.FetchInstances()
+
+	fmt.Printf("\n\n")
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+
+	total := 0
+	for client, instances := range t.FoundInstances {
+		region := client.Region.Name
+		fmt.Fprintf(w, "[%s]\t total instances: %+v \n", region, len(instances))
+		total += len(instances)
+	}
+
+	fmt.Fprintln(w)
+	w.Flush()
+}
