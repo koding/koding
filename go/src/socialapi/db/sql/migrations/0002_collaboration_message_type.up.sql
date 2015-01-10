faf53ee9dfb2233e370e043286d3e2f0de2b18b5
Can Yucel <can@koding.com>
2015-01-09 17:14:46 -0800

    migrate: add existential check for channel.payload column

--- go/src/socialapi/db/sql/migrations/0002_collaboration_message_type.up.sql
+++ go/src/socialapi/db/sql/migrations/0002_collaboration_message_type.up.sql
@@ -2,4 +2,12 @@
 -- ALTER TYPE "api"."channel_type_constant_enum" ADD VALUE IF NOT EXISTS 'collaboration';
 
 -- add new coloumn into channel
-ALTER TABLE "api"."channel" ADD COLUMN "payload" hstore;
+DO $$
+  BEGIN
+    BEGIN
+      ALTER TABLE "api"."channel" ADD COLUMN "payload" hstore;
+    EXCEPTION
+      WHEN duplicate_column THEN RAISE NOTICE 'column already exists';
+    END;
+  END;
+$$;
