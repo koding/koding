536382bdd6832c5b4edfd9079e123f13172f0ea5
Senthil <senthil@koding.com>
2015-01-15 16:41:52 -0800

    vmwatcher: return early if klient is not initialized

--- go/src/koding/vmwatcher/action.go
+++ go/src/koding/vmwatcher/action.go
@@ -12,6 +12,11 @@ type requestArgs struct {
 }
 
 func stopVm(machineId, reason string) error {
+	if controller.Klient == nil {
+		Log.Debug("Klient not initialized. Not stopping: %s", machineId)
+		return nil
+	}
+
 	_, err := controller.Klient.Tell("stop", &requestArgs{
 		MachineId: machineId, Reason: reason,
 	})
@@ -25,6 +30,11 @@ func blockUserAndDestroyVm(username, reason string) error {
 	machines, err := modelhelper.GetMachinesForUsername(username)
 	if err != nil {
 		return err
+	}
+
+	if controller.Klient == nil {
+		Log.Debug("Klient not initialized. Not stopping: %d", len(machines))
+		return nil
 	}
 
 	for _, machine := range machines {
