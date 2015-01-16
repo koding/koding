894b9da36ca698bfc6663186b253897e1fe304cc
Gokmen Goksel <gokmen@goksel.me>
2015-01-15 16:00:12 -0800

    - Slackifying...

--- scripts/assassin.py
+++ scripts/assassin.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 
 import os
+import sys
+
 import time
+import json
+
 import psutil
+import pycurl
 
-THRESHOLD = 90.0   # percentage
-REPEAT_EVERY = 2   # seconds
-MAX_OCCURRENCE = 5 # times
+THRESHOLD      = 90.0 # percentage
+REPEAT_EVERY   = 5    # seconds
+MAX_OCCURRENCE = 5    # times
+KILL_ENABLED   = False
 
-my_pid = os.getpid()
+my_pid   = os.getpid()
 bad_guys = {}
+
+PAYLOAD  = {
+    "channel"    : "#_devops",
+    "username"   : "py_assassin",
+    "icon_emoji" : ":snake:"
+}
+
+slack = pycurl.Curl()
+slack.setopt(pycurl.URL, "https://hooks.slack.com/services/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")
+slack.setopt(pycurl.POST, 1)
+
+def slack_it(message):
+
+    print message
+
+    PAYLOAD['text'] = message
+    slack.setopt(pycurl.POSTFIELDS, "payload=%s" % json.dumps(PAYLOAD))
+    slack.perform()
+
 
 def get_top_processes():
 
@@ -36,8 +61,9 @@ def get_top_processes():
 
 def kill(proc):
 
-    print "Killing: %s (PID %s)" % (proc.name(), proc.pid)
-    proc.kill()
+    slack_it("Killing: %s (PID %s)" % (proc.name(), proc.pid))
+    if KILL_ENABLED:
+        proc.kill()
     del bad_guys[proc.pid]
 
 
@@ -59,11 +85,14 @@ def checks():
         else:
             bad_guys[proc.pid] = dict( proc = proc, counter = 0 )
 
+    PROCESS_OUT = "Process '%s' (PID %s) is using more than %s " \
+                  "CPU (%s) in last %d seconds for the %d times."
+
     for pid, p in bad_guys.iteritems():
         [p, counter] = [p['proc'].dict, p['counter']]
-        print "Process '%s' (PID %s) is using more than %s CPU (%s) in last %d seconds for the %d times." % (
+        slack_it(PROCESS_OUT % (
             p['name'], pid, THRESHOLD, p['cpu_percent'], REPEAT_EVERY, counter
-        )
+        ))
 
 def main():
 
@@ -76,4 +105,5 @@ def main():
         pass
 
 if __name__ == '__main__':
+    print "Assassin started..."
     main()
