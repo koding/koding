c38f834888b5885737c1918f1fda172001370887
Senthil <senthil196@gmail.com>
2015-01-21 13:46:38 -0800

    paymentwebhook: map webhook -> actions for paypal, stripe

--- go/src/socialapi/workers/payment/paymentwebhook/paypal.go
+++ go/src/socialapi/workers/payment/paymentwebhook/paypal.go
@@ -1,8 +1,57 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"socialapi/workers/payment/paypal"
+
+	"github.com/coreos/go-log/log"
+)
+
+type paypalActionType func(string) error
+
+var paypalActionExpire = []paypalActionType{
+	paypal.ExpireSubscription,
+}
+
+var paypalActions = map[string][]paypalActionType{
+	"Denied":   paypalActionExpire,
+	"Expired":  paypalActionExpire,
+	"Failed":   paypalActionExpire,
+	"Reversed": paypalActionExpire,
+	"Voided":   paypalActionExpire,
+	"recurring_payment_profile_cancel": paypalActionExpire,
+}
+
+type paypalWebhookRequest struct {
+	TransactionType string `json:"txn_type"`
+	Status          string `json:"payment_status"`
+	PayerId         string `json:"payer_id"`
+}
 
 type paypalMux struct{}
 
 func (p *paypalMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var req paypalWebhookRequest
+
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		log.Error("Error marshalling Paypal webhook '%v' : %v", p, err)
+		return
+	}
+
+	actions, ok := paypalActions[req.Status]
+	if !ok {
+		actions, ok = paypalActions[req.TransactionType]
+		if !ok {
+			return
+		}
+	}
+
+	for _, action := range actions {
+		err := action(req.PayerId)
+		if err != nil {
+			log.Error("Paypal webhook: %s action failed: %s", req.PayerId, err)
+		}
+	}
 }
--- go/src/socialapi/workers/payment/paymentwebhook/stripe.go
+++ go/src/socialapi/workers/payment/paymentwebhook/stripe.go
@@ -1,8 +1,69 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"socialapi/workers/payment/stripe"
+
+	"github.com/coreos/go-log/log"
+)
+
+type stripeActionType func([]byte) error
+
+var stripeActions = map[string][]stripeActionType{
+	"customer.subscription.deleted": []stripeActionType{
+		stripe.SubscriptionDeletedWebhook,
+		// stopMachines,
+	},
+
+	"invoice.created": []stripeActionType{
+		stripe.InvoiceCreatedWebhook,
+	},
+
+	"customer.deleted": []stripeActionType{
+		stripe.CustomerDeletedWebhook,
+	},
+}
+
+type stripeWebhookRequest struct {
+	Name     string `json:"type"`
+	Created  int    `json:"created"`
+	Livemode bool   `json:"livemode"`
+	Id       string `json:"id"`
+	Data     struct {
+		Object interface{} `json:"object"`
+	} `json:"data"`
+}
 
 type stripeMux struct{}
 
 func (s *stripeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var req stripeWebhookRequest
+
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		log.Error("Error marshalling Stripe webhook '%v' : %v", s, err)
+		return
+	}
+
+	actions, ok := stripeActions[req.Name]
+	if !ok {
+		log.Error("Stripe webhook: %s not implemented", req.Name)
+		return
+	}
+
+	data, err := json.Marshal(req.Data.Object)
+	if err != nil {
+		log.Error("Error marshalling Stripe webhook '%v' : %v", s, err)
+		return
+	}
+
+	for _, action := range actions {
+		err := action(data)
+		if err != nil {
+			log.Error("Stripe webhook: %s action failed: %s", req.Name, err)
+		}
+	}
+
+	// return 200 to webhook
 }
