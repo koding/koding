3c5f18056cd878cfbcc66ff64fe876db29bb8ce2
Fatih Arslan <ftharsln@gmail.com>
2015-01-23 00:35:21 +0200

    collaboration: add Option struct to users
    This is needed so we can distinguis between the permanent mode and
    non permanent mode. Because sharing on Koding can be done in two ways,
    one is a temporary sharing which is caused by the collaboration part,
    and it's only valid if the sharer is online (so the user on the other
    end is not permanent).
    
    Another option is to fully share their VM. Which is the permanent mode.

--- collaboration/storage.go
+++ collaboration/storage.go
@@ -8,10 +8,28 @@ var (
 	ErrUserNotFound = errors.New("User not found")
 )
 
+// Option contains user specific settings
+type Option struct {
+	// Permananet means the user is shared
+	Permanent bool   `json:"permanent"`
+	Test      string `json:"test"`
+}
+
 type Storage interface {
-	Get(string) (string, error)
-	GetAll() ([]string, error)
-	Set(string, string) error
+	// Get returns the user property for a given user
+	Get(username string) (*Option, error)
+
+	// GetAll returns all users with their options
+	GetAll() (map[string]*Option, error)
+
+	// Set adds the given username and it's option. If there is a
+	// username already it will be overwritten
+	Set(string, *Option) error
+
+	// Delete deletes the given username
 	Delete(string) error
+
+	// Close closes the connection to the storage. For in memory
+	// implementations this can be a no-op operation
 	Close() error
 }
