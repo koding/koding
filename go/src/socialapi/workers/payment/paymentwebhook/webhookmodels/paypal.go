3e6e690e61f512ca90cdd861194eef60f4efdeb8
Senthil <senthil@koding.com>
2015-01-26 16:48:53 -0800

    paymentwebhook: unmarshal paypal webhook time properly

--- go/src/socialapi/workers/payment/paymentwebhook/webhookmodels/paypal.go
+++ go/src/socialapi/workers/payment/paymentwebhook/webhookmodels/paypal.go
@@ -1,10 +1,32 @@
 package webhookmodels
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+const PayplTimeLayout = "03:04:05 Jan 02, 2006 MST"
+
+type PaypalOwnTime struct {
+	time.Time
+}
+
+func (pt *PaypalOwnTime) UnmarshalJSON(b []byte) error {
+	var err error
+
+	pt.Time, err = time.Parse(PayplTimeLayout, strings.Replace(fmt.Sprintf("%s", b), `"`, "", -1))
+
+	return err
+}
+
 type PaypalGenericWebhook struct {
-	TransactionType string `json:"txn_type"`
-	Status          string `json:"payment_status"`
-	PayerId         string `json:"payer_id"`
-	Plan            string `json:"product_name"`
-	Amount          string `json:"amount"`
-	CurrencyCode    string `json:"current_code"`
+	TransactionType string        `json:"txn_type"`
+	Status          string        `json:"payment_status"`
+	PayerId         string        `json:"recurring_payment_id"`
+	Plan            string        `json:"product_name"`
+	Amount          string        `json:"amount"`
+	Currency        string        `json:"currency_code"`
+	NextPaymentDate PaypalOwnTime `json:"next_payment_date"`
+	PaymentDate     PaypalOwnTime `json:"payment_date"`
 }
