720d64f63b4e45f3d3155e351ce401b348977876
Cihangir SAVAS <cihangir@koding.com>
2015-01-27 18:12:50 -0800

    Socialapi: comment out unusable migration script

--- go/src/socialapi/db/sql/migrations/0008_add_linked_topic_enum_to_channel.up.sql
+++ go/src/socialapi/db/sql/migrations/0008_add_linked_topic_enum_to_channel.up.sql
@@ -2,4 +2,4 @@
 -- We need "linkedtopic" enum for keeping the state of the channel in the
 -- channel table, linked channels should not be listed in search etc
 --
-ALTER TYPE "api"."channel_type_constant_enum" ADD VALUE IF NOT EXISTS 'linkedtopic';
+-- ALTER TYPE "api"."channel_type_constant_enum" ADD VALUE IF NOT EXISTS 'linkedtopic';
