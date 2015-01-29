4d422d4be6d966b51fec65c5e9201a6823c1efc0
Senthil <senthil@koding.com>
2015-01-29 11:51:58 -0800

    paymentwebhook: remove unused kodignemail method

--- go/src/koding/kodingemail/options.go
+++ go/src/koding/kodingemail/options.go
@@ -1,12 +1,3 @@
 package kodingemail
 
 type Options map[string]string
-
-func (o Options) GetWithDefault(key string) string {
-	value, ok := o[key]
-	if !ok {
-		return "default"
-	}
-
-	return value
-}
