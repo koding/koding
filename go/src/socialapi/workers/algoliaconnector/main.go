1f9a391d4716cac717dc4e89f82a42d0ea65a1a7
Cihangir SAVAS <cihangir@koding.com>
2015-01-28 17:14:29 -0800

    Socialapi: listen channel update events

--- go/src/socialapi/workers/algoliaconnector/main.go
+++ go/src/socialapi/workers/algoliaconnector/main.go
@@ -27,6 +27,7 @@ func main() {
 	handler := algoliaconnector.New(r.Log, algolia, r.Conf.Algolia.IndexSuffix)
 	r.SetContext(handler)
 	r.Register(models.Channel{}).OnCreate().Handle((*algoliaconnector.Controller).TopicSaved)
+	r.Register(models.Channel{}).OnUpdate().Handle((*algoliaconnector.Controller).TopicUpdated)
 	r.Register(models.Account{}).OnCreate().Handle((*algoliaconnector.Controller).AccountSaved)
 	r.Register(models.ChannelMessageList{}).OnCreate().Handle((*algoliaconnector.Controller).MessageListSaved)
 	r.Register(models.ChannelMessageList{}).OnDelete().Handle((*algoliaconnector.Controller).MessageListDeleted)
