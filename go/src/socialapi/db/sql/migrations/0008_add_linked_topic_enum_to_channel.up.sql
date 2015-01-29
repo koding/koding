38dbf713176c2d0e5353a4c83c413b277a2ae97d
Cihangir SAVAS <cihangir@koding.com>
2015-01-28 17:59:01 -0800

    Socialapi: added a stub line for migration of new topic enum

--- go/src/socialapi/db/sql/migrations/0008_add_linked_topic_enum_to_channel.up.sql
+++ go/src/socialapi/db/sql/migrations/0008_add_linked_topic_enum_to_channel.up.sql
@@ -3,3 +3,5 @@
 -- channel table, linked channels should not be listed in search etc
 --
 -- ALTER TYPE "api"."channel_type_constant_enum" ADD VALUE IF NOT EXISTS 'linkedtopic';
+-- this is just a stub
+Select 1 from pg_tables;
