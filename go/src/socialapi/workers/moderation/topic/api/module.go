0baaa2746051ad5dccdc45aa5b66b53b0ceb5bfb
Cihangir SAVAS <cihangir@koding.com>
2015-01-28 16:41:49 -0800

    Socialapi: added more documentation

--- go/src/socialapi/workers/moderation/topic/api/module.go
+++ go/src/socialapi/workers/moderation/topic/api/module.go
@@ -8,60 +8,70 @@ import (
 	"socialapi/workers/common/response"
 )
 
-func CreateLink(u *url.URL, h http.Header, req *models.ChannelLink) (int, http.Header, interface{}, error) {
+// CreateLink creates a new link between two channels, root and leaf id should
+// be given in the request
+func CreateLink(u *url.URL, h http.Header, req *models.ChannelLink, context *models.Context) (int, http.Header, interface{}, error) {
 	rootId, err := request.GetURIInt64(u, "rootId")
 	if err != nil {
 		return response.NewBadRequest(err)
 	}
 
 	req.RootId = rootId
+
 	return response.HandleResultAndError(req, req.Create())
 }
 
-func GetLinks(u *url.URL, h http.Header, req *models.ChannelLink) (int, http.Header, interface{}, error) {
-	rootId, err := request.GetURIInt64(u, "rootId")
-	if err != nil {
+// GetLinks returns the leaves of a root channel
+func GetLinks(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
+	req := &models.ChannelLink{}
+	if err := prepareRequest(u, req); err != nil {
 		return response.NewBadRequest(err)
 	}
 
-	leafId, err := request.GetURIInt64(u, "leafId")
-	if err != nil {
-		return response.NewBadRequest(err)
-	}
-	req.RootId = rootId
-	req.LeafId = leafId
-
-	return response.HandleResultAndError(req.List(request.GetQuery(u)))
+	return response.HandleResultAndError(
+		req.List(request.GetQuery(u)),
+	)
 }
 
-func UnLink(u *url.URL, h http.Header, req *models.ChannelLink) (int, http.Header, interface{}, error) {
-	rootId, err := request.GetURIInt64(u, "rootId")
-	if err != nil {
+// Unlink removes the connection between
+func UnLink(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
+	req := &models.ChannelLink{}
+	if err := prepareRequest(u, req); err != nil {
 		return response.NewBadRequest(err)
 	}
 
-	leafId, err := request.GetURIInt64(u, "leafId")
-	if err != nil {
-		return response.NewBadRequest(err)
-	}
-	req.RootId = rootId
-	req.LeafId = leafId
 	return response.HandleResultAndError(req, req.UnLink())
 }
 
-func Blacklist(u *url.URL, h http.Header, req *models.ChannelLink) (int, http.Header, interface{}, error) {
+// Blacklist remove the channel from system completely, it shouldnt have any
+// leaf channels in order to be blacklisted
+func Blacklist(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
 	rootId, err := request.GetURIInt64(u, "rootId")
 	if err != nil {
 		return response.NewBadRequest(err)
 	}
 
+	req := models.ChannelLink{
+		RootId: rootId,
+	}
+
+	return response.HandleResultAndError(req, req.Blacklist())
+}
+
+// prepareRequest read the parameters from url and sets them into struct
+func prepareRequest(u *url.URL, req *models.ChannelLink) error {
+	rootId, err := request.GetURIInt64(u, "rootId")
+	if err != nil {
+		return err
+	}
+
 	leafId, err := request.GetURIInt64(u, "leafId")
 	if err != nil {
-		return response.NewBadRequest(err)
+		return err
 	}
 
 	req.RootId = rootId
 	req.LeafId = leafId
 
-	return response.HandleResultAndError(req, req.Blacklist())
+	return nil
 }
