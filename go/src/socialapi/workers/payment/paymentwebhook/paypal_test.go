0f4f5001e23e6c6235f2ae370cf5113956af177b
Senthil <senthil@koding.com>
2015-01-29 16:03:34 -0800

    paymentwebhook: add tests for empty webhook requests

--- go/src/socialapi/workers/payment/paymentwebhook/paypal_test.go
+++ go/src/socialapi/workers/payment/paymentwebhook/paypal_test.go
@@ -9,22 +9,41 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-var paypalWebhookUrl = "/paypal"
+func paypalTest(body []byte) (*httptest.ResponseRecorder, error) {
+	r, err := http.NewRequest("POST", "/", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	recorder := httptest.NewRecorder()
+
+	pp := &paypalMux{Controller: controller}
+	pp.ServeHTTP(recorder, r)
+
+	return recorder, nil
+}
 
 func TestPaypal(t *testing.T) {
 	Convey("Given webhook from paypal", t, func() {
 		Convey("When webhook isn't implemented", func() {
 			body := []byte(`{"type":"random.webhook"}`)
-			r, err := http.NewRequest("POST", paypalWebhookUrl, bytes.NewBuffer(body))
+			recorder, err := paypalTest(body)
+
 			So(err, ShouldBeNil)
-
-			recorder := httptest.NewRecorder()
-
-			pp := &paypalMux{}
-			pp.ServeHTTP(recorder, r)
 
 			Convey("Then it should return 200", func() {
 				So(recorder.Code, ShouldEqual, 200)
+			})
+		})
+
+		Convey("When webhook request is empty", func() {
+			body := []byte(``)
+			recorder, err := paypalTest(body)
+
+			So(err, ShouldBeNil)
+
+			Convey("Then it should return 500", func() {
+				So(recorder.Code, ShouldEqual, 500)
 			})
 		})
 	})
--- go/src/socialapi/workers/payment/paymentwebhook/stripe_test.go
+++ go/src/socialapi/workers/payment/paymentwebhook/stripe_test.go
@@ -9,10 +9,8 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-var stripeWebhookUrl = "/stripe"
-
 func stripeTest(body []byte) (*httptest.ResponseRecorder, error) {
-	r, err := http.NewRequest("POST", stripeWebhookUrl, bytes.NewBuffer(body))
+	r, err := http.NewRequest("POST", "/", bytes.NewBuffer(body))
 	if err != nil {
 		return nil, err
 	}
@@ -27,7 +25,7 @@ func stripeTest(body []byte) (*httptest.ResponseRecorder, error) {
 
 func TestStripe(t *testing.T) {
 	Convey("Given webhook from stripe", t, func() {
-		Convey("When webhook isn't implemented", func() {
+		Convey("When webhook action isn't implemented", func() {
 			body := []byte(`{"type":"random.webhook"}`)
 			recorder, err := stripeTest(body)
 
@@ -35,6 +33,17 @@ func TestStripe(t *testing.T) {
 
 			Convey("Then it should return 200", func() {
 				So(recorder.Code, ShouldEqual, 200)
+			})
+		})
+
+		Convey("When webhook request is empty", func() {
+			body := []byte(``)
+			recorder, err := stripeTest(body)
+
+			So(err, ShouldBeNil)
+
+			Convey("Then it should return 500", func() {
+				So(recorder.Code, ShouldEqual, 500)
 			})
 		})
 	})
