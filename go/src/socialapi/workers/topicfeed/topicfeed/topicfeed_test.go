49752c378f871538bce5995c696f07a293234a95
Cihangir SAVAS <cihangir@koding.com>
2015-01-30 16:47:40 -0800

    Socialapi: added test case for topic feed worker

--- go/src/socialapi/workers/topicfeed/topicfeed/topicfeed_test.go
+++ go/src/socialapi/workers/topicfeed/topicfeed/topicfeed_test.go
@@ -1,6 +1,8 @@
 package topicfeed
 
 import (
+	"math/rand"
+	"socialapi/models"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -18,9 +20,36 @@ func TestMarkedAsTroll(t *testing.T) {
 			So(topics[0], ShouldEqual, "topic")
 		})
 
-		Convey("public should be removed from topics list", func() {
+		Convey("duplicate public should be removed from topics list", func() {
 			topics := extractTopics("hi #public  #public  my topic")
 			So(len(topics), ShouldEqual, 0)
+		})
+	})
+}
+
+func TestIsEligible(t *testing.T) {
+	Convey("while testing isEligible", t, func() {
+		Convey("initial channel id should be set", func() {
+			c := models.NewChannelMessage()
+			c.InitialChannelId = 0
+			eligible, err := isEligible(c)
+			So(err, ShouldBeNil)
+			So(eligible, ShouldBeFalse)
+		})
+
+		Convey("type_constant should be Post", func() {
+			c := models.NewChannelMessage()
+			eligible, err := isEligible(c)
+			So(err, ShouldBeNil)
+			So(eligible, ShouldBeFalse)
+
+			Convey("when it is set to Post, should be eligible", func() {
+				c.InitialChannelId = rand.Int63()
+				c.TypeConstant = models.ChannelMessage_TYPE_POST
+				eligible, err := isEligible(c)
+				So(err, ShouldBeNil)
+				So(eligible, ShouldBeTrue)
+			})
 		})
 	})
 }
