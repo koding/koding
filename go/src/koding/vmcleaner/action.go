5de68bab95759df360339195b9dbdb8c9a197e09
Senthil <senthil@koding.com>
2015-02-04 12:31:25 -0800

    vmcleaner: add template for 3rd email warning

--- go/src/koding/vmcleaner/action.go
+++ go/src/koding/vmcleaner/action.go
@@ -11,6 +11,7 @@ var (
 	templates = map[int]string{
 		1: "8239b8ac-032d-4f90-80de-a144519a945c",
 		2: "a987a9c0-f116-4e75-bf18-bff3c1c11b37",
+		3: "fedf1228-4624-4c23-8dea-e9391c5d1e98",
 	}
 )
 
