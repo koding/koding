3a1db395cb15119a88f49085f91ec05699bd2e69
Senthil <senthil@koding.com>
2015-02-04 13:18:29 -0800

    vmcleaner: introduce controller, work around it

--- go/src/koding/vmcleaner/action.go
+++ go/src/koding/vmcleaner/action.go
@@ -5,23 +5,13 @@ import (
 	"koding/db/models"
 )
 
-type Action func(*models.User, int) error
-
-var (
-	templates = map[int]string{
-		1: "8239b8ac-032d-4f90-80de-a144519a945c",
-		2: "a987a9c0-f116-4e75-bf18-bff3c1c11b37",
-		3: "fedf1228-4624-4c23-8dea-e9391c5d1e98",
-	}
-)
-
 func SendEmail(user *models.User, levelId int) error {
 	templateId, ok := templates[levelId]
 	if !ok {
 		return fmt.Errorf("template not found for level: %v", levelId)
 	}
 
-	return email.SendTemplateEmail(user.Email, templateId, nil)
+	return controller.Email.SendTemplateEmail(user.Email, templateId, nil)
 }
 
 func DeleteVM(user *models.User, _ int) error {
--- go/src/koding/vmcleaner/main.go
+++ go/src/koding/vmcleaner/main.go
@@ -2,23 +2,112 @@ package main
 
 import (
 	"fmt"
+	"koding/db/mongodb/modelhelper"
+	"socialapi/workers/helper"
 	"time"
+
+	kiteConfig "github.com/koding/kite/config"
+
+	"github.com/koding/kite"
+	"github.com/koding/kodingemail"
+	"github.com/koding/multiconfig"
 )
 
-type Inactive struct {
-	Assigned               bool
-	AssignedAt, ModifiedAt time.Time
-}
+var (
+	WorkerName    = "vmcleaner"
+	WorkerVersion = "0.0.1"
 
-var Warnings = []*Warning{
-	FirstEmail, SecondEmail, ThirdEmail, FourthDeleteVM,
+	Log = helper.CreateLogger(WorkerName, false)
+
+	Warnings   []*Warning
+	controller *Controller
+)
+
+type Vmcleaner struct {
+	Mongo string `required:"true"`
+	Email struct {
+		Username string `required:"username"`
+		Password string `required:"password"`
+	} `required:"email"`
+
+	Kloud struct {
+		SecretKey string `required:"key"`
+		Address   string `required:"secret"`
+	} `required:"kloud"`
 }
 
 func main() {
+	conf := initializeConf()
+	modelhelper.Initialize(conf.Mongo)
+
+	defer func() {
+		modelhelper.Close()
+	}()
+
+	// initialize client to talk to kloud
+	kloud := conf.Kloud
+	kiteClient := initializeKiteClient(kloud.SecretKey, kloud.Address)
+
+	// initialize client to send email
+	email := initializeEmail(conf.Email.Username, conf.Email.Password)
+
+	Warnings = initializeWarnings(kiteClient, email)
+
 	for _, warning := range Warnings {
 		result := warning.Run()
 		fmt.Println(result)
 	}
+}
+
+func initializeConf() *Vmcleaner {
+	var conf *Vmcleaner
+
+	d := &multiconfig.DefaultLoader{
+		Loader: multiconfig.MultiLoader(
+			&multiconfig.EnvironmentLoader{Prefix: "KONFIG_VMCLEANER"},
+		),
+	}
+
+	d.MustLoad(conf)
+
+	return conf
+}
+
+func initializeKiteClient(kloudKey, kloudAddr string) *kite.Client {
+	var err error
+
+	// create new kite
+	k := kite.New(WorkerName, WorkerVersion)
+	config, err := kiteConfig.Get()
+	if err != nil {
+		Log.Fatal(err.Error())
+	}
+
+	// set skeleton config
+	k.Config = config
+
+	if k == nil {
+		Log.Info("kite not initialized in runner. Pass '-kite-init'")
+		return nil
+	}
+
+	// create a new connection to the cloud
+	kiteClient := k.NewClient(kloudAddr)
+	kiteClient.Auth = &kite.Auth{Type: "kloudctl", Key: kloudKey}
+
+	// dial the kloud address
+	if err := kiteClient.DialTimeout(time.Second * 10); err != nil {
+		Log.Error("%s. Is kloud/kontrol running?", err.Error())
+		return nil
+	}
+
+	Log.Debug("Connected to klient: %s", kloudAddr)
+
+	return kiteClient
+}
+
+func initializeEmail(username, password string) kodingemail.Client {
+	return kodingemail.NewSG(username, password)
 }
 
 func handleError(err error) {
