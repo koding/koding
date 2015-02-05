09cf64c435aae9d2bbec76ba6bd73527e5a275a9
Senthil <senthil@koding.com>
2015-02-04 16:20:05 -0800

    vmcleaner: better logs

--- go/src/koding/vmcleaner/result.go
+++ go/src/koding/vmcleaner/result.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 type Result struct {
 	Total, Successful, Failure int
-	Level                      int
+	Warning                    int
+}
+
+func (r *Result) String() string {
+	return fmt.Sprintf(
+		"Ran for warning: %v total: %v, successful: %v, failed: %v",
+		r.Warning, r.Total, r.Successful, r.Failure,
+	)
 }
