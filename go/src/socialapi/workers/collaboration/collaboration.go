b13dbba2a2c95b6ea6b47c704d4e61458552a243
Cihangir SAVAS <cihangir@koding.com>
2015-02-17 15:50:28 -0800

    Socialapi: fix PR comments

--- go/src/socialapi/workers/collaboration/collaboration.go
+++ go/src/socialapi/workers/collaboration/collaboration.go
@@ -19,18 +19,42 @@ import (
 const (
 	// FireEventName is a unique event name for the collaboration ping messages
 	FireEventName = "fire"
+
+	// KeyPrefix for redis
+	KeyPrefix = "collaboration"
+)
+
+var (
+	// durations
+	//
+	// send pings every 15minutes
+	pingDuration = time.Second * 15
+
+	// offset should be smaller than a ping
+	offsetDuration = time.Second * 10
+
+	// session should be terminated after this duration
+	terminateSessionDuration = pingDuration * 4
+
+	// ExpireSessionKeyDuration redis key expiration duration
+	ExpireSessionKeyDuration = pingDuration * 5
+
+	// how long the go routine will sleep
+	sleepTime = terminateSessionDuration + offsetDuration
+
+	// every go routine should be completed in this duration
+	deadLineDuration = sleepTime + time.Second*5
+
+	// errors
+	//
+	errSessionInvalid  = errors.New("session is invalid")
+	errDeadlineReached = errors.New("couldnt process the message in deadline")
 )
 
 // Controller holds the basic context data for handlers
 type Controller struct {
 	log   logging.Logger
 	redis *redis.RedisSession
-}
-
-// DefaultErrHandler handles the errors for collaboration worker
-func (t *Controller) DefaultErrHandler(delivery amqp.Delivery, err error) bool {
-	delivery.Nack(false, true)
-	return false
 }
 
 // New creates a controller
@@ -41,23 +65,11 @@ func New(log logging.Logger, redis *redis.RedisSession) *Controller {
 	}
 }
 
-// send pings every 15minutes
-var pingDuration = time.Second * 15
-
-// offset should be smaller than a ping
-var offsetDuration = time.Second * 10
-
-// session should be terminated after this duration
-var terminateSessionDuration = pingDuration * 4
-
-// ExpireSessionKeyDuration redis key expiration duration
-var ExpireSessionKeyDuration = pingDuration * 5
-
-// how long the go routine will sleep
-var sleepTime = terminateSessionDuration + offsetDuration
-
-// every go routine should be completed in this duration
-var deadLineDuration = sleepTime + time.Second*5
+// DefaultErrHandler handles the errors for collaboration worker
+func (t *Controller) DefaultErrHandler(delivery amqp.Delivery, err error) bool {
+	delivery.Nack(false, true)
+	return false
+}
 
 // Ping handles the pings coming from client side
 func (c *Controller) Ping(ping *models.Ping) error {
@@ -85,35 +97,34 @@ func (c *Controller) Ping(ping *models.Ping) error {
 		return nil
 	}
 
+	// wait syncronously
+	err = c.wait(ping)
+	if err != nil && err != errSessionInvalid {
+		return err
+	}
+
+	if err == errSessionInvalid {
+		// key should be there
+		// end the collab session
+		c.log.Info("session is not valid anymore, collab should be terminated")
+		return nil
+	}
+
+	return nil
+}
+
+func (c *Controller) wait(ping *models.Ping) error {
 	select {
 	// wait for terminate
 	// (session terminate duration + and some offset)
 	case <-time.After(sleepTime):
 		// check if another ping is set
 		// if the key is deleted, it means someone already deleted it
-		err := c.checkIfKeyIsValid(ping)
-		if err != nil && err != errSessionInvalid {
-			return err
-		}
-
-		if err == errSessionInvalid {
-			// key should be there
-			// end the collab session
-			c.log.Info("session is not valid anymore, collab should be terminated")
-			return nil
-		}
-
-		// session is still valid
-		return nil
-
-		// if we cant finish our process in deadline period, re-add the message
-		// to the RMQ
+		return c.checkIfKeyIsValid(ping)
 	case <-time.After(deadLineDuration):
-		return errors.New("couldnt process the message")
+		return errDeadlineReached
 	}
 }
-
-var errSessionInvalid = errors.New("session is invalid")
 
 func (c *Controller) checkIfKeyIsValid(ping *models.Ping) error {
 	var err error
@@ -136,8 +147,7 @@ func (c *Controller) checkIfKeyIsValid(ping *models.Ping) error {
 
 	unixSec, err := strconv.ParseInt(file, 10, 64)
 	if err != nil {
-		c.log.Error(err.Error())
-		// silently discard this case, if the time is invalid, we should not try
+		// discard this case, if the time is invalid, we should not try
 		// to process it again
 		return errSessionInvalid // key is not valid
 	}
@@ -155,13 +165,12 @@ func (c *Controller) checkIfKeyIsValid(ping *models.Ping) error {
 	return nil
 }
 
-const CACHEPREFIX = "collaboration"
-
+// PrepareFileKey prepares a key for redis
 func PrepareFileKey(fileId string) string {
 	return fmt.Sprintf(
 		"%s:%s:%s",
 		config.MustGet().Environment,
-		CACHEPREFIX,
+		KeyPrefix,
 		fileId,
 	)
 }
