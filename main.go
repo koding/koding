fff00bafe49b69e10a37954a56f1eee0ac1855b0
Fatih Arslan <ftharsln@gmail.com>
2015-02-20 10:59:56 +0200

    klient: add registration to Kontrol via Password
    This PR enables the user to create a kite.key automatically by
    authenticating to Kontrol directly with their Koding usernames and
    password. If successfull a kite.key is created and places at
    ~/.kite/kite.key  By default it's disabled.
    
    Usage is in the form of:
    
    klient -register
    klient -register -username arslan
    klient -register -username arslan -kontrol-url "https://foo.company.com"
    
    This is be used to register any third party machine to kontrol.

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"strings"
 	"time"
 
+	"github.com/koding/kite"
+	"github.com/koding/kite/config"
+	"github.com/koding/kite/kitekey"
 	"github.com/koding/klient/app"
 	"github.com/koding/klient/protocol"
 )
@@ -19,6 +26,12 @@ var (
 	flagRegisterURL = flag.String("register-url", "", "Change register URL to kontrol")
 	flagDebug       = flag.Bool("debug", false, "Debug mode")
 	flagScreenrc    = flag.String("screenrc", "/opt/koding/etc/screenrc", "Default screenrc path")
+
+	// Registration flags
+	flagUsername   = flag.String("username", "", "Username to be registered to Kontrol")
+	flagRegister   = flag.Bool("register", false, "Register to Kontrol with your Koding Password")
+	flagKontrolURL = flag.String("kontrol-url", "https://koding.com/kontrol/kite",
+		"Change kontrol URL to be used for registration")
 
 	// update parameters
 	flagUpdateInterval = flag.Duration("update-interval", time.Minute*5,
@@ -39,6 +52,14 @@ func realMain() int {
 	flag.Parse()
 	if *flagVersion {
 		fmt.Println(protocol.Version)
+		return 0
+	}
+
+	if *flagRegister {
+		if err := registerWithPassword(*flagKontrolURL, *flagUsername); err != nil {
+			fmt.Fprint(os.Stderr, err.Error())
+			return 1
+		}
 		return 0
 	}
 
@@ -63,4 +84,89 @@ func realMain() int {
 	a.Run()
 
 	return 0
+}
+
+// registerWithPassword registers to the given kontrolURL. The user
+func registerWithPassword(kontrolURL, username string) error {
+	var err error
+
+	// Open up a prompt if the username is not passed via a flag
+	if username == "" {
+		username, err = ask("Username:")
+		if err != nil {
+			return err
+		}
+		// User can just press enter to use the default on the prompt
+		if username == "" {
+			return errors.New("Username can not be empty.")
+		}
+	}
+
+	k := kite.New("klient", protocol.Version)
+	k.Config.Environment = protocol.Environment
+	k.Config.Region = protocol.Region
+	k.Config.Transport = config.XHRPolling
+	k.Config.Username = username
+
+	if _, err := kitekey.Read(); err == nil {
+		fmt.Println("Already registered. Registering again...")
+	}
+
+	kontrol := k.NewClient(kontrolURL)
+	if err := kontrol.Dial(); err != nil {
+		return err
+	}
+
+	result, err := kontrol.TellWithTimeout("registerMachine", 5*time.Minute, username)
+	if err != nil {
+		return err
+	}
+
+	if err := kitekey.Write(result.MustString()); err != nil {
+		return err
+	}
+
+	fmt.Println("Registered successfully")
+	return nil
+}
+
+// ask asks for an input from standard input and returns the result back. It is
+// extracted from mitcellh/cli to be used as a standalone function.
+func ask(query string) (string, error) {
+	if _, err := fmt.Fprint(os.Stdout, query+" "); err != nil {
+		return "", err
+	}
+
+	// Register for interrupts so that we can catch it and immediately
+	// return...
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	defer signal.Stop(sigCh)
+
+	// Ask for input in a go-routine so that we can ignore it.
+	errCh := make(chan error, 1)
+	lineCh := make(chan string, 1)
+	go func() {
+		r := bufio.NewReader(os.Stdin)
+		line, err := r.ReadString('\n')
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		lineCh <- strings.TrimRight(line, "\r\n")
+	}()
+
+	select {
+	case err := <-errCh:
+		return "", err
+	case line := <-lineCh:
+		return line, nil
+	case <-sigCh:
+		// Print a newline so that any further output starts properly
+		// on a new line.
+		fmt.Fprintln(os.Stdout)
+
+		return "", errors.New("interrupted")
+	}
 }
