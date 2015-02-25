f3dc3e508d4c05c2d83c6fe20389acee617bdf44
Cihangir SAVAS <cihangir@koding.com>
2015-02-24 18:38:27 -0800

    Socialapi: implement removing/unsharing users from klient

--- go/src/socialapi/workers/collaboration/collaboration.go
+++ go/src/socialapi/workers/collaboration/collaboration.go
@@ -7,11 +7,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"koding/db/mongodb/modelhelper"
+	"koding/kites/kloud/klient"
 	"net/http"
 	"socialapi/config"
 	socialapimodels "socialapi/models"
 	"socialapi/workers/collaboration/models"
 	"strconv"
+	"strings"
 	"time"
 
 	"code.google.com/p/goauth2/oauth"
@@ -192,6 +194,12 @@ func (c *Controller) EndSession(ping *models.Ping) error {
 	}, errChan)
 
 	c.goWithRetry(func() error {
+		// first remove the users from klient
+		if err := c.RemoveUsersFromMachine(ping); err != nil {
+			return err
+		}
+
+		// then remove them from the db
 		return c.UnshareVM(ping)
 	}, errChan)
 
@@ -282,6 +290,103 @@ func (c *Controller) UnshareVM(ping *models.Ping) error {
 	}
 
 	return modelhelper.UnshareMachineByUid(ws.MachineUID)
+}
+
+func (c *Controller) RemoveUsersFromMachine(ping *models.Ping) error {
+	// if channel id is nil, there is nothing to do
+	if ping.ChannelId == "" {
+		return nil
+	}
+
+	ws, err := modelhelper.GetWorkspaceByChannelId(ping.ChannelId)
+	if err != nil && err != mgo.ErrNotFound {
+		return err
+	}
+
+	// if the workspace is not there, nothing to do
+	if err != mgo.ErrNotFound {
+		return nil
+	}
+
+	m, err := modelhelper.GetMachineByUid(ws.MachineUID)
+	if err != nil && err != mgo.ErrNotFound {
+		return err
+	}
+
+	// if the machine is not there, nothing to do
+	if err != mgo.ErrNotFound {
+		return nil
+	}
+
+	// Get the klient.
+	klientRef, err := klient.ConnectTimeout(c.kite, m.QueryString, time.Second*10)
+	if err != nil {
+		if err == klient.ErrDialingFailed || err == kite.ErrNoKitesAvailable {
+			c.log.Error(
+				"[%s] Klient is not registered to Kontrol. Err: %s",
+				m.QueryString,
+				err,
+			)
+
+			return nil // if the machine is not open, we cant do anything
+		}
+
+		return err
+	}
+
+	type req struct {
+		Username string
+
+		// we are gonna use this propery here, just for reference
+		Permanent bool
+	}
+
+	var iterErr error
+	for _, user := range m.Users {
+		// do not unshare from owner user
+		if user.Sudo && user.Owner {
+			continue
+		}
+
+		// fetch user for its username
+		u, err := modelhelper.GetUserById(user.Id.Hex())
+		if err != nil {
+			c.log.Error(err.Error())
+			// if we cant find the regarding user, do not do anything
+			if err == mgo.ErrNotFound {
+				continue
+			}
+
+			iterErr = err
+			// do not stop iterating, unshare from others
+			continue
+		}
+
+		param := req{
+			Username: u.Name,
+		}
+
+		_, err = klientRef.Client().Tell("klient.unshare", param)
+		if err != nil {
+			// those are so error prone, force klient side not to change the API
+			// or make them exported to some other package?
+			if strings.Contains(err.Error(), "user is permanent") {
+				continue
+			}
+
+			if strings.Contains(err.Error(), "user is not in the shared list") {
+				continue
+			}
+
+			c.log.Error(err.Error())
+			iterErr = err
+			continue // do not stop iterating, unshare from others
+		}
+	}
+
+	// iterErr will be nil if we dont encounter to any error in iter
+	return iterErr
+
 }
 
 func (c *Controller) DeleteDriveDoc(ping *models.Ping) error {
