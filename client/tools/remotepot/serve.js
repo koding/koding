c82bf5ac2d21066f985daf25a8aba3a33f3eb369
tetsuo <ogunduz@gmail.com>
2015-02-27 20:11:29 +0200

    client/tools/remotepot: cors

--- client/tools/remotepot/serve.js
+++ client/tools/remotepot/serve.js
@@ -1,4 +1,12 @@
+var ROOT_DIR = __dirname + '/../../../website';
+
 var http = require('http');
-var ecstatic = require('ecstatic');
-http.createServer(ecstatic({ root: __dirname + '/../../../website' })).listen(8090);
+var ecstatic = require('ecstatic')(ROOT_DIR);
+
+var server = http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  ecstatic(req, res);
+});
+
+server.listen(8090);
 console.log('listening on :8090');
