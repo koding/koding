2100d736ab7d98caf398f8bf7b469d544fa3df0f
Cihangir SAVAS <cihangir@koding.com>
2015-03-01 18:39:39 -0800

    Socialapi:  created tests for UnshareVM

--- go/src/socialapi/workers/collaboration/operations_test.go
+++ go/src/socialapi/workers/collaboration/operations_test.go
@@ -154,4 +154,95 @@ func TestCollaborationOperationsEndPrivateMessage(t *testing.T) {
 		})
 	})
 }
+
+func TestCollaborationOperationsUnshareVM(t *testing.T) {
+	r := runner.New("collaboration-UnshareVM-tests")
+	err := r.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	defer r.Close()
+
+	modelhelper.Initialize(r.Conf.Mongo)
+	defer modelhelper.Close()
+
+	redisConn := helper.MustInitRedisConn(r.Conf)
+	defer redisConn.Close()
+
+	handler := New(r.Log, redisConn, r.Conf, r.Kite)
+
+	Convey("while testing UnshareVM", t, func() {
+		req := &models.Ping{
+			AccountId: 1,
+			FileId:    fmt.Sprintf("%d", rand.Int63()),
+		}
+		Convey("should be able to create the channel and workspace first", func() {
+			creator := socialapimodels.CreateAccountWithTest() // init account
+			c := socialapimodels.NewChannel()                  // init channel
+			c.CreatorId = creator.Id                           // set Creator id
+			c.TypeConstant = socialapimodels.Channel_TYPE_COLLABORATION
+			So(c.Create(), ShouldBeNil)
+			cp, err := c.AddParticipant(creator.Id)
+			So(err, ShouldBeNil)
+			So(cp, ShouldNotBeNil)
+
+			req.AccountId = c.CreatorId // set real account id
+			req.ChannelId = c.Id        // set real channel id
+
+			// sample  machine struct
+			m := &mongomodels.Machine{
+				ObjectId: bson.NewObjectId(),
+				Uid:      bson.NewObjectId().Hex(),
+				Users: []mongomodels.MachineUser{
+
+					mongomodels.MachineUser{ // real owner
+						Id:    bson.ObjectIdHex(creator.OldId),
+						Sudo:  true,
+						Owner: true,
+					},
+					mongomodels.MachineUser{ // secondary owner
+						Id:    bson.NewObjectId(),
+						Sudo:  false,
+						Owner: true,
+					},
+				},
+				CreatedAt: time.Now().UTC(),
+				Status: mongomodels.MachineStatus{
+					State:      "running",
+					ModifiedAt: time.Now().UTC(),
+				},
+				Assignee:    mongomodels.MachineAssignee{},
+				UserDeleted: false,
+			}
+
+			So(modelhelper.CreateMachine(m), ShouldBeNil)
+
+			ws := &mongomodels.Workspace{
+				ObjectId:     bson.NewObjectId(),
+				Name:         "My Workspace",
+				Slug:         "my-workspace",
+				ChannelId:    strconv.FormatInt(req.ChannelId, 10),
+				MachineUID:   m.Uid,
+				MachineLabel: "koding-vm-0",
+				Owner:        "cihangir",
+				RootPath:     "/home/cihangir",
+				IsDefault:    true,
+			}
+
+			So(modelhelper.CreateWorkspace(ws), ShouldBeNil)
+
+			Convey("should be able to UnshareVM", func() {
+				err = handler.UnshareVM(req)
+				So(err, ShouldBeNil)
+				Convey("remove users should not be in the machine", func() {
+					m2, err := modelhelper.GetMachineByUid(m.Uid)
+					So(err, ShouldBeNil)
+					So(m2, ShouldNotBeNil)
+					So(len(m2.Users), ShouldEqual, 1)
+					So(m2.Users[0].Id.Hex(), ShouldEqual, creator.OldId)
+				})
+			})
+		})
+	})
 }
