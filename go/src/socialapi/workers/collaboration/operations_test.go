5c8b1b93d505eacb3c6126f0ee0184cd098c605c
Cihangir SAVAS <cihangir@koding.com>
2015-03-01 18:39:17 -0800

    Socialapi:  created tests for EndPrivateMessage

--- go/src/socialapi/workers/collaboration/operations_test.go
+++ go/src/socialapi/workers/collaboration/operations_test.go
@@ -63,4 +63,95 @@ func TestCollaborationOperationsDeleteDriveDoc(t *testing.T) {
 		})
 	})
 }
+
+func TestCollaborationOperationsEndPrivateMessage(t *testing.T) {
+	r := runner.New("collaboration-EndPrivateMessage-tests")
+	err := r.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	defer r.Close()
+
+	modelhelper.Initialize(r.Conf.Mongo)
+	defer modelhelper.Close()
+
+	redisConn := helper.MustInitRedisConn(r.Conf)
+	defer redisConn.Close()
+
+	handler := New(r.Log, redisConn, r.Conf, r.Kite)
+
+	Convey("while testing EndPrivateMessage", t, func() {
+		req := &models.Ping{
+			AccountId: 1,
+			FileId:    fmt.Sprintf("%d", rand.Int63()),
+		}
+		Convey("should be able to create the channel first", func() {
+			creator := socialapimodels.CreateAccountWithTest() // init account
+			c := socialapimodels.NewChannel()                  // init channel
+			c.CreatorId = creator.Id                           // set Creator id
+			c.TypeConstant = socialapimodels.Channel_TYPE_COLLABORATION
+			So(c.Create(), ShouldBeNil)
+			cp, err := c.AddParticipant(creator.Id)
+			So(err, ShouldBeNil)
+			So(cp, ShouldNotBeNil)
+
+			req.AccountId = c.CreatorId // set real account id
+			req.ChannelId = c.Id        // set real channel id
+
+			ws := &mongomodels.Workspace{
+				ObjectId:     bson.NewObjectId(),
+				Name:         "My Workspace",
+				Slug:         "my-workspace",
+				ChannelId:    strconv.FormatInt(req.ChannelId, 10),
+				MachineUID:   bson.NewObjectId().Hex(),
+				MachineLabel: "koding-vm-0",
+				Owner:        "cihangir",
+				RootPath:     "/home/cihangir",
+				IsDefault:    true,
+			}
+
+			So(modelhelper.CreateWorkspace(ws), ShouldBeNil)
+
+			Convey("should be able to delete channel", func() {
+				err = handler.EndPrivateMessage(req)
+				So(err, ShouldBeNil)
+				Convey("deleted channel should not be exist", func() {
+					channel := socialapimodels.NewChannel()
+					err := channel.ById(req.ChannelId)
+					So(err, ShouldEqual, bongo.RecordNotFound)
+				})
+				Convey("channel id in workspace should not be exist", func() {
+					ws2, err := modelhelper.GetWorkspaceByChannelId(
+						strconv.FormatInt(req.ChannelId, 10),
+					)
+					So(err, ShouldEqual, mgo.ErrNotFound)
+					So(ws2, ShouldEqual, nil)
+				})
+			})
+
+			Convey("if not a participant, should not be able to delete channel", func() {
+				req.AccountId = 1
+				err = handler.EndPrivateMessage(req)
+				So(err, ShouldBeNil)
+				Convey("channel should exist", func() {
+					channel := socialapimodels.NewChannel()
+					err := channel.ById(req.ChannelId)
+					So(err, ShouldBeNil)
+				})
+			})
+
+			Convey("if channel doesnt exists, should success", func() {
+				req.ChannelId = 1
+				err = handler.EndPrivateMessage(req)
+				So(err, ShouldBeNil)
+				Convey("channel should not exist", func() {
+					channel := socialapimodels.NewChannel()
+					err := channel.ById(req.ChannelId)
+					So(err, ShouldEqual, bongo.RecordNotFound)
+				})
+			})
+		})
+	})
+}
 }
