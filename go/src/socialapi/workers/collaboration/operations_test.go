6fa1b92c77bc2ad293cfdcdf32a04740876936e1
Cihangir SAVAS <cihangir@koding.com>
2015-03-03 17:50:30 -0800

    Socialapi: implemented test cases for multiple workspaces

--- go/src/socialapi/workers/collaboration/operations_test.go
+++ go/src/socialapi/workers/collaboration/operations_test.go
@@ -11,14 +11,13 @@ import (
 	"socialapi/workers/helper"
 	"strconv"
 
-	"github.com/koding/bongo"
-
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 
 	"testing"
 	"time"
 
+	"github.com/koding/bongo"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -101,6 +100,7 @@ func TestCollaborationOperationsEndPrivateMessage(t *testing.T) {
 
 			ws := &mongomodels.Workspace{
 				ObjectId:     bson.NewObjectId(),
+				OriginId:     bson.NewObjectId(),
 				Name:         "My Workspace",
 				Slug:         "my-workspace",
 				ChannelId:    strconv.FormatInt(req.ChannelId, 10),
@@ -173,76 +173,291 @@ func TestCollaborationOperationsUnshareVM(t *testing.T) {
 	handler := New(r.Log, redisConn, r.Conf, r.Kite)
 
 	Convey("while testing UnshareVM", t, func() {
-		req := &models.Ping{
-			AccountId: 1,
-			FileId:    fmt.Sprintf("%d", rand.Int63()),
-		}
+
 		Convey("should be able to create the channel and workspace first", func() {
-			creator := socialapimodels.CreateAccountWithTest() // init account
-			c := socialapimodels.NewChannel()                  // init channel
-			c.CreatorId = creator.Id                           // set Creator id
-			c.TypeConstant = socialapimodels.Channel_TYPE_COLLABORATION
-			So(c.Create(), ShouldBeNil)
-			cp, err := c.AddParticipant(creator.Id)
-			So(err, ShouldBeNil)
-			So(cp, ShouldNotBeNil)
-
-			req.AccountId = c.CreatorId // set real account id
-			req.ChannelId = c.Id        // set real channel id
-
-			// sample  machine struct
-			m := &mongomodels.Machine{
-				ObjectId: bson.NewObjectId(),
-				Uid:      bson.NewObjectId().Hex(),
-				Users: []mongomodels.MachineUser{
-
-					mongomodels.MachineUser{ // real owner
-						Id:    bson.ObjectIdHex(creator.OldId),
-						Sudo:  true,
-						Owner: true,
-					},
-					mongomodels.MachineUser{ // secondary owner
-						Id:    bson.NewObjectId(),
-						Sudo:  false,
-						Owner: true,
-					},
-				},
-				CreatedAt: time.Now().UTC(),
-				Status: mongomodels.MachineStatus{
-					State:      "running",
-					ModifiedAt: time.Now().UTC(),
-				},
-				Assignee:    mongomodels.MachineAssignee{},
-				UserDeleted: false,
-			}
-
-			So(modelhelper.CreateMachine(m), ShouldBeNil)
-
-			ws := &mongomodels.Workspace{
-				ObjectId:     bson.NewObjectId(),
-				Name:         "My Workspace",
-				Slug:         "my-workspace",
-				ChannelId:    strconv.FormatInt(req.ChannelId, 10),
-				MachineUID:   m.Uid,
-				MachineLabel: "koding-vm-0",
-				Owner:        "cihangir",
-				RootPath:     "/home/cihangir",
-				IsDefault:    true,
-			}
-
-			So(modelhelper.CreateWorkspace(ws), ShouldBeNil)
 
 			Convey("should be able to UnshareVM", func() {
-				err = handler.UnshareVM(req)
+
+				creator, err := socialapimodels.CreateAccountInBothDbs() // init account
+				So(err, ShouldBeNil)
+
+				participant1, err := socialapimodels.CreateAccountInBothDbs()
+				So(err, ShouldBeNil)
+
+				participant2, err := socialapimodels.CreateAccountInBothDbs()
+				So(err, ShouldBeNil)
+
+				m1, m1ws1 := prepareSingleWorkspace(creator, participant1, participant2)
+
+				channelId, err := strconv.ParseInt(m1ws1.ChannelId, 10, 64)
+				So(err, ShouldBeNil)
+
+				req1 := &models.Ping{
+					AccountId: creator.Id,
+					FileId:    fmt.Sprintf("%d", rand.Int63()),
+					ChannelId: channelId,
+				}
+
+				err = handler.UnshareVM(req1)
 				So(err, ShouldBeNil)
 				Convey("remove users should not be in the machine", func() {
-					m2, err := modelhelper.GetMachineByUid(m.Uid)
+					mm1, err := modelhelper.GetMachineByUid(m1.Uid)
 					So(err, ShouldBeNil)
-					So(m2, ShouldNotBeNil)
-					So(len(m2.Users), ShouldEqual, 1)
-					So(m2.Users[0].Id.Hex(), ShouldEqual, creator.OldId)
+					So(mm1, ShouldNotBeNil)
+					So(len(mm1.Users), ShouldEqual, 1)
+					ownerUser, err := modelhelper.GetUserByAccountId(creator.OldId)
+					So(err, ShouldBeNil)
+					So(mm1.Users[0].Id.Hex(), ShouldEqual, ownerUser.ObjectId.Hex())
+				})
+			})
+
+			Convey("if participant and owner shares multiple workspaces", func() {
+
+				creator, err := socialapimodels.CreateAccountInBothDbs() // init account
+				So(err, ShouldBeNil)
+
+				participant1, err := socialapimodels.CreateAccountInBothDbs()
+				So(err, ShouldBeNil)
+
+				participant2, err := socialapimodels.CreateAccountInBothDbs()
+				So(err, ShouldBeNil)
+
+				_, _, m2, m2ws1, _ := prepareWorkspace(creator, participant1, participant2)
+
+				Convey("remove from first workspace", func() {
+					channelId, err := strconv.ParseInt(m2ws1.ChannelId, 10, 64)
+					So(err, ShouldBeNil)
+
+					req := &models.Ping{
+						AccountId: creator.Id,
+						FileId:    fmt.Sprintf("%d", rand.Int63()),
+						ChannelId: channelId,
+					}
+
+					err = handler.UnshareVM(req)
+					So(err, ShouldBeNil)
+					Convey("participant should still be in the second machine", func() {
+						mm2, err := modelhelper.GetMachineByUid(m2.Uid)
+						So(err, ShouldBeNil)
+						So(mm2, ShouldNotBeNil)
+						So(len(mm2.Users), ShouldEqual, 3)
+
+						ownerUser, err := modelhelper.GetUserByAccountId(creator.OldId)
+						So(err, ShouldBeNil)
+						So(mm2.Users[0].Id.Hex(), ShouldEqual, ownerUser.ObjectId.Hex())
+
+						participant1User, err := modelhelper.GetUserByAccountId(participant1.OldId)
+						So(err, ShouldBeNil)
+						So(mm2.Users[1].Id.Hex(), ShouldEqual, participant1User.ObjectId.Hex())
+
+						participant2User, err := modelhelper.GetUserByAccountId(participant2.OldId)
+						So(err, ShouldBeNil)
+						So(mm2.Users[2].Id.Hex(), ShouldEqual, participant2User.ObjectId.Hex())
+					})
 				})
 			})
 		})
 	})
+}
+
+func prepareWorkspace(creator, participant1, participant2 *socialapimodels.Account) (
+	*mongomodels.Machine, // m1
+	*mongomodels.Workspace, // m1 ws1
+	*mongomodels.Machine, // m2
+	*mongomodels.Workspace, // m2 ws1
+	*mongomodels.Workspace, // m2 ws2
+) {
+
+	m1, m1ws1 := prepareSingleWorkspace(creator, participant1, participant2)
+
+	ownerUser, err := modelhelper.GetUserByAccountId(creator.OldId)
+	So(err, ShouldBeNil)
+
+	participant1User, err := modelhelper.GetUserByAccountId(participant1.OldId)
+	So(err, ShouldBeNil)
+
+	participant2User, err := modelhelper.GetUserByAccountId(participant2.OldId)
+	So(err, ShouldBeNil)
+
+	// sample  machine struct
+	m2 := &mongomodels.Machine{
+		ObjectId: bson.NewObjectId(),
+		Uid:      bson.NewObjectId().Hex(),
+		Users: []mongomodels.MachineUser{
+			mongomodels.MachineUser{ // real owner
+				Id:    ownerUser.ObjectId,
+				Sudo:  true,
+				Owner: true,
+			},
+			mongomodels.MachineUser{ // secondary owner
+				Id:    participant1User.ObjectId,
+				Sudo:  false,
+				Owner: true,
+			},
+			mongomodels.MachineUser{ // random
+				Id:    participant2User.ObjectId,
+				Sudo:  false,
+				Owner: true,
+			},
+		},
+		CreatedAt: time.Now().UTC(),
+		Status: mongomodels.MachineStatus{
+			State:      "running",
+			ModifiedAt: time.Now().UTC(),
+		},
+		Assignee:    mongomodels.MachineAssignee{},
+		UserDeleted: false,
+	}
+
+	So(modelhelper.CreateMachine(m2), ShouldBeNil)
+
+	c3 := socialapimodels.NewChannel() // init channel
+	c3.CreatorId = creator.Id          // set Creator id
+	c3.TypeConstant = socialapimodels.Channel_TYPE_COLLABORATION
+	So(c3.Create(), ShouldBeNil)
+
+	c3p1, err := c3.AddParticipant(creator.Id)
+	So(err, ShouldBeNil)
+	So(c3p1, ShouldNotBeNil)
+
+	c3p2, err := c3.AddParticipant(participant1.Id)
+	So(err, ShouldBeNil)
+	So(c3p2, ShouldNotBeNil)
+
+	c3p3, err := c3.AddParticipant(participant2.Id)
+	So(err, ShouldBeNil)
+	So(c3p3, ShouldNotBeNil)
+
+	c4 := socialapimodels.NewChannel() // init channel
+	c4.CreatorId = creator.Id          // set Creator id
+	c4.TypeConstant = socialapimodels.Channel_TYPE_COLLABORATION
+	So(c4.Create(), ShouldBeNil)
+
+	c4p1, err := c4.AddParticipant(creator.Id)
+	So(err, ShouldBeNil)
+	So(c4p1, ShouldNotBeNil)
+
+	c4p2, err := c4.AddParticipant(participant1.Id)
+	So(err, ShouldBeNil)
+	So(c4p2, ShouldNotBeNil)
+
+	c4p3, err := c4.AddParticipant(participant2.Id)
+	So(err, ShouldBeNil)
+	So(c4p3, ShouldNotBeNil)
+
+	m2ws1 := &mongomodels.Workspace{
+		ObjectId:     bson.NewObjectId(),
+		OriginId:     ownerUser.ObjectId,
+		Name:         "My Workspace",
+		Slug:         "m2ws1",
+		ChannelId:    strconv.FormatInt(c3.Id, 10),
+		MachineUID:   m2.Uid,
+		MachineLabel: "koding-vm-0",
+		Owner:        "cihangir",
+		RootPath:     "/home/cihangir",
+		IsDefault:    true,
+	}
+
+	So(modelhelper.CreateWorkspace(m2ws1), ShouldBeNil)
+
+	m2ws2 := &mongomodels.Workspace{
+		ObjectId:     bson.NewObjectId(),
+		OriginId:     ownerUser.ObjectId,
+		Name:         "My Workspace",
+		Slug:         "m2ws2",
+		ChannelId:    strconv.FormatInt(c4.Id, 10),
+		MachineUID:   m2.Uid,
+		MachineLabel: "koding-vm-0",
+		Owner:        "cihangir",
+		RootPath:     "/home/cihangir",
+		IsDefault:    true,
+	}
+
+	So(modelhelper.CreateWorkspace(m2ws2), ShouldBeNil)
+
+	return m1, m1ws1, m2, m2ws1, m2ws2
+}
+
+func prepareSingleWorkspace(creator, participant1, participant2 *socialapimodels.Account) (
+	*mongomodels.Machine, // m1
+	*mongomodels.Workspace, // m1 ws1
+) {
+
+	ownerUser, err := modelhelper.GetUserByAccountId(creator.OldId)
+	So(err, ShouldBeNil)
+
+	participant1User, err := modelhelper.GetUserByAccountId(participant1.OldId)
+	So(err, ShouldBeNil)
+
+	participant2User, err := modelhelper.GetUserByAccountId(participant2.OldId)
+	So(err, ShouldBeNil)
+
+	//
+	// create the first channel
+	//
+	c1 := socialapimodels.NewChannel() // init channel
+	c1.CreatorId = creator.Id          // set Creator id
+	c1.TypeConstant = socialapimodels.Channel_TYPE_COLLABORATION
+	So(c1.Create(), ShouldBeNil)
+
+	c1p1, err := c1.AddParticipant(creator.Id)
+	So(err, ShouldBeNil)
+	So(c1p1, ShouldNotBeNil)
+
+	c1p2, err := c1.AddParticipant(participant1.Id)
+	So(err, ShouldBeNil)
+	So(c1p2, ShouldNotBeNil)
+
+	c1p3, err := c1.AddParticipant(participant2.Id)
+	So(err, ShouldBeNil)
+	So(c1p3, ShouldNotBeNil)
+
+	// sample  machine struct
+	m1 := &mongomodels.Machine{
+		ObjectId: bson.NewObjectId(),
+		Uid:      bson.NewObjectId().Hex(),
+		Users: []mongomodels.MachineUser{
+			mongomodels.MachineUser{ // real owner
+				Id:    ownerUser.ObjectId,
+				Sudo:  true,
+				Owner: true,
+			},
+			mongomodels.MachineUser{ // secondary owner
+				Id:    participant1User.ObjectId,
+				Sudo:  false,
+				Owner: true,
+			},
+			mongomodels.MachineUser{ // random
+				Id:    participant2User.ObjectId,
+				Sudo:  false,
+				Owner: true,
+			},
+		},
+		CreatedAt: time.Now().UTC(),
+		Status: mongomodels.MachineStatus{
+			State:      "running",
+			ModifiedAt: time.Now().UTC(),
+		},
+		Assignee:    mongomodels.MachineAssignee{},
+		UserDeleted: false,
+	}
+
+	So(modelhelper.CreateMachine(m1), ShouldBeNil)
+
+	m1ws1 := &mongomodels.Workspace{
+		ObjectId:     bson.NewObjectId(),
+		OriginId:     ownerUser.ObjectId,
+		Name:         "My Workspace",
+		Slug:         "m1ws1",
+		ChannelId:    strconv.FormatInt(c1.Id, 10),
+		MachineUID:   m1.Uid,
+		MachineLabel: "koding-vm-0",
+		Owner:        "cihangir",
+		RootPath:     "/home/cihangir",
+		IsDefault:    true,
+	}
+
+	So(modelhelper.CreateWorkspace(m1ws1), ShouldBeNil)
+
+	return m1, m1ws1
 }
