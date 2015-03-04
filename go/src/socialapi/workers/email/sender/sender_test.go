6a0a26aed297a7e3759055f8cf6148df22e667cc
mehmetalisavas <mehmetali.savas@yandex.com>
2015-03-04 20:08:05 +0200

    mailsender/tests: tests for mailsender is added

--- go/src/socialapi/workers/email/sender/sender_test.go
+++ go/src/socialapi/workers/email/sender/sender_test.go
@@ -33,11 +33,54 @@ func TestNew(t *testing.T) {
 	defer r.Close()
 
 	Convey("while creating new controller for Mail worker", t, func() {
-		Convey("it should return Controller struct", func() {
+		Convey("error should not be nil", func() {
+			_ = &Mail{
+				To:       "mehmet@koding.com",
+				Subject:  "Test message",
+				Text:     "Example",
+				From:     "team@koding.com",
+				FromName: "koding",
+			}
+
 			sg := sendgrid.NewSendGridClient(r.Conf.Email.Username, r.Conf.Email.Password)
 			c := New(r.Log, sg)
 			So(c, ShouldNotBeNil)
+		})
+	})
+}
 
+type fakeClient struct {
+	mail *sendgrid.SGMail
+}
+
+func (f *fakeClient) Send(mail *sendgrid.SGMail) error {
+	f.mail = mail
+	return nil
+}
+
+func TestSend(t *testing.T) {
+	r := runner.New("test")
+	if err := r.Init(); err != nil {
+		t.Fatalf("couldn't start bongo %s", err.Error())
+	}
+	defer r.Close()
+
+	Convey("while sending mail for Mail worker", t, func() {
+		Convey("its property should be equal to Mail fields", func() {
+			fc := &fakeClient{}
+			c := New(r.Log, fc)
+			So(c, ShouldNotBeNil)
+			m := &Mail{
+				To:       "mehmet@koding.com",
+				Subject:  "Test message",
+				Text:     "Example",
+				From:     "mail@koding.com",
+				FromName: "koding",
+			}
+			err := c.Send(m)
+			So(err, ShouldBeNil)
+			So("mehmet@koding.com", ShouldBeIn, fc.mail.To)
+			So(fc.mail.FromName, ShouldEqual, "koding")
 		})
 	})
 
