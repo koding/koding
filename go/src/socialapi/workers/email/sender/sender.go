e887ecedc9327890f229afed9874075b473cc7fa
mehmetalisavas <mehmetali.savas@yandex.com>
2015-03-04 10:37:22 +0200

    mailsender: implemented basic functionality for mail sender worker

--- go/src/socialapi/workers/email/sender/sender.go
+++ go/src/socialapi/workers/email/sender/sender.go
@@ -3,46 +3,65 @@ package sender
 import (
 	"fmt"
 
-	"github.com/streadway/amqp"
-
+	"github.com/koding/bongo"
 	"github.com/koding/logging"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/streadway/amqp"
 )
 
 // Controller holds required instances for processing events
 type Controller struct {
-	// logging instance
-	log logging.Logger
+	log      logging.Logger
+	sendgrid *sendgrid.SGClient
 }
 
-// New Creates a new controller for realtime package
-func New(log logging.Logger) *Controller {
-	ffc := &Controller{
-		log: log,
+// New Creates a new controller for mail worker
+func New(log logging.Logger, s *sendgrid.SGClient) *Controller {
+	return &Controller{
+		log:      log,
+		sendgrid: s,
 	}
-
-	return ffc
 }
 
-type Mail struct {
-	Body string
-}
-
-func (m Mail) GetId() int64 {
-	return 0
-}
-
-func (m Mail) BongoName() string {
-	return "api.mail"
+func Send(m *Mail) error {
+	return bongo.B.PublishEvent("send", m)
 }
 
 // DefaultErrHandler controls the errors, return false if an error occurred
-func (r *Controller) DefaultErrHandler(delivery amqp.Delivery, err error) bool {
-	r.log.Error("an error occurred deleting realtime event", err)
+func (c *Controller) DefaultErrHandler(delivery amqp.Delivery, err error) bool {
+	c.log.Error("an error occurred %+v", err.Error())
 	delivery.Nack(false, true)
+
 	return false
 }
 
-func (f *Controller) Send(m *Mail) error {
+func (c *Controller) Send(m *Mail) error {
 	fmt.Println(m)
+
+	message := sendgrid.NewMail()
+	if err := message.AddTo(m.To); err != nil {
+		return err
+	}
+
+	if err := message.SetFrom("mail@koding.com"); err != nil {
+		return err
+	}
+
+	if m.From != "" {
+		message.SetFrom(m.From)
+	}
+
+	message.SetText(m.Text)
+	message.SetHTML(m.HTML)
+	message.SetSubject(m.Subject)
+	message.SetFromName(m.FromName)
+	if err := message.SetReplyTo(m.ReplyTo); err != nil {
+		return err
+	}
+
+	if err := c.sendgrid.Send(message); err != nil {
+		return fmt.Errorf("an error occurred while sending email error as %+v ", err.Error())
+	}
+
 	return nil
 }
