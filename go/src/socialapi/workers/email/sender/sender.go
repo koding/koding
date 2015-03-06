b4ba199b4070d11038faa69a69a390a95b5d9b30
mehmetalisavas <mehmetali.savas@yandex.com>
2015-03-07 01:57:34 +0200

    mailsender: some changes on sender

--- go/src/socialapi/workers/email/sender/sender.go
+++ go/src/socialapi/workers/email/sender/sender.go
@@ -11,40 +11,29 @@ import (
 
 // Controller holds required instances for processing events
 type Controller struct {
-	log    logging.Logger
-	client Emailer
+	log logging.Logger
+	//client Emailer
+	emailer *SendGridMail
 }
 
 type Emailer interface {
-	Send(*sendgrid.SGMail) error
+	//Send(*sendgrid.SGMail) error
+	Send(*Mail) error
+}
+
+type SendGridMail struct {
+	sendgrid *sendgrid.SGClient
 }
 
 // New Creates a new controller for mail worker
-func New(log logging.Logger, c Emailer) *Controller {
+func New(log logging.Logger, sg *SendGridMail) *Controller {
 	return &Controller{
-		log:    log,
-		client: c,
+		log:     log,
+		emailer: sg,
 	}
 }
 
-// Send gets the mail struct that includes the message
-// when we call this function, it sends the given mail to the
-// address that will be sent.
-func Send(m *Mail) error {
-	return bongo.B.PublishEvent("send", m)
-}
-
-// DefaultErrHandler controls the errors, return false if an error occurred
-func (c *Controller) DefaultErrHandler(delivery amqp.Delivery, err error) bool {
-	c.log.Error("an error occurred %+v", err.Error())
-	delivery.Nack(false, true)
-
-	return false
-}
-
-// Send creates and sets the message that will be sent,
-// and sends the message according to the mail adress
-func (c *Controller) Send(m *Mail) error {
+func (sg *SendGridMail) Send(m *Mail) error {
 	message := sendgrid.NewMail()
 
 	if err := message.AddTo(m.To); err != nil {
@@ -69,9 +58,67 @@ func (c *Controller) Send(m *Mail) error {
 		}
 	}
 
-	if err := c.client.Send(message); err != nil {
+	if err := sg.sendgrid.Send(message); err != nil {
 		return fmt.Errorf("an error occurred while sending email error as %+v ", err.Error())
 	}
 
 	return nil
+}
+
+// Send gets the mail struct that includes the message
+// when we call this function, it sends the given mail to the
+// address that will be sent.
+func Send(m *Mail) error {
+	return bongo.B.PublishEvent("send", m)
+}
+
+// DefaultErrHandler controls the errors, return false if an error occurred
+func (c *Controller) DefaultErrHandler(delivery amqp.Delivery, err error) bool {
+	c.log.Error("an error occurred %+v", err.Error())
+	delivery.Nack(false, true)
+
+	return false
+}
+
+// Send creates and sets the message that will be sent,
+// and sends the message according to the mail adress
+
+func (c *Controller) Send(m *Mail) error {
+
+	err := c.emailer.Send(m)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+	// message := sendgrid.NewMail()
+
+	// if err := message.AddTo(m.To); err != nil {
+	// 	return err
+	// }
+
+	// if err := message.SetFrom("mail@koding.com"); err != nil {
+	// 	return err
+	// }
+
+	// if m.From != "" {
+	// 	message.SetFrom(m.From)
+	// }
+
+	// message.SetText(m.Text)
+	// message.SetHTML(m.HTML)
+	// message.SetSubject(m.Subject)
+	// message.SetFromName(m.FromName)
+	// if m.ReplyTo != "" {
+	// 	if err := message.SetReplyTo(m.ReplyTo); err != nil {
+	// 		return err
+	// 	}
+	// }
+
+	// if err := c.client.Send(message); err != nil {
+	// 	return fmt.Errorf("an error occurred while sending email error as %+v ", err.Error())
+	// }
+
+	// return nil
 }
