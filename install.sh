63983338502b5a3d60f19bc9b197ff1f0eb1ba86
Fatih Arslan <ftharsln@gmail.com>
2015-03-06 22:30:14 +0200

    install.sh: fix sed substitution

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ if [[ ! "$(uname)" = "Linux" ]]; then
 fi
 
 CHANNEL="development"
-
-echo "Using ${CHANNEL} channel"
-
 LATESTVERSION=$(curl -s https://s3.amazonaws.com/koding-klient/${CHANNEL}/latest-version.txt)
 LATESTURL="https://s3.amazonaws.com/koding-klient/${CHANNEL}/latest/klient_0.1.${LATESTVERSION}_${CHANNEL}_amd64.deb"
 KONTROLURL="https://koding.com/kontrol/kite"
@@ -17,17 +14,17 @@ echo "Downloading and installing klient 0.1.${LATESTVERSION}"
 curl -s $LATESTURL -o klient.deb
 sudo dpkg -i --force-all klient.deb > /dev/null
 
-echo "Authenticate to ${KONTROLURL}"
+echo "Authenticating to ${KONTROLURL}"
 sudo -E /opt/kite/klient/klient -register -kite-home "/etc/kite" --kontrol-url "$KONTROLURL"
 
 if [ ! -f /etc/kite/kite.key ]; then
-    echo "Kite.key not found. Aborting installation"
+    echo "/etc/kite/kite.key not found. Aborting installation"
     exit -1
 fi
 
 # Production kontrol might return a different kontrol URL. Let us control this aspect.
 escaped_var=$(printf '%s\n' "$KONTROLURL" | sed 's:[/&\]:\\&:g;s/$/\\/')
-sudo sed -i "s/\.\/klient -kontrol-url $escaped_var \.\/klient/g/" "/etc/init/klient.conf"
+sudo sed -i "s/\.\/klient/\.\/klient -kontrol-url $escaped_var /g" "/etc/init/klient.conf"
 
 # We need to restart it so it pick up the new environment variable
 sudo service klient restart
