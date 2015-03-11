36aceb05fd5b85b14c7e0dfe8048de0da3dd14c4
mehmetalisavas <mehmetali.savas@yandex.com>
2015-03-11 17:17:36 +0200

    mailsender/refactoring: mailsender is centralized

--- go/src/socialapi/workers/email/emailmodels/mailer.go
+++ go/src/socialapi/workers/email/emailmodels/mailer.go
@@ -4,17 +4,36 @@ import (
 	"errors"
 	"fmt"
 	"socialapi/models"
-
-	"github.com/sendgrid/sendgrid-go"
+	"socialapi/workers/email/sender"
 )
+
+// type Mailer struct {
+// 	UserContact   *UserContact
+// 	Body          string
+// 	Subject       string
+// 	EmailSettings *EmailSettings
+// 	Information   string
+// }
 
 type Mailer struct {
 	UserContact   *UserContact
-	Body          string
-	Subject       string
+	Mail          *sender.Mail
 	EmailSettings *EmailSettings
 	Information   string
 }
+
+// func NewMailer(a *models.Account, es *EmailSettings) (*Mailer, error) {
+// 	// Fetch user contact
+// 	uc, err := FetchUserContactWithToken(a.Id)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return &Mailer{
+// 		UserContact:   uc,
+// 		EmailSettings: es,
+// 	}, nil
+// }
 
 func NewMailer(a *models.Account, es *EmailSettings) (*Mailer, error) {
 	// Fetch user contact
@@ -30,8 +49,8 @@ func NewMailer(a *models.Account, es *EmailSettings) (*Mailer, error) {
 }
 
 func (m *Mailer) SendMail(contentType, body, subject string) error {
-	m.Body = body
-	m.Subject = subject
+	m.Mail.Text = body
+	m.Mail.Subject = subject
 
 	if err := m.validateMailer(); err != nil {
 		return err
@@ -46,32 +65,85 @@ func (m *Mailer) SendMail(contentType, body, subject string) error {
 		return err
 	}
 
-	m.Body = content
+	m.Mail.Text = content
+	m.Mail.To = m.getRecipient()
+	m.Mail.From = m.EmailSettings.DefaultFromMail
+	m.Mail.FromName = m.EmailSettings.DefaultFromName
 
-	sg := sendgrid.NewSendGridClient(m.EmailSettings.Username, m.EmailSettings.Password)
-	fullname := fmt.Sprintf("%s %s", m.UserContact.FirstName, m.UserContact.LastName)
-
-	message := sendgrid.NewMail()
-	message.AddTo(m.getRecipient())
-	message.AddToName(fullname)
-	message.SetSubject(m.Subject)
-	message.SetHTML(m.Body)
-	message.SetFrom(m.EmailSettings.DefaultFromMail)
-	message.SetFromName(m.EmailSettings.DefaultFromName)
-
-	if err := sg.Send(message); err != nil {
+	if err := sender.Send(m.Mail); err != nil {
 		return fmt.Errorf("an error occurred while sending notification email to %s: %s", m.UserContact.Username, err)
 	}
 
 	return nil
+
+	// sg := sendgrid.NewSendGridClient(m.EmailSettings.Username, m.EmailSettings.Password)
+	// fullname := fmt.Sprintf("%s %s", m.UserContact.FirstName, m.UserContact.LastName)
+
+	// message := sendgrid.NewMail()
+	// message.AddTo(m.getRecipient())
+	// message.AddToName(fullname)
+	// message.SetSubject(m.Subject)
+	// message.SetHTML(m.Body)
+	// message.SetFrom(m.EmailSettings.DefaultFromMail)
+	// message.SetFromName(m.EmailSettings.DefaultFromName)
+
+	// if err := sg.Send(message); err != nil {
+	// 	return fmt.Errorf("an error occurred while sending notification email to %s: %s", m.UserContact.Username, err)
+	// }
+
+	// return nil
 }
 
+// func (m *Mailer) SendMail(contentType, body, subject string) error {
+// 	m.Body = body
+// 	m.Subject = subject
+
+// 	if err := m.validateMailer(); err != nil {
+// 		return err
+// 	}
+
+// 	if err := m.UserContact.GenerateToken(contentType); err != nil {
+// 		return err
+// 	}
+
+// 	content, err := m.prepareContentWithLayout(contentType)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	m.Body = content
+
+// 	sg := sendgrid.NewSendGridClient(m.EmailSettings.Username, m.EmailSettings.Password)
+// 	fullname := fmt.Sprintf("%s %s", m.UserContact.FirstName, m.UserContact.LastName)
+
+// 	message := sendgrid.NewMail()
+// 	message.AddTo(m.getRecipient())
+// 	message.AddToName(fullname)
+// 	message.SetSubject(m.Subject)
+// 	message.SetHTML(m.Body)
+// 	message.SetFrom(m.EmailSettings.DefaultFromMail)
+// 	message.SetFromName(m.EmailSettings.DefaultFromName)
+
+// 	if err := sg.Send(message); err != nil {
+// 		return fmt.Errorf("an error occurred while sending notification email to %s: %s", m.UserContact.Username, err)
+// 	}
+
+// 	return nil
+// }
+
 func (m *Mailer) prepareContentWithLayout(contentType string) (string, error) {
-	lc := NewLayoutContent(m.UserContact, contentType, m.Body)
+	lc := NewLayoutContent(m.UserContact, contentType, m.Mail.Text)
 	lc.Information = m.Information
 
 	return lc.Render()
 }
+
+// func (m *Mailer) prepareContentWithLayout(contentType string) (string, error) {
+// 	lc := NewLayoutContent(m.UserContact, contentType, m.Body)
+// 	lc.Information = m.Information
+
+// 	return lc.Render()
+// }
 
 func (m *Mailer) getRecipient() string {
 	if m.EmailSettings.ForcedRecipient != "" {
@@ -81,12 +153,32 @@ func (m *Mailer) getRecipient() string {
 	return m.UserContact.Email
 }
 
+// func (m *Mailer) validateMailer() error {
+// 	if m.Body == "" {
+// 		return errors.New("Mailer body is not set")
+// 	}
+
+// 	if m.Subject == "" {
+// 		return errors.New("Mailer subject is not set")
+// 	}
+
+// 	if m.EmailSettings == nil {
+// 		return errors.New("Mailer email settings is not set")
+// 	}
+
+// 	if m.UserContact == nil {
+// 		return errors.New("User contact settings is not set")
+// 	}
+
+// 	return nil
+// }
+
 func (m *Mailer) validateMailer() error {
-	if m.Body == "" {
+	if m.Mail.Text == "" {
 		return errors.New("Mailer body is not set")
 	}
 
-	if m.Subject == "" {
+	if m.Mail.Subject == "" {
 		return errors.New("Mailer subject is not set")
 	}
 
