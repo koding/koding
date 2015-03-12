42c41c7b2db0f991f40d54c3b3dcd392bbcf2a3e
Fatih Arslan <fatih@koding.com>
2015-03-12 09:56:54 +0200

    go: update sendgrid-go, fixes a nasty bug of accumulating CLOSE_WAIT sockets

--- go/src/github.com/sendgrid/sendgrid-go/sendgrid.go
+++ go/src/github.com/sendgrid/sendgrid-go/sendgrid.go
@@ -7,8 +7,11 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
+
+const Version = "1.1.0"
 
 func timeoutHandler(network, address string) (net.Conn, error) {
 	return net.DialTimeout(network, address, time.Duration(5*time.Second))
@@ -57,6 +60,9 @@ func (sg *SGClient) buildURL(m *SGMail) (url.Values, error) {
 	for i := 0; i < len(m.To); i++ {
 		values.Add("to[]", m.To[i])
 	}
+	for i := 0; i < len(m.Cc); i++ {
+		values.Add("cc[]", m.Cc[i])
+	}
 	for i := 0; i < len(m.Bcc); i++ {
 		values.Add("bcc[]", m.Bcc[i])
 	}
@@ -87,14 +93,24 @@ func (sg *SGClient) Send(m *SGMail) error {
 	if e != nil {
 		return e
 	}
-	r, e := sg.Client.PostForm(sg.APIMail, values)
+	req, e := http.NewRequest("POST", sg.APIMail, strings.NewReader(values.Encode()))
 	if e != nil {
-		return fmt.Errorf("sendgrid.go: error:%v; response:%v", e, r)
+		return e
 	}
-	if r.StatusCode == http.StatusOK {
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("User-Agent", "sendgrid/"+Version+";go")
+	res, e := sg.Client.Do(req)
+	if e != nil {
+		return fmt.Errorf("sendgrid.go: error:%v; response:%v", e, res)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusOK {
 		return nil
 	}
-	body, _ := ioutil.ReadAll(r.Body)
-	r.Body.Close()
-	return fmt.Errorf("sendgrid.go: code:%d error:%v body:%s", r.StatusCode, e, body)
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	return fmt.Errorf("sendgrid.go: code:%d error:%v body:%s", res.StatusCode, e, body)
 }
