ed2987acaddb361844ee49f4c6b79d3aed41c9e8
Cihangir SAVAS <cihangir@koding.com>
2015-03-15 23:55:47 -0700

    Runner: runner has its own init system now

---
+++ go/src/socialapi/workers/common/runner/redis.go
@@ -0,0 +1,37 @@
+package runner
+
+import "github.com/koding/redis"
+
+var redisConn *redis.RedisSession
+
+func MustInitRedisConn(c *Config) *redis.RedisSession {
+	r, err := InitRedisConn(c)
+	if err != nil {
+		panic(err)
+	}
+
+	return r
+}
+
+func InitRedisConn(c *Config) (*redis.RedisSession, error) {
+	// if redisconn is already created, return early, return ofthen
+	if redisConn != nil {
+		return redisConn, nil
+	}
+
+	r, err := redis.NewRedisSession(&redis.RedisConf{Server: c.Redis.URL, DB: c.Redis.DB})
+	if err != nil {
+		return nil, err
+	}
+
+	redisConn = r
+	return r, nil
+}
+
+func MustGetRedisConn() *redis.RedisSession {
+	if redisConn == nil {
+		panic("Redis connection is nil You should call \"MustInitRedisConn(server string)\" first")
+	}
+
+	return redisConn
+}
