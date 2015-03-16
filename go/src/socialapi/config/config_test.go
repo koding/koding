e4af465b215de7486b5729f2a88f678ef8ee11f8
Cihangir SAVAS <cihangir@koding.com>
2015-03-15 23:56:43 -0700

    Socialapi: fix tests

--- go/src/socialapi/config/config_test.go
+++ go/src/socialapi/config/config_test.go
@@ -10,8 +10,6 @@ import (
 const (
 	testConfigPath = "./test.toml"
 	env            = "MehmetAli"
-	hostname       = "Koding"
-	port           = 5672
 )
 
 func TestConfigMustRead(t *testing.T) {
@@ -24,18 +22,12 @@ func TestConfigMustRead(t *testing.T) {
 			So(func() { MustRead("testConfigPath") }, ShouldPanic)
 		})
 		Convey("setting socialapi env should override config", func() {
-			err := os.Setenv("KONFIG_SOCIALAPI_ENVIRONMENT", env)
+			err := os.Setenv("KONFIG_SOCIALAPI_PROTOCOL", env)
 			So(err, ShouldBeNil)
 			// just to be sure about the function will not panic
 			So(func() { MustRead(testConfigPath) }, ShouldNotPanic)
 			a := MustRead(testConfigPath)
-			So(a.Environment, ShouldEqual, env)
-		})
-		Convey("setting socialapi hostname should override config", func() {
-			err := os.Setenv("KONFIG_SOCIALAPI_HOSTNAME", hostname)
-			So(err, ShouldBeNil)
-			aPath := MustRead(testConfigPath)
-			So(aPath.Hostname, ShouldEqual, hostname)
+			So(a.Protocol, ShouldEqual, env)
 		})
 	})
 }
