6278ff0ec65c597f3117ff54574e3244decd59c5
Cihangir SAVAS <cihangir@koding.com>
2015-03-16 15:54:07 -0700

    socialapi: added new test case for moderation needed topic search operations

--- go/src/socialapi/workers/topicfeed/topicfeed/topicfeed_test.go
+++ go/src/socialapi/workers/topicfeed/topicfeed/topicfeed_test.go
@@ -1,10 +1,15 @@
 package topicfeed
 
 import (
+	"fmt"
 	"math/rand"
 	"socialapi/models"
+	"socialapi/request"
+	"socialapi/workers/common/runner"
+	"socialapi/workers/topicfeed/topicfeed"
 	"testing"
 
+	"github.com/koding/bongo"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -49,6 +54,76 @@ func TestIsEligible(t *testing.T) {
 				eligible, err := isEligible(c)
 				So(err, ShouldBeNil)
 				So(eligible, ShouldBeTrue)
+			})
+		})
+	})
+}
+
+func TestMessageSaved(t *testing.T) {
+	r := runner.New("test")
+	if err := r.Init(); err != nil {
+		t.Fatalf("couldnt start bongo %s", err.Error())
+	}
+	defer r.Close()
+
+	controller := topicfeed.New(r.Log)
+
+	Convey("while testing MessageSaved", t, func() {
+
+		Convey("newly created channels should be marked with needs moderation", func() {
+			account := models.CreateAccountWithTest()
+			groupChannel := models.CreateTypedPublicChannelWithTest(account.Id, models.Channel_TYPE_GROUP)
+
+			// just a random topic name
+			topicName := models.RandomName()
+			fmt.Println("topicName-->", topicName)
+			c := models.NewChannelMessage()
+			c.InitialChannelId = groupChannel.Id
+			c.AccountId = account.Id
+			c.Body = "my test topic #" + topicName
+			c.TypeConstant = models.ChannelMessage_TYPE_POST
+
+			// create with unscoped
+			err := bongo.B.Unscoped().Table(c.TableName()).Create(c).Error
+			So(err, ShouldBeNil)
+
+			So(controller.MessageSaved(c), ShouldBeNil)
+
+			Convey("we should not be able to find them", func() {
+				channels, err := models.NewChannel().Search(&request.Query{
+					Name:      topicName,
+					GroupName: groupChannel.GroupName,
+					AccountId: account.Id,
+				})
+				So(err, ShouldBeNil)
+				So(len(channels), ShouldEqual, 0)
+			})
+
+			Convey("after removing needs moderation flag", func() {
+				// byname doesnt filter
+				channel, err := models.NewChannel().ByName(&request.Query{
+					Name:      topicName,
+					GroupName: groupChannel.GroupName,
+					AccountId: account.Id,
+				})
+				So(err, ShouldBeNil)
+				So(channel, ShouldNotBeNil)
+
+				channel.MetaBits.UnMark(models.NeedsModeration)
+
+				So(channel.Update(), ShouldBeNil)
+
+				Convey("we should be able to search them", func() {
+					channels, err := models.NewChannel().Search(&request.Query{
+						Name:      topicName,
+						GroupName: groupChannel.GroupName,
+						AccountId: account.Id,
+						Privacy:   channel.PrivacyConstant,
+					})
+					So(err, ShouldBeNil)
+					So(len(channels), ShouldEqual, 1)
+					So(channels[0].Id, ShouldEqual, channel.Id)
+				})
 			})
 		})
 	})
