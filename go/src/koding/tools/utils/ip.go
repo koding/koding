c4db347ffe82c8a5764e69b0be5abc1df9895082
mehmetalisavas <mehmetali.savas@yandex.com>
2015-03-18 19:56:34 +0200

    socialapi/postip: utils package is moved to another path

--- go/src/koding/tools/utils/ip.go
+++ go/src/koding/tools/utils/ip.go
@@ -5,11 +5,6 @@ import (
 	"strings"
 )
 
-type ipAddress string
-
-// IP address helpers taken
-// from https://groups.google.com/forum/#!topic/golang-nuts/lomWKs0kOfE
-
 // Request.RemoteAddress contains port, which we want to remove i.e.:
 // "[::1]:58292" => "[::1]"
 func ipAddrFromRemoteAddr(s string) string {
@@ -28,12 +23,12 @@ func GetIpAddress(r *http.Request) string {
 		return ipAddrFromRemoteAddr(r.RemoteAddr)
 	}
 	if hdrForwardedFor != "" {
-		// X-Forwarded-For is potentially a list of addresses separated with ","
+
 		parts := strings.Split(hdrForwardedFor, ",")
 		for i, p := range parts {
 			parts[i] = strings.TrimSpace(p)
 		}
-		// TODO: should return first non-local address
+
 		return parts[0]
 	}
 	return hdrRealIp
