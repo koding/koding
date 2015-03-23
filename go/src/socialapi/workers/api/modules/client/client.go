2e32c192671beb38d0cc6137b6e2f9df61c815b1
mehmetalisavas <mehmetali.savas@yandex.com>
2015-03-23 18:16:46 +0200

    workers/modules: client file is added to modules for geoip

---
+++ go/src/socialapi/workers/api/modules/client/client.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"net/http"
+	"net/url"
+	"socialapi/models"
+	"socialapi/workers/common/response"
+	"socialapi/workers/helper"
+)
+
+// Location returns the current approximate location of the requester
+func Location(u *url.URL, h http.Header, c *models.Context) {
+	record, err := helper.MustGetGeoIPDB().City(c.Client.IP)
+	if err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	return response.NewOK(
+		map[string]interface{}{
+			"location": record.City.Names["en"],
+		},
+	)
+}
