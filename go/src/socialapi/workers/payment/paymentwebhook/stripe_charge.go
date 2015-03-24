a4983ae85846489bbcccbb28219475d3810c479d
Senthil <senthil@koding.com>
2015-03-24 12:57:44 -0700

    payment: make getting amount/refunded easier to read

--- go/src/socialapi/workers/payment/paymentwebhook/stripe_charge.go
+++ go/src/socialapi/workers/payment/paymentwebhook/stripe_charge.go
@@ -6,22 +6,22 @@ import (
 )
 
 func stripePaymentRefunded(raw []byte, c *Controller) error {
-	amountGetter := func(req *webhookmodels.StripeCharge) string {
-		return formatStripeAmount(req.Currency, req.AmountRefunded)
+	amountGetter := func(req *webhookmodels.StripeCharge) float64 {
+		return req.AmountRefunded
 	}
 
 	return stripeChargeHelper(raw, c, PaymentRefunded, amountGetter)
 }
 
 func stripePaymentFailed(raw []byte, c *Controller) error {
-	amountGetter := func(req *webhookmodels.StripeCharge) string {
-		return formatStripeAmount(req.Currency, req.Amount)
+	amountGetter := func(req *webhookmodels.StripeCharge) float64 {
+		return req.Amount
 	}
 
 	return stripeChargeHelper(raw, c, PaymentFailed, amountGetter)
 }
 
-type amountGetterFn func(req *webhookmodels.StripeCharge) string
+type amountGetterFn func(req *webhookmodels.StripeCharge) float64
 
 func stripeChargeHelper(raw []byte, c *Controller, act Action, aFn amountGetterFn) error {
 	var req *webhookmodels.StripeCharge
@@ -36,7 +36,8 @@ func stripeChargeHelper(raw []byte, c *Controller, act Action, aFn amountGetterF
 		return err
 	}
 
-	opts := map[string]interface{}{"price": aFn(req)}
+	amount := formatStripeAmount(req.Currency, aFn(req))
+	opts := map[string]interface{}{"price": amount}
 
 	Log.Info("Stripe: Sent invoice email to: %s", user.Email)
 
