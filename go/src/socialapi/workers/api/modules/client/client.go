0c22d05fd861315cd920377359c7b25746239145
mehmetalisavas <mehmetali.savas@yandex.com>
2015-03-24 14:41:52 +0200

    geoip: return arguments are don correctly

--- go/src/socialapi/workers/api/modules/client/client.go
+++ go/src/socialapi/workers/api/modules/client/client.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Location returns the current approximate location of the requester
-func Location(u *url.URL, h http.Header, c *models.Context) {
+func Location(u *url.URL, h http.Header, c *models.Context) (int, http.Header, interface{}, error) {
 	record, err := helper.MustGetGeoIPDB().City(c.Client.IP)
 	if err != nil {
 		return response.NewBadRequest(err)
