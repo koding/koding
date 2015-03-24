1f86cab57881b36a4df1062d335a98f7d61a29b4
Cihangir SAVAS <cihangir@koding.com>
2015-03-24 11:05:59 -0700

    Socialapi: added deleted handler back

--- go/src/socialapi/workers/moderation/topic/api/handlers.go
+++ go/src/socialapi/workers/moderation/topic/api/handlers.go
@@ -21,6 +21,16 @@ func AddHandlers(m *mux.Mux, metric *metrics.Metrics) {
 
 	m.AddHandler(
 		handler.Request{
+			Handler:  DeleteLink,
+			Name:     models.ModerationChannelDeleteLink,
+			Type:     handler.DeleteRequest,
+			Endpoint: "/moderation/channel/{rootId}/link/{leafId}",
+			Metrics:  metric,
+		},
+	)
+
+	m.AddHandler(
+		handler.Request{
 			Handler:  GetLinks,
 			Name:     models.ModerationChannelGetLink,
 			Type:     handler.GetRequest,
