0407e7c18c0b9b415c2bb13817d5ef4f010d3fa7
Cihangir SAVAS <cihangir@koding.com>
2015-03-31 20:31:38 -0700

    npm/package: block usage of ^ * and ~ in package json files

---
+++ scripts/check_package_json_constraints.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# package json should not have special npm chars
+if grep -q  '\"^\|\"~\|\"\*' ./package.json; then
+    echo "package json has (^,*,~) in it, commit without them."
+    exit 1;
+else
+    exit 0;
+fi
+
+# client package json should not have special npm chars
+if grep -q  '\"^\|\"~\|\"\*' ./client/package.json; then
+    echo "package json has (^,*,~) in it, commit without them."
+    exit 1;
+else
+    exit 0;
+fi
