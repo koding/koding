ad734e26033864c44db05bfc68ce55d3c246c1e3
Cihangir SAVAS <cihangir@koding.com>
2015-03-31 20:33:56 -0700

    npm/package: more descriptive error

--- scripts/check_package_json_constraints.sh
+++ scripts/check_package_json_constraints.sh
@@ -10,7 +10,7 @@ fi
 
 # client package json should not have special npm chars
 if grep -q  '\"^\|\"~\|\"\*' ./client/package.json; then
-    echo "package json has (^,*,~) in it, commit without them."
+    echo "client package json has (^,*,~) in it, commit without them."
     exit 1;
 else
     exit 0;
