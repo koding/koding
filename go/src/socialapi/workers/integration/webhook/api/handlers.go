30a81677b2347fdc0639225492f8fd074c9c7226
Can Yucel <can@koding.com>
2015-04-13 14:47:44 -0700

    webhook: move webhook and handlers files under api package

--- go/src/socialapi/workers/integration/webhook/api/handlers.go
+++ go/src/socialapi/workers/integration/webhook/api/handlers.go
@@ -1,4 +1,4 @@
-package webhook
+package api
 
 import (
 	"socialapi/workers/common/handler"
--- go/src/socialapi/workers/integration/webhook/api/webhook.go
+++ go/src/socialapi/workers/integration/webhook/api/webhook.go
@@ -1,10 +1,11 @@
-package webhook
+package api
 
 import (
 	"errors"
 	"net/http"
 	"net/url"
 	"socialapi/workers/common/response"
+	"socialapi/workers/integration/webhook"
 
 	"github.com/koding/logging"
 )
@@ -13,29 +14,37 @@ var (
 	ErrBodyNotSet    = errors.New("body is not set")
 	ErrChannelNotSet = errors.New("channel is not set")
 	ErrTokenNotSet   = errors.New("token is not set")
+	ErrGroupNotSet   = errors.New("group name is not set")
 )
 
 type WebhookRequest struct {
-	Body        string
-	ChannelName string
-	Token       string
+	*webhook.Message
+	GroupName string
+	Token     string
 }
 
 type Handler struct {
 	log logging.Logger
+	bot *webhook.Bot
 }
 
-func NewHandler(l logging.Logger) *Handler {
+func NewHandler(l logging.Logger) (*Handler, error) {
+	bot, err := webhook.NewBot()
+	if err != nil {
+		return nil, err
+	}
+
 	return &Handler{
 		log: l,
-	}
+		bot: bot,
+	}, nil
 }
 
 func (h *Handler) Push(u *url.URL, header http.Header, r *WebhookRequest) (int, http.Header, interface{}, error) {
 	val := u.Query().Get("token")
 	r.Token = val
 
-	if err := validateRequest(r); err != nil {
+	if err := r.validate(); err != nil {
 		return response.NewBadRequest(err)
 	}
 
@@ -48,10 +57,14 @@ func (h *Handler) Push(u *url.URL, header http.Header, r *WebhookRequest) (int, 
 		return response.NewAccessDenied(errors.New(""))
 	}
 
-	return response.NewNotImplemented()
+	if err := h.bot.SendMessage(r.Message); err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	return response.NewOK(nil)
 }
 
-func validateRequest(r *WebhookRequest) error {
+func (r *WebhookRequest) validate() error {
 	if r.Token == "" {
 		return ErrTokenNotSet
 	}
@@ -60,14 +73,22 @@ func validateRequest(r *WebhookRequest) error {
 		return ErrBodyNotSet
 	}
 
-	if r.ChannelName == "" {
+	// TODO we don't need this ChannelName, it will remain
+	// until we add TeamInteraction tables
+	if r.ChannelId == 0 {
 		return ErrChannelNotSet
 	}
 
-	return nil
+	if r.GroupName == "" {
+		return ErrGroupNotSet
+	}
 
+	return nil
 }
 
 var verifyRequest = func(r *WebhookRequest) (bool, error) {
+	// verify token
+
+	// verify channel id
 	return false, nil
 }
