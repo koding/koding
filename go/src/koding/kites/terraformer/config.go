0e2e6e54e1b764b3d04a8cef170388ff93d4ac28
Cihangir SAVAS <cihangir@koding.com>
2015-04-14 15:53:11 -0700

    Terraformer: remove unnecessary configs

--- go/src/koding/kites/terraformer/config.go
+++ go/src/koding/kites/terraformer/config.go
@@ -2,9 +2,6 @@ package terraformer
 
 // Config defines the configuration.
 type Config struct {
-	// IP
-	IP string `required:"true"`
-
 	// Port
 	Port int `required:"true"`
 
@@ -16,9 +13,4 @@ type Config struct {
 
 	// Enable debug mode
 	Debug bool
-
-	KontrolURL string `required:"true"`
-
-	// Public
-	Public bool // Try to register with a public ip
 }
