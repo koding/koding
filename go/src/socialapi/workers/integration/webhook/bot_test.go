97e84832459f951c078055a4cd7e93b00b322134
Can Yucel <can@koding.com>
2015-04-16 16:16:18 -0700

    webhook: change payload field name to channelIntegration for channel message

--- go/src/socialapi/workers/integration/webhook/bot_test.go
+++ go/src/socialapi/workers/integration/webhook/bot_test.go
@@ -46,7 +46,7 @@ func TestSendMessage(t *testing.T) {
 			message := &Message{}
 			message.Body = "testmessage"
 			message.ChannelId = channel.Id
-			message.TeamIntegrationId = 13
+			message.ChannelIntegrationId = 13
 			err := bot.SendMessage(message)
 			So(err, ShouldBeNil)
 
@@ -57,7 +57,7 @@ func TestSendMessage(t *testing.T) {
 			So(m.InitialChannelId, ShouldEqual, message.ChannelId)
 			So(m.AccountId, ShouldEqual, bot.account.Id)
 			So(m.MetaBits, ShouldEqual, models.Bot)
-			So(*(m.GetPayload("teamIntegrationId")), ShouldEqual, "13")
+			So(*(m.GetPayload("channelIntegrationId")), ShouldEqual, "13")
 
 		})
 	})
