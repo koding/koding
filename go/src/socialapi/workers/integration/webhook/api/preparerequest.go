023fc3b43e1ef33ed8be486e334b374e784da4cd
Can Yucel <can@koding.com>
2015-04-19 17:13:43 -0700

    webhook: add preparerequest struct for using prepare handler input data

---
+++ go/src/socialapi/workers/integration/webhook/api/preparerequest.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"socialapi/workers/integration/webhook"
+	"socialapi/workers/integration/webhook/services"
+)
+
+type PrepareRequest struct {
+	Data  *services.ServiceInput
+	Token string
+	Name  string
+}
+
+func (p *PrepareRequest) validate() error {
+	if p.Token == "" {
+		return ErrTokenNotSet
+	}
+
+	if p.Name == "" {
+		return ErrNameNotSet
+	}
+
+	return nil
+}
+
+func (p *PrepareRequest) verify() (*webhook.Integration, error) {
+	i := webhook.NewIntegration()
+	err := i.ByName(p.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return i, nil
+}
