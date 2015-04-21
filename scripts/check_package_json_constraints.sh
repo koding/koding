66db0f032b6ea8b44308502472a3e301a5e04db3
Cihangir SAVAS <cihangir@koding.com>
2015-04-21 11:06:53 -0700

    Scripts: fix checker logic

--- scripts/check_package_json_constraints.sh
+++ scripts/check_package_json_constraints.sh
@@ -4,12 +4,7 @@
 if grep -q  '\"^\|\"~\|\"\*' ./package.json; then
     echo "package json has (^,*,~) in it, commit without them."
     exit 1;
-else
-    exit 0;
-fi
-
-# client package json should not have special npm chars
-if grep -q  '\"^\|\"~\|\"\*' ./client/package.json; then
+elif grep -q  '\"^\|\"~\|\"\*' ./client/package.json; then
     echo "client package json has (^,*,~) in it, commit without them."
     exit 1;
 else
