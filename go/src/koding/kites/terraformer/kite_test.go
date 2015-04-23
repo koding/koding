d3f16c1411b3debf858c9fd45eed691ec7e42efe
Cihangir SAVAS <cihangir@koding.com>
2015-04-22 22:47:27 -0700

    Terraformer: init test kite with full functionality

--- go/src/koding/kites/terraformer/kite_test.go
+++ go/src/koding/kites/terraformer/kite_test.go
@@ -1,6 +1,11 @@
 package terraformer
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"runtime/debug"
 	"testing"
 
 	"koding/kites/common"
@@ -9,6 +14,9 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/koding/kite"
 	"github.com/koding/multiconfig"
+	"github.com/kr/pretty"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
 )
 
 var variables = map[string]string{
@@ -21,15 +29,50 @@ var variables = map[string]string{
 
 func withKite(t *testing.T, f func(k *kite.Kite) error) {
 	conf := &Config{}
-
 	// Load the config, reads environment variables or from flags
 	multiconfig.New().MustLoad(conf)
 
+	if !conf.Debug {
+		// hashicorp.terraform outputs many logs, discard them
+		log.SetOutput(ioutil.Discard)
+	}
+
 	log := common.NewLogger(Name, conf.Debug)
 
-	c, err := kodingcontext.Init()
+	// init s3 auth
+	awsAuth, err := aws.GetAuth(conf.AWS.Key, conf.AWS.Secret)
 	if err != nil {
-		t.Errorf("err while creating koding context %s", err.Error())
+		log.Fatal(err.Error())
+	}
+
+	// we are only using us east
+	awsS3Bucket := s3.New(awsAuth, aws.USEast).Bucket(conf.AWS.Bucket)
+
+	if err := awsS3Bucket.PutBucket(s3.Private); err != nil {
+		if s3err, ok := err.(*s3.Error); ok {
+			t.Errorf("s3err %# v", pretty.Formatter(s3err))
+		} else {
+			t.Errorf("err while creating bucket %s", err)
+		}
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Errorf("err while getting working dir %s", err)
+	}
+
+	localFileBasePath := path.Join(wd, conf.AWS.Bucket)
+
+	if err := os.MkdirAll(localFileBasePath, os.ModePerm); err != nil {
+		t.Errorf("err while creating local folder %s", err)
+	}
+
+	ls := kodingcontext.NewFileStorage(localFileBasePath)
+	rs := kodingcontext.NewS3Storage(awsS3Bucket)
+
+	c, err := kodingcontext.Init(ls, rs)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 	defer kodingcontext.Close()
 
@@ -38,11 +81,13 @@ func withKite(t *testing.T, f func(k *kite.Kite) error) {
 		t.Errorf("err while creating kite %s", err.Error())
 	}
 	k.Config.DisableAuthentication = true
+
 	go k.Run()
 	defer k.Close()
 	<-k.ServerReadyNotify()
 
 	if err := f(k); err != nil {
+		debug.PrintStack()
 		t.Errorf("failed with %s", err.Error())
 	}
 }
