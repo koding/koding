4d2e9c27e9db2ea3dff7daf3bb4ab55da6c2c22f
mehmetalisavas <mehmetali.savas@yandex.com>
2015-04-23 03:42:53 +0300

    socialaapi: likedmessage func is added to rest

--- go/src/socialapi/rest/interactions.go
+++ go/src/socialapi/rest/interactions.go
@@ -50,17 +50,19 @@ func DeleteInteraction(interactionType string, postId, accountId int64) error {
 }
 
 func ListInteractedMesssagesInteraction(iType string, accountId int64, token string) ([]*models.ChannelMessageContainer, error) {
-	url := fmt.Sprintf("/account/%d/interactions/%s", accountId, iType)
+	url := fmt.Sprintf("/account/%d/interaction/%s", accountId, iType)
 
-	var cm []*models.ChannelMessageContainer
 	res, err := sendRequestWithAuth("GET", url, nil, token)
 	if err != nil {
-		return cm, err
+		return nil, err
 	}
 
+	// var cm []*models.ChannelMessageContainer
+	cm := make([]*models.ChannelMessageContainer, 0)
 	err = json.Unmarshal(res, &cm)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(cm)
 	return cm, nil
 }
