60c45566cac4f7bea81772953ec76ec4c597710e
Cihangir SAVAS <cihangir@koding.com>
2015-04-22 22:55:25 -0700

    Terraformer: update main file with the latest structure

--- go/src/koding/kites/cmd/terraformer/main.go
+++ go/src/koding/kites/cmd/terraformer/main.go
@@ -6,8 +6,12 @@ import (
 	"koding/kites/terraformer"
 	"koding/kites/terraformer/kodingcontext"
 	"log"
+	"os"
+	"path"
 
 	"github.com/koding/multiconfig"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
 )
 
 func main() {
@@ -22,22 +26,43 @@ func main() {
 
 	log := common.NewLogger(terraformer.Name, conf.Debug)
 
-	c, err := kodingcontext.Init()
+	// init s3 auth
+	awsAuth, err := aws.GetAuth(conf.AWS.Key, conf.AWS.Secret)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// we are only using us east
+	awsS3Bucket := s3.New(awsAuth, aws.USEast).Bucket(conf.AWS.Bucket)
+
+	if err := awsS3Bucket.PutBucket(s3.Private); err != nil {
+		if s3err, ok := err.(*s3.Error); ok {
+			log.Fatal("s3err %# v", s3err)
+		} else {
+			log.Fatal("err while creating bucket %s", err)
+		}
+	}
+
+	//TODO(cihangir) find a better place to store the temp files
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("err while getting working dir %s", err)
+	}
+
+	localFileBasePath := path.Join(wd, conf.AWS.Bucket)
+
+	if err := os.MkdirAll(localFileBasePath, os.ModePerm); err != nil {
+		log.Fatal("err while creating local folder %s", err)
+	}
+
+	ls := kodingcontext.NewFileStorage(conf.AWS.Bucket)
+	rs := kodingcontext.NewS3Storage(awsS3Bucket)
+
+	c, err := kodingcontext.Init(ls, rs)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	defer kodingcontext.Close()
-
-	// init s3 auth
-	// awsAuth, err := aws.GetAuth(conf.AWS.Key, conf.AWS.Secret)
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// we are only using us east
-	// awsS3Bucket := s3.New(awsAuth, aws.USEast).Bucket(conf.AWS.Bucket)
-	// fmt.Println("awsS3Bucket-->", awsS3Bucket)
-	c.Storage = kodingcontext.FileStorage{}
 
 	k, err := terraformer.NewKite(conf, c, log)
 	if err != nil {
