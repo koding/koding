8d20296034a8dfc5b7bee3363000183132a5d14e
Can Yucel <can@koding.com>
2015-04-24 11:43:28 -0700

    webhook: implement Output method in iterable service

--- go/src/socialapi/workers/integration/webhook/services/iterable.go
+++ go/src/socialapi/workers/integration/webhook/services/iterable.go
@@ -1,14 +1,35 @@
 package services
 
+import "fmt"
+
 type Iterable struct {
+	Email      string
+	EventName  string
+	DataFields map[string]interface{}
+	CampaignId string
 }
 
-func NewIterable() Iterable {
-	return Iterable{}
+func NewIterable(input *ServiceInput) (Iterable, error) {
+	i := Iterable{}
+
+	// for now just getting
+	email := input.Key("email")
+	i.Email, _ = email.(string)
+
+	eventName := input.Key("eventName")
+	i.EventName, _ = eventName.(string)
+
+	dataFields := input.Key("dateFields")
+	i.DataFields, _ = dataFields.(map[string]interface{})
+
+	campaignId := input.Key("campaignId")
+	i.CampaignId, _ = campaignId.(string)
+
+	return i, nil
 }
 
 func (i Iterable) PrepareMessage(input *ServiceInput) string {
-	return ""
+	return fmt.Sprintf("You received %s event in our %s campaign", i.EventName, i.CampaignId)
 }
 
 func (i Iterable) Validate(input *ServiceInput) []error {
@@ -16,5 +37,19 @@ func (i Iterable) Validate(input *ServiceInput) []error {
 }
 
 func (i Iterable) PrepareEndpoint(token string) string {
-	return ""
+
+	return fmt.Sprintf("/api/integration/webhook/push/%s", token)
+}
+
+func (i Iterable) Output(input *ServiceInput) *ServiceOutput {
+	val := input.Key("groupName")
+	groupName, _ := val.(string)
+	if groupName == "" {
+		groupName = "koding"
+	}
+
+	return &ServiceOutput{
+		Email:     i.Email,
+		GroupName: groupName,
+	}
 }
