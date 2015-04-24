642f92e02f68d5c4316658fb75d2100a31e76ab0
Can Yucel <can@koding.com>
2015-04-24 10:35:36 -0700

    webhook: change ServiceFactory.Create method signature

--- go/src/socialapi/workers/integration/webhook/services/servicefactory.go
+++ go/src/socialapi/workers/integration/webhook/services/servicefactory.go
@@ -4,28 +4,19 @@ import "errors"
 
 var ErrServiceNotFound = errors.New("service is not found")
 
-type ServiceFactory struct {
-	services map[string]Service
-}
+type ServiceFactory struct{}
 
 func NewServiceFactory() *ServiceFactory {
 	sf := &ServiceFactory{}
-	sf.services = getServices()
 
 	return sf
 }
 
-func getServices() map[string]Service {
-	return map[string]Service{
-		"iterable": NewIterable(),
-	}
-}
-
-func (sf *ServiceFactory) Create(serviceName string) (Service, error) {
-	service, ok := sf.services[serviceName]
-	if !ok {
-		return nil, ErrServiceNotFound
+func (sf *ServiceFactory) Create(serviceName string, input *ServiceInput) (Service, error) {
+	switch serviceName {
+	case "iterable":
+		return NewIterable(input)
 	}
 
-	return service, nil
+	return nil, ErrServiceNotFound
 }
--- go/src/socialapi/workers/integration/webhook/services/servicefactory_test.go
+++ go/src/socialapi/workers/integration/webhook/services/servicefactory_test.go
@@ -10,13 +10,13 @@ func TestServiceFactory(t *testing.T) {
 	Convey("while testing service factory", t, func() {
 		Convey("it should return error when service does not implemented", func() {
 			sf := NewServiceFactory()
-			_, err := sf.Create("heleley")
+			_, err := sf.Create("heleley", &ServiceInput{})
 			So(err, ShouldEqual, ErrServiceNotFound)
 		})
 
 		Convey("it should able to return iterable service", func() {
 			sf := NewServiceFactory()
-			_, err := sf.Create("iterable")
+			_, err := sf.Create("iterable", &ServiceInput{})
 			So(err, ShouldBeNil)
 		})
 	})
