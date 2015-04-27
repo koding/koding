846677119030352ff634902d692560c38a97276e
Can Yucel <can@koding.com>
2015-04-26 18:51:25 -0700

    webhook: add integration test

---
+++ go/src/socialapi/workers/integration/webhook/tests/integration_test.go
@@ -0,0 +1,114 @@
+package tests
+
+import (
+	"koding/db/mongodb/modelhelper"
+	"socialapi/config"
+	"socialapi/models"
+	"socialapi/request"
+	"socialapi/rest"
+	"socialapi/workers/integration/webhook"
+	"socialapi/workers/integration/webhook/api"
+	"socialapi/workers/integration/webhook/services"
+	"testing"
+
+	"github.com/koding/runner"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func newPrepareRequest(email string) *services.ServiceInput {
+	return &services.ServiceInput{
+		"email":     email,
+		"eventName": "emailOpen",
+		"dataFields": map[string]interface{}{
+			"templateId": float64(15120),
+			"device":     "Gmail",
+			"createdAt":  "2015-04-24 21:35:02 +00:00",
+			"campaignId": float64(5654),
+			"userAgent":  "Mozilla/5.0 (Windows NT 5.1; rv:11.0) Gecko Firefox/11.0 (via ggpht.com GoogleImageProxy)",
+			"ip":         "66.249.93.139",
+		},
+	}
+}
+
+func newPushRequest(channelId int64, groupName string) *api.WebhookRequest {
+	wr := &api.WebhookRequest{
+		GroupName: groupName,
+	}
+	wr.Body = "hey"
+	wr.ChannelId = channelId
+
+	return wr
+}
+
+func newBotChannelRequest(nick, groupName string) *api.BotChannelRequest {
+	return &api.BotChannelRequest{
+		GroupName: groupName,
+		Username:  nick,
+	}
+}
+
+func TestWebhook(t *testing.T) {
+	r := runner.New("test webhook")
+	if err := r.Init(); err != nil {
+		t.Fatalf("couldnt start bongo %s", err.Error())
+	}
+	defer r.Close()
+
+	appConfig := config.MustRead(r.Conf.Path)
+	modelhelper.Initialize(appConfig.Mongo)
+	defer modelhelper.Close()
+
+	channelIntegration := webhook.CreateTestChannelIntegration(t)
+
+	Convey("While sending a message to push endpoint", t, func() {
+
+		account, err := models.CreateAccountInBothDbsWithNick("sinan")
+		So(err, ShouldBeNil)
+
+		channel := models.CreateTypedGroupedChannelWithTest(account.Id, models.Channel_TYPE_TOPIC, channelIntegration.GroupName)
+		_, err = channel.AddParticipant(account.Id)
+		So(err, ShouldBeNil)
+
+		err = rest.MakePushRequest(newPushRequest(channel.Id, channelIntegration.GroupName), channelIntegration.Token)
+		So(err, ShouldBeNil)
+
+		ses, err := models.FetchOrCreateSession(account.Nick)
+		So(err, ShouldBeNil)
+		So(ses, ShouldNotBeNil)
+
+		resp, err := rest.GetHistory(channel.Id,
+			&request.Query{
+				AccountId: account.Id,
+			},
+			ses.ClientId,
+		)
+
+		So(err, ShouldBeNil)
+		So(len(resp.MessageList), ShouldEqual, 1)
+	})
+
+	Convey("While fetching participant bot channel", t, func() {
+		account, err := models.CreateAccountInBothDbsWithNick("sinan")
+		So(err, ShouldBeNil)
+		channel := models.CreateTypedGroupedChannelWithTest(account.Id, models.Channel_TYPE_GROUP, models.RandomName())
+		_, err = channel.AddParticipant(account.Id)
+		So(err, ShouldBeNil)
+
+		channelId, err := rest.MakeBotChannelRequest(newBotChannelRequest(account.Nick, channel.GroupName))
+
+		So(err, ShouldBeNil)
+		So(channelId, ShouldNotEqual, 0)
+	})
+
+	Convey("While pushing a message to prepare endpoint", t, func() {
+		_, err := models.CreateAccountInBothDbsWithNick("ctf")
+		So(err, ShouldBeNil)
+
+		err = rest.MakePrepareRequest(newPrepareRequest("xxx@koding.com"), channelIntegration.Token)
+		So(err, ShouldNotBeNil)
+
+		err = rest.MakePrepareRequest(newPrepareRequest("ctf@koding.com"), channelIntegration.Token)
+		So(err, ShouldBeNil)
+	})
+
+}
