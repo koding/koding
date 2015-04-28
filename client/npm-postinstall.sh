ccc7a44f16ad116c881dc9106bde9fb6fbdeab2d
Sonmez Kartal <sonmez@koding.com>
2015-04-28 23:23:31 +0300

    Update wercker node modules

--- client/npm-postinstall.sh
+++ client/npm-postinstall.sh
@@ -3,3 +3,13 @@
 set -o errexit
 
 cd $(dirname $0)
+
+UPDATE_WERCKER_NODE_MODULES=../scripts/wercker/update-node-modules
+
+if [ "$CI" = "true" -a "$WERCKER" = "true" ]; then
+  $UPDATE_WERCKER_NODE_MODULES
+  $UPDATE_WERCKER_NODE_MODULES builder
+  $UPDATE_WERCKER_NODE_MODULES landing
+fi
+
+exit 0
