27bcb833c3f3da19f7a0b5f3e8d69fdaf9a4a40d
Sonmez Kartal <sonmez@koding.com>
2015-04-28 20:47:37 +0300

    Change working directory to install node modules

--- scripts/install-npm.sh
+++ scripts/install-npm.sh
@@ -2,34 +2,36 @@
 
 install()
 {
-  REF=`dirname $0`
-  BASEDIR=`cd $REF/.. && pwd -P`
-  TARGET=${BASEDIR}/${RELATIVE_DIR}
+  if [ -n "$WORKING_DIR" ]; then
+    if [ ! -d $WORKING_DIR ]; then
+      echo -e "\033[0;31merror\033[0m: install-npm failed, $WORKING_DIR does not exist"
+      exit 1
+    fi
 
-  if [ ! -d $TARGET ]; then
-    echo -e "\033[0;31merror\033[0m: install-npm failed, $TARGET does not exist"
-    exit 1
+    cd $WORKING_DIR
+  else
+    WORKING_DIR=$(basename $(pwd))
   fi
 
-  echo -e "\033[0;32m${RELATIVE_DIR}\033[0m: verifying npm dependencies"
-  npm --prefix $TARGET ls > /dev/null 2>&1
+  echo -e "\033[0;32m${WORKING_DIR}\033[0m: verifying npm dependencies"
+  npm ls > /dev/null 2>&1
 
   if [ $? -eq 0 ]; then
     exit 0
   fi
 
-  echo -e "\033[0;33m${RELATIVE_DIR}\033[0m: installing npm dependencies"
-  npm install --prefix $TARGET $NPM_ARGS
+  echo -e "\033[0;33m${WORKING_DIR}\033[0m: installing npm dependencies"
+  npm install $NPM_ARGS
 }
 
 NPM_ARGS=
 
 while getopts ":d:usp" OPTION; do
   case $OPTION in
-    d) RELATIVE_DIR=$OPTARG ;;
+    d) WORKING_DIR=$OPTARG ;;
     u) NPM_ARGS+=" --unsafe-perm" ;;
     s) NPM_ARGS+=" --silent" ;;
   esac
 done
 
-install $RELATIVE_DIR
+install
