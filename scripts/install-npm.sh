bf4d850cf3069edbdf86d216ea720aa0234c8de7
Sonmez Kartal <sonmez@koding.com>
2015-04-28 20:22:45 +0300

    Reformat install-npm script

--- scripts/install-npm.sh
+++ scripts/install-npm.sh
@@ -5,38 +5,37 @@ install()
   REF=`dirname $0`
   BASEDIR=`cd $REF/.. && pwd -P`
   TARGET=${BASEDIR}/${RELATIVE_DIR}
-  if [ -d $TARGET ]
-  then
-    echo -e "\033[0;32m${RELATIVE_DIR}\033[0m: verifying npm dependencies"
-    npm --prefix $TARGET ls > /dev/null 2>&1
-    if [ $? -ne 0 ]
-    then
-      echo -e "\033[0;33m${RELATIVE_DIR}\033[0m: installing npm dependencies"
-      npm install --prefix $TARGET $NPM_ARGS
-    else
-      if [ ! -z $FORCE_PREINSTALL ]
-      then
-        echo -e "\033[0;34m${RELATIVE_DIR}\033[0m: running npm preinstall script"
-        npm run preinstall --prefix $TARGET $NPM_ARGS
-      fi
-    fi
-  else
+
+  if [ ! -d $TARGET ]; then
     echo -e "\033[0;31merror\033[0m: install-npm failed, $TARGET does not exist"
+    exit 1
+  fi
+
+  echo -e "\033[0;32m${RELATIVE_DIR}\033[0m: verifying npm dependencies"
+  npm --prefix $TARGET ls > /dev/null 2>&1
+
+  if [ $? -eq 0 ]; then
+    exit 0
+  fi
+
+  echo -e "\033[0;33m${RELATIVE_DIR}\033[0m: installing npm dependencies"
+  npm install --prefix $TARGET $NPM_ARGS
+
+  if [ -n "$FORCE_PREINSTALL" ]; then
+    echo -e "\033[0;34m${RELATIVE_DIR}\033[0m: running npm preinstall script"
+    npm run preinstall --prefix $TARGET $NPM_ARGS
   fi
 }
 
 NPM_ARGS=
 
 while getopts ":d:usp" OPTION; do
-case $OPTION in
-d) RELATIVE_DIR=$OPTARG;;
-u) NPM_ARGS+=" --unsafe-perm";;
-s) NPM_ARGS+=" --silent";;
-p) FORCE_PREINSTALL=TRUE;;
-esac
+  case $OPTION in
+    d) RELATIVE_DIR=$OPTARG ;;
+    u) NPM_ARGS+=" --unsafe-perm" ;;
+    s) NPM_ARGS+=" --silent" ;;
+    p) FORCE_PREINSTALL=TRUE ;;
+  esac
 done
 
-if [ ! -z $RELATIVE_DIR ]
-then
-  install $RELATIVE_DIR
-fi
+install $RELATIVE_DIR
