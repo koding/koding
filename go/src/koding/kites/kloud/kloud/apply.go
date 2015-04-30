5098ab4433d9cba7680e99eb8cb635d35c0f922a
Fatih Arslan <fatih@koding.com>
2015-04-30 12:31:59 +0300

    kloud/apply: fetch jMachines to be validated and updated

--- go/src/koding/kites/kloud/kloud/apply.go
+++ go/src/koding/kites/kloud/kloud/apply.go
@@ -5,7 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"koding/kites/kloud/contexthelper/session"
+	"koding/kites/kloud/provider/generic"
 	"koding/kites/kloud/terraformer"
+
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 
 	"golang.org/x/net/context"
 
@@ -43,6 +47,11 @@ func (k *Kloud) Apply(r *kite.Request) (interface{}, error) {
 	sess, ok := session.FromContext(ctx)
 	if !ok {
 		return nil, errors.New("session context is not passed")
+	}
+
+	_, err := fetchMachines(ctx, args.MachineIds...)
+	if err != nil {
+		return nil, err
 	}
 
 	creds, err := fetchCredentials(r.Username, sess.DB, args.PublicKeys)
@@ -88,10 +97,51 @@ func (k *Kloud) Apply(r *kite.Request) (interface{}, error) {
 	return nil, errors.New("not implemented yet")
 }
 
+func fetchMachines(ctx context.Context, ids ...string) ([]*generic.Machine, error) {
+	sess, ok := session.FromContext(ctx)
+	if !ok {
+		return nil, errors.New("session context is not passed")
+	}
+
+	mongodbIds := make([]bson.ObjectId, len(ids))
+	for i, id := range ids {
+		mongodbIds[i] = bson.ObjectIdHex(id)
+	}
+
+	machines := make([]*generic.Machine, 0)
+	if err := sess.DB.Run("jMachines", func(c *mgo.Collection) error {
+		return c.Find(bson.M{"_id": bson.M{"$in": mongodbIds}}).All(&machines)
+	}); err != nil {
+		return nil, err
+	}
+
+	for i, m := range machines {
+		fmt.Printf("[%d] %v\n", i, m)
+	}
+
+	return nil, nil
+}
+
 func updateMachines(ctx context.Context, data *Machines, ids ...string) error {
-	_, ok := session.FromContext(ctx)
+	sess, ok := session.FromContext(ctx)
 	if !ok {
 		return errors.New("session context is not passed")
+	}
+
+	for _, id := range ids {
+		bsonId := bson.ObjectIdHex(id)
+
+		sess.DB.Run("jMachines", func(c *mgo.Collection) error {
+			return c.UpdateId(
+				bsonId,
+				bson.M{"$set": bson.M{
+					"meta.instanceId": "",
+					"queryString":     "",
+					"meta.region":     "us-east-1",
+				}},
+			)
+		})
+
 	}
 
 	return nil
