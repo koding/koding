f656d4fd39e8246b9a7f5d22eb137b90ef60f7c5
Senthil <senthil@koding.com>
2015-05-14 16:39:38 -0700

    analytics: fix pr comments

--- scripts/upload-sm-rollbar.sh
+++ scripts/upload-sm-rollbar.sh
@@ -4,22 +4,23 @@
 # make it easier to debug client side errors that occur in
 # production.
 
-website=website/a/p/p
-rollbar_token=6b1f2079d843423fbc0037b59bd13486
-version=`cat VERSION`
-folderpath=$website/$version
+WEBSITE=$(dirname $0)/../website/a/p/p
+ROLLBAR_TOKEN=6b1f2079d843423fbc0037b59bd13486
+VERSION=`cat VERSION`
+FOLDERPATH=$WEBSITE/$VERSION
 
-if [ ! -d $folderpath ]; then
-  echo "$folderpath dir doesn't exists"
+if [ ! -d $FOLDERPATH ]; then
+  echo "ERROR: '$FOLDERPATH' dir doesn't exists...sourcemaps won't be uploaded to Rollbar"
+  exit 1
 fi
 
-cd $folderpath
+cd $FOLDERPATH
 for file in $(ls -l | grep ".js$" | awk '{print $9}')
 do
   curl https://api.rollbar.com/api/1/sourcemap \
-    -F access_token=$rollbar_token\
-    -F version=$version \
-    -F minified_url=http://sandbox.koding.com/a/p/p/$version/$file \
+    -F access_token=$ROLLBAR_TOKEN\
+    -F version=$VERSION \
+    -F minified_url=https://koding.com/a/p/p/$VERSION/$file \
     -F source_map=@$file.map
 done
 
