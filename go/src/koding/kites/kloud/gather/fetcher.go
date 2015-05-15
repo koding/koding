ba9f94eaf74b89f677b526e5a73bf4174566f5ff
Senthil <senthil@koding.com>
2015-05-15 15:02:44 -0700

    gather: add fetcher to download scripts from s3

---
+++ go/src/koding/kites/kloud/gather/fetcher.go
@@ -0,0 +1,110 @@
+package gather
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
+)
+
+const (
+	CONTENT_TYPE_TAR       = "application/tar"
+	DEFAULT_BUCKET_NAME    = "gather-vm-metrics"
+	DEFAULT_SCRIPTS_FOLDER = "scripts"
+)
+
+type Fetcher interface {
+	Download(string) error
+}
+
+type S3Fetcher struct {
+	AccessKey, SecretKey    string
+	BucketName, ScriptsFile string
+}
+
+func (s *S3Fetcher) Bucket() *s3.Bucket {
+	auth := aws.Auth{
+		AccessKey: s.AccessKey,
+		SecretKey: s.SecretKey,
+	}
+
+	return s3.New(auth, aws.USEast).Bucket(s.BucketName)
+}
+
+func (s *S3Fetcher) Download(folderName string) error {
+	// prefix, delim, marker, max
+	l, err := s.Bucket().List(s.ScriptsFile, "", "", 1)
+	if err != nil {
+		return err
+	}
+
+	contents := l.Contents
+	if len(contents) == 0 {
+		return ErrScriptsFileNotFound
+	}
+
+	key := contents[0].Key
+	rc, err := s.Bucket().GetReader(key)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	w, err := os.Create(filepath.Join(folderName, key))
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	_, err = io.Copy(w, rc)
+
+	return err
+}
+
+func (s *S3Fetcher) Upload(folderName string) error {
+	if err := exists(folderName); err != nil {
+		return err
+	}
+
+	tarFile := folderName + ".tar"
+	if err := tarFolder(folderName, tarFile); err != nil {
+		return err
+	}
+
+	file, err := os.Open(tarFile)
+	if err != nil {
+		return err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	return s.Bucket().PutReader(
+		tarFile, bufio.NewReader(file), fileInfo.Size(), CONTENT_TYPE_TAR, s3.Private,
+	)
+}
+
+//----------------------------------------------------------
+// Helpers
+//----------------------------------------------------------
+
+func tarFolder(folderName, outputFileName string) error {
+	_, err := exec.Command("tar", "-cvf", outputFileName, folderName).Output()
+	return err
+}
+
+// exists checks if folder or file exists
+func exists(name string) error {
+	var err error
+	if _, err = os.Stat(name); os.IsNotExist(err) {
+		return ErrFolderNotFound
+	}
+
+	return err
+}
---
+++ go/src/koding/kites/kloud/gather/fetcher_test.go
@@ -0,0 +1,77 @@
+package gather
+
+import (
+	"io/ioutil"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func getFetcher() *S3Fetcher {
+	return &S3Fetcher{
+		AccessKey:  "AKIAJFKDHRJ7Q5G4MOUQ",
+		SecretKey:  "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",
+		BucketName: DEFAULT_BUCKET_NAME,
+	}
+}
+
+func TestFetcher(t *testing.T) {
+	Convey("Given commmand to upload scripts", t, func() {
+		fetcher := getFetcher()
+
+		Convey("When scripts folder doesn't exist", func() {
+			Convey("Then it should return error", func() {
+				err := fetcher.Upload("")
+				So(err, ShouldEqual, ErrFolderNotFound)
+			})
+		})
+
+		Convey("Then it should tar folder", func() {
+			tarFile := DEFAULT_SCRIPTS_FOLDER + ".tar"
+
+			err := tarFolder(DEFAULT_SCRIPTS_FOLDER, tarFile)
+			So(err, ShouldBeNil)
+
+			err = exists(tarFile)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Then it should upload folder", func() {
+			err := fetcher.Upload(DEFAULT_SCRIPTS_FOLDER)
+			So(err, ShouldBeNil)
+		})
+	})
+
+	Convey("Given commmand to download scripts", t, func() {
+		fetcher := getFetcher()
+		fetcher.BucketName = ""
+
+		Convey("When scripts bucket doesn't exist", func() {
+			Convey("Then it should return error", func() {
+				err := fetcher.Download("")
+				So(err, ShouldEqual, ErrScriptsFileNotFound)
+			})
+		})
+
+		Convey("When scripts folder doesn't exist", func() {
+			Convey("Then it should return error", func() {
+				err := fetcher.Download("")
+				So(err, ShouldEqual, ErrScriptsFileNotFound)
+			})
+		})
+
+		Convey("Then it should download scripts folder", func() {
+			fetcher.BucketName = "gather-vm-metrics"
+			fetcher.ScriptsFile = "scripts.tar"
+
+			folderName, err := ioutil.TempDir("", "")
+			So(err, ShouldBeNil)
+
+			err = fetcher.Download(folderName)
+			So(err, ShouldBeNil)
+
+			err = exists(folderName + "/scripts.tar")
+			So(err, ShouldBeNil)
+		})
+	})
+}
