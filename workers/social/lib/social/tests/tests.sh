27acc12227b138f50f344902f5ad2990153caf73
Ozan Kaşıkçı <ozan@koding.com>
2015-05-19 02:09:27 +0300

    integration of social api testing

--- workers/social/lib/social/tests/tests.sh
+++ workers/social/lib/social/tests/tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../../../../node_modules/mocha/bin/mocha --compilers coffee:coffee-script/register --require coffee-script *.test.coffee
+../../../../../node_modules/mocha/bin/mocha --compilers coffee:coffee-script/register --require coffee-script ../**/*.test.coffee
