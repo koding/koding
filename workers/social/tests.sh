1bd0776c753b0f4a4dd3bf6adf658f6338a8b884
Ozan Kaşıkçı <ozan@koding.com>
2015-05-19 17:14:27 +0300

    cleaned up tests.sh

--- workers/social/tests.sh
+++ workers/social/tests.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-../../node_modules/mocha/bin/mocha --compilers coffee:coffee-script/register --require coffee-script ./**/*.test.coffee
+cd $(dirname $0)
+
+MOCHA_BIN_FILE="../../node_modules/mocha/bin/mocha"
+MOCHA_OPTIONS="--compilers coffee:coffee-script/register --require coffee-script"
+TEST_FILES="./**/*.test.coffee"
+
+MOCHA_RUN_CMD="$MOCHA_BIN_FILE $MOCHA_OPTIONS $TEST_FILES"
+
+$MOCHA_RUN_CMD
