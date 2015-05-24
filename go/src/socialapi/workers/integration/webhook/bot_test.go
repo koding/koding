6827eb45e9a7b439b413620055e67af795807de5
Can Yucel <can@koding.com>
2015-05-24 16:31:23 -0700

    webhook: update test helper for supporting parallel tests

--- go/src/socialapi/workers/integration/webhook/bot_test.go
+++ go/src/socialapi/workers/integration/webhook/bot_test.go
@@ -14,108 +14,104 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-var (
-	r *runner.Runner
-)
-
-func initialize(t *testing.T) {
-
-	r = runner.New("test")
+func tearUp(t *testing.T, f func()) {
+	r := runner.New("test")
 	if err := r.Init(); err != nil {
 		t.Fatalf("something went wrong: %s", err)
 	}
+	defer r.Close()
 
 	appConfig := config.MustRead(r.Conf.Path)
 	r.Log.SetLevel(logging.CRITICAL)
 
 	modelhelper.Initialize(appConfig.Mongo)
+	defer modelhelper.Close()
 
 	botAcc, err := models.CreateAccountInBothDbsWithNick("bot")
 	if err != nil || botAcc == nil {
 		t.Fatalf("could not create bot account: %s", err)
 	}
+	f()
 }
 
 func TestSendMessage(t *testing.T) {
 
-	initialize(t)
-	defer r.Close()
-	defer modelhelper.Close()
+	tearUp(t, func() {
+		Convey("while testing bot", t, func() {
 
-	Convey("while testing bot", t, func() {
-
-		bot, err := NewBot()
-		So(err, ShouldBeNil)
-
-		rand.Seed(time.Now().UTC().UnixNano())
-
-		groupName := models.RandomGroupName()
-
-		channel := models.CreateTypedGroupedChannelWithTest(bot.account.Id, models.Channel_TYPE_TOPIC, groupName)
-
-		Convey("bot should be able to create message", func() {
-			message := &Message{}
-			message.Body = "testmessage"
-			message.ChannelId = channel.Id
-			message.ChannelIntegrationId = 13
-			err := bot.SendMessage(message)
+			bot, err := NewBot()
 			So(err, ShouldBeNil)
 
-			m, err := channel.FetchLastMessage()
-			So(err, ShouldBeNil)
-			So(m, ShouldNotBeNil)
-			So(m.Body, ShouldEqual, message.Body)
-			So(m.InitialChannelId, ShouldEqual, message.ChannelId)
-			So(m.AccountId, ShouldEqual, bot.account.Id)
-			So(m.TypeConstant, ShouldEqual, models.ChannelMessage_TYPE_BOT)
-			So(*(m.GetPayload("channelIntegrationId")), ShouldEqual, "13")
+			rand.Seed(time.Now().UTC().UnixNano())
 
+			groupName := models.RandomGroupName()
+
+			channel := models.CreateTypedGroupedChannelWithTest(bot.account.Id, models.Channel_TYPE_TOPIC, groupName)
+
+			Convey("bot should be able to create message", func() {
+				message := &Message{}
+				message.Body = "testmessage"
+				message.ChannelId = channel.Id
+				message.ChannelIntegrationId = 13
+				err := bot.SendMessage(message)
+				So(err, ShouldBeNil)
+
+				m, err := channel.FetchLastMessage()
+				So(err, ShouldBeNil)
+				So(m, ShouldNotBeNil)
+				So(m.Body, ShouldEqual, message.Body)
+				So(m.InitialChannelId, ShouldEqual, message.ChannelId)
+				So(m.AccountId, ShouldEqual, bot.account.Id)
+				So(m.TypeConstant, ShouldEqual, models.ChannelMessage_TYPE_BOT)
+				So(*(m.GetPayload("channelIntegrationId")), ShouldEqual, "13")
+
+			})
 		})
 	})
+
 }
 
 func TestFetchBotChannel(t *testing.T) {
 
-	initialize(t)
-	defer r.Close()
-	defer modelhelper.Close()
-
-	Convey("while testing bot", t, func() {
-		bot, err := NewBot()
-		So(err, ShouldBeNil)
-
-		acc, err := models.CreateAccountInBothDbsWithNick("bot-" + models.RandomName())
-		So(err, ShouldBeNil)
-		So(acc, ShouldNotBeNil)
-
-		groupName := models.RandomGroupName()
-		Convey("we should be able to create bot channel for each user", func() {
-			// make sure the bot channel for the user does not exist
-			channel, err := bot.fetchBotChannel(acc, groupName)
-			So(err, ShouldEqual, bongo.RecordNotFound)
-
-			channel, err = bot.fetchOrCreateChannel(acc, groupName)
+	tearUp(t, func() {
+		Convey("while testing bot", t, func() {
+			bot, err := NewBot()
 			So(err, ShouldBeNil)
-			So(channel, ShouldNotBeNil)
-			So(channel.TypeConstant, ShouldEqual, models.Channel_TYPE_BOT)
-			So(channel.CreatorId, ShouldEqual, acc.Id)
+
+			acc, err := models.CreateAccountInBothDbsWithNick("bot-" + models.RandomName())
+			So(err, ShouldBeNil)
+			So(acc, ShouldNotBeNil)
+
+			groupName := models.RandomGroupName()
+			Convey("we should be able to create bot channel for each user", func() {
+				// make sure the bot channel for the user does not exist
+				channel, err := bot.fetchBotChannel(acc, groupName)
+				So(err, ShouldEqual, bongo.RecordNotFound)
+
+				channel, err = bot.fetchOrCreateChannel(acc, groupName)
+				So(err, ShouldBeNil)
+				So(channel, ShouldNotBeNil)
+				So(channel.TypeConstant, ShouldEqual, models.Channel_TYPE_BOT)
+				So(channel.CreatorId, ShouldEqual, acc.Id)
+			})
+
+			Convey("we should be able to fetch bot channel when it is already created", func() {
+				// make sure the channel already exists
+				channel, err := bot.createBotChannel(acc, groupName)
+				So(err, ShouldBeNil)
+				So(channel, ShouldNotBeNil)
+				So(channel.TypeConstant, ShouldEqual, models.Channel_TYPE_BOT)
+				So(channel.CreatorId, ShouldEqual, acc.Id)
+
+				channel.AddParticipant(acc.Id)
+
+				testchannel, err := bot.fetchOrCreateChannel(acc, groupName)
+				So(err, ShouldBeNil)
+				So(testchannel, ShouldNotBeNil)
+				So(testchannel.Id, ShouldEqual, channel.Id)
+			})
+
 		})
-
-		Convey("we should be able to fetch bot channel when it is already created", func() {
-			// make sure the channel already exists
-			channel, err := bot.createBotChannel(acc, groupName)
-			So(err, ShouldBeNil)
-			So(channel, ShouldNotBeNil)
-			So(channel.TypeConstant, ShouldEqual, models.Channel_TYPE_BOT)
-			So(channel.CreatorId, ShouldEqual, acc.Id)
-
-			channel.AddParticipant(acc.Id)
-
-			testchannel, err := bot.fetchOrCreateChannel(acc, groupName)
-			So(err, ShouldBeNil)
-			So(testchannel, ShouldNotBeNil)
-			So(testchannel.Id, ShouldEqual, channel.Id)
-		})
-
 	})
+
 }
