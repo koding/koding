1fd2d80b45d635e73f24972ef108d85419aa4c95
Fatih Arslan <fatih@koding.com>
2015-05-27 15:15:58 +0300

    tunnelserver: validate config before we continute

--- go/src/koding/kites/tunnelserver/tunnelserver.go
+++ go/src/koding/kites/tunnelserver/tunnelserver.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"koding/kites/kloud/pkg/dnsclient"
 	"log"
+	"os"
 
 	"github.com/koding/kite"
+	"github.com/koding/logging"
 	"github.com/koding/multiconfig"
 	"github.com/koding/streamtunnel"
 	"github.com/mitchellh/goamz/aws"
@@ -23,37 +25,40 @@ type tunnelServer struct {
 	Debug           bool
 	BaseVirtualHost string `required:"true"`
 	// Public Address of the running server
-	ServerAddr string
+	ServerAddr string `required:"true"`
 
 	HostedZone string
 	AccessKey  string
 	SecretKey  string
 
-	server *streamtunnel.Server
-	dns    *dnsclient.Route53
+	Log    logging.Logger
+	Server *streamtunnel.Server
+	Dns    *dnsclient.Route53
 }
 
 func main() {
 	t := new(tunnelServer)
 	m := multiconfig.New()
 	m.MustLoad(t)
+	m.MustValidate(t)
 
 	auth := aws.Auth{
 		AccessKey: t.AccessKey,
 		SecretKey: t.SecretKey,
 	}
 
-	t.dns = dnsclient.NewRoute53Client(t.HostedZone, auth)
-	t.server = streamtunnel.NewServer(&streamtunnel.ServerConfig{
+	t.Dns = dnsclient.NewRoute53Client(t.HostedZone, auth)
+	t.Server = streamtunnel.NewServer(&streamtunnel.ServerConfig{
 		Debug: t.Debug,
 	})
+	t.Log = newLogger("tunnelkite", t.Debug)
 
 	k := kite.New("tunnelserver", "0.0.1")
 	k.Config.DisableAuthentication = true
 	k.Config.Port = t.Port
 
 	k.HandleFunc("register", t.Register)
-	k.HandleHTTP("/{rest:.*}", t.server)
+	k.HandleHTTP("/{rest:.*}", t.Server)
 
 	k.Run()
 }
@@ -61,21 +66,24 @@ func main() {
 func (t *tunnelServer) Register(r *kite.Request) (interface{}, error) {
 	log.Printf("registering user '%s'\n", r.Username)
 
-	virtualHost := fmt.Sprintf("%s.%s", r.Username, t.BaseVirtualHost)
+	virtualHost := fmt.Sprintf("%s.54.85.221.74%s", r.Username, t.BaseVirtualHost)
 	identifier := randomID(32)
 
-	domain := r.Username + "." + t.dns.HostedZone()
+	domain := r.Username + "." + t.Dns.HostedZone()
 
-	if err := t.dns.Upsert(domain, t.ServerAddr); err != nil {
+	t.Log.Debug("Adding domain '%s' with to IP Address '%s'", domain, t.ServerAddr)
+	if err := t.Dns.Upsert(domain, t.ServerAddr); err != nil {
 		return nil, err
 	}
 
-	t.server.AddHost(virtualHost, identifier)
+	t.Log.Debug("Adding host '%s' with identifer '%s'", virtualHost, identifier)
+	t.Server.AddHost(virtualHost, identifier)
 
 	// cleanup domain and delete in memory virtualhost
-	t.server.OnDisconnect(identifier, func() error {
-		t.server.DeleteHost(virtualHost)
-		return t.dns.Delete(domain)
+	t.Server.OnDisconnect(identifier, func() error {
+		t.Log.Debug("Deleting host '%s' and domain '%s'", virtualHost, domain)
+		t.Server.DeleteHost(virtualHost)
+		return t.Dns.Delete(domain)
 	})
 
 	log.Printf("tunnel added: %s\n", virtualHost)
@@ -91,4 +99,18 @@ func randomID(length int) string {
 	r := make([]byte, length*6/8)
 	rand.Read(r)
 	return base64.URLEncoding.EncodeToString(r)
+}
+
+func newLogger(name string, debug bool) logging.Logger {
+	log := logging.NewLogger(name)
+	logHandler := logging.NewWriterHandler(os.Stderr)
+	logHandler.Colorize = true
+	log.SetHandler(logHandler)
+
+	if debug {
+		log.SetLevel(logging.DEBUG)
+		logHandler.SetLevel(logging.DEBUG)
+	}
+
+	return log
 }
