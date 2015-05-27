541f6c6f9d81661386d9634652ee437879619895
Fatih Arslan <fatih@koding.com>
2015-05-27 13:23:26 +0300

    tunnelserver: add dns client

--- go/src/koding/kites/tunnelserver/tunnelserver.go
+++ go/src/koding/kites/tunnelserver/tunnelserver.go
@@ -4,11 +4,13 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"koding/kites/kloud/pkg/dnsclient"
 	"log"
 
 	"github.com/koding/kite"
 	"github.com/koding/multiconfig"
 	"github.com/koding/streamtunnel"
+	"github.com/mitchellh/goamz/aws"
 )
 
 type registerResult struct {
@@ -20,46 +22,62 @@ type config struct {
 	Port            int
 	BaseVirtualHost string `required:"true"`
 	Debug           bool
+
+	// Public Address of the running server
+	ServerAddr string
+
+	HostedZone string
+	AccessKey  string
+	SecretKey  string
 }
 
-var (
-	server *streamtunnel.Server
-)
+type tunnelServer struct {
+	BaseVirtualHost string
+	server          *streamtunnel.Server
+	dns             *dnsclient.Route53
+}
 
 func main() {
 	conf := new(config)
 	m := multiconfig.New()
 	m.MustLoad(conf)
 
-	server = streamtunnel.NewServer(&streamtunnel.ServerConfig{
-		Debug: conf.Debug,
-	})
+	auth := aws.Auth{
+		AccessKey: conf.AccessKey,
+		SecretKey: conf.SecretKey,
+	}
+
+	t := &tunnelServer{
+		BaseVirtualHost: conf.BaseVirtualHost,
+		server: streamtunnel.NewServer(&streamtunnel.ServerConfig{
+			Debug: conf.Debug,
+		}),
+		dns: dnsclient.NewRoute53Client(conf.HostedZone, auth),
+	}
 
 	k := kite.New("tunnelserver", "0.0.1")
 	k.Config.DisableAuthentication = true
 	k.Config.Port = conf.Port
 
-	k.Handle("register", Register(conf.BaseVirtualHost))
-	k.HandleHTTP("/{rest:.*}", server)
+	k.HandleFunc("register", t.Register)
+	k.HandleHTTP("/{rest:.*}", t.server)
 
 	k.Run()
 }
 
-func Register(baseVirtualHost string) kite.HandlerFunc {
-	return func(r *kite.Request) (interface{}, error) {
-		log.Printf("registering user '%s'\n", r.Username)
+func (t *tunnelServer) Register(r *kite.Request) (interface{}, error) {
+	log.Printf("registering user '%s'\n", r.Username)
 
-		virtualHost := fmt.Sprintf("%s.%s", r.Username, baseVirtualHost)
-		identifier := randomID(32)
+	virtualHost := fmt.Sprintf("%s.%s", r.Username, t.BaseVirtualHost)
+	identifier := randomID(32)
 
-		server.AddHost(virtualHost, identifier)
-		log.Printf("tunnel added: %s\n", virtualHost)
+	t.server.AddHost(virtualHost, identifier)
+	log.Printf("tunnel added: %s\n", virtualHost)
 
-		return registerResult{
-			VirtualHost: virtualHost,
-			Identifier:  identifier,
-		}, nil
-	}
+	return registerResult{
+		VirtualHost: virtualHost,
+		Identifier:  identifier,
+	}, nil
 }
 
 // randomID generates a random string of the given length
