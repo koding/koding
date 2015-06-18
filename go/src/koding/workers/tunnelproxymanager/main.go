8fb3e81e4c5c9195724e2005b4eb6311f808ba4b
Cihangir SAVAS <cihangir@koding.com>
2015-06-17 18:01:49 -0700

    Tunnel: use region from a variable

--- go/src/koding/workers/tunnelproxymanager/main.go
+++ go/src/koding/workers/tunnelproxymanager/main.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/service/sns"
 	"github.com/koding/logging"
@@ -21,13 +20,15 @@ func main() {
 
 	// Load the config, it's reads environment variables or from flags
 	multiconfig.New().MustLoad(conf)
+	region := "us-east-1"
+
 	config := &aws.Config{
 		Credentials: credentials.NewStaticCredentials(
 			conf.AccessKeyID,
 			conf.SecretAccessKey,
 			"",
 		),
-		Region:     "us-east-1",
+		Region:     region,
 		Logger:     os.Stdout,
 		MaxRetries: 5,
 	}
@@ -43,7 +44,7 @@ func main() {
 		log.Fatal("EB Env Name can not be empty")
 	}
 
-	recordManager, err := New(config, log)
+	recordManager, err := NewRecordManager(config, log, region)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -52,7 +53,7 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	queueName := "SQS-ElasticBeanstalkNotifications-Environment-" + ebEnvName
+	queueName := "tunnelproxymanager-" + ebEnvName
 
 	l, err := NewLifeCycle(config, log, queueName)
 	if err != nil {
@@ -83,46 +84,13 @@ func main() {
 		for {
 
 			params := &sns.PublishInput{
-				Message: aws.String("bu benim mesajin bodysi"), // Required
-				// MessageAttributes: map[string]*sns.MessageAttributeValue{
-				// 	"Key": { // Required
-				// 		DataType:    aws.String("String"), // Required
-				// 		BinaryValue: []byte("PAYLOAD"),
-				// 		StringValue: aws.String("String"),
-				// 	},
-				// 	// More values...
-				// },
-				// MessageStructure: aws.String("messageStructure"),
-				// Subject:          aws.String("subject"),
-				// TargetARN:        aws.String("String"),
+				Message:  aws.String("bu benim mesajin bodysi"), // Required
 				TopicARN: l.topicARN,
 			}
 			resp, err := l.sns.Publish(params)
 			fmt.Println("resp-->", *resp.MessageID)
-			// _, err := l.sqs.SendMessage(&sqs.SendMessageInput{
-			// 	MessageBody:  aws.String("bu benim mesajin bodysi"), // Required
-			// 	QueueURL:     l.queueURL,                            // Required
-			// 	DelaySeconds: aws.Long(0),
-			// 	MessageAttributes: map[string]*sqs.MessageAttributeValue{
-			// 		"Key": &sqs.MessageAttributeValue{ // Required
-			// 			DataType:    aws.String("String"), // Required
-			// 			StringValue: aws.String("String value"),
-			// 		},
-			// 	},
-			// })
 			if err != nil {
-				if awsErr, ok := err.(awserr.Error); ok {
-					// Generic AWS Error with Code, Message, and original error (if any)
-					fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
-					if reqErr, ok := err.(awserr.RequestFailure); ok {
-						// A service error occurred
-						fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
-					}
-				} else {
-					// This case should never be hit, the SDK should always return an
-					// error which satisfies the awserr.Error interface.
-					fmt.Println(err.Error())
-				}
+				fmt.Println(err.Error())
 			}
 
 			time.Sleep(time.Second * 3)
@@ -130,7 +98,7 @@ func main() {
 	}()
 
 	err = l.Listen(func(body *string) error {
-		// fmt.Println("body-->", *body)
+		fmt.Println("got body-->")
 		return nil
 	})
 	if err != nil {
