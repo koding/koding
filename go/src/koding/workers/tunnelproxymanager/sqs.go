262bb632d9e0b4e41cf62f12a4241718361420a0
Cihangir SAVAS <cihangir@koding.com>
2015-06-19 17:32:21 -0700

    Tunnel: better documentation and testability for SQS

--- go/src/koding/workers/tunnelproxymanager/sqs.go
+++ go/src/koding/workers/tunnelproxymanager/sqs.go
@@ -1,4 +1,4 @@
-package main
+package tunnelproxymanager
 
 import (
 	"encoding/json"
@@ -7,57 +7,103 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/koding/logging"
 )
+
+var (
+	errSQSNameNotSet  = errors.New("SNS Name must not be empty")
+	errSQSNotSet      = errors.New("SNS is not set")
+	errQueueURLNotSet = errors.New("queue url not set")
+)
+
+// attributes holds configuration parameters for SQS queue
+var attributes = map[string]*string{ // Required
+	// The time in seconds that the delivery of all messages in the
+	// queue will be delayed
+	"DelaySeconds": aws.String("0"),
+
+	//  The limit of how many bytes a message can contain
+	"MaximumMessageSize": aws.String("262144"), // 256 KiB
+
+	// The number of seconds Amazon SQS retains a message. Integer
+	// representing seconds, from 60 (1 minute) to 1209600 (14 days).
+	"MessageRetentionPeriod": aws.String("3600"), // 1 hour
+
+	//The time for which a ReceiveMessage call will wait for a message
+	//to arrive
+	"ReceiveMessageWaitTimeSeconds": aws.String("20"),
+
+	// The visibility timeout for the queue. An integer from 0 to 43200
+	// (12 hours). Messages will be available again if they are not
+	// deleted in VisibilityTimeout
+	"VisibilityTimeout": aws.String("60"), // 60 secs
+}
 
 // MakeSureSQS configures a queue for listening to a predefined system
 func (l *LifeCycle) MakeSureSQS(name string) error {
-	sqsLogger := l.log.New("SQS")
-	sqsLogger.Debug("working...")
+	if name == "" {
+		return errSQSNameNotSet
+	}
+
+	if l.sqs == nil {
+		return errSQSNotSet
+	}
 
 	queueName := "SQS-" + name
-	// create queue is idempotent, if it is already created returns existing one
-	// all Attributes should be same tho
-	createQueueResp, err := l.sqs.CreateQueue(&sqs.CreateQueueInput{
-		QueueName: aws.String(queueName), // Required
-		Attributes: map[string]*string{ // Required
-			// The time in seconds that the delivery of all messages in the
-			// queue will be delayed
-			"DelaySeconds": aws.String("0"),
 
-			//  The limit of how many bytes a message can contain
-			"MaximumMessageSize": aws.String("262144"), // 256 KiB
+	sqsLogger := l.log.New("SQS").New("queueName", queueName)
+	sqsLogger.Debug("Preparing SQS Queue")
 
-			// The number of seconds Amazon SQS retains a message. Integer
-			// representing seconds, from 60 (1 minute) to 1209600 (14 days).
-			"MessageRetentionPeriod": aws.String("3600"), // 1 hour
+	if err := l.createQueue(sqsLogger, queueName); err != nil {
+		return err
+	}
 
-			//The time for which a ReceiveMessage call will wait for a message
-			//to arrive
-			"ReceiveMessageWaitTimeSeconds": aws.String("20"),
+	sqsLogger.Debug("Configuring Queue...")
 
-			// The visibility timeout for the queue. An integer from 0 to 43200
-			// (12 hours). Messages will be available again if they are not
-			// deleted in VisibilityTimeout
-			"VisibilityTimeout": aws.String("3600"), // 60 secs
+	if err := l.configureQueue(sqsLogger); err != nil {
+		return err
+	}
+
+	sqsLogger.Debug("SQS Queue is ready")
+	return nil
+}
+
+func (l *LifeCycle) createQueue(sqsLogger logging.Logger, queueName string) error {
+
+	// CreateQueue is idempotent, if it is already created returns existing one
+	// all Attributes should be same with consecutive calls
+	createQueueResp, err := l.sqs.CreateQueue(
+		&sqs.CreateQueueInput{
+			QueueName:  aws.String(queueName), // Required
+			Attributes: attributes,
 		},
-	})
+	)
 
 	if err != nil {
 		return err
 	}
 
-	l.queueURL = createQueueResp.QueueURL
+	l.queueURL = createQueueResp.QueueURL // dont forget to assign queue url
 
-	sqsLogger.Debug("Queue is created URL: %s", *l.queueURL)
-	sqsLogger.Debug("Configuring Queue...")
+	sqsLogger.Debug("SQS Queue is created")
 
-	resp, err := l.sqs.GetQueueAttributes(&sqs.GetQueueAttributesInput{
-		QueueURL: l.queueURL, // Required
-		AttributeNames: []*string{
-			aws.String("QueueArn"),
-			aws.String("Policy"),
+	return nil
+}
+
+func (l *LifeCycle) configureQueue(sqsLogger logging.Logger) error {
+	if l.queueURL == nil {
+		return errQueueURLNotSet
+	}
+
+	resp, err := l.sqs.GetQueueAttributes(
+		&sqs.GetQueueAttributesInput{
+			QueueURL: l.queueURL, // Required
+			AttributeNames: []*string{
+				aws.String("QueueArn"),
+				aws.String("Policy"),
+			},
 		},
-	})
+	)
 	if err != nil {
 		return err
 	}
@@ -72,8 +118,7 @@ func (l *LifeCycle) MakeSureSQS(name string) error {
 	}
 	l.queueARN = queueARN
 
-	p1 := newDefaultPolicy(*l.topicARN, *l.queueARN)
-	b, err := json.Marshal(p1)
+	b, err := newDefaultPolicy(*l.topicARN, *l.queueARN)
 	if err != nil {
 		return err
 	}
@@ -82,21 +127,24 @@ func (l *LifeCycle) MakeSureSQS(name string) error {
 	//   QueueArn: "arn:aws:sqs:us-east-1:616271189586:SQS-ElasticBeanstalkNotifications-Environment-cihangir",
 	//   Policy: "{\"Version\":\"2012-10-17\",\"Id\":\"arn:aws:sqs:us-east-1:616271189586:SQS-ElasticBeanstalkNotifications-Environment-cihangir/SQSDefaultPolicy\",\"Statement\":[{\"Sid\":\"tunnelproxy_dev_1\",\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"*\"},\"Action\":\"SQS:SendMessage\",\"Resource\":\"arn:aws:sqs:us-east-1:616271189586:SQS-ElasticBeanstalkNotifications-Environment-cihangir\",\"Condition\":{\"ArnEquals\":{\"aws:SourceArn\":\"arn:aws:sns:us-east-1:616271189586:tunnelproxymanager_test\"}}}]}"
 	// }
-	if !isValid(resp.Attributes, string(b)) {
-		sqsLogger.Debug("Queue Policy is not correct, fixing it...")
-		_, err := l.sqs.SetQueueAttributes(&sqs.SetQueueAttributesInput{
-			Attributes: map[string]*string{ // Required
-				"Policy": aws.String(string(b)), // Required
-			},
-			QueueURL: l.queueURL,
-		})
-		if err != nil {
-			return err
-		}
-		sqsLogger.Debug("Queue Policy configured properly")
+	// If it is valid, nothing to do here
+	if isValid(resp.Attributes, b) {
+		return nil
 	}
 
-	sqsLogger.Debug("Queue is ready")
+	sqsLogger.Debug("Queue Policy is not correct, fixing it...")
+	_, err = l.sqs.SetQueueAttributes(&sqs.SetQueueAttributesInput{
+		Attributes: map[string]*string{
+			"Policy": aws.String(b),
+		},
+		QueueURL: l.queueURL,
+	})
+	if err != nil {
+		return err
+	}
+
+	sqsLogger.Debug("Queue Policy is configured properly")
+
 	return nil
 }
 
@@ -115,8 +163,8 @@ type statement struct {
 	Condition map[string]interface{} `json:"Condition"`
 }
 
-func newDefaultPolicy(topicARN, queueARN string) *policy {
-	return &policy{
+func newDefaultPolicy(topicARN, queueARN string) (string, error) {
+	p := &policy{
 		Version: "2012-10-17",
 		ID:      fmt.Sprintf("%s/SQSDefaultPolicy", queueARN),
 		Statement: []statement{
@@ -136,6 +184,13 @@ func newDefaultPolicy(topicARN, queueARN string) *policy {
 			},
 		},
 	}
+
+	b, err := json.Marshal(p)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
 
 func isValid(attr map[string]*string, b string) bool {
@@ -151,5 +206,6 @@ func isValid(attr map[string]*string, b string) bool {
 	if b != *attr["Policy"] {
 		return false
 	}
+
 	return true
 }
