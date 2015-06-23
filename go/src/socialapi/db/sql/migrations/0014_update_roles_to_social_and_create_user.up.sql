3a65ee9655ee5e69cce91b4ad17147f12d509b34
Fatih Arslan <fatih@koding.com>
2015-06-23 16:23:09 +0300

    wercker: another try

--- go/src/socialapi/db/sql/migrations/0014_update_roles_to_social_and_create_user.up.sql
+++ go/src/socialapi/db/sql/migrations/0014_update_roles_to_social_and_create_user.up.sql
@@ -64,6 +64,14 @@ GRANT kontrol TO kontrolapp201506;
 
 ALTER USER paymentro with password 'N8bG8ZVjp2y87Zxfi3';
 
+-- create a schema for our tables
+CREATE SCHEMA kite;
+
+-- give usage access to schema for our role
+GRANT USAGE ON SCHEMA kite TO kontrol;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 --
 -- create key table for storing key pairs
 --
@@ -80,27 +88,30 @@ CREATE TABLE IF NOT EXISTS "kite"."key" (
     CONSTRAINT "key_private_unique" UNIQUE ("private") NOT DEFERRABLE INITIALLY IMMEDIATE,
     CONSTRAINT "key_created_at_lte_deleted_at_check" CHECK (created_at <= deleted_at)
 ) WITH (OIDS = FALSE);
+
 GRANT SELECT, INSERT, UPDATE ON "kite"."key" TO "kontrol"; -- dont allow deletion from this table
 
--- add key_id column into kite table
-DO $$
-  BEGIN
-    BEGIN
-      ALTER TABLE kite.kite ADD COLUMN "key_id" UUID NOT NULL;
-    EXCEPTION
-      WHEN duplicate_column THEN RAISE NOTICE 'key_id column already exists';
-    END;
-  END;
-$$;
+-- create the table
+CREATE UNLOGGED TABLE "kite"."kite" (
+    username TEXT NOT NULL,
+    environment TEXT NOT NULL,
+    kitename TEXT NOT NULL,
+    version TEXT NOT NULL,
+    region TEXT NOT NULL,
+    hostname TEXT NOT NULL,
+    id uuid PRIMARY KEY,
+    url TEXT NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'), -- you may set a global timezone
+    updated_at timestamptz NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    key_id UUID NOT NULL,
 
--- create foreign constraint between kite.kite.key_id and kite.key.id
-DO $$
-  BEGIN
-    BEGIN
-      ALTER TABLE kite.kite ADD CONSTRAINT "kite_key_id_fkey" FOREIGN KEY ("key_id") REFERENCES kite.key (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE;
-    EXCEPTION
-      WHEN duplicate_object THEN RAISE NOTICE 'kite_key_id_fkey already exists';
-    END;
-  END;
-$$;
+    CONSTRAINT "kite_key_id_fkey" FOREIGN KEY ("key_id") REFERENCES kite.key (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE
+);
 
+-- add proper permissions for table
+GRANT SELECT, INSERT, UPDATE, DELETE ON "kite"."kite" TO "kontrol";
+
+-- create the index, but drop first if exists
+DROP INDEX IF EXISTS kite_updated_at_btree_idx;
+
+CREATE INDEX kite_updated_at_btree_idx ON "kite"."kite" USING BTREE (updated_at DESC);
