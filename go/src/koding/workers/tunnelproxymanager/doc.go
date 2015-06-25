9d56be41f3015e562447b2368d8442073ac2778c
Cihangir SAVAS <cihangir@koding.com>
2015-06-25 14:44:42 -0700

    Tunnel: added doc

---
+++ go/src/koding/workers/tunnelproxymanager/doc.go
@@ -0,0 +1,22 @@
+/*
+Package tunnelproxymanager manages tunnel proxy server records.
+
+Usage:
+    Usage of tunnelproxymanager:
+       -accesskeyid=: Change value of AccessKeyID.
+       -autoscalingname=: Change value of AutoScalingName.
+       -debug=false: Change value of Debug.
+       -ebenvname=: Change value of EBEnvName.
+       -region=: Change value of Region.
+       -secretaccesskey=: Change value of SecretAccessKey.
+
+Generated environment variables:
+   CONFIG_EBENVNAME
+   CONFIG_REGION
+   CONFIG_AUTOSCALINGNAME
+   CONFIG_ACCESSKEYID
+   CONFIG_SECRETACCESSKEY
+   CONFIG_DEBUG
+
+*/
+package tunnelproxymanager
