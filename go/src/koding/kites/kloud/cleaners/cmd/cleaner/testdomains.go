0df90f7134c73f0197343e1d8c24a19c4f2c2032
Fatih Arslan <fatih@koding.com>
2015-07-01 02:06:16 +0300

    testdomains: finish Process task

--- go/src/koding/kites/kloud/cleaners/cmd/cleaner/testdomains.go
+++ go/src/koding/kites/kloud/cleaners/cmd/cleaner/testdomains.go
@@ -3,33 +3,61 @@ package main
 import (
 	"fmt"
 	"koding/kites/kloud/pkg/dnsclient"
-	"log"
 )
 
 type TestDomains struct {
-	DNS *dnsclient.Route53
+	DNS     *dnsclient.Route53
+	records map[string]*dnsclient.Record
+	err     error
 }
 
 func (t *TestDomains) Process() {
 	fmt.Println("Processing TestDomains")
 
-	records, err := t.DNS.GetAll("")
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	prevRecord := ""
+	lastRecord := ""
+	t.records = make(map[string]*dnsclient.Record, 0)
 
-	for _, record := range records {
-		fmt.Printf("%+v\n", record.Name)
+	for {
+		records, err := t.DNS.GetAll(lastRecord)
+		if err != nil {
+			t.err = err
+			return
+		}
+
+		lastRecord = records[len(records)-1].Name
+		if lastRecord == prevRecord {
+			break
+		}
+
+		// do not include the first record, because it's alread included in the
+		// previous round
+		for _, record := range records[1:] {
+			// do not add NS records
+			if record.Name == "dev.koding.io." {
+				continue
+			}
+
+			t.records[record.Name] = record
+		}
 	}
 }
 
 func (t *TestDomains) Run() {
-	fmt.Println("Running TestDomains")
+	if len(t.records) == 0 {
+		return
+	}
+
+	fmt.Println("Removing '%d' test domains", len(t.records))
 }
 
 func (t *TestDomains) Result() string {
-	return ""
+	if t.err != nil {
+		return fmt.Sprintf("testDomains: error '%s'", t.err.Error())
+	}
+
+	return fmt.Sprintf("removed '%d' development domains from dev.koding.io hosted zone",
+		len(t.records))
 }
 
 func (t *TestDomains) Info() *taskInfo {
