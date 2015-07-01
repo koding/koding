25ad9b54d75117eff05f1660cb61a45ede6b303a
Cihangir SAVAS <cihangir@koding.com>
2015-07-01 00:19:09 -0700

    Scripts: check socialapi code quality if there is any code change

---
+++ scripts/check_socialapi_code_quality.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#  make relative paths work.
+cd $(dirname $0)/..
+
+v=$(git diff-tree -r --name-only --no-commit-id master `git rev-parse HEAD` ./go/src/socialapi 2>&1)
+if [ -z "$v" ]; then
+    echo "nothing has changed under ./go/src/socialapi"
+    exit 0
+fi
+
+echo "checking cyclo complexity"
+./go/bin/gocyclo -top 20 ./go/src/socialapi/*/**/**.go
+
+echo "checking deadcode"
+./scripts/deadcode.sh
+
+echo "checking go vet"
+#./scripts/govet.sh
+
+echo "checking unused variables"
+./scripts/govarcheck.sh
+
+echo "more aggressive go vet"
+./scripts/go-nyetcheck.sh
