3b276ad64bbf02680375463ffb004c4d463a3465
Cihangir SAVAS <cihangir@koding.com>
2015-06-30 17:03:43 -0700

    Tunnelproxymanager: remove aws credentials

--- go/src/koding/workers/tunnelproxymanager/test_run.sh
+++ go/src/koding/workers/tunnelproxymanager/test_run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 set -o errexit
 
-export CONFIG_EBENVNAME="cihangir3"
+export CONFIG_EBENVNAME="test"
 export CONFIG_REGION="us-east-1"
-export CONFIG_AUTOSCALINGNAME="awseb-e-ps6yvwi873-stack-AWSEBAutoScalingGroup-H7SOTEVY95MP"
-export CONFIG_ACCESSKEYID="AKIAIM3GAPJAIWTFZOJQ"
-export CONFIG_SECRETACCESSKEY="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"
+export CONFIG_AUTOSCALINGNAME="CONFIG_AUTOSCALINGNAME"
+export CONFIG_ACCESSKEYID="CONFIG_ACCESSKEYID"
+export CONFIG_SECRETACCESSKEY="CONFIG_SECRETACCESSKEY"
 export CONFIG_DEBUG="true"
 
 go test -c && ./tunnelproxymanager.test
