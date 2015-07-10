cb714cd5937603dc71f4fdd613d213006f717a8b
Fatih Arslan <ftharsln@gmail.com>
2015-07-10 03:04:50 +0300

    tunnel: resolve address for upcoming IP caching

--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -1,7 +1,9 @@
 package tunnel
 
 import (
-	"errors"
+	"fmt"
+	"net"
+	"strings"
 
 	"github.com/koding/klient/Godeps/_workspace/src/github.com/koding/kite"
 	"github.com/koding/klient/Godeps/_workspace/src/github.com/koding/tunnel"
@@ -14,27 +16,45 @@ type registerResult struct {
 }
 
 func Start(k *kite.Kite, conf *tunnel.ClientConfig) error {
-	// Change tunnel server based on environment
-	if conf.ServerAddr == "" {
-		switch protocol.Environment {
-		case "development":
-			conf.ServerAddr = "devtunnelproxy.koding.com:80"
-		case "production":
-			conf.ServerAddr = "tunnelproxy.koding.com:80"
-		}
-	}
-
 	tunnelkite := kite.New("tunnelclient", "0.0.1")
 	tunnelkite.Config = k.Config.Copy()
-
 	if conf.Debug {
 		tunnelkite.SetLogLevel(kite.DEBUG)
 	}
 
+	// Change tunnel server based on environment
 	if conf.ServerAddr == "" {
-		return errors.New("Tunnel server addr is empty")
+		switch protocol.Environment {
+		case "development":
+			conf.ServerAddr = "devtunnelproxy.koding.com"
+		case "production":
+			conf.ServerAddr = "tunnelproxy.koding.com"
+		default:
+			return fmt.Errorf("Tunnel server address is empty. No env found: %s",
+				protocol.Environment)
+		}
 	}
 
+	// Check if the addr is valid IP, the user might pass to us a valid IP.  If
+	// it's not valid, we're going to resolve to the first addr we get.
+	if net.ParseIP(conf.ServerAddr) == nil {
+		k.Log.Debug("Resolving '%s'", conf.ServerAddr)
+		resolved, err := resolvedAddr(conf.ServerAddr)
+		if err != nil {
+			// just log if we couldn't resolve it
+			k.Log.Warning("couldn't resolve '%s: %s", conf.ServerAddr, err)
+		} else {
+			k.Log.Debug("Address resolved to '%s'", resolved)
+			conf.ServerAddr = resolved
+		}
+	}
+
+	// TODO(arslan): store resolved IP to boltdb and use it
+
+	// append port if absent
+	conf.ServerAddr = addPort(conf.ServerAddr, "80")
+
+	k.Log.Debug("Connecting to tunnel server IP: '%s'", conf.ServerAddr)
 	tunnelserver := tunnelkite.NewClient("http://" + conf.ServerAddr + "/kite")
 	// Enable it later if needed
 	// tunnelserver.LocalKite.Config.Transport = config.XHRPolling
@@ -78,4 +98,30 @@ func callRegister(tunnelserver *kite.Client) (*registerResult, error) {
 	}
 
 	return result, nil
+}
+
+func resolvedAddr(host string) (string, error) {
+	addr, err := net.LookupHost(host)
+	if err != nil {
+		return "", err
+	}
+
+	if len(addr) == 0 {
+		return "", fmt.Errorf("no resolved addresses found for '%s'", host)
+	}
+
+	return addr[0], nil
+}
+
+// hasPort detecths if the given name has a port or not
+func hasPort(s string) bool { return strings.LastIndex(s, ":") > strings.LastIndex(s, "]") }
+
+// addPort adds the port and returns "host:port". If the host already contains
+// a port, it returns it.
+func addPort(host, port string) string {
+	if ok := hasPort(host); ok {
+		return host
+	}
+
+	return host + ":" + port
 }
