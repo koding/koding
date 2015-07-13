83cb5007b2ba7c8b04a018e7a9b4f96dc1ecd74c
Fatih Arslan <fatih@koding.com>
2015-07-13 10:58:57 +0300

    bootstrap: change keyname to koding, more understandable

--- go/src/koding/kites/kloud/kloud/bootstrap.go
+++ go/src/koding/kites/kloud/kloud/bootstrap.go
@@ -81,7 +81,7 @@ func (k *Kloud) Bootstrap(r *kite.Request) (interface{}, error) {
 			return nil, err
 		}
 
-		keyName := "kloud-deployment-" + r.Username + "-" + strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+		keyName := "koding-deployment-" + r.Username + "-" + strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
 		finalBootstrap, err = appendKeyName(finalBootstrap, keyName)
 		if err != nil {
 			return nil, err
