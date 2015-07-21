f5713e78b550ab9e299618d245071dfa62f47175
Can Yucel <can@koding.com>
2015-07-21 13:51:19 -0700

    integration: update github events in migration script

--- go/src/socialapi/workers/migrator/controller/migrator.go
+++ go/src/socialapi/workers/migrator/controller/migrator.go
@@ -142,10 +142,11 @@ Click on **Webhooks & Services** in the left navigation, and then press the **Ad
 		webhookmodels.NewEvent("commit_comment", "New comment on commit"),
 		webhookmodels.NewEvent("pull_request", "Pull request opened or closed"),
 		webhookmodels.NewEvent("issues", "Issues opened or closed"),
-		webhookmodels.NewEvent("issue_comment", "New comment on issue or pull request"),
+		webhookmodels.NewEvent("issue_comment", "New comment on issue"),
 		webhookmodels.NewEvent("deployment_status", "Show deployment statuses"),
 		webhookmodels.NewEvent("create", "Branch or tag created"),
 		webhookmodels.NewEvent("delete", "Branch or tag deleted"),
+		webhookmodels.NewEvent("pull_request_review_comment", "new comment on pull request"),
 	)
 
 	githubInt.AddSettings("authorizable", true)
