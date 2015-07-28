01c2d37f517032f2e52e033239f68a77707b2603
Fatih Arslan <ftharsln@gmail.com>
2015-07-28 11:38:37 +0300

    elasticips: filter non associated IPs

--- go/src/koding/kites/kloud/cleaners/cmd/cleaner/elasticips.go
+++ go/src/koding/kites/kloud/cleaners/cmd/cleaner/elasticips.go
@@ -3,27 +3,47 @@ package main
 import (
 	"fmt"
 	"koding/kites/kloud/cleaners/lookup"
+
+	"github.com/mitchellh/goamz/ec2"
 )
 
 type ElasticIPs struct {
 	Lookup *lookup.Lookup
+
+	nonused map[string][]ec2.Address
+	err     error
 }
 
-func (i *ElasticIPs) Process() {
-	addressess := i.Lookup.FetchIpAddresses()
+func (e *ElasticIPs) Process() {
+	addressess := e.Lookup.FetchIpAddresses()
+
+	e.nonused = make(map[string][]ec2.Address)
 
 	for region, a := range addressess {
-		fmt.Printf("%s has total %d addressess\n", region, len(a))
+		nonAssociatedAddressess := make([]ec2.Address, 0)
+		for _, address := range a {
+			if address.AssociationId == "" {
+				nonAssociatedAddressess = append(nonAssociatedAddressess, address)
+			}
+		}
+
+		fmt.Printf("region '%s' has %d addresses (which %d are not associated) \n",
+			region, len(a), len(nonAssociatedAddressess))
+		e.nonused[region] = nonAssociatedAddressess
 	}
 }
 
-func (i *ElasticIPs) Run() {
+func (e *ElasticIPs) Run() {
 }
 
-func (i *ElasticIPs) Result() string {
+func (e *ElasticIPs) Result() string {
+	if e.err != nil {
+		return fmt.Sprintf("elasticIPs: error '%s'", e.err.Error())
+	}
+
 	return ""
 }
 
-func (i *ElasticIPs) Info() *taskInfo {
+func (e *ElasticIPs) Info() *taskInfo {
 	return nil
 }
