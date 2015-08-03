ae3fae1d0403a6d1f35ca6d4f73d54912dc327bd
Fatih Arslan <ftharsln@gmail.com>
2015-08-03 15:25:38 +0300

    logfetcher: initial commit

---
+++ logfetcher/logfetcher.go
@@ -0,0 +1,11 @@
+package logfetcher
+
+import (
+	"errors"
+
+	"github.com/koding/klient/Godeps/_workspace/src/github.com/koding/kite"
+)
+
+func Fetch(r *kite.Request) (interface{}, error) {
+	return nil, errors.New("not implemented yet")
+}
---
+++ logfetcher/logfetcher_test.go
@@ -0,0 +1,7 @@
+package logfetcher
+
+import "testing"
+
+func TestFetch(t *testing.T) {
+	t.Error("not implemented yet")
+}
