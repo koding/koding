a14fee2b3d41ade02e004191ac86d79cf845ba46
Fatih Arslan <ftharsln@gmail.com>
2015-08-03 15:33:55 +0300

    logfetcher: setup test client

--- logfetcher/logfetcher_test.go
+++ logfetcher/logfetcher_test.go
@@ -1,6 +1,34 @@
 package logfetcher
 
-import "testing"
+import (
+	"log"
+	"testing"
+
+	"github.com/koding/klient/Godeps/_workspace/src/github.com/koding/kite"
+)
+
+var (
+	lf     *kite.Kite
+	remote *kite.Client
+)
+
+func init() {
+	lf := kite.New("logfetcher", "0.0.1")
+	lf.Config.DisableAuthentication = true
+	lf.Config.Port = 3639
+	lf.HandleFunc("fetch", Fetch)
+
+	go lf.Run()
+	<-lf.ServerReadyNotify()
+
+	remoteKite := kite.New("remote", "0.0.1")
+	remoteKite.Config.Username = "remote"
+	remote = remoteKite.NewClient("http://127.0.0.1:3639/kite")
+	err := remote.Dial()
+	if err != nil {
+		log.Fatal("err")
+	}
+}
 
 func TestFetch(t *testing.T) {
 	t.Error("not implemented yet")
