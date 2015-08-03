8ff7a68063212018e66fc0372e306b123f042be8
Fatih Arslan <ftharsln@gmail.com>
2015-08-03 15:59:48 +0300

    logfetcher: use tail package to tail the logs

--- logfetcher/logfetcher.go
+++ logfetcher/logfetcher.go
@@ -2,6 +2,9 @@ package logfetcher
 
 import (
 	"errors"
+	"fmt"
+
+	"github.com/ActiveState/tail"
 
 	"github.com/koding/klient/Godeps/_workspace/src/github.com/koding/kite"
 )
@@ -11,5 +14,25 @@ type Request struct {
 }
 
 func Fetch(r *kite.Request) (interface{}, error) {
+	var params *Request
+	if r.Args == nil {
+		return nil, errors.New("arguments are not passed")
+	}
+
+	if r.Args.One().Unmarshal(&params) != nil || params.Path == "" {
+		return nil, errors.New("{ path: [string] }")
+	}
+
+	t, err := tail.TailFile(params.Path, tail.Config{
+		Follow: true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for line := range t.Lines {
+		fmt.Println(line.Text)
+	}
+
 	return nil, errors.New("not implemented yet")
 }
