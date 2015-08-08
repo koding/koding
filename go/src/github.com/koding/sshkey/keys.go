9f1c375da81cdaf1ea6d703c23f26af664426101
mehmetalisavas <mehmetali.savas@yandex.com>
2015-08-08 21:28:25 +0300

    ssh: key pairs are generated

---
+++ go/src/github.com/koding/sshkey/keys.go
@@ -0,0 +1,55 @@
+package sshkey
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func GenerateKeys() {
+	// genereate key pair
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2014)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	/// convert to private key
+	privateKeyDer := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privateKeyDer,
+	}
+	privateKeyPem := string(pem.EncodeToMemory(&privateKeyBlock))
+
+	// convert to public key
+	publicKey := privateKey.PublicKey
+	publicKeyDer, err := x509.MarshalPKIXPublicKey(&publicKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	publicKeyBlock := pem.Block{
+		Type:    "PUBLIC KEY",
+		Headers: nil,
+		Bytes:   publicKeyDer,
+	}
+	publicKeyPem := string(pem.EncodeToMemory(&publicKeyBlock))
+
+	fmt.Println(privateKeyPem)
+	fmt.Println(publicKeyPem)
+
+	////
+	pub, err := ssh.NewPublicKey(&publicKey)
+	if err != nil {
+		fmt.Println("err.Error()-->", err.Error())
+	}
+
+	fmt.Println("ssh-rsa", (base64.StdEncoding.EncodeToString(pub.Marshal())))
+}
