d798176db5736e591d180b19a666225335f072f2
mehmetalisavas <mehmetali.savas@yandex.com>
2015-08-12 14:54:27 +0300

    sshkey: handler for generate key is added

---
+++ go/src/github.com/koding/sshkey/handler.go
@@ -0,0 +1,24 @@
+package sshkey
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, req *http.Request) {
+	pub, priv, err := Generate()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	keyPair := make(map[string]interface{})
+	keyPair["public"] = pub
+	keyPair["private"] = priv
+
+	err = json.NewEncoder(w).Encode(keyPair)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+}
