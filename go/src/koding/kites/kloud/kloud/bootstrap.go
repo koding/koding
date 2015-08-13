f5926ec88352625dda6ff6c001ecffa955d92101
Fatih Arslan <ftharsln@gmail.com>
2015-08-13 16:44:57 +0300

    kloud: change access_key to aws_access_key ...

--- go/src/koding/kites/kloud/kloud/bootstrap.go
+++ go/src/koding/kites/kloud/kloud/bootstrap.go
@@ -241,8 +241,8 @@ type awsTemplateData struct {
 var awsBootstrap = `{
     "provider": {
         "aws": {
-            "access_key": "${var.access_key}",
-            "secret_key": "${var.secret_key}",
+            "access_key": "${var.aws_access_key}",
+            "secret_key": "${var.aws_secret_key}",
             "region": "${var.region}"
         }
     },
