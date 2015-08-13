312975eb56077e16fd05c388a38c8f0ec60bd9c9
Ozan Kaşıkçı <ozan@koding.com>
2015-08-13 21:11:48 +0300

    fix wrong dir names in scripts/lint-coffeescript.sh

--- scripts/lint-coffeescript.sh
+++ scripts/lint-coffeescript.sh
@@ -2,12 +2,12 @@
 
 ROOT_DIR=$(dirname $0)/..
 COFFEELINT_BIN="$ROOT_DIR/node_modules/coffeelint/bin/coffeelint"
-SOCIAL_WORKER_COFFEE_DIR="$ROOT_DIR/workers"
-NODE_WEBSERVER_COFFEE_DIR="$ROOT_DIR/servers"
+WORKERS_DIR="$ROOT_DIR/workers"
+SERVERS_DIR="$ROOT_DIR/servers"
 
 function runCoffeeLint {
   $COFFEELINT_BIN $1
 }
 
 echo 'Linting nodejs web servers directory'
-runCoffeeLint $NODE_WEBSERVER_COFFEE_DIR
+runCoffeeLint $SERVERS_DIR
