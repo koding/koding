14191c06b9d29be53deeaeb61c86061268482045
Cihangir SAVAS <cihangir@koding.com>
2015-08-26 15:49:56 -0700

    Kite: set default values

--- go/src/koding/kites/cmd/provider-vagrantkite/main.go
+++ go/src/koding/kites/cmd/provider-vagrantkite/main.go
@@ -6,6 +6,13 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 )
 
+func init() {
+	vagrantkite.Version = "0.0.1"
+	vagrantkite.Name = "vagrantkite"
+	vagrantkite.Environment = "terraform"
+	vagrantkite.Region = "terraform"
+}
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: vagrantkite.Provider,
