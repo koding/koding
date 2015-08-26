831e051757d029b4edafbc32ef35555e38fda0ea
Cihangir SAVAS <cihangir@koding.com>
2015-08-26 15:49:37 -0700

    Kite: make consts var

--- go/src/koding/kites/terraformplugins/vagrantkite/config.go
+++ go/src/koding/kites/terraformplugins/vagrantkite/config.go
@@ -9,11 +9,11 @@ import (
 	"github.com/koding/logging"
 )
 
-const (
-	version     = "0.0.1"
-	name        = "vagrantkite"
-	environment = "terraform"
-	region      = "terraform"
+var (
+	Version     = "0.0.1"
+	Name        = "vagrantkite"
+	Environment = "terraform"
+	Region      = "terraform"
 )
 
 type Client struct {
@@ -21,10 +21,10 @@ type Client struct {
 	Log  logging.Logger
 }
 
-// Client returns  client for accessing github.
+// NewClient returns client for accessing remote klient.
 func NewClient() (*Client, error) {
 	// init kite here
-	k := kite.New(name, version)
+	k := kite.New(Name, Version)
 
 	var err error
 	k.Config, err = kiteconfig.Get()
@@ -34,11 +34,12 @@ func NewClient() (*Client, error) {
 
 	// no need to set, will be set randomly.
 	// k.Config.Port = 9876
-	k.Config.Environment = environment
-	k.Config.Region = region
+	k.Config.Environment = Environment
+	k.Config.Region = Region
+
 	return &Client{
 		Kite: k,
-		Log:  common.NewLogger(name, false),
+		Log:  common.NewLogger(Name, false),
 	}, nil
 
 }
