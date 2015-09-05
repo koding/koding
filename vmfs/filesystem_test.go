e7e1ba30d5f4504d5b20176841b23410400b08bd
Senthil <senthil@koding.com>
2015-09-04 18:05:16 -0700

    move fakeTransport test helper to vmfs

--- vmfs/filesystem_test.go
+++ vmfs/filesystem_test.go
@@ -1,6 +1,8 @@
 package vmfs
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/koding/fuseklient/transport"
@@ -12,6 +14,43 @@ var (
 	remotePath = "/fusemount"
 	localPath  = "/Users/senthil/work/fuse/fuseklient/mount"
 )
+
+type fakeTransport struct {
+	TripMethodName string
+	TripResponse   interface{}
+}
+
+func (f *fakeTransport) Trip(methodName string, req interface{}, res interface{}) error {
+	if f.TripMethodName != methodName {
+		return fmt.Errorf("Expected '%s' as methodName, got '%s'", f.TripMethodName, methodName)
+	}
+
+	bytes, err := json.Marshal(f.TripResponse)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(bytes, &res)
+}
+func TestFakeTransportStub(t *testing.T) {
+	Convey("Given fake transport", t, func() {
+		ft := &fakeTransport{
+			TripMethodName: "test",
+			TripResponse:   struct{ Name string }{"res"},
+		}
+
+		Convey("It should return error if method names don't match", func() {
+			So(ft.Trip("badmethodname", "", ""), ShouldNotBeNil)
+		})
+
+		Convey("It should return unmarshal mock into response", func() {
+			res := struct{ Name string }{}
+
+			So(ft.Trip("test", "", &res), ShouldBeNil)
+			So(res.Name, ShouldEqual, "res")
+		})
+	})
+}
 
 func TestFileSystem(t *testing.T) {
 	Convey("Given filesystem", t, func() {
