33e6c20519ec1692235a8737e9cf6fdae6df61e2
Senthil <senthil@koding.com>
2015-09-08 17:47:43 -0700

    move FuseConfig to own folder so it can be used in fs pkg

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+// FuseConfig contains user customizable options for mounting.
+type FuseConfig struct {
+	// IP is ip of user VM to connect.
+	IP string `required:"true"`
+
+	// RemotePath is path to folder in user VM to be mounted locally.
+	RemotePath string `required:"true"`
+
+	// LocalPath is path to folder in local to serve as mount point.
+	LocalPath string `required:"true"`
+
+	// MountName is identifier for the mounted folder.
+	MountName string `default:"fuseklient"`
+
+	// SshUser is the optional arg used in `auth/save-kite-key.sh`.
+	// TODO: remove this after intergrating with Klient.
+	SshUser string
+
+	// Debug determines if debug logs should be shown to user.
+	Debug bool `default:true`
+}
