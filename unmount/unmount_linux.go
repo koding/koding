a89e0f6b556e889855974b3cd4e3f7db6073f196
Senthil <senthil@koding.com>
2015-09-13 00:09:27 -0700

    remove 'Unmounting...' log

--- unmount/unmount_linux.go
+++ unmount/unmount_linux.go
@@ -8,8 +8,6 @@ import (
 // Unmount un mounts Fuse mounted local folder. Mount exists separate to
 // lifecycle of this program and needs to be cleaned up when this exists.
 func Unmount(folder string) error {
-	fmt.Println("Unmounting...\n")
-
 	if _, err := exec.Command("sudo", "umount", folder).CombinedOutput(); err != nil {
 		fmt.Printf("Unmounting failed. Please do `sudo umount %s`.\n", folder)
 	}
