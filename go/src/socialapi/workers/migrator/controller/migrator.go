5639a6c6b71574fb04c57ef4a3a4b65711e6fa2e
mehmetalisavas <mehmetali.savas@yandex.com>
2015-09-14 15:18:36 +0300

    integration: type constants are added

--- go/src/socialapi/workers/migrator/controller/migrator.go
+++ go/src/socialapi/workers/migrator/controller/migrator.go
@@ -129,6 +129,7 @@ func (mwc *Controller) describeIntegrations() ([]*webhookmodels.Integration, err
 	githubInt.Summary = "Source control and code management."
 	githubInt.IconPath = "https://koding-cdn.s3.amazonaws.com/temp-images/github.png"
 	githubInt.Description = "GitHub offers online source code hosting for Git projects, with powerful collaboration, code review, and issue tracking. \n \n This integration will post commits, pull requests, and activity on GitHub Issues to a channel in Koding."
+	githubInt.TypeConstant = webhookmodels.Integration_TYPE_INCOMING
 	githubInt.Instructions = `
 #### Step 1
 
@@ -171,6 +172,7 @@ Click on **Webhooks & Services** in the left navigation, and then press the **Ad
 	travisInt.Summary = "Hosted software build services."
 	travisInt.IconPath = "https://koding-cdn.s3.amazonaws.com/temp-images/travisci.png"
 	travisInt.Description = "Travis CI is a continuous integration platform that takes care of running your software tests and deploying your apps. This integration will allow your team to receive notifications in Koding for normal branch builds, and for pull requests, as well."
+	travisInt.TypeConstant = webhookmodels.Integration_TYPE_INCOMING
 	travisInt.IsPublished = false
 
 	integrations = append(integrations, travisInt)
@@ -182,6 +184,7 @@ Click on **Webhooks & Services** in the left navigation, and then press the **Ad
 	pivotalInt.Summary = "Collaborative, lightweight agile project management."
 	pivotalInt.IconPath = "https://koding-cdn.s3.amazonaws.com/temp-images/pivotaltracker.png"
 	pivotalInt.Description = "Pivotal Tracker is an agile project management tool that shows software teams their work in progress and allows them to track upcoming milestones. This integration will post updates to a channel in Koding whenever a story activity occurs in Pivotal Tracker."
+	pivotalInt.TypeConstant = webhookmodels.Integration_TYPE_INCOMING
 
 	integrations = append(integrations, pivotalInt)
 
@@ -192,6 +195,7 @@ Click on **Webhooks & Services** in the left navigation, and then press the **Ad
 	pagerdutyInt.Summary = "On-call scheduling, alerting, and incident tracking."
 	pagerdutyInt.IconPath = "https://s3.amazonaws.com/koding-cdn/temp-images/pagerduty.png"
 	pagerdutyInt.Description = "PagerDuty provides IT alert monitoring, on-call scheduling, escalation policies and incident tracking to fix problems in your apps, servers and websites."
+	pagerdutyInt.TypeConstant = webhookmodels.Integration_TYPE_INCOMING
 
 	pagerdutyInt.Settings = gorm.Hstore{}
 
@@ -235,7 +239,6 @@ func (mwc *Controller) EnsureIntegrations() {
 			}
 		} else {
 			integration.Id = i.Id
-			integration.TypeConstant = i.TypeConstant
 			if err = integration.Update(); err != nil {
 				mwc.log.Error("Could not update integration: %s", err)
 			}
