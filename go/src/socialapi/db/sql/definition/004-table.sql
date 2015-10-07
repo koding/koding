47b62aad467e7f58ee315c22e544c6b7e6618d2a
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-07 17:30:21 +0300

    social/moderation: deletedAt is added into social db

--- go/src/socialapi/db/sql/definition/004-table.sql
+++ go/src/socialapi/db/sql/definition/004-table.sql
@@ -234,7 +234,8 @@ CREATE TABLE "api"."channel_link" (
     ),
     "root_id" BIGINT NOT NULL,
     "leaf_id" BIGINT NOT NULL,
-    "created_at" TIMESTAMP(6) WITH TIME ZONE NOT NULL
+    "created_at" TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    "deleted_at" TIMESTAMP (6) WITH TIME ZONE
 ) WITH (OIDS=FALSE);
 -- give required channel_link permissions
 GRANT SELECT, INSERT, DELETE ON "api"."channel_link" TO "social";
