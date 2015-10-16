5731d0f181c5b32e132fbb97e8a26233fc90dfaf
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-16 22:23:48 +0300

    social/tests: payload test of integration is added

--- go/src/socialapi/workers/integration/webhook/tests/integration_test.go
+++ go/src/socialapi/workers/integration/webhook/tests/integration_test.go
@@ -131,6 +131,48 @@ func TestWebhook(t *testing.T) {
 
 	})
 
+	Convey("We should be able to successfully receive payload of github push messages via middleware", t, func() {
+		channelIntegration, topicChannel := webhook.CreateTestChannelIntegration(t)
+
+		account, err := models.CreateAccountInBothDbsWithNick(models.RandomName())
+		So(err, ShouldBeNil)
+
+		channel := models.CreateTypedGroupedChannelWithTest(account.Id, models.Channel_TYPE_GROUP, channelIntegration.GroupName)
+
+		_, err = channel.AddParticipant(account.Id)
+		So(err, ShouldBeNil)
+		_, err = topicChannel.AddParticipant(account.Id)
+		So(err, ShouldBeNil)
+
+		err = rest.DoGithubPush(githubPushEventData, channelIntegration.Token)
+		So(err, ShouldBeNil)
+
+		ses, err := models.FetchOrCreateSession(account.Nick, channelIntegration.GroupName)
+		So(err, ShouldBeNil)
+		So(ses, ShouldNotBeNil)
+
+		tick := time.Tick(time.Millisecond * 200)
+		deadLine := time.After(10 * time.Second)
+		for {
+			select {
+			case <-tick:
+				resp, err := rest.GetHistory(topicChannel.Id,
+					&request.Query{},
+					ses.ClientId,
+				)
+				So(err, ShouldBeNil)
+				if len(resp.MessageList) > 0 {
+					So(len(resp.MessageList), ShouldEqual, 1)
+					So(*resp.MessageList[0].Message.Payload["eventType"], ShouldEqual, "push")
+					return
+				}
+			case <-deadLine:
+				So(errors.New("Could not fetch messages"), ShouldBeNil)
+			}
+		}
+
+	})
+
 	Convey("We should be able to successfully receive pivotal push messages via middleware", t, func() {
 		channelIntegration, topicChannel := webhook.CreateTestChannelIntegration(t)
 
