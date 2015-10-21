dafddc128bf89a9d51fcdea0682e9a6e27cfb636
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-22 02:10:45 +0300

    notificationSettings: bongo after & before functions are added

--- go/src/socialapi/models/notificationsettings_bongo.go
+++ go/src/socialapi/models/notificationsettings_bongo.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"time"
+
+	"github.com/koding/bongo"
+)
+
 // TO-DO
 //
 // Are you sure to add this notification settings into the api schema
@@ -8,10 +14,51 @@ package models
 //~Mehmet Ali
 const NotificationSettingsBongoName = "api.channel"
 
-func (ns NotificationSettings) GetId() int64 {
-	return ns.Id
+func (n NotificationSettings) GetId() int64 {
+	return n.Id
 }
 
-func (ns NotificationSettings) BongoName() string {
+func (n NotificationSettings) BongoName() string {
 	return NotificationSettingsBongoName
+}
+
+func (n *NotificationSettings) AfterCreate() {
+	bongo.B.AfterCreate(n)
+}
+
+func (n *NotificationSettings) AfterUpdate() {
+	bongo.B.AfterUpdate(n)
+}
+
+func (n *NotificationSettings) BeforeCreate() error {
+	if err := n.validateBeforeOps(); err != nil {
+		return err
+	}
+	n.CreatedAt = time.Now().UTC()
+	n.UpdatedAt = time.Now().UTC()
+
+	return
+}
+
+func (n *NotificationSettings) validateBeforeOps() error {
+	if n.AccountId == 0 {
+		return ErrAccountIdIsNotSet
+	}
+
+	if n.ChannelId == 0 {
+		return ErrChannelIdIsNotSet
+	}
+
+	a := NewAccount()
+	if err := a.ById(n.AccountId); err != nil {
+		return err
+	}
+
+	l := NewChannel()
+	if err := l.ById(n.ChannelId); err != nil {
+		return err
+	}
+	// We should add group && group control ??
+
+	return nil
 }
