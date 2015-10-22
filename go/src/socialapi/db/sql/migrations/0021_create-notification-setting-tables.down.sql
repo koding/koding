c64f85a4dcc7b3774fc2d38f0f0f9ed5a18957a3
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-22 23:13:31 +0300

    sql: status constant is added into drop migration

--- go/src/socialapi/db/sql/migrations/0021_create-notification-setting-tables.down.sql
+++ go/src/socialapi/db/sql/migrations/0021_create-notification-setting-tables.down.sql
@@ -2,5 +2,4 @@
 DROP TABLE IF EXISTS "notification"."notification_settings";
 
 DROP SEQUENCE IF EXISTS "notification"."notification_settings_id_seq";
-
-
+DROP TYPE "notification"."notification_settings_status_constant_enum";
