2b0e59cf042d756cda518adc9b34f732666955f6
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-22 22:23:19 +0300

    sql: notification settings table and fields are added

--- go/src/socialapi/db/sql/migrations/0021_create-notification-setting-tables.up.sql
+++ go/src/socialapi/db/sql/migrations/0021_create-notification-setting-tables.up.sql
@@ -17,3 +17,28 @@ DO $$
     END;
   END;
 $$;
+
+GRANT USAGE ON SEQUENCE "notification"."notification_settings_id_seq" TO "social";
+
+CREATE TABLE IF NOT EXISTS "notification"."notification_settings"  (
+    "id" BIGINT NOT NULL DEFAULT nextval(
+        'notification.channel_link_id_seq' :: regclass
+    ),
+    "channel_id" BIGINT NOT NULL,
+    "account_id" BIGINT NOT NULL,
+    "desktop_setting" TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    "mobile_setting" TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    "is_muted" BOOLEAN NOT NULL DEFAULT FALSE,
+    "is_suppressed" BOOLEAN NOT NULL DEFAULT FALSE,
+    "created_at" TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    "updated_at" TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+
+    -- create constraints along with table creation
+    PRIMARY KEY ("id") NOT DEFERRABLE INITIALLY IMMEDIATE,
+    CONSTRAINT "notification_settings_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "notification"."channel" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE,
+    CONSTRAINT "notification_settings_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "notification"."channel" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE,
+    CONSTRAINT "channel_link_root_id_leaf_id_key" UNIQUE ("root_id","leaf_id") NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS=FALSE);
+
+-- give required channel_link permissions
+GRANT SELECT, INSERT, DELETE ON "notification"."channel_link" TO "social";
