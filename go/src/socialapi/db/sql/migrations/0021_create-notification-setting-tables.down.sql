d2c1370c4a758ffbac4d4ee427d414e7a6115246
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-22 22:22:51 +0300

    sql: notification settings down migration is added

---
+++ go/src/socialapi/db/sql/migrations/0021_create-notification-setting-tables.down.sql
@@ -0,0 +1,6 @@
+
+DROP TABLE "notification"."notification_settings";
+DROP SEQUENCE "notification"."notification_settings_id_seq";
+DROP TYPE "notification"."notification_type_constant_enum";
+DROP INDEX IF EXISTS "notification"."channel_integration_token_idx";
+
