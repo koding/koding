d5bb98d489b219437ae83cb49ea7445a891cb7eb
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-22 22:33:50 +0300

    sql: migrate down needs are fixed

--- go/src/socialapi/db/sql/migrations/0021_create-notification-setting-tables.down.sql
+++ go/src/socialapi/db/sql/migrations/0021_create-notification-setting-tables.down.sql
@@ -1,6 +1,6 @@
 
-DROP TABLE "notification"."notification_settings";
-DROP SEQUENCE "notification"."notification_settings_id_seq";
-DROP TYPE "notification"."notification_type_constant_enum";
-DROP INDEX IF EXISTS "notification"."channel_integration_token_idx";
+DROP TABLE IF EXISTS "notification"."notification_settings";
+
+DROP SEQUENCE IF EXISTS "notification"."notification_settings_id_seq";
+
 
