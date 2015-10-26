45de4cfef17d19776fbc86ff453cdd89c54c7451
Lee Olayvar <leeolayvar@gmail.com>
2015-10-26 13:46:50 -0700

    klientctl: Incremented the update version check

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ const (
 	// Version is the current version of klientctl. This number is used
 	// by CheckUpdate to determine if current version is behind or equal to latest
 	// version on S3 bucket.
-	Version = 1
+	Version = 2
 
 	osName = runtime.GOOS
 
