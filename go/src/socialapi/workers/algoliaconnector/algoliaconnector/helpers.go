3ee3015ffe59c98ad8fe4c29e85fbbcd9be0848b
Cihangir SAVAS <cihangir@koding.com>
2015-11-04 11:40:19 -0800

    Socialapi: fix PR comments

--- go/src/socialapi/workers/algoliaconnector/algoliaconnector/helpers.go
+++ go/src/socialapi/workers/algoliaconnector/algoliaconnector/helpers.go
@@ -57,32 +57,24 @@ func (f *Controller) partialUpdate(indexName string, record map[string]interface
 
 // AddUniqueTag Adds a number or string element to an array based-attribute if it doesn’t exist
 func (f *Controller) AddUniqueTag(indexName string, objectID, value interface{}) error {
-	return f.partialUpdate(indexName, map[string]interface{}{
-		"objectID": objectID,
-		"_tags": map[string]interface{}{
-			"_operation": "AddUnique",
-			"value":      value,
-		},
-	})
+	return f.tagAction("AddUnique", indexName, objectID, value)
 }
 
 // RemoveTag Removes the first element from an array-based attribute
 func (f *Controller) RemoveTag(indexName string, objectID, value interface{}) error {
-	return f.partialUpdate(indexName, map[string]interface{}{
-		"objectID": objectID,
-		"_tags": map[string]interface{}{
-			"_operation": "Remove",
-			"value":      value,
-		},
-	})
+	return f.tagAction("Remove", indexName, objectID, value)
 }
 
 // AddTag Appends a number or string element to an array-based attribute
 func (f *Controller) AddTag(indexName string, objectID, value interface{}) error {
+	return f.tagAction("Add", indexName, objectID, value)
+}
+
+func (f *Controller) tagAction(action, indexName string, objectID, value interface{}) error {
 	return f.partialUpdate(indexName, map[string]interface{}{
 		"objectID": objectID,
 		"_tags": map[string]interface{}{
-			"_operation": "Add",
+			"_operation": action,
 			"value":      value,
 		},
 	})
