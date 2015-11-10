83500c5d0b4b326090d5dcee62eea5ce51b0b067
Rafal Jeczalik <rjeczalik@gmail.com>
2015-11-10 16:45:34 +0100

    api/amazon: migrate to aws-sdk-go

--- go/src/koding/kites/kloud/api/amazon/elastic.go
+++ go/src/koding/kites/kloud/api/amazon/elastic.go
@@ -1,30 +1,12 @@
 package amazon
 
-import "github.com/mitchellh/goamz/ec2"
-
-func (a *Amazon) AllocateAddress() (*ec2.AllocateAddressResp, error) {
-	return a.Client.AllocateAddress(&ec2.AllocateAddress{Domain: "vpc"})
-}
-
-func (a *Amazon) AssociateAddress(instanceId, allocationId string) (*ec2.AssociateAddressResp, error) {
-	return a.Client.AssociateAddress(&ec2.AssociateAddress{
-		InstanceId:   instanceId,
-		AllocationId: allocationId,
-	})
-}
-
-func (a *Amazon) AllocateAndAssociateIP(instanceId string) (string, error) {
-	allocateResp, err := a.Client.AllocateAddress(&ec2.AllocateAddress{Domain: "vpc"})
+func (a *Amazon) AllocateAndAssociateIP(instanceID string) (string, error) {
+	allocID, publicIP, err := a.Client.AllocateAddress("vpc")
 	if err != nil {
 		return "", err
 	}
-
-	if _, err := a.Client.AssociateAddress(&ec2.AssociateAddress{
-		InstanceId:   instanceId,
-		AllocationId: allocateResp.AllocationId,
-	}); err != nil {
+	if err = a.Client.AssociateAddress(instanceID, allocID); err != nil {
 		return "", err
 	}
-
-	return allocateResp.PublicIp, nil
+	return publicIP, nil
 }
