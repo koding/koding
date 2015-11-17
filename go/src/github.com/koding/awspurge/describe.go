60cd9937d21b47b4fea2f2bcdeabf838a93f7a43
Fatih Arslan <ftharsln@gmail.com>
2015-11-17 17:35:26 +0200

    awspurge: simplify fetching for upcoming rate limit

--- go/src/github.com/koding/awspurge/describe.go
+++ go/src/github.com/koding/awspurge/describe.go
@@ -1,120 +1,51 @@
 package awspurge
 
 import (
-	"sync"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/hashicorp/go-multierror"
 )
 
-func (p *Purge) describeElbResources(fn func(*elb.ELB) (interface{}, error)) (map[string]interface{}, error) {
-	var (
-		wg sync.WaitGroup
-		mu sync.Mutex
+func (p *Purge) describeElbResources(fn func(*elb.ELB) error) {
+	for _, s := range p.services.elb {
+		p.fetchWg.Add(1)
 
-		multiErrors error
-	)
-
-	output := make(map[string]interface{})
-
-	for r, s := range p.services.elb {
-		wg.Add(1)
-
-		go func(region string, svc *elb.ELB) {
-			out, err := fn(svc)
+		go func(svc *elb.ELB) {
+			err := fn(svc)
 			if err != nil {
-				mu.Lock()
-				multiErrors = multierror.Append(multiErrors, err)
-				mu.Unlock()
-				wg.Done()
-				return
+				p.fetchMu.Lock()
+				p.fetchErrs = multierror.Append(p.fetchErrs, err)
+				p.fetchMu.Unlock()
 			}
 
-			mu.Lock()
-			output[region] = out
-			mu.Unlock()
-
-			wg.Done()
-		}(r, s)
+			p.fetchWg.Done()
+		}(s)
 	}
-
-	wg.Wait()
-
-	return output, multiErrors
 }
 
-func (p *Purge) describeResources(fn func(*ec2.EC2) (interface{}, error)) (map[string]interface{}, error) {
-	var (
-		wg sync.WaitGroup
-		mu sync.Mutex
+func (p *Purge) describeResources(fn func(*ec2.EC2) error) {
+	for _, s := range p.services.ec2 {
+		p.fetchWg.Add(1)
 
-		multiErrors error
-	)
-
-	output := make(map[string]interface{})
-
-	for r, s := range p.services.ec2 {
-		wg.Add(1)
-
-		go func(region string, svc *ec2.EC2) {
-			out, err := fn(svc)
+		go func(svc *ec2.EC2) {
+			err := fn(svc)
 			if err != nil {
-				mu.Lock()
-				multiErrors = multierror.Append(multiErrors, err)
-				mu.Unlock()
-				wg.Done()
-				return
+				p.fetchMu.Lock()
+				p.fetchErrs = multierror.Append(p.fetchErrs, err)
+				p.fetchMu.Unlock()
 			}
 
-			mu.Lock()
-			output[region] = out
-			mu.Unlock()
-
-			wg.Done()
-		}(r, s)
+			p.fetchWg.Done()
+		}(s)
 	}
-
-	wg.Wait()
-
-	return output, multiErrors
 }
 
-// DescribeVolumes returns all volumes per region.
-func (p *Purge) DescribeVolumes() (map[string][]*ec2.Volume, error) {
-	describeVolumes := func(svc *ec2.EC2) (interface{}, error) {
-		resp, err := svc.DescribeVolumes(nil)
-		if err != nil {
-			return nil, err
-		}
-
-		return resp.Volumes, nil
-	}
-
-	out, err := p.describeResources(describeVolumes)
-	if err != nil {
-		return nil, err
-	}
-
-	volumes := make(map[string][]*ec2.Volume)
-	for region, v := range out {
-		vols, ok := v.([]*ec2.Volume)
-		if !ok {
-			continue
-		}
-		volumes[region] = vols
-	}
-
-	return volumes, nil
-}
-
-// DescribeInstances returns all instances per region.
-func (p *Purge) DescribeInstances() (map[string][]*ec2.Instance, error) {
-	describeInstances := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchInstances() {
+	describeInstances := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeInstances(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		instances := make([]*ec2.Instance, 0)
@@ -126,332 +57,247 @@ func (p *Purge) DescribeInstances() (map[string][]*ec2.Instance, error) {
 			}
 		}
 
-		return instances, nil
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].instances = instances
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(describeInstances)
-	if err != nil {
-		return nil, err
-	}
-
-	instances := make(map[string][]*ec2.Instance)
-	for region, i := range out {
-		ins, ok := i.([]*ec2.Instance)
-		if !ok {
-			continue
-		}
-		instances[region] = ins
-	}
-
-	return instances, nil
+	p.describeResources(describeInstances)
 }
 
-// DescribePairs returns all key pairs per region
-func (p *Purge) DescribeKeyPairs() (map[string][]*ec2.KeyPairInfo, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchVolumes() {
+	describeVolumes := func(svc *ec2.EC2) error {
+		resp, err := svc.DescribeVolumes(nil)
+		if err != nil {
+			return err
+		}
+
+		volumes := resp.Volumes
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].volumes = volumes
+		p.resourceMu.Unlock()
+		return nil
+	}
+
+	p.describeResources(describeVolumes)
+}
+
+func (p *Purge) FetchKeyPairs() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeKeyPairs(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		return resp.KeyPairs, nil
+		resources := resp.KeyPairs
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].keyPairs = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.KeyPairInfo)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.KeyPairInfo)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribePlacementGroups returns all placementGroups per region
-func (p *Purge) DescribePlacementGroups() (map[string][]*ec2.PlacementGroup, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchPlacementGroups() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribePlacementGroups(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		return resp.PlacementGroups, nil
+		resources := resp.PlacementGroups
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].placementGroups = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.PlacementGroup)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.PlacementGroup)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeAddresses returns all elastic IP's per region
-func (p *Purge) DescribeAddresses() (map[string][]*ec2.Address, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchAddresses() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeAddresses(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		return resp.Addresses, nil
+		resources := resp.Addresses
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].addresses = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.Address)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.Address)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeSnapshots returns all snapshots per region
-func (p *Purge) DescribeSnapshots() (map[string][]*ec2.Snapshot, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchSnapshots() {
+	fn := func(svc *ec2.EC2) error {
 		input := &ec2.DescribeSnapshotsInput{
 			OwnerIds: stringSlice("self"),
 		}
 
 		resp, err := svc.DescribeSnapshots(input)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		return resp.Snapshots, nil
+		resources := resp.Snapshots
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].snapshots = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.Snapshot)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.Snapshot)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeSecurityGroups returns all security groups per region
-func (p *Purge) DescribeSecurityGroups() (map[string][]*ec2.SecurityGroup, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchSecurityGroups() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeSecurityGroups(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		return resp.SecurityGroups, nil
+		resources := resp.SecurityGroups
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].securityGroups = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.SecurityGroup)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.SecurityGroup)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeLoadBalancers returns all load balancers per region
-func (p *Purge) DescribeLoadBalancers() (map[string][]*elb.LoadBalancerDescription, error) {
-	fn := func(svc *elb.ELB) (interface{}, error) {
+func (p *Purge) FetchLoadBalancers() {
+	fn := func(svc *elb.ELB) error {
 		resp, err := svc.DescribeLoadBalancers(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		return resp.LoadBalancerDescriptions, nil
+		loadBalancers := resp.LoadBalancerDescriptions
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].loadBalancers = loadBalancers
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeElbResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*elb.LoadBalancerDescription)
-	for region, r := range out {
-		resources, ok := r.([]*elb.LoadBalancerDescription)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeElbResources(fn)
 }
 
-// DescribeVpcs all vpcs per region
-func (p *Purge) DescribeVpcs() (map[string][]*ec2.Vpc, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchVpcs() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeVpcs(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		return resp.Vpcs, nil
+		resources := resp.Vpcs
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].vpcs = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.Vpc)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.Vpc)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeSubnets all subnets per region
-func (p *Purge) DescribeSubnets() (map[string][]*ec2.Subnet, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchSubnets() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeSubnets(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		return resp.Subnets, nil
+
+		resources := resp.Subnets
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].subnets = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.Subnet)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.Subnet)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeNetworkAcls all network acls per region
-func (p *Purge) DescribeNetworkAcls() (map[string][]*ec2.NetworkAcl, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchNetworkAcls() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeNetworkAcls(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		return resp.NetworkAcls, nil
+
+		resources := resp.NetworkAcls
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].networkAcls = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.NetworkAcl)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.NetworkAcl)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeInternetGatewats all internet gateways per region
-func (p *Purge) DescribeInternetGateways() (map[string][]*ec2.InternetGateway, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchInternetGateways() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeInternetGateways(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		return resp.InternetGateways, nil
+
+		resources := resp.InternetGateways
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].internetGateways = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.InternetGateway)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.InternetGateway)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
-// DescribeRouteTables all internet gateways per region
-func (p *Purge) DescribeRouteTables() (map[string][]*ec2.RouteTable, error) {
-	fn := func(svc *ec2.EC2) (interface{}, error) {
+func (p *Purge) FetchRouteTables() {
+	fn := func(svc *ec2.EC2) error {
 		resp, err := svc.DescribeRouteTables(nil)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		return resp.RouteTables, nil
+
+		resources := resp.RouteTables
+		region := *svc.Config.Region
+
+		p.resourceMu.Lock()
+		p.resources[region].routeTables = resources
+		p.resourceMu.Unlock()
+		return nil
 	}
 
-	out, err := p.describeResources(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	output := make(map[string][]*ec2.RouteTable)
-	for region, r := range out {
-		resources, ok := r.([]*ec2.RouteTable)
-		if !ok {
-			continue
-		}
-		output[region] = resources
-	}
-
-	return output, nil
+	p.describeResources(fn)
 }
 
 // stringSlice is an helper method to convert a slice of strings into a slice
