7cfe9ca4f4a7986b3c773fa883f5d4d2ddfc3281
Lee Olayvar <leeolayvar@gmail.com>
2015-11-18 10:50:22 -0800

    uninstall: Refactored Uninstall.Uninstall(), for ease of testing

--- uninstall.go
+++ uninstall.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -48,6 +49,8 @@ type Uninstall struct {
 	// KlientDirectory       == kite/klient
 	// KlientParentDirectory == /opt
 	// FullKlientBinPath     == /opt/kite/klient/klient
+	//
+	// See also: Uninstall.RemoveKlientDirectories()
 	KlientParentDirectory string
 
 	// *All* of the directories of the klient binary to be removed, if empty. It's
@@ -64,9 +67,14 @@ type Uninstall struct {
 
 	// The klient.sh filename to be removed.
 	KlientshFilename string
+
+	// remover is a remove func, which can be used to play safe for testing. Typically
+	// equals os.Remove.
+	remover func(string) error
 }
 
-// Uninstall actually runs the uninstall process, configured by the structs fields.
+// Uninstall actually runs the uninstall process, configured by the structs fields,
+// and implemented by all the individual uninstall methods.
 //
 // Note that Uninstall cleans up the directories if empty, but does not print
 // warning messages about this. This is to avoid spam, as one failure to remove a
@@ -89,71 +97,27 @@ func (u *Uninstall) Uninstall() (string, int) {
 		}
 	}
 
-	// Remove kiteHome, if empty
-	if u.KiteKeyDirectory != "" {
-		// As with most file operations, checking the state and then acting is an
-		// error prone and non-atomic endevour. As such, we're just removing the
-		// directory here, and if it is non-empty this operation will fail.
-		os.Remove(u.KiteKeyDirectory)
+	// Remove the klient/klient.sh files
+	if err := u.RemoveKlientFiles(); err != nil {
+		warnings = append(warnings, fmt.Sprintf(
+			"Warning: Failed to remove %s files. This is not a critical issue.\n",
+			u.KlientName,
+		))
 	}
 
-	// Remove the Klient bin
-	if p := filepath.Join(u.KlientDirectory, u.KlientFilename); p != "" {
-		if err := os.Remove(p); err != nil {
-			warnings = append(warnings, fmt.Sprintf(
-				"Warning: Failed to remove %s binary. This is not a critical issue.\n",
-				u.KlientName,
-			))
-		}
-	}
-
-	// Remove klient.sh
-	if p := filepath.Join(u.KlientDirectory, u.KlientshFilename); p != "" {
-		if err := os.Remove(p); err != nil {
-			warnings = append(warnings, fmt.Sprintf(
-				"Warning: Failed to remove %s supporting file. This is not a critical issue.\n",
-				u.KlientName,
-			))
-		}
-	}
-
-	// Remove the klient directories repeatedly up until the parent.
+	// Remove the klient directories
 	//
-	// Notes about the loops if checks:
-	//
-	//     // Ensures that u.KlientDirectory is not empty:
-	//     p != ""
-	//     // Ensures that the filepath is not root, in the case that
-	//     // u.KlientDirectory == "/foo/bar" or "/opt/kite/klient"
-	//     p != string(filepath.Separator)
-	//     // Ensures that filepath is not the current directory, ie the end of the
-	//     // KlientDirectories
-	//     p != "."
-	for p := u.KlientDirectory; p != "" && p != string(filepath.Separator) && p != "."; p = filepath.Dir(p) {
-		rmP := filepath.Join(u.KlientParentDirectory, p)
-
-		// Just to be extra safe, since we're loop removing, lets confirm that it's
-		// not the parent dir. In multiple forms.
-		if rmP == u.KlientParentDirectory || filepath.Clean(rmP) == filepath.Clean(u.KlientParentDirectory) {
-			break
-		}
-
-		// If there is any error, just bail out of the loop since we can't delete P's
-		// parent if we were unable to delete P to begin with.
-		if err := os.Remove(rmP); err != nil {
-			break
-		}
-	}
+	// Note that we're ignoring any errors in removing the directories to avoid
+	// spamming the user with warnings.
+	u.RemoveKlientDirectories()
 
 	// Remove the klientctl binary itself.
 	// (The current binary is removing itself.. So emo...)
-	if u.KlientctlPath != "" {
-		if err := os.Remove(u.KlientctlPath); err != nil {
-			warnings = append(warnings, fmt.Sprintf(
-				"Warning: Failed to remove %s binary. This is not a critical issue.\n",
-				u.KlientctlName,
-			))
-		}
+	if err := u.RemoveKlientctl(); err != nil {
+		warnings = append(warnings, fmt.Sprintf(
+			"Warning: Failed to remove %s binary. This is not a critical issue.\n",
+			u.KlientctlName,
+		))
 	}
 
 	if len(warnings) != 0 {
@@ -189,7 +153,129 @@ func UninstallCommand(c *cli.Context) (string, int) {
 		KlientDirectory:       "kite/klient",
 		KlientFilename:        "klient",
 		KlientshFilename:      "klient.sh",
+		remover:               os.Remove,
 	}
 
 	return uninstaller.Uninstall()
+}
+
+// RemoveKiteKey removes the kite key and kitekeydirectory
+func (u *Uninstall) RemoveKiteKey() error {
+	// Remove the kitekey
+	if u.KiteKeyDirectory == "" {
+		return errors.New("KiteKeyDirectory cannot be empty")
+	}
+
+	if u.KiteKeyFilename == "" {
+		return errors.New("KiteKeyFilename cannot be empty")
+	}
+
+	p := filepath.Join(u.KiteKeyDirectory, u.KiteKeyFilename)
+	if err := os.Remove(p); err != nil {
+		return err
+	}
+
+	// Remove kiteHome, if empty. Do not return failed folder removals for a good ux.
+	//
+	// As with most file operations, checking the state and then acting is an
+	// error prone and non-atomic endevour. As such, we're just removing the
+	// directory here, and if it is non-empty this operation will fail.
+	os.Remove(u.KiteKeyDirectory)
+
+	return nil
+}
+
+// RemoveKlientFiles removes the klient bin, klient.sh, but not the klient
+// directories. For directories, see Uninstall.RemoveKlientDirectories
+func (u *Uninstall) RemoveKlientFiles() error {
+	if u.KlientParentDirectory == "" {
+		return errors.New("KlientParentDirectory cannot be empty")
+	}
+
+	if u.KlientDirectory == "" {
+		return errors.New("KlientDirectory cannot be empty")
+	}
+
+	if u.KlientFilename == "" {
+		return errors.New("KlientFilename cannot be empty")
+	}
+
+	if u.KlientshFilename == "" {
+		return errors.New("KlientshFilename cannot be empty")
+	}
+
+	// Remove klient bin
+	klientPath := filepath.Join(u.KlientDirectory, u.KlientDirectory)
+	if err := os.Remove(klientPath); err != nil {
+		return err
+	}
+
+	// Remove klient.sh
+	klientshPath := filepath.Join(u.KlientDirectory, u.KlientshFilename)
+	if err := os.Remove(klientshPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RemoveKlientDirectories removes the klient directories recursively, up until and
+// not including the Uninstall.KlientParentDirectory. As an example:
+//
+// KlientBin		         == klient
+// KlientDirectory       == kite/klient
+// KlientParentDirectory == /opt
+// FullKlientBinPath     == /opt/kite/klient/klient
+//
+// In the above, /opt/kite/klient and /opt/kite would be removed, but not
+// /opt. If /opt/kite/klient is not empty, nothing is removed.
+func (u *Uninstall) RemoveKlientDirectories() error {
+	if u.KlientDirectory == "" {
+		return errors.New("KlientDirectory cannot be empty")
+	}
+
+	if u.KlientParentDirectory == "" {
+		return errors.New("KlientParentDirectory cannot be empty")
+	}
+
+	if filepath.IsAbs(u.KlientDirectory) {
+		return errors.New("Cannot use absolute path directory")
+	}
+
+	// Remove the klient directories repeatedly up until the parent.
+	//
+	// Note that the "." check is because Dir will return the dot (current directory)
+	// when there are no other dirs to return. Example:
+	//
+	// 		filepath.Dir("foo/bar/baz") // foo/bar
+	// 		filepath.Dir("foo/bar") 		// foo
+	// 		filepath.Dir("foo") 				// .
+	for p := u.KlientDirectory; p != "."; p = filepath.Dir(p) {
+		rmP := filepath.Join(u.KlientParentDirectory, p)
+
+		// Just to be extra safe, since we're loop removing, lets confirm that it's
+		// not the parent dir. In multiple forms.
+		if rmP == u.KlientParentDirectory || filepath.Clean(rmP) == filepath.Clean(u.KlientParentDirectory) {
+			return errors.New("Directory to be removed equalled the KlientParentDirectory")
+		}
+
+		// If there is any error, return it. Generally we don't care about removal
+		// errors, but if anything fails we can't do anything with future parent dirs,
+		// so there's nothing to be done.
+		if err := os.Remove(rmP); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// RemoveKlientctl removes the klient binary
+// (The current binary is removing itself.. So emo...)
+func (u *Uninstall) RemoveKlientctl() error {
+	if u.KlientctlPath == "" {
+		return errors.New("KlientctlPath cannot be empty")
+	}
+
+	return os.Remove(u.KlientctlPath)
 }
