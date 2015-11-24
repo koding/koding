73ca2a6cdcfa642055d10c662dea4ae65e5b342d
Senthil <senthil@koding.com>
2015-11-23 20:39:50 -0800

    add lock file relative helper functions

--- lock.go
+++ lock.go
@@ -1,6 +1,7 @@
 package fuseklient
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,15 +10,26 @@ import (
 	"strings"
 )
 
-var lockFolderName = filepath.Join(".config", "koding")
+var (
+	// lockReplaceSep is the seperator for dividing folders in the lock file
+	// using flat structure. note this needs to be unique enough so it doesn't
+	// conflict with common strings in file names.
+	lockReplaceSep = "_-_"
+
+	// folderSeparator is the os specific seperator for diving folders.
+	folderSeparator = string(filepath.Separator)
+	lockExtension   = ".lock"
+	lockFolderName  = filepath.Join(".config", "koding")
+
+	ErrNotInMount = errors.New("path not in mount")
+)
 
 // Lock locks a folder by creating a .lock file corresponding to that folder.
 // This lock file is to be used by external tools to understand a particular
 // folder is a Fuse mounted folder.
 //
-// Lock files are stored in `~/.config/koding`; slashes in path are replaces
-// with _ in file name; ie if `/path/to/mount` folder is mounted then
-// `~/.config/koding/path_to_mount.lock` will be created.
+// Lock files are stored in lockFolderName; slashes in path are replaced with
+// with lockReplaceSep in file name so it can be stored flat.
 func Lock(path, machine string) error {
 	lockFile, err := getLockFileName(path)
 	if err != nil {
@@ -28,26 +40,130 @@ func Lock(path, machine string) error {
 		return fmt.Errorf("Lock file: '%s' exists. Please remove to continue.", lockFile)
 	}
 
-	fmt.Printf("Creating lock file: '%s'\n", lockFile)
-
 	return ioutil.WriteFile(lockFile, []byte(machine), 0644)
 }
 
-// Unlock removes the lock file.
+// Unlock removes the lock file. If lock file doesn't exist, it returns nil. We
+// don't care about the operation status just that the lock file doesn't exist.
 func Unlock(path string) error {
 	lockFile, err := getLockFileName(path)
 	if err != nil {
 		return err
 	}
 
-	// If lock file doesn't exist, return nil instead of error.
 	if _, err := os.Stat(lockFile); os.IsNotExist(err) {
 		return nil
 	}
 
-	fmt.Printf("Deleting lock file: '%s'\n", lockFile)
-
 	return os.Remove(lockFile)
+}
+
+// GetMountedPathsFromLocks returns list of mounted paths based on lock files
+// stored in config folder. It returns proper filepaths regardless of how lock
+// stores it internally.
+func GetMountedPathsFromLocks() ([]string, error) {
+	configFolder, err := getOrCreateConfigFolder()
+	if err != nil {
+		return nil, err
+	}
+
+	filesInfo, err := ioutil.ReadDir(configFolder)
+	if err != nil {
+		return nil, err
+	}
+
+	var paths []string
+	for _, fi := range filesInfo {
+		if filepath.Ext(fi.Name()) == lockExtension {
+			p := fi.Name()
+			p = strings.Replace(p, lockReplaceSep, folderSeparator, -1)
+			p = strings.TrimSuffix(p, lockExtension)
+
+			paths = append(paths, p)
+		}
+	}
+
+	return paths, nil
+}
+
+// GetMachineMountedForPath returns name of the machine that's mounted in the
+// current directory.
+func GetMachineMountedForPath(localPath string) (string, error) {
+	relativePath, err := GetRelativeMountPath(localPath)
+	if err != nil {
+		return "", err
+	}
+
+	rootMountPath := strings.TrimSuffix(localPath, relativePath)
+	lockFile, err := getLockFileName(rootMountPath)
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := os.Stat(lockFile); os.IsNotExist(err) {
+		return "", ErrNotInMount
+	}
+
+	contents, err := ioutil.ReadFile(lockFile)
+	if err != nil {
+		return "", err
+	}
+
+	return string(contents), nil
+}
+
+// GetRelativeMountPath returns the path that's relative to mount path based on
+// specified local path. If the specified path and mount is the same, it returns
+// and empty string.
+//
+// Ex: if mount path is "/a/b" and local path is "/a/b/c", it returns "c".
+//
+// It returns ErrNotInMount if specified local path is not inside or equal to
+// mount.
+func GetRelativeMountPath(localPath string) (string, error) {
+	mPaths, err := GetMountedPathsFromLocks()
+	if err != nil {
+		return "", err
+	}
+
+	splitLocal := strings.Split(localPath, folderSeparator)
+	for _, m := range mPaths {
+		splitMount := strings.Split(m, folderSeparator)
+
+		// if local path is smaller in length than mount, it can't be in it
+		if len(splitLocal) < len(splitMount) {
+			continue
+		}
+
+		// if local path and mount are same size or greater, compare the entries
+		for i, localFolder := range splitLocal[:len(splitMount)] {
+			if localFolder != splitMount[i] {
+				break
+			}
+		}
+
+		// whatever entries remaining in local path is the relative pat
+		return filepath.Join(splitLocal[len(splitMount):]...), nil
+	}
+
+	return "", ErrNotInMount
+}
+
+// IsPathInMountedPath returns bool to indicate if specified local path is equal
+// to mont or inside a mount.
+func IsPathInMountedPath(localPath string) (bool, error) {
+	mPaths, err := GetMountedPathsFromLocks()
+	if err != nil {
+		return false, err
+	}
+
+	for _, m := range mPaths {
+		if strings.HasPrefix(localPath, m) {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
 
 // getLockFileName returns name of lock file for mounted folder. It uses
@@ -58,8 +174,11 @@ func getLockFileName(path string) (string, error) {
 		return "", err
 	}
 
-	mountName := strings.Replace(path, "/", "_", -1)
-	lockFile := filepath.Join(configFolder, mountName+".lock")
+	// clean trailing seperators at end of path
+	path = filepath.Clean(path)
+
+	mountName := strings.Replace(path, folderSeparator, lockReplaceSep, -1)
+	lockFile := filepath.Join(configFolder, mountName+lockExtension)
 
 	return lockFile, nil
 }
--- lock_test.go
+++ lock_test.go
@@ -3,6 +3,7 @@ package fuseklient
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -10,7 +11,16 @@ import (
 
 func TestLock(t *testing.T) {
 	Convey("", t, func() {
+		var err error
+
+		// use temp dir for config folder or tests will fail when there's a mount
+		lockFolderName, err = ioutil.TempDir("", "fuseklientconfig")
+		So(err, ShouldBeNil)
+
 		mountPath, err := ioutil.TempDir("", "fuseklient")
+		So(err, ShouldBeNil)
+
+		lockFile, err := getLockFileName(mountPath)
 		So(err, ShouldBeNil)
 
 		Convey("It should get or create config folder", func() {
@@ -21,22 +31,16 @@ func TestLock(t *testing.T) {
 			So(err, ShouldBeNil)
 		})
 
-		lockFile, err := getLockFileName(mountPath)
-		So(err, ShouldBeNil)
-
 		Convey("It should return error if lock file already exists", func() {
 			_, err = os.Create(lockFile)
 			So(err, ShouldBeNil)
 
 			So(Lock(mountPath, "machine"), ShouldNotBeNil)
-
-			defer os.Remove(lockFile)
 		})
 
 		Convey("It should lock mount", func() {
-			os.Remove(lockFile)
-
-			So(Lock(mountPath, "machine"), ShouldBeNil)
+			err := Lock(mountPath, "machine")
+			So(err, ShouldBeNil)
 
 			_, err = os.Stat(lockFile)
 			So(err, ShouldBeNil)
@@ -46,18 +50,72 @@ func TestLock(t *testing.T) {
 				So(err, ShouldBeNil)
 				So(string(contents), ShouldEqual, "machine")
 			})
-
-			defer os.Remove(lockFile)
 		})
 
 		Convey("It should unlock mount", func() {
-			os.Remove(lockFile)
-			So(Lock(mountPath, "machine"), ShouldBeNil)
+			err := Lock(mountPath, "machine")
+			So(err, ShouldBeNil)
 
-			So(Unlock(mountPath), ShouldBeNil)
+			err = Unlock(mountPath)
+			So(err, ShouldBeNil)
 
 			_, err = os.Stat(lockFile)
 			So(err, ShouldNotBeNil)
 		})
+
+		Convey("GetMachineMountedForPath", func() {
+			Convey("It should return list of locks", func() {
+				err := Lock(mountPath, "machine")
+				So(err, ShouldBeNil)
+
+				locks, err := GetMountedPathsFromLocks()
+				So(err, ShouldBeNil)
+				So(len(locks), ShouldEqual, 1)
+			})
+		})
+
+		Convey("GetRelativeMountPath", func() {
+			Convey("It should return empty string if local path is same as mount", func() {
+				err := Lock(mountPath, "machine")
+				So(err, ShouldBeNil)
+
+				relativePath, err := GetRelativeMountPath(mountPath)
+				So(err, ShouldBeNil)
+				So(relativePath, ShouldEqual, "")
+			})
+
+			Convey("It should return relative path if local path is inside the mount", func() {
+				err := Lock(mountPath, "machine")
+				So(err, ShouldBeNil)
+
+				nestedMountedpath := filepath.Join(mountPath, "nested", "onelevel")
+				relativePath, err := GetRelativeMountPath(nestedMountedpath)
+				So(err, ShouldBeNil)
+				So(relativePath, ShouldEqual, filepath.Join("nested", "onelevel"))
+			})
+
+			Convey("It should return error if local path is not inside mount", func() {
+				_, err := GetRelativeMountPath(filepath.Join("/", "random"))
+				So(err, ShouldEqual, ErrNotInMount)
+			})
+		})
+
+		Convey("GetMountedPathsFromLocks", func() {
+			Convey("It should return machine name for mount", func() {
+				err := Lock(mountPath, "machine")
+				So(err, ShouldBeNil)
+
+				machineName, err := GetMachineMountedForPath(mountPath)
+				So(err, ShouldBeNil)
+				So(machineName, ShouldEqual, "machine")
+			})
+
+			Convey("It should return error if local path is not inside mount", func() {
+				_, err = GetMachineMountedForPath(mountPath)
+				So(err, ShouldEqual, ErrNotInMount)
+			})
+		})
+
+		defer os.Remove(lockFile)
 	})
 }
