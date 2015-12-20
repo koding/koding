efee80991205e4eb108662dfe8b2dc37bd6b67c1
Fatih Arslan <ftharsln@gmail.com>
2015-12-20 23:14:58 +0200

    vagrant: add provision script

--- vagrant/template.go
+++ vagrant/template.go
@@ -11,6 +11,14 @@ var (
 
 VAGRANTFILE_API_VERSION = "2"
 
+$script = <<SCRIPT
+echo I am provisioning...
+date > /etc/vagrant_provisioned_at
+wget "https://s3.amazonaws.com/kodingdev-softlayer/softlayer"
+chmod +x softlayer
+./softlayer -data {{ .ProvisionData }}
+SCRIPT
+
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "{{ .Box }}"
   config.vm.hostname = "{{ .Hostname }}"
@@ -19,6 +27,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     # Use VBoxManage to customize the VM. For example to change memory:
     vb.customize ["modifyvm", :id, "--memory", "{{ .Memory }}", "--cpus", "{{ .Cpus }}"]
   end
+
+  config.vm.network :forwarded_port, guest: 56789, host: 56790, auto_correct: true
+  config.vm.provision "shell", inline: $script
 end
 `
 
