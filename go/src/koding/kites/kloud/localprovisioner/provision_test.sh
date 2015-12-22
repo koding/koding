15c9ae789cc3843afcf9d0a81dc94661960dde27
Fatih Arslan <ftharsln@gmail.com>
2015-12-22 18:40:23 +0200

    local: add also test file

---
+++ go/src/koding/kites/kloud/localprovisioner/provision_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Change to your own Koding Repo's Go golder
+KODING_REPO_GO="/Users/fatih/Code/koding/go"
+
+(cd ${KODING_REPO_GO} && ./build.sh)
+
+cd $GOBIN
+
+(cd /Users/fatih/Code/koding/go/src/koding/kites/kloud/localprovisioner && go build)
+
+# Fill the remaining parts
+KONTROL_POSTGRES_PASSWORD=kontrolapp201506 \
+KONTROL_STORAGE=postgres \
+KONTROL_POSTGRES_USERNAME=kontrolapp201506 \
+KONTROL_POSTGRES_DBNAME=social \
+KONTROL_POSTGRES_HOST=192.168.59.103 \
+KLOUD_MONGODB_URL=192.168.59.103:27017/koding \
+KLOUD_ACCESSKEY="" \
+KLOUD_SECRETKEY="" \
+KLOUD_TESTACCOUNT_ACCESSKEY="" \
+KLOUD_TESTACCOUNT_SECRETKEY="" \
+KLOUD_TESTACCOUNT_SLUSERNAME="" \
+KLOUD_TESTACCOUNT_SLAPIKEY="" \
+KLOUD_USER_PUBLICKEY="" \
+KLOUD_USER_PRIVATEKEY="" \
+TERRAFORMER_KEY="" \
+TERRAFORMER_SECRET="" \
+${KODING_REPO_GO}/src/koding/kites/kloud/localprovisioner/localprovisioner
+
+cd ${KODING_REPO_GO}/src/koding/kites/kloud/localprovisioner
+
