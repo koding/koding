9a71822a2b86928e3511e006720e9d85a3e42f24
Lee Olayvar <leeolayvar@gmail.com>
2016-01-04 07:43:40 -0800

    list: Added fish-like paths to mounted path column

--- list.go
+++ list.go
@@ -10,6 +10,23 @@ import (
 	"github.com/koding/kite"
 )
 
+type kiteInfo struct {
+	IP           string
+	VMName       string
+	Hostname     string
+	MachineLabel string
+	Mounts       []mountInfo
+	Teams        []string
+
+	// TODO: DEPRECATE
+	MountedPaths []string
+}
+
+type mountInfo struct {
+	RemotePath string `json:"remotePath"`
+	LocalPath  string `json:"localPath"`
+}
+
 // ListCommand returns list of remote machines belonging to user or that can be
 // accessed by the user.
 func ListCommand(c *cli.Context) int {
@@ -46,21 +63,26 @@ func ListCommand(c *cli.Context) int {
 			team = "koding.com"
 		}
 
+		// TODO: The UX for displaying multiple mounts is not decided, and
+		// we only support a single mount for now anyway. So, listing will just default
+		// to a single mount.
+		var formattedMount string
+		if len(info.Mounts) > 0 {
+			// TODO: "fishify" the mount paths.
+			formattedMount = fmt.Sprintf(
+				"%s -> %s",
+				FishifyPath(info.Mounts[0].LocalPath),
+				FishifyPath(info.Mounts[0].RemotePath),
+			)
+		}
+
 		fmt.Fprintf(w, "  %d.\t%s\t%s\t%s\t%s\t%s\n",
-			i+1, team, info.MachineLabel, info.IP, info.VMName, strings.Join(info.MountedPaths, ", "))
+			i+1, team, info.MachineLabel, info.IP, info.VMName, formattedMount,
+		)
 	}
 	w.Flush()
 
 	return 0
-}
-
-type kiteInfo struct {
-	IP           string
-	VMName       string
-	Hostname     string
-	MachineLabel string
-	MountedPaths []string
-	Teams        []string
 }
 
 func getListOfMachines(kite *kite.Client) ([]kiteInfo, error) {
@@ -79,4 +101,41 @@ func getListOfMachines(kite *kite.Client) ([]kiteInfo, error) {
 	}
 
 	return infos, nil
+}
+
+// FishifyPath takes a path and returnes a "Fish" like path.
+// Example:
+//
+//     /foo/bar/baz/bat
+//
+// Becomes:
+//
+//     /foo/b/b/bat
+//
+// Note that this is different from Fish, in that it shows the root directory. This
+// is done so that a mounted directory that has the same name as the remote directory
+// is easier to distinguish.
+func FishifyPath(p string) string {
+	sep := string(os.PathSeparator)
+	l := strings.Split(p, sep)
+
+	first := true
+	// premature optimize the end, since we'll need it on every iteration
+	last := len(l) - 1
+
+	for i, s := range l {
+		if s == "" || i == last {
+			continue
+		}
+
+		// If this is the first path segment, don't shorten it
+		if first {
+			first = false
+			continue
+		}
+
+		l[i] = s[:1]
+	}
+
+	return strings.Join(l, sep)
 }
