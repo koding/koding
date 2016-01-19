e7879afd077610bad25267dced682de504cac392
Lee Olayvar <leeolayvar@gmail.com>
2016-01-19 10:34:30 -0800

    test: Added BlockIO struct for Stdin Mocking

--- mount_test.go
+++ mount_test.go
@@ -6,10 +6,67 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/koding/klientctl/klientctlerrors"
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+// BlockingIO is a struct primarily for mocking the behavior of Stdin. Stdin blocks
+// the Reader while waiting for input, so mocking Stdin with a typical Buffer can
+// often be problematic, because the buffer will return empty of there is no input.
+//
+// BlockingIO works around this by using a channel to block the read until a write
+// exists.
+type BlockingIO struct {
+	C chan []byte
+}
+
+func NewBlockingIO() *BlockingIO {
+	return &BlockingIO{
+		C: make(chan []byte, 5),
+	}
+}
+
+func (io *BlockingIO) Close() error {
+	close(io.C)
+	// No errors to return atm, but keeping the API consistent with other Close()
+	return nil
+}
+
+func (io *BlockingIO) Read(p []byte) (int, error) {
+	cp := <-io.C
+	l := len(cp)
+	// if p wasn't big enough to fit all the data from cp, add the rest back onto the
+	// channel. The next Read will consume it. Note that this is racey, but it's good
+	// enough for unit tests. If we encounter the obvious race condition(s), we can
+	// add a simple lock for this.
+	if len(cp) > len(p) {
+		l := len(p)
+		io.C <- cp[l:]
+	}
+	for i := 0; i < l; i++ {
+		p[i] = cp[i]
+	}
+	return l, nil
+}
+
+func (io *BlockingIO) Write(p []byte) (int, error) {
+	io.C <- p[:]
+	return len(p), nil
+}
+
+func (io *BlockingIO) SleepyWriteString(t time.Duration, ss ...string) {
+	for _, s := range ss {
+		time.Sleep(t)
+		fmt.Fprint(io, s)
+	}
+}
+
+func (io *BlockingIO) SleepyWriteStringAndClose(t time.Duration, ss ...string) {
+	io.SleepyWriteString(t, ss...)
+	io.Close()
+}
 
 // Ignoring perm errors/etc, just using this as a simple shorthand for
 // tests
@@ -30,20 +87,28 @@ func TestAskToCreate(t *testing.T) {
 			os.RemoveAll(askDir)
 			os.MkdirAll(askDir, 0755)
 
-			var in, out bytes.Buffer
+			var out bytes.Buffer
+			in := NewBlockingIO()
 			// We're giving it an invalid input, so it would normally error out.
 			// But, because the directory exists, it should *not* error out.
-			fmt.Fprintf(&in, "foo\nbar\nbaz\nbam\n")
-			err := askToCreate(askDir, &in, &out)
+			go in.SleepyWriteStringAndClose(
+				10*time.Millisecond,
+				"foo\n", "bar\n", "baz\n", "bam\n",
+			)
+			err := askToCreate(askDir, in, &out)
 			So(err, ShouldBeNil)
 		})
 
 		Convey("Should create the folder if the user chooses yes", func() {
 			os.RemoveAll(askDir)
 
-			var in, out bytes.Buffer
-			fmt.Fprintf(&in, "yes\n")
-			err := askToCreate(askDir, &in, &out)
+			var out bytes.Buffer
+			in := NewBlockingIO()
+			go in.SleepyWriteStringAndClose(
+				10*time.Millisecond,
+				"yes\n",
+			)
+			err := askToCreate(askDir, in, &out)
 			So(err, ShouldBeNil)
 			So(exists(askDir), ShouldBeTrue)
 		})
@@ -51,9 +116,13 @@ func TestAskToCreate(t *testing.T) {
 		Convey("Should not create the folder and error, if the user chooses no", func() {
 			os.RemoveAll(askDir)
 
-			var in, out bytes.Buffer
-			fmt.Fprintf(&in, "no\n")
-			err := askToCreate(askDir, &in, &out)
+			var out bytes.Buffer
+			in := NewBlockingIO()
+			go in.SleepyWriteStringAndClose(
+				10*time.Millisecond,
+				"no\n",
+			)
+			err := askToCreate(askDir, in, &out)
 			So(err, ShouldEqual, klientctlerrors.ErrUserCancelled)
 			So(exists(askDir), ShouldBeFalse)
 		})
@@ -61,9 +130,13 @@ func TestAskToCreate(t *testing.T) {
 		Convey("Should retry asking the user if unexpected input, 3 times", func() {
 			os.RemoveAll(askDir)
 
-			var in, out bytes.Buffer
-			fmt.Fprint(&in, "foo\nbar\nyes\n")
-			err := askToCreate(askDir, &in, &out)
+			var out bytes.Buffer
+			in := NewBlockingIO()
+			go in.SleepyWriteStringAndClose(
+				10*time.Millisecond,
+				"foo\n", "bar\n", "yes\n",
+			)
+			err := askToCreate(askDir, in, &out)
 			So(err, ShouldBeNil)
 			So(exists(askDir), ShouldBeTrue)
 		})
@@ -71,9 +144,13 @@ func TestAskToCreate(t *testing.T) {
 		Convey("Should fail after retrying 4 times", func() {
 			os.RemoveAll(askDir)
 
-			var in, out bytes.Buffer
-			fmt.Fprintf(&in, "foo\nbar\nbaz\nyes\n")
-			err := askToCreate(askDir, &in, &out)
+			var out bytes.Buffer
+			in := NewBlockingIO()
+			go in.SleepyWriteStringAndClose(
+				10*time.Millisecond,
+				"foo\n", "bar\n", "baz\n", "yes\n",
+			)
+			err := askToCreate(askDir, in, &out)
 			So(err, ShouldNotBeNil)
 			So(exists(askDir), ShouldBeFalse)
 		})
