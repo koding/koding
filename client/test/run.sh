d0a95fb2ad54001602dddba6115e3cef9d458420
Sonmez Kartal <sonmez@koding.com>
2016-01-26 01:59:38 +0200

    Introduce IGNORED_TEST_CASES environment variable

--- client/test/run.sh
+++ client/test/run.sh
@@ -21,32 +21,48 @@ function get_test_suite_path() {
   echo $(get_test_group_path)/$TEST_SUITE.js
 }
 
-function run_test_suite() {
+function list_test_cases() {
   TEST_FILE=$(get_test_suite_path $TEST_GROUP $TEST_SUITE)
-  $NIGHTWATCH_CMD --test $TEST_FILE $*
+  node -p "Object.keys(require('./`get_test_suite_path`')).join('\n')"
+}
+
+function run_test_case() {
+  TEST_FILE=$(get_test_suite_path $TEST_GROUP $TEST_SUITE)
+  $NIGHTWATCH_CMD --test $TEST_FILE --testcase $TEST_CASE
+}
+
+function run_test_suite() {
+  for TEST_CASE in $(list_test_cases); do
+    $BASH_SOURCE $TEST_GROUP $TEST_SUITE $TEST_CASE
+  done
+}
+
+function run_test_suite_file() {
+  FILE=$1
+  TEST_SUITE=$(basename -s '.js' $FILE)
+  $BASH_SOURCE $TEST_GROUP $TEST_SUITE
 }
 
 function run_test_group() {
-  $NIGHTWATCH_CMD --group $(get_test_group_path)
+  for FILE in $(find $(get_test_group_path) -name '*.js'); do
+    run_test_suite_file $FILE
+  done
 }
 
 function run_all_test_groups() {
-  find $BUILD_DIR/ -name '*.js' \
-       ! -path "$BUILD_DIR/helpers/*" \
-       ! -path "$BUILD_DIR/utils/*" \
-       -exec $NIGHTWATCH_CMD --group {} \;
+  for FILE in $(find $BUILD_DIR -type d -mindepth 1 -maxdepth 1 \
+       ! -path "$BUILD_DIR/helpers" \
+       ! -path "$BUILD_DIR/utils"); do
+    $BASH_SOURCE ${FILE#$BUILD_DIR/}
+  done
 }
 
-function finish() {
-  CODE=$1
-
+function cleanup() {
   if [ "$(hostname)" == 'wercker-test-instance' ]; then
     if [ $CODE -ne 0 ]; then
-      mv users.json users-$1-$2.json
+      mv users.json users-$TEST_GROUP-$$TEST_SUITE-$TEST_CASE.json
     fi
   fi
-
-  exit $CODE
 }
 
 TEST_GROUP=$1
@@ -54,7 +70,15 @@ TEST_SUITE=$2
 TEST_CASE=$3
 
 if [ -n "$TEST_CASE" ]; then
-  run_test_suite --testcase $TEST_CASE
+  if [ -n "$IGNORED_TEST_CASES" ]; then
+    if echo $IGNORED_TEST_CASES | grep $TEST_GROUP/$TEST_SUITE/$TEST_CASE; then
+      exit 0
+    fi
+  fi
+  run_test_case
+  EXIT_CODE=$!
+  cleanup
+  exit $EXIT_CODE
 elif [ -n "$TEST_SUITE" ]; then
   run_test_suite
 elif [ -n "$TEST_GROUP" ]; then
@@ -62,5 +86,3 @@ elif [ -n "$TEST_GROUP" ]; then
 else
   run_all_test_groups
 fi
-
-finish $!
