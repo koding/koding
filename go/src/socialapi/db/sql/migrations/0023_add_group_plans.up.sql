87a0c5a1aed0502d414d05977d29bd4282bd77ad
Senthil <senthil@koding.com>
2016-02-10 01:18:48 +0000

    payment: add all migration to a single file

--- go/src/socialapi/db/sql/migrations/0023_add_group_plans.up.sql
+++ go/src/socialapi/db/sql/migrations/0023_add_group_plans.up.sql
@@ -1,9 +1,26 @@
+--CREATE TYPE "payment"."customer_type_enum" AS ENUM (
+--  'group',
+--  'account'
+--);
+--ALTER TYPE "payment"."customer_type_enum" OWNER TO "social";
+--ALTER TABLE payment.customer ADD COLUMN "type" "payment"."customer_type_enum";
+
+--CREATE TYPE "payment"."plan_type_enum" AS ENUM (
+--  'group',
+--  'account'
+--);
+--ALTER TYPE "payment"."plan_type_enum" OWNER TO "social";
+--ALTER TABLE payment.plan ADD COLUMN "type" "payment"."plan_type_enum";
+
 --ALTER TYPE "payment"."plan_title_enum" ADD VALUE IF NOT EXISTS 'bootstrap' AFTER 'super';
 --ALTER TYPE "payment"."plan_title_enum" ADD VALUE IF NOT EXISTS 'startup' AFTER 'bootstrap';
 --ALTER TYPE "payment"."plan_title_enum" ADD VALUE IF NOT EXISTS 'enterprise' AFTER 'startup';
 
---INSERT INTO "payment"."plan" (interval, title, provider, provider_plan_id, amount_in_cents)
---  VALUES ('month', 'bootstrap', 'stripe', 'bootstrap_month', 300)
---  , ('month', 'startup', 'stripe', 'startup_month', 3000)
---  , ('month', 'enterprise', 'stripe', 'enterprise_month', 10000)
---  ;
+--INSERT INTO "payment"."plan" (interval, title, provider, provider_plan_id, amount_in_cents, type)
+--  VALUES ('month', 'bootstrap', 'stripe', 'bootstrap_month', 300, 'group')
+--  , ('month', 'startup', 'stripe', 'startup_month', 3000, 'group')
+--  , ('month', 'enterprise', 'stripe', 'enterprise_month', 10000, 'group')
+--;
+
+--UPDATE plan SET type='account' WHERE type IS NULL;
+--UPDATE customer SET type='account' WHERE type IS NULL;
--- go/src/socialapi/db/sql/payment_definition/modifications/006-add-plan-title-enum.sql
+++
@@ -1,9 +0,0 @@
-ALTER TYPE "payment"."plan_title_enum" ADD VALUE IF NOT EXISTS 'bootstrap' AFTER 'super';
-ALTER TYPE "payment"."plan_title_enum" ADD VALUE IF NOT EXISTS 'startup' AFTER 'bootstrap';
-ALTER TYPE "payment"."plan_title_enum" ADD VALUE IF NOT EXISTS 'enterprise' AFTER 'startup';
-
-INSERT INTO "payment"."plan" (interval, title, provider, provider_plan_id, amount_in_cents, 'type')
-  VALUES ('month', 'bootstrap', 'stripe', 'bootstrap_month', 300, 'group')
-  , ('month', 'startup', 'stripe', 'startup_month', 3000, 'group')
-  , ('month', 'enterprise', 'stripe', 'enterprise_month', 10000, 'group')
-  ;
