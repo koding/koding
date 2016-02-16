4c6c2d473a4013f0bc2b8336ff711f151e526d73
Koding <Tihomir Piha>
2016-02-16 20:57:40 +0000

    GEN-1963 cloudfoundry/gosigar

--- go/src/github.com/cloudfoundry/gosigar/sigar_interface.go
+++ go/src/github.com/cloudfoundry/gosigar/sigar_interface.go
@@ -1,6 +1,45 @@
-// Copyright (c) 2012 VMware, Inc.
-
 package sigar
+
+import (
+	"time"
+)
+
+type Sigar interface {
+	CollectCpuStats(collectionInterval time.Duration) (<-chan Cpu, chan<- struct{})
+	GetLoadAverage() (LoadAverage, error)
+	GetMem() (Mem, error)
+	GetSwap() (Swap, error)
+	GetFileSystemUsage(string) (FileSystemUsage, error)
+}
+
+type Cpu struct {
+	User    uint64
+	Nice    uint64
+	Sys     uint64
+	Idle    uint64
+	Wait    uint64
+	Irq     uint64
+	SoftIrq uint64
+	Stolen  uint64
+}
+
+func (cpu *Cpu) Total() uint64 {
+	return cpu.User + cpu.Nice + cpu.Sys + cpu.Idle +
+		cpu.Wait + cpu.Irq + cpu.SoftIrq + cpu.Stolen
+}
+
+func (cpu Cpu) Delta(other Cpu) Cpu {
+	return Cpu{
+		User:    cpu.User - other.User,
+		Nice:    cpu.Nice - other.Nice,
+		Sys:     cpu.Sys - other.Sys,
+		Idle:    cpu.Idle - other.Idle,
+		Wait:    cpu.Wait - other.Wait,
+		Irq:     cpu.Irq - other.Irq,
+		SoftIrq: cpu.SoftIrq - other.SoftIrq,
+		Stolen:  cpu.Stolen - other.Stolen,
+	}
+}
 
 type LoadAverage struct {
 	One, Five, Fifteen float64
@@ -22,22 +61,6 @@ type Swap struct {
 	Total uint64
 	Used  uint64
 	Free  uint64
-}
-
-type Cpu struct {
-	User    uint64
-	Nice    uint64
-	Sys     uint64
-	Idle    uint64
-	Wait    uint64
-	Irq     uint64
-	SoftIrq uint64
-	Stolen  uint64
-}
-
-func (self *Cpu) Total() uint64 {
-	return self.User + self.Nice + self.Sys + self.Idle +
-		self.Wait + self.Irq + self.SoftIrq + self.Stolen
 }
 
 type CpuList struct {
