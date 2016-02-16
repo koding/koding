bab0953eab9a60143b824a5034591081e4c4ffdb
Koding <Tihomir Piha>
2016-02-16 20:55:55 +0000

    GEN-1963 chuckpreslar/inflect

--- go/src/github.com/chuckpreslar/inflect/types/irregular.go
+++ go/src/github.com/chuckpreslar/inflect/types/irregular.go
@@ -1,9 +1,12 @@
+// Package types contains common types useful to the inflect package.
 package types
+
+import "strings"
 
 // IrregularType provides a structure for irregular words that do not follow standard rules.
 type IrregularType struct {
-  Singular string // The singular form of the irregular word.
-  Plural   string // The plural form of the irregular word.
+	Singular string // The singular form of the irregular word.
+	Plural   string // The plural form of the irregular word.
 }
 
 // IrregularsType defines a slice of pointers to IrregularType.
@@ -11,20 +14,21 @@ type IrregularsType []*IrregularType
 
 // IsIrregular returns an IrregularType and bool if the IrregularsType slice contains the word.
 func (self IrregularsType) IsIrregular(str string) (*IrregularType, bool) {
-  for _, irregular := range self {
-    if irregular.Singular == str || irregular.Plural == str {
-      return irregular, true
-    }
-  }
+	str = strings.ToLower(str)
+	for _, irregular := range self {
+		if strings.ToLower(irregular.Singular) == str || strings.ToLower(irregular.Plural) == str {
+			return irregular, true
+		}
+	}
 
-  return nil, false
+	return nil, false
 }
 
 // Irregular if a factory method to a new IrregularType.
 func Irregular(singular, plural string) (irregular *IrregularType) {
-  irregular = new(IrregularType)
-  irregular.Singular = singular
-  irregular.Plural = plural
+	irregular = new(IrregularType)
+	irregular.Singular = singular
+	irregular.Plural = plural
 
-  return
+	return
 }
