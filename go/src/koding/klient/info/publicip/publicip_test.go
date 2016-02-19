3102b7ee1dc611fad1545968d3a69f551c3a4d97
Lee Olayvar <leeolayvar@gmail.com>
2016-02-19 21:51:53 +0000

    klient: Added PublicIPRetry, and PublicIP host rotation
    The 2nd host and host rotation helps to ensure that if one of
    the IP providers _(even us, eventually)_  is down, Klient can still
    start and potentially run normally.
    
    This does not "fix" the panic, due to the panic being intended.
    The panic behavior (Seen here:
    https://github.com/koding/koding/blob/master/go/src/koding/klient/app/klient.go#L386)
    Is not inherently bad. Klient cannot do much if it cannot get its IP,
    and if it is failing to even communicate, restarting (after a panic)
    is a reasonable response.
    
    This PR mainly focuses on the reliance to echoip, and a single provider.

---
+++ go/src/koding/klient/info/publicip/publicip_test.go
@@ -0,0 +1,80 @@
+package publicip
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPublicIPRetry(t *testing.T) {
+	var callCount int
+	var eventualSuccessReq int
+	successTS := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			callCount++
+			fmt.Fprint(w, "0.1.2.3")
+		}))
+	defer successTS.Close()
+
+	failTS := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			callCount++
+		}))
+	defer failTS.Close()
+
+	eventualSuccessTS := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			callCount++
+			if callCount > eventualSuccessReq {
+				fmt.Fprint(w, "0.1.2.3")
+			}
+		}))
+	defer eventualSuccessTS.Close()
+
+	Convey("Given a max of valid and invalid hosts", t, func() {
+		callCount = 0
+
+		hosts := []string{
+			failTS.URL,
+			successTS.URL,
+			failTS.URL,
+		}
+
+		Convey("It should return the first succeeding host", func() {
+			ip, err := publicIPRetry(hosts, 10, 0, nil)
+			So(err, ShouldBeNil)
+			So(ip, ShouldNotBeNil)
+			So(ip.String(), ShouldEqual, "0.1.2.3")
+			So(callCount, ShouldEqual, 2)
+		})
+
+		Convey("It should only return an error after exceeding maxRetries", func() {
+			ip, err := publicIPRetry(hosts, 1, 0, nil)
+			So(err, ShouldNotBeNil)
+			So(ip, ShouldBeNil)
+			So(callCount, ShouldEqual, 1)
+		})
+	})
+
+	Convey("Given eventually succeeding hosts", t, func() {
+		callCount = 0
+		eventualSuccessReq = 3
+
+		hosts := []string{
+			failTS.URL,
+			eventualSuccessTS.URL,
+			failTS.URL,
+		}
+
+		Convey("It should repeat the entire hosts list until success within maxRetries", func() {
+			ip, err := publicIPRetry(hosts, 10, 0, nil)
+			So(err, ShouldBeNil)
+			So(ip, ShouldNotBeNil)
+			So(ip.String(), ShouldEqual, "0.1.2.3")
+			So(callCount, ShouldEqual, 5)
+		})
+	})
+}
