1f293542772d0274b628e09ecf41989b7cb4ddcb
Senthil <senthil@koding.com>
2016-02-23 17:04:54 -0800

    sql: run type/enum creation in transactions

--- go/src/socialapi/db/sql/migrations/0022_add_type_to_customer.up.sql
+++ go/src/socialapi/db/sql/migrations/0022_add_type_to_customer.up.sql
@@ -1,18 +1,46 @@
--- CustomerType
-CREATE TYPE "payment"."customer_type_enum" AS ENUM (
-    'group',
-    'account'
-);
-ALTER TYPE "payment"."customer_type_enum" OWNER TO "social";
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'customer_type_enum') THEN
+      CREATE TYPE "payment"."customer_type_enum" AS ENUM (
+        'group',
+        'account'
+      );
 
-ALTER TABLE payment.customer ADD COLUMN "type" "payment"."customer_type_enum";
+      ALTER TYPE "payment"."customer_type_enum" OWNER TO "social";
+    END IF;
+  END;
+$$;
 
--- PlanType
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'plan_type_enum') THEN
+      CREATE TYPE "payment"."plan_type_enum" AS ENUM (
+        'group',
+        'account'
+      );
 
-CREATE TYPE "payment"."plan_type_enum" AS ENUM (
-    'group',
-    'account'
-);
-ALTER TYPE "payment"."plan_type_enum" OWNER TO "social";
+      ALTER TYPE "payment"."plan_type_enum" OWNER TO "social";
+    END IF;
+  END;
+$$;
 
-ALTER TABLE payment.plan ADD COLUMN "type" "payment"."plan_type_enum";
+DO $$
+  BEGIN
+    BEGIN
+      ALTER TABLE payment.customer ADD COLUMN "type" "payment"."customer_type_enum";
+      EXCEPTION
+      WHEN duplicate_column THEN RAISE NOTICE 'column already exists';
+    END;
+  END;
+$$;
+
+DO $$
+  BEGIN
+    BEGIN
+      ALTER TABLE payment.plan ADD COLUMN "type" "payment"."plan_type_enum";
+      EXCEPTION
+      WHEN duplicate_column THEN RAISE NOTICE 'column already exists';
+    END;
+  END;
+$$;
+
