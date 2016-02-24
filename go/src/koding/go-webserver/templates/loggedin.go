9342deaa813ff3cd447cb26545365c3a22e4f14d
Stefan Cosma <stefan@koding.com>
2016-02-24 19:13:33 +0000

    Added script to go webserver

--- go/src/koding/go-webserver/templates/loggedin.go
+++ go/src/koding/go-webserver/templates/loggedin.go
@@ -57,6 +57,15 @@ var LoggedInHome = `
         a.appendChild(r);
       })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
+    <script type="text/javascript">
+      var _hsq = window._hsq = window._hsq || [];
+      (function(d,s,i,r) {
+        if (d.getElementById(i)){return;}
+          var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+          n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/1593820.js';
+          e.parentNode.insertBefore(n, e);
+      })(document,"script","hs-analytics",300000);
+    </script>
 </body>
 </html>
 `
