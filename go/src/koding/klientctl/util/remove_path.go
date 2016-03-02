c2260578642f528582f05d0a969521309462e156
Senthil <senthil@koding.com>
2016-03-01 16:42:35 -0800

    klientctl: fix padding

--- go/src/koding/klientctl/util/remove_path.go
+++ go/src/koding/klientctl/util/remove_path.go
@@ -25,7 +25,7 @@ func NewRemovePath() *RemovePath {
 
 // RemovePath removes given path if:
 //	1. path is not an import path, see NeverRemovePaths
-//  2. if path is a folder, it should be empty
+//	2. if path is a folder, it should be empty
 func (r *RemovePath) Do(path string) error {
 	if path == "" {
 		return errors.New("Path is empty.")
