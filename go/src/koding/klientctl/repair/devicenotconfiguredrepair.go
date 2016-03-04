70079ae8d02eaddba4865ab776103dd715185b39
Lee Olayvar <leeolayvar@gmail.com>
2016-03-04 22:28:23 +0000

    klientctl: Added DeviceNotConfiguredRepair

--- go/src/koding/klientctl/repair/devicenotconfiguredrepair.go
+++ go/src/koding/klientctl/repair/devicenotconfiguredrepair.go
@@ -1,18 +1,22 @@
 package repair
 
 import (
-	"errors"
+	"fmt"
 	"koding/klient/remote/req"
 	"koding/klientctl/util"
 	"os"
+	"strings"
 
 	"github.com/koding/logging"
 )
 
-// PermDeniedRepair looks at the given mount directory and checks if
-// gives us permission denied. If so, it might be an indication that
-// the directory is no longer mounted. Remount.
-type PermDeniedRepair struct {
+// dncSuffix is the suffix of the error mesage returned by Golang when trying to
+// open a not-configured device directory.
+const dncSuffix = "device not configured"
+
+// DeviceNotConfiguredRepair looks at the given mount directory and checks if
+// is a device not configured. If so, remount.
+type DeviceNotConfiguredRepair struct {
 	Log logging.Logger
 
 	Stdout *util.Fprint
@@ -26,11 +30,11 @@ type PermDeniedRepair struct {
 	}
 }
 
-func (r *PermDeniedRepair) String() string {
-	return "PermDeniedRepair"
+func (r *DeviceNotConfiguredRepair) String() string {
+	return "DeviceNotConfiguredRepair"
 }
 
-func (r *PermDeniedRepair) getMountPath() (string, error) {
+func (r *DeviceNotConfiguredRepair) getMountPath() (string, error) {
 	// Get the mount dir.
 	info, err := r.Klient.RemoteMountInfo(r.MountName)
 	if err != nil {
@@ -42,7 +46,7 @@ func (r *PermDeniedRepair) getMountPath() (string, error) {
 
 // Status stats the mount directory that klient returns, and if it errors with
 // Permission denied, return an error.
-func (r *PermDeniedRepair) Status() error {
+func (r *DeviceNotConfiguredRepair) Status() error {
 	path, err := r.getMountPath()
 
 	// If we can't even get the mount dir from klient, return the error
@@ -55,16 +59,26 @@ func (r *PermDeniedRepair) Status() error {
 		return nil
 	}
 
-	// Stat should not fail if it's a perm denied (655) problem, the mount exists.
-	// So, we ignore this error.
-	fi, err := os.Stat(path)
+	f, err := os.Open(path)
+
+	// If open fails with device not configured, we return not okay.
+	if err != nil && strings.HasSuffix(err.Error(), dncSuffix) {
+		return fmt.Errorf("Mount %q at %q not configured", r.MountName, path)
+	}
+
+	// If it's another error, log it. In this case there's nothing to do,
+	// we don't know what's wrong.
 	if err != nil {
 		r.Log.Warning("Encountered ignored error. err:%s", err)
 		return nil
 	}
 
-	if fi.Mode() == os.FileMode(0655)|os.ModeDir {
-		return errors.New("MountFolder has the wrong permissions, 655.")
+	// Not returning an error for mount closing failure. Just logging it.
+	if err := f.Close(); err != nil {
+		r.Log.Warning(
+			"Failed to close MountDir. mountName:%s, dir:%s, err:%s",
+			r.MountName, path, err,
+		)
 	}
 
 	return nil
@@ -72,8 +86,8 @@ func (r *PermDeniedRepair) Status() error {
 
 // Repair simply remounts the given directory, and then checks the status one more
 // time before exiting.
-func (r *PermDeniedRepair) Repair() error {
-	r.Stdout.Printlnf("Mount dir contains the wrong permissions, remounting..")
+func (r *DeviceNotConfiguredRepair) Repair() error {
+	r.Stdout.Printlnf("Mount is no longer valid, remounting...")
 
 	if err := r.Klient.RemoteRemount(r.MountName); err != nil {
 		r.Stdout.Printlnf("Unable to remount %s", r.MountName)
@@ -81,7 +95,7 @@ func (r *PermDeniedRepair) Repair() error {
 	}
 
 	if err := r.Status(); err != nil {
-		r.Stdout.Printlnf("Unable to repair permissions issue.")
+		r.Stdout.Printlnf("Unable to repair mount issue.")
 		return err
 	}
 
