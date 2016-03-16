7a8aa0220eead540d185742ceb47805113fa5cc4
Senthil <senthil@koding.com>
2016-03-16 10:52:26 -0700

    fuseklient: remove logging on Unmount failures

--- go/src/koding/fuseklient/unmount_linux.go
+++ go/src/koding/fuseklient/unmount_linux.go
@@ -1,16 +1,10 @@
 package fuseklient
 
-import (
-	"fmt"
-	"os/exec"
-)
+import "os/exec"
 
 // Unmount un mounts Fuse mounted local folder. Mount exists separate to
 // lifecycle of this program and needs to be cleaned up when this exists.
 func Unmount(folder string) error {
-	if _, err := exec.Command("sudo", "umount", "-l", folder).CombinedOutput(); err != nil {
-		fmt.Printf("Unmounting failed. Please do `sudo umount %s`.\n", folder)
-	}
-
-	return nil
+	_, err := exec.Command("sudo", "umount", "-l", folder).CombinedOutput()
+	return err
 }
