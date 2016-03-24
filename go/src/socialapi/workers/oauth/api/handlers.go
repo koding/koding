4555d49b5d64ace298968cac453383e7fbc5f699
mehmetalisavas <mehmetali.savas@yandex.com>
2016-03-24 03:12:40 +0200

    go/oauth: add mongo session data into OauthHandler func

--- go/src/socialapi/workers/oauth/api/handlers.go
+++ go/src/socialapi/workers/oauth/api/handlers.go
@@ -1,9 +1,14 @@
 package api
 
 import (
+	"koding/db/mongodb"
 	"socialapi/config"
 	"socialapi/workers/common/handler"
 	"socialapi/workers/common/mux"
+
+	"github.com/RangelReale/osin"
+	"github.com/osin-mongo-storage/mgostore"
+	"gopkg.in/mgo.v2"
 )
 
 const (
@@ -13,7 +18,11 @@ const (
 
 // AddHandlers adds handlers for slack integration
 func AddHandlers(m *mux.Mux, config *config.Config) {
-	oauth := &Oauth{}
+	mgo = mongodb.NewMongoDB(config.Mongo)
+	session := mgo.Session
+	dbName := ""
+
+	oauth := NewOAuthHandler(session, dbName)
 	m.AddUnscopedHandler(
 		handler.Request{
 			Handler:  oauth.AuthorizeClient,
@@ -31,4 +40,20 @@ func AddHandlers(m *mux.Mux, config *config.Config) {
 		},
 	)
 
+}
+
+func NewOAuthHandler(session *mgo.Session, dbName string) *Oauth {
+	sconfig := osin.NewServerConfig()
+	sconfig.AllowedAuthorizeTypes = osin.AllowedAuthorizeType{osin.CODE, osin.TOKEN}
+	sconfig.AllowedAccessTypes = osin.AllowedAccessType{osin.AUTHORIZATION_CODE,
+		osin.REFRESH_TOKEN, osin.PASSWORD, osin.CLIENT_CREDENTIALS, osin.ASSERTION}
+	sconfig.AllowGetAccessRequest = true
+	storage := mgostore.New(session, dbName)
+	server := osin.NewServer(sconfig, storage)
+
+	return &Oauth{
+		sconfig: sconfig,
+		server:  server,
+		Storage: storage,
+	}
 }
