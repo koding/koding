b545f90e35f733aa127e7a436c07b52da92c07da
Senthil <senthil@koding.com>
2016-03-28 16:40:58 -0700

    klient: turn on linux support back for kd again

--- go/src/koding/klient/app/klient_linux.go
+++
@@ -1,9 +0,0 @@
-package app
-
-// Remote handlers are disabled for linux, currently.
-func (k *Klient) addRemoteHandlers() {}
-
-// Remote is disabled for linux, currently.
-func (k *Klient) initRemote() {
-	k.log.Warning("Disabling Remote Initialization. Klientctl should not use this binary.")
-}
