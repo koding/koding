910971da57ef1237cca651c03b7d374b85beffa8
mehmetalisavas <mehmetali.savas@yandex.com>
2016-03-30 17:07:06 +0300

    go/api: add status int of the machine

--- go/src/socialapi/workers/kodingapi/api/api.go
+++ go/src/socialapi/workers/kodingapi/api/api.go
@@ -22,6 +22,7 @@ func Info(u *url.URL, h http.Header, _ interface{}, context *models.Context) (in
 	return response.NewOK(nil)
 }
 
+// GetMachine gets the machine with machine id
 func GetMachine(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
 	if err := checkAuthorization(h); err != nil {
 		return response.NewBadRequest(err)
@@ -35,6 +36,24 @@ func GetMachine(u *url.URL, h http.Header, _ interface{}, context *models.Contex
 
 	// return response.New(userinfo)
 	return response.HandleResultAndError(machine, err)
+}
+
+// GetMachineStatus gets status of the  machine
+func GetMachineStatus(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
+	if err := checkAuthorization(h); err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	machineId := u.Query().Get("id")
+	machine, err := modelhelper.GetMachine(machineId)
+	if err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	status := machine.State()
+
+	// return response.New(userinfo)
+	return response.HandleResultAndError(status, err)
 }
 
 func checkAuthorization(h http.Header) error {
