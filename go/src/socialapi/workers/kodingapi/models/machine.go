d8a157c562df9f8151f98ba8d7aca857a107b868
mehmetalisavas <mehmetali.savas@yandex.com>
2016-04-09 02:27:15 +0300

    kodingapi: enable created struct by gene again

--- go/src/socialapi/workers/kodingapi/models/machine.go
+++ go/src/socialapi/workers/kodingapi/models/machine.go
@@ -1,27 +1,20 @@
 // Package models holds generated struct for Machine.
 package models
 
-// import "github.com/cihangir/govalidator"
-import "koding/db/models"
+import "github.com/cihangir/govalidator"
+// import "koding/db/models"
 
 // Machine represents a registered Account's Machine Info
 type Machine struct {
-	Machine *models.Machine
-	Status *models.MachineStatus
+	// The unique identifier for a Machine
+	ID int64 `json:"id,omitempty,string"`
+	// Username is the user name of the machine
+	Username string `json:"username"`
+	// Owner return if the machine's username is owner or not.
+	Owner bool `json:"owner,omitempty"`
+	// State is the current status of the machine.
+	State string `json:"state,omitempty"`
 }
-
-//
-// // Machine represents a registered Account's Machine Info
-// type Machine struct {
-// 	// The unique identifier for a Machine
-// 	ID int64 `json:"id,omitempty,string"`
-// 	// Username is the user name of the machine
-// 	Username string `json:"username"`
-// 	// Owner return if the machine's username is owner or not.
-// 	Owner bool `json:"owner,omitempty"`
-// 	// State is the current status of the machine.
-// 	State string `json:"state,omitempty"`
-// }
 
 // NewMachine creates a new Machine struct with default values
 func NewMachine() *Machine {
@@ -30,10 +23,9 @@ func NewMachine() *Machine {
 
 // Validate validates the Machine struct
 func (m *Machine) Validate() error {
-	return nil
-	// return govalidator.NewMulti(govalidator.MaxLength(m.State, 30),
-		// govalidator.MaxLength(m.Username, 20),
+	return govalidator.NewMulti(govalidator.MaxLength(m.State, 30),
+		govalidator.MaxLength(m.Username, 20),
 		// govalidator.Min(float64(m.ID), 1.000000),
-		// govalidator.MinLength(m.Username, 4)).Validate()
+		govalidator.MinLength(m.Username, 4)).Validate()
 }
 //
