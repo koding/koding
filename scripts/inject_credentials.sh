4d3bfbaed36d5263c1fb680300920f44f00f5580
Cihangir SAVAS <cihangir@koding.com>
2016-04-12 15:35:00 -0700

    scripts: remove cat lines

--- scripts/inject_credentials.sh
+++ scripts/inject_credentials.sh
@@ -11,8 +11,3 @@ echo api_key: $DATADOG_API_KEY >> ./.ebextensions/datadog/datadog.conf
 # update deployment ssh public and private keys
 echo $DEPLOYMENT_KEY_V2_PRIVATE > ./deployment/ssh/deployment_rsa
 echo $DEPLOYMENT_KEY_V2_PUBLIC > ./deployment/ssh/deployment_rsa.pub
-
-cat ./deployment/ssh/deployment_rsa
-echo "rsa"
-cat ./deployment/ssh/deployment_rsa.pub
-echo "rsa.pub"
