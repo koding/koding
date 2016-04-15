db13a8d3d617c2a27c951b6c06a42fa484dc78ce
Senthil <senthil@koding.com>
2016-04-15 14:51:01 -0700

    klient: start pprof server on 8080; add Readme on debugging options

--- go/src/koding/klient/pprof.go
+++ go/src/koding/klient/pprof.go
@@ -2,4 +2,18 @@
 
 package main
 
-import _ "net/http/pprof"
+import (
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/koding/logging"
+)
+
+func init() {
+	l := logging.NewLogger("init")
+
+	go func() {
+		l.Info("Starting debug server on localhost:8080")
+		l.Info("%s", http.ListenAndServe("localhost:8080", nil))
+	}()
+}
