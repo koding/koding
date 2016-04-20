a28f2d545706419b3fbbca15027e9612e8ed935a
mehmetalisavas <mehmetali.savas@yandex.com>
2016-04-20 22:02:21 +0300

    oauth: change package name of oauth api

--- go/src/socialapi/workers/oauth/api/api.go
+++ go/src/socialapi/workers/oauth/api/api.go
@@ -1,15 +1,15 @@
-package main
+package api
 
 import (
 	"errors"
 	"fmt"
 	"koding/db/models"
 	"koding/db/mongodb/modelhelper"
+	"log"
 	"net/http"
 
 	"github.com/RangelReale/osin"
-	"github.com/RangelReale/osincli"
-	"github.com/osin-mongo-storage/mgostore"
+	"github.com/kr/pretty"
 )
 
 var (
@@ -21,25 +21,20 @@ var (
 type Oauth struct {
 	sconfig *osin.ServerConfig
 	server  *osin.Server
-	Storage *mgostore.MongoStorage
+	Storage osin.Storage
 }
 
 func (o *Oauth) AuthorizeClient(w http.ResponseWriter, r *http.Request) {
-	server := oauth.server
+	server := o.server
 	resp := server.NewResponse()
+	defer resp.Close()
+
 	if ar := server.HandleAuthorizeRequest(resp, r); ar != nil {
 
-		session, err := getSession(r)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
+		session := HandleLoginPage(ar, w, r)
+		if session == nil {
 			return
 		}
-
-		// Handle the login page
-
-		// if !example.HandleLoginPage(ar, w, r) {
-		// return
-		// }
 
 		// ~to-do , needs to be added users data
 		ar.UserData = session.Username
@@ -48,19 +43,35 @@ func (o *Oauth) AuthorizeClient(w http.ResponseWriter, r *http.Request) {
 
 	}
 	if resp.IsError && resp.InternalError != nil {
+		fmt.Printf("resp %# v", pretty.Formatter(resp))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	if !resp.IsError {
-		resp.Output["custom_parameter"] = 187723
-		return
+		resp.Output["custom_parameter"] = 199231234
+		// return
 	}
-	osin.OutputJSON(resp, w, r)
+	if err := osin.OutputJSON(resp, w, r); err != nil {
+		log.Fatalf("osing output error: %s", err.Error())
+	}
+}
+
+func HandleLoginPage(ar *osin.AuthorizeRequest, w http.ResponseWriter, r *http.Request) *models.Session {
+	session, err := getSession(r)
+	if err != nil || session == nil {
+		w.Header().Set("Location", "/Login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return nil
+	}
+
+	return session
 }
 
 func (o *Oauth) GenerateToken(w http.ResponseWriter, r *http.Request) {
-	server := oauth.server
+	server := o.server
 	resp := server.NewResponse()
+	defer resp.Close()
+
 	if ar := server.HandleAccessRequest(resp, r); ar != nil {
 		switch ar.Type {
 		case osin.AUTHORIZATION_CODE:
@@ -72,51 +83,85 @@ func (o *Oauth) GenerateToken(w http.ResponseWriter, r *http.Request) {
 		}
 		server.FinishAccessRequest(resp, r, ar)
 	}
+	fmt.Printf("resp %# v", pretty.Formatter(resp))
 	if resp.IsError && resp.InternalError != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	if !resp.IsError {
 		resp.Output["custom_parameter"] = 19923
-		return
+		// return
 	}
+	if err := osin.OutputJSON(resp, w, r); err != nil {
+		log.Fatalf("osing output error: %s", err.Error())
+	}
+}
+
+func (o *Oauth) Info(w http.ResponseWriter, r *http.Request) {
+	server := o.server
+	resp := server.NewResponse()
+	defer resp.Close()
+
+	if ir := server.HandleInfoRequest(resp, r); ir != nil {
+		server.FinishInfoRequest(resp, r, ir)
+	}
+
 	osin.OutputJSON(resp, w, r)
 }
 
-func (o *Oauth) Callback(w http.ResponseWriter, r *http.Request) {
-	areq := client.NewAuthorizeRequest(osincli.CODE)
-
-	if areqdata, err := areq.HandleRequest(r); err == nil {
-		treq := client.NewAccessRequest(osincli.AUTHORIZATION_CODE, areqdata)
-
-		// exchange the authorize token for the access token
-		ad, er := treq.GetToken()
-		if er == nil {
-			w.Write([]byte(fmt.Sprintf("Access token: %+v\n\n", ad)))
-
-			// use the token in ad.AccessToken
-		} else {
-			w.Write([]byte(fmt.Sprintf("ERROR: %s\n", err)))
-		}
-	} else {
-		w.Write([]byte(fmt.Sprintf("ERROR: %s\n", err)))
-	}
-
-}
+///// TEST FOR OAUTH SERVER ////
+//
+// config := &osincli.ClientConfig{
+// 	ClientId:     "koding_client_id",
+// 	ClientSecret: "koding_secret",
+// 	AuthorizeUrl: "http://dev.koding.com:8090/api/social/oauth/authorize",
+// 	TokenUrl:     "http://dev.koding.com:8090/api/social/oauth/token",
+// 	// RedirectUrl:              "http://dev.koding.com:8090/api/social/oauth/callback",
+// 	RedirectUrl:              "http://dev.koding.com:8090/api/social/appauth",
+// 	ErrorsInStatusCode:       true,
+// 	SendClientSecretInParams: true,
+// 	Scope: "user_role",
+// }
+//
+// func (o *Oauth) Callback(w http.ResponseWriter, r *http.Request) {
+// 	client, err := osincli.NewClient(config)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	areq := client.NewAuthorizeRequest(osincli.CODE)
+//
+// 	if areqdata, err := areq.HandleRequest(r); err == nil {
+// 		treq := client.NewAccessRequest(osincli.AUTHORIZATION_CODE, areqdata)
+//
+// 		// exchange the authorize token for the access token
+// 		ad, er := treq.GetToken()
+// 		if er == nil {
+// 			w.Write([]byte(fmt.Sprintf("Access token: %+v\n\n", ad)))
+//
+// 			// use the token in ad.AccessToken
+// 		} else {
+// 			w.Write([]byte(fmt.Sprintf("ERROR: %s\n", err)))
+// 		}
+// 	} else {
+// 		w.Write([]byte(fmt.Sprintf("ERROR: %s\n", err)))
+// 	}
+//
+// }
 
 func getSession(r *http.Request) (*models.Session, error) {
 	cookie, err := r.Cookie("clientId")
 	if err != nil {
-		return "", ErrClientIdNotFound
+		return nil, ErrClientIdNotFound
 	}
 
 	if cookie.Value == "" {
-		return "", ErrCookieValueNotFound
+		return nil, ErrCookieValueNotFound
 	}
 
 	session, err := modelhelper.GetSession(cookie.Value)
 	if err != nil {
-		return "", ErrSessionNotFound
+		return nil, ErrSessionNotFound
 	}
 
 	return session, nil
