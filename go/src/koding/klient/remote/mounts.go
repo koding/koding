17d4337a1d60b470adba487d5f9d657f8c1ba837
Lee Olayvar <leeolayvar@gmail.com>
2016-04-29 13:49:42 -0700

    klient: Fixed status display for repeated remounts

--- go/src/koding/klient/remote/mounts.go
+++ go/src/koding/klient/remote/mounts.go
@@ -202,19 +202,12 @@ func (r *Remote) restoreMount(m *mount.Mount) (err error) {
 		log.SetLevel(logging.DEBUG)
 	}
 
-	remoteMachine, err := r.GetDialedMachine(m.MountName)
+	// First get the plain machine, we don't care about it being dialed or valid as
+	// we will potentially just be setting the status with it.
+	remoteMachine, err := r.GetMachine(m.MountName)
 	if err != nil {
 		return err
 	}
-
-	if remoteMachine.IsMountingLocked() {
-		log.Warning("Restore mount was attempted but the machine is mount locked")
-		return machine.ErrMachineActionIsLocked
-	}
-
-	// Lock and defer unlock the machine mount actions
-	remoteMachine.LockMounting()
-	defer remoteMachine.UnlockMounting()
 
 	// Update the status based on the return value. Note that it's possible to
 	// return before this call, if we can't get the machine, but that's a non-issue
@@ -229,6 +222,25 @@ func (r *Remote) restoreMount(m *mount.Mount) (err error) {
 			remoteMachine.SetStatus(machine.MachineStatusUnknown, "")
 		}
 	}()
+
+	// Now try to get a valid, dialed machine. We're doing this *after* the
+	// machine's setstatus defer, so that we can set autoRemountingAgain as needed.
+	//
+	// Note that we're not getting the instance here, because if we cannot get a
+	// dialed machine then remoteMachine will be set to nil, causing a panic
+	// in the defer above. Regardless, it's the same instance, we don't need it.
+	if _, err := r.GetDialedMachine(m.MountName); err != nil {
+		return err
+	}
+
+	if remoteMachine.IsMountingLocked() {
+		log.Warning("Restore mount was attempted but the machine is mount locked")
+		return machine.ErrMachineActionIsLocked
+	}
+
+	// Lock and defer unlock the machine mount actions
+	remoteMachine.LockMounting()
+	defer remoteMachine.UnlockMounting()
 
 	fsMountName, _ := mountcli.NewMountcli().FindMountNameByPath(m.LocalPath)
 	if fsMountName != "" {
