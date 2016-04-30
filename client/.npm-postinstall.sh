c27b1f8fea62b0a5b09463414e46dbeca35c80d5
Sonmez Kartal <sonmez@koding.com>
2016-04-30 02:02:19 +0000

    Register npm-install timestamp in client immediately

--- client/.npm-postinstall.sh
+++ client/.npm-postinstall.sh
@@ -2,14 +2,14 @@
 
 set -o errexit
 
+pushd $(dirname $0)
+
+touch node_modules/.npm-install.timestamp
+
 [ "$CI" != "true" -o "$WERCKER" != "true" ] && exit 0
 
 [ "$WERCKER_GIT_REPOSITORY" != "koding" ] && exit 0
 [ "$WERCKER_GIT_BRANCH" != "master" ] && exit 0
-
-pushd $(dirname $0)
-
-touch node_modules/.npm-install.timestamp
 
 UPDATE_WERCKER_NODE_MODULES=../scripts/wercker/update-node-modules
 
