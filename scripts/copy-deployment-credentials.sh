daf69cd6e364a3bea31eca1236b72a9cbef0933d
Sonmez Kartal <sonmez@koding.com>
2016-05-16 14:03:45 +0300

    Fetch only a single revision of credential repositories
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/copy-deployment-credentials.sh
+++ scripts/copy-deployment-credentials.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git clone git@github.com:koding/credential.git
-git clone git@github.com:koding/vault.git
+git clone --depth 1 git@github.com:koding/credential.git
+git clone --depth 1 git@github.com:koding/vault.git
+
 cp -Rv credential/config/* config/
 cp -Rv credential/scripts/* scripts/
