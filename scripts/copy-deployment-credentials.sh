235c2528a4aa577bc1c61fa51cd7833a384e4e7d
Sonmez Kartal <sonmez@koding.com>
2016-05-16 14:04:17 +0300

    Copy credentials in vault repository
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/copy-deployment-credentials.sh
+++ scripts/copy-deployment-credentials.sh
@@ -3,5 +3,5 @@
 git clone --depth 1 git@github.com:koding/credential.git
 git clone --depth 1 git@github.com:koding/vault.git
 
-cp -Rv credential/config/* config/
-cp -Rv credential/scripts/* scripts/
+cp -Rv credential/config/* vault/config/* config/
+cp -Rv credential/scripts/* vault/scripts/* scripts/
