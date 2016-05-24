2fb75bed2b1adf117570f3ea4d5a843184c01e8f
Rafal Jeczalik <rjeczalik@gmail.com>
2016-05-24 16:57:07 +0200

    api/amazon: add API for dealing with images

--- go/src/koding/kites/kloud/api/amazon/client.go
+++ go/src/koding/kites/kloud/api/amazon/client.go
@@ -864,3 +864,128 @@ func (c *Client) ModifyInstance(params *ec2.ModifyInstanceAttributeInput) error 
 	_, err := c.EC2.ModifyInstanceAttribute(params)
 	return awsError(err)
 }
+
+// CreateImage is a wrapper for (*ec2.EC2).CreateImage.
+func (c *Client) CreateImage(instanceID, name string) (imageID string, err error) {
+	params := &ec2.CreateImageInput{
+		InstanceId: aws.String(instanceID),
+		Name:       aws.String(name),
+	}
+
+	resp, err := c.EC2.CreateImage(params)
+	if err != nil {
+		return "", awsError(err)
+	}
+
+	return aws.StringValue(resp.ImageId), nil
+}
+
+// Image is a wrapper for ec2.Image.
+type Image struct {
+	Raw *ec2.Image
+}
+
+// Snapshot gives the ID and size of the snapshot that image uses.
+//
+// NOTE(rjeczalik): The API was built for provider/aws migration of koding
+// instances, which had only a single EBS.
+func (i *Image) Snapshot() (string, int64, error) {
+	for _, dev := range i.Raw.BlockDeviceMappings {
+		if dev.Ebs == nil {
+			continue
+		}
+
+		return aws.StringValue(dev.Ebs.SnapshotId), aws.Int64Value(dev.Ebs.VolumeSize), nil
+	}
+
+	return "", 0, fmt.Errorf("no snapshots found for %s image", aws.StringValue(i.Raw.ImageId))
+}
+
+// DescribeImage looks up an image by its id.
+//
+// If call succeeds but no snapshots were found, it returns non-nil
+// *NotFoundError.
+func (c *Client) DescribeImage(imageID string) (*Image, error) {
+	params := &ec2.DescribeImagesInput{
+		ImageIds: []*string{aws.String(imageID)},
+	}
+
+	resp, err := c.EC2.DescribeImages(params)
+	if err != nil {
+		return nil, awsError(err)
+	}
+
+	if len(resp.Images) == 0 {
+		return nil, newNotFoundError("Image", fmt.Errorf("no image found with id=%s", imageID))
+	}
+
+	if len(resp.Images) > 1 {
+		c.Log.Warning("more than one image found with id=%s", imageID)
+	}
+
+	return &Image{
+		Raw: resp.Images[0],
+	}, nil
+}
+
+// WaitImage blocks until an imgage given by the imageID becomes
+// available.
+func (c *Client) WaitImage(imageID string) error {
+	params := &ec2.DescribeImagesInput{
+		ImageIds: []*string{aws.String(imageID)},
+	}
+
+	return awsError(c.EC2.WaitUntilImageAvailable(params))
+}
+
+// AllowCopyImage modifies the image attributes to allow access
+// by a user given by the accountID.
+//
+// It also modifies its snapshot's permissions to allow the access.
+func (c *Client) AllowCopyImage(image *Image, accountID string) error {
+	snapshotID, _, err := image.Snapshot()
+	if err != nil {
+		return err
+	}
+
+	imgParams := &ec2.ModifyImageAttributeInput{
+		ImageId: image.Raw.ImageId,
+		LaunchPermission: &ec2.LaunchPermissionModifications{
+			Add: []*ec2.LaunchPermission{{
+				UserId: aws.String(accountID),
+			}},
+		},
+	}
+
+	_, err = c.EC2.ModifyImageAttribute(imgParams)
+	if err != nil {
+		return awsError(err)
+	}
+
+	snapParams := &ec2.ModifySnapshotAttributeInput{
+		SnapshotId:    aws.String(snapshotID),
+		Attribute:     aws.String("createVolumePermission"),
+		OperationType: aws.String("add"),
+		UserIds:       []*string{aws.String(accountID)},
+	}
+
+	_, err = c.EC2.ModifySnapshotAttribute(snapParams)
+	return awsError(err)
+}
+
+// CopyImage imports the given source image, which may belong to different
+// user.
+func (c *Client) CopyImage(srcImageID, srcRegion, newName string) (newImageID string, err error) {
+	params := &ec2.CopyImageInput{
+		SourceImageId: aws.String(srcImageID),
+		SourceRegion:  aws.String(srcRegion),
+		Name:          aws.String(newName),
+	}
+
+	resp, err := c.EC2.CopyImage(params)
+	if err != nil {
+		return "", awsError(err)
+	}
+
+	return aws.StringValue(resp.ImageId), nil
+}
