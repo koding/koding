5a66dc8f5f13f23d72cd9bcaf2deea80bddd9761
Sonmez Kartal <sonmez@koding.com>
2016-05-26 00:02:42 +0300

    Clone pull request branch of credential repositories
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/copy-deployment-credentials.sh
+++ scripts/copy-deployment-credentials.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-git clone --depth 1 git@github.com:koding/credential.git
-git clone --depth 1 git@github.com:koding/vault.git
+function clone() {
+  declare REPOSITORY=$1
+  declare BRANCH=$2
+  git clone --branch $BRANCH --depth 1 git@github.com:koding/$REPOSITORY.git
+}
+
+BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+BRANCH=${BRANCH:master}
+
+clone vault $BRANCH || clone vault master
+clone credential $BRANCH || clone credential master
 
 cp -Rv credential/config/* vault/config/* config/
 cp -Rv credential/scripts/* vault/scripts/* scripts/
