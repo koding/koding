6d05d09889bb79f217592111429d6dc7345c77ca
mehmetalisavas <mehmetali.savas@yandex.com>
2016-06-02 22:13:15 +0300

    go/datadog: add eventexporter

---
+++ go/src/github.com/koding/eventexporter/datadogexporter.go
@@ -0,0 +1,102 @@
+package eventexporter
+
+import (
+	"fmt"
+	"strings"
+
+	kodingmetrics "github.com/koding/metrics"
+	"github.com/kr/pretty"
+)
+
+type DatadogExporter struct {
+	datadog *kodingmetrics.DogStatsD
+}
+
+func NewDatadogExporter(d *kodingmetrics.DogStatsD) *DatadogExporter {
+	return &DatadogExporter{datadog: d}
+}
+
+func (d *DatadogExporter) Send(m *Event) error {
+	fmt.Printf("m %# v\n", pretty.Formatter(m))
+	eventName := strings.Replace(m.Name, " ", "_", -1)
+
+	tags := make([]string, 0)
+	if m.User.Email != "" {
+		tags = append(tags, fmt.Sprintf("email:%s", m.User.Email))
+	}
+
+	if m.User.Username != "" {
+		tags = append(tags, fmt.Sprintf("username:%s", m.User.Username))
+	}
+
+	for key, val := range m.Properties {
+		value, ok := val.(string)
+		if !ok {
+			continue
+		}
+
+		if isAllowed(key) && value != "" {
+			tags = append(tags, fmt.Sprintf("%s:%s", clean(key), clean(val.(string))))
+		}
+	}
+
+	fmt.Println("eventName-->", eventName)
+	fmt.Println("tags-->", tags)
+
+	return d.datadog.Count(eventName, 1, tags, 1)
+}
+
+func clean(s string) string {
+	return strings.Replace(s, " ", "_", -1)
+}
+
+func isAllowed(ß string) bool {
+	whitelist := []string{
+		"host",
+		"groupName",
+		"email",
+		"inviter",
+		"subject",
+		"env",
+		"category",
+		"label",
+		"firstName",
+		"invitesCount",
+	}
+	for _, val := range whitelist {
+		if ß == val {
+			return true
+		}
+	}
+
+	return false
+}
+func (d *DatadogExporter) Close() error {
+	return nil
+}
+
+type MultiExporter []Exporter
+
+func NewMultiExporter(e ...Exporter) MultiExporter {
+	return MultiExporter(e)
+}
+
+func (m MultiExporter) Send(event *Event) error {
+	for _, e := range m {
+		if err := e.Send(event); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m MultiExporter) Close() error {
+	for _, e := range m {
+		if err := e.Close(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
