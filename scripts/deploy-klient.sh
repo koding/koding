ddf18635681c89080118496ff0cfb860a29b5662
Rafal Jeczalik <rjeczalik@gmail.com>
2016-06-03 15:25:02 +0200

    deploy: reformatted with shfmt
    github.com/mvdan/sh

--- scripts/deploy-klient.sh
+++ scripts/deploy-klient.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 REPO_PATH=$(git rev-parse --show-toplevel)
 
 # configure s3cmd
-cat > $HOME/.s3cfg <<EOF
+cat >$HOME/.s3cfg <<EOF
 [default]
 access_key=$S3_KEY_ID
 secret_key=$S3_KEY_SECRET
@@ -21,19 +21,19 @@ export S3DIR="s3://koding-klient/$KLIENT_CHANNEL"
 s3cmd get $S3DIR/latest-version.txt version
 
 export OLDBUILDNO=$(cat version)
-export NEWBUILDNO=$(($OLDBUILDNO+1))
+export NEWBUILDNO=$(($OLDBUILDNO + 1))
 export KLIENT_PREFIX="klient-0.1.$NEWBUILDNO"
 export KLIENT_DEB="klient_0.1.${NEWBUILDNO}_${KLIENT_CHANNEL}_amd64.deb"
 
 echo "New version will be: $NEWBUILDNO"
 rm -f version
 
-klient-build() {
+klient_build() {
 	GOOS="${1:-}" GOARCH=amd64 go build -v -ldflags "-X koding/klient/protocol.Version 0.1.$NEWBUILDNO -X koding/klient/protocol.Environment $KLIENT_CHANNEL" -o klient koding/klient
 }
 
 # build klient binary for linux
-klient-build
+klient_build
 
 # validate klient version
 [[ $(./klient -version) == "0.1.$NEWBUILDNO" ]]
@@ -46,7 +46,7 @@ mv klient.gz ${KLIENT_PREFIX}.gz
 go run "${REPO_PATH}/go/src/koding/klient/build/build.go" -e $KLIENT_CHANNEL -b $NEWBUILDNO
 dpkg -f $KLIENT_DEB
 
-klient-build "darwin"
+klient_build "darwin"
 gzip -9 -N -f klient
 mv klient.gz ${KLIENT_PREFIX}.darwin_amd64.gz
 
@@ -61,11 +61,11 @@ cp -f $KLIENT_DEB klient.deb
 
 # Cleanup the latest/ folder and put the latest one in there
 s3cmd del --recursive $S3DIR/latest/
-s3cmd -P put klient.deb klient.gz klient.darwin_amd64.gz  $S3DIR/latest/
+s3cmd -P put klient.deb klient.gz klient.darwin_amd64.gz $S3DIR/latest/
 
 # Update latest-version.txt with the latest version
 s3cmd del $S3DIR/latest-version.txt
-echo $NEWBUILDNO > latest-version.txt
+echo $NEWBUILDNO >latest-version.txt
 s3cmd -P put latest-version.txt $S3DIR/latest-version.txt
 
 #  Update install.sh file
