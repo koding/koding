1ec195c754a748d0ac6a0c0d97531f7eb421c62b
Cihangir SAVAS <cihangir@koding.com>
2016-06-25 01:18:57 +0300

    go: fix import name

--- go/src/koding/workers/tunnelproxymanager/records.go
+++ go/src/koding/workers/tunnelproxymanager/records.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/route53"
-	"github.com/crowdmob/goamz/ec2"
 	"github.com/koding/logging"
 )
 
