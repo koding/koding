f15d5410dce8c2895a736e9b66d2e7b9b149cdca
Sinan Yasar <sinan@koding.com>
2016-06-24 18:08:36 -0700

    go: webserver: taken segment injection from header to body

--- go/src/koding/go-webserver/templates/loggedin.go
+++ go/src/koding/go-webserver/templates/loggedin.go
@@ -14,6 +14,12 @@ var LoggedInHome = `
     {{.UnsupportedHTML}}
 
     <script>
+      !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
+        analytics.load("{{.Segment}}");
+      }}();
+    </script>
+
+    <script>
       var _globals = {
         config: {{.Runtime}},
         isLoggedInOnLoad: true,
@@ -42,7 +48,7 @@ var LoggedInHome = `
     </script>
 
     {{if not .Impersonating }}
-      <script type="text/javascript">
+      <script>
         (function () {
         var _user_id = '{{.User.GetWithDefaultStr "Username" }}'; var _session_id = '{{.User.GetWithDefaultStr "SessionId" }}'; var _sift = _sift || []; _sift.push(['_setAccount', 'f270274999']); _sift.push(['_setUserId', _user_id]); _sift.push(['_setSessionId', _session_id]); _sift.push(['_trackPageview']); (function() { function ls() { var e = document.createElement('script'); e.type = 'text/javascript'; e.async = true; e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s); } if (window.attachEvent) { window.attachEvent('onload', ls); } else { window.addEventListener('load', ls, false); } })();
         })();
@@ -59,6 +65,7 @@ var LoggedInHome = `
         a.appendChild(r);
       })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
+
 </body>
 </html>
 `
