f0141ebc6e7e748c9b8cfc35534b69eee193f11c
mehmetalisavas <mehmetali.savas@yandex.com>
2016-06-29 08:06:34 +0300

    go/models: add company struct into mongo models

--- go/src/koding/db/models/company.go
+++ go/src/koding/db/models/company.go
@@ -1,11 +1,48 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"errors"
+	"strings"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 type Company struct {
-	Id        bson.ObjectId `bson:"_id" json:"-"`
-	Name      string        `bson:"name" json:"-"`
-	Slug      string        `bson:"slug" json:"-"`
-	Employees int           `bson:"employees" json:"-"`
-	Domain    int           `bson:"domain" json:"-"`
+	Id bson.ObjectId `bson:"_id" json:"-"`
+	// Name holds the name of the company,
+	// Company name is case sensitive, so use slug instead of Name for company operations
+	// e.g : Koding, PubNub ...
+	Name string `bson:"name" json:"-"`
+	// Slug is the LowerCase format of the company name
+	// this satify us consistency on company operations
+	// e.g : koding, pubnub ...
+	Slug string `bson:"slug" json:"-"`
+	// Employees holds the employee count of the companies
+	// e.g: koding members; 20
+	Employees int `bson:"employees" json:"-"`
+	// Domain is the domain of the company
+	// e.g: koding.com
+	Domain string `bson:"domain" json:"-"`
+}
+
+// Error values for returning values
+var (
+	ErrCompanyNameIsEmpty   = errors.New("company name is empty")
+	ErrCompanyEmployeesZero = errors.New("company employees is zero")
+)
+
+func (c *Company) CheckValues() error {
+	if c.Name == "" {
+		return ErrCompanyNameIsEmpty
+	}
+
+	if c.Slug == "" {
+		c.Slug = strings.ToLower(c.Name)
+	}
+
+	if c.Employees == 0 {
+		return ErrCompanyEmployeesZero
+	}
+
+	return nil
 }
