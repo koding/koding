da91c6022cf4d8d139c5a66b86a82d17f98c754e
mehmetalisavas <mehmetali.savas@yandex.com>
2016-06-30 22:39:15 +0300

    go: use original package for clearbit

--- go/src/github.com/f2prateek/clearbit-go/main/main.go
+++ go/src/github.com/f2prateek/clearbit-go/main/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/f2prateek/clearbit-go"
 )
 
-func ExampleCombined() {
+func main() {
 	c := clearbit.New("9d961e7ac862a6bc430f783da5cf9422")
 
 	combined, err := c.Enrichment().Combined("mehmet@koding.com")
@@ -17,6 +17,7 @@ func ExampleCombined() {
 
 	fmt.Println(*combined.Person.Name.FullName)
 	fmt.Println(*combined.Company.Name)
+	fmt.Println(*combined.Company.Metrics.Employees)
 
 	// Output:
 	// Prateek Srivastava
