25698f6dc2ad7c5f76037cbc87193e6f49c05f76
mehmetalisavas <mehmetali.savas@yandex.com>
2016-07-01 06:06:55 +0300

    go/team: reduce code complexity and increase readability

--- go/src/socialapi/workers/team/team.go
+++ go/src/socialapi/workers/team/team.go
@@ -118,42 +118,47 @@ func (c *Controller) HandleCreator(channel *models.Channel) error {
 		return err
 	}
 
+	if userData.Company == nil {
+		return nil
+	}
+
+	if userData.Company.Name == nil {
+		return nil
+	}
+
 	// if code line reach to here, it means that we got user's company data,
 	// after that we are going to update user's data.
 	var company *mongomodels.Company
-	if userData.Company != nil {
-		if userData.Company.Name != nil {
-			company, err = modelhelper.GetCompanyByNameOrSlug(*userData.Company.Name)
-			if err != nil && err != mgo.ErrNotFound {
-				return err
-			}
 
-			// if company is not found in db, then create new one
-			// after creation, update user's company with company id
-			if err == mgo.ErrNotFound {
-				companyData, err := checkValuesForCompany(userData.Company)
-				if err != nil {
-					return nil
-				}
+	company, err = modelhelper.GetCompanyByNameOrSlug(*userData.Company.Name)
+	if err != nil && err != mgo.ErrNotFound {
+		return err
+	}
 
-				// create company in db if it doesn't exist
-				if err := modelhelper.CreateCompany(companyData); err != nil {
-					return err
-				}
-				company, err = modelhelper.GetCompanyByNameOrSlug(companyData.Name)
-				if err != nil {
-					return err
-				}
-
-			}
-
-			// update the company info of user if company exist in mongo
-			selector := bson.M{"username": user.Name}
-			update := bson.M{"companyId": company.Id}
-			if err := modelhelper.UpdateUser(selector, update); err != nil {
-				return err
-			}
+	// if company is not found in db, then create new one
+	// after creation, update user's company with company id
+	if err == mgo.ErrNotFound {
+		companyData, err := checkValuesForCompany(userData.Company)
+		if err != nil {
+			return nil
 		}
+
+		// create company in db if it doesn't exist
+		if err := modelhelper.CreateCompany(companyData); err != nil {
+			return err
+		}
+		company, err = modelhelper.GetCompanyByNameOrSlug(companyData.Name)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	// update the company info of user if company exist in mongo
+	selector := bson.M{"username": user.Name}
+	update := bson.M{"companyId": company.Id}
+	if err := modelhelper.UpdateUser(selector, update); err != nil {
+		return err
 	}
 
 	return nil
