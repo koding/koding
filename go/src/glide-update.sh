26b6bb81c31d10a310c86e7ce3988313fed9d292
Koding <rjeczalik@gmail.com>
2016-07-21 19:12:07 +0000

    koding: glide update

--- go/src/glide-update.sh
+++ go/src/glide-update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# This script is a hack for glide update.
+#
+# We use a forked sources for Terraform, in which
+# we make certain error types being non-retryable.
+#
+# We should instead use vanilla Terraform and configure
+# it to behave as we require. Or extend it and ditto.
+#
+# TODO(rjeczalik): remove this script
+
 set -euo pipefail
 
 GOPATH=$(git rev-parse --show-toplevel)/go
