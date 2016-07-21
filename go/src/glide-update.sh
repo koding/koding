a8b03742e71085f2668d7173eab7e0608067df73
Koding <rjeczalik@gmail.com>
2016-07-21 21:43:41 +0000

    koding: update glide

--- go/src/glide-update.sh
+++ go/src/glide-update.sh
@@ -8,6 +8,11 @@
 # We should instead use vanilla Terraform and configure
 # it to behave as we require. Or extend it and ditto.
 #
+# We use a forked sources for gorm, since ./run buildservices
+# relies on behaviour of that specific version, otherwise
+# building services fails with a lot of pq errors.
+# This should be fixed.
+#
 # TODO(rjeczalik): remove this script
 
 set -euo pipefail
@@ -17,11 +22,21 @@ GOPATH=$(git rev-parse --show-toplevel)/go
 pushd "$GOPATH/src"
 
 rm -rf vendor
+
 glide cc
 glide update --strip-vcs || true
+
 rm -rf vendor/github.com/hashicorp/terraform
 git clone git@github.com:koding/terraform vendor/github.com/hashicorp/terraform
 rm -rf vendor/github.com/hashicorp/terraform/.git
+
+rm -rf vendor/github.com/jinzhu/gorm
+git clone git@github.com:koding/gorm vendor/github.com/jinzhu/gorm
+rm -rf vendor/github.com/jinzhu/gorm/.git
+
+rm -rf vendor/github.com/koding/kite
+git clone git@github.com:rjeczalik/kite vendor/github.com/koding/kite
+rm -rf vendor/github.com/koding/kite/.git
 
 popd
 
