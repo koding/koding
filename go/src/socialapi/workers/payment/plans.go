8d0e770f4a7acbac9c13429fb62a0062ba946590
Cihangir SAVAS <cihangir@koding.com>
2016-08-16 15:32:57 +0300

    go/payment: add default plan creator

---
+++ go/src/socialapi/workers/payment/plans.go
@@ -0,0 +1,111 @@
+package payment
+
+import (
+	"strings"
+
+	stripe "github.com/stripe/stripe-go"
+	stripeplan "github.com/stripe/stripe-go/plan"
+)
+
+// Up to 10 users:  $49.97 per developer per month
+// Up to 50 users:  $39.97 per developer per month
+// Over 50 users:  $34.97 per developer per month
+
+const (
+	usd = stripe.Currency("USD")
+)
+
+// TrialPeriod: Specifies a trial period in (an integer number of) days. If you
+// include a trial period, the customer won’t be billed for the first time until
+// the trial period ends. If the customer cancels before the trial period is
+// over, she’ll never be billed at all.
+
+var Plans = []*stripe.PlanParams{
+	// Forever free koding
+	&stripe.PlanParams{
+		Amount:        0,
+		Interval:      stripeplan.Month,
+		IntervalCount: 1,
+		TrialPeriod:   0,
+		Name:          "Free Forever",
+		Currency:      usd,
+		ID:            "p_free_forever",
+		Statement:     "FREE",
+	},
+
+	// 7 days free koding
+	&stripe.PlanParams{
+		Amount:        0,
+		Interval:      stripeplan.Week,
+		IntervalCount: 1,
+		TrialPeriod:   7,
+		Name:          "Free For 7 days",
+		Currency:      usd,
+		ID:            "p_free_for_7_days",
+		Statement:     "FREE FOR 7 DAYS",
+	},
+
+	// 30 days free koding
+	&stripe.PlanParams{
+		Amount:        0,
+		Interval:      stripeplan.Month,
+		IntervalCount: 1,
+		TrialPeriod:   30,
+		Name:          "Free For 30 days",
+		Currency:      usd,
+		ID:            "p_free_for_30_days",
+		Statement:     "FREE FOR 30 DAYS",
+	},
+
+	&stripe.PlanParams{
+		Amount:        4997,
+		Interval:      stripeplan.Month,
+		IntervalCount: 1,
+		TrialPeriod:   0,
+		Name:          "Up to 10 users",
+		Currency:      usd,
+		ID:            "p_up_to_10",
+		Statement:     "UP TO 10 USERS",
+	},
+	&stripe.PlanParams{
+		Amount:        3997,
+		Interval:      stripeplan.Month,
+		IntervalCount: 1,
+		TrialPeriod:   0,
+		Name:          "Up to 50 users",
+		Currency:      usd,
+		ID:            "p_up_to_50",
+		Statement:     "UP TO 50 USERS",
+	},
+	&stripe.PlanParams{
+		Amount:        3497,
+		Interval:      stripeplan.Month,
+		IntervalCount: 1,
+		TrialPeriod:   0,
+		Name:          "Over 50 users",
+		Currency:      usd,
+		ID:            "p_over_50",
+		Statement:     "OVER 50 USERS",
+	},
+}
+
+// CreateDefaultPlans creates predefined default plans. This is meant to be run
+// when the worker starts to be sure the plans are there.
+func CreateDefaultPlans() error {
+	for _, planParams := range Plans {
+		p, err := stripeplan.Get(planParams.ID, nil)
+		if err == nil && p != nil && p.ID == planParams.ID {
+			continue
+		}
+
+		if _, err := stripeplan.New(planParams); err != nil {
+			if strings.Contains(err.Error(), "already exists") {
+				continue
+			}
+
+			return err
+		}
+	}
+
+	return nil
+}
