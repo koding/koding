fbb4536a4e7cd50909cf08dc0adfdca9f3a0ff70
Cihangir SAVAS <cihangir@koding.com>
2016-08-16 15:34:08 +0300

    go/payment: remove stripe and paypal test runner

--- go/src/socialapi/workers/payment/tests.sh
+++ go/src/socialapi/workers/payment/tests.sh
@@ -3,11 +3,5 @@
 echo "running payment tests"
 go test -c socialapi/workers/payment/ && ./payment.test -c $(dirname $0)/../../config/dev.toml -test.v=true
 
-echo "running stripe tests"
-go test -c socialapi/workers/payment/stripe/ && ./stripe.test -c $(dirname $0)/../../config/dev.toml -test.v=true
-
-echo "running paypal tests"
-go test -c socialapi/workers/payment/paypal/ && ./paypal.test -c $(dirname $0)/../../config/dev.toml -test.v=true
-
 echo "running webhook tests"
 go test -c socialapi/workers/payment/paymentwebhook/ && ./paymentwebhook.test -c $(dirname $0)/../../config/dev.toml -test.v=true
