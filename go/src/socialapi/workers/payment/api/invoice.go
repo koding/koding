ef4a9ee0a1dd5563a8da29bd3579146c38a92f9b
Cihangir SAVAS <cihangir@koding.com>
2016-08-21 16:34:27 +0300

    go/payment: add invoice listing functionality

---
+++ go/src/socialapi/workers/payment/api/invoice.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"socialapi/models"
+	"socialapi/workers/common/response"
+	"socialapi/workers/payment"
+)
+
+func ListInvoice(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
+	if err := checkContext(context); err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	urlQuery := u.Query()
+	limit, _ := strconv.Atoi(urlQuery.Get("limit"))
+	if limit == 0 {
+		limit = 10
+	}
+
+	return response.HandleResultAndError(
+		payment.ListInvoicesForGroup(
+			context.GroupName,
+			urlQuery.Get("startingAfter"),
+			limit,
+		),
+	)
+}
