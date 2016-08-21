01f0bb5b5166d99e03f98d9f327bc0bdb7a9946e
Cihangir SAVAS <cihangir@koding.com>
2016-08-21 13:37:13 +0300

    go/payment: remove webhook test runner

--- go/src/socialapi/workers/payment/tests.sh
+++ go/src/socialapi/workers/payment/tests.sh
@@ -3,5 +3,3 @@
 echo "running payment tests"
 go test -c socialapi/workers/payment/ && ./payment.test -c $(dirname $0)/../../config/dev.toml -test.v=true
 
-echo "running webhook tests"
-go test -c socialapi/workers/payment/paymentwebhook/ && ./paymentwebhook.test -c $(dirname $0)/../../config/dev.toml -test.v=true
