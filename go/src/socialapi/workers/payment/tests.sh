2f464f4b872d65bc8c5174aff5f6de39046fc01a
Cihangir SAVAS <cihangir@koding.com>
2016-08-22 18:52:43 +0300

    go/payment: add payment api tests

--- go/src/socialapi/workers/payment/tests.sh
+++ go/src/socialapi/workers/payment/tests.sh
@@ -3,3 +3,6 @@
 echo "running payment tests"
 go test -c socialapi/workers/payment/ && ./payment.test -c $(dirname $0)/../../config/dev.toml -test.v=true
 
+echo "running api tests"
+go test -c socialapi/workers/payment/api && ./api.test -c $(dirname $0)/../../config/dev.toml -test.v=true
+
