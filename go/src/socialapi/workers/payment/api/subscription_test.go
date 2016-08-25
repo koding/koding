18efeded95f8c4a5dbcdd25cf2197342d3f4eae0
Cihangir SAVAS <cihangir@koding.com>
2016-08-25 13:20:41 +0300

    go/payment: add test cases to validate assumptions based on payment

--- go/src/socialapi/workers/payment/api/subscription_test.go
+++ go/src/socialapi/workers/payment/api/subscription_test.go
@@ -78,3 +78,125 @@ func TestCreateSubscriptionWithPlan(t *testing.T) {
 		})
 	})
 }
+
+// make sure we can subscribe to a paid plan without a CC if it has trial period
+func TestSubscribingToPaidPlanWithWithTrialPeriodHavingNoCC(t *testing.T) {
+	Convey("Given stub data", t, func() {
+		withTestServer(t, func(endpoint string) {
+			withStubData(endpoint, func(username, groupName, sessionID string) {
+
+				pp := &stripe.PlanParams{
+					Amount:        12345,
+					Interval:      stripeplan.Month,
+					IntervalCount: 1,
+					TrialPeriod:   1, // trial for one day
+					Name:          fmt.Sprintf("plan for %s", username),
+					Currency:      currency.USD,
+					ID:            fmt.Sprintf("plan_for_%s", username),
+					Statement:     "NAN-FREE",
+				}
+
+				plan, err := stripeplan.New(pp)
+				So(err, ShouldBeNil)
+
+				withSubscription(endpoint, groupName, sessionID, plan.ID, func(subscriptionID string) {
+					So(subscriptionID, ShouldNotBeEmpty)
+					Convey("We should be able to create the subscription", func() {
+						getURL := fmt.Sprintf("%s%s", endpoint, EndpointSubscriptionGet)
+						res, err := rest.DoRequestWithAuth("GET", getURL, nil, sessionID)
+						tests.ResultedWithNoErrorCheck(res, err)
+
+						v := &stripe.Sub{}
+						err = json.Unmarshal(res, v)
+						So(err, ShouldBeNil)
+						So(v.Status, ShouldEqual, "trialing")
+
+						_, err = stripeplan.Del(plan.ID)
+						So(err, ShouldBeNil)
+
+					})
+				})
+			})
+		})
+	})
+}
+
+// make sure we can subscribe to 7 days-free plan with more trial period
+func TestSubscribingToPaidPlanWithWithDifferentTrialPeriodThanDefault(t *testing.T) {
+	Convey("Given stub data", t, func() {
+		withTestServer(t, func(endpoint string) {
+			withStubData(endpoint, func(username, groupName, sessionID string) {
+				createURL := fmt.Sprintf("%s%s", endpoint, EndpointSubscriptionCreate)
+				deleteURL := fmt.Sprintf("%s%s", endpoint, EndpointSubscriptionDelete)
+				group, err := modelhelper.GetGroup(groupName)
+				tests.ResultedWithNoErrorCheck(group, err)
+
+				pp := &stripe.PlanParams{
+					Amount:        12345,
+					Interval:      stripeplan.Month,
+					IntervalCount: 1,
+					TrialPeriod:   1, // trial for one day
+					Name:          fmt.Sprintf("plan for %s", username),
+					Currency:      currency.USD,
+					ID:            fmt.Sprintf("plan_for_%s", username),
+					Statement:     "NAN-FREE",
+				}
+
+				plan, err := stripeplan.New(pp)
+				So(err, ShouldBeNil)
+
+				req, err := json.Marshal(&stripe.SubParams{
+					Customer: group.Payment.Customer.ID,
+					Plan:     plan.ID,
+					TrialEnd: time.Now().Add(time.Hour * 48).Unix(),
+				})
+				tests.ResultedWithNoErrorCheck(req, err)
+
+				sub, err := rest.DoRequestWithAuth("POST", createURL, req, sessionID)
+				tests.ResultedWithNoErrorCheck(sub, err)
+
+				v := &stripe.Sub{}
+				err = json.Unmarshal(sub, v)
+				So(err, ShouldBeNil)
+
+				So(v.TrialEnd, ShouldBeGreaterThan, time.Now().Add(time.Hour*24*time.Duration(pp.TrialPeriod)).Unix())
+
+				Convey("We should be able to cancel the subscription", func() {
+					res, err := rest.DoRequestWithAuth("DELETE", deleteURL, req, sessionID)
+					tests.ResultedWithNoErrorCheck(res, err)
+
+					v := &stripe.Sub{}
+					err = json.Unmarshal(res, v)
+					So(err, ShouldBeNil)
+					So(v.Status, ShouldEqual, "canceled")
+				})
+			})
+		})
+	})
+}
+
+// make sure we cant subscribe to a paid plan without a CC
+func TestSubscribingToPaidPlanWithWithNoTrialPeriodHavingNoCC(t *testing.T) {
+	Convey("Given stub data", t, func() {
+		withTestServer(t, func(endpoint string) {
+			withStubData(endpoint, func(username, groupName, sessionID string) {
+				withNonFreeTestPlan(func(planID string) {
+					group, err := modelhelper.GetGroup(groupName)
+					tests.ResultedWithNoErrorCheck(group, err)
+
+					Convey("We should be able to create a subscription", func() {
+						req, err := json.Marshal(&stripe.SubParams{
+							Customer: group.Payment.Customer.ID,
+							Plan:     planID,
+						})
+						tests.ResultedWithNoErrorCheck(req, err)
+
+						createURL := fmt.Sprintf("%s%s", endpoint, EndpointSubscriptionCreate)
+						_, err = rest.DoRequestWithAuth("POST", createURL, req, sessionID)
+						So(err, ShouldNotBeNil)
+					})
+				})
+			})
+		})
+	})
+}
