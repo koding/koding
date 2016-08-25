7d8c2cde373e3ec07921c6a18ad0d6b730be3bb9
Umut Sirin <umutsirin1@gmail.com>
2016-08-25 02:20:17 -0700

    storybook: add full width/height container

--- client/.storybook/centered.js
+++ client/.storybook/centered.js
@@ -2,18 +2,21 @@ import React from 'react'
 
 const style = {
   height: '100vh',
-  width: '100vw',
   position: 'absolute',
   top: 0,
-  left: 0,
+  left: 30,
   bottom: 0,
-  right: 0,
+  right: 30,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center'
 }
 
 export default (story) => (
-  <div style={style}>{story()}</div>
+  <div style={style}>
+    <div style={{width: '100%'}}>
+      {story()}
+    </div>
+  </div>
 )
 
