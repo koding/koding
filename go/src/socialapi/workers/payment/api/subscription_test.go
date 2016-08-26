69bc1c3660d4566cd8f1cb4f772e6b7f6546ce17
Cihangir SAVAS <cihangir@koding.com>
2016-08-26 18:13:54 +0300

    go/payment: at the end of trailing period ( when we got the invoice created event ) subscription is still in trailing state

--- go/src/socialapi/workers/payment/api/subscription_test.go
+++ go/src/socialapi/workers/payment/api/subscription_test.go
@@ -13,6 +13,7 @@ import (
 	stripe "github.com/stripe/stripe-go"
 	currency "github.com/stripe/stripe-go/currency"
 	stripeplan "github.com/stripe/stripe-go/plan"
+	stripesub "github.com/stripe/stripe-go/sub"
 )
 
 func TestCreateSubscription(t *testing.T) {
@@ -195,6 +196,75 @@ func TestSubscribingToPaidPlanWithWithNoTrialPeriodHavingNoCC(t *testing.T) {
 						_, err = rest.DoRequestWithAuth("POST", createURL, req, sessionID)
 						So(err, ShouldNotBeNil)
 					})
+				})
+			})
+		})
+	})
+}
+
+func TestAtTheEndOfTrialPeriodSubscriptionStatusIsStillTrialing(t *testing.T) {
+	Convey("Given stub data", t, func() {
+		withTestServer(t, func(endpoint string) {
+			withStubData(endpoint, func(username, groupName, sessionID string) {
+				withTestCreditCardToken(func(token string) {
+					req, err := json.Marshal(&stripe.CustomerParams{
+						Source: &stripe.SourceParams{
+							Token: token,
+						},
+					})
+					tests.ResultedWithNoErrorCheck(req, err)
+
+					res, err := rest.DoRequestWithAuth(
+						"POST",
+						fmt.Sprintf("%s%s", endpoint, EndpointCustomerUpdate),
+						req,
+						sessionID,
+					)
+					tests.ResultedWithNoErrorCheck(res, err)
+
+					createURL := fmt.Sprintf("%s%s", endpoint, EndpointSubscriptionCreate)
+					group, err := modelhelper.GetGroup(groupName)
+					tests.ResultedWithNoErrorCheck(group, err)
+
+					pp := &stripe.PlanParams{
+						Amount:        12345,
+						Interval:      stripeplan.Month,
+						IntervalCount: 1,
+						TrialPeriod:   1, // trial for one day
+						Name:          fmt.Sprintf("plan for %s", username),
+						Currency:      currency.USD,
+						ID:            fmt.Sprintf("plan_for_%s", username),
+						Statement:     "NAN-FREE",
+					}
+					plan, err := stripeplan.New(pp)
+					So(err, ShouldBeNil)
+					defer stripeplan.Del(plan.ID)
+
+					req, err = json.Marshal(&stripe.SubParams{
+						Customer: group.Payment.Customer.ID,
+						Plan:     plan.ID,
+					})
+					tests.ResultedWithNoErrorCheck(req, err)
+
+					res, err = rest.DoRequestWithAuth("POST", createURL, req, sessionID)
+					tests.ResultedWithNoErrorCheck(res, err)
+
+					sub := &stripe.Sub{}
+					err = json.Unmarshal(res, sub)
+					So(err, ShouldBeNil)
+
+					subParams := &stripe.SubParams{
+						Customer: group.Payment.Customer.ID,
+						Plan:     plan.ID,
+						TrialEnd: time.Now().UTC().Add(time.Second * 60 * 5).Unix(),
+					}
+					sub, err = stripesub.Update(sub.ID, subParams)
+					tests.ResultedWithNoErrorCheck(sub, err)
+
+					sub, err = stripesub.Get(sub.ID, nil)
+					tests.ResultedWithNoErrorCheck(sub, err)
+
+					So(sub.Status, ShouldEqual, "trialing")
 				})
 			})
 		})
