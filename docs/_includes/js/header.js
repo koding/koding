7f5f72f93d404a8db5704e5f98ebdb39dfcc7245
Sinan Yasar <sinan@koding.com>
2016-09-13 01:05:45 -0700

    jekyll: header component initial

---
+++ docs/_includes/js/header.js
@@ -0,0 +1,38 @@
+document.addEventListener('scroll', function(event){
+
+  header = document.querySelectorAll('header.Homepage-Header').item(0);
+  if (!header) return null;
+
+  if (document.body.scrollTop < 100) {
+    if (!document.body.classList.contains('StickyHeader')) {
+      return null;
+    }
+    document.body.classList.remove('StickyHeader');
+  } else if (document.body.scrollTop < 780) {
+    if (!document.body.classList.contains('StickyHeader')) {
+      return null;
+    }
+    document.body.classList.remove('in');
+    setTimeout(function(){
+      document.body.classList.remove('StickyHeader');
+    }, 400);
+  } else {
+
+    if (document.body.classList.contains('StickyHeader')) {
+      return null;
+    }
+    document.body.classList.add('StickyHeader');
+    setTimeout(function(){
+      document.body.classList.add('in');
+    }, 400);
+  }
+
+});
+
+document.querySelector('.burger').onclick = function () {
+  if (document.body.classList.contains('burgerToggle')) {
+    document.body.classList.remove('burgerToggle');
+  } else {
+    document.body.classList.add('burgerToggle');
+  }
+}
