koding/koding

commit d0f097d6283a9afc603a8e3b24aaa96035ef2bb6

provider: refactor Info method to use concrete response value

go/src/koding/kites/kloud/provider/aws/stackplan.go
@@ -119,8 +119,6 @@ func (s *Stack) InjectAWSData() (stackplan.KiteMap, error) {
 			Hostname: s.Req.Username, // no typo here. hostname = username
 		}
 
-		s.Builder.InterpolateField(instance, resourceName, "user_data")
-
 		if b, ok := instance["debug"].(bool); ok && b {
 			s.Debug = true
 			delete(instance, "debug")
@@ -141,6 +139,8 @@ func (s *Stack) InjectAWSData() (stackplan.KiteMap, error) {
 		}
 
 		instance["user_data"] = string(userdata)
+
+		s.Builder.InterpolateField(instance, resourceName, "user_data")
 
 		// create independent kiteKey for each machine and create a Terraform
 		// lookup map, which is used in conjuctuon with the `count.index`

go/build.sh
@@ -15,73 +15,73 @@ koding-go-install() {
 }
 
 export COMMANDS=(
-  koding/broker
-  koding/rerouting
-  koding/kites/kontrol
-  koding/kites/kloud
-  koding/kites/kloud/kloudctl
-  koding/kites/cmd/terraformer
-  koding/kites/cmd/tunnelserver
-  koding/workers/guestcleanerworker
-  koding/go-webserver
-  koding/workers/cmd/tunnelproxymanager
-  koding/vmwatcher
-  koding/workers/janitor
-  koding/workers/gatheringestor
-  koding/workers/appstoragemigrator
-  koding/kites/kloud/cleaners/cmd/cleaner
-  koding/kites/kloud/scripts/userdebug
-  koding/kites/kloud/scripts/sl
-  koding/klient
-  koding/scripts/multiec2ssh
+	koding/broker
+	koding/rerouting
+	koding/kites/kontrol
+	koding/kites/kloud
+	koding/kites/kloud/kloudctl
+	koding/kites/cmd/terraformer
+	koding/kites/cmd/tunnelserver
+	koding/workers/guestcleanerworker
+	koding/go-webserver
+	koding/workers/cmd/tunnelproxymanager
+	koding/vmwatcher
+	koding/workers/janitor
+	koding/workers/gatheringestor
+	koding/workers/appstoragemigrator
+	koding/kites/kloud/cleaners/cmd/cleaner
+	koding/kites/kloud/scripts/userdebug
+	koding/kites/kloud/scripts/sl
+	koding/klient
+	koding/scripts/multiec2ssh
 
-  socialapi/workers/api
-  socialapi/workers/cmd/notification
-  socialapi/workers/cmd/pinnedpost
-  socialapi/workers/cmd/popularpost
-  socialapi/workers/cmd/trollmode
-  socialapi/workers/cmd/populartopic
-  socialapi/workers/cmd/realtime
-  socialapi/workers/cmd/realtime/gatekeeper
-  socialapi/workers/cmd/realtime/dispatcher
-  socialapi/workers/cmd/topicfeed
-  socialapi/workers/cmd/migrator
-  socialapi/workers/cmd/sitemap/sitemapfeeder
-  socialapi/workers/cmd/sitemap/sitemapgenerator
-  socialapi/workers/cmd/sitemap/sitemapinitializer
-  socialapi/workers/cmd/algoliaconnector
-  socialapi/workers/cmd/algoliaconnector/deletedaccountremover
-  socialapi/workers/cmd/topicmoderation
-  socialapi/workers/cmd/collaboration
-  socialapi/workers/cmd/email/activityemail
-  socialapi/workers/cmd/email/dailyemail
-  socialapi/workers/cmd/email/privatemessageemailfeeder
-  socialapi/workers/cmd/email/privatemessageemailsender
-  socialapi/workers/cmd/email/emailsender
-  socialapi/workers/cmd/team
-  socialapi/workers/cmd/integration/webhook
-  socialapi/workers/algoliaconnector/tagmigrator
-  socialapi/workers/algoliaconnector/contentmigrator
-  socialapi/workers/cmd/integration/eventsender
-  socialapi/workers/cmd/integration/webhookmiddleware
+	socialapi/workers/api
+	socialapi/workers/cmd/notification
+	socialapi/workers/cmd/pinnedpost
+	socialapi/workers/cmd/popularpost
+	socialapi/workers/cmd/trollmode
+	socialapi/workers/cmd/populartopic
+	socialapi/workers/cmd/realtime
+	socialapi/workers/cmd/realtime/gatekeeper
+	socialapi/workers/cmd/realtime/dispatcher
+	socialapi/workers/cmd/topicfeed
+	socialapi/workers/cmd/migrator
+	socialapi/workers/cmd/sitemap/sitemapfeeder
+	socialapi/workers/cmd/sitemap/sitemapgenerator
+	socialapi/workers/cmd/sitemap/sitemapinitializer
+	socialapi/workers/cmd/algoliaconnector
+	socialapi/workers/cmd/algoliaconnector/deletedaccountremover
+	socialapi/workers/cmd/topicmoderation
+	socialapi/workers/cmd/collaboration
+	socialapi/workers/cmd/email/activityemail
+	socialapi/workers/cmd/email/dailyemail
+	socialapi/workers/cmd/email/privatemessageemailfeeder
+	socialapi/workers/cmd/email/privatemessageemailsender
+	socialapi/workers/cmd/email/emailsender
+	socialapi/workers/cmd/team
+	socialapi/workers/cmd/integration/webhook
+	socialapi/workers/algoliaconnector/tagmigrator
+	socialapi/workers/algoliaconnector/contentmigrator
+	socialapi/workers/cmd/integration/eventsender
+	socialapi/workers/cmd/integration/webhookmiddleware
 
-  vendor/github.com/koding/kite/kitectl
-  vendor/github.com/canthefason/go-watcher
-  vendor/github.com/mattes/migrate
-  vendor/github.com/alecthomas/gocyclo
-  vendor/github.com/remyoudompheng/go-misc/deadcode
-  vendor/github.com/jteeuwen/go-bindata/go-bindata
+	vendor/github.com/koding/kite/kitectl
+	vendor/github.com/canthefason/go-watcher
+	vendor/github.com/mattes/migrate
+	vendor/github.com/alecthomas/gocyclo
+	vendor/github.com/remyoudompheng/go-misc/deadcode
+	vendor/github.com/jteeuwen/go-bindata/go-bindata
 )
 
 export TERRAFORM_COMMANDS=(
-  vendor/github.com/hashicorp/terraform/builtin/bins/provider-aws
-  vendor/github.com/hashicorp/terraform/builtin/bins/provider-terraform
-  vendor/github.com/hashicorp/terraform/builtin/bins/provider-null
-  vendor/github.com/hashicorp/terraform/builtin/bins/provisioner-file
-  vendor/github.com/hashicorp/terraform/builtin/bins/provisioner-local-exec
-  vendor/github.com/hashicorp/terraform/builtin/bins/provisioner-remote-exec
-  koding/kites/cmd/provider-vagrant
-  vendor/github.com/koding/terraform-provider-github/cmd/provider-github
+	vendor/github.com/hashicorp/terraform/builtin/bins/provider-aws
+	vendor/github.com/hashicorp/terraform/builtin/bins/provider-terraform
+	vendor/github.com/hashicorp/terraform/builtin/bins/provider-null
+	vendor/github.com/hashicorp/terraform/builtin/bins/provisioner-file
+	vendor/github.com/hashicorp/terraform/builtin/bins/provisioner-local-exec
+	vendor/github.com/hashicorp/terraform/builtin/bins/provisioner-remote-exec
+	koding/kites/cmd/provider-vagrant
+	vendor/github.com/koding/terraform-provider-github/cmd/provider-github
 )
 
 # source configuration for kloud providers

go/src/koding/kites/kloud/kloudctl/command/info.go
@@ -44,7 +44,7 @@ func (i *Info) SingleMachine(id string, k *kite.Client) (string, error) {
 		return "", err
 	}
 
-	return result.State, nil
+	return result.State.String(), nil
 }
 
 func (i *Info) Action(args []string) error {

go/src/koding/kites/kloud/provider/aws/info.go
@@ -7,13 +7,14 @@ import (
 	"koding/kites/kloud/api/amazon"
 	"koding/kites/kloud/klient"
 	"koding/kites/kloud/machinestate"
+	"koding/kites/kloud/stack"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/koding/kite"
 	"golang.org/x/net/context"
 )
 
-func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
+func (m *Machine) Info(ctx context.Context) (*stack.InfoResponse, error) {
 	dbState := m.State()
 	resultState := dbState
 	reason := "not known yet"
@@ -21,8 +22,8 @@ func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
 	// return lazily if it's a progress state, such as "Building, Stopping,
 	// etc.."
 	if dbState.InProgress() {
-		return map[string]string{
-			"State": dbState.String(),
+		return &stack.InfoResponse{
+			State: dbState,
 		}, nil
 	}
 
@@ -74,8 +75,8 @@ func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
 			reason = "Klient is active and healthy."
 			resultState = machinestate.Running
 
-			return map[string]string{
-				"State": resultState.String(),
+			return &stack.InfoResponse{
+				State: resultState,
 			}, nil
 		}
 
@@ -87,15 +88,15 @@ func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
 			if dbState == machinestate.Stopped {
 				m.Log.Debug("Info result: Returning db state '%s' because the klient is not available. Username: %s",
 					dbState, m.User.Name)
-				return map[string]string{
-					"State": machinestate.Stopped.String(),
+				return &stack.InfoResponse{
+					State: machinestate.Stopped,
 				}, nil
 			}
 		}
 	}
 
 	m.Log.Debug("Info result: '%s'. Username: %s", resultState, m.User.Name)
-	return map[string]string{
-		"State": resultState.String(),
+	return &stack.InfoResponse{
+		State: resultState,
 	}, nil
 }

go/src/koding/kites/kloud/provider/disabled/provider.go
@@ -58,7 +58,7 @@ func (p *Provider) Destroy(context.Context) error {
 }
 
 // Info implements the stack.Infoer interface.
-func (p *Provider) Info(context.Context) (map[string]string, error) {
+func (p *Provider) Info(context.Context) (*stack.InfoResponse, error) {
 	return nil, p.err
 }
 

go/src/koding/kites/kloud/provider/vagrant/info.go
@@ -7,24 +7,21 @@ import (
 	"koding/db/mongodb/modelhelper"
 	"koding/kites/kloud/klient"
 	"koding/kites/kloud/machinestate"
+	"koding/kites/kloud/stack"
 
 	"github.com/koding/kite"
 	"golang.org/x/net/context"
 )
 
-func toObject(s machinestate.State) map[string]string {
-	return map[string]string{
-		"State": s.String(),
-	}
-}
-
-func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
+func (m *Machine) Info(ctx context.Context) (*stack.InfoResponse, error) {
 	dbState := m.State()
 	resultState := dbState
 	reason := "not known yet"
 
 	if dbState.InProgress() {
-		return toObject(dbState), nil
+		return &stack.InfoResponse{
+			State: dbState,
+		}, nil
 	}
 
 	defer func() {
@@ -44,7 +41,9 @@ func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
 		reason = "Klient is active and healthy."
 		resultState = machinestate.Running
 
-		return toObject(resultState), nil
+		return &stack.InfoResponse{
+			State: resultState,
+		}, nil
 	case err == klient.ErrDialingFailed || err == kite.ErrNoKitesAvailable:
 		m.Log.Debug("Klient is not registered to Kontrol. Err: %s", err)
 
@@ -54,7 +53,9 @@ func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
 		m.Log.Debug("Info result: Returning db state '%s' because the klient"+
 			" is not available. Username: %s", dbState, m.User.Name)
 
-		return toObject(resultState), nil
+		return &stack.InfoResponse{
+			State: resultState,
+		}, nil
 	}
 
 	reason = "Klient is not reachable"
@@ -75,8 +76,9 @@ func (m *Machine) Info(ctx context.Context) (map[string]string, error) {
 		resultState = machinestate.Terminated
 	}
 
-	return toObject(resultState), nil
-
+	return &stack.InfoResponse{
+		State: resultState,
+	}, nil
 }
 
 func (m *Machine) fixInconsistentState(actual, db machinestate.State, reason string) {

go/src/koding/kites/kloud/stack/methods.go
@@ -3,27 +3,27 @@ package stack
 import (
 	"errors"
 	"fmt"
+	"strings"
+
 	"koding/db/mongodb/modelhelper"
 	"koding/kites/kloud/contexthelper/request"
 	"koding/kites/kloud/contexthelper/session"
 	"koding/kites/kloud/machinestate"
-	"strings"
 
 	"github.com/koding/kite"
-	"github.com/mitchellh/mapstructure"
 	"golang.org/x/net/context"
 )
 
 // InfoResponse is returned from a info method
 type InfoResponse struct {
 	// State defines the state of the machine
-	State string
+	State machinestate.State `json:"state"`
 
 	// Name defines the name of the machine.
-	Name string
+	Name string `json:"name,omitempty"`
 
 	// InstanceType defines the type of the given machine
-	InstanceType string
+	InstanceType string `json:"instanceType,omitempty"`
 }
 
 func (k *Kloud) Info(r *kite.Request) (interface{}, error) {
@@ -39,7 +39,7 @@ func (k *Kloud) Info(r *kite.Request) (interface{}, error) {
 
 	if stater.State() == machinestate.NotInitialized {
 		return &InfoResponse{
-			State: machinestate.NotInitialized.String(),
+			State: machinestate.NotInitialized,
 			Name:  "not-initialized-instance",
 		}, nil
 	}
@@ -50,18 +50,13 @@ func (k *Kloud) Info(r *kite.Request) (interface{}, error) {
 	}
 
 	ctx := request.NewContext(context.Background(), r)
-	infoData, err := i.Info(ctx)
+	response, err := i.Info(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	var response *InfoResponse
-	if err := mapstructure.Decode(infoData, &response); err != nil {
-		return nil, errors.New("info response error: " + err.Error())
-	}
-
-	if response.State == machinestate.Unknown.String() {
-		response.State = stater.State().String()
+	if response.State == machinestate.Unknown {
+		response.State = stater.State()
 	}
 
 	return response, nil

go/src/koding/kites/kloud/stack/provider.go
@@ -85,7 +85,9 @@ type Stack interface {
 type Machine interface {
 	Start(context.Context) error
 	Stop(context.Context) error
-	Info(context.Context) (map[string]string, error)
+	Info(context.Context) (*InfoResponse, error)
+	State() machinestate.State
+	ProviderName() string
 }
 
 type Builder interface {
@@ -117,7 +119,7 @@ type Restarter interface {
 }
 
 type Infoer interface {
-	Info(ctx context.Context) (map[string]string, error)
+	Info(ctx context.Context) (*InfoResponse, error)
 }
 
 type Snapshotter interface {

commit 7fb68ae7c05efe6091db37a36ba5c82329d482b6

kloud: fix user-data interpolations
Broken by:

    https://github.com/koding/koding/commit/d0f097d6283a9afc603a8e3b24aaa96035ef2bb6

Fixes:

    [10:36:52] Starting to process the template...
    [10:36:52] Something went wrong with the template:
    [10:36:52] Cloudinit template is not a valid YAML file. Debugfile:
    /tmp/kloud-cloudinit539095275 (error code: 900)
    [10:36:52] Parsing failed, please check your template and try again

go/src/koding/kites/kloud/provider/aws/stackplan.go
@@ -119,6 +119,8 @@ func (s *Stack) InjectAWSData() (stackplan.KiteMap, error) {
 			Hostname: s.Req.Username, // no typo here. hostname = username
 		}
 
+		s.Builder.InterpolateField(instance, resourceName, "user_data")
+
 		if b, ok := instance["debug"].(bool); ok && b {
 			s.Debug = true
 			delete(instance, "debug")
@@ -139,8 +141,6 @@ func (s *Stack) InjectAWSData() (stackplan.KiteMap, error) {
 		}
 
 		instance["user_data"] = string(userdata)
-
-		s.Builder.InterpolateField(instance, resourceName, "user_data")
 
 		// create independent kiteKey for each machine and create a Terraform
 		// lookup map, which is used in conjuctuon with the `count.index`