d6f5e9d41b9bf8b4d362da5c98807f42530c7067
Rafal Jeczalik <rjeczalik@gmail.com>
2016-10-11 14:15:59 +0200

    object: ensure arguments are of the same type in Merge

--- go/src/koding/kites/kloud/utils/object/merge.go
+++ go/src/koding/kites/kloud/utils/object/merge.go
@@ -4,12 +4,28 @@ import "reflect"
 
 // Merge overwrites fields in v1 with a value of complementary
 // field in v2, if the field in v2 is non-empty.
+//
+// TODO(rjeczalik): Does not not merge struct fields
+// recursively yet.
 func Merge(v1, v2 interface{}) {
-	vv2 := reflect.ValueOf(v2).Elem()
+	vv1 := reflect.ValueOf(v1)
+	if vv1.Kind() == reflect.Ptr {
+		vv1 = vv1.Elem()
+	}
+
+	vv2 := reflect.ValueOf(v2)
+	if vv2.Kind() == reflect.Ptr {
+		vv2 = vv2.Elem()
+	}
+
 	if !vv2.IsValid() {
 		return
 	}
-	vv1 := reflect.ValueOf(v1).Elem()
+
+	if vv1.Type() != vv2.Type() {
+		return
+	}
+
 	for i := 0; i < vv1.NumField(); i++ {
 		field := vv2.Field(i)
 		var empty bool
