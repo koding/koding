d04759eb222a1306ffc536f0dc86355de364660f
Sinan Yasar <sinan@koding.com>
2016-10-14 16:51:23 -0700

    client: jekyll: make the default avatar img protocolless

--- docs/_includes/js/blog-home.js
+++ docs/_includes/js/blog-home.js
@@ -3,7 +3,7 @@ var ready = function($){
   $authors.each(function(index){
     var email = $(this).attr('alt'),
         $img  = $(this).find('img'),
-        default_img = encodeURIComponent("http://koding-cdn.s3.amazonaws.com/blog-default-user-avatar.png"),
+        default_img = encodeURIComponent("//koding-cdn.s3.amazonaws.com/blog-default-user-avatar.png"),
         gravatar_path = "//gravatar.com/avatar/";
 
     var src = gravatar_path + md5(email) + "?d=" + default_img + "&s=100";
