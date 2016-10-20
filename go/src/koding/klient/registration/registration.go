f647cc144fb6e8716e63537d9669e3e0d0b0bb5e
Rafal Jeczalik <rjeczalik@gmail.com>
2016-10-20 19:48:51 +0200

    klient/registration: set defaults for kontrol URL and kite home

--- go/src/koding/klient/registration/registration.go
+++ go/src/koding/klient/registration/registration.go
@@ -46,6 +46,14 @@ func Register(kontrolURL, kiteHome, username, token string, debug bool) error {
 		k.SetLogLevel(kite.DEBUG)
 	}
 
+	if kiteHome == "" {
+		kiteHome = konfig.Konfig.KiteHome()
+	}
+
+	if kontrolURL == "" {
+		kontrolURL = konfig.Konfig.KontrolURL
+	}
+
 	// Production Koding servers are only working over HTTP
 	k.Config.Transport = config.XHRPolling
 
@@ -63,7 +71,7 @@ func Register(kontrolURL, kiteHome, username, token string, debug bool) error {
 	}
 
 	kontrol := k.NewClient(kontrolURL)
-	if err := kontrol.Dial(); err != nil {
+	if err := kontrol.DialTimeout(30 * time.Second); err != nil {
 		return err
 	}
 	defer kontrol.Close()
