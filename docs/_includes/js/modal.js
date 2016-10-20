fc9e4b48a4577ad1c766e5227bc2140345af87dc
Andrew Casal <andrewjcasal@gmail.com>
2016-10-20 13:12:39 -0700

    youtube optional, css changes

--- docs/_includes/js/modal.js
+++ docs/_includes/js/modal.js
@@ -1,6 +1,7 @@
 (function() {
 
-	var showModal = document.querySelectorAll('.showModal');
+	var showModal = document.querySelectorAll('.showModal'),
+      youtube   = document.getElementsByTagName("iframe")[0];
 
 	var initialize = function() {
     [].forEach.call(showModal, function(el) {
@@ -13,13 +14,18 @@
 
         overlay.classList.add('isShown');
         modal.classList.add('isShown');
-        toggleVideo('show');
+
+        if (youtube) {
+          toggleVideo(youtube, 'show');
+        }
 
         [].forEach.call([close, overlay], function(el) {
           el.addEventListener('click', function(e) {
             overlay.classList.remove('isShown');
             modal.classList.remove('isShown');
-            toggleVideo('hide');
+            if (youtube) {
+              toggleVideo(youtube, 'hide');
+            }
           })
         });
       });
@@ -27,8 +33,8 @@
 	}
   initialize();
 
-  var toggleVideo = function(state) {
-    var iframe = document.getElementsByTagName("iframe")[0].contentWindow;
+  var toggleVideo = function(youtube, state) {
+    var iframe = youtube.contentWindow;
     var func = state == 'hide' ? 'pauseVideo' : 'playVideo';
     iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
   }
