bbd968f4151dbcfbe1c4531d28be859618a25b76
mehmetalisavas <mehmetali.savas@yandex.com>
2016-10-20 15:45:15 +0300

    socialapi/popular: remove main functions related with populars

--- go/src/socialapi/workers/api/main.go
+++ go/src/socialapi/workers/api/main.go
@@ -15,7 +15,6 @@ import (
 	"socialapi/workers/api/modules/notificationsetting"
 	"socialapi/workers/api/modules/participant"
 	"socialapi/workers/api/modules/pinnedactivity"
-	"socialapi/workers/api/modules/popular"
 	"socialapi/workers/api/modules/privatechannel"
 	"socialapi/workers/api/modules/reply"
 	collaboration "socialapi/workers/collaboration/api"
@@ -75,7 +74,6 @@ func main() {
 	messagelist.AddHandlers(m)
 	participant.AddHandlers(m)
 	pinnedactivity.AddHandlers(m)
-	popular.AddHandlers(m)
 	privatechannel.AddHandlers(m)
 	reply.AddHandlers(m)
 	notificationsetting.AddHandlers(m)
