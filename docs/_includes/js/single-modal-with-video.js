3f841946970bf60ad3c4320ae094b456fb45c41a
Andrew Casal <andrewjcasal@gmail.com>
2016-11-01 17:58:51 -0700

    videos progress

--- docs/_includes/js/single-modal-with-video.js
+++ docs/_includes/js/single-modal-with-video.js
@@ -1,19 +1,32 @@
 (function() {
 
   var showModal = document.querySelectorAll('.showModal'),
-      youtube   = document.getElementsByTagName("iframe")[0];
+      youtube   = document.getElementsByTagName("iframe")[0],
+      youtube_id;
 
   var initialize = function() {
     [].forEach.call(showModal, function(el) {
       el.addEventListener('click', function(e) {
         var close, overlay, modal;
 
-        overlay = document.querySelector('.Overlay');
-        modal   = document.querySelector('.Modal');
-        close   = document.querySelector('.u-modalClose');
+        overlay = document.createElement("div");
+        overlay.className = "Overlay";
 
+        modal   = document.createElement('div');
+        modal.className = "Modal";
+        modal.innerHTML = '<a href="#" class="u-icon u-modalClose"></a>';
+
+        close   = modal.querySelector('.u-modalClose');
+
+        youtube_id = e.currentTarget.getAttribute('data-youtube-id');
+        modal.innerHTML += '<iframe width="656" height="369" src="https://www.youtube.com/embed/' + youtube_id + '?enablejsapi=1"></iframe>';
+
+        document.getElementsByTagName('body')[0].appendChild(overlay);
+        document.getElementsByTagName('body')[0].appendChild(modal);
         overlay.classList.add('isShown');
         modal.classList.add('isShown');
+
+        youtube = document.getElementsByTagName("iframe")[0];
 
         if (youtube) {
           toggleVideo(youtube, 'show');
@@ -21,8 +34,9 @@
 
         [].forEach.call([close, overlay], function(el) {
           el.addEventListener('click', function(e) {
-            overlay.classList.remove('isShown');
-            modal.classList.remove('isShown');
+            overlay.remove();
+            modal.remove();
+
             if (youtube) {
               toggleVideo(youtube, 'hide');
             }
