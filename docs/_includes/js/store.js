2f81fd16027b6033c37f6abb4555aa0bed80de55
Andrew Casal <andrewjcasal@gmail.com>
2016-11-02 16:11:57 -0700

    Remove video functionality from modal file, scale up

--- docs/_includes/js/store.js
+++ docs/_includes/js/store.js
@@ -1,4 +1,6 @@
 (function() {
+  window.LANDING_UTILS = window.LANDING_UTILS || {};
+
   var searchBar      = document.querySelector('.SearchBar'),
       searchBarInput = document.querySelector('.SearchBar input'),
       searchResults  = document.querySelector('.SearchBar-resultsData'),
@@ -8,6 +10,8 @@
       dropdownOptions = document.querySelectorAll('.Dropdown .Dropdown-options a'),
 
       removeIcon = document.querySelector('.remove-icon'),
+
+      modalContent = document.querySelector('.modal-content');
 
       itemDetailsTabs = document.querySelectorAll('.ItemDetails ul li');
 
@@ -47,6 +51,13 @@
       [].forEach.call(itemDetailsTabs, function(el) {
         el.addEventListener('click', selectTab);
       });
+    }
+
+    if (modalContent && LANDING_UTILS.modal) {
+      var modalOptions = {};
+      modalOptions.content = modalContent.innerHTML;
+
+      LANDING_UTILS.modal(modalOptions);
     }
   }
 
---
+++ docs/_includes/js/training.js
@@ -0,0 +1,32 @@
+(function() {
+
+  window.LANDING_UTILS = window.LANDING_UTILS || {};
+
+  var modalOptions = {}, player;
+
+  modalOptions.onLoaded = function(clickEvent) {
+    var youtube_id = clickEvent.currentTarget.getAttribute('data-youtube-id');
+
+    player = new YT.Player('ModalBody', {
+        width: 656,
+        height: 369,
+        videoId: youtube_id,
+        events: {
+          'onReady': onPlayerReady
+        }
+    });
+
+    document.querySelector('#Modal').classList.add('Modal--withVideo');
+  }
+
+  modalOptions.onDestroy = function() {
+    player.destroy();
+  }
+
+  var onPlayerReady = function(event) {
+    event.target.playVideo();
+  }
+
+  var modal = LANDING_UTILS.modal(modalOptions);
+
+})();
