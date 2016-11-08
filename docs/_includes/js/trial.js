781270fd36e10da2669f771a1400471b33d4439c
Andrew Casal <andrewjcasal@gmail.com>
2016-11-08 11:42:05 -0800

    move code

--- docs/_includes/js/trial.js
+++ docs/_includes/js/trial.js
@@ -1,6 +1,8 @@
 (function() {
 
-  var modalOptions = {}, player;
+  var modalOptions = {}, player,
+      contact      = document.querySelectorAll('.contact'),
+      Intercom     = Intercom || false;
 
   modalOptions.onLoaded = function(clickEvent) {
 
@@ -25,5 +27,15 @@
   }
 
   var modal = LANDING_UTILS.modal(modalOptions);
+
+  [].forEach.call(contact, function(el) {
+    el.addEventListener('click', function(evt) {
+      evt.preventDefault();
+
+      if (Intercom) {
+        Intercom('show')
+      }
+    });
+  });
 
 })();
