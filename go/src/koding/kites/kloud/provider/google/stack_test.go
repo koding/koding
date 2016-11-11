6f64d5efff41e7e0fa3fe22b3578cd6a023c2e00
Pawel Knap <pawelknap88@gmail.com>
2016-11-11 22:31:34 +0100

    provider/google: add unit tests for flatten function

--- go/src/koding/kites/kloud/provider/google/stack_test.go
+++ go/src/koding/kites/kloud/provider/google/stack_test.go
@@ -6,67 +6,152 @@ import (
 )
 
 func TestAddPublicKey(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		Name      string
 		Metadata  map[string]interface{}
 		User      string
 		PublicKey string
 		Expected  map[string]interface{}
 	}{
-		{
-			Name: `no user SSH keys`,
+		"no user SSH keys": {
 			Metadata: map[string]interface{}{
-				`unrelated`: 5,
+				"unrelated": 5,
 			},
-			User:      `user`,
-			PublicKey: `pk`,
+			User:      "user",
+			PublicKey: "pk",
 			Expected: map[string]interface{}{
-				`ssh-keys`:  `user:pk`,
-				`unrelated`: 5,
+				"ssh-keys":  "user:pk",
+				"unrelated": 5,
 			},
 		},
-		{
-			Name: `user SSH keys new format`,
+		"user SSH keys new format": {
 			Metadata: map[string]interface{}{
-				`ssh-keys`: `user_custom:pk_custom`,
+				"ssh-keys": "user_custom:pk_custom",
 			},
-			User:      `user`,
-			PublicKey: `pk`,
+			User:      "user",
+			PublicKey: "pk",
 			Expected: map[string]interface{}{
-				`ssh-keys`: `user:pk\nuser_custom:pk_custom`,
+				"ssh-keys": "user:pk\\nuser_custom:pk_custom",
 			},
 		},
-		{
-			Name: `user SSH keys deprecated format`,
+		"user SSH keys deprecated format": {
 			Metadata: map[string]interface{}{
-				`sshKeys`: `user_custom:pk_custom`,
+				"sshKeys": "user_custom:pk_custom",
 			},
-			User:      `user`,
-			PublicKey: `pk`,
+			User:      "user",
+			PublicKey: "pk",
 			Expected: map[string]interface{}{
-				`sshKeys`: `user:pk\nuser_custom:pk_custom`,
+				"sshKeys": "user:pk\\nuser_custom:pk_custom",
 			},
 		},
-		{
-			Name: `user SSH keys both formats`,
+		"user SSH keys both formats": {
 			Metadata: map[string]interface{}{
-				`ssh-keys`: `user_custom_new:pk_custom_new`,
-				`sshKeys`:  `user_custom_old:pk_custom_old`,
+				"ssh-keys": "user_custom_new:pk_custom_new",
+				"sshKeys":  "user_custom_old:pk_custom_old",
 			},
-			User:      `user`,
-			PublicKey: `pk`,
+			User:      "user",
+			PublicKey: "pk",
 			Expected: map[string]interface{}{
-				`ssh-keys`: `user:pk\nuser_custom_new:pk_custom_new`,
-				`sshKeys`:  `user_custom_old:pk_custom_old`,
+				"ssh-keys": "user:pk\\nuser_custom_new:pk_custom_new",
+				"sshKeys":  "user_custom_old:pk_custom_old",
 			},
 		},
 	}
 
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
 			metadata := addPublicKey(test.Metadata, test.User, test.PublicKey)
 			if !reflect.DeepEqual(metadata, test.Expected) {
-				t.Fatalf(`want metadata = %#v; got %#v`, test.Expected, metadata)
+				t.Fatalf("want metadata = %#v; got %#v", test.Expected, metadata)
+			}
+		})
+	}
+}
+
+func TestFlatten(t *testing.T) {
+	tests := map[string]struct {
+		Data     []map[string]interface{}
+		Expected map[string]interface{}
+	}{
+		"nil data": {
+			Data:     nil,
+			Expected: map[string]interface{}{},
+		},
+		"nil map": {
+			Data: []map[string]interface{}{
+				nil,
+				{
+					"user-data": "data",
+				},
+			},
+			Expected: map[string]interface{}{
+				"user-data": "data",
+			},
+		},
+		"single map": {
+			Data: []map[string]interface{}{
+				{
+					"user-data": "data",
+				},
+			},
+			Expected: map[string]interface{}{
+				"user-data": "data",
+			},
+		},
+		"redefined map fields": {
+			Data: []map[string]interface{}{
+				{
+					"user-data": "first",
+				},
+				{
+					"user-data": "second",
+				},
+			},
+			Expected: map[string]interface{}{
+				"user-data": "first\nsecond",
+			},
+		},
+		"different map fields": {
+			Data: []map[string]interface{}{
+				{
+					"aaaa": "first",
+				},
+				{
+					"bbbb": "second",
+				},
+			},
+			Expected: map[string]interface{}{
+				"aaaa": "first",
+				"bbbb": "second",
+			},
+		},
+		"map fields mix": {
+			Data: []map[string]interface{}{
+				{
+					"aaaa": "first",
+				},
+				{
+					"bbbb": "second",
+				},
+				{
+					"aaaa": "A",
+					"bbbb": "B",
+					"cccc": "C",
+				},
+			},
+			Expected: map[string]interface{}{
+				"aaaa": "first\nA",
+				"bbbb": "second\nB",
+				"cccc": "C",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			flattened := flatten(test.Data)
+			if !reflect.DeepEqual(flattened, test.Expected) {
+				t.Fatalf("want flattened = %#v; got %#v", test.Expected, flattened)
 			}
 		})
 	}
