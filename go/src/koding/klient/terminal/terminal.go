8efae178f58ba7db992f9d6c93596fa0ea5041d3
Rafal Jeczalik <rjeczalik@gmail.com>
2016-11-14 04:44:17 +0100

    klient/terminal: do not use sudo if not available

--- go/src/koding/klient/terminal/terminal.go
+++ go/src/koding/klient/terminal/terminal.go
@@ -255,7 +255,13 @@ func (t *Terminal) Connect(r *kite.Request) (interface{}, error) {
 	}
 
 	args = append(args, command.Args...)
-	cmd := exec.Command("/usr/bin/sudo", args...)
+	var cmd *exec.Cmd
+
+	if _, err := exec.LookPath("sudo"); err == nil {
+		cmd = exec.Command("/usr/bin/sudo", args...)
+	} else {
+		cmd = exec.Command(args[1], args[2:]...)
+	}
 
 	// For test use this, sudo is not going to work
 	// cmd := exec.Command(command.Name, command.Args...)
