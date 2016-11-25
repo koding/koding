5d126c7842f46fb7fa6a45e514249b1366e54dc2
Cihangir SAVAS <cihangir@koding.com>
2016-11-25 23:36:37 +0300

    socialapi/payment: do not allow customer deletion via API

--- go/src/socialapi/workers/payment/api/customer.go
+++ go/src/socialapi/workers/payment/api/customer.go
@@ -11,8 +11,11 @@ import (
 	stripe "github.com/stripe/stripe-go"
 )
 
-// DeleteCustomer deletes customer for a group
+// DeleteCustomer deletes customer for a group. Here for symmetry.
 func DeleteCustomer(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
+	// do not allow customer deletion, causes losing track of transactions.
+	return http.StatusForbidden, nil, nil, nil
+
 	if err := checkContext(context); err != nil {
 		return response.NewBadRequest(err)
 	}
