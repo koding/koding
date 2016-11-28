d45d71c297a0edde1f71c13720e16928af5c1361
Cihangir SAVAS <cihangir@koding.com>
2016-11-28 14:22:22 +0300

    socialapi/payment: add new handler for checking if the group has a source or not

--- go/src/socialapi/workers/payment/api/card.go
+++ go/src/socialapi/workers/payment/api/card.go
@@ -18,4 +18,29 @@ func DeleteCreditCard(u *url.URL, h http.Header, _ interface{}, context *models.
 	return response.HandleResultAndError(
 		payment.DeleteCreditCardForGroup(context.GroupName),
 	)
+// HasCreditCard returns the existance status of group's credit card
+func HasCreditCard(u *url.URL, h http.Header, _ interface{}, context *models.Context) (int, http.Header, interface{}, error) {
+	if !context.IsLoggedIn() {
+		return response.NewBadRequest(models.ErrNotLoggedIn)
+	}
+
+	group, err := modelhelper.GetGroup(context.GroupName)
+	if err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	if group.Payment.Customer.ID == "" {
+		return response.NewNotFound()
+	}
+
+	err = payment.CheckCustomerHasSource(group.Payment.Customer.ID)
+	if err == payment.ErrCustomerSourceNotExists {
+		return response.NewNotFound()
+	}
+
+	if err != nil {
+		return response.NewBadRequest(err)
+	}
+
+	return response.NewDefaultOK()
 }
