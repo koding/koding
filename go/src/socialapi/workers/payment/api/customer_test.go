3d6bda25fa0f7c116020f95658b87491d2457f54
Cihangir SAVAS <cihangir@koding.com>
2016-11-29 16:08:08 +0300

    socialapi/payment: add test case for Amount, Subtotal And the Total

--- go/src/socialapi/workers/payment/api/customer_test.go
+++ go/src/socialapi/workers/payment/api/customer_test.go
@@ -10,6 +10,8 @@ import (
 
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/customer"
+	"github.com/stripe/stripe-go/invoice"
 )
 
 func TestCustomer(t *testing.T) {
@@ -92,6 +94,76 @@ func TestCouponApply(t *testing.T) {
 							So(v.Discount.Coupon.ID, ShouldEqual, couponID)
 							So(v.Discount.Coupon.Valid, ShouldBeTrue)
 							So(v.Discount.Coupon.Deleted, ShouldBeFalse)
+						})
+					})
+				})
+			})
+		})
+	})
+}
+
+// TestBalanceApply does not test anything on our end. And i am not trying to be
+// clever with testing stripe. This test is here only for making sure about the
+// logic of Amount, Subtotal And the Total. This is the third time i am
+// forgetting the logic and wanted to document it here with code.
+func TestBalanceApply(t *testing.T) {
+	Convey("Given a user who subscribed to a paid plan", t, func() {
+		withTestServer(t, func(endpoint string) {
+			withStubData(endpoint, func(username, groupName, sessionID string) {
+				withNonFreeTestPlan(func(planID string) {
+					addCreditCardToUserWithChecks(endpoint, sessionID)
+					withSubscription(endpoint, groupName, sessionID, planID, func(subscriptionID string) {
+						withTestCoupon(func(couponID string) {
+							Convey("After adding balance to the user", func() {
+								group, err := modelhelper.GetGroup(groupName)
+								tests.ResultedWithNoErrorCheck(group, err)
+								var subtotal int64 = 12345
+								// A negative amount represents a credit that
+								// decreases the amount due on an invoice; a
+								// positive amount increases the amount due on
+								// an invoice.
+								var balance int64 = -150
+								var coupon int64 = 100
+
+								expectedAmount := subtotal - coupon - (-balance) // negate the balance
+								cp := &stripe.CustomerParams{
+									Balance: balance,
+									Coupon:  couponID,
+								}
+
+								c, err := customer.Update(group.Payment.Customer.ID, cp)
+								tests.ResultedWithNoErrorCheck(c, err)
+
+								Convey("Customer should have discount", func() {
+									So(c, ShouldNotBeNil)
+									So(c.Balance, ShouldEqual, balance)
+
+									Convey("Invoice should the discount", func() {
+										i, err := invoice.GetNext(&stripe.InvoiceParams{Customer: c.ID})
+										tests.ResultedWithNoErrorCheck(i, err)
+										So(i.Subtotal, ShouldEqual, subtotal)
+										So(i.Subtotal, ShouldBeGreaterThan, i.Total)
+										So(i.Subtotal, ShouldEqual, i.Total+coupon) // dont forget to negate
+
+										So(i.Total, ShouldEqual, subtotal-coupon)
+										So(i.Total, ShouldBeGreaterThan, i.Amount)
+										So(i.Total, ShouldEqual, i.Amount+(-balance))
+
+										So(i.Amount, ShouldEqual, i.Total-(-balance))
+										So(i.Amount, ShouldEqual, expectedAmount)
+										// Subtotal = amount + coupon + balance
+										// Total    = amount + coupon
+										// Amount   = the final price that customer will pay.
+
+										// Subtotal:     12345,
+										// Total:        12245,
+										// Amount:       12145,
+
+										// Expected: '12245'
+										// Actual:   '12445'
+									})
+								})
+							})
 						})
 					})
 				})
