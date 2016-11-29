66bb3479f4dec4ed511fb95a00db517a01a6595a
Pawel Knap <pawelknap88@gmail.com>
2016-11-30 00:45:10 +0100

    klient: add tests for dynamic client

---
+++ go/src/koding/klient/machine/dynamic_test.go
@@ -0,0 +1,106 @@
+package machine_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"koding/klient/machine"
+	"koding/klient/machine/machinetest"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func TestDynamicClientOnOff(t *testing.T) {
+	var (
+		serv    = &machinetest.Server{}
+		builder = machinetest.NewNilBuilder()
+	)
+
+	dc, err := machine.NewDynamicClient(machinetest.DynamicClientOpts(serv, builder))
+	if err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+	defer dc.Close()
+
+	// Server is in unknown state.
+	if status := dc.Status(); status.State != machine.StateUnknown {
+		t.Fatalf("want state = %s; got %s", machine.StateUnknown, status.State)
+	}
+
+	// Server starts responding.
+	serv.TurnOn()
+	if err := builder.WaitForBuild(time.Second); err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+	if n := builder.BuildsCount(); n != 1 {
+		t.Fatalf("want builds count = 1; got %d", n)
+	}
+	if status := dc.Status(); status.State != machine.StateOnline {
+		t.Fatalf("want state = %s; got %s", machine.StateOnline, status.State)
+	}
+
+	// Stop server.
+	serv.TurnOff()
+	if err := builder.WaitForBuild(time.Second); err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+	if n := builder.BuildsCount(); n != 2 {
+		t.Fatalf("want builds count = 2; got %d", n)
+	}
+	if status := dc.Status(); status.State != machine.StateOffline {
+		t.Fatalf("want state = %s; got %s", machine.StateOffline, status.State)
+	}
+}
+
+func TestDynamicClientContext(t *testing.T) {
+	var (
+		serv    = &machinetest.Server{}
+		builder = machinetest.NewNilBuilder()
+	)
+
+	dc, err := machine.NewDynamicClient(machinetest.DynamicClientOpts(serv, builder))
+	if err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+	defer dc.Close()
+
+	serv.TurnOn()
+	if err := builder.WaitForBuild(time.Second); err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+
+	const ContextWorkers = 10
+
+	var g errgroup.Group
+	for i := 0; i < ContextWorkers; i++ {
+		g.Go(func() error {
+			select {
+			case <-dc.Context().Done():
+				return errors.New("context closed unexpectedly")
+			case <-time.After(50 * time.Microsecond):
+				return nil
+			}
+		})
+	}
+	// Machine is on so dynamic client should not close its channel.
+	if err := g.Wait(); err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+
+	serv.TurnOff()
+	for i := 0; i < ContextWorkers; i++ {
+		g.Go(func() error {
+			select {
+			case <-dc.Context().Done():
+				return nil
+			case <-time.After(time.Second):
+				return errors.New("timed out")
+			}
+		})
+	}
+	// Machine is off so its context channel should be closed by dynamic client.
+	if err := g.Wait(); err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+}
