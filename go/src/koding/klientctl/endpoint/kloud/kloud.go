1cba7261afa5e158b4a845038cf2ec0e6fab332d
Rafal Jeczalik <rjeczalik@gmail.com>
2016-11-30 11:20:47 +0100

    kd: add "auth login" subcommand

--- go/src/koding/klientctl/endpoint/kloud/kloud.go
+++ go/src/koding/klientctl/endpoint/kloud/kloud.go
@@ -31,6 +31,7 @@ var kdCacheOpts = &cfg.CacheOptions{
 // used.
 type Transport interface {
 	Call(method string, arg, reply interface{}) error
+	Valid() error
 }
 
 // DefaultClient is a default client used by Cache, Kite,
@@ -163,6 +164,14 @@ func (kt *KiteTransport) kloud() (*kite.Client, error) {
 	}
 
 	return kt.kKloud, nil
+}
+
+func (kt *KiteTransport) Valid() error {
+	// In order to test whether we're able to authenticate with kloud
+	// we need to call some kite method. For that purpose we
+	// use builtin "kite.print" method with empty string, since
+	// this is the only nop method available.
+	return kt.Call("kite.print", "", &struct{}{})
 }
 
 func Cache() *cfg.Cache { return DefaultClient.Cache() }
