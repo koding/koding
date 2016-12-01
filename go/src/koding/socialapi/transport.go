c26f82269f4c1f34539b0a419e5451e38f68ceab
Rafal Jeczalik <rjeczalik@gmail.com>
2016-12-01 17:27:50 +0100

    socialapi: add generic transport

---
+++ go/src/koding/socialapi/transport.go
@@ -0,0 +1,244 @@
+package socialapi
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+)
+
+const maxBodyLen = 2 << 20 // 2 MiB
+
+// contextKey is a value for use with context.WithValue.
+type contextKey struct {
+	name string
+}
+
+// SessionContextKey is a context key. It can be used in
+// HTTP handlers to attach a client session to a request.
+//
+// The session is then read by the Transport and used
+// for authentication.
+//
+// The associated value will be of *Session type.
+var SessionContextKey = &contextKey{"client-session"}
+
+// Session represents
+type Session struct {
+	// ClientID
+	ClientID string
+
+	// Username
+	Username string
+
+	// Team
+	Team string
+}
+
+// Valid implements the stack.Validator interface.
+func (s *Session) Valid() error {
+	if s == nil {
+		return errors.New("session is missing")
+	}
+	if s.ClientID == "" {
+		return errors.New("empty client ID")
+	}
+	return nil
+}
+
+type AuthFunc func(*AuthOptions) (*Session, error)
+
+type cacheKey struct {
+	Username string
+	Team     string
+}
+
+type SessionCache struct {
+	AuthFunc AuthFunc
+
+	cache map[cacheKey]*Session
+}
+
+func NewSessionCache(fn AuthFunc) *SessionCache {
+	return &SessionCache{
+		AuthFunc: fn,
+		cache:    make(map[cacheKey]*Session),
+	}
+}
+
+// Auth
+func (s *SessionCache) Auth(opts *AuthOptions) (*Session, error) {
+	if s.AuthFunc == nil {
+		panic("socialapi: AuthFunc is nil")
+	}
+
+	return nil, nil
+}
+
+// AuthOptions
+type AuthOptions struct {
+	// Session
+	Session *Session
+
+	// Refresh
+	Refresh bool
+
+	// Request
+	Request *http.Request
+}
+
+// Transport is a signing transport that
+// authorizes each request with jSession.clientId.
+type Transport struct {
+	http.RoundTripper
+
+	// Host
+	Host string
+
+	// RetryNum
+	RetryNum int
+
+	// AuthFunc
+	AuthFunc AuthFunc
+}
+
+type httpTransport interface {
+	http.RoundTripper
+	httpRequestCanceler
+	httpIdleConnectionsCloser
+}
+
+type httpRequestCanceler interface {
+	CancelRequest(*http.Request)
+}
+
+type httpIdleConnectionsCloser interface {
+	CloseIdleConnections()
+}
+
+var _ httpTransport = (*Transport)(nil)
+
+func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if t.AuthFunc == nil {
+		panic("socialapi: AuthFunc is nil")
+	}
+
+	reqCopy := copyRequest(req) // per RoundTripper contract
+
+	switch req.Body.(type) {
+	case nil:
+	case io.ReadSeeker:
+	default:
+		p, err := ioutil.ReadAll(io.LimitReader(reqCopy.Body, maxBodyLen))
+		if err != nil {
+			return nil, err // non-retryable
+		}
+
+		reqCopy.Body = ioutil.NopCloser(bytes.NewReader(p))
+	}
+
+	var refresh bool
+	var lastErr error
+	var session *Session
+
+	if s, ok := reqCopy.Context().Value(SessionContextKey).(*Session); ok {
+		session = s
+	}
+
+	for attempts := t.retryNum(); attempts > 0; attempts-- {
+		if reqCopy.Body != nil {
+			if _, err := reqCopy.Body.(io.ReadSeeker).Seek(0, io.SeekStart); err != nil {
+				return nil, err // non-retryable
+			}
+		}
+
+		if err := session.Valid(); err != nil || refresh {
+			opts := &AuthOptions{
+				Session: session,
+				Refresh: refresh,
+				Request: reqCopy,
+			}
+
+			s, err := t.AuthFunc(opts)
+			if err != nil {
+				return nil, err // non-retryable
+			}
+
+			if err := s.Valid(); err != nil {
+				return nil, err // non-retryable
+			}
+
+			session = s
+			refresh = false
+		}
+
+		reqCopy.Header.Set("Authorization", "Bearer "+session.ClientID)
+		if t.Host != "" {
+			reqCopy.Host = t.Host
+		}
+
+		resp, err := t.RoundTripper.RoundTrip(reqCopy)
+		if e, ok := err.(net.Error); ok && (e.Temporary() || e.Timeout()) {
+			lastErr = err
+			continue // retry
+		}
+
+		if err != nil {
+			return nil, err // non-retryable
+		}
+
+		if resp.StatusCode == http.StatusUnauthorized {
+			lastErr = errors.New(http.StatusText(resp.StatusCode))
+			refresh = true
+			continue // retry and force new session
+		}
+
+		return resp, nil
+	}
+
+	return nil, fmt.Errorf("socialapi: error sending request: %v", lastErr)
+}
+
+func (t *Transport) CancelRequest(req *http.Request) {
+	if rc, ok := t.RoundTripper.(httpRequestCanceler); ok {
+		rc.CancelRequest(req)
+	}
+}
+
+func (t *Transport) CloseIdleConnections() {
+	if icl, ok := t.RoundTripper.(httpIdleConnectionsCloser); ok {
+		icl.CloseIdleConnections()
+	}
+}
+
+func (t *Transport) retryNum() int {
+	if t.RetryNum > 0 {
+		return t.RetryNum
+	}
+	return 3
+}
+
+func copyURL(u *url.URL) *url.URL {
+	uCopy := *u
+	if u.User != nil {
+		userCopy := *u.User
+		uCopy.User = &userCopy
+	}
+
+	return &uCopy
+}
+
+func copyRequest(req *http.Request) *http.Request {
+	reqCopy := new(http.Request)
+	*reqCopy = *req
+	reqCopy.URL = copyURL(req.URL)
+	reqCopy.Header = make(http.Header, len(req.Header))
+	for k, s := range req.Header {
+		reqCopy.Header[k] = append([]string(nil), s...)
+	}
+	return reqCopy
+}
