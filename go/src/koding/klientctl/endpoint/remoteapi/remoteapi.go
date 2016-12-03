27271637be4b7b4ccf7a65ae3cc33e5ae8d80f6c
Rafal Jeczalik <rjeczalik@gmail.com>
2016-12-03 14:39:56 +0100

    endpoint/remoteapi: add Client

---
+++ go/src/koding/klientctl/endpoint/remoteapi/remoteapi.go
@@ -0,0 +1,58 @@
+package remoteapi
+
+import (
+	"net/url"
+	"sync"
+
+	"koding/klientctl/config"
+	"koding/klientctl/endpoint/kloud"
+	"koding/remoteapi"
+	"koding/remoteapi/client"
+	"koding/socialapi"
+	"koding/socialapi/socialkite"
+)
+
+var DefaultClient = &Client{}
+
+type Client struct {
+	Kloud *kloud.Client
+
+	once sync.Once // for c.init()
+	c    *remoteapi.Client
+}
+
+func (c *Client) New(session *socialapi.Session) *client.Koding {
+	c.init()
+
+	return c.c.New(session)
+}
+
+func (c *Client) init() {
+	c.once.Do(c.initClient)
+}
+
+func (c *Client) initClient() {
+	c.c = &remoteapi.Client{
+		Transport: &socialapi.Transport{
+			AuthFunc: (&socialkite.KloudAuth{
+				Kite: c.kloud().Transport,
+				Storage: &socialkite.Storage{
+					Cache: c.kloud().Cache(),
+				},
+			}).Auth,
+		},
+	}
+
+	if u, err := url.Parse(config.Konfig.RemoteURL); err == nil {
+		c.c.Endpoint = u
+	}
+}
+
+func (c *Client) kloud() *kloud.Client {
+	if c.Kloud != nil {
+		return c.Kloud
+	}
+	return kloud.DefaultClient
+}
+
+func New(session *socialapi.Session) *client.Koding { return DefaultClient.New(session) }
