ed1b0c76a3c8e68fac0065ccbf81d95e403af086
Cihangir SAVAS <cihangir@koding.com>
2016-12-05 13:53:11 +0300

    socialapi: add test for client

--- go/src/socialapi/workers/presence/api/handlers_test.go
+++ go/src/socialapi/workers/presence/api/handlers_test.go
@@ -10,6 +10,7 @@ import (
 	"socialapi/workers/common/mux"
 	"socialapi/workers/common/tests"
 	"socialapi/workers/presence"
+	"socialapi/workers/presence/client"
 	"testing"
 
 	"github.com/koding/runner"
@@ -41,6 +42,28 @@ func TestPing(t *testing.T) {
 						tests.ResultedWithNoErrorCheck(req, err)
 						Convey("internal endpoint without auth", func() {
 							_, err := rest.DoRequestWithAuth("POST", privateURL, req, "")
+							So(err, ShouldBeNil)
+						})
+					})
+				})
+			})
+		})
+	})
+}
+
+func TestPingWithClient(t *testing.T) {
+	Convey("Given testing user & group", t, func() {
+		withTestServer(t, func(endpoint string) {
+			withStubData(endpoint, func(username, groupName, sessionID string) {
+				Convey("We should be able to send the ping request", func() {
+					Convey("with public client", func() {
+						c := client.NewPublic(endpoint)
+						err := c.Ping(sessionID, "groupName")
+						So(err, ShouldBeNil)
+
+						Convey("with internal client", func() {
+							c := client.NewInternal(endpoint)
+							err := c.Ping(username, groupName)
 							So(err, ShouldBeNil)
 						})
 					})
