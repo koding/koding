5f7f4e2788d08c20bbc41c37f70eac89197e9764
Sinan Yasar <sinan@koding.com>
2016-12-10 19:58:02 -0800

    client: jekyll: minor home hero fixes

--- docs/_includes/js/header.js
+++ docs/_includes/js/header.js
@@ -1,6 +1,6 @@
 document.addEventListener('scroll', function(event){
 
-  header = document.querySelectorAll('header.Homepage-Header').item(0);
+  var header = document.querySelectorAll('header.Homepage-Header').item(0);
   if (!header) return null;
 
   if (document.body.scrollTop < 100) {
@@ -8,7 +8,7 @@ document.addEventListener('scroll', function(event){
       return null;
     }
     document.body.classList.remove('StickyHeader');
-  } else if (document.body.scrollTop < 780) {
+  } else if (document.body.scrollTop < 710) {
     if (!document.body.classList.contains('StickyHeader')) {
       return null;
     }
