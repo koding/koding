25c45a9380a286c4b8633aa750d0cfd8a1b0e412
Rafal Jeczalik <rjeczalik@gmail.com>
2016-12-20 12:52:42 +0100

    kd/config: add package for managing default configurations

---
+++ go/src/koding/klientctl/endpoint/config/config.go
@@ -0,0 +1,142 @@
+package config
+
+import (
+	"sync"
+
+	"koding/kites/config"
+	"koding/kites/config/configstore"
+	"koding/kites/kloud/stack"
+	"koding/klientctl/ctlcli"
+	"koding/klientctl/endpoint/kloud"
+)
+
+var DefaultClient = &Client{}
+
+type Client struct {
+	Kloud *kloud.Client
+
+	once     sync.Once // for c.init()
+	store    *configstore.Client
+	cache    *config.Cache
+	defaults config.Konfigs
+}
+
+func (c *Client) Used() (*config.Konfig, error) {
+	c.init()
+
+	return c.store.Used()
+}
+
+func (c *Client) Use(k *config.Konfig) error {
+	c.init()
+
+	return c.store.Use(k)
+}
+
+func (c *Client) List() config.Konfigs {
+	c.init()
+
+	return c.store.List()
+}
+
+func (c *Client) Set(key, value string) error {
+	c.init()
+
+	return c.store.Set(key, value)
+}
+
+func (c *Client) Close() (err error) {
+	c.init()
+
+	if len(c.defaults) != 0 {
+		err = c.cache.SetValue("konfigs.default", c.defaults)
+	}
+
+	return nonil(err, c.cache.Close())
+}
+
+func (c *Client) Defaults() (*config.Konfig, error) {
+	used, err := c.store.Used()
+	if err != nil {
+		return nil, err
+	}
+
+	id := used.ID()
+
+	if defaults, ok := c.defaults[id]; ok {
+		return defaults, nil
+	}
+
+	var req = &stack.ConfigMetadataRequest{}
+	var resp stack.ConfigMetadataResponse
+
+	if err := c.kloud().Call("config.metadata", req, &resp); err != nil {
+		return nil, err
+	}
+
+	defaults := &config.Konfig{
+		Endpoints: resp.Metadata.Endpoints,
+	}
+
+	c.defaults[defaults.ID()] = defaults
+
+	return defaults, nil
+}
+
+func (c *Client) Reset() error {
+	used, err := c.store.Used()
+	if err != nil {
+		return err
+	}
+
+	defaults, err := c.Defaults()
+	if err != nil {
+		return err
+	}
+
+	used.Endpoints = defaults.Endpoints
+
+	return c.store.Use(used)
+}
+
+func (c *Client) init() {
+	c.once.Do(c.initClient)
+}
+
+func (c *Client) initClient() {
+	c.store = &configstore.Client{}
+	c.cache = config.NewCache(c.store.CacheOptions("konfig"))
+	c.defaults = make(config.Konfigs)
+
+	c.store.Cache = c.cache
+
+	// Ignoring read error, if it's non-nil then empty cache is going to
+	// be used instead.
+	_ = c.cache.GetValue("konfigs.default", &c.defaults)
+
+	// Flush cache on exit.
+	ctlcli.CloseOnExit(c)
+}
+
+func (c *Client) kloud() *kloud.Client {
+	if c.Kloud != nil {
+		return c.Kloud
+	}
+	return kloud.DefaultClient
+}
+
+func nonil(err ...error) error {
+	for _, e := range err {
+		if e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func List() config.Konfigs              { return DefaultClient.List() }
+func Set(key, value string) error       { return DefaultClient.Set(key, value) }
+func Use(k *config.Konfig) error        { return DefaultClient.Use(k) }
+func Used() (*config.Konfig, error)     { return DefaultClient.Used() }
+func Defaults() (*config.Konfig, error) { return DefaultClient.Defaults() }
+func Reset() error                      { return DefaultClient.Reset() }
