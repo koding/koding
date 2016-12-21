336cc7dbbca914eb3073b832629f77cf5f250656
Rafal Jeczalik <rjeczalik@gmail.com>
2016-12-21 17:44:26 +0100

    tunnelproxy: do not retry on authentication error

--- go/src/koding/kites/tunnelproxy/client.go
+++ go/src/koding/kites/tunnelproxy/client.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -471,9 +472,12 @@ func (c *Client) handleReg(resp *RegisterResult, err error) error {
 
 	c.retry++
 
+	// kite package sends errors as string message.
+	isAuthError := strings.Contains(err.Error(), "authenticationError:")
+
 	// If we exceeded number of max retries or we were not connected before,
 	// we use default tunnelserver kite URL.
-	if c.connected == nil || c.retry >= c.opts.maxRegisterRetry() {
+	if c.connected == nil || c.retry >= c.opts.maxRegisterRetry() || isAuthError {
 		c.tunnelKiteURL = c.opts.tunnelKiteURL()
 		c.connected = nil
 	}
