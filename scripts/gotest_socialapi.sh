b7d2ac598426681bd83d314167803edbc5cf91f9
mehmetalisavas <mehmetali.savas@yandex.com>
2016-12-30 23:13:07 +0300

    scripts: add flag with env run & compile flag support

--- scripts/gotest_socialapi.sh
+++ scripts/gotest_socialapi.sh
@@ -3,11 +3,11 @@
 #  make relative paths work.
 cd $(dirname $0)/..
 
-CONFIG=${2:-'-c=./go/src/socialapi/config/dev.toml'}
+RUN_FLAGS=${RUN_FLAGS:-'-c=./go/src/socialapi/config/dev.toml'}
 action="sh -c"
 
-go list -f '{{if len .TestGoFiles}}"go test -v -cover -o={{.Dir}}/{{.Name}}.test -c {{.ImportPath}}"{{end}}' $1 | grep -v vendor | xargs -L 1 $action
-go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.test -test.coverprofile={{.Dir}}/coverage.txt "{{end}}' $1 | xargs -L 1 -I{} $action {}$CONFIG
+go list -f '{{if len .TestGoFiles}}"go test -v -cover -o={{.Dir}}/{{.Name}}.test -c {{.ImportPath}} "{{end}}' $1 | grep -v vendor | xargs -L 1 -I{} $action {}$COMPILE_FLAGS
+go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.test -test.coverprofile={{.Dir}}/coverage.txt "{{end}}' $1 | xargs -L 1 -I{} $action {}$RUN_FLAGS
 go list -f '{{if len .TestGoFiles}}"rm {{.Dir}}/{{.Name}}.test "{{end}}' $1 | xargs -L 1 $action
 
 bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN -X gcov -X fix
