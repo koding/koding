0bea9861204b0d9379675b6ff3d9557ccb19ecfe
mehmetalisavas <mehmetali.savas@yandex.com>
2016-12-30 23:40:31 +0300

    scripts: call go test from socialapi with custom config path

--- scripts/gotest_socialapi.sh
+++ scripts/gotest_socialapi.sh
@@ -3,9 +3,7 @@
 #  make relative paths work.
 cd $(dirname $0)/..
 
-RUN_FLAGS=${RUN_FLAGS:-'-c=./go/src/socialapi/config/dev.toml'}
-action="sh -c"
+confPath="-c="$(pwd)"/go/src/socialapi/config/dev.toml"
+export RUN_FLAGS=${RUN_FLAGS:-$confPath}
 
-go list -f '{{if len .TestGoFiles}}"go test -v -cover -o={{.Dir}}/{{.Name}}.test -c {{.ImportPath}} "{{end}}' $1 | grep -v vendor | xargs -L 1 -I{} $action {}$COMPILE_FLAGS
-go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.test -test.coverprofile={{.Dir}}/coverage.txt "{{end}}' $1 | xargs -L 1 -I{} $action {}$RUN_FLAGS
-go list -f '{{if len .TestGoFiles}}"rm {{.Dir}}/{{.Name}}.test "{{end}}' $1 | xargs -L 1 $action
+./scripts/gotest.sh $@
