33285b79c6251ef129fed83adba573073953f767
mehmetalisavas <mehmetali.savas@yandex.com>
2016-12-30 23:40:12 +0300

    scripts: rename file

--- scripts/gocover.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-filepath=$1
-
-go list -f '{{if len .TestGoFiles}}"go test -v -race -coverprofile={{.Dir}}/{{.Name}}.coverprofile {{.ImportPath}}"{{end}}' $filepath | xargs -L 1 sh -c
