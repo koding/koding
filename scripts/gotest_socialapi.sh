dcfd81870a2fbcb0ac23d4802bfecfae3cd963ae
mehmetalisavas <mehmetali.savas@yandex.com>
2016-12-30 23:19:36 +0300

    scripts: add testcoverages into the wercker

--- scripts/gotest_socialapi.sh
+++ scripts/gotest_socialapi.sh
@@ -9,5 +9,3 @@ action="sh -c"
 go list -f '{{if len .TestGoFiles}}"go test -v -cover -o={{.Dir}}/{{.Name}}.test -c {{.ImportPath}} "{{end}}' $1 | grep -v vendor | xargs -L 1 -I{} $action {}$COMPILE_FLAGS
 go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.test -test.coverprofile={{.Dir}}/coverage.txt "{{end}}' $1 | xargs -L 1 -I{} $action {}$RUN_FLAGS
 go list -f '{{if len .TestGoFiles}}"rm {{.Dir}}/{{.Name}}.test "{{end}}' $1 | xargs -L 1 $action
-
-bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN -X gcov -X fix
