144bfdb57e3d8048995fb82b2f1c09638d021806
mehmetalisavas <mehmetali.savas@yandex.com>
2016-12-30 22:28:35 +0300

    scripts: remove commentout lines inside from script

--- scripts/gotest_socialapi.sh
+++ scripts/gotest_socialapi.sh
@@ -3,16 +3,11 @@
 #  make relative paths work.
 cd $(dirname $0)/..
 
-echo $1
 CONFIG=${2:-'-c=./go/src/socialapi/config/dev.toml'}
 action="sh -c"
 
-# echo "echo bok" |  xargs -L 1 -I {} $action {}"bok"
 go list -f '{{if len .TestGoFiles}}"go test -v -cover -o={{.Dir}}/{{.Name}}.test -c {{.ImportPath}}"{{end}}' $1 | grep -v vendor | xargs -L 1 $action
-# go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.test -test.coverprofile={{.Dir}}/{{.Name}}.coverprofile "{{end}}' $1 | xargs -L 1 -I{} $action {}$CONFIG
 go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.test -test.coverprofile={{.Dir}}/coverage.txt "{{end}}' $1 | xargs -L 1 -I{} $action {}$CONFIG
 go list -f '{{if len .TestGoFiles}}"rm {{.Dir}}/{{.Name}}.test "{{end}}' $1 | xargs -L 1 $action
-# echo "mode: set" > coverage.txt
-# go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.coverprofile"{{end}}' $1 | xargs -L 1 -I{} tail -n +2 {} >> coverage.txt
 
 bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN -X gcov -X fix
