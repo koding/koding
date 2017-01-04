fc34343267fb857640eac044839bcc4ecaffab03
Pawel Knap <pawelknap88@gmail.com>
2017-01-05 00:49:33 +0100

    klient/mount: add mounts package

---
+++ go/src/koding/klient/machine/machinegroup/mounts/mounts.go
@@ -0,0 +1,169 @@
+package mounts
+
+import (
+	"fmt"
+	"path/filepath"
+	"sync"
+
+	"koding/klient/machine"
+	"koding/klient/machine/mount"
+)
+
+// Mounts store mounts of all machines in the group.
+type Mounts struct {
+	mu sync.RWMutex
+	m  map[machine.ID]*mount.MountBook
+}
+
+// New creates an empty Mounts object.
+func New() *Mounts {
+	return &Mounts{
+		m: make(map[machine.ID]*mount.MountBook),
+	}
+}
+
+// Add adds provided mount to a given machine.
+func (ms *Mounts) Add(id machine.ID, mountID mount.ID, m mount.Mount) error {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+
+	mb, ok := ms.m[id]
+	if !ok {
+		mb = mount.NewMountBook()
+	}
+
+	mb.Add(mountID, m)
+	ms.m[id] = mb
+
+	return nil
+}
+
+// Remove removes a given mount from the cache.
+func (ms *Mounts) Remove(mountID mount.ID) error {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+
+	for id, mb := range ms.m {
+		all := mb.All()
+		switch _, ok := all[mountID]; {
+		case ok && len(all) == 1:
+			// Drop the entire mount book.
+			delete(ms.m, id)
+			return nil
+		case ok:
+			mb.Remove(mountID)
+			return nil
+		}
+	}
+
+	return nil
+}
+
+// Drop removes all mounts which are binded to provided machine ID.
+func (ms *Mounts) Drop(id machine.ID) error {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+	delete(ms.m, id)
+
+	return nil
+}
+
+// Path returns mount ID which mount's local path is equal to provided
+// argument.
+func (ms *Mounts) Path(path string) (mount.ID, error) {
+	if !filepath.IsAbs(path) {
+		return "", fmt.Errorf("path %q is not absolute", path)
+	}
+
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	for _, mb := range ms.m {
+		if mountID, err := mb.Path(path); err == nil {
+			return mountID, nil
+		}
+	}
+
+	return "", mount.ErrMountNotFound
+}
+
+// RemotePath returns all mount IDs which RemotePath field matches provided
+// argument. Path must be absolute and cleaned. mount.ErrMountNotFound is
+// returned if there is no mount with provided ID.
+func (ms *Mounts) RemotePath(path string) (ids mount.IDSlice, err error) {
+	if !filepath.IsAbs(path) {
+		return nil, fmt.Errorf("path %q is not absolute", path)
+	}
+
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	for _, mb := range ms.m {
+		if mids, err := mb.RemotePath(path); err == nil {
+			ids = append(ids, mids...)
+		}
+	}
+
+	if len(ids) == 0 {
+		return nil, mount.ErrMountNotFound
+	}
+
+	return ids, nil
+}
+
+// MachineID gets machine ID based on provided mount ID. mount.ErrMountNotFound
+// is returned if there is no mount with provided ID.
+func (ms *Mounts) MachineID(mountID mount.ID) (machine.ID, error) {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	for id, mb := range ms.m {
+		for storedID := range mb.All() {
+			if mountID == storedID {
+				return id, nil
+			}
+		}
+	}
+
+	return "", mount.ErrMountNotFound
+}
+
+// All returns a copy of all mounts connected with a given machine.
+func (ms *Mounts) All(id machine.ID) (map[mount.ID]mount.Mount, error) {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	if mb, ok := ms.m[id]; ok {
+		return mb.All(), nil
+	}
+
+	return nil, machine.ErrMachineNotFound
+}
+
+// Registered returns all machines that are managed by mounts object.
+func (ms *Mounts) Registered() machine.IDSlice {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	registered := make(machine.IDSlice, 0, len(ms.m))
+	for id := range ms.m {
+		registered = append(registered, id)
+	}
+
+	return registered
+}
+
+// all returns all stored mounts.
+func (ms *Mounts) all() map[machine.ID]*mount.MountBook {
+	all := make(map[machine.ID]*mount.MountBook)
+
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+	for id, mb := range ms.m {
+		// It is save to return pointer here since mount book's internal
+		// fields are synchronized internally.
+		all[id] = mb
+	}
+
+	return all
+}
