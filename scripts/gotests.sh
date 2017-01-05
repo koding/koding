c9b5373d4acaa4d14a86c88fef2225d4aded1995
mehmetalisavas <mehmetali.savas@yandex.com>
2017-01-05 06:51:18 +0300

    scripts: use new script for wercker & add TODO for flags

--- scripts/gotests.sh
+++ scripts/gotests.sh
@@ -3,15 +3,18 @@
 #  make relative paths work.
 cd $(dirname $0)/..
 
-echo "current $1 $2"
-
 export COMPILE_FLAGS=${COMPILE_FLAGS:-""}
 
 if [ "$1" == "test_socialapi" ]; then
   shift
   confPath="-c="$(pwd)"/go/src/socialapi/config/dev.toml"
-  export RUN_FLAGS="$RUN_FLAGS $confPath"
+  # export RUN_FLAGS="$RUN_FLAGS $confPath"
+  # TODO
+  # If there is more than 1 argument for run flag config file + another flag
+  # then there is an error in bash script
+  export RUN_FLAGS="$confPath"
   echo $RUN_FLAGS
+
   ./scripts/gotest.sh $@
   unset COMPILE_FLAGS
 
