17daa4ae9de239d95ef4dda1d33288c76f7b16e8
mehmetalisavas <mehmetali.savas@yandex.com>
2017-01-05 23:11:33 +0300

    scripts: change function name & make consistent code lines

--- scripts/gotests.sh
+++ scripts/gotests.sh
@@ -10,19 +10,19 @@ function compile () {
     go list -f '{{if len .TestGoFiles}}"go test -v -cover -c {{.ImportPath}} -o={{.Dir}}/{{.Name}}.test "{{end}}' $1 | grep -v vendor | xargs -L 1 -I{} $action "{}$COMPILE_FLAGS"
 }
 
-function run() {
+function run () {
     go list -f '{{if len .TestGoFiles}}"{{.Dir}}/{{.Name}}.test -test.coverprofile={{.Dir}}/coverage.txt "{{end}}' $1 | grep -v vendor | xargs -L 1 -I{} $action "{}$RUN_FLAGS"
 }
 
-function runWithCD() {
+function runWithCD () {
     go list -f '{{if len .TestGoFiles}}"cd {{.Dir}} && ./{{.Name}}.test -test.coverprofile={{.Dir}}/coverage.txt "{{end}}' $1 | grep -v vendor | xargs -L 1 -I{} $action "{}$RUN_FLAGS"
 }
 
-function clean() {
+function clean () {
     go list -f '{{if len .TestGoFiles}}"rm {{.Dir}}/{{.Name}}.test "{{end}}' $1 | xargs -L 1 $action
 }
 
-function test() {
+function runAll () {
     compile $@
     run $@
     clean $@
@@ -30,17 +30,17 @@ function test() {
 
 if [ "$1" == "socialapi" ]; then
   shift
-
   export RUN_FLAGS=${RUN_FLAGS:-"-c=./go/src/socialapi/config/dev.toml"}
-  test $@
+  
+  runAll $@
 
 elif  [ "$1" == "kites" ]; then
     shift
     export COMPILE_FLAGS=${COMPILE_FLAGS:-"-race"}
-    
+
     compile  $@
     runWithCD $@
     clean $@
 else
-  test $@
+  runAll $@
 fi
