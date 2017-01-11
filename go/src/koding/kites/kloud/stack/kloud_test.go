56b629381c44e57f6ff4e59f9b2ebb7de1fe70e8
Rafal Jeczalik <rjeczalik@gmail.com>
2017-01-11 22:17:55 +0100

    stack: add test for ReadProviders

---
+++ go/src/koding/kites/kloud/stack/kloud_test.go
@@ -0,0 +1,54 @@
+package stack_test
+
+import (
+	"koding/kites/kloud/stack"
+	"reflect"
+	"testing"
+)
+
+func TestReadProviders(t *testing.T) {
+	cases := map[string]struct {
+		tmpl      string
+		providers []string
+		err       bool
+	}{
+		"single provider": {
+			`{"provider": {"google": {}}}`,
+			[]string{"google"},
+			false,
+		},
+		"multiple providers": {
+			`{"provider": {"google": {}, "aws": {}}}`,
+			[]string{"aws", "google"},
+			false,
+		},
+		"no providers": {
+			`{"provider": {}}`,
+			[]string{},
+			false,
+		},
+		"invalid template": {
+			"[]",
+			nil,
+			true,
+		},
+	}
+
+	for name, cas := range cases {
+		t.Run(name, func(t *testing.T) {
+			providers, err := stack.ReadProviders([]byte(cas.tmpl))
+
+			if cas.err {
+				if err == nil {
+					t.Fatal("expected err to be non-nil")
+				}
+
+				return
+			}
+
+			if !reflect.DeepEqual(providers, cas.providers) {
+				t.Fatalf("got %#v, want %#v", providers, cas.providers)
+			}
+		})
+	}
+}
