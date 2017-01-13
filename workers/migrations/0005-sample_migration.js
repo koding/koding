ed74d0ae16f538f358e37dd334cd96297057100b
Cihangir SAVAS <cihangir@koding.com>
2017-01-13 17:52:20 +0300

    workers: do not run migrations for non dev & default envs

--- workers/migrations/0005-sample_migration.js
+++ workers/migrations/0005-sample_migration.js
@@ -15,6 +15,11 @@
 var mongodb = require('mongodb');
 
 exports.up = function(db, next){
+  var env = process.env.KONFIG_ENVIRONMENT;
+  if (env !== "dev" && env !== "default") {
+    return next();
+  }
+
   coll = db.collection('jAccount');
   coll.find({ 'profile.nickname': 'admin' }, next);
 };
