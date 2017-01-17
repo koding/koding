b5541fd51baa22184f6a732b2f0f7414db57b85b
Sinan Yasar <sinan@koding.com>
2017-01-17 07:49:19 -0800

    client: pricing: drawer reused for discounted packages

--- docs/_includes/js/pricing.js
+++ docs/_includes/js/pricing.js
@@ -1,29 +1,6 @@
 var ready = function($) {
 
-  var $more = $("#Pricing-PriceSegments--more");
-  var $hide = $("#Pricing-PriceSegments--close");
-  var $show = $(".Pricing-PriceSegments--showMore");
-  var $main = $("#Pricing-PriceSegments--devTeams");
   var $footnotes = $("#footnotes");
-  $hide.click(function(event){
-    event.stopPropagation();
-    event.preventDefault();
-
-    $more.removeClass('in')
-    $main.removeClass('open')
-
-    return false;
-  });
-
-  $show.click(function(event){
-    event.stopPropagation();
-    event.preventDefault();
-
-    $more.toggleClass('in')
-    $main.toggleClass('open')
-
-    return false;
-  });
 
   $('.contact').click(function(event){
     event.stopPropagation();
