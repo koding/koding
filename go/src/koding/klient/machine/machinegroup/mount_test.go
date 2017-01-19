70586af02f8372e754d621f5c837ee65a1b36a4c
Pawel Knap <pawelknap88@gmail.com>
2017-01-19 05:50:01 +0100

    klient/machine: add tests to mount head handler

---
+++ go/src/koding/klient/machine/machinegroup/mount_test.go
@@ -0,0 +1,73 @@
+package machinegroup
+
+import (
+	"testing"
+	"time"
+
+	"koding/klient/machine"
+	"koding/klient/machine/client/clienttest"
+	"koding/klient/machine/index"
+	"koding/klient/machine/mount/mounttest"
+)
+
+func TestHeadMount(t *testing.T) {
+	var (
+		builder = clienttest.NewBuilder(nil)
+		id      = machine.ID("serv")
+	)
+
+	wd, m, clean, err := mounttest.MountDirs()
+	if err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+	defer clean()
+
+	g, err := New(testOptions(wd, builder))
+	if err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+	defer g.Close()
+
+	// Add connected remote machine.
+	createReq := &CreateRequest{
+		Addresses: map[machine.ID][]machine.Addr{
+			id: {clienttest.TurnOnAddr()},
+		},
+	}
+	if _, err := g.Create(createReq); err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+	if err := builder.WaitForBuild(time.Second); err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+
+	// Head testing mount.
+	headMountReq := &HeadMountRequest{
+		ID:    id,
+		Mount: m,
+	}
+	headMountRes, err := g.HeadMount(headMountReq)
+	if err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+
+	if headMountRes.ExistMountID != "" {
+		t.Errorf("want mount does not exist; got: %s", headMountRes.ExistMountID)
+	}
+	if headMountRes.AbsRemotePath != m.RemotePath {
+		t.Errorf("want remote path = %s; got %s", m.RemotePath, headMountRes.AbsRemotePath)
+	}
+
+	// Compare indexes.
+	idx, err := index.NewIndexFiles(m.RemotePath)
+	if err != nil {
+		t.Fatalf("want err = nil; got %v", err)
+	}
+
+	if resc, idxc := headMountRes.AllCount, idx.Count(-1); resc != idxc {
+		t.Errorf("want file count = %d; got %d", idxc, resc)
+	}
+	if resds, idxds := headMountRes.AllDiskSize, idx.DiskSize(-1); resds != idxds {
+		t.Errorf("want disk size = %d; got %d", idxds, resds)
+	}
+}
