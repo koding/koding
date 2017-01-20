98def842098fd4e45f25101893e7fecc955c2c70
mehmetalisavas <mehmetali.savas@yandex.com>
2017-01-20 17:26:19 +0300

    scripts: generate foldernames with command checkings

--- scripts/gotests.sh
+++ scripts/gotests.sh
@@ -20,7 +20,7 @@ function compile () {
     # make comma-separated
     export PKGS_DELIM=$(echo "$PKGS" | paste -sd "," -)
 
-    go list -f '{{if len .TestGoFiles}}"go test -race -v -cover -c {{.ImportPath}} -o={{.Dir}}/{{.Name}}.test -coverpkg='$PKGS_DELIM' "{{end}}' $PKGS | grep -v vendor | xargs -L 1 -I{} $action "{}$COMPILE_FLAGS"
+    go list -f '{{if len .TestGoFiles}}"go test -v -cover -c {{.ImportPath}} -o={{.Dir}}/{{.Name}}.test -coverpkg='$PKGS_DELIM' "{{end}}' $PKGS | grep -v vendor | xargs -L 1 -I{} $action "{}$COMPILE_FLAGS"
 }
 
 # run runs the binary file that created before (with compile function)
@@ -32,11 +32,9 @@ function run () {
 }
 
 function merge () {
-    # echo "TESTING THE MERGING HERE"
-    #
-    # IFS=' ' read -r -a array <<< $(go list -f '{{if len .TestGoFiles}}"{{.Dir}}/coverage.txt "{{end}}' $1 | grep -v vendor | xargs -L 1 -I{} echo {})
-    go list -f '{{if len .TestGoFiles}}"{{.Dir}}/coverage.txt "{{end}}' $1 | grep -v vendor | xargs $COVMERGE > testcoverage.txt
+    folder=$(createFolder $1)
 
+    go list -f '{{if len .TestGoFiles}}"{{.Dir}}/coverage.txt"{{end}}' $1 | grep -v vendor | xargs $COVMERGE > "$folder"
 }
 
 # runWithCD runs like run function.
@@ -54,20 +52,60 @@ function runAll () {
     compile $@
     run $@
     merge $@
+    removeCoverProfiles $@
     clean $@
 }
 
-if [ "$1" == "socialapi" ]; then
-  shift
-  export RUN_FLAGS=${RUN_FLAGS:-"-c=./go/src/socialapi/config/dev.toml"}
-  runAll $@
+# removeCoverProfiles deletes the coverage.txt files after merging all
+# coverages into 1 coverage.txt(merged coverage file won't be deleted)
+function removeCoverProfiles () {
+    go list -f '{{if len .TestGoFiles}}"{{.Dir}}/coverage.txt"{{end}}' $1 | grep -v vendor | xargs rm
+}
 
-elif  [ "$1" == "kites" ]; then
+# commandExists checks if the given parameter command is exits or not
+function commandExists() {
+    command -v $1 >/dev/null 2>&1
+}
+
+# generateFolderName generates a random string with given parameter
+# according to given parameter & command checking
+function generateFolderName() {
+    if commandExists md5 ; then
+        echo $1 | md5
+    elif commandExists md5sum ; then
+        echo -n $1 | md5sum | awk '{print $1}'
+    else
+        date +%s%N
+    fi
+}
+
+# createFolder creates a '/coverage' folder if doesn't exists
+# then appends the generated random string as folder name
+# e.g:
+# assume random string : $generatedName
+# path will be -> koding/go/src/coverage/$generatedName
+function createFolder() {
+    name=$(generateFolderName $1)
+    mkdir -p "$KD/go/src/coverages"
+    mkdir -p "$KD/go/src/coverages/$name"
+    touch "$KD/go/src/coverages/$name/coverage.txt"
+    folder="$KD/go/src/coverages/$name/coverage.txt"
+    echo $folder
+}
+
+if  [ "$1" == "kites" ]; then
     shift
+    export COMPILE_FLAGS=${COMPILE_FLAGS:-"-race"}
+
     compile  $@
     runWithCD $@
     clean $@
 
+elif [ "$1" == "socialapi" ]; then
+    shift
+    export RUN_FLAGS=${RUN_FLAGS:-"-c=./go/src/socialapi/config/dev.toml"}
+
+    runAll $@
 else
     runAll $@
 fi
