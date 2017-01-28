db95485afc3f7916bffc5ee99cd7f6f2cc746e68
Sonmez Kartal <sonmez@koding.com>
2017-01-28 01:21:16 +0000

    Improve dead code check process
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/check_socialapi_code_quality.sh
+++ scripts/check_socialapi_code_quality.sh
@@ -10,6 +10,10 @@ git diff-tree -r --exit-code --name-only --no-commit-id HEAD \
 
 export GOPATH=${GOPATH:-$(pwd)/go}
 
+echo "checking deadcode"
+ls --directory $GOPATH/src/socialapi/*/**/**/ | \
+  xargs go/bin/deadcode
+
 echo "checking cyclo complexity (disabled due to go1.6 switch - fixme!)"
 # Due to go1.6 gocyclo check started suddently to work showing
 # a number of complex functions:
@@ -19,9 +23,6 @@ echo "checking cyclo complexity (disabled due to go1.6 switch - fixme!)"
 # Please fix the code and lower the value back to 20.
 #
 # ./go/bin/gocyclo -top 28 ./go/src/socialapi/*/**/**.go
-
-echo "checking deadcode"
-./scripts/deadcode.sh
 
 echo "checking unused variables"
 ./scripts/govarcheck.sh
