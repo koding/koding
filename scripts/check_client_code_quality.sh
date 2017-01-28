cfc810a87bb06f1eb3d24dfa8e9a58c223cb6e88
Sonmez Kartal <sonmez@koding.com>
2017-01-28 03:14:08 +0000

    Fix unused variable check process for CoffeeScript
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/check_client_code_quality.sh
+++ scripts/check_client_code_quality.sh
@@ -5,10 +5,8 @@ set -o errexit
 #  make relative paths work.
 cd $(dirname $0)/..
 
-path="./client"
-
 git diff-tree -r --exit-code --name-only --no-commit-id HEAD \
     client && exit 0
 
 echo "checking unused variables"
-path=$path ./scripts/coffeevarcheck.sh
+node node_modules/coffee-unused --src client --skip-parse-error
