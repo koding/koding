aa588aec70abf6856da33782af52368ec6a6b2f1
Sonmez Kartal <sonmez@koding.com>
2017-01-28 01:43:35 +0000

    Fix unused variable check process for Go
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/check_socialapi_code_quality.sh
+++ scripts/check_socialapi_code_quality.sh
@@ -14,6 +14,10 @@ echo "checking deadcode"
 ls --directory $GOPATH/src/socialapi/*/**/**/ | \
   xargs go/bin/deadcode
 
+echo "checking unused variables"
+echo $(cd $GOPATH/src; ls --directory socialapi/*/**/**/) | \
+  xargs $GOPATH/bin/varcheck
+
 echo "checking cyclo complexity (disabled due to go1.6 switch - fixme!)"
 # Due to go1.6 gocyclo check started suddently to work showing
 # a number of complex functions:
@@ -23,6 +27,3 @@ echo "checking cyclo complexity (disabled due to go1.6 switch - fixme!)"
 # Please fix the code and lower the value back to 20.
 #
 # ./go/bin/gocyclo -top 28 ./go/src/socialapi/*/**/**.go
-
-echo "checking unused variables"
-./scripts/govarcheck.sh
