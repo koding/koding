e30b60ead525c43e708fb4a1b741eab2d26eeb74
Sonmez Kartal <sonmez@koding.com>
2017-01-30 12:27:31 +0000

    Execute script as postgres user
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- go/src/socialapi/db/sql/definition/create.sh
+++ go/src/socialapi/db/sql/definition/create.sh
@@ -3,89 +3,89 @@
 pushd $(dirname $0)/..
 
 # clear database
-sudo -u postgres psql postgres --file definition/000-cleanup.sql
+psql postgres --file definition/000-cleanup.sql
 
 # add UTF-8 support
-sudo -u postgres psql postgres --file definition/000-UTF8.sql
+psql postgres --file definition/000-UTF8.sql
 
 # create initial database
-sudo -u postgres psql postgres --file definition/001-database.sql
+psql postgres --file definition/001-database.sql
 
 # create  schema definitions
-sudo -u postgres psql social --file definition/002-schema.sql
+psql social --file definition/002-schema.sql
 
 # create sequences
-sudo -u postgres psql social --file definition/003-sequence.sql
+psql social --file definition/003-sequence.sql
 
 # create sequence functions
-sudo -u postgres psql social --file definition/003-sequencefunction.sql
+psql social --file definition/003-sequencefunction.sql
 
 # create tables
-sudo -u postgres psql social --file definition/004-table.sql
+psql social --file definition/004-table.sql
 
 # ctreate constraints
-sudo -u postgres  psql social --file definition/005-constraint.sql
+ psql social --file definition/005-constraint.sql
 
 # create kontrol specific tables
 if [ -d "kontrol" ]; then
-  sudo -u postgres psql social --file kontrol/001-schema.sql
-  sudo -u postgres psql social --file kontrol/002-table.sql
+  psql social --file kontrol/001-schema.sql
+  psql social --file kontrol/002-table.sql
 fi
 
 # NOTIFICATION WORKER SQL IMPORTS
 
 # create sequences
-sudo -u postgres psql social --file notification_definition/002-schema.sql
+psql social --file notification_definition/002-schema.sql
 
 # create sequences
-sudo -u postgres psql social --file notification_definition/003-sequence.sql
+psql social --file notification_definition/003-sequence.sql
 
 # create tables
-sudo -u postgres psql social --file notification_definition/004-table.sql
+psql social --file notification_definition/004-table.sql
 
 # create constraints
-sudo -u postgres psql social --file notification_definition/005-constraint.sql
+psql social --file notification_definition/005-constraint.sql
 
 # SITEMAP WORKER SQL IMPORTS
 
 # create sequences
-sudo -u postgres psql social --file sitemap_definition/002-schema.sql
+psql social --file sitemap_definition/002-schema.sql
 
 # create sequences
-sudo -u postgres psql social --file sitemap_definition/003-sequence.sql
+psql social --file sitemap_definition/003-sequence.sql
 
 # create tables
-sudo -u postgres psql social --file sitemap_definition/004-table.sql
+psql social --file sitemap_definition/004-table.sql
 
 # create constraints
-sudo -u postgres psql social --file sitemap_definition/005-constraint.sql
+psql social --file sitemap_definition/005-constraint.sql
 
 # PAYMENT SQL IMPORTS
 
 # create sequences
-sudo -u postgres psql social --file payment_definition/002-schema.sql
+psql social --file payment_definition/002-schema.sql
 
 # create sequences
-sudo -u postgres psql social --file payment_definition/003-sequence.sql
+psql social --file payment_definition/003-sequence.sql
 
 # create tables
-sudo -u postgres psql social --file payment_definition/004-table.sql
+psql social --file payment_definition/004-table.sql
 
 # create constraints
-sudo -u postgres psql social --file payment_definition/005-constraint.sql
+psql social --file payment_definition/005-constraint.sql
 
-sudo -u postgres psql social --file payment_definition/006-paymentro.sql
-
-# create sequences
-sudo -u postgres psql social --file integration_definition/002-schema.sql
+psql social --file payment_definition/006-paymentro.sql
 
 # create sequences
-sudo -u postgres psql social --file integration_definition/003-sequence.sql
+psql social --file integration_definition/002-schema.sql
+
+# create sequences
+psql social --file integration_definition/003-sequence.sql
 
 # create tables
-sudo -u postgres psql social --file integration_definition/004-table.sql
+psql social --file integration_definition/004-table.sql
 
 # create constraints
-sudo -u postgres psql social --file integration_definition/005-constraint.sql
+psql social --file integration_definition/005-constraint.sql
 
 # TODO: if you make changes to this file, don't forget `create-wercker.sh`
