59384c3c393315fe042406f35b2dc7162573dcf7
mehmetalisavas <mehmetali.savas@yandex.com>
2017-02-02 16:33:13 +0300

    go/workers: fix wrong spellings

--- go/src/koding/db/mongodb/modelhelper/common.go
+++ go/src/koding/db/mongodb/modelhelper/common.go
@@ -23,13 +23,6 @@ func NewObjectId() bson.ObjectId {
 	return bson.NewObjectId()
 }
 
-func deleteByIdQuery(id string) func(c *mgo.Collection) error {
-	selector := Selector{"_id": GetObjectId(id)}
-	return func(c *mgo.Collection) error {
-		return c.Remove(selector)
-	}
-}
-
 func updateByIdQuery(id string, data interface{}) func(*mgo.Collection) error {
 	return func(c *mgo.Collection) error {
 		return c.UpdateId(GetObjectId(id), data)
@@ -39,28 +32,6 @@ func updateByIdQuery(id string, data interface{}) func(*mgo.Collection) error {
 func updateQuery(s Selector, data interface{}) func(*mgo.Collection) error {
 	return func(c *mgo.Collection) error {
 		return c.Update(s, data)
-	}
-}
-
-// TODO this is not functional
-func findAllQuery(s Selector, o Options, data interface{}) func(*mgo.Collection) error {
-	return func(c *mgo.Collection) error {
-		q := c.Find(s)
-		decorateQuery(q, o)
-		return q.All(data)
-	}
-}
-
-func countQuery(s Selector, o Options, count *int) func(*mgo.Collection) error {
-	return func(c *mgo.Collection) error {
-		q := c.Find(s)
-		decorateQuery(q, o)
-		result, err := q.Count()
-		if err != nil {
-			return err
-		}
-		*count = result
-		return nil
 	}
 }
 
@@ -79,17 +50,5 @@ func checkExistence(id string, exists *bool) func(*mgo.Collection) error {
 func insertQuery(data interface{}) func(*mgo.Collection) error {
 	return func(c *mgo.Collection) error {
 		return c.Insert(data)
-	}
-}
-
-func decorateQuery(q *mgo.Query, o Options) {
-	if o.Limit != 0 {
-		q = q.Limit(o.Limit)
-	}
-	if o.Sort != "" {
-		q = q.Sort(o.Sort)
-	}
-	if o.Skip != 0 {
-		q = q.Skip(o.Skip)
 	}
 }
