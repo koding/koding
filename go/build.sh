c1c758481b678017ac7fed9e5018f97246b59914
mehmetalisavas <mehmetali.savas@yandex.com>
2017-02-02 16:24:15 +0300

    go/build: install gometalinter stable version

--- go/build.sh
+++ go/build.sh
@@ -49,6 +49,7 @@ export COMMANDS=(
 	vendor/github.com/jteeuwen/go-bindata/go-bindata
 	vendor/github.com/wadey/gocovmerge
 	vendor/github.com/opennota/check/cmd/varcheck
+	vendor/gopkg.in/gometalinter.v1
 )
 
 export TERRAFORM_COMMANDS=(
