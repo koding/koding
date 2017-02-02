57af398f2d83e88f7636f42be93d76300b6a9603
mehmetalisavas <mehmetali.savas@yandex.com>
2017-02-02 16:28:40 +0300

    go/compute_provider: fix wrong spellings

--- go/src/koding/remoteapi/client/compute_provider/post_remote_api_compute_provider_fetch_usage_responses.go
+++ go/src/koding/remoteapi/client/compute_provider/post_remote_api_compute_provider_fetch_usage_responses.go
@@ -49,7 +49,7 @@ func NewPostRemoteAPIComputeProviderFetchUsageOK() *PostRemoteAPIComputeProvider
 
 /*PostRemoteAPIComputeProviderFetchUsageOK handles this case with default header values.
 
-Request processed succesfully
+Request processed successfully
 */
 type PostRemoteAPIComputeProviderFetchUsageOK struct {
 	Payload *models.DefaultResponse
