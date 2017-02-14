e1465e0a28080aae16ac8787ec43fc320cd93cce
Rafal Jeczalik <rjeczalik@gmail.com>
2017-02-14 22:10:20 +0100

    machine/index: shallow copy Sub on lookup

--- go/src/koding/klient/machine/index/index.go
+++ go/src/koding/klient/machine/index/index.go
@@ -18,12 +18,6 @@ import (
 	"github.com/djherbis/times"
 )
 
-// TODO:
-//
-// - PromiseAdd / PromiseDel
-// - copy Sub on Lookup
-//
-
 // Version stores current version of index.
 const Version = 1
 
