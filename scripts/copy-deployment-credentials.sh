f8f4ec0ec45b6543dc986e01a1f529664fcb917b
Sonmez Kartal <sonmez@koding.com>
2017-02-17 04:29:50 +0300

    Clone via GitHub access token if possible
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/copy-deployment-credentials.sh
+++ scripts/copy-deployment-credentials.sh
@@ -3,7 +3,13 @@
 function clone() {
 	declare repository=$1
 	declare branch=$2
-	git clone --branch $branch --depth 1 git@github.com:koding/$repository.git
+	declare repository_url="git@github.com:koding/$repository.git"
+
+	if [[ -n "$GITHUB_ACCESS_TOKEN" ]]; then
+		repository_url="https://$GITHUB_ACCESS_TOKEN:x-oauth-basic@github.com/koding/$repository.git"
+	fi
+
+	git clone --branch $branch --depth 1 $repository_url
 }
 
 BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
