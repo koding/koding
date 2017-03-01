761c0cd1cd12114f52bc74cf0d86c426ba412dc2
Sonmez Kartal <sonmez@koding.com>
2017-03-01 11:46:32 +0300

    Remove deprecated node_modules caching process
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- client/.npm-postinstall.sh
+++ client/.npm-postinstall.sh
@@ -5,16 +5,3 @@ set -o errexit
 pushd $(dirname $0)
 
 touch node_modules/.npm-install.timestamp
-
-[ -z "$CI" ] && exit 0
-
-[ $(git rev-parse --abbrev-ref HEAD) != "master" ] && exit 0
-
-UPDATE_WERCKER_NODE_MODULES=../scripts/wercker/update-node-modules
-
-$UPDATE_WERCKER_NODE_MODULES
-$UPDATE_WERCKER_NODE_MODULES landing
-
-popd
-
-exit 0
