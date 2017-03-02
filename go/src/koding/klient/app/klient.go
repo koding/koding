b38f667d8972d85c93322d4f5ed06ea2e9654f6d
Rafal Jeczalik <rjeczalik@gmail.com>
2017-03-03 00:27:02 +0100

    klient: add internal upload command

--- go/src/koding/klient/app/klient.go
+++ go/src/koding/klient/app/klient.go
@@ -160,6 +160,7 @@ type KlientConfig struct {
 	LogBucketRegion   string
 	LogBucketName     string
 	LogUploadInterval time.Duration
+	LogLevel          kite.Level
 
 	Metadata     string
 	MetadataFile string
@@ -247,12 +248,6 @@ func NewKlient(conf *KlientConfig) (*Klient, error) {
 	}
 
 	k := newKite(conf)
-	k.Config.VerifyAudienceFunc = verifyAudience
-
-	if k.Config.KontrolURL == "" || k.Config.KontrolURL == "http://127.0.0.1:3000/kite" ||
-		!konfig.Konfig.Endpoints.Kontrol().Equal(konfig.Builtin.Endpoints.Kontrol()) {
-		k.Config.KontrolURL = konfig.Konfig.Endpoints.Kontrol().Public.String()
-	}
 
 	term := terminal.New(k.Log, conf.ScreenrcPath, usg.Reset)
 
@@ -845,6 +840,20 @@ func (k *Klient) Close() {
 	k.kite.Close()
 }
 
+// NewUploader creates new uploader value from the given klient configuration.
+func NewUploader(kconf *KlientConfig) *uploader.Uploader {
+	k := newKite(kconf)
+	k.SetLogLevel(kite.ERROR)
+
+	return uploader.New(&uploader.Options{
+		KeygenURL: konfig.Konfig.Endpoints.Kloud().Public.String(),
+		Kite:      k,
+		Bucket:    kconf.logBucketName(),
+		Region:    kconf.logBucketRegion(),
+		Log:       k.Log,
+	})
+}
+
 func newKite(kconf *KlientConfig) *kite.Kite {
 	k := kite.NewWithConfig(kconf.Name, kconf.Version, konfig.Konfig.KiteConfig())
 
@@ -860,6 +869,13 @@ func newKite(kconf *KlientConfig) *kite.Kite {
 	// replace kontrolURL if's being overidden
 	if kconf.KontrolURL != "" {
 		k.Config.KontrolURL = kconf.KontrolURL
+	}
+
+	k.Config.VerifyAudienceFunc = verifyAudience
+
+	if k.Config.KontrolURL == "" || k.Config.KontrolURL == "http://127.0.0.1:3000/kite" ||
+		!konfig.Konfig.Endpoints.Kontrol().Equal(konfig.Builtin.Endpoints.Kontrol()) {
+		k.Config.KontrolURL = konfig.Konfig.Endpoints.Kontrol().Public.String()
 	}
 
 	return k
--- go/src/koding/klient/main.go
+++ go/src/koding/klient/main.go
@@ -155,7 +155,7 @@ func realMain() int {
 	}
 
 	if len(f.Args()) != 0 {
-		if err := handleInternalCommand(f.Arg(0)); err != nil {
+		if err := handleInternalCommand(conf, f.Arg(0), f.Args()[1:]...); err != nil {
 			log.Fatal(err)
 		}
 
@@ -173,7 +173,7 @@ func realMain() int {
 	return 0
 }
 
-func handleInternalCommand(cmd string) (err error) {
+func handleInternalCommand(conf *app.KlientConfig, cmd string, args ...string) (err error) {
 	// The following commands are intended for internal use
 	// only. They are used by kloud to install klient
 	// where no kd is available.
@@ -198,6 +198,8 @@ func handleInternalCommand(cmd string) (err error) {
 		}
 
 		err = klientsvc.Start()
+	case "upload":
+		err = app.Upload(app.NewUploader(conf), args...)
 	default:
 		return fmt.Errorf("unrecognized command: %s", cmd)
 	}
