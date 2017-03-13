33cc07bf15d42d623a450257fbd1b0a0fdf794ea
Rafal Jeczalik <rjeczalik@gmail.com>
2017-03-13 21:35:21 +0100

    kd: alias machine commands

--- go/src/koding/klientctl/main.go
+++ go/src/koding/klientctl/main.go
@@ -520,6 +520,15 @@ func run(args []string) {
 		},
 	}
 
+	// Alias commands.
+	app.Commands = append(app.Commands,
+		find(app.Commands, "machine", "list"),
+		find(app.Commands, "machine", "ssh"),
+		find(app.Commands, "machine", "mount"),
+		find(app.Commands, "machine", "umount"),
+		find(app.Commands, "machine", "exec"),
+	)
+
 	if experimental {
 		app.Commands = append(app.Commands,
 			cli.Command{
@@ -765,4 +774,25 @@ func ExitWithMessage(f ExitingWithMessageCommand, log logging.Logger, cmd string
 
 		return nil
 	}
+}
+
+func find(cmds cli.Commands, names ...string) cli.Command {
+	last := len(names) - 1
+
+	for _, name := range names[:last] {
+		for _, cmd := range cmds {
+			if cmd.Name == name {
+				cmds = cmd.Subcommands
+				break
+			}
+		}
+	}
+
+	for _, cmd := range cmds {
+		if cmd.Name == names[last] {
+			return cmd
+		}
+	}
+
+	return cli.Command{}
 }
