b55a1fe0ef873a5445aaf068e64d024337fe86a6
Sonmez Kartal <sonmez@koding.com>
2017-03-21 22:40:44 +0300

    Install misspell
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/gometalinter.sh
+++ scripts/gometalinter.sh
@@ -6,6 +6,7 @@ export GOPATH=${GOPATH:-$(pwd)/go} \
        GOBIN=${GOBIN:-$GOPATH/bin}
 
 go get github.com/gordonklaus/ineffassign
+go get github.com/client9/misspell/cmd/misspell
 go get gopkg.in/alecthomas/gometalinter.v1/...
 
 $GOBIN/gometalinter.v1 --config="$GOPATH/src/.gometalinter.json" \
