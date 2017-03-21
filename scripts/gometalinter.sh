f999dbfd3e20fdaa1f00b3d418de477917327597
Sonmez Kartal <sonmez@koding.com>
2017-03-21 22:40:25 +0300

    Install ineffassign
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/gometalinter.sh
+++ scripts/gometalinter.sh
@@ -5,6 +5,7 @@ set -o errexit
 export GOPATH=${GOPATH:-$(pwd)/go} \
        GOBIN=${GOBIN:-$GOPATH/bin}
 
+go get github.com/gordonklaus/ineffassign
 go get gopkg.in/alecthomas/gometalinter.v1/...
 
 $GOBIN/gometalinter.v1 --config="$GOPATH/src/.gometalinter.json" \
