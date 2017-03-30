5ad28e5f718e937cb89cf706413867f37fab5c5c
Sinan Yasar <sinan@koding.com>
2017-03-30 14:15:25 -0700

    minor fixes

--- client/analytics/lib/routehandler.js
+++ client/analytics/lib/routehandler.js
@@ -2,7 +2,7 @@ import kd from 'kd'
 import lazyrouter from 'app/lazyrouter'
 
 export default function () {
-  return lazyrouter.bind('analytics', (type, info, state, path, ctx) => {
-    return kd.singletons.appManager.open('Analytics')
+  lazyrouter.bind('analytics', (type, info, state, path, ctx) => {
+    kd.singletons.appManager.open('Analytics')
   })
 }
