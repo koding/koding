64051000ff4ec3a9c8b2ec2263aa9f057e13c45a
Rafal Jeczalik <rjeczalik@gmail.com>
2017-04-03 16:04:55 +0200

    kloud/metadata: add inline doc

--- go/src/koding/kites/kloud/metadata/metadata.go
+++ go/src/koding/kites/kloud/metadata/metadata.go
@@ -10,7 +10,11 @@ import (
 //go:generate $GOPATH/bin/go-bindata -mode 420 -modtime 1470666525 -pkg metadata -o provision.sh.go provision.sh
 //go:generate gofmt -l -w -s provision.sh.go
 
-// TODO(rjeczalik): make it configurable
+// Default asset URLs used by kloud when deploying klient.
+//
+// They are being downloaded on a remote vm during provisioning.
+//
+// TODO(rjeczalik): make it configurable during ./configure = add to kites/config/config.json
 var (
 	DefaultEntrypointBaseURL = "https://koding-klient.s3.amazonaws.com/entrypoint"
 	DefaultScreenURL         = "https://koding-dl.s3.amazonaws.com/screen.tar.gz"
@@ -27,13 +31,23 @@ func mustAsset(file string) []byte {
 	return p
 }
 
+// Config is used to build cloud-init from user_data
+// in order to install klient service on a remote vm
+// and authenticate and connect to Koding.
 type Config struct {
-	Konfig   *config.Konfig
-	KiteKey  string
-	Userdata string
-	Debug    bool
+	Konfig   *config.Konfig // Koding endpoints configuration
+	KiteKey  string         // KiteKey used by klient to authenticate with Koding
+	Userdata string         // User script to run after provisioning, if any
+	Debug    bool           // Whether klient should be started in debug mode; configurable by debug field in stack template
 }
 
+// New builds new cloud-init for the given configuration.
+//
+// If cfg.Userdata is a cloud-init file, it will be
+// merged back into kloud's one. If there are conflicts
+// between the two cloud-init scripts, stack build will
+// fail - we do not allow user to overwrite kloud's
+// cloud-init.
 func New(cfg *Config) (CloudInit, error) {
 	metadata, err := newMetadata(cfg)
 	if err != nil {
