d9c6ee57be7160b121419f999b7a5af449b01e2b
Rafal Jeczalik <rjeczalik@gmail.com>
2017-04-04 00:08:12 +0200

    kloud/metadata: ensure /var/log/klient.log is owned by user

--- go/src/koding/kites/kloud/metadata/provision.sh
+++ go/src/koding/kites/kloud/metadata/provision.sh
@@ -19,6 +19,7 @@ mkdir -p /opt/kite/klient
 curl --location --silent --show-error --retry 5 "$${KLIENT_URL}" --output /tmp/klient.gz
 curl --location --silent --show-error --retry 5 "$${SCREEN_URL}" --output /tmp/screen.tar.gz
 
+touch /var/log/klient.log
 tar -C / -xf /tmp/screen.tar.gz
 
 # TODO(rjeczalik): Move the below to klient (altogether with installing screen).
@@ -41,5 +42,6 @@ fi
 gzip --decompress --force --stdout /tmp/klient.gz > /opt/kite/klient/klient
 chmod +x /opt/kite/klient/klient
 chown -R "$${KODING_USERNAME}:$${KODING_USERNAME}" /opt/kite/klient
+chown "$${KODING_USERNAME}:$${KODING_USERNAME}" /var/log/klient.log
 
 /opt/kite/klient/klient -metadata-user "$${KODING_USERNAME}" -metadata-file /var/lib/koding/metadata.json run
