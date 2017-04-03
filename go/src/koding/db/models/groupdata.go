ccc51738f4e928b8dd1209f05f34dd330e4f4a59
Cihangir SAVAS <cihangir@koding.com>
2017-04-03 16:15:00 +0300

    go/models: fix bson tags

--- go/src/koding/db/models/groupdata.go
+++ go/src/koding/db/models/groupdata.go
@@ -1,8 +1,10 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
 // GroupData holds a group's private info.
 type GroupData struct {
-	ID   string `json:"_id"`
-	Slug string `json:"slug"`
-	Data *Data  `bson:"data,omitempty" json:"data,omitempty"`
+	ID   bson.ObjectId `json:"_id" bson:"_id"`
+	Slug string        `json:"slug" bson:"slug"`
+	Data *Data         `json:"data,omitempty" bson:"data,omitempty"`
 }
