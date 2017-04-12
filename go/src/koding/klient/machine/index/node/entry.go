30b49feb77e6e487e01a37ea4ab5f6ee81a81646
Pawel Knap <pawelknap88@gmail.com>
2017-04-12 20:47:35 +0200

    klient/machine: remove synchronization primitives from node.Entry

--- go/src/koding/klient/machine/index/node/entry.go
+++ go/src/koding/klient/machine/index/node/entry.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"sync/atomic"
 	"time"
 
 	"github.com/djherbis/times"
@@ -44,22 +43,40 @@ func (ep EntryPromise) String() string {
 	return string(buf[:len(epMapping)])
 }
 
-// realEntry describes the part of entry which is independent of underlying file
+// Has checks if provided promise is set.
+func (ep EntryPromise) HasPromise(promise EntryPromise) bool {
+	return ep&promise == promise
+}
+
+// Deleted checks if the promise is set to be deleted.
+func (ep EntryPromise) Deleted() bool {
+	const del = EntryPromiseDel | EntryPromiseUnlink
+
+	return ep&del != 0
+}
+
+// Swap flips the value of a promise field, setting the set bits and
+// unsetting the unset ones.
+func (ep *EntryPromise) Swap(set, unset EntryPromise) {
+	*ep = (*ep | set) &^ unset
+}
+
+// File describes the part of entry which is independent of underlying file
 // system. The fields of this type can be stored on disk and transferred over
-// network.
-type realEntry struct {
+// the network.
+type File struct {
 	CTime int64       `json:"c"` // Metadata change time since EPOCH.
 	MTime int64       `json:"m"` // File data change time since EPOCH.
 	Size  int64       `json:"s"` // Size of the file.
 	Mode  os.FileMode `json:"o"` // File mode and permission bits.
 }
 
-// virtualEntry stores virtual file system dependent data that is lost during
+// Virtual stores virtual file system dependent data that is lost during
 // serialization and should be recreated by VFS which manages the entries.
-type virtualEntry struct {
-	inode    uint64       // Inode ID of a mounted file.
-	refCount int32        // Reference count of file handlers.
-	promise  EntryPromise // Metadata of files's memory state.
+type Virtual struct {
+	Inode    uint64       // Inode ID of a mounted file.
+	RefCount int32        // Reference count of file handlers.
+	Promise  EntryPromise // Metadata of files's memory state.
 }
 
 var (
@@ -69,8 +86,8 @@ var (
 
 // Entry represents a single file registered to index.
 type Entry struct {
-	real    realEntry
-	virtual virtualEntry
+	File    File
+	Virtual Virtual
 }
 
 // NewEntry creates a new entry that describes the wile with specified size and
@@ -93,7 +110,7 @@ func NewEntryFileInfo(info os.FileInfo) *Entry {
 // NewEntryTime creates a new entry with custom file change and modify times.
 func NewEntryTime(ctime, mtime, size int64, mode os.FileMode) *Entry {
 	return &Entry{
-		real: realEntry{
+		File: File{
 			CTime: ctime,
 			MTime: mtime,
 			Size:  size,
@@ -117,16 +134,11 @@ func NewEntryFile(path string) (*Entry, error) {
 // RefCount field is ignored and set to 0.
 func (e *Entry) Clone() *Entry {
 	return &Entry{
-		real: realEntry{
-			CTime: e.CTime(),
-			MTime: e.MTime(),
-			Size:  e.Size(),
-			Mode:  e.Mode(),
-		},
-		virtual: virtualEntry{
-			inode:    e.Inode(),
-			refCount: 0,
-			promise:  e.Promise(),
+		File: e.File,
+		Virtual: Virtual{
+			Inode:    e.Virtual.Inode,
+			RefCount: 0,
+			Promise:  e.Virtual.Promise,
 		},
 	}
 }
@@ -136,147 +148,50 @@ func (e *Entry) Clone() *Entry {
 //
 // RefCount and Promise fields are ignored.
 func (e *Entry) MergeIn(f *Entry) {
-	if t := f.CTime(); t != 0 {
-		e.SetCTime(t)
+	if t := f.File.CTime; t != 0 {
+		e.File.CTime = t
 	}
-	if t := f.MTime(); t != 0 {
-		e.SetMTime(t)
+	if t := f.File.MTime; t != 0 {
+		e.File.MTime = t
 	}
-	if n := f.Size(); n != 0 {
-		e.SetSize(n)
+	if n := f.File.Size; n != 0 {
+		e.File.Size = n
 	}
-	if m := f.Mode(); m != 0 {
-		e.SetMode(m)
+	if m := f.File.Mode; m != 0 {
+		e.File.Mode = m
 	}
-	if n := f.Inode(); n != 0 {
-		e.SetInode(n)
-	}
-}
 
-// CTime atomically gets entry change time in UNIX nano format.
-func (e *Entry) CTime() int64 {
-	return atomic.LoadInt64(&e.real.CTime)
-}
-
-// SetCTime atomically sets entry's change time. Time must be in UNIX nano format.
-func (e *Entry) SetCTime(nano int64) {
-	atomic.StoreInt64(&e.real.CTime, nano)
-}
-
-// MTime atomically gets entry modification time in UNIX nano format.
-func (e *Entry) MTime() int64 {
-	return atomic.LoadInt64(&e.real.MTime)
-}
-
-// SetMTime atomically sets entry's modification time. Time must be in UNIX nano
-// format.
-func (e *Entry) SetMTime(nano int64) {
-	atomic.StoreInt64(&e.real.MTime, nano)
-}
-
-// Size atomically gets entry size in bytes.
-func (e *Entry) Size() int64 {
-	return atomic.LoadInt64(&e.real.Size)
-}
-
-// SetSize atomically sets entry's size. The size is provided in bytes.
-func (e *Entry) SetSize(size int64) {
-	atomic.StoreInt64(&e.real.Size, size)
-}
-
-// Mode atomically gets entry file mode.
-func (e *Entry) Mode() os.FileMode {
-	return os.FileMode(atomic.LoadUint32((*uint32)(&e.real.Mode)))
-}
-
-// SetMode atomically sets entry file mode.
-func (e *Entry) SetMode(mode os.FileMode) {
-	atomic.StoreUint32((*uint32)(&e.real.Mode), (uint32)(mode))
-}
-
-// Inode gets virtual file system inode.
-func (e *Entry) Inode() uint64 {
-	return atomic.LoadUint64(&e.virtual.inode)
-}
-
-// SetInode sets virtual file system inode.
-func (e *Entry) SetInode(inode uint64) {
-	atomic.StoreUint64(&e.virtual.inode, inode)
-}
-
-// RefCount returns the current state of Entry's reference counter.
-func (e *Entry) RefCount() int32 {
-	return atomic.LoadInt32(&e.virtual.refCount)
-}
-
-// IncRefCount increments entry reference counter.
-func (e *Entry) IncRefCount() int32 {
-	return atomic.AddInt32(&e.virtual.refCount, 1)
-}
-
-// DecRefCount decrements entry reference counter.
-func (e *Entry) DecRefCount() int32 {
-	return atomic.AddInt32(&e.virtual.refCount, -1)
-}
-
-// Promise returns Entry's promise state.
-func (e *Entry) Promise() EntryPromise {
-	return EntryPromise(atomic.LoadUint32((*uint32)(&e.virtual.promise)))
-}
-
-// HasPromise checks if provider promise is set in given entry.
-func (e *Entry) HasPromise(promise EntryPromise) bool {
-	return EntryPromise(atomic.LoadUint32((*uint32)(&e.virtual.promise)))&promise == promise
-}
-
-// Deleted checks if the entry is promised to be deleted.
-func (e *Entry) Deleted() bool {
-	return e.HasPromise(EntryPromiseDel) || e.HasPromise(EntryPromiseUnlink)
-}
-
-// SwapPromise flips the value of a promise field, setting the set bits and
-// unsetting the unset ones. This function is thread safe.
-func (e *Entry) SwapPromise(set, unset EntryPromise) EntryPromise {
-	for {
-		older := atomic.LoadUint32((*uint32)(&e.virtual.promise))
-		updated := (older | uint32(set)) &^ uint32(unset)
-
-		if atomic.CompareAndSwapUint32((*uint32)(&e.virtual.promise), older, updated) {
-			return EntryPromise(updated)
-		}
+	if n := f.Virtual.Inode; n != 0 {
+		e.Virtual.Inode = n
 	}
 }
 
-// String implements fmt.Stringer interface. It pretty prints entry.
+// String implements fmt.Stringer interface. It pretty prints the entry.
 func (e *Entry) String() string {
 	var (
-		ctime = time.Unix(0, e.CTime())
-		mtime = time.Unix(0, e.MTime())
+		ctime = time.Unix(0, e.File.CTime)
+		mtime = time.Unix(0, e.File.MTime)
 	)
 
 	return fmt.Sprintf("[INODE %d, REFS %d, PROMISE %s][CTIME %s, MTIME %s, SIZE %d, MODE %s]",
-		e.Inode(), e.RefCount(), e.Promise(),
-		ctime.Format(time.StampMilli), mtime.Format(time.StampMilli), e.Size(), e.Mode(),
+		e.Virtual.Inode, e.Virtual.RefCount, e.Virtual.Promise,
+		ctime.Format(time.StampMilli), mtime.Format(time.StampMilli), e.File.Size, e.File.Mode,
 	)
 }
 
-// MarshalJSON satisfies json.Marshaler interface. It safely marshals entry
-// real data to JSON format.
+// MarshalJSON satisfies json.Marshaler interface. It marshals only the entry
+// file data to JSON format.
 func (e *Entry) MarshalJSON() ([]byte, error) {
-	real := realEntry{
-		CTime: e.CTime(),
-		MTime: e.MTime(),
-		Size:  e.Size(),
-		Mode:  e.Mode(),
-	}
-
-	return json.Marshal(real)
+	return json.Marshal(e.File)
 }
 
-// UnmarshalJSON satisfies json.Unmarshaler interface. It is used to unmarshal
-// data into private entry fields.
+// UnmarshalJSON satisfies json.Unmarshaler interface.
 func (e *Entry) UnmarshalJSON(data []byte) error {
-	return json.Unmarshal(data, &e.real)
+	if err := json.Unmarshal(data, &e.File); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getPowerOf2(i uint64) (count int) {
