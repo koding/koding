9da6a4f3df273f1d7378756830a0b9442b0b6d5e
Pawel Knap <pawelknap88@gmail.com>
2017-04-27 23:02:00 +0200

    klient/machine: do not include .Trash directory to index on darwin

--- go/src/koding/klient/machine/mount/sync.go
+++ go/src/koding/klient/machine/mount/sync.go
@@ -113,6 +113,10 @@ type Sync struct {
 	iu  *IdxUpdate   // local index updater.
 }
 
+func (s *Sync) Idx() *index.Index {
+	return s.idx
+}
+
 // NewSync creates a new sync instance for a given mount. It ensures basic mount
 // directory structure. This function is blocking.
 func NewSync(mountID ID, m Mount, opts Options) (*Sync, error) {
