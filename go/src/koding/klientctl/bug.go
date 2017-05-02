f90f02026694baa2eeecef30566799d8a6dfb503
Rafal Jeczalik <rjeczalik@gmail.com>
2017-05-02 17:47:08 +0200

    kd/bug: reuse external issue template

--- go/src/koding/klientctl/bug.go
+++ go/src/koding/klientctl/bug.go
@@ -20,6 +20,8 @@ import (
 	"github.com/skratchdot/open-golang/open"
 )
 
+//go:generate $GOPATH/bin/go-bindata -mode 420 -modtime 1475345133 -pkg main -prefix ../../../../.github/ -o issue.md.go ../../../../.github/ISSUE_TEMPLATE.md
+
 type BugMetadata struct {
 	KiteID      string                  `json:"kiteID"`
 	Konfig      *config.Konfig          `json:"konfig"`
@@ -151,32 +153,4 @@ func signature(uri string) string {
 	return hex.EncodeToString([]byte(u.Path))
 }
 
-const issueBody = `<!--- Provide a general summary of the issue in the Title above -->
-
-## Signature
-` + "```\n%s\n```" + `
-
-## Expected Behavior
-<!--- If you're describing a bug, tell us what should happen -->
-<!--- If you're suggesting a change/improvement, tell us how it should work -->
-
-## Current Behavior
-<!--- If describing a bug, tell us what happens instead of the expected behavior -->
-<!--- If suggesting a change/improvement, explain the difference from current behavior -->
-
-## Possible Solution
-<!--- Not obligatory, but suggest a fix/reason for the bug, -->
-<!--- or ideas how to implement the addition or change -->
-
-## Steps to Reproduce (for bugs)
-<!--- Provide a link to a live example, or an unambiguous set of steps to -->
-<!--- reproduce this bug. Include code to reproduce, if relevant -->
-1.
-2.
-3.
-4.
-
-## Context
-<!--- How has this issue affected you? What are you trying to accomplish? -->
-<!--- Providing context helps us come up with a solution that is most useful in the real world -->
-`
+var issueBody = string(append(MustAsset("ISSUE_TEMPLATE.md"), []byte("\n### Signature\n```\n%s\n```\n")...))
