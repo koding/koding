b9eb0f1e5a4b56aff61ccba75ac5d3b9946608bb
Cihangir SAVAS <cihangir@koding.com>
2017-05-03 17:02:52 +0300

    kloud/metrics: add handler for collecting metrics into kloud

---
+++ go/src/koding/kites/kloud/metrics/metrics.go
@@ -0,0 +1,64 @@
+package metrics
+
+import (
+	"io"
+	"net"
+
+	"github.com/koding/kite"
+)
+
+// Publisher sends DD events to agent.
+type Publisher struct {
+	conn io.WriteCloser
+}
+
+// NewPublisherWithConn creates new Publisher for sending the incoming events to
+// DataDog agent.
+func NewPublisherWithConn(conn io.WriteCloser) *Publisher {
+	return &Publisher{
+		conn: conn,
+	}
+}
+
+// NewPublisher creates a new publisher with default DD agent address.
+func NewPublisher() (*Publisher, error) {
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8125")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewPublisherWithConn(conn), nil
+}
+
+// PublishRequest represents a request type for "metrics.publish" kloud's
+// kite method.
+type PublishRequest struct {
+	Data GzippedPayload `json:"data"`
+}
+
+// Publish is a kite.Handler for "metrics.publish" kite method.
+func (p *Publisher) Publish(r *kite.Request) (interface{}, error) {
+	var req PublishRequest
+	if err := r.Args.One().Unmarshal(&req); err != nil {
+		return nil, err
+	}
+
+	for _, data := range req.Data {
+		_, err := p.conn.Write(data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, nil
+}
+
+// Close closes the underlying connection.
+func (p *Publisher) Close() error {
+	return p.conn.Close()
+}
