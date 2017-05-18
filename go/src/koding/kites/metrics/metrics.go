2661d1393cdedd9b15d6506d69eb7922e95e39e5
Cihangir SAVAS <cihangir@koding.com>
2017-05-17 21:47:36 -0700

    kites/metrics: if error happens while trying to open bolt connection, fallback to inmemory

--- go/src/koding/kites/metrics/metrics.go
+++ go/src/koding/kites/metrics/metrics.go
@@ -12,11 +12,14 @@ import (
 
 // Metrics wraps metric
 type Metrics struct {
-	bolt    *BoltQueue
+	storage Storage
 	Datadog *statsd.Client
 }
 
 // New creates new metrics collector.
+//
+// It will try to store the metrics in best available storage. First will try
+// storing in BoltDb but if opening fails, will fallback to in memory storage.
 func New(app string) (*Metrics, error) {
 	boltPath := filepath.Join(kitesconfig.KodingHome(), "metrics.bolt")
 
@@ -24,42 +27,46 @@ func New(app string) (*Metrics, error) {
 }
 
 // NewWithPath creates new metrics collector with storing data to given path.
+// See New func.
 func NewWithPath(boltPath, app string) (*Metrics, error) {
-	boltConn, err := NewBoltQueue(boltPath)
+	var storage Storage
+	var err error
+	storage, err = NewBoltStorage(boltPath)
 	if err != nil {
-		return nil, err
+		storage = newInMemStorage()
 	}
 
-	return NewWithBoltQueue(boltConn, app)
+	return NewWithStorage(storage, app)
 }
 
-// NewWithDB creates a new metric collector with given bolt db instance.
+// NewWithDB creates a new metric collector with given bolt db instance. See New
+// func.
 func NewWithDB(db *bolt.DB, app string) (*Metrics, error) {
-	boltConn, err := NewBoltQueueWithDB(db)
+	storage, err := NewBoltStorageWithDB(db)
 	if err != nil {
 		return nil, err
 	}
 
-	return NewWithBoltQueue(boltConn, app)
+	return NewWithStorage(storage, app)
 }
 
-// NewWithBoltQueue creates a new metric collector with given bolt queue.
-func NewWithBoltQueue(bq *BoltQueue, app string) (*Metrics, error) {
-	dd, err := statsd.NewWithConn(bq)
+// NewWithStorage creates a new metric collector with given bolt storage.
+func NewWithStorage(q Storage, app string) (*Metrics, error) {
+	dd, err := statsd.NewWithConn(q)
 	if err != nil {
 		return nil, err
 	}
 
 	dd.Namespace = app + "_"
 	return &Metrics{
-		bolt:    bq,
+		storage: q,
 		Datadog: dd,
 	}, nil
 }
 
 // Process gets the records and deletes them after operation.
 func (m *Metrics) Process(f OperatorFunc) error {
-	_, err := m.bolt.ConsumeN(-1, f)
+	_, err := m.storage.ConsumeN(-1, f)
 	return err
 }
 
@@ -70,7 +77,7 @@ func (m *Metrics) ProcessContext(ctx context.Context, n int, f OperatorFunc) err
 		case <-ctx.Done():
 			return ctx.Err()
 		default:
-			processedCount, err := m.bolt.ConsumeN(n, f)
+			processedCount, err := m.storage.ConsumeN(n, f)
 			if err != nil {
 				return err
 			}
@@ -89,8 +96,8 @@ func (m *Metrics) Close() error {
 		return nil
 	}
 
-	if m.bolt != nil {
-		return m.bolt.Close()
+	if m.storage != nil {
+		return m.storage.Close()
 	}
 
 	return nil
