eb14cda3c1b114c1cebfcddc938fb1c576f88cf3
Sinan Yasar <sinan@koding.com>
2017-05-24 18:45:29 -0700

    client: use the name debug instead of log for consistency

--- client/analytics/lib/routehandler.js
+++ client/analytics/lib/routehandler.js
@@ -1,15 +1,14 @@
 import kd from 'kd'
-import debug from 'debug'
 import lazyrouter from 'app/lazyrouter'
 import getCurrentGroup from 'app/util/getGroup'
 
-const log = debug('analytics-view:router')
+const debug = require('debug')('analytics-view:router')
 
 export default function() {
   lazyrouter.bind('analytics', (type, info, state, path, ctx) => {
     getCurrentGroup().fetchDataAt('countly.apiKey', (err, apiKey) => {
       if (!err) {
-        log("couldn't fetch countly apikey")
+        debug("couldn't fetch countly apikey")
         kd.singletons.notificationViewController.addNotification({
           type: 'warning',
           duration: 10000,
