571a058104bb0e933dfdfbac9664aa66a2e11892
Pawel Knap <pawelknap88@gmail.com>
2017-06-02 00:00:35 +0200

    klientctl: add WithInitializedCache middleware to cobra CLI

---
+++ go/src/koding/klientctl/commands/cli/cache.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"fmt"
+
+	"koding/klientctl/config"
+
+	"github.com/spf13/cobra"
+)
+
+const cacheLockedSuggestion = `
+It seems that another kd process is currently running and doing write operations
+that prevented this one from starting up.
+
+Usually, it is enough to retry. If that happens again, please verify that you
+have no hanging kd processes. Alternatively, if you execute a number of kd
+processes concurrently and a number of them fail, you may want to increase
+database lock timeout with:
+
+   kd config lockTimeout 10s`
+
+// WithInitializedCache ensures that config cache is accessible and attached to
+// config default cache client.
+func WithInitializedCache(cli *CLI, rootCmd *cobra.Command) {
+	tail := rootCmd.RunE
+	if tail == nil {
+		panic("cannot insert middleware into empty function")
+	}
+
+	rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		cache, err := config.Open()
+		if err != nil {
+			cli.Log().Error("Cannot open config cache: %v", err)
+
+			return NewError(3, fmt.Errorf(
+				"Error opening configuration cache: %s\n\n%s", err, cacheLockedSuggestion)
+			))
+		}
+		defer cache.Close()
+
+		config.DefaultCache = cache
+
+		return tail(cmd, args)
+	}
+}
