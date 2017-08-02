9a21ee69091ca303f87f18a9764e2077f80fe846
Gokmen Goksel <gokmen@goksel.me>
2017-08-01 19:20:06 -0700

    CheckNodeVersion: support node versions greater than equal 6

--- scripts/check-node-version.sh
+++ scripts/check-node-version.sh
@@ -4,12 +4,12 @@ VERSION=$(node --version | sed -e 's/^v//')
 
 while IFS=".", read MAJOR MINOR REVISION; do
 	MISMATCH=1
-	if [[ $MAJOR -eq 6 ]]; then
+	if [[ $MAJOR -ge 6 ]]; then
 		MISMATCH=
 	fi
 done < <(echo $VERSION)
 
 if [[ -n "$MISMATCH" ]]; then
-	echo "error: node version must be 6"
+	echo "error: node version must be > 6"
 	exit 1
 fi
